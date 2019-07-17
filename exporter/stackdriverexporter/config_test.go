2806371762879c3e0c5e3b446a71fcd6183592ec
songy23 <songya@google.com>
2019-07-16 17:26:48 -0700

    Add Stackdriver Exporter and configs.
    Fixes #6.

---
+++ exporter/stackdriverexporter/config_test.go
@@ -0,0 +1,52 @@
+// Copyright 2019, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package stackdriverexporter
+
+import (
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/open-telemetry/opentelemetry-service/config"
+	"github.com/open-telemetry/opentelemetry-service/config/configmodels"
+	"github.com/open-telemetry/opentelemetry-service/exporter"
+)
+
+var _ = config.RegisterTestFactories()
+
+func TestLoadConfig(t *testing.T) {
+
+	factory := exporter.GetFactory(typeStr)
+
+	cfg, err := config.LoadConfigFile(t, path.Join(".", "testdata", "config.yaml"))
+
+	require.NoError(t, err)
+	require.NotNil(t, cfg)
+
+	assert.Equal(t, len(cfg.Exporters), 2)
+
+	r0 := cfg.Exporters["stackdriver"]
+	assert.Equal(t, r0, factory.CreateDefaultConfig())
+
+	r1 := cfg.Exporters["stackdriver/customname"].(*Config)
+	assert.Equal(t, r1.ExporterSettings,
+		configmodels.ExporterSettings{
+			TypeVal: typeStr,
+			NameVal: "stackdriver/customname",
+			Enabled: true,
+		})
+}
