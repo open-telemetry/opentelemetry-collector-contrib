1bc956f705d421e049e0f8658e1be7e39e979397
Aliaksei Urbanski <mimworkmail@gmail.com>
2019-11-07 03:11:45 +0300

    Add test coverage collecting (#128)

---
+++ scripts/coverage.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+function cov {
+    pytest \
+        --ignore-glob=*/setup.py \
+        --cov ${1} \
+        --cov-append \
+        --cov-branch \
+        --cov-report='' \
+        ${1}
+}
+
+
+coverage erase
+
+cov opentelemetry-api
+cov opentelemetry-sdk
+cov ext/opentelemetry-ext-http-requests
+cov ext/opentelemetry-ext-jaeger
+cov ext/opentelemetry-ext-opentracing-shim
+cov ext/opentelemetry-ext-wsgi
+cov examples/opentelemetry-example-app
+
+coverage report
+coverage xml
