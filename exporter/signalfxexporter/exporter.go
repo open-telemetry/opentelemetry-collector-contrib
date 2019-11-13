186166d10bb18bdc092bd13f0331a25af119d0b7
Paulo Janotti <paulo@omnition.io>
2019-11-13 08:00:01 -0800

    Add a SignalFx exporter
    Create a SignalFx exporter, at this stage it only supports metrics.

---
+++ exporter/signalfxexporter/exporter.go
@@ -0,0 +1,220 @@
+// Copyright 2019, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package signalfxexporter
+
+import (
+	"bytes"
+	"compress/gzip"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/open-telemetry/opentelemetry-collector/consumer/consumerdata"
+	"github.com/open-telemetry/opentelemetry-collector/consumer/consumererror"
+	"github.com/open-telemetry/opentelemetry-collector/exporter"
+	"github.com/open-telemetry/opentelemetry-collector/exporter/exporterhelper"
+	sfxpb "github.com/signalfx/com_signalfx_metrics_protobuf"
+	"go.uber.org/zap"
+)
+
+// New returns a new SignalFx exporter.
+func New(
+	config *Config,
+	logger *zap.Logger,
+) (exporter.MetricsExporter, error) {
+
+	if config == nil {
+		return nil, errors.New("nil config")
+	}
+
+	if config.Name() == "" {
+		config.SetType(typeStr)
+		config.SetName(typeStr)
+	}
+
+	if config.Realm == "" && config.URL == "" {
+		err := fmt.Errorf(
+			"%q config requires a non-empty \"realm\" or \"url\"",
+			config.Name())
+		return nil, err
+	}
+
+	if config.Realm != "" && config.URL != "" {
+		err := fmt.Errorf(
+			"%q \"realm\" and \"url\" cannot be specified at the same time",
+			config.Name())
+		return nil, err
+	}
+
+	var url string
+	if config.URL != "" {
+		url = config.URL
+	} else {
+		url = fmt.Sprintf(
+			"https://ingest.%s.signalfx.com/v2/datapoint", config.Realm)
+	}
+
+	if config.Timeout == 0 {
+		config.Timeout = 5 * time.Second
+	}
+
+	if config.Timeout < 0 {
+		err := fmt.Errorf(
+			"%q config cannot have a negative \"timeout\"",
+			config.Name())
+		return nil, err
+	}
+
+	headers, err := buildHeaders(config)
+	if err != nil {
+		return nil, err
+	}
+
+	s := &httpSender{
+		url:     url,
+		headers: headers,
+		client: &http.Client{
+			// TODO: What other settings of http.Client to expose via config?
+			//  Or what others change from default values?
+			Timeout: config.Timeout,
+		},
+		logger: logger,
+		zippers: sync.Pool{New: func() interface{} {
+			return gzip.NewWriter(nil)
+		}},
+	}
+
+	exp, err := exporterhelper.NewMetricsExporter(
+		&config.ExporterSettings,
+		s.pushMetricsData,
+		exporterhelper.WithTracing(true),
+		exporterhelper.WithMetrics(true))
+
+	return exp, err
+}
+
+// httpSender sends the data to the SignalFx backend.
+type httpSender struct {
+	url     string
+	headers map[string]string
+	client  *http.Client
+	logger  *zap.Logger
+	zippers sync.Pool
+}
+
+func (s *httpSender) pushMetricsData(
+	ctx context.Context,
+	md consumerdata.MetricsData,
+) (droppedTimeSeries int, err error) {
+
+	sfxDataPoints, numDroppedTimeseries, err := metricDataToSingalFxV2(s.logger, md)
+	if err != nil {
+		return exporterhelper.NumTimeSeries(md), consumererror.Permanent(err)
+	}
+
+	body, compressed, err := s.encodeBody(sfxDataPoints)
+	if err != nil {
+		return exporterhelper.NumTimeSeries(md), consumererror.Permanent(err)
+	}
+
+	req, err := http.NewRequest("POST", s.url, body)
+	if err != nil {
+		return exporterhelper.NumTimeSeries(md), consumererror.Permanent(err)
+	}
+
+	for k, v := range s.headers {
+		req.Header.Set(k, v)
+	}
+
+	if compressed {
+		req.Header.Set("Content-Encoding", "gzip")
+	}
+
+	resp, err := s.client.Do(req)
+	if err != nil {
+		return exporterhelper.NumTimeSeries(md), err
+	}
+
+	io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+
+	// SignalFx accepts all 2XX codes.
+	if resp.StatusCode < http.StatusOK || resp.StatusCode >= http.StatusMultipleChoices {
+		err = fmt.Errorf(
+			"HTTP %d %q",
+			resp.StatusCode,
+			http.StatusText(resp.StatusCode))
+		return exporterhelper.NumTimeSeries(md), err
+	}
+
+	return numDroppedTimeseries, nil
+}
+
+func buildHeaders(config *Config) (map[string]string, error) {
+	headers := map[string]string{
+		"Connection":   "keep-alive",
+		"Content-Type": "application/x-protobuf",
+		"User-Agent":   "OpenTelemetry-Collector SignalFx Exporter/v0.0.1",
+	}
+
+	if config.AccessToken != "" {
+		headers["X-Sf-Token"] = config.AccessToken
+	}
+
+	// Add any custom headers from the config. They will override the pre-defined
+	// ones above in case of conflict, but, not the content encoding one since
+	// the latter one is defined according to the payload.
+	for k, v := range config.Headers {
+		headers[k] = v
+	}
+
+	return headers, nil
+}
+
+func (s *httpSender) encodeBody(dps []*sfxpb.DataPoint) (bodyReader io.Reader, compressed bool, err error) {
+	msg := &sfxpb.DataPointUploadMessage{
+		Datapoints: dps,
+	}
+	body, err := proto.Marshal(msg)
+	if err != nil {
+		return nil, false, err
+	}
+	return s.getReader(body)
+}
+
+// avoid attempting to compress things that fit into a single ethernet frame
+func (s *httpSender) getReader(b []byte) (io.Reader, bool, error) {
+	var err error
+	if len(b) > 1500 {
+		buf := new(bytes.Buffer)
+		w := s.zippers.Get().(*gzip.Writer)
+		defer s.zippers.Put(w)
+		w.Reset(buf)
+		_, err = w.Write(b)
+		if err == nil {
+			err = w.Close()
+			if err == nil {
+				return buf, true, nil
+			}
+		}
+	}
+	return bytes.NewReader(b), false, err
+}
---
+++ exporter/signalfxexporter/factory_test.go
@@ -0,0 +1,146 @@
+// Copyright 2019, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package signalfxexporter
+
+import (
+	"testing"
+	"time"
+
+	"github.com/open-telemetry/opentelemetry-collector/config/configcheck"
+	"github.com/open-telemetry/opentelemetry-collector/config/configerror"
+	"github.com/open-telemetry/opentelemetry-collector/config/configmodels"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+)
+
+func TestCreateDefaultConfig(t *testing.T) {
+	factory := Factory{}
+	cfg := factory.CreateDefaultConfig()
+	assert.NotNil(t, cfg, "failed to create default config")
+	assert.NoError(t, configcheck.ValidateConfig(cfg))
+}
+
+func TestCreateMetricsExporter(t *testing.T) {
+	factory := Factory{}
+	cfg := factory.CreateDefaultConfig()
+
+	assert.Equal(t, typeStr, factory.Type())
+	_, err := factory.CreateMetricsExporter(zap.NewNop(), cfg)
+	assert.NoError(t, err)
+}
+
+func TestCreateTraceExporter(t *testing.T) {
+	factory := Factory{}
+	cfg := factory.CreateDefaultConfig()
+	_, err := factory.CreateTraceExporter(zap.NewNop(), cfg)
+	assert.Equal(t, configerror.ErrDataTypeIsNotSupported, err)
+}
+
+func TestCreateInstanceViaFactory(t *testing.T) {
+	factory := Factory{}
+
+	cfg := factory.CreateDefaultConfig()
+	exp, err := factory.CreateMetricsExporter(
+		zap.NewNop(),
+		cfg)
+	assert.NoError(t, err)
+	assert.NotNil(t, exp)
+
+	// Set values that don't have a valid default.
+	expCfg := cfg.(*Config)
+	expCfg.AccessToken = "testToken"
+	expCfg.Realm = "us1"
+	exp, err = factory.CreateMetricsExporter(
+		zap.NewNop(),
+		cfg)
+	assert.NoError(t, err)
+	require.NotNil(t, exp)
+
+	assert.NoError(t, exp.Shutdown())
+}
+
+func TestFactory_CreateMetricsExporter(t *testing.T) {
+	f := &Factory{}
+	config := &Config{
+		ExporterSettings: configmodels.ExporterSettings{
+			TypeVal: typeStr,
+			NameVal: typeStr,
+		},
+		AccessToken: "testToken",
+		Realm:       "us1",
+		Headers: map[string]string{
+			"added-entry": "added value",
+			"dot.test":    "test",
+		},
+		Timeout: 2 * time.Second,
+	}
+
+	te, err := f.CreateMetricsExporter(zap.NewNop(), config)
+	assert.NoError(t, err)
+	assert.NotNil(t, te)
+}
+
+func TestFactory_CreateMetricsExporterFails(t *testing.T) {
+	tests := []struct {
+		name         string
+		config       *Config
+		errorMessage string
+	}{
+		{
+			name: "negative_duration",
+			config: &Config{
+				ExporterSettings: configmodels.ExporterSettings{
+					TypeVal: typeStr,
+					NameVal: typeStr,
+				},
+				AccessToken: "testToken",
+				Realm:       "lab",
+				Timeout:     -2 * time.Second,
+			},
+			errorMessage: "\"signalfx\" config cannot have a negative \"timeout\"",
+		},
+		{
+			name: "empty_realm_and_url",
+			config: &Config{
+				ExporterSettings: configmodels.ExporterSettings{
+					TypeVal: typeStr,
+					NameVal: typeStr,
+				},
+			},
+			errorMessage: "\"signalfx\" config requires a non-empty \"realm\" or \"url\"",
+		},
+		{
+			name: "realm_and_url",
+			config: &Config{
+				ExporterSettings: configmodels.ExporterSettings{
+					TypeVal: typeStr,
+					NameVal: typeStr,
+				},
+				URL:   "https://some.url",
+				Realm: "us0",
+			},
+			errorMessage: "\"signalfx\" \"realm\" and \"url\" cannot be specified at the same time",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := &Factory{}
+			te, err := f.CreateMetricsExporter(zap.NewNop(), tt.config)
+			assert.EqualError(t, err, tt.errorMessage)
+			assert.Nil(t, te)
+		})
+	}
+}
