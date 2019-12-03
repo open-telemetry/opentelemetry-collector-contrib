27c711869fd37afce3b58c8d411abd79cf2fc829
Peter Wiese <petercwiese@gmail.com>
2019-12-03 09:54:21 -0800

    Adding a trace exporter to Azure Monitor (#39)
    * Adding a trace exporter to Azure Monitor
    
    This exporter transforms the current wire format Spans into constructs
    defined in
    [ApplicationInsights-Go](github.com/Microsoft/ApplicationInsights-Go/appinsights/contracts).
    
    Once a Span is transformed it is sent to Azure Monitor via a transportChannel
    interface. There is an implementation of that interface method inside
    ApplicationInsights-Go package that ultimately ultimately sent to Azure
    Monitor via a REST API. This package takes care of appropriate retries,
    batching, and buffering (in-memory only).
    
    Once the OpenTelemetry wire format is incorporated into the collector I
    will make the switch to that format. The changes should not be too bad.
    
    This PR does not:
    - export Span events
    - export Span links,
    - implement a metrics exporter
    
    Test code coverage is > 90%
    
    * Wiring up the Azure Monitor exporter via local module redirect
    
    * Adding slightly more test coverage
    
    * Addressing some PR comments
    
    * Rename toHex -> idToHex
    
    * Address more PR comments
    
    * Plumb zap.Logger through to the exporter
    
    * Satisfy impi import validation
    
    * Sanitize envelope fix
    
    * Numeric attribute values -> measurements collection
    
    * Remove the exporter Debug configuration flag and just use the information available in the zap.Logger
    
    * Adding a config validation check
    
    * Addressing PR comments

---
+++ exporter/azuremonitorexporter/time_utils.go
@@ -0,0 +1,46 @@
+// Copyright 2019, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package azuremonitorexporter
+
+// Contains code common to both trace and metrics exporters
+import (
+	"fmt"
+	"time"
+
+	timestamp "github.com/golang/protobuf/ptypes/timestamp"
+)
+
+func toTime(t *timestamp.Timestamp) time.Time {
+	return time.Unix(t.Seconds, int64(t.Nanos))
+}
+
+// Formats a Duration into the form DD.HH:MM:SS.MMMMMM
+func formatDuration(d time.Duration) string {
+	day := d / (time.Hour * 24)
+	d -= day * (time.Hour * 24)
+
+	h := d / time.Hour
+	d -= h * time.Hour
+
+	m := d / time.Minute
+	d -= m * time.Minute
+
+	s := d / time.Second
+	d -= s * time.Second
+
+	us := (d / time.Microsecond)
+
+	return fmt.Sprintf("%02d.%02d:%02d:%02d.%06d", day, h, m, s, us)
+}
