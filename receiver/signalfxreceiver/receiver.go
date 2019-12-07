9a9b3061210474e0d8ebc217a04f4370b6a55020
Paulo Janotti <paulo@omnition.io>
2019-12-07 14:12:21 -0800

    Add SignalFx Receiver
    Add a receiver supporting SignalFx metrics.

---
+++ receiver/signalfxreceiver/receiver.go
@@ -0,0 +1,255 @@
+// Copyright 2019, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package signalfxreceiver
+
+import (
+	"compress/gzip"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/gorilla/mux"
+	"github.com/open-telemetry/opentelemetry-collector/consumer"
+	"github.com/open-telemetry/opentelemetry-collector/observability"
+	"github.com/open-telemetry/opentelemetry-collector/oterr"
+	"github.com/open-telemetry/opentelemetry-collector/receiver"
+	"github.com/open-telemetry/opentelemetry-collector/translator/conventions"
+	sfxpb "github.com/signalfx/com_signalfx_metrics_protobuf"
+	"go.opencensus.io/trace"
+	"go.uber.org/zap"
+)
+
+const (
+	defaultServerTimeout = 20 * time.Second
+
+	responseInvalidMethod      = "Only \"POST\" method is supported"
+	responseInvalidContentType = "\"Content-Type\" must be \"application/x-protobuf\""
+	responseInvalidEncoding    = "\"Content-Encoding\" must be \"gzip\" or empty"
+	responseErrGzipReader      = "Error on gzip body"
+	responseErrReadBody        = "Failed to read message body"
+	responseErrUnmarshalBody   = "Failed to unmarshal message body"
+	responseErrNextConsumer    = "Internal Server Error"
+)
+
+var (
+	errNilNextConsumer = errors.New("nil nextConsumer")
+	errEmptyEndpoint   = errors.New("empty endpoint")
+)
+
+// sfxReceiver implements the receiver.TraceReceiver for Zipkin Scribe protocol.
+type sfxReceiver struct {
+	sync.Mutex
+	logger       *zap.Logger
+	config       *Config
+	nextConsumer consumer.MetricsConsumer
+	server       *http.Server
+
+	startOnce sync.Once
+	stopOnce  sync.Once
+}
+
+var _ receiver.MetricsReceiver = (*sfxReceiver)(nil)
+
+func (r *sfxReceiver) MetricsSource() string {
+	const metricsSource string = "SignalFx"
+	return metricsSource
+}
+
+// New creates the SignalFx receiver with the given configuration.
+func New(
+	logger *zap.Logger,
+	config Config,
+	nextConsumer consumer.MetricsConsumer,
+) (receiver.MetricsReceiver, error) {
+
+	if nextConsumer == nil {
+		return nil, errNilNextConsumer
+	}
+
+	if config.Endpoint == "" {
+		return nil, errEmptyEndpoint
+	}
+
+	// Handle config zero values.
+	if config.Name() == "" {
+		config.SetType(typeStr)
+		config.SetName(typeStr)
+	}
+
+	r := &sfxReceiver{
+		logger:       logger,
+		config:       &config,
+		nextConsumer: nextConsumer,
+		server: &http.Server{
+			Addr: config.Endpoint,
+			// TODO: Evaluate what properties should be configurable, for now
+			//		set some hard-coded values.
+			ReadHeaderTimeout: defaultServerTimeout,
+			WriteTimeout:      defaultServerTimeout,
+		},
+	}
+
+	mux := mux.NewRouter()
+	mux.HandleFunc("/v2/datapoint", r.handleReq)
+	r.server.Handler = mux
+
+	return r, nil
+}
+
+// StartMetricsReception tells the receiver to start its processing.
+// By convention the consumer of the data received is set at creation time.
+func (r *sfxReceiver) StartMetricsReception(host receiver.Host) error {
+	r.Lock()
+	defer r.Unlock()
+
+	err := oterr.ErrAlreadyStarted
+	r.startOnce.Do(func() {
+		err = nil
+
+		go func() {
+			if err := r.server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+				host.ReportFatalError(err)
+			}
+		}()
+	})
+
+	return err
+}
+
+// StopMetricsReception tells the receiver that should stop reception,
+// giving it a chance to perform any necessary clean-up.
+func (r *sfxReceiver) StopMetricsReception() error {
+	r.Lock()
+	defer r.Unlock()
+
+	err := oterr.ErrAlreadyStopped
+	r.stopOnce.Do(func() {
+		err = r.server.Close()
+	})
+	return err
+}
+
+func (r *sfxReceiver) handleReq(resp http.ResponseWriter, req *http.Request) {
+	// Tracing the request to make it visible via z-pages.
+	reqCtx := req.Context()
+	spanCtx, span := trace.StartSpan(reqCtx, r.config.Name())
+	defer span.End()
+
+	if req.Method != "POST" {
+		r.failRequest(resp, http.StatusBadRequest, responseInvalidMethod, nil, span)
+		return
+	}
+
+	if req.Header.Get("Content-Type") != "application/x-protobuf" {
+		r.failRequest(resp, http.StatusUnsupportedMediaType, responseInvalidContentType, nil, span)
+		return
+	}
+
+	encoding := req.Header.Get("Content-Encoding")
+	if encoding != "" && encoding != "gzip" {
+		r.failRequest(resp, http.StatusUnsupportedMediaType, responseInvalidEncoding, nil, span)
+		return
+	}
+
+	var err error
+	bodyReader := req.Body
+	if encoding == "gzip" {
+		bodyReader, err = gzip.NewReader(bodyReader)
+		if err != nil {
+			r.failRequest(resp, http.StatusBadRequest, responseErrGzipReader, err, span)
+			return
+		}
+	}
+
+	body, err := ioutil.ReadAll(bodyReader)
+	if err != nil {
+		r.failRequest(resp, http.StatusBadRequest, responseErrReadBody, err, span)
+		return
+	}
+
+	msg := &sfxpb.DataPointUploadMessage{}
+	if err := proto.Unmarshal(body, msg); err != nil {
+		r.failRequest(resp, http.StatusBadRequest, responseErrUnmarshalBody, err, span)
+		return
+	}
+
+	recvCtx := observability.ContextWithReceiverName(spanCtx, r.config.Name())
+	if len(msg.Datapoints) == 0 {
+		observability.RecordMetricsForMetricsReceiver(recvCtx, 0, 0)
+		return
+	}
+
+	md, numDroppedTimeseries := SignalFxV2ToMetricsData(r.logger, msg.Datapoints)
+
+	err = r.nextConsumer.ConsumeMetricsData(spanCtx, *md)
+	if err != nil {
+		observability.RecordMetricsForMetricsReceiver(
+			recvCtx,
+			len(msg.Datapoints),
+			len(msg.Datapoints))
+		r.failRequest(resp, http.StatusInternalServerError, responseErrNextConsumer, err, span)
+		return
+	}
+
+	observability.RecordMetricsForMetricsReceiver(
+		recvCtx,
+		len(msg.Datapoints),
+		numDroppedTimeseries)
+
+	resp.WriteHeader(http.StatusAccepted)
+}
+
+func (r *sfxReceiver) failRequest(
+	resp http.ResponseWriter,
+	httpStatusCode int,
+	msg string,
+	err error,
+	reqSpan *trace.Span,
+) {
+	resp.WriteHeader(httpStatusCode)
+	if msg != "" {
+		_, writeErr := resp.Write([]byte(msg))
+		if writeErr != nil {
+			r.logger.Warn(
+				"Error writing HTTP response message",
+				zap.Error(writeErr),
+				zap.String("receiver", r.config.Name()))
+		}
+	}
+
+	reqSpan.AddAttributes(
+		trace.Int64Attribute(conventions.AttributeHTTPStatusCode, int64(httpStatusCode)),
+		trace.StringAttribute(conventions.AttributeHTTPStatusText, msg))
+	traceStatus := trace.Status{
+		Code: trace.StatusCodeInvalidArgument,
+	}
+	if httpStatusCode == http.StatusInternalServerError {
+		traceStatus.Code = trace.StatusCodeInternal
+	}
+	if err != nil {
+		traceStatus.Message = err.Error()
+	}
+	reqSpan.SetStatus(traceStatus)
+
+	r.logger.Warn(
+		"SignalFx receiver request failed",
+		zap.Int("http_status_code", httpStatusCode),
+		zap.String("msg", msg),
+		zap.Error(err), // It handles nil error
+		zap.String("receiver", r.config.Name()))
+}
---
+++ receiver/signalfxreceiver/signalfxv2_to_metricdata.go
@@ -0,0 +1,229 @@
+// Copyright 2019, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package signalfxreceiver
+
+import (
+	"errors"
+	"fmt"
+
+	metricspb "github.com/census-instrumentation/opencensus-proto/gen-go/metrics/v1"
+	"github.com/golang/protobuf/ptypes/timestamp"
+	"github.com/open-telemetry/opentelemetry-collector/consumer/consumerdata"
+	sfxpb "github.com/signalfx/com_signalfx_metrics_protobuf"
+	"go.uber.org/zap"
+)
+
+var (
+	errSFxNilDatum                   = errors.New("nil datum value for data-point")
+	errSFxMetricTypeEnumNotSupported = errors.New("data-point of MetricType_ENUM type is not supported")
+
+	errSFxUnexpectedInt64DatumType   = errors.New("datum value type of int64 is unexpected")
+	errSFxUnexpectedFloat64DatumType = errors.New("datum value type of float64 is unexpected")
+	errSFxUnexpectedStringDatumType  = errors.New("datum value type of string is unexpected")
+	errSFxNoDatumValue               = errors.New("no datum value present for data-point")
+)
+
+// SignalFxV2ToMetricsData converts SignalFx proto data points to
+// consumerdata.MetricsData. Returning the converted data and the number of
+// dropped time series.
+func SignalFxV2ToMetricsData(
+	logger *zap.Logger,
+	sfxDataPoints []*sfxpb.DataPoint,
+) (*consumerdata.MetricsData, int) {
+
+	// TODO: not optimized at all, basically regenerating everything for each
+	// 	data point.
+	numDroppedTimeSeries := 0
+	md := &consumerdata.MetricsData{}
+	metrics := make([]*metricspb.Metric, 0, len(sfxDataPoints))
+	for _, sfxDataPoint := range sfxDataPoints {
+		if sfxDataPoint == nil {
+			// TODO: Log or metric for this odd ball?
+			continue
+		}
+
+		// First check if the type is convertible and the data point is consistent.
+		metricType, err := convertType(sfxDataPoint)
+		if err != nil {
+			numDroppedTimeSeries++
+			logger.Debug("SignalFx data-point type conversion error",
+				zap.Error(err),
+				zap.String("metric", sfxDataPoint.GetMetric()))
+			continue
+		}
+		point, err := buildPoint(sfxDataPoint, metricType)
+		if err != nil {
+			numDroppedTimeSeries++
+			logger.Debug("SignalFx data-point datum conversion error",
+				zap.Error(err),
+				zap.String("metric", sfxDataPoint.GetMetric()))
+			continue
+		}
+
+		labelKeys, labelValues := buildLabelKeysAndValues(sfxDataPoint.Dimensions)
+		descriptor := buildDescriptor(sfxDataPoint, labelKeys, metricType)
+		ts := &metricspb.TimeSeries{
+			// TODO: StartTimestamp can be set if each cumulative time series are
+			//  	tracked but right now it is not clear if it brings benefits.
+			//		Perhaps as an option so cost is "pay for play".
+			LabelValues: labelValues,
+			Points:      []*metricspb.Point{point},
+		}
+		metric := &metricspb.Metric{
+			MetricDescriptor: descriptor,
+			Timeseries:       []*metricspb.TimeSeries{ts},
+		}
+		metrics = append(metrics, metric)
+	}
+
+	md.Metrics = metrics
+	return md, numDroppedTimeSeries
+}
+
+func convertType(
+	sfxDataPoint *sfxpb.DataPoint,
+) (descType metricspb.MetricDescriptor_Type, err error) {
+
+	// Combine metric type with the actual data point type
+	sfxMetricType := sfxDataPoint.GetMetricType()
+	sfxDatum := sfxDataPoint.Value
+	if sfxDatum == nil {
+		return descType, errSFxNilDatum
+	}
+
+	switch sfxMetricType {
+	case sfxpb.MetricType_GAUGE:
+		// Numerical: Periodic, instantaneous measurement of some state.
+		descType = metricspb.MetricDescriptor_GAUGE_DOUBLE
+		if sfxDatum.IntValue != nil {
+			descType = metricspb.MetricDescriptor_GAUGE_INT64
+		}
+
+	case sfxpb.MetricType_COUNTER:
+		// Numerical: Count of occurrences. Generally non-negative integers.
+		fallthrough
+
+	case sfxpb.MetricType_CUMULATIVE_COUNTER:
+		// Tracks a value that increases over time, where only the difference is important.
+		descType = metricspb.MetricDescriptor_CUMULATIVE_DOUBLE
+		if sfxDatum.IntValue != nil {
+			descType = metricspb.MetricDescriptor_CUMULATIVE_INT64
+		}
+
+	case sfxpb.MetricType_ENUM:
+		// String: Used for non-continuous quantities (that is, measurements where there is a fixed
+		// set of meaningful values). This is essentially a special case of gauge.
+		// TODO: add a way to support this type?
+		err = errSFxMetricTypeEnumNotSupported
+
+	default:
+		err = fmt.Errorf("unknown data-point type (%d)", sfxMetricType)
+	}
+
+	return descType, err
+}
+
+func buildPoint(
+	sfxDataPoint *sfxpb.DataPoint,
+	expectedMetricType metricspb.MetricDescriptor_Type,
+) (*metricspb.Point, error) {
+	if sfxDataPoint.Value == nil {
+		return nil, errSFxNilDatum
+	}
+
+	p := &metricspb.Point{
+		Timestamp: convertTimestamp(sfxDataPoint.GetTimestamp()),
+	}
+
+	switch {
+	case sfxDataPoint.Value.IntValue != nil:
+		mismatch := expectedMetricType != metricspb.MetricDescriptor_CUMULATIVE_INT64 &&
+			expectedMetricType != metricspb.MetricDescriptor_GAUGE_INT64
+		if mismatch {
+			return nil, errSFxUnexpectedInt64DatumType
+		}
+		p.Value = &metricspb.Point_Int64Value{Int64Value: *sfxDataPoint.Value.IntValue}
+
+	case sfxDataPoint.Value.DoubleValue != nil:
+		mismatch := expectedMetricType != metricspb.MetricDescriptor_CUMULATIVE_DOUBLE &&
+			expectedMetricType != metricspb.MetricDescriptor_GAUGE_DOUBLE
+		if mismatch {
+			return nil, errSFxUnexpectedFloat64DatumType
+		}
+		p.Value = &metricspb.Point_DoubleValue{DoubleValue: *sfxDataPoint.Value.DoubleValue}
+
+	case sfxDataPoint.Value.StrValue != nil:
+		return nil, errSFxUnexpectedStringDatumType
+
+	default:
+		return nil, errSFxNoDatumValue
+	}
+
+	return p, nil
+}
+
+func convertTimestamp(msec int64) *timestamp.Timestamp {
+	if msec == 0 {
+		return nil
+	}
+
+	ts := &timestamp.Timestamp{
+		Seconds: msec / 1e3,
+		Nanos:   int32(msec%1e3) * 1e6,
+	}
+	return ts
+}
+
+func buildDescriptor(
+	sfxDataPoint *sfxpb.DataPoint,
+	labelKeys []*metricspb.LabelKey,
+	metricType metricspb.MetricDescriptor_Type,
+) *metricspb.MetricDescriptor {
+
+	// TODO: Evaluate performance impact with different datasets to see if it
+	//  is worth to cache these.
+	descriptor := &metricspb.MetricDescriptor{
+		Name: sfxDataPoint.GetMetric(),
+		// Description: no value to go here
+		// Unit:        no value to go here
+		Type:      metricType,
+		LabelKeys: labelKeys,
+	}
+
+	return descriptor
+}
+
+func buildLabelKeysAndValues(
+	dimensions []*sfxpb.Dimension,
+) ([]*metricspb.LabelKey, []*metricspb.LabelValue) {
+	keys := make([]*metricspb.LabelKey, 0, len(dimensions))
+	values := make([]*metricspb.LabelValue, 0, len(dimensions))
+	for _, dim := range dimensions {
+		if dim == nil {
+			// TODO: Log or metric this odd ball
+			continue
+		}
+		lk := &metricspb.LabelKey{Key: *dim.Key}
+		keys = append(keys, lk)
+
+		lv := &metricspb.LabelValue{}
+		if dim.Value != nil {
+			lv.Value = *dim.Value
+			lv.HasValue = true
+		}
+		values = append(values, lv)
+	}
+	return keys, values
+}
