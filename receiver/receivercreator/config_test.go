d5e081b005b4d7fad795c2a85677e6526d8d762b
Jay Camp <jay.r.camp@gmail.com>
2020-04-07 00:16:28 -0400

    Add receiver_creator to create receivers at runtime (#145)
    * Add receiver_creator to create receivers at runtime
    
    This implements a new receiver called receiver_creator. It can start and stop
    other types of receivers at runtime. The use for it will be able to discover
    services running (such as on Kubernetes or Docker) and be able to start a
    receiver to monitor that service. When the service is no longer running its
    receiver will also be stopped.
    
    The receiver does not yet react to discovery of services as that is still a
    work in progress. This PR is being done as a checkpoint and the
    receiver_creator is not yet included in the root go.mod or default components
    while it is under heavy development.
    
    Main work to be done:
    
    - Implement service discovery
    - Use default factories instead of hard-coded list
    
    * update to latest interfaces
    
    * Address review feedback
    
    * Address review comments
    
    * fix tests
    
    * fetch receiver factory once
    
    * remove extraneous whitespace
    
    * fix import ordering
    
    * review updates
    
    * review updates

---
+++ receiver/receivercreator/config_test.go
@@ -0,0 +1,82 @@
+// Copyright 2020, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package receivercreator
+
+import (
+	"path"
+	"testing"
+
+	"github.com/open-telemetry/opentelemetry-collector/component"
+	"github.com/open-telemetry/opentelemetry-collector/config"
+	"github.com/open-telemetry/opentelemetry-collector/config/configmodels"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type mockHostFactories struct {
+	component.MockHost
+	factories config.Factories
+}
+
+// GetFactory of the specified kind. Returns the factory for a component type.
+func (mh *mockHostFactories) GetFactory(kind component.Kind, componentType string) component.Factory {
+	switch kind {
+	case component.KindReceiver:
+		return mh.factories.Receivers[componentType]
+	case component.KindProcessor:
+		return mh.factories.Processors[componentType]
+	case component.KindExporter:
+		return mh.factories.Exporters[componentType]
+	case component.KindExtension:
+		return mh.factories.Extensions[componentType]
+	}
+	return nil
+}
+
+func exampleCreatorFactory(t *testing.T) (component.Host, *configmodels.Config) {
+	factories, err := config.ExampleComponents()
+	require.Nil(t, err)
+
+	factory := &Factory{}
+	factories.Receivers[typeStr] = factory
+	cfg, err := config.LoadConfigFile(
+		t, path.Join(".", "testdata", "config.yaml"), factories,
+	)
+
+	require.NoError(t, err)
+	require.NotNil(t, cfg)
+
+	assert.Equal(t, len(cfg.Receivers), 2)
+
+	return &mockHostFactories{factories: factories}, cfg
+}
+
+func TestLoadConfig(t *testing.T) {
+	_, cfg := exampleCreatorFactory(t)
+	factory := &Factory{}
+
+	r0 := cfg.Receivers["receiver_creator"]
+	assert.Equal(t, r0, factory.CreateDefaultConfig())
+
+	r1 := cfg.Receivers["receiver_creator/1"].(*Config)
+
+	assert.NotNil(t, r1)
+	assert.Len(t, r1.subreceiverConfigs, 1)
+	assert.Contains(t, r1.subreceiverConfigs, "examplereceiver/1")
+	assert.Equal(t, "test rule", r1.subreceiverConfigs["examplereceiver/1"].Rule)
+	assert.Equal(t, userConfigMap{
+		"endpoint": "localhost:12345",
+	}, r1.subreceiverConfigs["examplereceiver/1"].config)
+}
---
+++ receiver/receivercreator/receiver.go
@@ -0,0 +1,149 @@
+// Copyright 2020, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package receivercreator
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/open-telemetry/opentelemetry-collector/component"
+	"github.com/open-telemetry/opentelemetry-collector/config"
+	"github.com/open-telemetry/opentelemetry-collector/config/configmodels"
+	"github.com/open-telemetry/opentelemetry-collector/consumer"
+	"github.com/open-telemetry/opentelemetry-collector/oterr"
+	"go.uber.org/zap"
+)
+
+var (
+	errNilNextConsumer = errors.New("nil nextConsumer")
+)
+
+var _ component.MetricsReceiver = (*receiverCreator)(nil)
+
+// receiverCreator implements consumer.MetricsConsumer.
+type receiverCreator struct {
+	nextConsumer consumer.MetricsConsumerOld
+	logger       *zap.Logger
+	cfg          *Config
+	receivers    []component.Receiver
+}
+
+// new creates the receiver_creator with the given parameters.
+func new(logger *zap.Logger, nextConsumer consumer.MetricsConsumerOld, cfg *Config) (component.MetricsReceiver, error) {
+	if nextConsumer == nil {
+		return nil, errNilNextConsumer
+	}
+
+	r := &receiverCreator{
+		logger:       logger,
+		nextConsumer: nextConsumer,
+		cfg:          cfg,
+	}
+	return r, nil
+}
+
+// loadRuntimeReceiverConfig loads the given subreceiverConfig merged with config values
+// that may have been discovered at runtime.
+func (rc *receiverCreator) loadRuntimeReceiverConfig(factory component.ReceiverFactoryOld,
+	staticSubConfig *subreceiverConfig, discoveredConfig userConfigMap) (configmodels.Receiver, error) {
+	mergedConfig := config.NewViper()
+
+	// Merge in the config values specified in the config file.
+	if err := mergedConfig.MergeConfigMap(staticSubConfig.config); err != nil {
+		return nil, fmt.Errorf("failed to merge subreceiver config from config file: %v", err)
+	}
+
+	// Merge in discoveredConfig containing values discovered at runtime.
+	if err := mergedConfig.MergeConfigMap(discoveredConfig); err != nil {
+		return nil, fmt.Errorf("failed to merge subreceiver config from discovered runtime values: %v", err)
+	}
+
+	viperConfig := config.NewViper()
+
+	// Load config under <receiver>/<id> since loadReceiver and CustomUnmarshaler expects this structure.
+	viperConfig.Set(staticSubConfig.fullName, mergedConfig.AllSettings())
+
+	receiverConfig, err := config.LoadReceiver(staticSubConfig.fullName, viperConfig, map[string]component.ReceiverFactoryBase{
+		staticSubConfig.receiverType: factory,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to load subreceiver config: %v", err)
+	}
+	// Sets dynamically created receiver to something like receiver_creator/1/redis{endpoint="localhost:6380"}.
+	// TODO: Need to make sure this is unique (just endpoint is probably not totally sufficient).
+	receiverConfig.SetName(fmt.Sprintf("%s/%s{endpoint=%q}", rc.cfg.Name(), staticSubConfig.fullName, mergedConfig.GetString("endpoint")))
+	return receiverConfig, nil
+}
+
+// createRuntimeReceiver creates a receiver that is discovered at runtime.
+func (rc *receiverCreator) createRuntimeReceiver(factory component.ReceiverFactoryOld, cfg configmodels.Receiver) (component.MetricsReceiver, error) {
+	return factory.CreateMetricsReceiver(rc.logger, cfg, rc.nextConsumer)
+}
+
+// Start receiver_creator.
+func (rc *receiverCreator) Start(host component.Host) error {
+	// TODO: Temporarily load a single instance of all subreceivers for testing.
+	// Will be done in reaction to observer events later.
+	for _, subconfig := range rc.cfg.subreceiverConfigs {
+		factory := host.GetFactory(component.KindReceiver, subconfig.receiverType)
+
+		if factory == nil {
+			rc.logger.Error("unable to lookup factory for receiver", zap.String("receiver", subconfig.receiverType))
+			continue
+		}
+
+		receiverFactory := factory.(component.ReceiverFactoryOld)
+
+		cfg, err := rc.loadRuntimeReceiverConfig(receiverFactory, subconfig, userConfigMap{})
+		if err != nil {
+			return err
+		}
+		recvr, err := rc.createRuntimeReceiver(receiverFactory, cfg)
+		if err != nil {
+			return err
+		}
+
+		if err := recvr.Start(host); err != nil {
+			return fmt.Errorf("failed starting subreceiver %s: %v", cfg.Name(), err)
+		}
+
+		rc.receivers = append(rc.receivers, recvr)
+	}
+
+	// TODO: Can result in some receivers left running if an error is encountered
+	// but starting receivers here is only temporary and will be removed when
+	// observer interface added.
+
+	return nil
+}
+
+// Shutdown stops the receiver_creator and all its receivers started at runtime.
+func (rc *receiverCreator) Shutdown() error {
+	var errs []error
+
+	for _, recvr := range rc.receivers {
+		if err := recvr.Shutdown(); err != nil {
+			// TODO: Should keep track of which receiver the error is associated with
+			// but require some restructuring.
+			errs = append(errs, err)
+		}
+	}
+
+	if len(errs) > 0 {
+		return fmt.Errorf("shutdown on %d receivers failed: %v", len(errs), oterr.CombineErrors(errs))
+	}
+
+	return nil
+}
