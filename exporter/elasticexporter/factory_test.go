619a7af378c87db8d27bf0343bb92f1e12bd895c
Andrew Wilkins <axwalk@gmail.com>
2020-06-02 01:19:53 +0800

    elasticexporter: add Elastic APM exporter (#240)
    This PR introduces an exporter for [Elastic APM](https://www.elastic.co/apm). The exporter works by translating spans and metrics into the ND-JSON format expected by Elastic APM Server, and sending over HTTP.
    
    Currently only spans are supported. Code for translating metrics exists, but is not yet wired up to the exporter; we'll do that once the switch over to the new metrics model is done.
    
    Not all of the OpenTelemetry model is covered by Elastic APM. In particular, there's currently no support for links or span events. We'll add support for events later, and most likely links too (see https://github.com/elastic/apm/issues/122).
    
    **Testing:**
    
    Unit tests added for translating resources, spans, and metrics to the Elastic APM model. This has been tested using a mock in-memory Elastic APM Server. Coverage is > 80%.
    
    Manually tested, sending to an [Elastic Cloud](https://cloud.elastic.co/) deployment.
    
    **Documentation:**
    
    Added a README, which describes the exporter's config.
    Metrics are currently not exported; we'll wait for
    the data model changes to settle, so we can build
    the translation off the OTLP representation.

---
+++ exporter/elasticexporter/factory_test.go
@@ -0,0 +1,56 @@
+// Copyright 2020, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package elasticexporter
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/config/configcheck"
+	"go.uber.org/zap"
+)
+
+func TestCreateDefaultConfig(t *testing.T) {
+	factory := Factory{}
+	cfg := factory.CreateDefaultConfig()
+	assert.NotNil(t, cfg, "failed to create default config")
+	assert.NoError(t, configcheck.ValidateConfig(cfg))
+}
+
+func TestCreateExporter(t *testing.T) {
+	factory := Factory{}
+	cfg := factory.CreateDefaultConfig()
+	eCfg := cfg.(*Config)
+	eCfg.APMServerURL = "http://testing.invalid"
+
+	te, err := factory.CreateTraceExporter(
+		context.Background(),
+		component.ExporterCreateParams{Logger: zap.NewNop()},
+		eCfg,
+	)
+	assert.NoError(t, err)
+	assert.NotNil(t, te, "failed to create trace exporter")
+
+	me, err := factory.CreateMetricsExporter(
+		context.Background(),
+		component.ExporterCreateParams{Logger: zap.NewNop()},
+		eCfg,
+	)
+	assert.Error(t, err)
+	assert.EqualError(t, err, "telemetry type is not supported")
+	assert.Nil(t, me)
+}
