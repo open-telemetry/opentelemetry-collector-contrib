1536a863da95c5c9b04999bab140a19656dee9b1
Andrew Xue <aaxue@google.com>
2020-06-04 00:33:36 -0400

    cloud-trace: Cloud Trace exporter (#698)
    Co-authored-by: Cheng-Lung Sung <clsung@gmail.com>

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -36,6 +36,7 @@ cov ext/opentelemetry-ext-flask
 cov ext/opentelemetry-ext-requests
 cov ext/opentelemetry-ext-jaeger
 cov ext/opentelemetry-ext-opentracing-shim
+cov ext/opentelemetry-exporter-cloud-trace
 cov ext/opentelemetry-ext-wsgi
 cov ext/opentelemetry-ext-zipkin
 cov docs/examples/opentelemetry-example-app
