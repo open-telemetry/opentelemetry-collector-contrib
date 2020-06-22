66e647b30f0ddbedcd534d887fe5c22aa51a9bcf
Dmitrii Anoshin <anoshindx@gmail.com>
2020-06-22 14:06:01 -0700

    Add long-running stability tests (#308)
    End-2-end tests running for an hour with fixed resource limits. If limits are reached tests fail.
    
    Current state of the collector shows a slow grow in the memory usage, which may indicate a memory leak that needs to be investigated. Once this solved, this tests can be upgraded to not use a fixed limit, but check resources consumption at the beginning and making sure it doesn't grow more than in a specified small percentage after that.
    
    **Link to tracking Issue:** https://github.com/open-telemetry/opentelemetry-collector/issues/1022

--- testbed/runtests.sh
+++ testbed/runtests.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-cd tests
+TESTS_DIR=${TESTS_DIR:-tests}
+
+cd ${TESTS_DIR}
 
 SED="sed"
 
@@ -10,11 +12,12 @@ PASS_COLOR=$(printf "\033[32mPASS\033[0m")
 FAIL_COLOR=$(printf "\033[31mFAIL\033[0m")
 TEST_COLORIZE="${SED} 's/PASS/${PASS_COLOR}/' | ${SED} 's/FAIL/${FAIL_COLOR}/'"
 
+mkdir -p results/junit
+
 TESTBED_CONFIG=local.yaml go test -v ${TEST_ARGS} 2>&1 | tee results/testoutput.log | bash -c "${TEST_COLORIZE}"
 
 testStatus=${PIPESTATUS[0]}
 
-mkdir -p results/junit
 go-junit-report < results/testoutput.log > results/junit/results.xml
 
 bash -c "cat results/TESTRESULTS.md | ${TEST_COLORIZE}"
