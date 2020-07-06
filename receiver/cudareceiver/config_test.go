61d71e5a0337310fedf4db3baadcc8e1cdafb239
Yoshi Yamaguchi <145104+ymotongpoo@users.noreply.github.com>
2020-07-06 01:53:36 +0000

    Fix package to use test assert

--- receiver/cudareceiver/config_test.go
+++ receiver/cudareceiver/config_test.go
@@ -19,8 +19,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"gotest.tools/assert"
 
 	"go.opentelemetry.io/collector/config"
 	"go.opentelemetry.io/collector/config/configmodels"
