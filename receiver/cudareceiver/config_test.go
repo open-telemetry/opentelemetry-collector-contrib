6d1765d503a70eba14abb3eb6c18cdd1ffc40197
Yoshi Yamaguchi <145104+ymotongpoo@users.noreply.github.com>
2020-07-09 06:16:31 +0000

    Fix some lint issue

--- receiver/cudareceiver/config_test.go
+++ receiver/cudareceiver/config_test.go
@@ -21,7 +21,6 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-
 	"go.opentelemetry.io/collector/config"
 	"go.opentelemetry.io/collector/config/configmodels"
 )
