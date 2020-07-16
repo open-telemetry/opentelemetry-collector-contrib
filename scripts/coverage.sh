52c2719b7aa8c9fd0cf2581f24457fa7acde7664
Andrew Xue <aaxue@google.com>
2020-07-16 16:12:44 -0400

    remove google exporter files (#918)

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -36,8 +36,6 @@ cov ext/opentelemetry-ext-flask
 cov ext/opentelemetry-ext-requests
 cov ext/opentelemetry-ext-jaeger
 cov ext/opentelemetry-ext-opentracing-shim
-cov ext/opentelemetry-exporter-cloud-trace
-cov ext/opentelemetry-exporter-cloud-monitoring
 cov ext/opentelemetry-ext-wsgi
 cov ext/opentelemetry-ext-zipkin
 cov docs/examples/opentelemetry-example-app
