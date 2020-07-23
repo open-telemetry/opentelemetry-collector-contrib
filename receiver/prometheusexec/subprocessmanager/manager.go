e8068e8bbd18c5e96cb5ccf4f7a958e24593cf21
Nicolas MacBeth <nicolasmacbeth@google.com>
2020-07-23 19:50:00 +0000

    config, subprocess manager and tests

---
+++ receiver/prometheusexec/subprocessmanager/manager.go
@@ -0,0 +1,145 @@
+// Copyright 2020, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package subprocessmanager
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/kballard/go-shellquote"
+	"go.uber.org/zap"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/prometheusexec/subprocessmanager/config"
+)
+
+// Process struct holds all the info needed to instantiate a subprocess
+type Process struct {
+	Command    string
+	Port       int
+	Env        []config.EnvConfig
+	CustomName string
+}
+
+const (
+	// HealthyProcessTime is the default time a process needs to stay alive to be considered healthy
+	HealthyProcessTime time.Duration = 30 * time.Minute
+	// HealthyCrashCount is the amount of times a process can crash (within the healthyProcessTime) before being considered unstable - it may be trying to find a port
+	HealthyCrashCount int = 3
+	// delayMutiplier is the factor by which the delay scales
+	delayMultiplier float64 = 2.0
+	// initialDelay is the initial delay before a process is restarted
+	initialDelay time.Duration = 1 * time.Second
+)
+
+// Run will start the process and keep track of running time
+func (proc *Process) Run(logger *zap.Logger) (time.Duration, error) {
+	var (
+		start     time.Time
+		elapsed   time.Duration
+		argsSlice []string
+	)
+
+	// Parse the command line string into arguments
+	args, err := shellquote.Split(proc.Command)
+	if err != nil {
+		return elapsed, fmt.Errorf("[%v] could not parse command error: %v", proc.CustomName, err)
+	}
+	// Separate the executable from the flags for the Command object
+	if len(args) > 1 {
+		argsSlice = args[1:]
+	}
+
+	// Create the command object and attach current os environment + environment variables defined by user
+	childProcess := exec.Command(args[0], argsSlice...)
+	childProcess.Env = os.Environ()
+	childProcess.Env = append(childProcess.Env, formatEnvSlice(&proc.Env)...)
+
+	// Handle the subprocess output in a goroutine, and pipe the stderr/stdout into one stream
+	cmdReader, err := childProcess.StdoutPipe()
+	if err != nil {
+		return elapsed, fmt.Errorf("[%v] could not get the command's output pipe, err: %v", proc.CustomName, err)
+	}
+	childProcess.Stderr = childProcess.Stdout
+	go proc.pipeSubprocessOutput(bufio.NewReader(cmdReader), logger)
+
+	// Start and stop timer (elapsed) right before and after executing the command
+	start = time.Now()
+	errProcess := childProcess.Start()
+	if errProcess != nil {
+		return elapsed, fmt.Errorf("[%v] process could not start: %v", proc.CustomName, errProcess)
+	}
+
+	errProcess = childProcess.Wait()
+	elapsed = time.Since(start)
+	if errProcess != nil {
+		return elapsed, fmt.Errorf("[%v] process error: %v", proc.CustomName, errProcess)
+	}
+
+	return elapsed, nil
+}
+
+// Log every line of the subprocesse's output using zap
+func (proc *Process) pipeSubprocessOutput(reader *bufio.Reader, logger *zap.Logger) {
+	// Infinite reading loop until EOF (pipe is closed)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil && err != io.EOF {
+			logger.Info("subprocess logging failed", zap.String("subprocess name", proc.CustomName), zap.String("error", err.Error()))
+			break
+		}
+
+		line = strings.TrimSpace(line)
+		if line != "" {
+			logger.Info("subprocess output line", zap.String("subprocess name", proc.CustomName), zap.String("output", line))
+		}
+
+		// Leave this function when error is EOF (stderr/stdout pipe was closed)
+		if err == io.EOF {
+			break
+		}
+	}
+}
+
+// formatEnvSlice will loop over the key-value pairs and format the slice correctly for use by the Command object ("name=value")
+func formatEnvSlice(envs *[]config.EnvConfig) []string {
+	if len(*envs) == 0 {
+		return nil
+	}
+
+	envSlice := make([]string, len(*envs))
+	for i, env := range *envs {
+		envSlice[i] = fmt.Sprintf("%v=%v", env.Name, env.Value)
+	}
+
+	return envSlice
+}
+
+// GetDelay will compute the exponential backoff for a given process according to its crash count and time alive
+func GetDelay(elapsed time.Duration, crashCount int) time.Duration {
+	// Return initialDelay if the process is healthy (lasted longer than health duration) or has less or equal than 3 crashes - it could be trying to find a port
+	if elapsed > HealthyProcessTime || crashCount <= HealthyCrashCount {
+		return initialDelay
+	}
+
+	// Return initialDelay times 2 to the power of crashCount-3 (to offset for the 3 allowed crashes) added to a random number
+	return initialDelay * time.Duration(math.Pow(delayMultiplier, float64(crashCount-HealthyCrashCount)+rand.Float64()))
+}
---
+++ receiver/prometheusexec/subprocessmanager/manager_test.go
@@ -0,0 +1,218 @@
+// Copyright 2020, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package subprocessmanager
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"go.uber.org/zap"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/prometheusexec/subprocessmanager/config"
+)
+
+var (
+	getDelayTests = []struct {
+		name       string
+		elapsed    time.Duration
+		crashCount int
+		want       time.Duration
+	}{
+		{
+			name:       "healthy process",
+			elapsed:    15 * time.Second,
+			crashCount: 2,
+			want:       1 * time.Second,
+		},
+		{
+			name:       "healthy process",
+			elapsed:    15 * time.Hour,
+			crashCount: 6,
+			want:       1 * time.Second,
+		},
+		{
+			name:       "unhealthy process 1",
+			elapsed:    15 * time.Second,
+			crashCount: 4,
+		},
+		{
+			name:       "unhealthy process 2",
+			elapsed:    15 * time.Second,
+			crashCount: 5,
+		},
+		{
+			name:       "unhealthy process 3",
+			elapsed:    15 * time.Second,
+			crashCount: 6,
+		},
+		{
+			name:       "unhealthy process 4",
+			elapsed:    15 * time.Second,
+			crashCount: 7,
+		},
+	}
+
+	formatEnvSliceTests = []struct {
+		name     string
+		envSlice *[]config.EnvConfig
+		want     []string
+		wantNil  bool
+	}{
+		{
+			name:     "empty slice",
+			envSlice: &[]config.EnvConfig{},
+			want:     nil,
+			wantNil:  true,
+		},
+		{
+			name: "one entry",
+			envSlice: &[]config.EnvConfig{
+				{
+					Name:  "DATA_SOURCE",
+					Value: "password:username",
+				},
+			},
+			want: []string{
+				"DATA_SOURCE=password:username",
+			},
+			wantNil: false,
+		},
+		{
+			name: "three entries",
+			envSlice: &[]config.EnvConfig{
+				{
+					Name:  "DATA_SOURCE",
+					Value: "password:username",
+				},
+				{
+					Name:  "",
+					Value: "",
+				},
+				{
+					Name:  "john",
+					Value: "doe",
+				},
+			},
+			want: []string{
+				"DATA_SOURCE=password:username",
+				"=",
+				"john=doe",
+			},
+			wantNil: false,
+		},
+	}
+
+	runTests = []struct {
+		name        string
+		process     *Process
+		wantElapsed time.Duration
+		wantErr     bool
+	}{
+		{
+			name: "normal process 1, error process exit",
+			process: &Process{
+				Command: "go run ../testdata/test_crasher.go",
+				Port:    0,
+				Env: []config.EnvConfig{
+					{
+						Name:  "DATA_SOURCE",
+						Value: "username:password@(url:port)/dbname",
+					},
+				},
+				CustomName: "main",
+			},
+			wantElapsed: 4 * time.Millisecond,
+			wantErr:     false,
+		},
+		{
+			name: "normal process 2, normal process exit",
+			process: &Process{
+				Command: "go version",
+				Port:    0,
+				Env: []config.EnvConfig{
+					{
+						Name:  "DATA_SOURCE",
+						Value: "username:password@(url:port)/dbname",
+					},
+				},
+				CustomName: "main",
+			},
+			wantElapsed: 0 * time.Nanosecond,
+			wantErr:     false,
+		},
+		{
+			name: "shellquote error",
+			process: &Process{
+				Command:    "command flag='something",
+				Port:       0,
+				Env:        []config.EnvConfig{},
+				CustomName: "main",
+			},
+			wantElapsed: 0,
+			wantErr:     true,
+		},
+	}
+)
+
+func TestGetDelay(t *testing.T) {
+	var previousResult time.Duration
+	for _, test := range getDelayTests {
+		t.Run(test.name, func(t *testing.T) {
+			got := GetDelay(test.elapsed, test.crashCount)
+			if test.name == "healthy process" {
+				if !reflect.DeepEqual(got, test.want) {
+					t.Errorf("GetDelay() got = %v, want %v", got, test.want)
+					return
+				}
+			}
+			if previousResult > got {
+				t.Errorf("GetDelay() got = %v, want something larger than the previous result %v", got, previousResult)
+			}
+			previousResult = got
+		})
+	}
+}
+
+func TestFormatEnvSlice(t *testing.T) {
+	for _, test := range formatEnvSliceTests {
+		t.Run(test.name, func(t *testing.T) {
+			got := formatEnvSlice(test.envSlice)
+			if test.wantNil && got != nil {
+				t.Errorf("formatEnvSlice() got = %v, wantNil %v", got, test.wantNil)
+				return
+			}
+			if !reflect.DeepEqual(got, test.want) {
+				t.Errorf("getCustomName() got = %v, want %v", got, test.want)
+			}
+		})
+	}
+}
+
+func TestRun(t *testing.T) {
+	for _, test := range runTests {
+		t.Run(test.name, func(t *testing.T) {
+			logger, _ := zap.NewProduction()
+			got, err := test.process.Run(logger)
+			if test.wantErr && err == nil {
+				t.Errorf("Run() got = %v, wantErr %v", got, test.wantErr)
+				return
+			}
+			if got < test.wantElapsed {
+				t.Errorf("Run() got = %v, want larger than %v", got, test.wantElapsed)
+			}
+		})
+	}
+}
