886226e10413e53adaef81bf7a0699cc28d5e355
Shaocheng Wang <shaochew@amazon.com>
2020-08-04 11:36:01 -0700

    AWSEMFExporter - Add License

--- exporter/awsemfexporter/emf_exporter.go
+++ exporter/awsemfexporter/emf_exporter.go
@@ -1,3 +1,17 @@
+// Copyright 2020, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package awsemfexporter
 
 import (
@@ -96,7 +110,7 @@ func (emf *emfExporter) pushMetricsData(_ context.Context, md pdata.Metrics) (dr
 	if emf.token != "" {
 		putLogEventsInput.SequenceToken = aws.String(emf.token)
 	}
-	tmpToken := emf.svcStructuredLog.PutLogEvents(putLogEventsInput, defaultRetryCount)
+	tmpToken := emf.svcStructuredLog.PutLogEvents(putLogEventsInput, emf.retryCnt)
 	if tmpToken != nil {
 		emf.token = *tmpToken
 	}
--- exporter/awsemfexporter/emf_exporter_test.go
+++ exporter/awsemfexporter/emf_exporter_test.go
@@ -1,10 +1,21 @@
+// Copyright 2020, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package awsemfexporter
 
 import (
 	"context"
-	"testing"
-	"time"
-
 	commonpb "github.com/census-instrumentation/opencensus-proto/gen-go/agent/common/v1"
 	metricspb "github.com/census-instrumentation/opencensus-proto/gen-go/metrics/v1"
 	resourcepb "github.com/census-instrumentation/opencensus-proto/gen-go/resource/v1"
@@ -15,6 +26,7 @@ import (
 	"go.opentelemetry.io/collector/consumer/consumerdata"
 	"go.opentelemetry.io/collector/consumer/pdatautil"
 	"go.uber.org/zap"
+	"testing"
 )
 
 func TestPushMetricsData(t *testing.T) {
@@ -23,11 +35,12 @@ func TestPushMetricsData(t *testing.T) {
 	factory := new(Factory)
 	expCfg := factory.CreateDefaultConfig().(*Config)
 	expCfg.Region = "us-west-2"
+	expCfg.MaxRetries = 0
 	exp, err := New(expCfg, component.ExporterCreateParams{Logger: zap.NewNop()})
 	assert.Nil(t, err)
 	assert.NotNil(t, exp)
 
-	mdPrev := consumerdata.MetricsData{
+	mdata := consumerdata.MetricsData{
 		Node: &commonpb.Node{
 			ServiceInfo: &commonpb.ServiceInfo{Name: "test-emf"},
 			LibraryInfo: &commonpb.LibraryInfo{ExporterVersion: "SomeVersion"},
@@ -70,53 +83,7 @@ func TestPushMetricsData(t *testing.T) {
 			},
 		},
 	}
-	mdCur := consumerdata.MetricsData{
-		Node: &commonpb.Node{
-			ServiceInfo: &commonpb.ServiceInfo{Name: "test-emf"},
-			LibraryInfo: &commonpb.LibraryInfo{ExporterVersion: "SomeVersion"},
-		},
-		Resource: &resourcepb.Resource{
-			Labels: map[string]string{
-				"resource": "R1",
-			},
-		},
-		Metrics: []*metricspb.Metric{
-			{
-				MetricDescriptor: &metricspb.MetricDescriptor{
-					Name:        "spanCounter",
-					Description: "Counting all the spans",
-					Unit:        "Count",
-					Type:        metricspb.MetricDescriptor_CUMULATIVE_INT64,
-					LabelKeys: []*metricspb.LabelKey{
-						{Key: "spanName"},
-						{Key: "isItAnError"},
-					},
-				},
-				Timeseries: []*metricspb.TimeSeries{
-					{
-						LabelValues: []*metricspb.LabelValue{
-							{Value: "testSpan"},
-							{Value: "false"},
-						},
-						Points: []*metricspb.Point{
-							{
-								Timestamp: &timestamp.Timestamp{
-									Seconds: 101,
-								},
-								Value: &metricspb.Point_Int64Value{
-									Int64Value: 12,
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-	md := pdatautil.MetricsFromMetricsData([]consumerdata.MetricsData{mdPrev})
-	require.NoError(t, exp.ConsumeMetrics(ctx, md))
-	md = pdatautil.MetricsFromMetricsData([]consumerdata.MetricsData{mdCur})
-	time.Sleep(1 * time.Second)
+	md := pdatautil.MetricsFromMetricsData([]consumerdata.MetricsData{mdata})
 	require.NoError(t, exp.ConsumeMetrics(ctx, md))
 	require.NoError(t, exp.Shutdown(ctx))
 }
