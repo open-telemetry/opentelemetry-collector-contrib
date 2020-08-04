06e07b3088154ba1d5949e23898e50efd0a486d8
Leighton Chen <lechen@microsoft.com>
2020-08-03 17:48:44 -0700

    Rename db framework packages from "ext" to "instrumentation" (#966)

---
+++ instrumentation/opentelemetry-instrumentation-sqlite3/CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changelog
+
+## Unreleased
+
+- Change package name to opentelemetry-instrumentation-sqlite3
+  ([#999](https://github.com/open-telemetry/opentelemetry-python/pull/999))
+
+## 0.8b0
+
+Released 2020-05-27
+
+- Initial release
---
+++ instrumentation/opentelemetry-instrumentation-sqlite3/tests/test_sqlite3.py
@@ -0,0 +1,83 @@
+# Copyright 2020, OpenTelemetry Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sqlite3
+
+from opentelemetry import trace as trace_api
+from opentelemetry.instrumentation.sqlite3 import SQLite3Instrumentor
+from opentelemetry.test.test_base import TestBase
+
+
+class TestSQLite3(TestBase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls._connection = None
+        cls._cursor = None
+        cls._tracer = cls.tracer_provider.get_tracer(__name__)
+        SQLite3Instrumentor().instrument(tracer_provider=cls.tracer_provider)
+        cls._connection = sqlite3.connect(":memory:")
+        cls._cursor = cls._connection.cursor()
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls._cursor:
+            cls._cursor.close()
+        if cls._connection:
+            cls._connection.close()
+
+    def validate_spans(self):
+        spans = self.memory_exporter.get_finished_spans()
+        self.assertEqual(len(spans), 2)
+        for span in spans:
+            if span.name == "rootSpan":
+                root_span = span
+            else:
+                child_span = span
+            self.assertIsInstance(span.start_time, int)
+            self.assertIsInstance(span.end_time, int)
+        self.assertIsNotNone(root_span)
+        self.assertIsNotNone(child_span)
+        self.assertEqual(root_span.name, "rootSpan")
+        self.assertEqual(child_span.name, "sqlite3")
+        self.assertIsNotNone(child_span.parent)
+        self.assertIs(child_span.parent, root_span.get_context())
+        self.assertIs(child_span.kind, trace_api.SpanKind.CLIENT)
+
+    def test_execute(self):
+        """Should create a child span for execute method
+        """
+        with self._tracer.start_as_current_span("rootSpan"):
+            self._cursor.execute(
+                "CREATE TABLE IF NOT EXISTS test (id integer)"
+            )
+        self.validate_spans()
+
+    def test_executemany(self):
+        """Should create a child span for executemany
+        """
+        with self._tracer.start_as_current_span("rootSpan"):
+            data = [("1",), ("2",), ("3",)]
+            stmt = "INSERT INTO test (id) VALUES (?)"
+            self._cursor.executemany(stmt, data)
+        self.validate_spans()
+
+    def test_callproc(self):
+        """Should create a child span for callproc
+        """
+        with self._tracer.start_as_current_span("rootSpan"), self.assertRaises(
+            Exception
+        ):
+            self._cursor.callproc("test", ())
+            self.validate_spans()
