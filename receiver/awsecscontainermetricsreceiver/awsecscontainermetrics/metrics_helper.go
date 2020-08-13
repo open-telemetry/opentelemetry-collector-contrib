4533aa74801f017dab46fbe4bd8082525131257f
Rayhan Hossain (Mukla.C) <hossain.rayhan@outlook.com>
2020-08-13 11:18:37 -0700

    Add skeleton for AWS ECS container metrics receiver (#463)
    This change adds the skeleton for AWS ECS container metrics receiver. It reads
    `collection_interval` from config file and ships some dummy metrics on that fixed interval.
    
    **Link to tracking Issue:** 
    Closes #458 
    
    **Testing:** 
    Unit test added and executed locally.
    
    **Documentation:** 
    README was added.

---
+++ receiver/awsecscontainermetricsreceiver/awsecscontainermetrics/metrics_helper.go
@@ -0,0 +1,47 @@
+// Copyright 2020, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package awsecscontainermetrics
+
+import (
+	"strconv"
+	"time"
+
+	metricspb "github.com/census-instrumentation/opencensus-proto/gen-go/metrics/v1"
+	"go.opentelemetry.io/collector/consumer/consumerdata"
+	"go.opentelemetry.io/collector/testutil/metricstestutil"
+)
+
+// GenerateDummyMetrics generates some dummy metrics
+func GenerateDummyMetrics() consumerdata.MetricsData {
+	md := consumerdata.MetricsData{}
+	ts := time.Now()
+	for i := 0; i < 5; i++ {
+		md.Metrics = append(md.Metrics,
+			metricstestutil.Gauge(
+				"test_"+strconv.Itoa(i),
+				[]string{"k0", "k1"},
+				metricstestutil.Timeseries(
+					time.Now(),
+					[]string{"v0", "v1"},
+					&metricspb.Point{
+						Timestamp: metricstestutil.Timestamp(ts),
+						Value:     &metricspb.Point_Int64Value{Int64Value: int64(i)},
+					},
+				),
+			),
+		)
+	}
+	return md
+}
