0594bb206cc4ce2a64d62c8cb353bc5c9d3e6571
Pablo Collins <pablo.collins@gmail.com>
2020-08-21 11:33:56 -0400

    sfxexporter: add disk utilization translations

--- exporter/signalfxexporter/translation/constants.go
+++ exporter/signalfxexporter/translation/constants.go
@@ -258,6 +258,25 @@ translation_rules:
   without_dimensions:
   - device
 
+# disk utilization
+- action: calculate_new_metric
+  metric_name: disk.utilization
+  operand1_metric: df_complex.used
+  operand2_metric: disk.total
+  operator: /
+- action: multiply_float
+  scale_factors_float:
+    disk.utilization: 100
+
+- action: calculate_new_metric
+  metric_name: disk.summary_utilization
+  operand1_metric: df_complex.used_total
+  operand2_metric: disk.summary_total
+  operator: /
+- action: multiply_float
+  scale_factors_float:
+    disk.summary_utilization: 100
+
 # convert disk I/O metrics
 - action: rename_dimension_keys
   metric_names:
