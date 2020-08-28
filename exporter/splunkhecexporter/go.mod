1428ed6f11a3c6fd849d1035e03ede2b5988f9ed
Antoine Toulme <atoulme@users.noreply.github.com>
2020-08-28 12:41:42 -0700

    Move splunkMetric to a common internal package so we can reuse it in a receiver (#841)

--- exporter/splunkhecexporter/go.mod
+++ exporter/splunkhecexporter/go.mod
@@ -4,9 +4,13 @@ go 1.14
 
 require (
 	github.com/census-instrumentation/opencensus-proto v0.3.0
+	github.com/open-telemetry/opentelemetry-collector-contrib/internal/common v0.0.0-00010101000000-000000000000
 	github.com/stretchr/testify v1.6.1
 	go.opentelemetry.io/collector v0.9.1-0.20200828041256-df1879c6390f
 	go.uber.org/zap v1.15.0
 	google.golang.org/grpc/examples v0.0.0-20200728194956-1c32b02682df // indirect
 	google.golang.org/protobuf v1.25.0
+
 )
+
+replace github.com/open-telemetry/opentelemetry-collector-contrib/internal/common => ../../internal/common
--- internal/common/splunk/common.go
+++ internal/common/splunk/common.go
@@ -14,6 +14,8 @@
 
 package splunk
 
+import "strings"
+
 const (
 	SFxAccessTokenHeader = "X-Sf-Token"
 	SFxAccessTokenLabel  = "com.splunk.signalfx.access_token"
@@ -22,4 +24,26 @@ const (
 type AccessTokenPassthroughConfig struct {
 	// Whether to associate datapoints with an organization access token received in request.
 	AccessTokenPassthrough bool `mapstructure:"access_token_passthrough"`
+}
+
+// Metric represents a metric in Splunk HEC format
+type Metric struct {
+	Time       float64                `json:"time"`                 // epoch time
+	Host       string                 `json:"host"`                 // hostname
+	Source     string                 `json:"source,omitempty"`     // optional description of the source of the event; typically the app's name
+	SourceType string                 `json:"sourcetype,omitempty"` // optional name of a Splunk parsing configuration; this is usually inferred by Splunk
+	Index      string                 `json:"index,omitempty"`      // optional name of the Splunk index to store the event in; not required if the token has a default index set in Splunk
+	Event      string                 `json:"event"`                // type of event: this is a metric.
+	Fields     map[string]interface{} `json:"fields"`               // metric data
+}
+
+// GetValues extracts metric key value pairs from a Splunk HEC metric.
+func (m Metric) GetValues() map[string]interface{} {
+	values := map[string]interface{}{}
+	for k, v := range m.Fields {
+		if strings.HasPrefix(k, "metric_name:") {
+			values[k[12:]] = v
+		}
+	}
+	return values
 }
