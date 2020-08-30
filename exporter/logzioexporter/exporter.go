9a036551f04bd5188a35d1060813ded0697c9648
yyyogev <yogev.metzuyanim@logz.io>
2020-08-30 16:46:55 +0300

    fetch span process from batch
    Signed-off-by: yyyogev <yogev.metzuyanim@logz.io>

--- exporter/logzioexporter/exporter.go
+++ exporter/logzioexporter/exporter.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/hashicorp/go-hclog"
+	"github.com/jaegertracing/jaeger/model"
 	"github.com/logzio/jaeger-logzio/store"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer/pdata"
@@ -81,6 +82,10 @@ func (exporter *logzioExporter) pushTraceData(ctx context.Context, traces pdata.
 	}
 	for _, batch := range batches {
 		for _, span := range batch.Spans {
+			span.Process = batch.Process
+			if span.Process == nil {
+				span.Process = &model.Process{}
+			}
 			if err := exporter.writer.WriteSpan(span); err != nil {
 				exporter.logger.Debug(fmt.Sprintf("dropped bad span: %s", span.String()))
 				droppedSpans++
