7dee17559dc17067142d04c981342cfa08f84d58
Ben Keith <benkeith@splunk.com>
2020-08-31 16:47:26 -0400

    Add AWSUniqueId and gcp_id generation in signalfxexporter (#829)
    These dimensions will be derived from the relevant resource attributes, if present.
    
    The resource attributes used to derive the values will be removed and thus
    not sent as dimensions.

--- exporter/signalfxexporter/translation/converter_test.go
+++ exporter/signalfxexporter/translation/converter_test.go
@@ -184,6 +184,118 @@ func Test_MetricDataToSignalFxV2(t *testing.T) {
 			},
 		},
 		{
+			name: "with_resources_cloud_partial_aws_dim",
+			metricsDataFn: func() consumerdata.MetricsData {
+				return consumerdata.MetricsData{
+					Resource: &resourcepb.Resource{
+						Labels: map[string]string{
+							"k/r0":             "vr0",
+							"k/r1":             "vr1",
+							"cloud.provider":   "ec2",
+							"cloud.account.id": "efgh",
+							"cloud.region":     "us-east",
+						},
+					},
+					Metrics: []*metricspb.Metric{
+						metricstestutil.Gauge("gauge_double_with_dims", keys, metricstestutil.Timeseries(tsUnix, values, doublePt)),
+					},
+				}
+			},
+			wantSfxDataPoints: []*sfxpb.DataPoint{
+				doubleSFxDataPoint(
+					"gauge_double_with_dims",
+					tsMSecs,
+					&sfxMetricTypeGauge,
+					append([]string{"cloud_account_id", "cloud_provider", "cloud_region", "k_r0", "k_r1"}, keys...),
+					append([]string{"efgh", "ec2", "us-east", "vr0", "vr1"}, values...),
+					doubleVal),
+			},
+		},
+		{
+			name: "with_resources_cloud_aws_dim",
+			metricsDataFn: func() consumerdata.MetricsData {
+				return consumerdata.MetricsData{
+					Resource: &resourcepb.Resource{
+						Labels: map[string]string{
+							"k/r0":             "vr0",
+							"k/r1":             "vr1",
+							"cloud.provider":   "ec2",
+							"cloud.account.id": "efgh",
+							"cloud.region":     "us-east",
+							"host.id":          "abcd",
+						},
+					},
+					Metrics: []*metricspb.Metric{
+						metricstestutil.Gauge("gauge_double_with_dims", keys, metricstestutil.Timeseries(tsUnix, values, doublePt)),
+					},
+				}
+			},
+			wantSfxDataPoints: []*sfxpb.DataPoint{
+				doubleSFxDataPoint(
+					"gauge_double_with_dims",
+					tsMSecs,
+					&sfxMetricTypeGauge,
+					append([]string{"AWSUniqueId", "k_r0", "k_r1"}, keys...),
+					append([]string{"abcd_us-east_efgh", "vr0", "vr1"}, values...),
+					doubleVal),
+			},
+		},
+		{
+			name: "with_resources_cloud_gce_dim_partial",
+			metricsDataFn: func() consumerdata.MetricsData {
+				return consumerdata.MetricsData{
+					Resource: &resourcepb.Resource{
+						Labels: map[string]string{
+							"k/r0":           "vr0",
+							"k/r1":           "vr1",
+							"cloud.provider": "gce",
+							"host.id":        "abcd",
+						},
+					},
+					Metrics: []*metricspb.Metric{
+						metricstestutil.Gauge("gauge_double_with_dims", keys, metricstestutil.Timeseries(tsUnix, values, doublePt)),
+					},
+				}
+			},
+			wantSfxDataPoints: []*sfxpb.DataPoint{
+				doubleSFxDataPoint(
+					"gauge_double_with_dims",
+					tsMSecs,
+					&sfxMetricTypeGauge,
+					append([]string{"cloud_provider", "host_id", "k_r0", "k_r1"}, keys...),
+					append([]string{"gce", "abcd", "vr0", "vr1"}, values...),
+					doubleVal),
+			},
+		},
+		{
+			name: "with_resources_cloud_gcp_dim",
+			metricsDataFn: func() consumerdata.MetricsData {
+				return consumerdata.MetricsData{
+					Resource: &resourcepb.Resource{
+						Labels: map[string]string{
+							"k/r0":             "vr0",
+							"k/r1":             "vr1",
+							"cloud.provider":   "gce",
+							"cloud.account.id": "efgh",
+							"host.id":          "abcd",
+						},
+					},
+					Metrics: []*metricspb.Metric{
+						metricstestutil.Gauge("gauge_double_with_dims", keys, metricstestutil.Timeseries(tsUnix, values, doublePt)),
+					},
+				}
+			},
+			wantSfxDataPoints: []*sfxpb.DataPoint{
+				doubleSFxDataPoint(
+					"gauge_double_with_dims",
+					tsMSecs,
+					&sfxMetricTypeGauge,
+					append([]string{"gcp_id", "k_r0", "k_r1"}, keys...),
+					append([]string{"efgh_abcd", "vr0", "vr1"}, values...),
+					doubleVal),
+			},
+		},
+		{
 			name: "distributions",
 			metricsDataFn: func() []consumerdata.MetricsData {
 				return []consumerdata.MetricsData{
