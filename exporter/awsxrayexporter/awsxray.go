326cf00b08f805b7db91bf7d00cc47cc4d95e7bc
Cong Zou <32532612+EdZou@users.noreply.github.com>
2020-08-30 20:56:45 -0500

    refactor debug info printing

--- exporter/awsxrayexporter/awsxray.go
+++ exporter/awsxrayexporter/awsxray.go
@@ -87,10 +87,9 @@ func NewTraceExporter(config configmodels.Exporter, logger *zap.Logger, cn connA
 				input := xray.PutTraceSegmentsInput{TraceSegmentDocuments: documents[offset:nextOffset]}
 				logger.Debug("request: " + input.String())
 				output, localErr := xrayClient.PutTraceSegments(&input)
-				if localErr != nil && !config.(*Config).LocalMode {
-					err = wrapErrorIfBadRequest(&localErr) // not test mode, so record error
+				if localErr != nil {
+					err = wrapErrorIfBadRequest(&localErr) // record error
 				}
-				logger.Debug("localErr: " + localErr.Error())
 				if output != nil {
 					logger.Debug("response: " + output.String())
 					if output.UnprocessedTraceSegments != nil {
