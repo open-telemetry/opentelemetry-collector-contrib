5d9e1181aba8c4421c04dcb1cac7ab410bb149a1
yyyogev <yogev.metzuyanim@logz.io>
2020-08-31 14:24:25 +0300

    rename variable
    Signed-off-by: yyyogev <yogev.metzuyanim@logz.io>

--- exporter/logzioexporter/config_test.go
+++ exporter/logzioexporter/config_test.go
@@ -10,23 +10,23 @@ import (
 	"testing"
 )
 
-func TestLoadConfig(t *testing.T) {
+func TestLoadConfig(tester *testing.T) {
 	factories, err := componenttest.ExampleComponents()
-	assert.Nil(t, err)
+	assert.Nil(tester, err)
 
 	factory := NewFactory()
 	factories.Exporters[configmodels.Type(typeStr)] = factory
 	cfg, err := configtest.LoadConfigFile(
-		t, path.Join(".", "testdata", "config.yaml"), factories,
+		tester, path.Join(".", "testdata", "config.yaml"), factories,
 	)
 
-	require.NoError(t, err)
-	require.NotNil(t, cfg)
+	require.NoError(tester, err)
+	require.NotNil(tester, cfg)
 
-	assert.Equal(t, len(cfg.Exporters), 1)
+	assert.Equal(tester, len(cfg.Exporters), 1)
 
 	config := cfg.Exporters["logzio"].(*Config)
-	assert.Equal(t, config, &Config{
+	assert.Equal(tester, config, &Config{
 		ExporterSettings: configmodels.ExporterSettings{TypeVal: typeStr, NameVal: typeStr},
 		Token:            "logzioTESTtoken",
 		Region:           "eu",
