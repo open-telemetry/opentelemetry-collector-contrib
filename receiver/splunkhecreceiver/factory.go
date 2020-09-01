36d112224dd8bd2ee531c0a67bc8efe79713ae8c
Antoine Toulme <atoulme@users.noreply.github.com>
2020-09-01 10:38:27 -0700

    Add a receiver for Splunk HEC (#840)
    * Add a receiver for Splunk HEC
    
    * Address code review comments
    
    * Add a doc.go doc file
    
    * fix doc reference

---
+++ receiver/splunkhecreceiver/factory.go
@@ -0,0 +1,108 @@
+// Copyright 2019, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package splunkhecreceiver
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"strconv"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/config/configerror"
+	"go.opentelemetry.io/collector/config/confighttp"
+	"go.opentelemetry.io/collector/config/configmodels"
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/receiver/receiverhelper"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/internal/common/splunk"
+)
+
+// This file implements factory for Splunk HEC receiver.
+
+const (
+	// The value of "type" key in configuration.
+	typeStr = "splunk_hec"
+
+	// Default endpoints to bind to.
+	defaultEndpoint = ":8088"
+)
+
+// NewFactory creates a factory for SignalFx receiver.
+func NewFactory() component.ReceiverFactory {
+	return receiverhelper.NewFactory(
+		typeStr,
+		createDefaultConfig,
+		receiverhelper.WithMetrics(createMetricsReceiver))
+}
+
+// CreateDefaultConfig creates the default configuration for Splunk HEC receiver.
+func createDefaultConfig() configmodels.Receiver {
+	return &Config{
+		ReceiverSettings: configmodels.ReceiverSettings{
+			TypeVal: typeStr,
+			NameVal: typeStr,
+		},
+		HTTPServerSettings: confighttp.HTTPServerSettings{
+			Endpoint: defaultEndpoint,
+		},
+		AccessTokenPassthroughConfig: splunk.AccessTokenPassthroughConfig{},
+	}
+}
+
+// extract the port number from string in "address:port" format. If the
+// port number cannot be extracted returns an error.
+func extractPortFromEndpoint(endpoint string) (int, error) {
+	_, portStr, err := net.SplitHostPort(endpoint)
+	if err != nil {
+		return 0, fmt.Errorf("endpoint is not formatted correctly: %s", err.Error())
+	}
+	port, err := strconv.ParseInt(portStr, 10, 0)
+	if err != nil {
+		return 0, fmt.Errorf("endpoint port is not a number: %s", err.Error())
+	}
+	if port < 1 || port > 65535 {
+		return 0, fmt.Errorf("port number must be between 1 and 65535")
+	}
+	return int(port), nil
+}
+
+// verify that the configured port is not 0
+func (rCfg *Config) validate() error {
+	_, err := extractPortFromEndpoint(rCfg.Endpoint)
+	return err
+}
+
+// CreateTraceReceiver creates a trace receiver based on provided config.
+func createTraceReceiver(
+	ctx context.Context,
+	params component.ReceiverCreateParams,
+	cfg configmodels.Receiver,
+	consumer consumer.TraceConsumer,
+) (component.TraceReceiver, error) {
+
+	return nil, configerror.ErrDataTypeIsNotSupported
+}
+
+// CreateMetricsReceiver creates a metrics receiver based on provided config.
+func createMetricsReceiver(
+	_ context.Context,
+	params component.ReceiverCreateParams,
+	cfg configmodels.Receiver,
+	consumer consumer.MetricsConsumer,
+) (component.MetricsReceiver, error) {
+
+	return nil, configerror.ErrDataTypeIsNotSupported
+}
---
+++ receiver/splunkhecreceiver/go.mod
@@ -0,0 +1,15 @@
+module github.com/open-telemetry/opentelemetry-collector-contrib/receiver/splunkhecreceiver
+
+go 1.14
+
+require (
+	github.com/open-telemetry/opentelemetry-collector-contrib/internal/common v0.0.0-00010101000000-000000000000
+	github.com/stretchr/testify v1.6.1
+	go.opentelemetry.io/collector v0.9.0
+	go.uber.org/zap v1.15.0
+	google.golang.org/grpc/examples v0.0.0-20200728194956-1c32b02682df // indirect
+)
+
+replace github.com/open-telemetry/opentelemetry-collector-contrib/exporter/splunkhecexporter => ../../exporter/splunkhecexporter
+
+replace github.com/open-telemetry/opentelemetry-collector-contrib/internal/common => ../../internal/common
