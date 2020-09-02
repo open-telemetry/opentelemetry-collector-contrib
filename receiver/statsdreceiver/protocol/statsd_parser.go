f20fd61b53a71b1db7858e41cecaf787a80a21f9
Nick Fischer <nicfisch@amazon.com>
2020-09-02 14:39:18 -0700

    Add label and gauge parsing

--- receiver/statsdreceiver/protocol/statsd_parser.go
+++ receiver/statsdreceiver/protocol/statsd_parser.go
@@ -33,8 +33,39 @@ var (
 // StatsDParser supports the Parse method for parsing StatsD messages with Tags.
 type StatsDParser struct{}
 
+type statsDMetric struct {
+	message          string
+	name             string
+	value            string
+	statsdMetricType string
+	metricType       metricspb.MetricDescriptor_Type
+	sampleRate       float64
+	labelKeys        []*metricspb.LabelKey
+	labelValues      []*metricspb.LabelValue
+}
+
+var timeNowFunc = func() int64 {
+	return time.Now().Unix()
+}
+
 // Parse returns an OTLP metric representation of the input StatsD string.
 func (p *StatsDParser) Parse(line string) (*metricspb.Metric, error) {
+	parsedMetric, err := parseMessageToMetric(line)
+	if err != nil {
+		return nil, err
+	}
+
+	metricPoint, err := buildPoint(parsedMetric)
+	if err != nil {
+		return nil, err
+	}
+
+	return buildMetric(parsedMetric, metricPoint), nil
+}
+
+func parseMessageToMetric(line string) (*statsDMetric, error) {
+	result := &statsDMetric{}
+
 	parts := strings.Split(line, "|")
 	if len(parts) < 2 {
 		return nil, fmt.Errorf("invalid message format: %s", line)
@@ -45,34 +76,66 @@ func (p *StatsDParser) Parse(line string) (*metricspb.Metric, error) {
 		return nil, fmt.Errorf("invalid <name>:<value> format: %s", parts[0])
 	}
 
-	metricName := parts[0][0:separatorIndex]
-	if metricName == "" {
+	result.name = parts[0][0:separatorIndex]
+	if result.name == "" {
 		return nil, errEmptyMetricName
 	}
-	metricValueString := parts[0][separatorIndex+1:]
-	if metricValueString == "" {
+	result.value = parts[0][separatorIndex+1:]
+	if result.value == "" {
 		return nil, errEmptyMetricValue
 	}
 
-	metricType := parts[1]
+	result.statsdMetricType = parts[1]
 
-	// TODO: add sample rate and tag parsing
+	additionalParts := parts[2:]
+	for _, part := range additionalParts {
+		// TODO: Sample rate doesn't currently have a place to go in the protocol
+		if strings.HasPrefix(part, "@") {
+			sampleRateStr := strings.TrimPrefix(part, "@")
 
-	metricPoint, err := buildPoint(metricType, metricValueString)
-	if err != nil {
-		return nil, err
+			f, err := strconv.ParseFloat(sampleRateStr, 64)
+			if err != nil {
+				return nil, fmt.Errorf("parse sample rate: %s", sampleRateStr)
+			}
+
+			result.sampleRate = f
+		} else if strings.HasPrefix(part, "#") {
+			tagsStr := strings.TrimPrefix(part, "#")
+
+			tagSets := strings.Split(tagsStr, ",")
+
+			result.labelKeys = make([]*metricspb.LabelKey, 0, len(tagSets))
+			result.labelValues = make([]*metricspb.LabelValue, 0, len(tagSets))
+
+			for _, tagSet := range tagSets {
+				tagParts := strings.Split(tagSet, ":")
+				if len(tagParts) != 2 {
+					return nil, fmt.Errorf("invalid tag format: %s", tagParts)
+				}
+				result.labelKeys = append(result.labelKeys, &metricspb.LabelKey{Key: tagParts[0]})
+				result.labelValues = append(result.labelValues, &metricspb.LabelValue{
+					Value:    tagParts[1],
+					HasValue: true,
+				})
+			}
+		} else {
+			return nil, fmt.Errorf("unrecognized message part: %s", part)
+		}
 	}
 
-	return buildMetric(metricName, metricPoint), nil
+	return result, nil
 }
 
-func buildMetric(metricName string, point *metricspb.Point) *metricspb.Metric {
+func buildMetric(metric *statsDMetric, point *metricspb.Point) *metricspb.Metric {
 	return &metricspb.Metric{
 		MetricDescriptor: &metricspb.MetricDescriptor{
-			Name: metricName,
+			Name:      metric.name,
+			Type:      metric.metricType,
+			LabelKeys: metric.labelKeys,
 		},
 		Timeseries: []*metricspb.TimeSeries{
 			{
+				LabelValues: metric.labelValues,
 				Points: []*metricspb.Point{
 					point,
 				},
@@ -81,29 +144,51 @@ func buildMetric(metricName string, point *metricspb.Point) *metricspb.Metric {
 	}
 }
 
-func buildPoint(metricType, metricValue string) (*metricspb.Point, error) {
+func buildPoint(parsedMetric *statsDMetric) (*metricspb.Point, error) {
 	point := &metricspb.Point{
 		Timestamp: &timestamp.Timestamp{
-			Seconds: time.Now().Unix(),
+			Seconds: timeNowFunc(),
 		},
 	}
 
-	switch metricType {
+	switch parsedMetric.statsdMetricType {
 	case "c":
-		// TODO: support both Int64 and Double values.
-		i, err := strconv.ParseInt(metricValue, 10, 64)
+		i, err := strconv.ParseInt(parsedMetric.value, 10, 64)
 		if err != nil {
-			f, err := strconv.ParseFloat(metricValue, 64)
+			f, err := strconv.ParseFloat(parsedMetric.value, 64)
 			if err != nil {
-				return nil, fmt.Errorf("parse metric value string: %s", metricValue)
+				return nil, fmt.Errorf("parse metric value string: %s", parsedMetric.value)
 			}
-			i = int64(f)
+			parsedMetric.metricType = metricspb.MetricDescriptor_CUMULATIVE_DOUBLE
+			point.Value = &metricspb.Point_DoubleValue{
+				DoubleValue: f,
+			}
+		} else {
+			parsedMetric.metricType = metricspb.MetricDescriptor_CUMULATIVE_INT64
+			point.Value = &metricspb.Point_Int64Value{
+				Int64Value: i,
+			}
 		}
-		point.Value = &metricspb.Point_Int64Value{
-			Int64Value: i,
+	case "g":
+		i, err := strconv.ParseInt(parsedMetric.value, 10, 64)
+		if err != nil {
+			f, err := strconv.ParseFloat(parsedMetric.value, 64)
+			if err != nil {
+				return nil, fmt.Errorf("parse metric value string: %s", parsedMetric.value)
+			}
+			parsedMetric.metricType = metricspb.MetricDescriptor_GAUGE_DOUBLE
+			point.Value = &metricspb.Point_DoubleValue{
+				DoubleValue: f,
+			}
+		} else {
+			parsedMetric.metricType = metricspb.MetricDescriptor_GAUGE_INT64
+			point.Value = &metricspb.Point_Int64Value{
+				Int64Value: i,
+			}
 		}
+	// TODO: handle "h", "ms" message types
 	default:
-		return nil, fmt.Errorf("unhandled metric type: %s", metricType)
+		return nil, fmt.Errorf("unhandled metric type: %s", parsedMetric.statsdMetricType)
 	}
 
 	return point, nil
