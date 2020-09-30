1dcfe8fdff4f383ebd081cb3f3097d7340179eff
yyyogev <yogev.metzuyanim@logz.io>
2020-09-30 20:19:22 +0300

    implement trace exporter
    Signed-off-by: yyyogev <yogev.metzuyanim@logz.io>

--- exporter/logzioexporter/exporter.go
+++ exporter/logzioexporter/exporter.go
@@ -15,9 +15,97 @@
 package logzioexporter
 
 import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/hashicorp/go-hclog"
+	"github.com/jaegertracing/jaeger/model"
+	"github.com/logzio/jaeger-logzio/store"
 	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/consumer/pdata"
+	"go.opentelemetry.io/collector/exporter/exporterhelper"
+	"go.opentelemetry.io/collector/translator/trace/jaeger"
 )
 
+const (
+	loggerName = "logzio-exporter"
+)
+
+// logzioExporter implements an OpenTelemetry trace exporter that exports all spans to Logz.io
+type logzioExporter struct {
+	accountToken string
+	writer       *store.LogzioSpanWriter
+	logger       hclog.Logger
+}
+
+var WriteSpanFunc func(span *model.Span) error
+var InternalTracesToJaegerTraces = jaeger.InternalTracesToJaegerProto
+
+func newLogzioExporter(config *Config, params component.ExporterCreateParams) (*logzioExporter, error) {
+	logger := hclog.New(&hclog.LoggerOptions{
+		Level:      hclog.Info,
+		Name:       loggerName,
+		JSONFormat: true,
+	})
+
+	if config == nil {
+		return nil, errors.New("exporter config can't be null")
+	}
+	writerConfig := store.LogzioConfig{
+		Region:            config.Region,
+		AccountToken:      config.Token,
+		CustomListenerURL: config.CustomEndpoint,
+	}
+
+	spanWriter, err := store.NewLogzioSpanWriter(writerConfig, logger)
+	if err != nil {
+		return nil, err
+	}
+
+	return &logzioExporter{
+		writer:       spanWriter,
+		accountToken: config.Token,
+		logger:       logger,
+	}, nil
+}
+
 func newLogzioTraceExporter(config *Config, params component.ExporterCreateParams) (component.TraceExporter, error) {
-	return nil, nil
+	exporter, err := newLogzioExporter(config, params)
+	if err != nil {
+		return nil, err
+	}
+	WriteSpanFunc = exporter.writer.WriteSpan
+	if err := config.validate(); err != nil {
+		return nil, err
+	}
+
+	return exporterhelper.NewTraceExporter(
+		config,
+		exporter.pushTraceData,
+		exporterhelper.WithShutdown(exporter.Shutdown))
+}
+
+func (exporter *logzioExporter) pushTraceData(ctx context.Context, traces pdata.Traces) (droppedSpansCount int, err error) {
+	droppedSpans := 0
+	batches, err := InternalTracesToJaegerTraces(traces)
+	if err != nil {
+		return traces.SpanCount(), err
+	}
+	for _, batch := range batches {
+		for _, span := range batch.Spans {
+			span.Process = batch.Process
+			if err := WriteSpanFunc(span); err != nil {
+				exporter.logger.Debug(fmt.Sprintf("dropped bad span: %s", span.String()))
+				droppedSpans++
+			}
+		}
+	}
+	return droppedSpans, nil
+}
+
+func (exporter *logzioExporter) Shutdown(ctx context.Context) error {
+	exporter.logger.Info("Closing logzio exporter..")
+	exporter.writer.Close()
+	return nil
 }
