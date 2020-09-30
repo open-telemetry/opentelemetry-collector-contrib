5d4a5b5f92ef3e740ddb6f3855f4dac07a3ec308
Leighton Chen <lechen@microsoft.com>
2020-09-30 10:36:57 -0400

    Use is_recording flag in flask, django, tornado, boto, botocore instrumentations (#1164)

--- instrumentation/opentelemetry-instrumentation-botocore/tests/test_botocore_instrumentation.py
+++ instrumentation/opentelemetry-instrumentation-botocore/tests/test_botocore_instrumentation.py
@@ -1,3 +1,19 @@
+# Copyright The OpenTelemetry Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from unittest.mock import Mock, patch
+
 import botocore.session
 from botocore.exceptions import ParamValidationError
 from moto import (  # pylint: disable=import-error
@@ -60,6 +76,23 @@ class TestBotocoreInstrumentor(TestBase):
             ),
         )
         self.assertEqual(span.name, "ec2.command")
+
+    @mock_ec2
+    def test_not_recording(self):
+        mock_tracer = Mock()
+        mock_span = Mock()
+        mock_span.is_recording.return_value = False
+        mock_tracer.start_span.return_value = mock_span
+        mock_tracer.use_span.return_value.__enter__ = mock_span
+        mock_tracer.use_span.return_value.__exit__ = mock_span
+        with patch("opentelemetry.trace.get_tracer") as tracer:
+            tracer.return_value = mock_tracer
+            ec2 = self.session.create_client("ec2", region_name="us-west-2")
+            ec2.describe_instances()
+            self.assertFalse(mock_span.is_recording())
+            self.assertTrue(mock_span.is_recording.called)
+            self.assertFalse(mock_span.set_attribute.called)
+            self.assertFalse(mock_span.set_status.called)
 
     @mock_ec2
     def test_traced_client_analytics(self):
