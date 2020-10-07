d7d3e23844b9a4e1711473d6d8e0eaf32f5659be
Dominik Rosiek <drosiek@sumologic.com>
2020-10-07 12:54:33 +0200

    Init Sumo Logic's exporter

---
+++ exporter/sumologicexporter/exporter.go
@@ -0,0 +1,61 @@
+// Copyright 2020 OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sumologicexporter
+
+import (
+	"context"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/consumer/pdata"
+	"go.opentelemetry.io/collector/exporter/exporterhelper"
+)
+
+type sumologicexporter struct {
+	config *Config
+}
+
+func initExporter(cfg *Config) (*sumologicexporter, error) {
+	se := &sumologicexporter{
+		config: cfg,
+	}
+
+	return se, nil
+}
+
+func newLogsExporter(
+	cfg *Config,
+	params component.ExporterCreateParams,
+) (component.LogsExporter, error) {
+	se, err := initExporter(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return exporterhelper.NewLogsExporter(
+		cfg,
+		params.Logger,
+		se.pushLogsData,
+		// Disable exporterhelper Timeout, since we are using a custom mechanism
+		// within exporter itself
+		exporterhelper.WithTimeout(exporterhelper.TimeoutSettings{Timeout: 0}),
+		exporterhelper.WithRetry(cfg.RetrySettings),
+		exporterhelper.WithQueue(cfg.QueueSettings),
+	)
+}
+
+// pushLogsData groups data with common metadata and send them together to Sumo Logic
+func (se *sumologicexporter) pushLogsData(context.Context, pdata.Logs) (droppedTimeSeries int, err error) {
+	return 0, nil
+}
---
+++ exporter/sumologicexporter/go.mod
@@ -0,0 +1,10 @@
+module github.com/open-telemetry/opentelemetry-collector-contrib/exporter/sumologicexporter
+
+go 1.14
+
+require (
+	github.com/docker/docker v17.12.0-ce-rc1.0.20200706150819-a40b877fbb9e+incompatible
+	github.com/stretchr/testify v1.6.1
+	go.opentelemetry.io/collector v0.13.1-0.20201103222843-de104da4f136
+	go.uber.org/zap v1.16.0
+)
