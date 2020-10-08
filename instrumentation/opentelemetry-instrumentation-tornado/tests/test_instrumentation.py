f2e6b55a29909dfd483bce62eb8e6570ded66267
Leighton Chen <lechen@microsoft.com>
2020-10-08 15:25:20 -0400

    Use is_recording flag in aiopg, asyncpg, dbapi, psycopg2, pymemcache, pymongo, redis, sqlalchemy instrumentations (#1212)

--- instrumentation/opentelemetry-instrumentation-tornado/tests/test_instrumentation.py
+++ instrumentation/opentelemetry-instrumentation-tornado/tests/test_instrumentation.py
@@ -138,7 +138,7 @@ class TestTornadoInstrumentation(TornadoTest):
         mock_span.is_recording.return_value = False
         mock_tracer.start_span.return_value = mock_span
         mock_tracer.use_span.return_value.__enter__ = mock_span
-        mock_tracer.use_span.return_value.__exit__ = mock_span
+        mock_tracer.use_span.return_value.__exit__ = True
         with patch("opentelemetry.trace.get_tracer") as tracer:
             tracer.return_value = mock_tracer
             self.fetch("/")
