dfb0621a9b456ea4eaf65a1fd0ff12a0ab7aac3b
Leighton Chen <lechen@microsoft.com>
2020-10-08 15:25:20 -0400

    Use is_recording flag in aiopg, asyncpg, dbapi, psycopg2, pymemcache, pymongo, redis, sqlalchemy instrumentations (#1212)

--- instrumentation/opentelemetry-instrumentation-redis/tests/test_redis.py
+++ instrumentation/opentelemetry-instrumentation-redis/tests/test_redis.py
@@ -34,6 +34,25 @@ class TestRedis(TestBase):
         self.assertEqual(span.name, "redis.command")
         self.assertEqual(span.kind, SpanKind.CLIENT)
 
+    def test_not_recording(self):
+        redis_client = redis.Redis()
+        RedisInstrumentor().instrument(tracer_provider=self.tracer_provider)
+
+        mock_tracer = mock.Mock()
+        mock_span = mock.Mock()
+        mock_span.is_recording.return_value = False
+        mock_tracer.start_span.return_value = mock_span
+        mock_tracer.use_span.return_value.__enter__ = mock_span
+        mock_tracer.use_span.return_value.__exit__ = True
+        with mock.patch("opentelemetry.trace.get_tracer") as tracer:
+            with mock.patch.object(redis_client, "connection"):
+                tracer.return_value = mock_tracer
+                redis_client.get("key")
+                self.assertFalse(mock_span.is_recording())
+                self.assertTrue(mock_span.is_recording.called)
+                self.assertFalse(mock_span.set_attribute.called)
+                self.assertFalse(mock_span.set_status.called)
+
     def test_instrument_uninstrument(self):
         redis_client = redis.Redis()
         RedisInstrumentor().instrument(tracer_provider=self.tracer_provider)
