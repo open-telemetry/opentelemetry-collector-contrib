975729b8c3b49215186cb38a15753aed79b41319
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2020-10-16 12:07:05 +0200

    Add cache package This will be used to cache metadata in-memory

--- exporter/datadogexporter/go.mod
+++ exporter/datadogexporter/go.mod
@@ -9,12 +9,13 @@ require (
 	github.com/cihub/seelog v0.0.0-20170130134532-f561c5e57575 // indirect
 	github.com/gogo/protobuf v1.3.1
 	github.com/klauspost/compress v1.10.10
+	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/stretchr/testify v1.6.1
 	github.com/zorkian/go-datadog-api v2.29.0+incompatible // indirect
+	go.opencensus.io v0.22.4
 	go.opentelemetry.io/collector v0.12.1-0.20201012183541-526f34200197
 	go.uber.org/zap v1.16.0
-	gopkg.in/zorkian/go-datadog-api.v2 v2.29.0
-	go.opencensus.io v0.22.4
 	google.golang.org/protobuf v1.25.0
 	gopkg.in/DataDog/dd-trace-go.v1 v1.26.0
+	gopkg.in/zorkian/go-datadog-api.v2 v2.29.0
 )
---
+++ exporter/datadogexporter/utils/cache/cache.go
@@ -0,0 +1,47 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cache
+
+import (
+	"time"
+
+	gocache "github.com/patrickmn/go-cache"
+)
+
+const (
+	CanonicalHostnameKey = "canonical_hostname"
+)
+
+var generalCache = gocache.New(20*time.Minute, 10*time.Minute)
+
+// SetNoExpire saves a value in cache indefinitely
+func SetNoExpire(key string, val interface{}) {
+	generalCache.Add(key, val, gocache.NoExpiration)
+}
+
+// SetExpire saves a value in cache for the default time
+func SetExpire(key string, val interface{}) {
+	generalCache.Add(key, val, gocache.DefaultExpiration)
+}
+
+// Get retrieves a value from cache
+func Get(key string) (interface{}, bool) {
+	return generalCache.Get(key)
+}
+
+// Delete retrieves a value from cache
+func Delete(key string) {
+	generalCache.Delete(key)
+}
