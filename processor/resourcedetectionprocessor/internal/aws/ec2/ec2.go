81daedc4c6fdcc02a4512ccd0d63027d3fa0cd89
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2020-10-21 09:59:26 +0200

    Address review comment

--- processor/resourcedetectionprocessor/internal/aws/ec2/ec2.go
+++ processor/resourcedetectionprocessor/internal/aws/ec2/ec2.go
@@ -16,6 +16,7 @@ package ec2
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/aws/aws-sdk-go/aws/session"
 	"go.opentelemetry.io/collector/consumer/pdata"
@@ -53,12 +54,12 @@ func (d *Detector) Detect(ctx context.Context) (pdata.Resource, error) {
 
 	meta, err := d.provider.get(ctx)
 	if err != nil {
-		return res, err
+		return res, fmt.Errorf("failed getting identity document: %w", err)
 	}
 
 	hostname, err := d.provider.hostname(ctx)
 	if err != nil {
-		return res, err
+		return res, fmt.Errorf("failed getting hostname: %w", err)
 	}
 
 	attr := res.Attributes()
