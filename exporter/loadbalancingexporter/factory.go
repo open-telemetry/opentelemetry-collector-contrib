8a6b76cf75441e57eae579779f6ba8482a44c514
Juraci Paixão Kröhling <juraci@kroehling.de>
2020-10-28 16:48:39 +0100

    Trace ID aware load-balancing exporter - 1/4 (#1348)
    Description:
    
        Initial skeleton
        README
    
    Link to tracking Issue: Partially solves #1724
    
    Testing: unit tests
    
    Documentation: README
    
    Signed-off-by: Juraci Paixão Kröhling juraci@kroehling.de

---
+++ exporter/loadbalancingexporter/factory.go
@@ -0,0 +1,50 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package loadbalancingexporter
+
+import (
+	"context"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/config/configmodels"
+	"go.opentelemetry.io/collector/exporter/exporterhelper"
+)
+
+const (
+	// The value of "type" key in configuration.
+	typeStr = "loadbalancing"
+)
+
+// NewFactory creates a factory for the exporter.
+func NewFactory() component.ExporterFactory {
+	return exporterhelper.NewFactory(
+		typeStr,
+		createDefaultConfig,
+		exporterhelper.WithTraces(createTraceExporter),
+	)
+}
+
+func createDefaultConfig() configmodels.Exporter {
+	return &Config{
+		ExporterSettings: configmodels.ExporterSettings{
+			TypeVal: typeStr,
+			NameVal: typeStr,
+		},
+	}
+}
+
+func createTraceExporter(_ context.Context, params component.ExporterCreateParams, cfg configmodels.Exporter) (component.TraceExporter, error) {
+	return newExporter(params, cfg)
+}
---
+++ exporter/loadbalancingexporter/go.mod
@@ -0,0 +1,9 @@
+module github.com/open-telemetry/opentelemetry-collector-contrib/exporter/loadbalancingexporter
+
+go 1.14
+
+require (
+	github.com/stretchr/testify v1.6.1
+	go.opentelemetry.io/collector v0.13.0
+	go.uber.org/zap v1.16.0
+)
