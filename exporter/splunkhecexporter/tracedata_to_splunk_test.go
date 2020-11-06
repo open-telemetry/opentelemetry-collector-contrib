422a647a05577b7577f3d19b5551ab2eda62637f
Antoine Toulme <atoulme@users.noreply.github.com>
2020-11-06 11:00:54 -0800

    Stop using internaldata (#1429)
    * Remove use of internaldata as much as possible. Map source and sourcetype to exported events.
    
    * Only translate to internal data once
    
    * Refactor the metric mapping to make it easier to read
    
    * Map manually spans
    
    * code review comments
    
    * Cache data points into a variable
    
    * Add more nil checks and variable assignments to cache iterating over slices
    
    * code review changes
    
    * intitialize slice
    
    * assign variable when iterating
    
    * Add link and event to span to get more test coverage
    
    * Add more tests to check if a metric has nil elements
    
    * Add tests for sums
    
    * Add more tests for complete coverage, add fields for spans
    
    * Fix trace export test
    
    * more coverage for nil points.
    
    * comment out untestable code to get past coverage threshold, to be uncommented in a separate PR once core is updated
    
    * Revert "comment out untestable code to get past coverage threshold, to be uncommented in a separate PR once core is updated"
    
    This reverts commit a8552c0959461fcd832426db63d1d04a6af1246c.
    
    * Add tests for nil metric
    
    * Bring to compat with 0.14 API

--- exporter/splunkhecexporter/tracedata_to_splunk_test.go
+++ exporter/splunkhecexporter/tracedata_to_splunk_test.go
@@ -15,39 +15,44 @@
 package splunkhecexporter
 
 import (
+	"encoding/hex"
 	"testing"
 
-	v1 "github.com/census-instrumentation/opencensus-proto/gen-go/trace/v1"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"go.opentelemetry.io/collector/consumer/consumerdata"
-	"go.opentelemetry.io/collector/translator/internaldata"
+	"go.opentelemetry.io/collector/consumer/pdata"
 	"go.uber.org/zap"
-	"google.golang.org/protobuf/types/known/timestamppb"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/internal/splunk"
 )
 
 func Test_traceDataToSplunk(t *testing.T) {
 	logger := zap.NewNop()
-	ts := &timestamppb.Timestamp{
-		Nanos: 123,
-	}
+	ts := pdata.TimestampUnixNano(123)
 
 	tests := []struct {
 		name                string
-		traceDataFn         func() consumerdata.TraceData
+		traceDataFn         func() pdata.Traces
 		wantSplunkEvents    []*splunk.Event
 		wantNumDroppedSpans int
 	}{
 		{
 			name: "valid",
-			traceDataFn: func() consumerdata.TraceData {
-				return consumerdata.TraceData{
-					Spans: []*v1.Span{
-						makeSpan("myspan", ts),
-					},
-				}
+			traceDataFn: func() pdata.Traces {
+				traces := pdata.NewTraces()
+				rs := pdata.NewResourceSpans()
+				rs.InitEmpty()
+				rs.Resource().InitEmpty()
+				rs.Resource().Attributes().InsertString("service.name", "myservice")
+				rs.Resource().Attributes().InsertString("host.hostname", "myhost")
+				rs.Resource().Attributes().InsertString("com.splunk.sourcetype", "mysourcetype")
+
+				ils := pdata.NewInstrumentationLibrarySpans()
+				ils.InitEmpty()
+				ils.Spans().Append(makeSpan("myspan", &ts))
+				rs.InstrumentationLibrarySpans().Append(ils)
+				traces.ResourceSpans().Append(rs)
+				return traces
 			},
 			wantSplunkEvents: []*splunk.Event{
 				commonSplunkEvent("myspan", ts),
@@ -55,21 +60,63 @@ func Test_traceDataToSplunk(t *testing.T) {
 			wantNumDroppedSpans: 0,
 		},
 		{
-			name: "missing_start_ts",
-			traceDataFn: func() consumerdata.TraceData {
-				return consumerdata.TraceData{
-					Spans: []*v1.Span{
-						makeSpan("myspan", nil),
-					},
-				}
+			name: "nil_rs",
+			traceDataFn: func() pdata.Traces {
+				traces := pdata.NewTraces()
+				rs := pdata.NewResourceSpans()
+				traces.ResourceSpans().Append(rs)
+				return traces
 			},
 			wantSplunkEvents:    []*splunk.Event{},
-			wantNumDroppedSpans: 1,
+			wantNumDroppedSpans: 0,
+		},
+		{
+			name: "nil_ils",
+			traceDataFn: func() pdata.Traces {
+				traces := pdata.NewTraces()
+				rs := pdata.NewResourceSpans()
+				rs.InitEmpty()
+				rs.Resource().InitEmpty()
+				rs.Resource().Attributes().InsertString("service.name", "myservice")
+				rs.Resource().Attributes().InsertString("host.hostname", "myhost")
+				rs.Resource().Attributes().InsertString("com.splunk.sourcetype", "mysourcetype")
+
+				ils := pdata.NewInstrumentationLibrarySpans()
+				rs.InstrumentationLibrarySpans().Append(ils)
+				traces.ResourceSpans().Append(rs)
+				return traces
+			},
+			wantSplunkEvents:    []*splunk.Event{},
+			wantNumDroppedSpans: 0,
+		},
+		{
+			name: "nil_span",
+			traceDataFn: func() pdata.Traces {
+				traces := pdata.NewTraces()
+				rs := pdata.NewResourceSpans()
+				rs.InitEmpty()
+				rs.Resource().InitEmpty()
+				rs.Resource().Attributes().InsertString("service.name", "myservice")
+				rs.Resource().Attributes().InsertString("host.hostname", "myhost")
+				rs.Resource().Attributes().InsertString("com.splunk.sourcetype", "mysourcetype")
+
+				ils := pdata.NewInstrumentationLibrarySpans()
+				ils.InitEmpty()
+				nilSpan := pdata.NewSpan()
+				ils.Spans().Append(nilSpan)
+				rs.InstrumentationLibrarySpans().Append(ils)
+				traces.ResourceSpans().Append(rs)
+				return traces
+			},
+			wantSplunkEvents:    []*splunk.Event{},
+			wantNumDroppedSpans: 0,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotEvents, gotNumDroppedSpans := traceDataToSplunk(logger, internaldata.OCToTraceData(tt.traceDataFn()), &Config{})
+			traces := tt.traceDataFn()
+
+			gotEvents, gotNumDroppedSpans := traceDataToSplunk(logger, traces, &Config{})
 			assert.Equal(t, tt.wantNumDroppedSpans, gotNumDroppedSpans)
 			require.Equal(t, len(tt.wantSplunkEvents), len(gotEvents))
 			for i, want := range tt.wantSplunkEvents {
@@ -80,27 +127,81 @@ func Test_traceDataToSplunk(t *testing.T) {
 	}
 }
 
-func makeSpan(name string, ts *timestamppb.Timestamp) *v1.Span {
-	trunceableName := &v1.TruncatableString{
-		Value: name,
+func makeSpan(name string, ts *pdata.TimestampUnixNano) pdata.Span {
+	span := pdata.NewSpan()
+	span.InitEmpty()
+	span.Attributes().InsertString("foo", "bar")
+	span.SetName(name)
+	if ts != nil {
+		span.SetStartTime(*ts)
 	}
-	return &v1.Span{
-		Name:      trunceableName,
-		StartTime: ts,
+	spanLink := pdata.NewSpanLink()
+	spanLink.InitEmpty()
+	spanLink.SetTraceState(pdata.TraceState("OK"))
+	bytes, _ := hex.DecodeString("12345678")
+	var traceID [16]byte
+	copy(traceID[:], bytes)
+	spanLink.SetTraceID(pdata.NewTraceID(traceID))
+	bytes, _ = hex.DecodeString("1234")
+	var spanID [8]byte
+	copy(spanID[:], bytes)
+	spanLink.SetSpanID(pdata.NewSpanID(spanID))
+	spanLink.Attributes().InsertInt("foo", 1)
+	spanLink.Attributes().InsertBool("bar", false)
+	foobarContents := pdata.NewAttributeValueArray()
+	foobarContents.ArrayVal().Append(pdata.NewAttributeValueString("a"))
+	foobarContents.ArrayVal().Append(pdata.NewAttributeValueString("b"))
+	spanLink.Attributes().Insert("foobar", foobarContents)
+	span.Links().Append(spanLink)
+
+	spanEvent := pdata.NewSpanEvent()
+	spanEvent.InitEmpty()
+	spanEvent.Attributes().InsertString("foo", "bar")
+	spanEvent.SetName("myEvent")
+	if ts != nil {
+		spanEvent.SetTimestamp(*ts + 3)
 	}
+	span.Events().Append(spanEvent)
+	return span
 }
 
 func commonSplunkEvent(
 	name string,
-	ts *timestamppb.Timestamp,
+	ts pdata.TimestampUnixNano,
 ) *splunk.Event {
-	trunceableName := &v1.TruncatableString{
-		Value: name,
-	}
-	span := v1.Span{Name: trunceableName, StartTime: ts}
 	return &splunk.Event{
-		Time:  timestampToEpochMilliseconds(ts),
-		Host:  "unknown",
-		Event: &span,
+		Time:       timestampToSecondsWithMillisecondPrecision(ts),
+		Host:       "myhost",
+		Source:     "myservice",
+		SourceType: "mysourcetype",
+		Event: HecSpan{Name: name, StartTime: ts,
+			TraceID:    "",
+			SpanID:     "",
+			ParentSpan: "",
+			Attributes: map[string]interface{}{
+				"foo": "bar",
+			},
+			EndTime: 0x0,
+			Kind:    "SPAN_KIND_UNSPECIFIED",
+			Status:  HecSpanStatus{Message: "", Code: ""},
+			Events: []HecEvent{
+				{
+					Attributes: map[string]interface{}{"foo": "bar"},
+					Name:       "myEvent",
+					Timestamp:  ts + 3,
+				},
+			},
+			Links: []HecLink{
+				{
+					Attributes: map[string]interface{}{"foo": int64(1), "bar": false, "foobar": []interface{}{"a", "b"}},
+					TraceID:    "12345678000000000000000000000000",
+					SpanID:     "1234000000000000",
+					TraceState: "OK",
+				},
+			},
+		},
+		Fields: map[string]interface{}{
+			"com.splunk.sourcetype": "mysourcetype", "host.hostname": "myhost", "service.name": "myservice",
+		},
 	}
 }
