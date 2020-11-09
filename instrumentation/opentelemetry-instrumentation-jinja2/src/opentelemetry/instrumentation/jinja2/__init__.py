507b41a9965e9c0d3db15ff118f1289582a94fdb
VictoryWekwa <victorywekwa@gmail.com>
2020-11-09 16:47:55 +0100

    Remove Unused Import in jinja2/ Victory Wekwa <victorywekwa@gmail.com>
    Signed-off-by: VictoryWekwa <victorywekwa@gmail.com>

--- instrumentation/opentelemetry-instrumentation-jinja2/src/opentelemetry/instrumentation/jinja2/__init__.py
+++ instrumentation/opentelemetry-instrumentation-jinja2/src/opentelemetry/instrumentation/jinja2/__init__.py
@@ -52,7 +52,6 @@ from opentelemetry.instrumentation.instrumentor import BaseInstrumentor
 from opentelemetry.instrumentation.jinja2.version import __version__
 from opentelemetry.instrumentation.utils import unwrap
 from opentelemetry.trace import SpanKind, get_tracer
-from opentelemetry.trace.status import Status, StatusCode
 
 logger = logging.getLogger(__name__)
 
