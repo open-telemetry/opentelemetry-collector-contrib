39592d17f962b5c6831b7f5993faa0d35a468555
Dominik Rosiek <58699848+sumo-drosiek@users.noreply.github.com>
2020-11-20 19:29:49 +0100

    [sumologicexporter] Initialise Sumo Logic exporter (#1565)
    * Init Sumo Logic's exporter
    
    * Update configuration to reflect designed functionalities
    
    * Add README.md
    
    * Fix lint
    
    * Add config verification
    
    * Use assert.EqualError
    
    * Use confighttp.HTTPClientSettings to provide endpoint and timeout
    
    * Remove Compress configuration option
    
    To disable compression empty string ("") should be used
    
    * Update exporter/sumologicexporter/README.md
    
    Co-authored-by: Juraci Paixão Kröhling <juraci.github@kroehling.de>
    
    * Update go modules
    
    * Fix default MaxRequestBodySize
    
    * Add sumologicexporter to CODEOWNERS
    
    * Add sumologicexporter to dependabot
    
    * Fix due to review
    
    * Bump collector to v0.14.1-0.20201112191733-c6d9a2be2223
    
    Co-authored-by: Juraci Paixão Kröhling <juraci.github@kroehling.de>

---
+++ exporter/sumologicexporter/factory.go
@@ -0,0 +1,76 @@
+// Copyright 2020 OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sumologicexporter
+
+import (
+	"context"
+	"fmt"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/config/configmodels"
+	"go.opentelemetry.io/collector/exporter/exporterhelper"
+)
+
+const (
+	// The value of "type" key in configuration.
+	typeStr = "sumologic"
+)
+
+// NewFactory returns a new factory for the sumologic exporter.
+func NewFactory() component.ExporterFactory {
+	return exporterhelper.NewFactory(
+		typeStr,
+		createDefaultConfig,
+		exporterhelper.WithLogs(createLogsExporter),
+	)
+}
+
+func createDefaultConfig() configmodels.Exporter {
+	qs := exporterhelper.CreateDefaultQueueSettings()
+	qs.Enabled = false
+
+	return &Config{
+		ExporterSettings: configmodels.ExporterSettings{
+			TypeVal: configmodels.Type(typeStr),
+			NameVal: typeStr,
+		},
+
+		CompressEncoding:   DefaultCompressEncoding,
+		MaxRequestBodySize: DefaultMaxRequestBodySize,
+		LogFormat:          DefaultLogFormat,
+		MetricFormat:       DefaultMetricFormat,
+		SourceCategory:     DefaultSourceCategory,
+		SourceName:         DefaultSourceName,
+		SourceHost:         DefaultSourceHost,
+		Client:             DefaultClient,
+
+		HTTPClientSettings: CreateDefaultHTTPClientSettings(),
+		RetrySettings:      exporterhelper.CreateDefaultRetrySettings(),
+		QueueSettings:      qs,
+	}
+}
+
+func createLogsExporter(
+	_ context.Context,
+	params component.ExporterCreateParams,
+	cfg configmodels.Exporter,
+) (component.LogsExporter, error) {
+	exp, err := newLogsExporter(cfg.(*Config), params)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create the logs exporter: %w", err)
+	}
+
+	return exp, nil
+}
