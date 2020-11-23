ff179eb567e912ae3ba040ff40fc22ba160494d0
Dominik Rosiek <drosiek@sumologic.com>
2020-11-23 17:18:40 +0100

    Use helper to convert attribute to string

--- exporter/sumologicexporter/filter.go
+++ exporter/sumologicexporter/filter.go
@@ -18,10 +18,10 @@ import (
 	"fmt"
 	"regexp"
 	"sort"
-	"strconv"
 	"strings"
 
 	"go.opentelemetry.io/collector/consumer/pdata"
+	tracetranslator "go.opentelemetry.io/collector/translator/trace"
 )
 
 type filter struct {
@@ -48,29 +48,6 @@ func newFilter(fields []string) (filter, error) {
 	}, nil
 }
 
-// convertAttributeToString returns string which represents given pdata.AttributeValue
-func (f *filter) convertAttributeToString(value pdata.AttributeValue) string {
-	switch value.Type() {
-	case pdata.AttributeValueSTRING:
-		return value.StringVal()
-	case pdata.AttributeValueBOOL:
-		return strconv.FormatBool(value.BoolVal())
-	case pdata.AttributeValueINT:
-		return strconv.FormatInt(value.IntVal(), 10)
-	case pdata.AttributeValueDOUBLE:
-		return fmt.Sprintf("%g", value.DoubleVal())
-	// For now only flat structure of the attributes is supported
-	case pdata.AttributeValueMAP:
-		return ""
-	case pdata.AttributeValueARRAY:
-		return ""
-	case pdata.AttributeValueNULL:
-		return ""
-	default:
-		return ""
-	}
-}
-
 // filter returns map of strings which matches at least one of the filter regexes
 func (f *filter) filter(attributes pdata.AttributeMap) map[string]string {
 	returnValue := make(map[string]string)
@@ -78,7 +55,7 @@ func (f *filter) filter(attributes pdata.AttributeMap) map[string]string {
 	attributes.ForEach(func(k string, v pdata.AttributeValue) {
 		for _, regex := range f.regexes {
 			if regex.MatchString(k) {
-				returnValue[k] = f.convertAttributeToString(v)
+				returnValue[k] = tracetranslator.AttributeValueToString(v, true)
 				return
 			}
 		}
@@ -96,7 +73,7 @@ func (f *filter) filterOut(attributes pdata.AttributeMap) map[string]string {
 				return
 			}
 		}
-		returnValue[k] = f.convertAttributeToString(v)
+		returnValue[k] = tracetranslator.AttributeValueToString(v, true)
 	})
 	return returnValue
 }
