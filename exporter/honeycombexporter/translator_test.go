f62ffbed33b63574d89c85f271e629c44c11792e
Chris Smith <chris.smith@zocdoc.com>
2020-11-25 10:36:29 -0500

    Update honeycomb exporter to use internal data format (#1689)
    * Update honeycomb exporter to use internal data format
    
    This has several breaking changes to the events produced by the exporter
    - service_name -> service.name
    - process.hostname -> host.name
    - process.pid -> opencensus.pid (for census users)
    - opencensus.start_timestamp -> opencensus.starttime
    - has_remote_parent was removed
    - source_format was removed
    
    New features
    - other span_kinds are now supported (consumer, producer, internal)
    
    * lint
    
    * use pdata status codes instead of OC
    add test coverage
    add library information to events
    
    * fix import order
    
    * add test coverage

--- exporter/honeycombexporter/translator_test.go
+++ exporter/honeycombexporter/translator_test.go
@@ -18,47 +18,27 @@ import (
 	"testing"
 	"time"
 
-	tracepb "github.com/census-instrumentation/opencensus-proto/gen-go/trace/v1"
+	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/collector/consumer/pdata"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func TestSpanAttributesToMap(t *testing.T) {
 
-	newSpanAttr := func(key string, value *tracepb.AttributeValue, count int32) *tracepb.Span_Attributes {
-		return &tracepb.Span_Attributes{
-			AttributeMap: map[string]*tracepb.AttributeValue{
-				key: value,
-			},
-			DroppedAttributesCount: count,
-		}
-	}
-
-	spanAttrs := []*tracepb.Span_Attributes{
-		newSpanAttr("foo", &tracepb.AttributeValue{
-			Value: &tracepb.AttributeValue_StringValue{
-				StringValue: &tracepb.TruncatableString{Value: "bar"},
-			},
-		}, 0),
-		newSpanAttr("foo", &tracepb.AttributeValue{
-			Value: &tracepb.AttributeValue_IntValue{
-				IntValue: 1234,
-			},
-		}, 0),
-		newSpanAttr("foo", &tracepb.AttributeValue{
-			Value: &tracepb.AttributeValue_BoolValue{
-				BoolValue: true,
-			},
-		}, 0),
-		newSpanAttr("foo", &tracepb.AttributeValue{
-			Value: &tracepb.AttributeValue_DoubleValue{
-				DoubleValue: 0.3145,
-			},
-		}, 0),
-		nil,
-		{
-			AttributeMap:           nil,
-			DroppedAttributesCount: 0,
-		},
+	spanAttrs := []pdata.AttributeMap{
+		pdata.NewAttributeMap().InitFromMap(map[string]pdata.AttributeValue{
+			"foo": pdata.NewAttributeValueString("bar"),
+		}),
+		pdata.NewAttributeMap().InitFromMap(map[string]pdata.AttributeValue{
+			"foo": pdata.NewAttributeValueInt(1234),
+		}),
+		pdata.NewAttributeMap().InitFromMap(map[string]pdata.AttributeValue{
+			"foo": pdata.NewAttributeValueBool(true),
+		}),
+		pdata.NewAttributeMap().InitFromMap(map[string]pdata.AttributeValue{
+			"foo": pdata.NewAttributeValueDouble(0.3145),
+		}),
+		pdata.NewAttributeMap(),
 	}
 
 	wantResults := []map[string]interface{}{
@@ -84,19 +64,45 @@ func TestSpanAttributesToMap(t *testing.T) {
 
 func TestTimestampToTime(t *testing.T) {
 	var t1 time.Time
-	emptyTime := timestampToTime(nil)
+	emptyTime := timestampToTime(pdata.TimestampUnixNano(0))
 	if t1 != emptyTime {
 		t.Errorf("Expected %+v, Got: %+v\n", t1, emptyTime)
 	}
 
 	t2 := time.Now()
 	seconds := t2.UnixNano() / 1000000000
-	nowTime := timestampToTime(&timestamppb.Timestamp{
-		Seconds: seconds,
-		Nanos:   int32(t2.UnixNano() - (seconds * 1000000000)),
-	})
+	nowTime := timestampToTime(pdata.TimestampToUnixNano(
+		&timestamppb.Timestamp{
+			Seconds: seconds,
+			Nanos:   int32(t2.UnixNano() - (seconds * 1000000000)),
+		}))
 
 	if !t2.Equal(nowTime) {
 		t.Errorf("Expected %+v, Got %+v\n", t2, nowTime)
 	}
+}
+
+func TestStatusCode(t *testing.T) {
+	status := pdata.NewSpanStatus()
+	assert.Equal(t, int32(pdata.StatusCodeOk), getStatusCode(status), "uninitialized")
+
+	status.InitEmpty()
+	assert.Equal(t, int32(pdata.StatusCodeUnset), getStatusCode(status), "empty")
+
+	status.SetCode(pdata.StatusCodeError)
+	assert.Equal(t, int32(pdata.StatusCodeError), getStatusCode(status), "error")
+
+	status.SetCode(pdata.StatusCodeOk)
+	assert.Equal(t, int32(pdata.StatusCodeOk), getStatusCode(status), "ok")
+}
+
+func TestStatusMessage(t *testing.T) {
+	status := pdata.NewSpanStatus()
+	assert.Equal(t, "STATUS_CODE_OK", getStatusMessage(status), "uninitialized")
+
+	status.InitEmpty()
+	assert.Equal(t, "STATUS_CODE_OK", getStatusMessage(status), "empty")
+
+	status.SetMessage("custom message")
+	assert.Equal(t, "custom message", getStatusMessage(status), "custom")
 }
