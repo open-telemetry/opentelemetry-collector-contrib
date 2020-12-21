a10d252f33751f96eb18acfb195557b8aa06c2a6
Raphael Koh <kohrapha@amazon.com>
2020-12-21 18:00:29 -0500

    Add factory tests

---
+++ extension/observer/ecsobserver/factory_test.go
@@ -0,0 +1,52 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ecsobserver
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/config/configcheck"
+	"go.uber.org/zap"
+)
+
+func TestNewFactory(t *testing.T) {
+	f := NewFactory()
+	assert.NotNil(t, f)
+	assert.Equal(t, typeStr, f.Type())
+}
+
+func TestCreateDefaultConfig(t *testing.T) {
+	f := NewFactory()
+	cfg := f.CreateDefaultConfig()
+	assert.NoError(t, configcheck.ValidateConfig(cfg))
+}
+
+func TestCreateExtension(t *testing.T) {
+	f := NewFactory()
+	cfg := f.CreateDefaultConfig()
+	assert.NoError(t, configcheck.ValidateConfig(cfg))
+
+	ext, err := f.CreateExtension(
+		context.Background(),
+		component.ExtensionCreateParams{Logger: zap.NewNop()},
+		cfg,
+	)
+	require.NoError(t, err)
+	require.NotNil(t, ext)
+}
