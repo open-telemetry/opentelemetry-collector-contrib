0551fa0866098fe8a70362a7d047991c604880f6
Przemek Maciolek <58699843+pmm-sumo@users.noreply.github.com>
2021-01-05 16:57:43 +0100

    groupbyattrsprocessor - part 2 - grouping logic (#1744) (#1792)
    * groupbyattrsprocessor - part 2 (grouping logic) #1744
    
    * Address PR comments to groupbyattrsprocessor
    
    * Refactor groupbyauthprocessor to cover more scenarios
    
    * Add more attribute grouping tests
    
    * Update processor/groupbyattrsprocessor/processor.go
    
    Co-authored-by: Tigran Najaryan <4194920+tigrannajaryan@users.noreply.github.com>
    
    Co-authored-by: Tigran Najaryan <4194920+tigrannajaryan@users.noreply.github.com>

--- processor/groupbyattrsprocessor/processor.go
+++ processor/groupbyattrsprocessor/processor.go
@@ -21,20 +21,117 @@ import (
 	"go.uber.org/zap"
 )
 
-type groupbyattrsprocessor struct {
+type groupByAttrsProcessor struct {
 	logger      *zap.Logger
 	groupByKeys []string
 }
 
 // ProcessTraces process traces and groups traces by attribute.
-func (gap *groupbyattrsprocessor) ProcessTraces(_ context.Context, td pdata.Traces) (pdata.Traces, error) {
-	// TODO
+func (gap *groupByAttrsProcessor) ProcessTraces(_ context.Context, td pdata.Traces) (pdata.Traces, error) {
+	rss := td.ResourceSpans()
+	extractedGroups := newSpansGroupedByAttrs()
 
-	return td, nil
+	for i := 0; i < rss.Len(); i++ {
+		rs := rss.At(i)
+
+		ilss := rs.InstrumentationLibrarySpans()
+		for j := 0; j < ilss.Len(); j++ {
+			ils := ilss.At(j)
+			for k := 0; k < ils.Spans().Len(); k++ {
+				span := ils.Spans().At(k)
+
+				groupedAnything, groupedAttrMap := gap.splitAttrMap(span.Attributes())
+				if groupedAnything {
+					mNumGroupedSpans.M(1)
+					// Some attributes are going to be moved from span to resource level,
+					// so we can delete those on the record level
+					deleteAttributes(groupedAttrMap, span.Attributes())
+				} else {
+					mNumNonGroupedSpans.M(1)
+				}
+
+				// Lets combine the base resource attributes + the extracted (grouped) attributes
+				// and keep them in the grouping entry
+				groupedSpans := extractedGroups.attributeGroup(rs.Resource(), groupedAttrMap)
+				matchingInstrumentationLibrarySpans(groupedSpans, ils.InstrumentationLibrary()).Spans().Append(span)
+			}
+		}
+	}
+
+	// Copy the grouped data into output
+	groupedTraces := pdata.NewTraces()
+	groupedResourceSpans := groupedTraces.ResourceSpans()
+	for _, eg := range *extractedGroups {
+		groupedResourceSpans.Append(eg)
+	}
+	mDistSpanGroups.M(int64(len(*extractedGroups)))
+
+	return groupedTraces, nil
 }
 
-func (gap *groupbyattrsprocessor) ProcessLogs(_ context.Context, ld pdata.Logs) (pdata.Logs, error) {
-	// TODO
+func (gap *groupByAttrsProcessor) ProcessLogs(_ context.Context, ld pdata.Logs) (pdata.Logs, error) {
+	rl := ld.ResourceLogs()
+	extractedGroups := newLogsGroupedByAttrs()
 
-	return ld, nil
+	for i := 0; i < rl.Len(); i++ {
+		ls := rl.At(i)
+
+		ills := ls.InstrumentationLibraryLogs()
+		for j := 0; j < ills.Len(); j++ {
+			ill := ills.At(j)
+			for k := 0; k < ill.Logs().Len(); k++ {
+				log := ill.Logs().At(k)
+
+				groupedAnything, groupedAttrMap := gap.splitAttrMap(log.Attributes())
+				if groupedAnything {
+					mNumGroupedLogs.M(1)
+					// Some attributes are going to be moved from log record to resource level,
+					// so we can delete those on the record level
+					deleteAttributes(groupedAttrMap, log.Attributes())
+				} else {
+					mNumNonGroupedLogs.M(1)
+				}
+
+				// Lets combine the base resource attributes + the extracted (grouped) attributes
+				// and keep them in the grouping entry
+				groupedLogs := extractedGroups.attributeGroup(ls.Resource(), groupedAttrMap)
+				matchingInstrumentationLibraryLogs(groupedLogs, ill.InstrumentationLibrary()).Logs().Append(log)
+			}
+		}
+
+	}
+
+	// Copy the grouped data into output
+	groupedLogs := pdata.NewLogs()
+	groupedResourceLogs := groupedLogs.ResourceLogs()
+	for _, eg := range *extractedGroups {
+		groupedResourceLogs.Append(eg)
+	}
+	mDistLogGroups.M(int64(len(*extractedGroups)))
+
+	return groupedLogs, nil
+}
+
+func deleteAttributes(attrsForRemoval, targetAttrs pdata.AttributeMap) {
+	attrsForRemoval.ForEach(func(key string, _ pdata.AttributeValue) {
+		targetAttrs.Delete(key)
+	})
+}
+
+// splitAttrMap splits the AttributeMap by groupByKeys and returns a tuple:
+//  - the first element indicates if anything was matched (true) or nothing (false)
+//  - the second element contains groupByKeys that match given keys
+func (gap *groupByAttrsProcessor) splitAttrMap(attrMap pdata.AttributeMap) (bool, pdata.AttributeMap) {
+	groupedAttrMap := pdata.NewAttributeMap()
+	groupedAnything := false
+
+	for _, attrKey := range gap.groupByKeys {
+		attrVal, found := attrMap.Get(attrKey)
+		if found {
+			groupedAttrMap.Insert(attrKey, attrVal)
+			groupedAnything = true
+		}
+	}
+
+	return groupedAnything, groupedAttrMap
 }
