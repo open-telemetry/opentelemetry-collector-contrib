f0b9afff1a5cc8a00f82b845b0af512f1168ea6c
Dominik Rosiek <drosiek@sumologic.com>
2021-01-08 09:05:13 +0100

    [sumologicexporter] Use prometheusFormatter in sendMetrics

--- exporter/sumologicexporter/sender.go
+++ exporter/sumologicexporter/sender.go
@@ -234,6 +234,8 @@ func (s *sender) sendMetrics(ctx context.Context, flds fields) ([]metricPair, er
 		var formattedLine string
 
 		switch s.config.MetricFormat {
+		case PrometheusFormat:
+			formattedLine = s.prometheusFormatter.metric2String(record)
 		default:
 			return nil, errors.New("unexpected metric format")
 		}
