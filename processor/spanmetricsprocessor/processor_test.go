f5befecafcd8ec7e885489b94492120daac0b56b
Albert <26584478+albertteoh@users.noreply.github.com>
2021-01-08 21:54:49 +0000

    Add spanmetricsprocessor readme, config, sample config, factory and tests (#1917)
    Signed-off-by: albertteoh <albert.teoh@logz.io>
    
    Adds a new processor for aggregating span data to produce Requests Error and Duration (R.E.D) metrics.
    
    This is the first PR with a few to follow; containing the structure of the processor including:
    - README
    - Config struct
    - Factory
    - Processor skeleton code
    - Tests
    
    Note: as advised by maintainers during the Agent/Collector SIG, a workaround configuration of adding a dummy receiver and pipeline is required due to current limitations of the pipeline design and constraints.
    
    **Processor vs Exporter:**
    This component can be implemented both as a _processor_ or _exporter_. The _processor_ path was taken because:
    - From the [Opentelemetry Collector Architecture document](https://github.com/open-telemetry/opentelemetry-collector/blob/master/docs/design.md#processors), "processors" **transform** data and "they can also generate **new data**". Thus, a processor seems to be more fitting for something that aggregates span data into metrics than an exporter.
    - Processors allow the flexibility of writing to a metrics exporter without the overhead of an additional pipeline, if no processing of metrics is required. An example is exporting span metrics directly to prometheus.
    - `host:port` configuration is not required as it is encapsulated in the configured exporter to write span metrics to.
    - If there is a need to process span metrics further, an additional pipeline can be introduced.
    
    The advantages with an _exporter_ approach are:
    - It is more explicit, and does not need dummy receivers. It's very clear what the pipeline is doing just from reading the configuration.
    - There are discussions on #403 on a proposal to introduce translators, which are essentially both exporters and receivers. If the spanmetricsprocessor adopts this approach, it should be an easier and more natural transition from exporters to translators.
    
    **Link to tracking Issue:** #403
    
    **Testing:** 
    - Unit tests.
    
    **Documentation:**
    - README file.
    - Sample config in `testdata` directory.
    - Comments in Config struct.

---
+++ processor/spanmetricsprocessor/processor_test.go
@@ -0,0 +1,123 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package spanmetricsprocessor
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/consumer/consumertest"
+	"go.opentelemetry.io/collector/consumer/pdata"
+	"go.uber.org/zap"
+	"google.golang.org/grpc/metadata"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/spanmetricsprocessor/mocks"
+)
+
+func TestProcessorStart(t *testing.T) {
+	p := &processorImp{
+		logger: zap.NewNop(),
+	}
+	p.Start(context.Background(), nil)
+
+	// Verify
+	// TODO: Add verification once processor logic is in place
+}
+
+func TestProcessorShutdown(t *testing.T) {
+	// Prepare
+	factory := NewFactory()
+	cfg := factory.CreateDefaultConfig().(*Config)
+
+	// Test
+	next := new(consumertest.TracesSink)
+	p, err := newProcessor(zap.NewNop(), cfg, next)
+	require.NoError(t, err)
+	err = p.Shutdown(context.Background())
+
+	// Verify
+	assert.NoError(t, err)
+}
+
+func TestProcessorCapabilities(t *testing.T) {
+	// Prepare
+	factory := NewFactory()
+	cfg := factory.CreateDefaultConfig().(*Config)
+
+	// Test
+	next := new(consumertest.TracesSink)
+	p, err := newProcessor(zap.NewNop(), cfg, next)
+	caps := p.GetCapabilities()
+
+	// Verify
+	assert.NoError(t, err)
+	assert.NotNil(t, p)
+	assert.Equal(t, false, caps.MutatesConsumedData)
+}
+
+func TestProcessorConsumeTraces(t *testing.T) {
+	for _, tc := range []struct {
+		name                  string
+		wantConsumeMetricsErr string
+		wantConsumeTracesErr  string
+	}{
+		{name: "success"},
+		{name: "consume metrics error", wantConsumeMetricsErr: "consume metrics error"},
+		{name: "consume traces error", wantConsumeTracesErr: "consume traces error"},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			// Prepare
+			mexp := &mocks.MetricsExporter{}
+			tcon := &mocks.TracesConsumer{}
+
+			// TODO: Add verification of metrics input to ConsumeMetrics
+			consumeMetricsErr := error(nil)
+			if tc.wantConsumeMetricsErr != "" {
+				consumeMetricsErr = fmt.Errorf(tc.wantConsumeMetricsErr)
+			}
+			mexp.On("ConsumeMetrics", mock.Anything, mock.Anything).Return(consumeMetricsErr)
+
+			consumeTracesErr := error(nil)
+			if tc.wantConsumeTracesErr != "" {
+				consumeTracesErr = fmt.Errorf(tc.wantConsumeTracesErr)
+			}
+			tcon.On("ConsumeTraces", mock.Anything, mock.Anything).Return(consumeTracesErr)
+
+			p := &processorImp{
+				logger:          zap.NewNop(),
+				metricsExporter: mexp,
+				nextConsumer:    tcon,
+			}
+			traces := pdata.NewTraces()
+
+			// Test
+			ctx := metadata.NewIncomingContext(context.Background(), nil)
+			err := p.ConsumeTraces(ctx, traces)
+
+			// Verify
+			if tc.wantConsumeMetricsErr != "" {
+				assert.EqualError(t, err, tc.wantConsumeMetricsErr)
+			} else if tc.wantConsumeTracesErr != "" {
+				assert.EqualError(t, err, tc.wantConsumeTracesErr)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
