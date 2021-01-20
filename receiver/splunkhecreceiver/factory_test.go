8d6041abaeb8af929c6bd4c9f0582bae28b68484
Antoine Toulme <atoulme@users.noreply.github.com>
2021-01-19 16:51:34 -0800

    Make the path of the splunk HEC receiver configurable (#2137)
    * Make the path of the splunk HEC receiver configurable
    
    * Add more coverage

--- receiver/splunkhecreceiver/factory_test.go
+++ receiver/splunkhecreceiver/factory_test.go
@@ -57,39 +57,15 @@ func TestFactoryType(t *testing.T) {
 	assert.Equal(t, configmodels.Type("splunk_hec"), NewFactory().Type())
 }
 
-func TestCreateValidEndpoint(t *testing.T) {
-	endpoint, err := extractPortFromEndpoint("localhost:123")
-	assert.NoError(t, err)
-	assert.Equal(t, 123, endpoint)
-}
-
-func TestCreateInvalidEndpoint(t *testing.T) {
-	endpoint, err := extractPortFromEndpoint("")
-	assert.EqualError(t, err, "endpoint is not formatted correctly: missing port in address")
-	assert.Equal(t, 0, endpoint)
-}
-
-func TestCreateNoPort(t *testing.T) {
-	endpoint, err := extractPortFromEndpoint("localhost:")
-	assert.EqualError(t, err, "endpoint port is not a number: strconv.ParseInt: parsing \"\": invalid syntax")
-	assert.Equal(t, 0, endpoint)
-}
-
-func TestCreateLargePort(t *testing.T) {
-	endpoint, err := extractPortFromEndpoint("localhost:65536")
-	assert.EqualError(t, err, "port number must be between 1 and 65535")
-	assert.Equal(t, 0, endpoint)
-}
-
 func TestValidate(t *testing.T) {
-	err := createDefaultConfig().(*Config).validate()
+	err := createDefaultConfig().(*Config).initialize()
 	assert.NoError(t, err)
 }
 
 func TestValidateBadEndpoint(t *testing.T) {
 	config := createDefaultConfig().(*Config)
 	config.Endpoint = "localhost:abr"
-	err := config.validate()
+	err := config.initialize()
 	assert.EqualError(t, err, "endpoint port is not a number: strconv.ParseInt: parsing \"abr\": invalid syntax")
 }
 
@@ -99,6 +75,26 @@ func TestCreateNilNextConsumerMetrics(t *testing.T) {
 
 	mReceiver, err := createMetricsReceiver(context.Background(), component.ReceiverCreateParams{Logger: zap.NewNop()}, cfg, nil)
 	assert.EqualError(t, err, "nil metricsConsumer")
+	assert.Nil(t, mReceiver, "receiver creation failed")
+}
+
+func TestCreateMetricsReceiverWithBadConfig(t *testing.T) {
+	cfg := createDefaultConfig().(*Config)
+	cfg.Endpoint = "localhost:1"
+	cfg.Path = " *[* "
+
+	mReceiver, err := createMetricsReceiver(context.Background(), component.ReceiverCreateParams{Logger: zap.NewNop()}, cfg, consumertest.NewMetricsNop())
+	assert.EqualError(t, err, "unexpected end of input")
+	assert.Nil(t, mReceiver, "receiver creation failed")
+}
+
+func TestCreateLogsReceiverWithBadConfig(t *testing.T) {
+	cfg := createDefaultConfig().(*Config)
+	cfg.Endpoint = "localhost:1"
+	cfg.Path = " *[* "
+
+	mReceiver, err := createLogsReceiver(context.Background(), component.ReceiverCreateParams{Logger: zap.NewNop()}, cfg, consumertest.NewLogsNop())
+	assert.EqualError(t, err, "unexpected end of input")
 	assert.Nil(t, mReceiver, "receiver creation failed")
 }
 
