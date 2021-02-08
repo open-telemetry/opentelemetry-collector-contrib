7cd26dc4d45fc7a1935fbea4808b7e37bd717465
Min Xia <xiami@amazon.com>
2021-02-08 11:13:47 -0800

    Enhance EMFExporter for Metrics Batching in AWS EMF Logs  (#2271)
    We sent a large PR https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/1891 to support batching the metrics on the same dimensions for AWS EMF Log request to save the customers' billing cost and request throughput. At the same time, there was a fairly large code refactor on EMFExporter. For better code review purpose, I plan to split https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/1891  to 2 PRs. (This is PR#1)
    
    In this PR, We refactored EMFExporter without introducing any new feature. For each OTel metrics data point, we defined `DataPoint` file, it wraps `pdata.DataPointSlice` to the custom structures for each type of metric data point. we also moved the metric data handling functions - data conversion and rate calculation to `datapoint`. 
    It also fixed the metric `timestamp` bug.

--- exporter/awsemfexporter/util_test.go
+++ exporter/awsemfexporter/util_test.go
@@ -17,8 +17,13 @@ package awsemfexporter
 import (
 	"testing"
 
+	commonpb "github.com/census-instrumentation/opencensus-proto/gen-go/agent/common/v1"
+	resourcepb "github.com/census-instrumentation/opencensus-proto/gen-go/resource/v1"
 	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/collector/consumer/consumerdata"
 	"go.opentelemetry.io/collector/consumer/pdata"
+	"go.opentelemetry.io/collector/translator/conventions"
+	"go.opentelemetry.io/collector/translator/internaldata"
 	"go.uber.org/zap"
 )
 
@@ -110,4 +115,146 @@ func TestReplacePatternNilAttrValue(t *testing.T) {
 	s := replacePatterns(input, attrMap, logger)
 
 	assert.Equal(t, "/aws/ecs/containerinsights/undefined/performance", s)
+}
+
+func TestGetNamespace(t *testing.T) {
+	defaultMetric := createMetricTestData()
+	testCases := []struct {
+		testName        string
+		metric          consumerdata.MetricsData
+		configNamespace string
+		namespace       string
+	}{
+		{
+			"non-empty namespace",
+			defaultMetric,
+			"namespace",
+			"namespace",
+		},
+		{
+			"empty namespace",
+			defaultMetric,
+			"",
+			"myServiceNS/myServiceName",
+		},
+		{
+			"empty namespace, no service namespace",
+			consumerdata.MetricsData{
+				Resource: &resourcepb.Resource{
+					Labels: map[string]string{
+						conventions.AttributeServiceName: "myServiceName",
+					},
+				},
+			},
+			"",
+			"myServiceName",
+		},
+		{
+			"empty namespace, no service name",
+			consumerdata.MetricsData{
+				Resource: &resourcepb.Resource{
+					Labels: map[string]string{
+						conventions.AttributeServiceNamespace: "myServiceNS",
+					},
+				},
+			},
+			"",
+			"myServiceNS",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.testName, func(t *testing.T) {
+			rms := internaldata.OCToMetrics(tc.metric)
+			rm := rms.ResourceMetrics().At(0)
+			namespace := getNamespace(&rm, tc.configNamespace)
+			assert.Equal(t, tc.namespace, namespace)
+		})
+	}
+}
+
+func TestGetLogInfo(t *testing.T) {
+	metric := consumerdata.MetricsData{
+		Node: &commonpb.Node{
+			ServiceInfo: &commonpb.ServiceInfo{Name: "test-emf"},
+			LibraryInfo: &commonpb.LibraryInfo{ExporterVersion: "SomeVersion"},
+		},
+		Resource: &resourcepb.Resource{
+			Labels: map[string]string{
+				"aws.ecs.cluster.name": "test-cluster-name",
+				"aws.ecs.task.id":      "test-task-id",
+			},
+		},
+	}
+	rm := internaldata.OCToMetrics(metric).ResourceMetrics().At(0)
+
+	testCases := []struct {
+		testName        string
+		namespace       string
+		configLogGroup  string
+		configLogStream string
+		logGroup        string
+		logStream       string
+	}{
+		{
+			"non-empty namespace, no config",
+			"namespace",
+			"",
+			"",
+			"/metrics/namespace",
+			"",
+		},
+		{
+			"empty namespace, no config",
+			"",
+			"",
+			"",
+			"",
+			"",
+		},
+		{
+			"non-empty namespace, config w/o pattern",
+			"namespace",
+			"test-logGroupName",
+			"test-logStreamName",
+			"test-logGroupName",
+			"test-logStreamName",
+		},
+		{
+			"empty namespace, config w/o pattern",
+			"",
+			"test-logGroupName",
+			"test-logStreamName",
+			"test-logGroupName",
+			"test-logStreamName",
+		},
+		{
+			"non-empty namespace, config w/ pattern",
+			"namespace",
+			"/aws/ecs/containerinsights/{ClusterName}/performance",
+			"{TaskId}",
+			"/aws/ecs/containerinsights/test-cluster-name/performance",
+			"test-task-id",
+		},
+		{
+			"empty namespace, config w/ pattern",
+			"",
+			"/aws/ecs/containerinsights/{ClusterName}/performance",
+			"{TaskId}",
+			"/aws/ecs/containerinsights/test-cluster-name/performance",
+			"test-task-id",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.testName, func(t *testing.T) {
+			config := &Config{
+				LogGroupName:  tc.configLogGroup,
+				LogStreamName: tc.configLogStream,
+			}
+			logGroup, logStream := getLogInfo(&rm, tc.namespace, config)
+			assert.Equal(t, tc.logGroup, logGroup)
+			assert.Equal(t, tc.logStream, logStream)
+		})
+	}
 }
