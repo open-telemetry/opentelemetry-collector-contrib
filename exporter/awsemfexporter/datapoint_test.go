3df707152d7dc28fe905e25cdf35b31b6d401aa9
Mengyi Zhou <zmengyi@amazon.com>
2021-03-09 23:10:47 -0800

    Extract metric calculator as an internal component

--- exporter/awsemfexporter/datapoint_test.go
+++ exporter/awsemfexporter/datapoint_test.go
@@ -307,7 +307,7 @@ func TestIntDataPointSliceAt(t *testing.T) {
 			assert.Equal(t, 1, dps.Len())
 			dp := dps.At(0)
 			if strings.Contains(tc.testName, "2nd rate") {
-				assert.True(t, (expectedDP.Value.(float64)-dp.Value.(float64)) < 0.01)
+				assert.InDelta(t, expectedDP.Value.(float64), dp.Value.(float64), 0.01)
 			} else {
 				assert.Equal(t, expectedDP, dp)
 			}
@@ -383,7 +383,7 @@ func TestDoubleDataPointSliceAt(t *testing.T) {
 			assert.Equal(t, 1, dps.Len())
 			dp := dps.At(0)
 			if strings.Contains(tc.testName, "2nd rate") {
-				assert.True(t, (expectedDP.Value.(float64)-dp.Value.(float64)) < 0.002)
+				assert.InDelta(t, expectedDP.Value.(float64), dp.Value.(float64), 0.002)
 			} else {
 				assert.Equal(t, expectedDP, dp)
 			}
@@ -505,15 +505,21 @@ func TestCalculateRate(t *testing.T) {
 	assert.Equal(t, float64(0), rate)
 
 	rate = calculateRate(intRateKey, intVal2, time2)
-	assert.Equal(t, float64(1), rate)
+	assert.InDelta(t, float64(1), rate, 0.1)
 	rate = calculateRate(doubleRateKey, doubleVal2, time2)
-	assert.Equal(t, 0.5, rate)
+	assert.InDelta(t, 0.5, rate, 0.1)
 
 	// Test change of data type
 	rate = calculateRate(intRateKey, doubleVal3, time3)
-	assert.Equal(t, float64(0.51), rate)
+	assert.InDelta(t, float64(0.51), rate, 0.1)
 	rate = calculateRate(doubleRateKey, intVal3, time3)
-	assert.Equal(t, float64(19.5), rate)
+	assert.InDelta(t, float64(19.5), rate, 0.1)
+}
+
+func calculateRate(metricName string, value float64, timestampMs int64) interface{} {
+	time := time.Unix(0, timestampMs*int64(time.Millisecond))
+	val, _ := rateMetricCalculator.Calculate(metricName, nil, value, time)
+	return val
 }
 
 func TestGetDataPoints(t *testing.T) {
