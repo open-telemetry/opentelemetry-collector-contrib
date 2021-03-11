d37eb9f318977d4bbe40e0d701dbb3e0b5b6a4b3
Przemek Maciolek <58699843+pmm-sumo@users.noreply.github.com>
2021-03-11 16:16:26 +0100

    Enable groupbyatttrs processor (#2635)
    Introduces  processor for grouping the trace or log records by specified key values, and then extracting the attributes from the record to resource level.
    
    This initiative is broken down into 3 separate PRs:
    
    1. [Config](https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/1744) (already merged)
    
    2. [The processor logic](https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/1792) (already merged)
    
    👉  3. Enabling the component (this PR)
    
    _Example: consider having such input:_
    
    ```
    InstrumentationLibraryLogs #0
    LogRecord #0
    Timestamp: 1606158664897894000
    Severity:
    ShortName:
    Body: STRING("foo")
    Attributes:
         -> action: STRING(login)
         -> namespace_name: STRING(kube-apiserver-docker-desktop)
         -> pod_name: STRING(kube-system)
    ```
    
    With following config:
    
    ```
    groupbyattrs:
      group_by_keys:
        - pod_name
        - namespace_name
    ```
    
    The output is going to be:
    
    ```
    Resource labels:
         -> namespace_name: STRING(kube-apiserver-docker-desktop)
         -> pod_name: STRING(kube-system)
    InstrumentationLibraryLogs #0
    LogRecord #0
    Timestamp: 1606158617133076000
    Severity:
    ShortName:
    Body: STRING("foo")
    Attributes:
         -> action: STRING(login)
    ```
    
    **Link to tracking Issue:** [#1884](https://github.com/open-telemetry/opentelemetry-collector/issues/1884)
    
    **Testing:** Unit tests added, manual tests performed on logs and traces
    
    **Documentation:** Processor README.md included

--- cmd/otelcontribcol/components.go
+++ cmd/otelcontribcol/components.go
@@ -45,6 +45,7 @@ import (
 	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/httpforwarder"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/observer/hostobserver"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/observer/k8sobserver"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/groupbyattrsprocessor"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/groupbytraceprocessor"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/k8sprocessor"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/metricstransformprocessor"
@@ -163,6 +164,7 @@ func components() (component.Factories, error) {
 	}
 
 	processors := []component.ProcessorFactory{
+		groupbyattrsprocessor.NewFactory(),
 		groupbytraceprocessor.NewFactory(),
 		k8sprocessor.NewFactory(),
 		metricstransformprocessor.NewFactory(),
