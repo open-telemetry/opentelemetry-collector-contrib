eb4bce4a279420a5cdecec9e6821f1e3a71dd468
Xitric <KasperSchultzDavidsen@gmail.com>
2021-04-15 10:11:47 +0200

    Added span attributes to trace exporter

--- exporter/humioexporter/traces_exporter.go
+++ exporter/humioexporter/traces_exporter.go
@@ -24,25 +24,27 @@ import (
 	"go.uber.org/zap"
 )
 
+// HumioLink represents a relation between two spans
 type HumioLink struct {
 	TraceId    string `json:"trace_id"`
 	SpanId     string `json:"span_id"`
 	TraceState string `json:"state"`
 }
 
+// HumioSpan represents a span as it is stored inside Humio
 type HumioSpan struct {
-	TraceId           string            `json:"trace_id"`
-	SpanId            string            `json:"span_id"`
-	ParentSpanId      string            `json:"parent_id,omitempty"`
-	Name              string            `json:"name"`
-	Kind              string            `json:"kind"`
-	Start             int64             `json:"start"`
-	End               int64             `json:"end"`
-	StatusCode        string            `json:"status"`
-	StatusDescription string            `json:"status_descr"`
-	ServiceName       string            `json:"service"`
-	Links             HumioLink         `json:"links"`
-	Attributes        map[string]string `json:"attributes"`
+	TraceId           string                 `json:"trace_id"`
+	SpanId            string                 `json:"span_id"`
+	ParentSpanId      string                 `json:"parent_id,omitempty"`
+	Name              string                 `json:"name"`
+	Kind              string                 `json:"kind"`
+	Start             int64                  `json:"start"`
+	End               int64                  `json:"end"`
+	StatusCode        string                 `json:"status"`
+	StatusDescription string                 `json:"status_descr"`
+	ServiceName       string                 `json:"service"`
+	Links             []*HumioLink           `json:"links"`
+	Attributes        map[string]interface{} `json:"attributes"`
 }
 
 type humioTracesExporter struct {
@@ -70,6 +72,10 @@ func (e *humioTracesExporter) pushTraceData(ctx context.Context, td pdata.Traces
 	}
 
 	err = e.client.sendStructuredEvents(ctx, evts)
+	if err == nil {
+		return nil
+	}
+
 	if consumererror.IsPermanent(err) {
 		return err
 	}
@@ -90,7 +96,6 @@ func (e *humioTracesExporter) tracesToHumioEvents(td pdata.Traces) ([]*HumioStru
 		if sName, ok := r.Attributes().Get(conventions.AttributeServiceName); ok {
 			serviceName = sName.StringVal()
 		}
-		// TODO: All additional attributes should propably be dumped as well
 
 		evts := make([]*HumioStructuredEvent, 0, resSpan.InstrumentationLibrarySpans().Len())
 
@@ -98,34 +103,14 @@ func (e *humioTracesExporter) tracesToHumioEvents(td pdata.Traces) ([]*HumioStru
 		instSpans := resSpan.InstrumentationLibrarySpans()
 		for j := 0; j < instSpans.Len(); j++ {
 			instSpan := instSpans.At(j)
-			// TODO: Use
-			// lib := instSpan.InstrumentationLibrary()
-			// lib.Name()
-			// lib.Version()
+			lib := instSpan.InstrumentationLibrary()
 
 			// Lastly, we get access to the actual spans
 			otelSpans := instSpan.Spans()
 			for k := 0; k < otelSpans.Len(); k++ {
 				otelSpan := otelSpans.At(k)
 
-				evts = append(evts, &HumioStructuredEvent{
-					Timestamp: otelSpan.StartTimestamp().AsTime(),
-					AsUnix:    e.cfg.Traces.UnixTimestamps,
-					Attributes: &HumioSpan{
-						TraceId:           otelSpan.TraceID().HexString(),
-						SpanId:            otelSpan.SpanID().HexString(),
-						ParentSpanId:      otelSpan.ParentSpanID().HexString(),
-						Name:              otelSpan.Name(),
-						Kind:              otelSpan.Kind().String(),
-						Start:             otelSpan.StartTimestamp().AsTime().UnixNano(),
-						End:               otelSpan.EndTimestamp().AsTime().UnixNano(),
-						StatusCode:        otelSpan.Status().Code().String(),
-						StatusDescription: otelSpan.Status().Message(),
-						ServiceName:       serviceName,
-						// TODO: Links
-						// TODO: Dump additional attributes
-					},
-				})
+				evts = append(evts, e.spanToHumioEvent(otelSpan, lib, r))
 			}
 		}
 
@@ -139,6 +124,86 @@ func (e *humioTracesExporter) tracesToHumioEvents(td pdata.Traces) ([]*HumioStru
 	}
 
 	return results, nil
+}
+
+func (e *humioTracesExporter) spanToHumioEvent(span pdata.Span, inst pdata.InstrumentationLibrary, res pdata.Resource) *HumioStructuredEvent {
+	attr := toHumioAttributes(span.Attributes(), res.Attributes())
+	attr["instrumentationLibrary.name"] = inst.Name()
+	attr["instrumentationLibrary.version"] = inst.Version()
+
+	serviceName := ""
+	if sName, ok := res.Attributes().Get(conventions.AttributeServiceName); ok {
+		// No need to store the service name in two places
+		delete(attr, conventions.AttributeServiceName)
+		serviceName = sName.StringVal()
+	}
+
+	return &HumioStructuredEvent{
+		Timestamp: span.StartTimestamp().AsTime(),
+		AsUnix:    e.cfg.Traces.UnixTimestamps,
+		Attributes: &HumioSpan{
+			TraceId:           span.TraceID().HexString(),
+			SpanId:            span.SpanID().HexString(),
+			ParentSpanId:      span.ParentSpanID().HexString(),
+			Name:              span.Name(),
+			Kind:              span.Kind().String(),
+			Start:             span.StartTimestamp().AsTime().UnixNano(),
+			End:               span.EndTimestamp().AsTime().UnixNano(),
+			StatusCode:        span.Status().Code().String(),
+			StatusDescription: span.Status().Message(),
+			ServiceName:       serviceName,
+			Links:             toHumioLinks(span.Links()),
+			Attributes:        attr,
+		},
+	}
+}
+
+func toHumioLinks(pLinks pdata.SpanLinkSlice) []*HumioLink {
+	links := make([]*HumioLink, 0, pLinks.Len())
+	for i := 0; i < pLinks.Len(); i++ {
+		link := pLinks.At(i)
+		links = append(links, &HumioLink{
+			TraceId:    link.TraceID().HexString(),
+			SpanId:     link.SpanID().HexString(),
+			TraceState: string(link.TraceState()),
+		})
+	}
+	return links
+}
+
+func toHumioAttributes(attrMaps ...pdata.AttributeMap) map[string]interface{} {
+	attr := make(map[string]interface{}, 0)
+	for _, attrMap := range attrMaps {
+		attrMap.ForEach(func(k string, v pdata.AttributeValue) {
+			attr[k] = toHumioAttributeValue(v)
+		})
+	}
+	return attr
+}
+
+func toHumioAttributeValue(rawVal pdata.AttributeValue) interface{} {
+	switch rawVal.Type() {
+	case pdata.AttributeValueSTRING:
+		return rawVal.StringVal()
+	case pdata.AttributeValueINT:
+		return rawVal.IntVal()
+	case pdata.AttributeValueDOUBLE:
+		return rawVal.DoubleVal()
+	case pdata.AttributeValueBOOL:
+		return rawVal.BoolVal()
+	case pdata.AttributeValueMAP:
+		return toHumioAttributes(rawVal.MapVal())
+	case pdata.AttributeValueARRAY:
+		arrVal := rawVal.ArrayVal()
+		arr := make([]interface{}, 0, arrVal.Len())
+		for i := 0; i < arrVal.Len(); i++ {
+			arr = append(arr, toHumioAttributeValue(arrVal.At(i)))
+		}
+		return arr
+	}
+
+	// Also handles AttributeValueNULL
+	return nil
 }
 
 func (e *humioTracesExporter) shutdown(context.Context) error {
