0fc40d76d93ae1d56f72f7e61ec0384283eabd52
Bogdan Drutu <bogdandrutu@gmail.com>
2021-04-21 11:59:58 -0700

    Change redis receiver to avoid unnecessary allocation when using Append (#3197)
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- receiver/redisreceiver/pdata_test.go
+++ receiver/redisreceiver/pdata_test.go
@@ -153,16 +153,21 @@ func TestKeyspaceMetrics(t *testing.T) {
 func TestNewPDM(t *testing.T) {
 	serverStartTime := pdata.TimestampFromTime(time.Unix(900, 0))
 	tb := testTimeBundle()
-	pdm := newIntMetric(&redisMetric{pdType: pdata.MetricDataTypeIntGauge}, pdata.NewIntDataPoint(), tb)
+
+	pdm := pdata.NewMetric()
+	initIntMetric(&redisMetric{pdType: pdata.MetricDataTypeIntGauge}, 0, tb, pdm)
 	assert.Equal(t, pdata.Timestamp(0), pdm.IntGauge().DataPoints().At(0).StartTimestamp())
 
-	pdm = newIntMetric(&redisMetric{pdType: pdata.MetricDataTypeIntSum}, pdata.NewIntDataPoint(), tb)
+	pdm = pdata.NewMetric()
+	initIntMetric(&redisMetric{pdType: pdata.MetricDataTypeIntSum}, 0, tb, pdm)
 	assert.Equal(t, serverStartTime, pdm.IntSum().DataPoints().At(0).StartTimestamp())
 
-	pdm = newDoubleMetric(&redisMetric{pdType: pdata.MetricDataTypeDoubleGauge}, pdata.NewDoubleDataPoint(), tb)
+	pdm = pdata.NewMetric()
+	initDoubleMetric(&redisMetric{pdType: pdata.MetricDataTypeDoubleGauge}, 0, tb, pdm)
 	assert.Equal(t, pdata.Timestamp(0), pdm.DoubleGauge().DataPoints().At(0).StartTimestamp())
 
-	pdm = newDoubleMetric(&redisMetric{pdType: pdata.MetricDataTypeDoubleSum}, pdata.NewDoubleDataPoint(), tb)
+	pdm = pdata.NewMetric()
+	initDoubleMetric(&redisMetric{pdType: pdata.MetricDataTypeDoubleSum}, 0, tb, pdm)
 	assert.Equal(t, serverStartTime, pdm.DoubleSum().DataPoints().At(0).StartTimestamp())
 }
 
