8443c7f68cb0e5a30f2a148b20f343cdb1b2b1a5
Eric Mustin <mustin.eric@gmail.com>
2021-04-28 10:36:28 -0400

    [exporter/datadog]: add max tag length (#3185)
    This PR is part of work to address Payload size errors reported here: https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/2676
    
    Combined with batching of spans/traces, spans with very large attribute values such as stacktraces can cause payload sizes to become excessive. This PR adds a max tag (a datadog span attribute, basically), value helper function, and truncates tag values when they're too  large, which brings this in line with Datadog-Agent limits: https://github.com/DataDog/datadog-agent/blob/140a4ee164261ef2245340c50371ba989fbeb038/pkg/trace/traceutil/truncate.go#L23
    
    Additionally, worth noting that work being done to directly import and re-use portions of the datadog-agent codebase will make this sort of thing much easier, and work being done within datadog to permit shorter batching times should also help resolve the Payload size errors being seen. However for now this PR can help.
    
    **Link to tracking Issue:** <Issue number if applicable> 
    
    https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/2676

--- exporter/datadogexporter/utils/trace_helpers_test.go
+++ exporter/datadogexporter/utils/trace_helpers_test.go
@@ -23,6 +23,19 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// ensure that truncation helper function truncates strings as expected
+// and accounts for the limit and multi byte ending characters
+// from https://github.com/DataDog/datadog-agent/blob/140a4ee164261ef2245340c50371ba989fbeb038/pkg/trace/traceutil/truncate_test.go#L15
+func TestTruncateUTF8Strings(t *testing.T) {
+	assert.Equal(t, "", TruncateUTF8("", 5))
+	assert.Equal(t, "télé", TruncateUTF8("télé", 5))
+	assert.Equal(t, "t", TruncateUTF8("télé", 2))
+	assert.Equal(t, "éé", TruncateUTF8("ééééé", 5))
+	assert.Equal(t, "ééééé", TruncateUTF8("ééééé", 18))
+	assert.Equal(t, "ééééé", TruncateUTF8("ééééé", 10))
+	assert.Equal(t, "ééé", TruncateUTF8("ééééé", 6))
+}
+
 func TestNormalizeTag(t *testing.T) {
 	for _, tt := range []struct{ in, out string }{
 		{in: "#test_starting_hash", out: "test_starting_hash"},
