8023ee137edc538a993237651bccac582f0a9514
John D <john@boostchicken.dev>
2021-05-13 00:29:54 -0700

    Basic Translator Benchmark test

--- receiver/datadogreceiver/go.mod
+++ receiver/datadogreceiver/go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gorilla/mux v1.8.0
 	github.com/stretchr/testify v1.7.0
 	github.com/tinylib/msgp v1.1.2
+	github.com/vmihailenco/msgpack/v4 v4.3.12
 	go.opentelemetry.io/collector v0.26.0
 	go.uber.org/zap v1.16.0
 )
--- receiver/datadogreceiver/translator_test.go
+++ receiver/datadogreceiver/translator_test.go
@@ -14,9 +14,70 @@
 
 package datadogreceiver
 
-import "testing"
+import (
+	"bytes"
+	"github.com/DataDog/datadog-agent/pkg/trace/exportable/pb"
+	"github.com/stretchr/testify/assert"
+	vmsgp "github.com/vmihailenco/msgpack/v4"
+	"net/http"
+	"testing"
+)
 
 func BenchmarkTranslator(b *testing.B) {
+	var data = [2]interface{}{
+		0: []string{
+			0:  "baggage",
+			1:  "item",
+			2:  "elasticsearch.version",
+			3:  "7.0",
+			4:  "my-name",
+			5:  "X",
+			6:  "my-service",
+			7:  "my-resource",
+			8:  "_dd.sampling_rate_whatever",
+			9:  "value whatever",
+			10: "sql",
+		},
+		1: [][][12]interface{}{
+			{
+				{
+					6,
+					4,
+					7,
+					uint64(1),
+					uint64(2),
+					uint64(3),
+					int64(123),
+					int64(456),
+					1,
+					map[interface{}]interface{}{
+						8: 9,
+						0: 1,
+						2: 3,
+					},
+					map[interface{}]float64{
+						5: 1.2,
+					},
+					10,
+				},
+			},
+		},
+	}
 
-	return
+	payload, err := vmsgp.Marshal(&data)
+	assert.NoError(b, err)
+	dc := pb.NewMsgpReader(bytes.NewReader(payload))
+	defer pb.FreeMsgpReader(dc)
+
+	var traces pb.Traces
+	if err := traces.DecodeMsgDictionary(dc); err != nil {
+		b.Fatal(err)
+	}
+	b.ResetTimer()
+	b.ReportAllocs()
+	b.SetBytes(int64(len(payload)))
+	req := &http.Request{RequestURI: "/v0.5/traces"}
+	for i := 0; i < b.N; i++ {
+		ToTraces(traces, req)
+	}
 }
