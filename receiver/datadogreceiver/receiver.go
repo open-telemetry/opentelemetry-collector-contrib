9f41cb8b9887dcfcd61dcac66dd245b29e75540b
John D <john@boostchicken.dev>
2021-05-12 23:04:14 -0700

    Moved translation engine and fixed translator

--- receiver/datadogreceiver/receiver.go
+++ receiver/datadogreceiver/receiver.go
@@ -16,20 +16,14 @@ package datadogreceiver
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"github.com/DataDog/datadog-agent/pkg/trace/exportable/pb"
 	"github.com/gorilla/mux"
-	"github.com/tinylib/msgp/msgp"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/component/componenterror"
 	"go.opentelemetry.io/collector/consumer"
-	"go.opentelemetry.io/collector/consumer/pdata"
 	"go.opentelemetry.io/collector/obsreport"
-	tracetranslator "go.opentelemetry.io/collector/translator/trace"
-	"mime"
 	"net/http"
-	"strings"
 	"sync"
 )
 
@@ -88,88 +82,23 @@ func (ddr *datadogReceiver) Shutdown(ctx context.Context) error {
 
 func (ddr *datadogReceiver) handleTraces(w http.ResponseWriter, req *http.Request) {
 	obsreport.StartTraceDataReceiveOp(ddr.longLivedCtx, typeStr, "http")
-	var traces pb.Traces
-	err := ddr.decodeRequest(req, &traces)
+	var ddTraces pb.Traces
+
+	err := decodeRequest(req, &ddTraces)
 	if err != nil {
 		http.Error(w, "Unable to unmarshal reqs", http.StatusInternalServerError)
 		obsreport.EndTraceDataReceiveOp(ddr.longLivedCtx, typeStr, 0, err)
 		return
 	}
 
-	otelTraces, err := ddr.processTraces(req.Context(), traces)
+	otelTraces := ToTraces(ddTraces)
+
+	err = ddr.nextConsumer.ConsumeTraces(req.Context(), otelTraces)
 	if err != nil {
 		http.Error(w, "Trace consumer errored out", http.StatusInternalServerError)
 	} else {
+		w.WriteHeader(http.StatusOK)
 		_, _ = w.Write([]byte("OK"))
 	}
 	obsreport.EndTraceDataReceiveOp(ddr.longLivedCtx, typeStr, otelTraces.SpanCount(), err)
-}
-
-func (ddr *datadogReceiver) processTraces(ctx context.Context, traces pb.Traces) (pdata.Traces, error) {
-	dest := pdata.NewTraces()
-	for _, trace := range traces {
-		for _, span := range trace {
-			newSpan := dest.ResourceSpans().AppendEmpty().InstrumentationLibrarySpans().AppendEmpty().Spans().AppendEmpty()
-			newSpan.SetTraceID(tracetranslator.UInt64ToTraceID(span.TraceID, span.TraceID))
-			newSpan.SetSpanID(tracetranslator.UInt64ToSpanID(span.SpanID))
-			newSpan.SetStartTimestamp(pdata.Timestamp(span.Start))
-			newSpan.SetEndTimestamp(pdata.Timestamp(span.Start + span.Duration))
-			newSpan.SetParentSpanID(tracetranslator.UInt64ToSpanID(span.ParentID))
-			newSpan.SetName(span.Name)
-			for k, v := range span.GetMeta() {
-				newSpan.Attributes().InsertString(k, v)
-			}
-			switch span.Type {
-			case "web":
-				newSpan.SetKind(pdata.SpanKindSERVER)
-			case "client":
-				newSpan.SetKind(pdata.SpanKindCLIENT)
-			default:
-				newSpan.SetKind(pdata.SpanKindUNSPECIFIED)
-			}
-
-		}
-	}
-	err := ddr.nextConsumer.ConsumeTraces(ctx, dest)
-	return dest, err
-}
-
-func (ddr *datadogReceiver) decodeRequest(req *http.Request, dest *pb.Traces) error {
-	switch mediaType := getMediaType(req); mediaType {
-	case "application/msgpack":
-		if strings.Contains(req.RequestURI, "v0.5") {
-			reader := pb.NewMsgpReader(req.Body)
-			defer pb.FreeMsgpReader(reader)
-			return dest.DecodeMsgDictionary(reader)
-		} else {
-			return msgp.Decode(req.Body, dest)
-		}
-	case "application/json":
-		fallthrough
-	case "text/json":
-		fallthrough
-	case "":
-		return json.NewDecoder(req.Body).Decode(dest)
-	default:
-		// do our best
-		if err1 := json.NewDecoder(req.Body).Decode(dest); err1 != nil {
-			if err2 := msgp.Decode(req.Body, dest); err2 != nil {
-				reader := pb.NewMsgpReader(req.Body)
-				defer pb.FreeMsgpReader(reader)
-				if err3 := dest.DecodeMsgDictionary(reader); err3 != nil {
-					return fmt.Errorf("could not decode JSON (%q), nor Msgpack (%q), nor v0.5 (%q)", err1, err2, err3)
-				}
-			}
-		}
-		return nil
-	}
-
-}
-
-func getMediaType(req *http.Request) string {
-	mt, _, err := mime.ParseMediaType(req.Header.Get("Content-Type"))
-	if err != nil {
-		return "application/json"
-	}
-	return mt
 }
---
+++ receiver/datadogreceiver/translator.go
@@ -0,0 +1,82 @@
+package datadogreceiver
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/DataDog/datadog-agent/pkg/trace/exportable/pb"
+	"github.com/tinylib/msgp/msgp"
+	"go.opentelemetry.io/collector/consumer/pdata"
+	tracetranslator "go.opentelemetry.io/collector/translator/trace"
+	"mime"
+	"net/http"
+	"strings"
+)
+
+func ToTraces(traces pb.Traces) pdata.Traces {
+	dest := pdata.NewTraces()
+	for _, trace := range traces {
+		ils := dest.ResourceSpans().AppendEmpty().InstrumentationLibrarySpans().AppendEmpty()
+		for _, span := range trace {
+			newSpan := ils.Spans().AppendEmpty()
+			newSpan.SetTraceID(tracetranslator.UInt64ToTraceID(span.TraceID, span.TraceID))
+			newSpan.SetSpanID(tracetranslator.UInt64ToSpanID(span.SpanID))
+			newSpan.SetStartTimestamp(pdata.Timestamp(span.Start))
+			newSpan.SetEndTimestamp(pdata.Timestamp(span.Start + span.Duration))
+			newSpan.SetParentSpanID(tracetranslator.UInt64ToSpanID(span.ParentID))
+			newSpan.SetName(span.Name)
+			for k, v := range span.GetMeta() {
+				newSpan.Attributes().InsertString(k, v)
+			}
+			switch span.Type {
+			case "web":
+				newSpan.SetKind(pdata.SpanKindSERVER)
+			case "client":
+				newSpan.SetKind(pdata.SpanKindCLIENT)
+			default:
+				newSpan.SetKind(pdata.SpanKindUNSPECIFIED)
+			}
+
+		}
+	}
+	return dest
+}
+
+func decodeRequest(req *http.Request, dest *pb.Traces) error {
+	switch mediaType := getMediaType(req); mediaType {
+	case "application/msgpack":
+		if strings.Contains(req.RequestURI, "v0.5") {
+			reader := pb.NewMsgpReader(req.Body)
+			defer pb.FreeMsgpReader(reader)
+			return dest.DecodeMsgDictionary(reader)
+		} else {
+			return msgp.Decode(req.Body, dest)
+		}
+	case "application/json":
+		fallthrough
+	case "text/json":
+		fallthrough
+	case "":
+		return json.NewDecoder(req.Body).Decode(dest)
+	default:
+		// do our best
+		if err1 := json.NewDecoder(req.Body).Decode(dest); err1 != nil {
+			if err2 := msgp.Decode(req.Body, dest); err2 != nil {
+				reader := pb.NewMsgpReader(req.Body)
+				defer pb.FreeMsgpReader(reader)
+				if err3 := dest.DecodeMsgDictionary(reader); err3 != nil {
+					return fmt.Errorf("could not decode JSON (%q), nor Msgpack (%q), nor v0.5 (%q)", err1, err2, err3)
+				}
+			}
+		}
+		return nil
+	}
+
+}
+
+func getMediaType(req *http.Request) string {
+	mt, _, err := mime.ParseMediaType(req.Header.Get("Content-Type"))
+	if err != nil {
+		return "application/json"
+	}
+	return mt
+}
