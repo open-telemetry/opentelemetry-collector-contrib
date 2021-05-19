f86f052bb3727455442acbf0832bce4278ae6f22
Daniel Jaglowski <jaglows3@gmail.com>
2021-05-19 10:01:49 -0400

    Add test that validates fingerprint.StartsWith when read from files (#145)

--- operator/builtin/input/file/fingerprint.go
+++ operator/builtin/input/file/fingerprint.go
@@ -25,6 +25,8 @@ const defaultFingerprintSize = 1000 // bytes
 const minFingerprintSize = 16       // bytes
 
 // Fingerprint is used to identify a file
+// A file's fingerprint is the first N bytes of the file,
+// where N is the fingerprintSize on the file_input operator
 type Fingerprint struct {
 	FirstBytes []byte
 }
@@ -56,6 +58,10 @@ func (f Fingerprint) Copy() *Fingerprint {
 
 // StartsWith returns true if the fingerprints are the same
 // or if the new fingerprint starts with the old one
+// This is important functionality for tracking new files,
+// since their initial size is typically less than that of
+// a fingerprint. As the file grows, its fingerprint is updated
+// until it reaches a maximum size, as configured on the operator
 func (f Fingerprint) StartsWith(old *Fingerprint) bool {
 	l0 := len(old.FirstBytes)
 	if l0 == 0 {
