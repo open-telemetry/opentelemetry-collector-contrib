88ee8056219bfd5d2ba2133089e45d715e268945
Rayhan Hossain <hossain.rayhan@outlook.com>
2021-05-19 18:18:32 -0700

    bug fix: check all datapoints for matching metric label values
    Signed-off-by: Rayhan Hossain <hossain.rayhan@outlook.com>

--- processor/metricstransformprocessor/metrics_transform_processor.go
+++ processor/metricstransformprocessor/metrics_transform_processor.go
@@ -60,7 +60,7 @@ type internalOperation struct {
 type internalFilter interface {
 	getMatches(toMatch metricNameMapping) []*match
 	getSubexpNames() []string
-	labelMatched(metric *metricspb.Metric) bool
+	labelMatched(metric *metricspb.Metric) *metricspb.Metric
 }
 
 type match struct {
@@ -78,8 +78,9 @@ func (f internalFilterStrict) getMatches(toMatch metricNameMapping) []*match {
 	if metrics, ok := toMatch[f.include]; ok {
 		matches := make([]*match, 0, 10)
 		for _, metric := range metrics {
-			if f.labelMatched(metric) {
-				matches = append(matches, &match{metric: metric})
+			matchedMetric := f.labelMatched(metric)
+			if matchedMetric != nil {
+				matches = append(matches, &match{metric: matchedMetric})
 			}
 		}
 		return matches
@@ -92,9 +93,13 @@ func (f internalFilterStrict) getSubexpNames() []string {
 	return nil
 }
 
-func (f internalFilterStrict) labelMatched(metric *metricspb.Metric) bool {
+func (f internalFilterStrict) labelMatched(metric *metricspb.Metric) *metricspb.Metric {
+	metricWithMatchedLabel := proto.Clone(metric).(*metricspb.Metric)
+	var timeSeriesWithMatchedLabel []*metricspb.TimeSeries
+	labelIndexValueMap := make(map[int]string)
+
 	if len(f.matchLabels) == 0 {
-		return true
+		return metric
 	}
 
 	for key, value := range f.matchLabels {
@@ -106,18 +111,35 @@ func (f internalFilterStrict) labelMatched(metric *metricspb.Metric) bool {
 			}
 
 			keyFound = true
-			if len(metric.Timeseries) > 0 && metric.Timeseries[0].LabelValues[idx].Value != value {
-				return false
-			}
+			labelIndexValueMap[idx] = value
 		}
 
-		// if a label-key is not found then return false only if the given label-value is non-empty. If a given label-value is empty
-		// and the key is not found then return true. In this approach we can make sure certain key is not present which is a valid use case.
+		// if a label-key is not found then return nil only if the given label-value is non-empty. If a given label-value is empty
+		// and the key is not found then move forward. In this approach we can make sure certain key is not present which is a valid use case.
 		if !keyFound && value != "" {
-			return false
+			return nil
 		}
 	}
-	return true
+
+	for _, timeseries := range metric.Timeseries {
+		allValuesMatched := true
+		for index, value := range labelIndexValueMap {
+			if timeseries.LabelValues[index].Value != value {
+				allValuesMatched = false
+				break
+			}
+		}
+		if allValuesMatched {
+			timeSeriesWithMatchedLabel = append(timeSeriesWithMatchedLabel, timeseries)
+		}
+	}
+
+	if len(timeSeriesWithMatchedLabel) == 0 {
+		return nil
+	}
+
+	metricWithMatchedLabel.Timeseries = timeSeriesWithMatchedLabel
+	return metricWithMatchedLabel
 }
 
 type internalFilterRegexp struct {
@@ -130,8 +152,9 @@ func (f internalFilterRegexp) getMatches(toMatch metricNameMapping) []*match {
 	for name, metrics := range toMatch {
 		if submatches := f.include.FindStringSubmatchIndex(name); submatches != nil {
 			for _, metric := range metrics {
-				if f.labelMatched(metric) {
-					matches = append(matches, &match{metric: metric, pattern: f.include, submatches: submatches})
+				matchedMetric := f.labelMatched(metric)
+				if matchedMetric != nil {
+					matches = append(matches, &match{metric: matchedMetric, pattern: f.include, submatches: submatches})
 				}
 			}
 		}
@@ -143,9 +166,13 @@ func (f internalFilterRegexp) getSubexpNames() []string {
 	return f.include.SubexpNames()
 }
 
-func (f internalFilterRegexp) labelMatched(metric *metricspb.Metric) bool {
+func (f internalFilterRegexp) labelMatched(metric *metricspb.Metric) *metricspb.Metric {
+	metricWithMatchedLabel := proto.Clone(metric).(*metricspb.Metric)
+	var timeSeriesWithMatchedLabel []*metricspb.TimeSeries
+	labelIndexValueMap := make(map[int]*regexp.Regexp)
+
 	if len(f.matchLabels) == 0 {
-		return true
+		return metric
 	}
 
 	for key, value := range f.matchLabels {
@@ -157,18 +184,35 @@ func (f internalFilterRegexp) labelMatched(metric *metricspb.Metric) bool {
 			}
 
 			keyFound = true
-			if len(metric.Timeseries) > 0 && !value.MatchString(metric.Timeseries[0].LabelValues[idx].Value) {
-				return false
-			}
+			labelIndexValueMap[idx] = value
 		}
 
-		// if a label-key is not found then return false only if the given label-value is non-empty. If a given label-value is empty
-		// and the key is not found then return true. In this approach we can make sure certain key is not present which is a valid use case.
+		// if a label-key is not found then return nil only if the given label-value is non-empty. If a given label-value is empty
+		// and the key is not found then move forward. In this approach we can make sure certain key is not present which is a valid use case.
 		if !keyFound && !value.MatchString("") {
-			return false
+			return nil
 		}
 	}
-	return true
+
+	for _, timeseries := range metric.Timeseries {
+		allValuesMatched := true
+		for index, value := range labelIndexValueMap {
+			if !value.MatchString(timeseries.LabelValues[index].Value) {
+				allValuesMatched = false
+				break
+			}
+		}
+		if allValuesMatched {
+			timeSeriesWithMatchedLabel = append(timeSeriesWithMatchedLabel, timeseries)
+		}
+	}
+
+	if len(timeSeriesWithMatchedLabel) == 0 {
+		return nil
+	}
+
+	metricWithMatchedLabel.Timeseries = timeSeriesWithMatchedLabel
+	return metricWithMatchedLabel
 }
 
 type metricNameMapping map[string][]*metricspb.Metric
