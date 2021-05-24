caa45075186ed2af7da3cd888177ec2e8ea61ecd
Shaun Creary <shauncreary@rentalcars.com>
2021-05-24 15:49:56 +0100

    merge loki encoding with the resource atttribute branch

--- exporter/lokiexporter/exporter.go
+++ exporter/lokiexporter/exporter.go
@@ -36,11 +36,12 @@ import (
 )
 
 type lokiExporter struct {
-	config             *Config
-	logger             *zap.Logger
-	client             *http.Client
-	attributesToLabels map[string]model.LabelName
-	wg                 sync.WaitGroup
+	config                 *Config
+	logger                 *zap.Logger
+	client                 *http.Client
+	attribLogsToLabels     map[string]model.LabelName
+	attribResoucesToLabels map[string]model.LabelName
+	wg                     sync.WaitGroup
 }
 
 func newExporter(config *Config, logger *zap.Logger) (*lokiExporter, error) {
@@ -110,7 +111,8 @@ func encode(pb proto.Message) ([]byte, error) {
 }
 
 func (l *lokiExporter) start(context.Context, component.Host) (err error) {
-	l.attributesToLabels = l.config.Labels.getAttributes()
+	l.attribLogsToLabels = l.config.Labels.getLogRecordAttributes()
+	l.attribResoucesToLabels = l.config.Labels.getResourceAttributes()
 	return nil
 }
 
@@ -124,20 +126,20 @@ func (l *lokiExporter) logDataToLoki(ld pdata.Logs) (pr *logproto.PushRequest, n
 	rls := ld.ResourceLogs()
 	for i := 0; i < rls.Len(); i++ {
 		ills := rls.At(i).InstrumentationLibraryLogs()
+		resource := rls.At(i).Resource()
 		for j := 0; j < ills.Len(); j++ {
 			logs := ills.At(j).Logs()
 			for k := 0; k < logs.Len(); k++ {
 				log := logs.At(k)
-				attribLabels, ok := l.convertAttributesToLabels(log.Attributes())
-				if !ok {
+
+				mergedLabels, dropped := l.convertAttributesAndMerge(log.Attributes(), resource.Attributes())
+				if dropped {
 					numDroppedLogs++
 					continue
 				}
-
-				labels := attribLabels.String()
-
-				// entry := convertLogToLokiEntry(log)
-				entry := convertLogToJsonEntry(log)
+				labels := mergedLabels.String()
+				entry := convertLogToLokiEntry(log)
+				// entry := convertLogToJsonEntry(log)
 
 				if stream, ok := streams[labels]; ok {
 					stream.Entries = append(stream.Entries, *entry)
@@ -165,10 +167,23 @@ func (l *lokiExporter) logDataToLoki(ld pdata.Logs) (pr *logproto.PushRequest, n
 	return pr, numDroppedLogs
 }
 
-func (l *lokiExporter) convertAttributesToLabels(attributes pdata.AttributeMap) (model.LabelSet, bool) {
+func (l *lokiExporter) convertAttributesAndMerge(logAttrs pdata.AttributeMap, resourceAttrs pdata.AttributeMap) (mergedAttributes model.LabelSet, dropped bool) {
+	logRecordAttributes := l.convertLogAttributesToLabels(logAttrs)
+	resourceAttributes := l.convertResourceAttributesToLabels(resourceAttrs)
+
+	// This prometheus model.labelset Merge function overwrites	the logRecordAttributes with resourceAttributes
+	mergedAttributes = logRecordAttributes.Merge(resourceAttributes)
+
+	if len(mergedAttributes) == 0 {
+		return nil, true
+	}
+	return mergedAttributes, false
+}
+
+func (l *lokiExporter) convertLogAttributesToLabels(attributes pdata.AttributeMap) model.LabelSet {
 	ls := model.LabelSet{}
 
-	for attr, attrLabelName := range l.attributesToLabels {
+	for attr, attrLabelName := range l.attribLogsToLabels {
 		av, ok := attributes.Get(attr)
 		if ok {
 			if av.Type() != pdata.AttributeValueTypeString {
@@ -179,11 +194,24 @@ func (l *lokiExporter) convertAttributesToLabels(attributes pdata.AttributeMap) 
 		}
 	}
 
-	if len(ls) == 0 {
-		return nil, false
+	return ls
+}
+
+func (l *lokiExporter) convertResourceAttributesToLabels(attributes pdata.AttributeMap) model.LabelSet {
+	ls := model.LabelSet{}
+
+	for attr, attrLabelName := range l.attribResoucesToLabels {
+		av, ok := attributes.Get(attr)
+		if ok {
+			if av.Type() != pdata.AttributeValueTypeString {
+				l.logger.Debug("Failed to convert attribute value to Loki label value, value is not a string", zap.String("attribute", attr))
+				continue
+			}
+			ls[attrLabelName] = model.LabelValue(av.StringVal())
+		}
 	}
 
-	return ls, true
+	return ls
 }
 
 func convertLogToLokiEntry(lr pdata.LogRecord) *logproto.Entry {
