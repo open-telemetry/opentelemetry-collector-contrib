9b60746dd000573c5c7997a30dfcd59a8a7de8e3
Tigran Najaryan <4194920+tigrannajaryan@users.noreply.github.com>
2021-05-25 18:52:17 -0400

    Disable unstable Syslog UDP test until it is fixed
    We have a bug recorded here https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/3095
    
    We will re-enable the test when the bug is fixed.

--- testbed/tests/log_test.go
+++ testbed/tests/log_test.go
@@ -101,15 +101,6 @@ func TestLog10kDPS(t *testing.T) {
 			},
 		},
 		{
-			name:     "syslog-udp",
-			sender:   datasenders.NewSyslogWriter("udp", testbed.DefaultHost, testbed.GetAvailablePort(t), 1),
-			receiver: testbed.NewOTLPDataReceiver(testbed.GetAvailablePort(t)),
-			resourceSpec: testbed.ResourceSpec{
-				ExpectedMaxCPU: 80,
-				ExpectedMaxRAM: 150,
-			},
-		},
-		{
 			name:     "syslog-tcp-batch-1",
 			sender:   datasenders.NewTCPUDPWriter("tcp", testbed.DefaultHost, testbed.GetAvailablePort(t), 1),
 			receiver: testbed.NewOTLPDataReceiver(testbed.GetAvailablePort(t)),
