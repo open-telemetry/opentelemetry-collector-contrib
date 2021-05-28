63437f7eb72c6b16f2d76d9637baa0c17e5767a2
Granville Schmidt <1246157+gramidt@users.noreply.github.com>
2021-05-27 19:47:34 -0600

    [signalfxexporter] Updated for upcoming client auth extension changes (#3511)
    Link to tracking Issue:
    #3507

--- exporter/signalfxexporter/correlation/correlation_test.go
+++ exporter/signalfxexporter/correlation/correlation_test.go
@@ -19,7 +19,11 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/component/componenttest"
+	"go.opentelemetry.io/collector/config/confighttp"
+	"go.opentelemetry.io/collector/config/configtls"
 	"go.opentelemetry.io/collector/consumer/pdata"
 	"go.uber.org/zap"
 )
@@ -33,6 +37,10 @@ func TestTrackerAddSpans(t *testing.T) {
 		},
 	)
 
+	err := tracker.Start(context.Background(), componenttest.NewNopHost())
+	require.NoError(t, err)
+	assert.NotNil(t, tracker.correlation, "correlation context should be set")
+
 	traces := pdata.NewTraces()
 	rs := traces.ResourceSpans().AppendEmpty()
 	attr := rs.Resource().Attributes()
@@ -40,13 +48,60 @@ func TestTrackerAddSpans(t *testing.T) {
 
 	// Add empty first, should ignore.
 	tracker.AddSpans(context.Background(), pdata.NewTraces())
-	assert.Nil(t, tracker.correlation)
 	assert.Nil(t, tracker.traceTracker)
 
 	tracker.AddSpans(context.Background(), traces)
 
-	assert.NotNil(t, tracker.correlation, "correlation context should be set")
 	assert.NotNil(t, tracker.traceTracker, "trace tracker should be set")
 
 	assert.NoError(t, tracker.Shutdown(context.Background()))
+}
+
+func TestTrackerStart(t *testing.T) {
+
+	tests := []struct {
+		name    string
+		config  *Config
+		wantErr bool
+		errMsg  string
+	}{
+		{
+			name: "invalid http client settings fails",
+			config: &Config{
+				HTTPClientSettings: confighttp.HTTPClientSettings{
+					Endpoint: "localhost:9090",
+					TLSSetting: configtls.TLSClientSetting{
+						TLSSetting: configtls.TLSSetting{
+							CAFile: "/non/existent",
+						},
+					},
+				},
+			},
+			wantErr: true,
+			errMsg:  "failed to create correlation API client: failed to load TLS config",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tracker := NewTracker(
+				tt.config,
+				"abcd",
+				component.ExporterCreateParams{
+					Logger: zap.NewNop(),
+				},
+			)
+
+			err := tracker.Start(context.Background(), componenttest.NewNopHost())
+
+			if tt.wantErr {
+				require.Error(t, err)
+				if tt.errMsg != "" {
+					require.Contains(t, err.Error(), tt.errMsg)
+				}
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
 }
