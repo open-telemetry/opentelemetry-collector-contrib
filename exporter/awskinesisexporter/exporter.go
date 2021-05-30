a6cf597ca9bbce311b4db700ed0c29194b453800
Sean ZO Marciniak <smarciniak@atlassian.com>
2021-05-31 09:43:57 +1000

    Fixing left over imports

--- exporter/awskinesisexporter/exporter.go
+++ exporter/awskinesisexporter/exporter.go
@@ -17,12 +17,11 @@ package awskinesisexporter
 import (
 	"context"
 
-	"go.uber.org/zap"
-
 	awskinesis "github.com/signalfx/opencensus-go-exporter-kinesis"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/consumer/pdata"
+	"go.uber.org/zap"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/awskinesisexporter/internal/translate"
 )
