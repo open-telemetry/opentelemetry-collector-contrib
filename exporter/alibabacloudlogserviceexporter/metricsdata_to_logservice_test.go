13ea237491255d69e42ae23ab98f0b6f0496ffdf
Bogdan Drutu <bogdandrutu@gmail.com>
2021-06-03 00:28:51 +0300

    Fix flaky test in alibabaexporter, remove unused public func (#3693)
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- exporter/alibabacloudlogserviceexporter/metricsdata_to_logservice_test.go
+++ exporter/alibabacloudlogserviceexporter/metricsdata_to_logservice_test.go
@@ -165,9 +165,7 @@ func TestMetricCornerCases(t *testing.T) {
 	assert.Equal(t, min(2, 1), 1)
 	assert.Equal(t, min(1, 1), 1)
 	var label KeyValues
-	label.AppendMap(map[string]string{
-		"a": "b",
-	})
+	label.Append("a", "b")
 	assert.Equal(t, label.String(), "a#$#b")
 }
 
@@ -175,10 +173,8 @@ func TestMetricLabelSanitize(t *testing.T) {
 	var label KeyValues
 	label.Append("_test", "key_test")
 	label.Append("0test", "key_0test")
-	label.AppendMap(map[string]string{
-		"test_normal": "test_normal",
-		"0test":       "key_0test",
-	})
+	label.Append("test_normal", "test_normal")
+	label.Append("0test", "key_0test")
 	assert.Equal(t, label.String(), "key_test#$#key_test|key_0test#$#key_0test|test_normal#$#test_normal|key_0test#$#key_0test")
 	label.Sort()
 	assert.Equal(t, label.String(), "key_0test#$#key_0test|key_0test#$#key_0test|key_test#$#key_test|test_normal#$#test_normal")
