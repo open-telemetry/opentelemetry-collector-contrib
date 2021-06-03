80d62d667cf9ad7f0b2c90f1d361515f9ca77465
Granville Schmidt <1246157+gramidt@users.noreply.github.com>
2021-06-03 14:08:33 -0600

    Update core to latest and configure components for the Client Authentication Extensions (2/2) (#3694)
    This is the second phase of #3507.
    
    **Link to tracking Issue:**
    #3692 
    
    Component changes are based on https://github.com/open-telemetry/opentelemetry-collector/pull/3340.
    
    Also related to https://github.com/open-telemetry/opentelemetry-collector/issues/3282

--- exporter/humioexporter/humio_client.go
+++ exporter/humioexporter/humio_client.go
@@ -107,8 +107,8 @@ type humioClient struct {
 }
 
 // Constructs a new HTTP client for sending payloads to Humio
-func newHumioClient(cfg *Config, logger *zap.Logger, _ component.Host) (exporterClient, error) {
-	client, err := cfg.HTTPClientSettings.ToClient()
+func newHumioClient(cfg *Config, logger *zap.Logger, host component.Host) (exporterClient, error) {
+	client, err := cfg.HTTPClientSettings.ToClient(host.GetExtensions())
 	if err != nil {
 		return nil, err
 	}
