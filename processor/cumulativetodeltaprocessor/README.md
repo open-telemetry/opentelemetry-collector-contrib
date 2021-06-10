9a9d3a1617333a2b4d45b4de7a1c1646c34bf511
Rayhan Hossain <hossain.rayhan@outlook.com>
2021-06-10 10:30:08 -0700

    skeleton for new cumulative to delta processor
    Signed-off-by: Rayhan Hossain <hossain.rayhan@outlook.com>

---
+++ processor/cumulativetodeltaprocessor/README.md
@@ -0,0 +1,8 @@
+# Cumulative to Delta Processor
+**Status: under development; Not recommended for production usage.**
+
+Supported pipeline types: metrics
+
+## Description
+
+The cumulative to delta processor (`cumulativetodeltaprocessor`) converts cumulative sum metrics to cumulative delta. 
---
+++ processor/cumulativetodeltaprocessor/processor.go
@@ -0,0 +1,59 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cumulativetodeltaprocessor
+
+import (
+	"context"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/consumer/pdata"
+	"go.opentelemetry.io/collector/processor/processorhelper"
+	"go.uber.org/zap"
+)
+
+type cumulativeToDeltaProcessor struct {
+	metrics []internalMetric
+	logger  *zap.Logger
+}
+
+var _ processorhelper.MProcessor = (*cumulativeToDeltaProcessor)(nil)
+
+type internalMetric struct {
+	name                  string
+	resourceAttributeKeys []string
+	metricLabelKeys       []string
+}
+
+func newCumulativeToDeltaProcessor(metrics []internalMetric, logger *zap.Logger) *cumulativeToDeltaProcessor {
+	return &cumulativeToDeltaProcessor{
+		metrics: metrics,
+		logger:  logger,
+	}
+}
+
+// Start is invoked during service startup.
+func (mgp *cumulativeToDeltaProcessor) Start(context.Context, component.Host) error {
+	return nil
+}
+
+// ProcessMetrics implements the MProcessor interface.
+func (mgp *cumulativeToDeltaProcessor) ProcessMetrics(_ context.Context, md pdata.Metrics) (pdata.Metrics, error) {
+	return md, nil
+}
+
+// Shutdown is invoked during service shutdown.
+func (mgp *cumulativeToDeltaProcessor) Shutdown(context.Context) error {
+	return nil
+}
