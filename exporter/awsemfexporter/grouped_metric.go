9177663c954f7e7e4e861e5d8230f8c7617f4b89
Aditya-Gollapudi <adityakg@princeton.edu>
2021-06-30 09:18:40 -0700

    readded container metric specfic values

--- exporter/awsemfexporter/grouped_metric.go
+++ exporter/awsemfexporter/grouped_metric.go
@@ -108,7 +108,9 @@ func addToGroupedMetric(pmd *pdata.Metric, groupedMetrics map[interface{}]*Group
 }
 
 type kubernetesObj struct{
-	Host string `json:"`
+	Container_name string `json:`
+	Docker internalDockerObj `json:`
+	Host string `json:`
 	Labels internalLabelsObj `json:`
 	Namespace_name string `json:`
 	Pod_id string `json:`
@@ -117,9 +119,13 @@ type kubernetesObj struct{
 	Service_name string `json:`
 }
 
+type internalDockerObj struct{
+	Container_id string `json:`
+}
+
 type internalLabelsObj struct{
 	App string `json:`
-	Pod-template-hash string `json:`
+	Pod_template_hash string `json: "pod-template-hash"`
 }
 
 type internalPodOwnersObj struct{
@@ -131,11 +137,16 @@ type internalPodOwnersObj struct{
 func addKubernetesWrapper(labels map[string]string) error{
 	//create schema
 	schema := kubernetesObj{}
+	schema.Container_name = "container_name"
+	schema.Docker =
+	internalDockerObj{
+		Container_id: "container_id"
+	}
 	schema.Host = "host_name"
 	schema.Labels =
 	internalLabelsObj{
 		App: "app",
-		Pod-template-hash: "pod-template-hash",
+		Pod_template_hash: "pod-template-hash",
 	}
 	schema.Namespace_name = "namespace_name"
 	schema.Pod_id = "pod_id" 
