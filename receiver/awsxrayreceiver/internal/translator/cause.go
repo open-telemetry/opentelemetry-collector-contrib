c76229eb5330aeb9fd49eba4df264d0012af13d0
Anthony Mirabella <a9@aneurysm9.com>
2021-07-16 11:32:18 -0400

    receiver/*: remove use of pdata slice Resize() (#4208)
    Signed-off-by: Anthony J Mirabella <a9@aneurysm9.com>

--- receiver/awsxrayreceiver/internal/translator/cause.go
+++ receiver/awsxrayreceiver/internal/translator/cause.go
@@ -56,10 +56,10 @@ func addCause(seg *awsxray.Segment, span *pdata.Span) {
 		// not sure whether there are existing events, so
 		// append new empty events instead
 		exceptionEventStartIndex := evts.Len()
-		evts.Resize(exceptionEventStartIndex + len(seg.Cause.Exceptions))
+		evts.EnsureCapacity(exceptionEventStartIndex + len(seg.Cause.Exceptions))
 
-		for i, excp := range seg.Cause.Exceptions {
-			evt := evts.At(exceptionEventStartIndex + i)
+		for _, excp := range seg.Cause.Exceptions {
+			evt := evts.AppendEmpty()
 			evt.SetName(conventions.AttributeExceptionEventName)
 			attrs := evt.Attributes()
 			attrs.Clear()
