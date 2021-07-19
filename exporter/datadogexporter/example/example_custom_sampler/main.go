f26a43fd944bf7e509d8c180a8ef7c937d87b195
ericmustin <mustin.eric@gmail.com>
2021-07-19 16:03:50 -0400

    [exporter/datadog]: add license

--- exporter/datadogexporter/example/example_custom_sampler/main.go
+++ exporter/datadogexporter/example/example_custom_sampler/main.go
@@ -1,6 +1,20 @@
-// This program sends one span to the Datadog Agent and is meant to illustrate how to connect an existing
-// OpenTelemtry-instrumented program. The default connection is HTTP and the gRPC connection is activated
-// by means of the -grpc flag.
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This program sends one span to the OpenTelemetry-Collector Agent and is meant to illustrate how to connect an existing
+// OpenTelemtry-instrumented programs using a DatadogTraceIDRatioBased s ampler.
+// The default connection is HTTP and the gRPC connection is activated by means of the -grpc flag.
 
 package main
 
