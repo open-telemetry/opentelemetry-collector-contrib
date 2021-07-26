88e8725b76d5f091cdcd880d6303e961d01b07c9
Bogdan Drutu <bogdandrutu@gmail.com>
2021-07-26 11:12:55 -0700

    Convert receiver/nginxreceiver to new Number metrics (#4325)
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- receiver/nginxreceiver/scraper_test.go
+++ receiver/nginxreceiver/scraper_test.go
@@ -71,22 +71,20 @@ Reading: 6 Writing: 179 Waiting: 106
 	for i := 0; i < ms.Len(); i++ {
 		m := ms.At(i)
 
-		var dps pdata.IntDataPointSlice
-
 		switch m.DataType() {
-		case pdata.MetricDataTypeIntGauge:
-			dps = m.IntGauge().DataPoints()
+		case pdata.MetricDataTypeGauge:
+			dps := m.Gauge().DataPoints()
 			require.Equal(t, 4, dps.Len())
 			for j := 0; j < dps.Len(); j++ {
 				dp := dps.At(j)
 				state, _ := dp.LabelsMap().Get(metadata.L.State)
 				label := fmt.Sprintf("%s state:%s", m.Name(), state)
-				metricValues[label] = dp.Value()
+				metricValues[label] = dp.IntVal()
 			}
-		case pdata.MetricDataTypeIntSum:
-			dps = m.IntSum().DataPoints()
+		case pdata.MetricDataTypeSum:
+			dps := m.Sum().DataPoints()
 			require.Equal(t, 1, dps.Len())
-			metricValues[m.Name()] = dps.At(0).Value()
+			metricValues[m.Name()] = dps.At(0).IntVal()
 		}
 	}
 
