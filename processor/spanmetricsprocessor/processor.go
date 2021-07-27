67769b37317847d93a4c840837a4be4edcac9dd5
alrex <aboten@lightstep.com>
2021-07-26 18:54:52 -0700

    spanmetricsprocessor: update IntSum/IntGauge Sum/Gauge (#4370)

--- processor/spanmetricsprocessor/processor.go
+++ processor/spanmetricsprocessor/processor.go
@@ -262,15 +262,15 @@ func (p *processorImp) collectLatencyMetrics(ilm pdata.InstrumentationLibraryMet
 func (p *processorImp) collectCallMetrics(ilm pdata.InstrumentationLibraryMetrics) {
 	for key := range p.callSum {
 		mCalls := ilm.Metrics().AppendEmpty()
-		mCalls.SetDataType(pdata.MetricDataTypeIntSum)
+		mCalls.SetDataType(pdata.MetricDataTypeSum)
 		mCalls.SetName("calls_total")
-		mCalls.IntSum().SetIsMonotonic(true)
-		mCalls.IntSum().SetAggregationTemporality(pdata.AggregationTemporalityCumulative)
+		mCalls.Sum().SetIsMonotonic(true)
+		mCalls.Sum().SetAggregationTemporality(pdata.AggregationTemporalityCumulative)
 
-		dpCalls := mCalls.IntSum().DataPoints().AppendEmpty()
+		dpCalls := mCalls.Sum().DataPoints().AppendEmpty()
 		dpCalls.SetStartTimestamp(pdata.TimestampFromTime(p.startTime))
 		dpCalls.SetTimestamp(pdata.TimestampFromTime(time.Now()))
-		dpCalls.SetValue(p.callSum[key])
+		dpCalls.SetIntVal(p.callSum[key])
 
 		dpCalls.LabelsMap().InitFromMap(p.metricKeyToDimensions[key])
 	}
