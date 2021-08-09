d7d6614465d0eae91e077ed98d19afff728acb90
Gleb Smirnov <gsmirnov@splunk.com>
2021-08-09 20:11:24 +0300

    Will the linter be happy now?

--- exporter/splunkhecexporter/client_test.go
+++ exporter/splunkhecexporter/client_test.go
@@ -28,14 +28,13 @@ import (
 	"testing"
 	"time"
 
-	"go.uber.org/zap/zaptest"
-
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component/componenttest"
 	"go.opentelemetry.io/collector/consumer/consumererror"
 	"go.opentelemetry.io/collector/model/pdata"
 	"go.opentelemetry.io/collector/translator/conventions"
+	"go.uber.org/zap/zaptest"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/internal/splunk"
 )
