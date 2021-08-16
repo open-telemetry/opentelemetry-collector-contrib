cc6906182fed6b5a2b90302675771ddd4d94ecd2
Alexis Perez <50466397+alexperez52@users.noreply.github.com>
2021-08-16 11:49:31 -0400

    added factory fixup

--- cmd/otelcontribcol/components.go
+++ cmd/otelcontribcol/components.go
@@ -128,6 +128,7 @@ func components() (component.Factories, error) {
 		kafkametricsreceiver.NewFactory(),
 		k8sclusterreceiver.NewFactory(),
 		kubeletstatsreceiver.NewFactory(),
+		opencensusreceiver.NewFactory(),
 		prometheusexecreceiver.NewFactory(),
 		receivercreator.NewFactory(),
 		redisreceiver.NewFactory(),
