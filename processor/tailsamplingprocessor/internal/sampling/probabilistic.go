79ec7b0cc063cecb11aca13de9217c3997a9fab8
Koenraad Verheyden <koenraad.verheyden@grafana.com>
2021-08-19 18:38:43 +0200

    Add new tail sampling processor policy: probabilistic (#3876)
    * Add new tail sampling processor policy: percentage
    
    * Update CHANGELOG.md
    
    * Fix tail_sampling_config.yaml
    
    * Fix typo
    
    * Reset counters to avoid overflow, improve tests
    
    * Move into internal as well
    
    * Use sampling_percentage similar to the probabilistic sampling processor
    
    Use sampling_percentage instead of percentage
    Make sampling_percentage a value between 0 and 100
    
    * Combine tests in a single table-test
    
    * Rename percentage -> probabilistic for consistency with probabilistic processor
    
    * Add IncludeAlreadySampled option
    
    * Clarify test cases
    
    * Use the same algorithm as the probabilistic sampling processor
    
    * Simplify if-else case
    
    * Typo
    
    * Order of tail sampling policies does not matter
    
    * Switch hashing implementation from murmur3 to fnv-1a
    
    * Lower amount of traces to sample in test to speed up tests

---
+++ processor/tailsamplingprocessor/internal/sampling/probabilistic.go
@@ -0,0 +1,91 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sampling
+
+import (
+	"hash/fnv"
+	"math"
+	"math/big"
+
+	"go.opentelemetry.io/collector/model/pdata"
+	"go.uber.org/zap"
+)
+
+const (
+	defaultHashSalt = "default-hash-seed"
+)
+
+type probabilisticSampler struct {
+	logger    *zap.Logger
+	threshold uint64
+	hashSalt  string
+}
+
+var _ PolicyEvaluator = (*probabilisticSampler)(nil)
+
+// NewProbabilisticSampler creates a policy evaluator that samples a percentage of
+// traces.
+func NewProbabilisticSampler(logger *zap.Logger, hashSalt string, samplingPercentage float64) PolicyEvaluator {
+	if hashSalt == "" {
+		hashSalt = defaultHashSalt
+	}
+
+	return &probabilisticSampler{
+		logger: logger,
+		// calculate threshold once
+		threshold: calculateThreshold(samplingPercentage / 100),
+		hashSalt:  hashSalt,
+	}
+}
+
+// OnLateArrivingSpans notifies the evaluator that the given list of spans arrived
+// after the sampling decision was already taken for the trace.
+// This gives the evaluator a chance to log any message/metrics and/or update any
+// related internal state.
+func (s *probabilisticSampler) OnLateArrivingSpans(Decision, []*pdata.Span) error {
+	s.logger.Debug("Triggering action for late arriving spans in probabilistic filter")
+	return nil
+}
+
+// Evaluate looks at the trace data and returns a corresponding SamplingDecision.
+func (s *probabilisticSampler) Evaluate(traceID pdata.TraceID, _ *TraceData) (Decision, error) {
+	s.logger.Debug("Evaluating spans in probabilistic filter")
+
+	traceIDBytes := traceID.Bytes()
+	if hashTraceID(s.hashSalt, traceIDBytes[:]) <= s.threshold {
+		return Sampled, nil
+	}
+
+	return NotSampled, nil
+}
+
+// calculateThreshold converts a ratio into a value between 0 and MaxUint64
+func calculateThreshold(ratio float64) uint64 {
+	// Use big.Float and big.Int to calculate threshold because directly convert
+	// math.MaxUint64 to float64 will cause digits/bits to be cut off if the converted value
+	// doesn't fit into bits that are used to store digits for float64 in Golang
+	boundary := new(big.Float).SetInt(new(big.Int).SetUint64(math.MaxUint64))
+	res, _ := boundary.Mul(boundary, big.NewFloat(ratio)).Uint64()
+	return res
+}
+
+// hashTraceID creates a hash using the FNV-1a algorithm.
+func hashTraceID(salt string, b []byte) uint64 {
+	hasher := fnv.New64a()
+	// the implementation fnv.Write() never returns an error, see hash/fnv/fnv.go
+	_, _ = hasher.Write([]byte(salt))
+	_, _ = hasher.Write(b)
+	return hasher.Sum64()
+}
