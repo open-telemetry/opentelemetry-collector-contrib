68b3835c5048a03b57637bb3996d99b842327921
Bogdan Drutu <bogdandrutu@gmail.com>
2021-08-23 17:07:12 -0700

    Move zipkinexporter from collector core to collector contrib (#4840)
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- exporter/zipkinexporter/zipkin.go
+++ exporter/zipkinexporter/zipkin.go
@@ -22,12 +22,12 @@ import (
 
 	"github.com/openzipkin/zipkin-go/proto/zipkin_proto3"
 	zipkinreporter "github.com/openzipkin/zipkin-go/reporter"
-
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/config/confighttp"
 	"go.opentelemetry.io/collector/consumer/consumererror"
 	"go.opentelemetry.io/collector/model/pdata"
-	"go.opentelemetry.io/collector/translator/trace/zipkinv2"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/translator/zipkin/zipkinv2"
 )
 
 var translator zipkinv2.FromTranslator
