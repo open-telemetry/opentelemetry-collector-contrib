f58c0136f5c464d77780c207178d13bb08266a36
tianduo <tianduo@amazon.com>
2021-09-08 10:52:41 -0700

    Modify unit tests and go mod

--- extension/awsecshealthcheckextension/factory_test.go
+++ extension/awsecshealthcheckextension/factory_test.go
@@ -20,13 +20,12 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"go.uber.org/zap"
-
-	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/component/componenttest"
 	"go.opentelemetry.io/collector/config"
 	"go.opentelemetry.io/collector/config/configcheck"
 	"go.opentelemetry.io/collector/config/confignet"
-	"go.opentelemetry.io/collector/testutil"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/internal/coreinternal/testutil"
 )
 
 func TestFactory_CreateDefaultConfig(t *testing.T) {
@@ -41,7 +40,7 @@ func TestFactory_CreateDefaultConfig(t *testing.T) {
 	}, cfg)
 
 	assert.NoError(t, configcheck.ValidateConfig(cfg))
-	ext, err := createExtension(context.Background(), component.ExtensionCreateSettings{Logger: zap.NewNop()}, cfg)
+	ext, err := createExtension(context.Background(), componenttest.NewNopExtensionCreateSettings(), cfg)
 	require.NoError(t, err)
 	require.NotNil(t, ext)
 }
@@ -50,7 +49,7 @@ func TestFactory_CreateExtension(t *testing.T) {
 	cfg := createDefaultConfig().(*Config)
 	cfg.TCPAddr.Endpoint = testutil.GetAvailableLocalAddress(t)
 
-	ext, err := createExtension(context.Background(), component.ExtensionCreateSettings{Logger: zap.NewNop()}, cfg)
+	ext, err := createExtension(context.Background(), componenttest.NewNopExtensionCreateSettings(), cfg)
 	require.NoError(t, err)
 	require.NotNil(t, ext)
 }
--- extension/awsecshealthcheckextension/go.mod
+++ extension/awsecshealthcheckextension/go.mod
@@ -4,9 +4,12 @@ go 1.17
 
 require (
 	github.com/jaegertracing/jaeger v1.25.0
+	github.com/open-telemetry/opentelemetry-collector-contrib/internal/coreinternal v0.34.0
 	github.com/stretchr/testify v1.7.0
 	go.opencensus.io v0.23.0
 	go.opentelemetry.io/collector v0.34.0
 	go.uber.org/zap v1.19.0
 	gotest.tools/v3 v3.0.3
 )
+
+replace github.com/open-telemetry/opentelemetry-collector-contrib/internal/coreinternal => ../../internal/coreinternal
