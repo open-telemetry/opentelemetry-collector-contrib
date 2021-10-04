1993f82a3b660d81166f5765fe832923aa25bf3f
Tigran Najaryan <4194920+tigrannajaryan@users.noreply.github.com>
2021-10-04 18:22:07 +0400

    Bump CPU limits for TestTraceBallast1kSPSWithAttrs (#5574)
    It is close to limits and fails sometimes: https://app.circleci.com/pipelines/github/open-telemetry/opentelemetry-collector-contrib/20760/workflows/98f9c85a-d289-4342-a3bd-aa92a608db54/jobs/165666

--- testbed/tests/trace_test.go
+++ testbed/tests/trace_test.go
@@ -243,14 +243,14 @@ func TestTraceBallast1kSPSWithAttrs(t *testing.T) {
 		{
 			attrCount:      100,
 			attrSizeByte:   50,
-			expectedMaxCPU: 80,
+			expectedMaxCPU: 100,
 			expectedMaxRAM: 2200,
 			resultsSummary: performanceResultsSummary,
 		},
 		{
 			attrCount:      10,
 			attrSizeByte:   1000,
-			expectedMaxCPU: 80,
+			expectedMaxCPU: 100,
 			expectedMaxRAM: 2200,
 			resultsSummary: performanceResultsSummary,
 		},
