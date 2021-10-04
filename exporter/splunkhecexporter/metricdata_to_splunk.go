e2d43c9e959cf169da4e5f9a7e5f1cda59d6e121
Antoine Toulme <antoine@lunar-ocean.com>
2021-10-04 09:11:26 -0700

    ensure all HEC mapped data can be marshalled to json, handling explicitly NaN and Inf values

--- exporter/splunkhecexporter/metricdata_to_splunk.go
+++ exporter/splunkhecexporter/metricdata_to_splunk.go
@@ -38,7 +38,26 @@ const (
 	sumSuffix = "_sum"
 	// bucketSuffix is the bucket metric value suffix.
 	bucketSuffix = "_bucket"
+	// nanValue is the string representation of a NaN value in HEC events
+	nanValue = "NaN"
+	// plusInfValue is the string representation of a +Inf value in HEC events
+	plusInfValue = "+Inf"
+	// minusInfValue is the string representation of a -Inf value in HEC events
+	minusInfValue = "-Inf"
 )
+
+func sanitizeFloat(value float64) interface{} {
+	if math.IsNaN(value) {
+		return nanValue
+	}
+	if math.IsInf(value, 1) {
+		return plusInfValue
+	}
+	if math.IsInf(value, -1) {
+		return minusInfValue
+	}
+	return value
+}
 
 func metricDataToSplunk(logger *zap.Logger, data pdata.Metrics, config *Config) ([]*splunk.Event, int) {
 	numDroppedTimeSeries := 0
@@ -90,7 +109,7 @@ func metricDataToSplunk(logger *zap.Logger, data pdata.Metrics, config *Config) 
 						case pdata.MetricValueTypeInt:
 							fields[metricFieldName] = dataPt.IntVal()
 						case pdata.MetricValueTypeDouble:
-							fields[metricFieldName] = dataPt.DoubleVal()
+							fields[metricFieldName] = sanitizeFloat(dataPt.DoubleVal())
 						}
 						fields[splunkMetricTypeKey] = pdata.MetricDataTypeGauge.String()
 						sm := createEvent(dataPt.Timestamp(), host, source, sourceType, index, fields)
@@ -157,7 +176,7 @@ func metricDataToSplunk(logger *zap.Logger, data pdata.Metrics, config *Config) 
 						case pdata.MetricValueTypeInt:
 							fields[metricFieldName] = dataPt.IntVal()
 						case pdata.MetricValueTypeDouble:
-							fields[metricFieldName] = dataPt.DoubleVal()
+							fields[metricFieldName] = sanitizeFloat(dataPt.DoubleVal())
 						}
 						fields[splunkMetricTypeKey] = pdata.MetricDataTypeSum.String()
 						sm := createEvent(dataPt.Timestamp(), host, source, sourceType, index, fields)
@@ -191,7 +210,7 @@ func metricDataToSplunk(logger *zap.Logger, data pdata.Metrics, config *Config) 
 							populateAttributes(fields, dataPt.Attributes())
 							dp := dataPt.QuantileValues().At(bi)
 							fields["qt"] = float64ToDimValue(dp.Quantile())
-							fields[metricFieldName+"_"+strconv.FormatFloat(dp.Quantile(), 'f', -1, 64)] = dp.Value()
+							fields[metricFieldName+"_"+strconv.FormatFloat(dp.Quantile(), 'f', -1, 64)] = sanitizeFloat(dp.Value())
 							fields[splunkMetricTypeKey] = pdata.MetricDataTypeSummary.String()
 							sm := createEvent(dataPt.Timestamp(), host, source, sourceType, index, fields)
 							splunkMetrics = append(splunkMetrics, sm)
