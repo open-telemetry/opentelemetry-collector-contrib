e0b2cedeb6dfa877e712b83b9df0a0f05542c6e0
Przemek Maciolek <pmaciolek@sumologic.com>
2021-10-04 09:13:57 +0200

    Update infrastructure_service->platform

--- processor/resourcedetectionprocessor/internal/aws/elasticbeanstalk/elasticbeanstalk_test.go
+++ processor/resourcedetectionprocessor/internal/aws/elasticbeanstalk/elasticbeanstalk_test.go
@@ -98,7 +98,7 @@ func Test_AttributesDetectedSuccessfully(t *testing.T) {
 	want := pdata.NewResource()
 	attr := want.Attributes()
 	attr.InsertString("cloud.provider", "aws")
-	attr.InsertString("cloud.infrastructure_service", "aws_elastic_beanstalk")
+	attr.InsertString("cloud.platform", "aws_elastic_beanstalk")
 	attr.InsertString("cloud.namespace", "ElasticBeanstalk")
 	attr.InsertString("deployment.environment", "BETA")
 	attr.InsertString("service.instance.id", "23")
