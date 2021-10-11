dd44390a4a3b64b3a9c9a1ca57e95903282765ac
Sean Marciniak <30928402+MovieStoreGuy@users.noreply.github.com>
2021-10-12 04:39:51 +1030

    awskinesisexporter: extending to allow for dynamic export types (#5440)
    * Updating the work to add in dynamic encoding and compression for
    kinesis exporter
    
    * Fixing typo and updating default
    
    * Rename local var to make it clear it is to be used for exporting
    
    * Moved NewEncoder method in to a more fiting, restricting to single name
    
    * Fixing test
    
    * Fixing importing issues
    
    * Adding code comment as to why export exists
    
    * removing Permanent error
    
    * Adding updates to mod files
    
    * Updating mods

--- exporter/awskinesisexporter/internal/key/key_test.go
+++ exporter/awskinesisexporter/internal/key/key_test.go
@@ -12,6 +12,20 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package batch_test
+package key_test
 
-// Tests for jeager encoder are part of the encode_marshaler_test.go
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/awskinesisexporter/internal/key"
+)
+
+func TestEnsureDifferentKeys(t *testing.T) {
+	t.Parallel()
+
+	k := key.Randomized(nil)
+	assert.NotEmpty(t, k, "Must have a string that has a value")
+	assert.NotEqual(t, k, key.Randomized(nil), "Must have different string values")
+}
