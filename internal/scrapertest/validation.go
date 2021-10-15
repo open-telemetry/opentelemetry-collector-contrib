4d3a57eda10ca8b98102f48d058d5b399e98128f
Mrod1598 <miguel.rodriguez@observiq.com>
2021-10-15 09:11:47 -0400

    Add license

--- internal/scrapertest/validation.go
+++ internal/scrapertest/validation.go
@@ -1,3 +1,17 @@
+// Copyright  The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package scrapertest
 
 import (
@@ -6,11 +20,11 @@ import (
 	"io/ioutil"
 	"testing"
 
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/scraperhelper"
+
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/model/otlp"
 	"go.opentelemetry.io/collector/model/pdata"
-
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/scraperhelper"
 )
 
 // ValidateScraper validates That a scraper extracts metrics correctly.
