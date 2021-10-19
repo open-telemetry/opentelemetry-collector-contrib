de841b977d089c5d147260e5b706dd8eb32d7284
ydrozhdzhal <ydrozhdzhal@lohika.com>
2021-10-19 16:07:52 +0300

    Added Google Cloud Spanner receiver to components. (#5830)
    Final part - added Google Cloud Spanner receiver to components.

--- internal/components/receivers_test.go
+++ internal/components/receivers_test.go
@@ -76,7 +76,7 @@ func TestDefaultReceivers(t *testing.T) {
 		},
 	}
 
-	assert.Equal(t, len(tests)+29 /* not tested */, len(rcvrFactories))
+	assert.Equal(t, len(tests)+30 /* not tested */, len(rcvrFactories))
 	for _, tt := range tests {
 		t.Run(string(tt.receiver), func(t *testing.T) {
 			factory, ok := rcvrFactories[tt.receiver]
