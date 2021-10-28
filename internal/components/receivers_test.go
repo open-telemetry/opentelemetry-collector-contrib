a729e1339b46b9875af26edca2b37a9bd840591b
Ateeqi <ateeqi@amazon.com>
2021-10-28 21:12:55 +0200

    edit explanation

--- internal/components/receivers_test.go
+++ internal/components/receivers_test.go
@@ -76,7 +76,7 @@ func TestDefaultReceivers(t *testing.T) {
 		},
 		{
 			receiver: "awscontainerinsightreceiver",
-			// TODO: skipped since it will only function in a container environment with procfs in expected location,
+			// TODO: skipped since it will only function in a container environment with procfs in expected location.
 			skipLifecyle: true,
 		},
 	}
