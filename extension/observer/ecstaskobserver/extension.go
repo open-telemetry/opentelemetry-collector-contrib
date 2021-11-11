47cdd661c541102c05d6b590305a1e98d916e2fa
Ryan Fitzpatrick <rmfitzpatrick@users.noreply.github.com>
2021-11-11 12:12:20 -0500

    ecs_task_observer: initial structure (#6121)
    Adding a feature - These changes introduce a new receiver creator-compatible watch observer for ECS tasks.  It differs from the existing ECS observer in that it supports only sidecar deployments and doesn't have a hard dependency on the Prometheus receiver, instead being able to sync with a receiver creator instance by listing [container endpoints](https://github.com/open-telemetry/opentelemetry-collector-contrib/blob/957eb18037f000f5740c4044fdf5ffeee4311ebc/extension/observer/endpoints.go#L166).  This approach was landed on after reviewing the existing and ECS observer config and implementation and from following the guidance of the initial observer's author: https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/5316#issuecomment-925545114.  It does not resolve [the outstanding item](https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/1395#issuecomment-747715986) for #1395, as I think observer endpoint creation should be added to the existing observer as well but should be done in unrelated changes.
    
    **Link to tracking Issue:**
    https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/5316
    
    **Testing:**
    ~No tests as this time since the changes just include the initial types and readme.~ Basic factory test to help ensure receiver creator type compatibility.*
    
    **Documentation:**
    Adding a readme with high level plan and config documentation generated by project configschema tool.

---
+++ extension/observer/ecstaskobserver/extension.go
@@ -0,0 +1,35 @@
+// Copyright  The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ecstaskobserver
+
+import (
+	"go.opentelemetry.io/collector/component"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/observer"
+)
+
+var _ component.Extension = (*ecsTaskObserver)(nil)
+var _ observer.EndpointsLister = (*ecsTaskObserver)(nil)
+
+type ecsTaskObserver struct {
+	component.Extension
+	config           *Config
+	endpointsWatcher *observer.EndpointsWatcher
+	telemetry        component.TelemetrySettings
+}
+
+// ListEndpoints is invoked by an observer.EndpointsWatcher helper to report task container endpoints.
+// It's required to implement observer.EndpointsLister
+func (e *ecsTaskObserver) ListEndpoints() []observer.Endpoint { return nil }
