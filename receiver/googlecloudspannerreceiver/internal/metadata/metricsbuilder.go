8a57a0f699e403537af64fb81be0ef1d59205d30
ydrozhdzhal <ydrozhdzhal@lohika.com>
2021-11-12 17:21:08 +0200

    Added metrics cardinality handling for Google Cloud Spanner receiver - part 3. (#6229)
    Final part to bind all together: algorithm, filter resolver metrics builder, receiver and config.

--- receiver/googlecloudspannerreceiver/internal/metadata/metricsbuilder.go
+++ receiver/googlecloudspannerreceiver/internal/metadata/metricsbuilder.go
@@ -15,60 +15,76 @@
 package metadata
 
 import (
-	"sort"
-
 	"go.opentelemetry.io/collector/model/pdata"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/googlecloudspannerreceiver/internal/filter"
 )
 
 const instrumentationLibraryName = "otelcol/googlecloudspannermetrics"
 
-type MetricsBuilder struct {
-	// TODO It is empty now but there will be fields soon
+type MetricsBuilder interface {
+	Build(dataPoints []*MetricsDataPoint) (pdata.Metrics, error)
+	Shutdown() error
 }
 
-func (b *MetricsBuilder) Build(dataPoints []*MetricsDataPoint) pdata.Metrics {
+type metricsFromDataPointBuilder struct {
+	filterResolver filter.ItemFilterResolver
+}
+
+func NewMetricsFromDataPointBuilder(filterResolver filter.ItemFilterResolver) MetricsBuilder {
+	return &metricsFromDataPointBuilder{
+		filterResolver: filterResolver,
+	}
+}
+
+func (b *metricsFromDataPointBuilder) Shutdown() error {
+	return b.filterResolver.Shutdown()
+}
+
+func (b *metricsFromDataPointBuilder) Build(dataPoints []*MetricsDataPoint) (pdata.Metrics, error) {
 	var metrics pdata.Metrics
 
-	groupedDataPoints := group(dataPoints)
+	groupedDataPoints, err := b.groupAndFilter(dataPoints)
+	if err != nil {
+		return pdata.Metrics{}, err
+	}
 
-	if len(groupedDataPoints) > 0 {
-		metrics = pdata.NewMetrics()
-		rms := metrics.ResourceMetrics()
-		rm := rms.AppendEmpty()
+	metrics = pdata.NewMetrics()
+	rms := metrics.ResourceMetrics()
+	rm := rms.AppendEmpty()
 
-		ilms := rm.InstrumentationLibraryMetrics()
-		ilm := ilms.AppendEmpty()
-		ilm.InstrumentationLibrary().SetName(instrumentationLibraryName)
+	ilms := rm.InstrumentationLibraryMetrics()
+	ilm := ilms.AppendEmpty()
+	ilm.InstrumentationLibrary().SetName(instrumentationLibraryName)
 
-		for key, points := range groupedDataPoints {
-			metric := ilm.Metrics().AppendEmpty()
-			metric.SetName(key.MetricName)
-			metric.SetUnit(key.MetricUnit)
-			metric.SetDataType(key.MetricDataType.MetricDataType())
+	for key, points := range groupedDataPoints {
+		metric := ilm.Metrics().AppendEmpty()
+		metric.SetName(key.MetricName)
+		metric.SetUnit(key.MetricUnit)
+		metric.SetDataType(key.MetricDataType.MetricDataType())
 
-			var dataPointSlice pdata.NumberDataPointSlice
+		var dataPointSlice pdata.NumberDataPointSlice
 
-			switch key.MetricDataType.MetricDataType() {
-			case pdata.MetricDataTypeGauge:
-				dataPointSlice = metric.Gauge().DataPoints()
-			case pdata.MetricDataTypeSum:
-				metric.Sum().SetAggregationTemporality(key.MetricDataType.AggregationTemporality())
-				metric.Sum().SetIsMonotonic(key.MetricDataType.IsMonotonic())
-				dataPointSlice = metric.Sum().DataPoints()
-			}
+		switch key.MetricDataType.MetricDataType() {
+		case pdata.MetricDataTypeGauge:
+			dataPointSlice = metric.Gauge().DataPoints()
+		case pdata.MetricDataTypeSum:
+			metric.Sum().SetAggregationTemporality(key.MetricDataType.AggregationTemporality())
+			metric.Sum().SetIsMonotonic(key.MetricDataType.IsMonotonic())
+			dataPointSlice = metric.Sum().DataPoints()
+		}
 
-			for _, point := range points {
-				point.CopyTo(dataPointSlice.AppendEmpty())
-			}
+		for _, point := range points {
+			point.CopyTo(dataPointSlice.AppendEmpty())
 		}
 	}
 
-	return metrics
+	return metrics, nil
 }
 
-func group(dataPoints []*MetricsDataPoint) map[MetricsDataPointKey][]*MetricsDataPoint {
+func (b *metricsFromDataPointBuilder) groupAndFilter(dataPoints []*MetricsDataPoint) (map[MetricsDataPointKey][]*MetricsDataPoint, error) {
 	if len(dataPoints) == 0 {
-		return nil
+		return nil, nil
 	}
 
 	groupedDataPoints := make(map[MetricsDataPointKey][]*MetricsDataPoint)
@@ -78,15 +94,57 @@ func group(dataPoints []*MetricsDataPoint) map[MetricsDataPointKey][]*MetricsDat
 		groupedDataPoints[groupingKey] = append(groupedDataPoints[groupingKey], dataPoint)
 	}
 
-	// TODO Apply cardinality filtering somewhere here for each group of dataPoints and rename method to groupAndFilter()
+	// Cardinality filtering + sorting points by metric name and timestamp
+	for groupingKey, points := range groupedDataPoints {
+		filteredPoints, err := b.filter(groupingKey.MetricName, points)
+		if err != nil {
+			return nil, err
+		}
 
-	// Sorting points by metric name and timestamp
-	for _, points := range groupedDataPoints {
-		sort.Slice(points, func(i, j int) bool {
-			return points[i].timestamp.Before(points[j].timestamp)
-		})
-
+		groupedDataPoints[groupingKey] = filteredPoints
 	}
 
-	return groupedDataPoints
+	return groupedDataPoints, nil
+}
+
+func (b *metricsFromDataPointBuilder) filter(metricName string, dataPoints []*MetricsDataPoint) ([]*MetricsDataPoint, error) {
+	itemFilter, err := b.filterResolver.Resolve(metricName)
+	if err != nil {
+		return nil, err
+	}
+
+	itemsForFiltering := make([]*filter.Item, len(dataPoints))
+
+	for i, dataPoint := range dataPoints {
+		itemsForFiltering[i], err = dataPoint.ToItem()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	filteredItems, err := itemFilter.Filter(itemsForFiltering)
+	if err != nil {
+		return nil, err
+	}
+
+	// Creating new slice instead of removing elements from source slice because removing by value is not efficient operation.
+	// Need to use such approach for preserving data points order.
+	filteredItemsSet := make(map[filter.Item]struct{})
+
+	for _, filteredItem := range filteredItems {
+		filteredItemsSet[*filteredItem] = struct{}{}
+	}
+
+	filteredDataPoints := make([]*MetricsDataPoint, len(filteredItems))
+	nextFilteredDataPointIndex := 0
+	for i, dataPointItem := range itemsForFiltering {
+		_, exists := filteredItemsSet[*dataPointItem]
+
+		if exists {
+			filteredDataPoints[nextFilteredDataPointIndex] = dataPoints[i]
+			nextFilteredDataPointIndex++
+		}
+	}
+
+	return filteredDataPoints, nil
 }
