0eb9b40689120ac53b0c46ef81493831a86ff1dc
Juraci Paixão Kröhling <juraci@kroehling.de>
2021-11-12 15:57:13 +0100

    New client.Info usage
    Fixes open-telemetry/opentelemetry-collector/issues/4420
    
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- receiver/jaegerreceiver/trace_receiver_test.go
+++ receiver/jaegerreceiver/trace_receiver_test.go
@@ -36,7 +36,6 @@ import (
 	jaegerthrift "github.com/jaegertracing/jaeger/thrift-gen/jaeger"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"go.opentelemetry.io/collector/client"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/component/componenttest"
 	"go.opentelemetry.io/collector/config"
@@ -47,7 +46,6 @@ import (
 	"go.opentelemetry.io/collector/consumer/consumertest"
 	"go.opentelemetry.io/collector/model/pdata"
 	conventions "go.opentelemetry.io/collector/model/semconv/v1.5.0"
-	"go.opentelemetry.io/collector/obsreport"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
@@ -99,39 +97,6 @@ func TestThriftHTTPBodyDecode(t *testing.T) {
 	gotBatch, hErr := jr.decodeThriftHTTPBody(r)
 	require.Nil(t, hErr, "failed to decode http body")
 	assert.Equal(t, batch, gotBatch)
-}
-
-func TestClientIPDetection(t *testing.T) {
-	ch := make(chan context.Context)
-	jr := jReceiver{
-		nextConsumer: traceConsumer{
-			func(ctx context.Context, _ pdata.Traces) {
-				ch <- ctx
-			},
-		},
-		grpcObsrecv: obsreport.NewReceiver(obsreport.ReceiverSettings{ReceiverCreateSettings: componenttest.NewNopReceiverCreateSettings()}),
-		httpObsrecv: obsreport.NewReceiver(obsreport.ReceiverSettings{ReceiverCreateSettings: componenttest.NewNopReceiverCreateSettings()}),
-	}
-	batch := &jaegerthrift.Batch{
-		Process: jaegerthrift.NewProcess(),
-		Spans:   []*jaegerthrift.Span{jaegerthrift.NewSpan()},
-	}
-	r, err := jaegerBatchToHTTPBody(batch)
-	require.NoError(t, err)
-
-	wantClient, ok := client.FromHTTP(r)
-	assert.True(t, ok)
-	jr.HandleThriftHTTPBatch(httptest.NewRecorder(), r)
-
-	select {
-	case ctx := <-ch:
-		gotClient, ok := client.FromContext(ctx)
-		assert.True(t, ok, "must get client back from context")
-		assert.Equal(t, wantClient, gotClient)
-		break
-	case <-time.After(time.Second * 2):
-		t.Error("next consumer did not receive the batch")
-	}
 }
 
 func TestReception(t *testing.T) {
