31fb3d03bdfd385d974003a55ebc84a7e306ecdc
David Poncelow <dponcelow@splunk.com>
2021-11-16 12:16:25 -0800

    Mongodbatlas add metrics (#5921)
    * poll should return an error, not log
    
    * changes to work with scraperhelper
    
    * rework metrics again (metadata.yaml doesn't match)
    
    * align metrics in metadata.yaml with name map
    
    * start using multierror
    
    * fix error handling with scraper, align metrics
    
    * break up poll function
    
    * remove sfx annotation comments
    
    * add mongodbatlas prefix to metric names
    
    * rework metric units and labels
    
    * split avg and max metrics
    
    Also reworded extended_documentation field to clarify intent
    
    * change two metrics to sum from gauge
    
    * rerun go generate

---
+++ receiver/mongodbatlasreceiver/internal/metric_conversion.go
@@ -0,0 +1,51 @@
+// Copyright  OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package internal
+
+import (
+	"errors"
+
+	"github.com/hashicorp/go-multierror"
+	"go.mongodb.org/atlas/mongodbatlas"
+	"go.opentelemetry.io/collector/model/pdata"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/mongodbatlasreceiver/internal/metadata"
+)
+
+func processMeasurements(
+	resource pdata.Resource,
+	measurements []*mongodbatlas.Measurements,
+) (pdata.Metrics, error) {
+	allErrors := make([]error, 0)
+	metricSlice := pdata.NewMetrics()
+	rm := metricSlice.ResourceMetrics().AppendEmpty()
+	resource.CopyTo(rm.Resource())
+	ilms := rm.InstrumentationLibraryMetrics().AppendEmpty()
+	for _, meas := range measurements {
+		metric, err := metadata.MeasurementsToMetric(meas, false)
+		if err != nil {
+			allErrors = append(allErrors, err)
+		} else {
+			if metric != nil {
+				// TODO: still handling skipping metrics, there's got to be better
+				metric.CopyTo(ilms.Metrics().AppendEmpty())
+			}
+		}
+	}
+	if len(allErrors) > 0 {
+		return metricSlice, multierror.Append(errors.New("errors occurred while processing measurements"), allErrors...)
+	}
+	return metricSlice, nil
+}
