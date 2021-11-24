7a1eeb9cbfd3d5eae8ee50361b58866e35fe17f0
Dmitrii Anoshin <anoshindx@gmail.com>
2021-11-24 02:54:19 -0800

    Run "make goporto" (#6423)

--- processor/redactionprocessor/config.go
+++ processor/redactionprocessor/config.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package redactionprocessor
+package redactionprocessor // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/redactionprocessor"
 
 import (
 	"go.opentelemetry.io/collector/config"
