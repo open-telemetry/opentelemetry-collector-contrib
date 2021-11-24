5e0e78e5e38681cb91347c29e5b0a72431a543c7
Dmitrii Anoshin <anoshindx@gmail.com>
2021-11-24 02:54:19 -0800

    Run "make goporto" (#6423)

--- extension/oidcauthextension/authdata.go
+++ extension/oidcauthextension/authdata.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package oidcauthextension
+package oidcauthextension // import "github.com/open-telemetry/opentelemetry-collector-contrib/extension/oidcauthextension"
 
 import "go.opentelemetry.io/collector/client"
 
