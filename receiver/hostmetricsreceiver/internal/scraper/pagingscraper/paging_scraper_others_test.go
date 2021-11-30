1e0c2fe0396e1910c43332fd7f3be462bb45f6fa
Thomas Barker <tgb@google.com>
2021-11-30 18:06:11 -0500

    `hostmetrics`: Upgrade to use gopsutil/v3 (#6468)
    This removes the dependency on the older gopsutil library.

--- receiver/hostmetricsreceiver/internal/scraper/pagingscraper/paging_scraper_others_test.go
+++ receiver/hostmetricsreceiver/internal/scraper/pagingscraper/paging_scraper_others_test.go
@@ -22,7 +22,7 @@ import (
 	"errors"
 	"testing"
 
-	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/v3/mem"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component/componenttest"
