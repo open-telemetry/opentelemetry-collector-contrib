1d5c8a89cbbf1f9d719305164ced02f044b34093
Dmitry <anoshindx@gmail.com>
2021-11-30 16:43:39 -0800

    [mdatagen] Extract test data yaml to files

--- cmd/mdatagen/loader_test.go
+++ cmd/mdatagen/loader_test.go
@@ -15,68 +15,10 @@
 package main
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
-)
-
-const (
-	allOptions = `
-name: metricreceiver
-attributes:
-  freeFormAttribute:
-    description: Attribute that can take on any value.
-
-  freeFormAttributeWithValue:
-    value: state
-    description: Attribute that has alternate value set.
-
-  enumAttribute:
-    description: Attribute with a known set of values.
-    enum: [red, green, blue]
-
-metrics:
-  system.cpu.time:
-    description: Total CPU seconds broken down by different states.
-    extended_documentation: Additional information on CPU Time can be found [here](https://en.wikipedia.org/wiki/CPU_time).
-    unit: s
-    sum:
-      monotonic: true
-      aggregation: cumulative
-    attributes: [freeFormAttribute, freeFormAttributeWithValue, enumAttribute]
-`
-
-	unknownMetricAttribute = `
-name: metricreceiver
-metrics:
-  system.cpu.time:
-    description: Total CPU seconds broken down by different states.
-    unit: s
-    sum:
-      monotonic: true
-      aggregation: cumulative
-    attributes: [missing]
-`
-	noMetricType = `
-name: metricreceiver
-metrics:
-  system.cpu.time:
-    description: Total CPU seconds broken down by different states.
-    unit: s
-    attributes:
-`
-	twoMetricTypes = `
-name: metricreceiver
-metrics:
-  system.cpu.time:
-    description: Total CPU seconds broken down by different states.
-    unit: s
-    gauge: {}
-    sum:
-      monotonic: true
-      aggregation: cumulative
-    attributes:
-`
 )
 
 func Test_loadMetadata(t *testing.T) {
@@ -88,7 +30,7 @@ func Test_loadMetadata(t *testing.T) {
 	}{
 		{
 			name: "all options",
-			yml:  allOptions,
+			yml:  "testdata/all_options.yaml",
 			want: metadata{
 				Name: "metricreceiver",
 				Attributes: map[attributeName]attribute{
@@ -118,21 +60,21 @@ func Test_loadMetadata(t *testing.T) {
 		},
 		{
 			name: "unknown metric attribute",
-			yml:  unknownMetricAttribute,
+			yml:  "testdata/unknown_metric_attribute.yaml",
 			want: metadata{},
 			wantErr: "error validating struct:\n\tmetadata.Metrics[system.cpu.time]." +
 				"Attributes[missing]: unknown attribute value\n",
 		},
 		{
 			name: "no metric type",
-			yml:  noMetricType,
+			yml:  "testdata/no_metric_type.yaml",
 			want: metadata{},
 			wantErr: "metric system.cpu.time doesn't have a metric type key, " +
 				"one of the following has to be specified: sum, gauge, histogram",
 		},
 		{
 			name: "two metric types",
-			yml:  twoMetricTypes,
+			yml:  "testdata/two_metric_types.yaml",
 			want: metadata{},
 			wantErr: "metric system.cpu.time has more than one metric type keys, " +
 				"only one of the following has to be specified: sum, gauge, histogram",
@@ -140,7 +82,9 @@ func Test_loadMetadata(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := loadMetadata([]byte(tt.yml))
+			ymlData, err := os.ReadFile(tt.yml)
+			require.NoError(t, err)
+			got, err := loadMetadata(ymlData)
 			if tt.wantErr != "" {
 				require.Error(t, err)
 				require.EqualError(t, err, tt.wantErr)
