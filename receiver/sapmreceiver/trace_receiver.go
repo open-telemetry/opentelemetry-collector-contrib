fc07947d62a93fc671de526dd33c3cafb326f425
Bogdan Drutu <bogdandrutu@gmail.com>
2021-12-03 15:15:42 -0800

    Use errors.Is to determine if an error is a specific type, for http/grpc stop errors (#6516) (#6517)
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- receiver/sapmreceiver/trace_receiver.go
+++ receiver/sapmreceiver/trace_receiver.go
@@ -44,9 +44,10 @@ var gzipWriterPool = &sync.Pool{
 // sapmReceiver receives spans in the Splunk SAPM format over HTTP
 type sapmReceiver struct {
 	settings component.TelemetrySettings
+	config   *Config
 
-	config *Config
-	server *http.Server
+	server     *http.Server
+	shutdownWG sync.WaitGroup
 
 	nextConsumer consumer.Traces
 
@@ -164,8 +165,10 @@ func (sr *sapmReceiver) Start(_ context.Context, host component.Host) error {
 	// create a server with the handler
 	sr.server = sr.config.HTTPServerSettings.ToServer(nr, sr.settings)
 
+	sr.shutdownWG.Add(1)
 	// run the server on a routine
 	go func() {
+		defer sr.shutdownWG.Done()
 		if errHTTP := sr.server.Serve(ln); !errors.Is(errHTTP, http.ErrServerClosed) && errHTTP != nil {
 			host.ReportFatalError(errHTTP)
 		}
@@ -175,7 +178,9 @@ func (sr *sapmReceiver) Start(_ context.Context, host component.Host) error {
 
 // Shutdown stops the the sapmReceiver's server.
 func (sr *sapmReceiver) Shutdown(context.Context) error {
-	return sr.server.Close()
+	err := sr.server.Close()
+	sr.shutdownWG.Wait()
+	return err
 }
 
 // this validates at compile time that sapmReceiver implements the component.TracesReceiver interface
