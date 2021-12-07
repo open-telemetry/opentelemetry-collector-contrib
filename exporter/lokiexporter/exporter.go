b71d1603088439391fa6280b31b7dd50e9bd0299
Juraci Paixão Kröhling <juraci@kroehling.de>
2021-12-07 14:50:56 +0100

    `lokiexporter`: add complete log record to body
    Fixes #6618
    
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- exporter/lokiexporter/exporter.go
+++ exporter/lokiexporter/exporter.go
@@ -51,9 +51,9 @@ func newExporter(config *Config, logger *zap.Logger) *lokiExporter {
 		logger: logger,
 	}
 	if config.Format == "json" {
-		lokiexporter.convert = convertLogToJSONEntry
+		lokiexporter.convert = lokiexporter.convertLogToJSONEntry
 	} else {
-		lokiexporter.convert = convertLogBodyToEntry
+		lokiexporter.convert = lokiexporter.convertLogBodyToEntry
 	}
 	return lokiexporter
 }
@@ -227,14 +227,50 @@ func (l *lokiExporter) convertAttributesToLabels(attributes pdata.AttributeMap, 
 	return ls
 }
 
-func convertLogBodyToEntry(lr pdata.LogRecord, res pdata.Resource) (*logproto.Entry, error) {
+func (l *lokiExporter) convertLogBodyToEntry(lr pdata.LogRecord, res pdata.Resource) (*logproto.Entry, error) {
+	body := lr.Body().StringVal()
+
+	if len(lr.Name()) > 0 {
+		body = fmt.Sprintf("name=%s %s", lr.Name(), body)
+	}
+	if len(lr.SeverityText()) > 0 {
+		body = fmt.Sprintf("severity=%s %s", lr.SeverityText(), body)
+	}
+	if lr.SeverityNumber() > 0 {
+		body = fmt.Sprintf("severityN=%d %s", lr.SeverityNumber(), body)
+	}
+	if !lr.TraceID().IsEmpty() {
+		body = fmt.Sprintf("traceID=%s %s", lr.TraceID().HexString(), body)
+	}
+	if !lr.SpanID().IsEmpty() {
+		body = fmt.Sprintf("spanID=%s %s", lr.SpanID().HexString(), body)
+	}
+
+	// fields not added to the accept-list as part of the component's config
+	// are added to the body, so that they can still be seen under "detected fields"
+	lr.Attributes().Range(func(k string, v pdata.AttributeValue) bool {
+		if _, found := l.config.Labels.Attributes[k]; !found {
+			body = fmt.Sprintf("%s=%s %s", k, v.AsString(), body)
+		}
+		return true
+	})
+
+	// same for resources: include all, except the ones that are explicitly added
+	// as part of the config, which are showing up at the top-level already
+	res.Attributes().Range(func(k string, v pdata.AttributeValue) bool {
+		if _, found := l.config.Labels.ResourceAttributes[k]; !found {
+			body = fmt.Sprintf("%s=%s %s", k, v.AsString(), body)
+		}
+		return true
+	})
+
 	return &logproto.Entry{
 		Timestamp: time.Unix(0, int64(lr.Timestamp())),
-		Line:      lr.Body().StringVal(),
+		Line:      body,
 	}, nil
 }
 
-func convertLogToJSONEntry(lr pdata.LogRecord, res pdata.Resource) (*logproto.Entry, error) {
+func (l *lokiExporter) convertLogToJSONEntry(lr pdata.LogRecord, res pdata.Resource) (*logproto.Entry, error) {
 	line, err := encodeJSON(lr, res)
 	if err != nil {
 		return nil, err
