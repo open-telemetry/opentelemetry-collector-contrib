a450d01b882eb4c3742b7b8cacc5c584ec3669bf
Zhihao Chen <zchen2@atlassian.com>
2021-12-13 10:52:07 +1100

    fix race condition issue

--- processor/spanmetricsprocessor/cache/cache.go
+++ processor/spanmetricsprocessor/cache/cache.go
@@ -14,7 +14,10 @@
 
 package cache // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/spanmetricsprocessor/cache"
 
-import lru "github.com/hashicorp/golang-lru"
+import (
+	lru "github.com/hashicorp/golang-lru"
+	"sync"
+)
 
 // Cache consists of an LRU cache and the evicted items from the LRU cache.
 // This data structure makes sure all the cached items can be retrieved either from the LRU cache or the evictedItems
@@ -24,13 +27,17 @@ import lru "github.com/hashicorp/golang-lru"
 type Cache struct {
 	*lru.Cache
 	evictedItems map[interface{}]interface{}
+	lock sync.RWMutex
 }
 
 // NewCache creates a Cache.
 func NewCache(size int) (*Cache, error) {
 	evictedItems := make(map[interface{}]interface{})
+	var lock sync.RWMutex
 	lruCache, err := lru.NewWithEvict(size, func(key interface{}, value interface{}) {
+		lock.Lock()
 		evictedItems[key] = value
+		lock.Unlock()
 	})
 	if err != nil {
 		return nil, err
@@ -39,12 +46,15 @@ func NewCache(size int) (*Cache, error) {
 	return &Cache{
 		Cache:        lruCache,
 		evictedItems: evictedItems,
+		lock: lock,
 	}, nil
 }
 
 // RemoveEvictedItems cleans all the evicted items.
 func (c *Cache) RemoveEvictedItems() {
+	c.lock.Lock()
 	c.evictedItems = make(map[interface{}]interface{})
+	c.lock.Unlock()
 }
 
 // Get retrieves an item from the LRU cache or evicted items.
@@ -52,6 +62,8 @@ func (c *Cache) Get(key interface{}) (interface{}, bool) {
 	if val, ok := c.Cache.Get(key); ok {
 		return val, ok
 	}
+	c.lock.RLock()
 	val, ok := c.evictedItems[key]
+	c.lock.RUnlock()
 	return val, ok
 }
