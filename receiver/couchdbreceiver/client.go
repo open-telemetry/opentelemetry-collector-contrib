a220e16e88736804b1180aab43fa12b17dc368ab
Jonathan Wamsley <jonathan.wamsley@bluemedora.com>
2021-12-14 15:44:06 -0500

    add couchdb client

---
+++ receiver/couchdbreceiver/client.go
@@ -0,0 +1,125 @@
+package couchdbreceiver
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+
+	"go.opentelemetry.io/collector/component"
+	"go.uber.org/zap"
+)
+
+// CouchDBClient defines the basic HTTP client interface.
+type CouchDBClient interface {
+	BuildReq(path string) (*http.Request, error)
+	Get(url string) ([]byte, error)
+	GetNodeNames() ([]string, error)
+	GetStats(nodeName string) (map[string]interface{}, error)
+}
+
+var _ CouchDBClient = (*couchDBClient)(nil)
+
+type couchDBClient struct {
+	client *http.Client
+	cfg    *Config
+	logger *zap.Logger
+}
+
+// NewCouchDBClient creates a new client to make requests for the CouchDB receiver.
+func NewCouchDBClient(cfg *Config, host component.Host, logger *zap.Logger) (CouchDBClient, error) {
+	client, err := cfg.ToClient(host.GetExtensions())
+	if err != nil {
+		return nil, fmt.Errorf("failed to create HTTP Client: %w", err)
+	}
+
+	return &couchDBClient{
+		client: client,
+		cfg:    cfg,
+		logger: logger,
+	}, nil
+}
+
+// Get issues an authorized Get requests to the specified url.
+func (cc *couchDBClient) Get(path string) ([]byte, error) {
+	req, err := cc.BuildReq(path)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := cc.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		if err = resp.Body.Close(); err != nil {
+			cc.logger.Warn("failed to close response body", zap.Error(err))
+		}
+	}()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body %w", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		if resp.StatusCode >= 400 {
+			cc.logger.Error("couchdb", zap.Error(err), zap.String("status_code", strconv.Itoa(resp.StatusCode)))
+		}
+		return nil, fmt.Errorf("request GET %s failed - %q", req.URL.String(), resp.Status)
+	}
+
+	return body, nil
+}
+
+// Nodes contains a list of all known connected node names.
+type Nodes struct {
+	AllNodes []string `json:"all_nodes"`
+}
+
+// GetNodeNames gets all known connected nodes names.
+func (cc *couchDBClient) GetNodeNames() ([]string, error) {
+	path := "/_membership"
+	body, err := cc.Get(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var nodes Nodes
+	err = json.Unmarshal(body, &nodes)
+	if err != nil {
+		return nil, err
+	}
+
+	return nodes.AllNodes, nil
+}
+
+// GetStats gets couchdb stats at a specific node name endpoint.
+func (cc *couchDBClient) GetStats(nodeName string) (map[string]interface{}, error) {
+	path := fmt.Sprintf("/_node/%s/_stats/couchdb", nodeName)
+	body, err := cc.Get(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var stats map[string]interface{}
+	err = json.Unmarshal(body, &stats)
+	if err != nil {
+		return nil, err
+	}
+
+	return stats, nil
+}
+
+func (cc *couchDBClient) BuildReq(path string) (*http.Request, error) {
+	url := cc.cfg.Endpoint + path
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.SetBasicAuth(cc.cfg.Username, cc.cfg.Password)
+	return req, nil
+}
---
+++ receiver/couchdbreceiver/client_test.go
@@ -0,0 +1,262 @@
+package couchdbreceiver
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/component/componenttest"
+	"go.opentelemetry.io/collector/config/confighttp"
+	"go.opentelemetry.io/collector/config/configtls"
+	"go.uber.org/zap"
+)
+
+func defaultConfig(t *testing.T, endpoint string) CouchDBClient {
+	couchdbClient, err := NewCouchDBClient(
+		&Config{
+			Username: "otelu",
+			Password: "otelp",
+			HTTPClientSettings: confighttp.HTTPClientSettings{
+				Endpoint: endpoint,
+			},
+		},
+		componenttest.NewNopHost(),
+		zap.NewNop())
+	require.Nil(t, err)
+	require.NotNil(t, couchdbClient)
+	return couchdbClient
+}
+
+func TestNewCouchDBClient(t *testing.T) {
+	t.Run("Invalid config", func(t *testing.T) {
+		couchdbClient, err := NewCouchDBClient(
+			&Config{
+				HTTPClientSettings: confighttp.HTTPClientSettings{
+					Endpoint: defaultEndpoint,
+					TLSSetting: configtls.TLSClientSetting{
+						TLSSetting: configtls.TLSSetting{
+							CAFile: "/non/existent",
+						},
+					},
+				}},
+			componenttest.NewNopHost(),
+			zap.NewNop())
+
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "failed to create HTTP Client: ")
+		require.Nil(t, couchdbClient)
+	})
+	t.Run("no error", func(t *testing.T) {
+		client, err := NewCouchDBClient(
+			&Config{},
+			componenttest.NewNopHost(),
+			zap.NewNop(),
+		)
+
+		require.NoError(t, err)
+		require.NotNil(t, client)
+	})
+}
+
+func TestGet(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		u, p, _ := r.BasicAuth()
+		if u == "" || p == "" {
+			w.WriteHeader(401)
+			return
+		}
+		if strings.Contains(r.URL.Path, "/_stats/couchdb") {
+			w.WriteHeader(200)
+			return
+		}
+		if strings.Contains(r.URL.Path, "/invalid_endpoint") {
+			w.WriteHeader(404)
+			return
+		}
+		if strings.Contains(r.URL.Path, "/invalid_body") {
+			w.Header().Set("Content-Length", "1")
+			return
+		}
+
+		w.WriteHeader(404)
+	}))
+	defer ts.Close()
+
+	t.Run("invalid url request", func(t *testing.T) {
+		url := ts.URL + " /space"
+		couchdbClient := defaultConfig(t, url)
+
+		result, err := couchdbClient.Get(url)
+		require.NotNil(t, err)
+		require.Nil(t, result)
+		require.Contains(t, err.Error(), "invalid port ")
+	})
+	t.Run("invalid endpoint", func(t *testing.T) {
+		url := ts.URL + "/invalid_endpoint"
+		couchdbClient := defaultConfig(t, url)
+
+		result, err := couchdbClient.Get(url)
+		require.NotNil(t, err)
+		require.Nil(t, result)
+		require.Contains(t, err.Error(), "404 Not Found")
+	})
+	t.Run("invalid body", func(t *testing.T) {
+		url := ts.URL + "/invalid_body"
+		couchdbClient := defaultConfig(t, url)
+
+		result, err := couchdbClient.Get(url)
+		require.NotNil(t, err)
+		require.Nil(t, result)
+		require.Contains(t, err.Error(), "failed to read response body ")
+	})
+	t.Run("401 Unauthorized", func(t *testing.T) {
+		url := ts.URL + "/_node/_local/_stats/couchdb"
+		couchdbClient, err := NewCouchDBClient(
+			&Config{
+				HTTPClientSettings: confighttp.HTTPClientSettings{
+					Endpoint: url,
+				},
+			},
+			componenttest.NewNopHost(),
+			zap.NewNop())
+		require.Nil(t, err)
+		require.NotNil(t, couchdbClient)
+
+		result, err := couchdbClient.Get(url)
+		require.Nil(t, result)
+		require.NotNil(t, err)
+		require.Contains(t, err.Error(), "401 Unauthorized")
+	})
+	t.Run("no error", func(t *testing.T) {
+		url := ts.URL + "/_node/_local/_stats/couchdb"
+		couchdbClient := defaultConfig(t, url)
+
+		result, err := couchdbClient.Get(url)
+		require.Nil(t, err)
+		require.NotNil(t, result)
+	})
+}
+
+func TestGetNodeNamesSingle(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.Contains(r.URL.Path, "/invalid_json") {
+			w.WriteHeader(200)
+			_, _ = w.Write([]byte(`{"}`))
+			return
+		}
+
+		if r.URL.Path == "/_membership" {
+			w.WriteHeader(200)
+			w.Write([]byte(`{"all_nodes":["nonode@nohost"],"cluster_nodes":["nonode@nohost"]}`))
+			return
+		}
+		w.WriteHeader(404)
+	}))
+	defer ts.Close()
+
+	t.Run("invalid endpoint", func(t *testing.T) {
+		couchdbClient := defaultConfig(t, "invalid")
+
+		actualNodeNames, err := couchdbClient.GetNodeNames()
+		require.NotNil(t, err)
+		require.Nil(t, actualNodeNames)
+	})
+	t.Run("invalid json", func(t *testing.T) {
+		couchdbClient := defaultConfig(t, ts.URL+"/invalid_json")
+
+		actualNodeNames, err := couchdbClient.GetNodeNames()
+		require.NotNil(t, err)
+		require.Nil(t, actualNodeNames)
+	})
+	t.Run("no error", func(t *testing.T) {
+		expectedNodeNames := []string{"nonode@nohost"}
+		couchdbClient := defaultConfig(t, ts.URL)
+
+		actualNodeNames, err := couchdbClient.GetNodeNames()
+		require.Nil(t, err)
+		require.EqualValues(t, expectedNodeNames, actualNodeNames)
+	})
+}
+
+func TestGetNodeNamesMultiple(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/_membership" {
+			w.WriteHeader(200)
+			w.Write([]byte(`{"all_nodes":["couchdb@couchdb0.otel.com","couchdb@couchdb1.otel.com","couchdb@couchdb2.otel.com"],"cluster_nodes":["couchdb@couchdb0.otel.com","couchdb@couchdb1.otel.com","couchdb@couchdb2.otel.com"]}`))
+			return
+		}
+		w.WriteHeader(404)
+	}))
+	defer ts.Close()
+
+	expectedNodeNames := []string{"couchdb@couchdb0.otel.com", "couchdb@couchdb1.otel.com", "couchdb@couchdb2.otel.com"}
+	couchdbClient := defaultConfig(t, ts.URL)
+
+	actualNodeNames, err := couchdbClient.GetNodeNames()
+	require.Nil(t, err)
+	require.EqualValues(t, expectedNodeNames, actualNodeNames)
+}
+
+func TestGetNodeStats(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		if strings.Contains(r.URL.Path, "/invalid_json") {
+			w.WriteHeader(200)
+			_, _ = w.Write([]byte(`{"}`))
+			return
+		}
+		if strings.Contains(r.URL.Path, "/_stats/couchdb") {
+			w.WriteHeader(200)
+			w.Write([]byte(`{"key":["value"]}`))
+			return
+		}
+		w.WriteHeader(404)
+	}))
+	defer ts.Close()
+
+	t.Run("invalid endpoint", func(t *testing.T) {
+		couchdbClient := defaultConfig(t, "invalid")
+
+		actualStats, err := couchdbClient.GetStats("_local")
+		require.NotNil(t, err)
+		require.Nil(t, actualStats)
+	})
+	t.Run("invalid json", func(t *testing.T) {
+		couchdbClient := defaultConfig(t, ts.URL+"/invalid_json")
+
+		actualStats, err := couchdbClient.GetStats("_local")
+		require.NotNil(t, err)
+		require.Nil(t, actualStats)
+	})
+	t.Run("no error", func(t *testing.T) {
+		expectedStats := map[string]interface{}{"key": []interface{}{"value"}}
+		couchdbClient := defaultConfig(t, ts.URL)
+
+		actualStats, err := couchdbClient.GetStats("_local")
+		require.Nil(t, err)
+		require.EqualValues(t, expectedStats, actualStats)
+	})
+}
+
+func TestBuildReq(t *testing.T) {
+	endpoint := "http://localhost:5984"
+	path := "/_membership"
+	couchdbClient := defaultConfig(t, endpoint)
+
+	req, err := couchdbClient.BuildReq(path)
+	require.NoError(t, err)
+	require.NotNil(t, req)
+	require.Equal(t, "application/json", req.Header["Content-Type"][0])
+	require.Equal(t, []string{"Basic b3RlbHU6b3RlbHA="}, req.Header["Authorization"])
+	require.Equal(t, endpoint+path, req.URL.String())
+}
+
+func TestBuildBadReq(t *testing.T) {
+	endpoint := "http://localhost:5984"
+	couchdbClient := defaultConfig(t, endpoint)
+
+	_, err := couchdbClient.BuildReq(" ")
+	require.Error(t, err)
+}
