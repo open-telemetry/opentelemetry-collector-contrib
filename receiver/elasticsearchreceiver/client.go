d22803189970a67d5a6d68d64d262fb7bad353df
Brandon Johnson <brandon.johnson@bluemedora.com>
2022-01-05 05:41:26 -0500

    add header to request 7.x compatible response

--- receiver/elasticsearchreceiver/client.go
+++ receiver/elasticsearchreceiver/client.go
@@ -133,6 +133,10 @@ func (c defaultElasticsearchClient) doRequest(ctx context.Context, path string) 
 		req.Header.Add("Authorization", c.authHeader)
 	}
 
+	// See https://www.elastic.co/guide/en/elasticsearch/reference/8.0/api-conventions.html#api-compatibility
+	// the compatible-with=7 should signal to newer version of Elasticsearch to use the v7.x API format
+	req.Header.Add("Accept", "application/vnd.elasticsearch+json; compatible-with=7")
+
 	resp, err := c.client.Do(req)
 	if err != nil {
 		return nil, err
