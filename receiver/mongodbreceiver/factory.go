dfc4be56c1b6213149cca2e18685035e8b78fc6a
schmikei <keith.schmitt@bluemedora.com>
2022-01-07 15:07:19 -0500

    add import comment to github.com/open-telemetry/opentelemetry-collector-contrib/receiver/mongodbreceiver

--- receiver/mongodbreceiver/factory.go
+++ receiver/mongodbreceiver/factory.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package mongodbreceiver
+package mongodbreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/mongodbreceiver"
 
 //go:generate mdatagen metadata.yaml
 
