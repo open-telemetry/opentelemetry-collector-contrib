c5b7fb30e1a2b99219907c2a2f6aa57c08edcc9c
schmikei <keith.schmitt@bluemedora.com>
2022-01-11 09:31:24 -0500

    fix lint err from license

--- receiver/mongodbreceiver/client_test.go
+++ receiver/mongodbreceiver/client_test.go
@@ -1,3 +1,17 @@
+// Copyright  The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package mongodbreceiver
 
 import (
@@ -205,7 +219,7 @@ func TestInitClientReplicaSet(t *testing.T) {
 			logger: zap.NewNop(),
 		}
 
-		ctx, cancel := context.WithCancel(context.Background())
+		ctx, cancel := context.WithTimeout(context.TODO(), 2*time.Minute)
 		defer cancel()
 
 		err := client.ensureClient(ctx)
@@ -224,7 +238,7 @@ func TestDisconnectSuccess(t *testing.T) {
 		logger: zap.NewNop(),
 	}
 	ctx, cancel := context.WithCancel(context.Background())
-	cancel()
+	defer cancel()
 
 	err := client.Disconnect(ctx)
 	require.NoError(t, err)
