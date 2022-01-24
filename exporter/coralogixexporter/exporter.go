80a8fb293f052f9b38534b5b3cdf17d4c2357ec3
ofirshmuel <67951914+ofirshmuel@users.noreply.github.com>
2022-01-24 16:18:22 +0200

    [exporter/coralogix] Initial implementation (#6816)
    * coralogix exporter second pr
    
    * remove env variables and update readme
    
    * fix conflicts
    
    * fix rebase master
    
    * fix config-example-1.yaml

---
+++ exporter/coralogixexporter/exporter.go
@@ -0,0 +1,43 @@
+// Copyright 2021, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package coralogixexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/coralogixexporter"
+
+import (
+	"context"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/model/pdata"
+	"go.uber.org/zap"
+)
+
+// CoralogixExporter by Coralogix
+type coralogixExporter struct {
+	cfg    Config
+	logger *zap.Logger
+	client coralogixClient
+}
+
+// NewCoralogixExporter by Coralogix
+func newCoralogixExporter(cfg *Config, params component.ExporterCreateSettings) *coralogixExporter {
+	return &coralogixExporter{
+		cfg:    *cfg,
+		logger: params.Logger,
+		client: *newCoralogixClient(cfg, params),
+	}
+}
+
+func (cx *coralogixExporter) tracesPusher(ctx context.Context, td pdata.Traces) error {
+	return cx.client.newPost(ctx, td)
+}
