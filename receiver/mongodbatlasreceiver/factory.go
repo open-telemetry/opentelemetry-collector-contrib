165ecded6dcd6578e5822b2795f4bfb2b8c7535d
David Poncelow <dponcelow@splunk.com>
2022-01-25 05:13:14 -0800

    [receiver/mongodbatlas] Add retry and backoff (#6943)
    * add retry and backoff to MongoDB Atlas receiver
    
    This receiver can run into throttling when monitoring a large
    cluster.
    
    * Add changelog entry.
    
    * use time.Duration for retry interval
    
    * add jitter to MongoDB Atlas retry
    
    * lint fixes
    
    * change retry configuration to match exporterhelper
    
    * register shutdown func to cancel backoff
    
    * lint fix

--- receiver/mongodbatlasreceiver/factory.go
+++ receiver/mongodbatlasreceiver/factory.go
@@ -21,6 +21,7 @@ import (
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/config"
 	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/exporter/exporterhelper"
 	"go.opentelemetry.io/collector/receiver/receiverhelper"
 	"go.opentelemetry.io/collector/receiver/scraperhelper"
 )
@@ -57,5 +58,6 @@ func createDefaultConfig() config.Receiver {
 	return &Config{
 		ScraperControllerSettings: scraperhelper.DefaultScraperControllerSettings(typeStr),
 		Granularity:               defaultGranularity,
+		RetrySettings:             exporterhelper.DefaultRetrySettings(),
 	}
 }
