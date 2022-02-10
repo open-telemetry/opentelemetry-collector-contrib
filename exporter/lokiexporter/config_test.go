c8215a4c7b2f7a4206f0ded8e6e106baad111026
Juraci Paixão Kröhling <juraci@kroehling.de>
2022-02-10 03:01:11 +0100

    [exporter/loki] Use record attributes as log labels (#7569)
    * [exporter/loki] Use record attributes as log labels
    
    Fixes #6001
    
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>
    
    * Fix statement that unknown keys are ignored
    
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>
    
    Co-authored-by: Anthony Mirabella <a9@aneurysm9.com>

--- exporter/lokiexporter/config_test.go
+++ exporter/lokiexporter/config_test.go
@@ -86,6 +86,9 @@ func TestLoadConfig(t *testing.T) {
 				"resource.name": "resource_name",
 				"severity":      "severity",
 			},
+			RecordAttributes: map[string]string{
+				"traceID": "traceid",
+			},
 		},
 		Format: "body",
 	}
@@ -202,7 +205,7 @@ func TestConfig_validate(t *testing.T) {
 					ResourceAttributes: nil,
 				},
 			},
-			errorMessage: "\"labels.attributes\" or \"labels.resource\" must be configured with at least one attribute",
+			errorMessage: "\"labels.attributes\", \"labels.resource\", or \"labels.record\" must be configured with at least one attribute",
 			shouldError:  true,
 		},
 		{
@@ -220,6 +223,33 @@ func TestConfig_validate(t *testing.T) {
 				Labels:   validAttribLabelsConfig,
 			},
 			shouldError: false,
+		},
+		{
+			name: "with valid `labels.record`",
+			fields: fields{
+				Endpoint: validEndpoint,
+				Labels: LabelsConfig{
+					RecordAttributes: map[string]string{
+						"traceID":   "traceID",
+						"spanID":    "spanID",
+						"severity":  "severity",
+						"severityN": "severityN",
+					},
+				},
+			},
+			shouldError: false,
+		},
+		{
+			name: "with invalid `labels.record`",
+			fields: fields{
+				Endpoint: validEndpoint,
+				Labels: LabelsConfig{
+					RecordAttributes: map[string]string{
+						"invalid": "Invalid",
+					},
+				},
+			},
+			shouldError: true,
 		},
 	}
 
@@ -260,7 +290,7 @@ func TestLabelsConfig_validate(t *testing.T) {
 				Attributes:         map[string]string{},
 				ResourceAttributes: map[string]string{},
 			},
-			errorMessage: "\"labels.attributes\" or \"labels.resource\" must be configured with at least one attribute",
+			errorMessage: "\"labels.attributes\", \"labels.resource\", or \"labels.record\" must be configured with at least one attribute",
 			shouldError:  true,
 		},
 		{
