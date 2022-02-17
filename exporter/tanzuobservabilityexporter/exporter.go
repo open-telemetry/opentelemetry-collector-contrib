dfb32f4bfde4a982e292b3a3926fb3abaccfd1e4
Sumit Deo <93740684+deosu@users.noreply.github.com>
2022-02-17 07:40:49 -0800

    [exporter/tanzuobservability] Add internal SDK metric tag (#7826)
    * Add internal SDK metrics
    
    Update TraceExporter and MetricExporter with a tag providing information
    about otel-collector version.
    
    * Update the changelog
    
    Add internal SDK metric tag.
    
    * Update InternalMetricsTags name
    
    Update InternalMetricsTag names to differentiate between metrics and traces exporter usage.
    
    * Update go.mod files
    
    Update go.mod & go.sum
    
    * Fix unit tests
    
    Update metrics_exporter_test.go
    
    * Fix go.sum
    
    Update go.sum after merge.

--- exporter/tanzuobservabilityexporter/exporter.go
+++ exporter/tanzuobservabilityexporter/exporter.go
@@ -23,6 +23,7 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/wavefronthq/wavefront-sdk-go/senders"
+	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/config"
 	"go.opentelemetry.io/collector/model/pdata"
 	"go.uber.org/multierr"
@@ -56,7 +57,7 @@ type tracesExporter struct {
 	logger *zap.Logger
 }
 
-func newTracesExporter(l *zap.Logger, c config.Exporter) (*tracesExporter, error) {
+func newTracesExporter(settings component.ExporterCreateSettings, c config.Exporter) (*tracesExporter, error) {
 	cfg, ok := c.(*Config)
 	if !ok {
 		return nil, fmt.Errorf("invalid config: %#v", c)
@@ -79,6 +80,7 @@ func newTracesExporter(l *zap.Logger, c config.Exporter) (*tracesExporter, error
 		MetricsPort:          2878,
 		TracingPort:          tracingPort,
 		FlushIntervalSeconds: 1,
+		SDKMetricsTags:       map[string]string{"otel.traces.collector_version": settings.BuildInfo.Version},
 	})
 	if err != nil {
 		return nil, fmt.Errorf("failed to create proxy sender: %v", err)
@@ -87,7 +89,7 @@ func newTracesExporter(l *zap.Logger, c config.Exporter) (*tracesExporter, error
 	return &tracesExporter{
 		cfg:    cfg,
 		sender: s,
-		logger: l,
+		logger: settings.Logger,
 	}, nil
 }
 
