780883b59b86894a5099e43624f8dd8dd0032414
Eugene <eugene.fedor@gmail.com>
2022-02-28 19:00:54 -0800

    linting

--- exporter/azureblobexporter/logexporter.go
+++ exporter/azureblobexporter/logexporter.go
@@ -22,7 +22,6 @@ import (
 	"go.opentelemetry.io/collector/exporter/exporterhelper"
 	"go.opentelemetry.io/collector/model/otlp"
 	"go.opentelemetry.io/collector/model/pdata"
-
 	"go.uber.org/zap"
 )
 
