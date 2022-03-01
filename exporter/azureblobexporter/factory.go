5cd9e891655cd13f5f903bddeede322d643af16a
Eugene <eugene.fedor@gmail.com>
2022-02-28 17:19:19 -0800

    change log

--- exporter/azureblobexporter/factory.go
+++ exporter/azureblobexporter/factory.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package azureblobexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/azuremonitorexporter"
+package azureblobexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/azureblobexporter"
 
 import (
 	"context"
