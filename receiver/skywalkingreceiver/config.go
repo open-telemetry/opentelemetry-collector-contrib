953fae3a84d6cdca5e564377189041acaddc06be
Jared Tan <jian.tan@daocloud.io>
2022-03-02 19:36:10 +0800

    [receiver/skywalking]  Create skywalking component folder/structure (#8107)
    * remove usage of Deprecated LogRecord.Name field.
    
    * first PR for skywaling/tracing receiver
    
    Signed-off-by: jian.tan <jian.tan@daocloud.io>
    
    * add change log
    
    Signed-off-by: jian.tan <jian.tan@daocloud.io>
    
    * fix go mod
    
    * fix go mod
    
    Signed-off-by: jian.tan <jian.tan@daocloud.io>
    
    * fix goimports order
    
    Signed-off-by: jian.tan <jian.tan@daocloud.io>
    
    * fix build
    
    * fix build
    
    * add sum
    
    Signed-off-by: jian.tan <jian.tan@daocloud.io>
    
    * polish
    
    Signed-off-by: jian.tan <jian.tan@daocloud.io>
    
    * update go sum
    
    Signed-off-by: jian.tan <jian.tan@daocloud.io>
    
    * polish go mod.
    
    Signed-off-by: jian.tan <jian.tan@daocloud.io>
    
    * make goporto
    
    Signed-off-by: jian.tan <jian.tan@daocloud.io>
    
    * polish
    
    Signed-off-by: jian.tan <jian.tan@daocloud.io>
    
    * polish
    
    Signed-off-by: jian.tan <jian.tan@daocloud.io>
    
    * fix lint
    
    Signed-off-by: jian.tan <jian.tan@daocloud.io>
    
    * Update factory.go
    
    * fix lint
    
    Signed-off-by: jian.tan <jian.tan@daocloud.io>
    
    * fix fmt
    
    Signed-off-by: jian.tan <jian.tan@daocloud.io>
    
    * fix unit test
    
    Signed-off-by: jian.tan <jian.tan@daocloud.io>
    
    * fix typo
    
    Signed-off-by: jian.tan <jian.tan@daocloud.io>
    
    * Update receiver/skywalkingreceiver/config.go
    
    Co-authored-by: Przemek Maciolek <58699843+pmm-sumo@users.noreply.github.com>
    
    * Update receiver/skywalkingreceiver/trace_receiver.go
    
    Co-authored-by: Przemek Maciolek <58699843+pmm-sumo@users.noreply.github.com>
    
    * Update receiver/skywalkingreceiver/trace_receiver.go
    
    Co-authored-by: Przemek Maciolek <58699843+pmm-sumo@users.noreply.github.com>
    
    * Update receiver/skywalkingreceiver/factory_test.go
    
    Co-authored-by: Przemek Maciolek <58699843+pmm-sumo@users.noreply.github.com>
    
    * Update config.yaml
    
    * Update receiver/skywalkingreceiver/trace_receiver.go
    
    Co-authored-by: Przemek Maciolek <58699843+pmm-sumo@users.noreply.github.com>
    
    * add more UT for trace receiver.
    
    Signed-off-by: jian.tan <jian.tan@daocloud.io>
    
    * fix fmt and lint
    
    Signed-off-by: jian.tan <jian.tan@daocloud.io>
    
    * add version.
    
    Signed-off-by: jian.tan <jian.tan@daocloud.io>
    
    * add com
    
    Signed-off-by: jian.tan <jian.tan@daocloud.io>
    
    * fix com
    
    Signed-off-by: jian.tan <jian.tan@daocloud.io>
    
    Co-authored-by: Przemek Maciolek <58699843+pmm-sumo@users.noreply.github.com>

---
+++ receiver/skywalkingreceiver/config.go
@@ -0,0 +1,94 @@
+// Copyright  OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package skywalkingreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/skywalkingreceiver"
+
+import (
+	"fmt"
+
+	"go.opentelemetry.io/collector/config"
+	"go.opentelemetry.io/collector/config/configgrpc"
+	"go.opentelemetry.io/collector/config/confighttp"
+)
+
+const (
+	// The config field id to load the protocol map from
+	protocolsFieldName = "protocols"
+)
+
+// Protocols is the configuration for the supported protocols.
+type Protocols struct {
+	GRPC *configgrpc.GRPCServerSettings `mapstructure:"grpc"`
+	HTTP *confighttp.HTTPServerSettings `mapstructure:"http"`
+}
+
+// Config defines configuration for skywalking receiver.
+type Config struct {
+	config.ReceiverSettings `mapstructure:",squash"` // squash ensures fields are correctly decoded in embedded struct
+	Protocols               `mapstructure:"protocols"`
+}
+
+var _ config.Receiver = (*Config)(nil)
+var _ config.Unmarshallable = (*Config)(nil)
+
+// Validate checks the receiver configuration is valid
+func (cfg *Config) Validate() error {
+	if cfg.GRPC == nil && cfg.HTTP == nil {
+		return fmt.Errorf("must specify at least one protocol when using the Skywalking receiver")
+	}
+
+	if cfg.GRPC != nil {
+		var err error
+		if _, err = extractPortFromEndpoint(cfg.GRPC.NetAddr.Endpoint); err != nil {
+			return fmt.Errorf("unable to extract port for the gRPC endpoint: %w", err)
+		}
+	}
+
+	if cfg.HTTP != nil {
+		if _, err := extractPortFromEndpoint(cfg.HTTP.Endpoint); err != nil {
+			return fmt.Errorf("unable to extract port for the HTTP endpoint: %w", err)
+		}
+	}
+
+	return nil
+}
+
+// Unmarshal a config.Parser into the config struct.
+func (cfg *Config) Unmarshal(componentParser *config.Map) error {
+	if componentParser == nil || len(componentParser.AllKeys()) == 0 {
+		return fmt.Errorf("empty config for Skywalking receiver")
+	}
+
+	// UnmarshalExact will not set struct properties to nil even if no key is provided,
+	// so set the protocol structs to nil where the keys were omitted.
+	err := componentParser.UnmarshalExact(cfg)
+	if err != nil {
+		return err
+	}
+
+	protocols, err := componentParser.Sub(protocolsFieldName)
+	if err != nil {
+		return err
+	}
+
+	if !protocols.IsSet(protoGRPC) {
+		cfg.GRPC = nil
+	}
+
+	if !protocols.IsSet(protoHTTP) {
+		cfg.HTTP = nil
+	}
+
+	return nil
+}
---
+++ receiver/skywalkingreceiver/factory_test.go
@@ -0,0 +1,163 @@
+// Copyright  OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package skywalkingreceiver
+
+import (
+	"context"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/component/componenterror"
+	"go.opentelemetry.io/collector/component/componenttest"
+	"go.opentelemetry.io/collector/config"
+	"go.opentelemetry.io/collector/config/configgrpc"
+	"go.opentelemetry.io/collector/config/confighttp"
+	"go.opentelemetry.io/collector/config/confignet"
+	"go.opentelemetry.io/collector/config/configtest"
+	"go.opentelemetry.io/collector/config/configtls"
+	"go.opentelemetry.io/collector/service/servicetest"
+)
+
+func TestTypeStr(t *testing.T) {
+	factory := NewFactory()
+
+	assert.Equal(t, "skywalking", string(factory.Type()))
+}
+
+func TestCreateDefaultConfig(t *testing.T) {
+	factory := NewFactory()
+	cfg := factory.CreateDefaultConfig()
+	assert.NotNil(t, cfg, "failed to create default config")
+	assert.NoError(t, configtest.CheckConfigStruct(cfg))
+}
+
+func TestCreateReceiver(t *testing.T) {
+	factory := NewFactory()
+	cfg := factory.CreateDefaultConfig()
+	// have to enable at least one protocol for the skywalking receiver to be created
+	cfg.(*Config).Protocols.GRPC = &configgrpc.GRPCServerSettings{
+		NetAddr: confignet.NetAddr{
+			Endpoint:  defaultGRPCBindEndpoint,
+			Transport: "tcp",
+		},
+	}
+	set := componenttest.NewNopReceiverCreateSettings()
+	tReceiver, err := factory.CreateTracesReceiver(context.Background(), set, cfg, nil)
+	assert.NoError(t, err, "receiver creation failed")
+	assert.NotNil(t, tReceiver, "receiver creation failed")
+
+	mReceiver, err := factory.CreateMetricsReceiver(context.Background(), set, cfg, nil)
+	assert.Equal(t, err, componenterror.ErrDataTypeIsNotSupported)
+	assert.Nil(t, mReceiver)
+}
+
+func TestCreateReceiverGeneralConfig(t *testing.T) {
+	factories, err := componenttest.NopFactories()
+	assert.NoError(t, err)
+
+	factory := NewFactory()
+	factories.Receivers[typeStr] = factory
+
+	cfg, err := servicetest.LoadConfigAndValidate(filepath.Join("testdata", "config.yaml"), factories)
+	require.NoError(t, err)
+	require.NotNil(t, cfg)
+
+	rCfg, ok := cfg.Receivers[config.NewComponentIDWithName(typeStr, "customname")]
+	require.True(t, ok)
+
+	set := componenttest.NewNopReceiverCreateSettings()
+	tReceiver, err := factory.CreateTracesReceiver(context.Background(), set, rCfg, nil)
+	assert.NoError(t, err, "receiver creation failed")
+	assert.NotNil(t, tReceiver, "receiver creation failed")
+
+	mReceiver, err := factory.CreateMetricsReceiver(context.Background(), set, rCfg, nil)
+	assert.Equal(t, err, componenterror.ErrDataTypeIsNotSupported)
+	assert.Nil(t, mReceiver)
+}
+
+func TestCreateDefaultGRPCEndpoint(t *testing.T) {
+	factory := NewFactory()
+	cfg := factory.CreateDefaultConfig()
+
+	cfg.(*Config).Protocols.GRPC = &configgrpc.GRPCServerSettings{
+		NetAddr: confignet.NetAddr{
+			Endpoint:  defaultGRPCBindEndpoint,
+			Transport: "tcp",
+		},
+	}
+	set := componenttest.NewNopReceiverCreateSettings()
+	r, err := factory.CreateTracesReceiver(context.Background(), set, cfg, nil)
+
+	assert.NoError(t, err, "unexpected error creating receiver")
+	assert.Equal(t, 11800, r.(*swReceiver).config.CollectorGRPCPort, "grpc port should be default")
+}
+
+func TestCreateTLSGPRCEndpoint(t *testing.T) {
+	factory := NewFactory()
+	cfg := factory.CreateDefaultConfig()
+
+	cfg.(*Config).Protocols.GRPC = &configgrpc.GRPCServerSettings{
+		NetAddr: confignet.NetAddr{
+			Endpoint:  defaultGRPCBindEndpoint,
+			Transport: "tcp",
+		},
+		TLSSetting: &configtls.TLSServerSetting{
+			TLSSetting: configtls.TLSSetting{
+				CertFile: "./testdata/server.crt",
+				KeyFile:  "./testdata/server.key",
+			},
+		},
+	}
+	set := componenttest.NewNopReceiverCreateSettings()
+
+	_, err := factory.CreateTracesReceiver(context.Background(), set, cfg, nil)
+	assert.NoError(t, err, "tls-enabled receiver creation failed")
+}
+
+func TestCreateTLSHTTPEndpoint(t *testing.T) {
+	factory := NewFactory()
+	cfg := factory.CreateDefaultConfig()
+
+	cfg.(*Config).Protocols.HTTP = &confighttp.HTTPServerSettings{
+		Endpoint: defaultHTTPBindEndpoint,
+		TLSSetting: &configtls.TLSServerSetting{
+			TLSSetting: configtls.TLSSetting{
+				CertFile: "./testdata/server.crt",
+				KeyFile:  "./testdata/server.key",
+			},
+		},
+	}
+
+	set := componenttest.NewNopReceiverCreateSettings()
+
+	_, err := factory.CreateTracesReceiver(context.Background(), set, cfg, nil)
+	assert.NoError(t, err, "tls-enabled receiver creation failed")
+}
+
+func TestCreateInvalidHTTPEndpoint(t *testing.T) {
+	factory := NewFactory()
+	cfg := factory.CreateDefaultConfig()
+
+	cfg.(*Config).Protocols.HTTP = &confighttp.HTTPServerSettings{
+		Endpoint: defaultHTTPBindEndpoint,
+	}
+	set := componenttest.NewNopReceiverCreateSettings()
+	r, err := factory.CreateTracesReceiver(context.Background(), set, cfg, nil)
+
+	assert.NoError(t, err, "unexpected error creating receiver")
+	assert.Equal(t, 12800, r.(*swReceiver).config.CollectorHTTPPort, "http port should be default")
+}
