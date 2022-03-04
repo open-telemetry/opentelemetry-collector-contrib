8891e0c2bd89a9427dfab6926cfa845f3e8cdc93
Cedric Ziel <cedric@cedric-ziel.com>
2022-03-05 00:24:24 +0100

    [receiver/dockerstatsreceiver] Add container.runtime attribute to container metrics

--- receiver/dockerstatsreceiver/metrics_test.go
+++ receiver/dockerstatsreceiver/metrics_test.go
@@ -102,6 +102,7 @@ func metricsData(
 
 func defaultLabels() map[string]string {
 	return map[string]string{
+		"container.runtime":    "docker",
 		"container.hostname":   "abcdef012345",
 		"container.id":         "a2596076ca048f02bcd16a8acd12a7ea2d3bc430d1cde095357239dd3925a4c3",
 		"container.image.name": "myImage",
