926b0a545619b5ad387251c9bddbc7395299bf7b
Shree Prakash <shreeprakashagrahari05@gmail.com>
2022-03-13 21:09:29 +0530

    opentelemetry-collector-contrib-8267 Correcting 5m and 15m normalized load

--- receiver/hostmetricsreceiver/internal/scraper/loadscraper/load_scraper.go
+++ receiver/hostmetricsreceiver/internal/scraper/loadscraper/load_scraper.go
@@ -77,8 +77,8 @@ func (s *scraper) scrape(_ context.Context) (pdata.Metrics, error) {
 	if s.config.CPUAverage {
 		divisor := float64(runtime.NumCPU())
 		avgLoadValues.Load1 = avgLoadValues.Load1 / divisor
-		avgLoadValues.Load5 = avgLoadValues.Load1 / divisor
-		avgLoadValues.Load15 = avgLoadValues.Load1 / divisor
+		avgLoadValues.Load5 = avgLoadValues.Load5 / divisor
+		avgLoadValues.Load15 = avgLoadValues.Load15 / divisor
 	}
 
 	metrics.EnsureCapacity(metricsLen)
