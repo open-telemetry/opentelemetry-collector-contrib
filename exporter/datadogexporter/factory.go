3bbf65bbff6b11a33b9cc60180f28f5d019f1cb0
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2022-03-15 14:26:37 +0100

    [exporter/datadog] Deprecate automatic environment variable detection (#8397)
    * [exporter/datadog] Deprecate automatic environment variable detection
    
    * Add changelog entry
    
    * Fix env variables test
    
    I can't really do this with the full struct without introducing public API
    that will have to be deleted later. Since we don't plan to add more environment
    variables, I think this way of doing the test is also okay.
    
    * [exporter/datadog] Add error when unmarshaling to new default fails

--- exporter/datadogexporter/factory.go
+++ exporter/datadogexporter/factory.go
@@ -53,6 +53,7 @@ func defaulttimeoutSettings() exporterhelper.TimeoutSettings {
 }
 
 // createDefaultConfig creates the default exporter configuration
+// TODO (#8396): Remove `os.Getenv` everywhere.
 func createDefaultConfig() config.Exporter {
 	return &ddconfig.Config{
 		ExporterSettings: config.NewExporterSettings(config.NewComponentID(typeStr)),
