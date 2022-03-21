a8d35ba8bb32f99bebd8b47d0e84a6e4ae0f8c1a
Mrod1598 <miguel.rodriguez@observiq.com>
2022-03-21 07:46:45 -0400

    [receiver/windowsperfcountersreceiver] Update how metrics are established (#8376)
    * Add ability to define metrics
    
    * Update tests
    
    * Update Readme
    
    * Add attributes to readme
    
    * Update values for metric config in readme
    
    * Update changelog
    
    * Fix lint
    
    * Fix misspelling
    
    * Force rerun
    
    * Rename keys in Counter config
    
    * Restructure metrics into map
    
    * Remove Value type & Add defaults
    
    * Remove extra test case
    
    * Fix tests
    
    * Add handling for no metrics defined
    
    * Add no metric definition test
    
    * Rework Readme wording
    
    * Add no metrics factory test
    
    * Add no Metrics config test
    
    * Add no metric specified test
    
    * Fix test
    
    * Add More Tests
    
    * Add ignoreValues option
    
    * Fix changelog

--- receiver/windowsperfcountersreceiver/config_test.go
+++ receiver/windowsperfcountersreceiver/config_test.go
@@ -43,9 +43,26 @@ func TestLoadConfig(t *testing.T) {
 
 	r0 := cfg.Receivers[config.NewComponentID(typeStr)]
 	defaultConfigSingleObject := factory.CreateDefaultConfig()
-	defaultConfigSingleObject.(*Config).PerfCounters = []PerfCounterConfig{{Object: "object", Counters: []string{"counter"}}}
+
+	counterConfig := CounterConfig{
+		Name:   "counter1",
+		Metric: "metric",
+	}
+	defaultConfigSingleObject.(*Config).PerfCounters = []PerfCounterConfig{{Object: "object", Counters: []CounterConfig{counterConfig}}}
+	defaultConfigSingleObject.(*Config).MetricMetaData = map[string]MetricConfig{
+		"metric": {
+			Description: "desc",
+			Unit:        "1",
+			Gauge:       GaugeMetric{},
+		},
+	}
 
 	assert.Equal(t, defaultConfigSingleObject, r0)
+
+	counterConfig2 := CounterConfig{
+		Name:   "counter2",
+		Metric: "metric2",
+	}
 
 	r1 := cfg.Receivers[config.NewComponentIDWithName(typeStr, "customname")].(*Config)
 	expectedConfig := &Config{
@@ -56,16 +73,131 @@ func TestLoadConfig(t *testing.T) {
 		PerfCounters: []PerfCounterConfig{
 			{
 				Object:   "object1",
-				Counters: []string{"counter1"},
+				Counters: []CounterConfig{counterConfig},
 			},
 			{
 				Object:   "object2",
-				Counters: []string{"counter1", "counter2"},
+				Counters: []CounterConfig{counterConfig, counterConfig2},
+			},
+		},
+		MetricMetaData: map[string]MetricConfig{
+			"metric": {
+				Description: "desc",
+				Unit:        "1",
+				Gauge:       GaugeMetric{},
+			},
+			"metric2": {
+				Description: "desc",
+				Unit:        "1",
+				Gauge:       GaugeMetric{},
 			},
 		},
 	}
 
 	assert.Equal(t, expectedConfig, r1)
+}
+
+func TestLoadConfigMetrics(t *testing.T) {
+	testCases := []struct {
+		TestName string
+		TestPath string
+		Expected Config
+	}{
+		{
+			TestName: "NoMetricsDefined",
+			TestPath: filepath.Join("testdata", "config-nometrics.yaml"),
+			Expected: Config{
+				PerfCounters: []PerfCounterConfig{
+					{
+						Object:   "object",
+						Counters: []CounterConfig{{Name: "counter1"}},
+					},
+				},
+			},
+		},
+		{
+			TestName: "NoMetricSpecified",
+			TestPath: filepath.Join("testdata", "config-nometricspecified.yaml"),
+			Expected: Config{
+				PerfCounters: []PerfCounterConfig{
+					{
+						Object:   "object",
+						Counters: []CounterConfig{{Name: "counter1"}},
+					},
+				},
+				MetricMetaData: map[string]MetricConfig{
+					"metric": {
+						Description: "desc",
+						Unit:        "1",
+						Gauge:       GaugeMetric{},
+					},
+				},
+			},
+		},
+		{
+			TestName: "SumMetric",
+			TestPath: filepath.Join("testdata", "config-summetric.yaml"),
+			Expected: Config{
+				PerfCounters: []PerfCounterConfig{
+					{
+						Object:   "object",
+						Counters: []CounterConfig{{Name: "counter1", Metric: "metric"}},
+					},
+				},
+				MetricMetaData: map[string]MetricConfig{
+					"metric": {
+						Description: "desc",
+						Unit:        "1",
+						Sum: SumMetric{
+							Aggregation: "cumulative",
+							Monotonic:   false,
+						},
+					},
+				},
+			},
+		},
+		{
+			TestName: "MetricUnspecifiedType",
+			TestPath: filepath.Join("testdata", "config-unspecifiedmetrictype.yaml"),
+			Expected: Config{
+				PerfCounters: []PerfCounterConfig{
+					{
+						Object:   "object",
+						Counters: []CounterConfig{{Name: "counter1", Metric: "metric"}},
+					},
+				},
+				MetricMetaData: map[string]MetricConfig{
+					"metric": {
+						Description: "desc",
+						Unit:        "1",
+						Gauge:       GaugeMetric{},
+					},
+				},
+			},
+		},
+	}
+	for _, test := range testCases {
+		t.Run(test.TestName, func(t *testing.T) {
+			factories, err := componenttest.NopFactories()
+			require.NoError(t, err)
+
+			factory := NewFactory()
+			factories.Receivers[typeStr] = factory
+			cfg, err := servicetest.LoadConfigAndValidate(test.TestPath, factories)
+
+			require.NoError(t, err)
+			require.NotNil(t, cfg)
+
+			assert.Equal(t, len(cfg.Receivers), 1)
+
+			actualReceiver := cfg.Receivers[config.NewComponentID(typeStr)]
+			expectedReceiver := factory.CreateDefaultConfig()
+			expectedReceiver.(*Config).PerfCounters = test.Expected.PerfCounters
+			expectedReceiver.(*Config).MetricMetaData = test.Expected.MetricMetaData
+
+			assert.Equal(t, expectedReceiver, actualReceiver)
+		})
+	}
 }
 
 func TestLoadConfig_Error(t *testing.T) {
@@ -117,8 +249,8 @@ func TestLoadConfig_Error(t *testing.T) {
 				"%s: %s; %s; %s; %s",
 				errorPrefix,
 				negativeCollectionIntervalErr,
-				fmt.Sprintf(emptyInstanceErr, "object"),
 				fmt.Sprintf(noCountersErr, "object"),
+				fmt.Sprintf(emptyInstanceErr, "object"),
 				noObjectNameErr,
 			),
 		},
--- receiver/windowsperfcountersreceiver/go.mod
+++ receiver/windowsperfcountersreceiver/go.mod
@@ -16,11 +16,11 @@ require (
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/knadh/koanf v1.4.0 // indirect
-	github.com/kr/pretty v0.3.0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/mapstructure v1.4.3 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/open-telemetry/opentelemetry-collector-contrib/internal/scrapertest v0.47.0
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
@@ -33,4 +33,9 @@ require (
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
+
+require (
+	github.com/kr/text v0.2.0 // indirect
+	github.com/rogpeppe/go-internal v1.6.1 // indirect
 )
