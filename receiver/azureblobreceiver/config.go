2fa2b964f2dc0263733beb76e5ccc956071c744c
Eugene <eugene.fedor@gmail.com>
2022-03-24 16:18:52 -0700

    linting

--- receiver/azureblobreceiver/config.go
+++ receiver/azureblobreceiver/config.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package azureblobreceiver
+package azureblobreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/azureblobreceiver"
 
 import (
 	"go.opentelemetry.io/collector/config"
