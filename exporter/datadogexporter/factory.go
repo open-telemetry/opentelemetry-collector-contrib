a5c5da9011a2bfa82ee4075d678e830025e966b3
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2022-03-25 16:00:09 +0100

    [exporter/datadog] Add `metrics::sums::cumulative_monotonic_mode` setting; deprecate `metrics::send_monotonic_counter` (#8490)
    * [exporter/datadog] Add `metrics::sums::cumulative_monotonic_mode` setting; deprecate `metrics::send_monotonic_counter`
    
    * Add CHANGELOG entries for this change
    
    * [exporter/datadog] Update documentation
    
    * [exporter/datadog] Add unit tests for renaming error
    
    * Fix formatting
    
    * [exporter/datadog] Minor copyedit on error message
    
    * [exporter/datadog] Fix comment
    
    * Address review comments

--- exporter/datadogexporter/factory.go
+++ exporter/datadogexporter/factory.go
@@ -95,6 +95,9 @@ func (*factory) createDefaultConfig() config.Exporter {
 				Mode:         "distributions",
 				SendCountSum: false,
 			},
+			SumConfig: ddconfig.SumConfig{
+				CumulativeMonotonicMode: ddconfig.CumulativeMonotonicSumModeToDelta,
+			},
 		},
 
 		Traces: ddconfig.TracesConfig{
