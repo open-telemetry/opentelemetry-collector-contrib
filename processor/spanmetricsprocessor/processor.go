bd4c2432f3c302482a4d1ce45b4f33db78b95ae5
Thanh Tran <thanh@canva.com>
2022-04-04 10:21:53 +1000

    Revert "Change max duration to max float64 (#2)" (#5)
    This reverts commit 8b97c75bd3ecdb9645998cef948751d8f65774a0.

--- processor/spanmetricsprocessor/processor.go
+++ processor/spanmetricsprocessor/processor.go
@@ -46,7 +46,8 @@ const (
 )
 
 var (
-	maxDurationMs = math.MaxFloat64
+	maxDuration   = time.Duration(math.MaxInt64)
+	maxDurationMs = durationToMillis(maxDuration)
 
 	defaultLatencyHistogramBucketsMs = []float64{
 		2, 4, 6, 8, 10, 50, 100, 200, 400, 800, 1000, 1400, 2000, 5000, 10_000, 15_000, maxDurationMs,
