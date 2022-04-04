b1022ebcd6ef71b9e0b8517dfcad9af133c1d0b2
Daniel Jaglowski <jaglows3@gmail.com>
2022-04-04 14:00:19 -0400

    Raise RAM constraint on filelog test (#9023)

--- testbed/tests/log_test.go
+++ testbed/tests/log_test.go
@@ -58,7 +58,7 @@ func TestLog10kDPS(t *testing.T) {
 			receiver: testbed.NewOTLPDataReceiver(testbed.GetAvailablePort(t)),
 			resourceSpec: testbed.ResourceSpec{
 				ExpectedMaxCPU: 30,
-				ExpectedMaxRAM: 100,
+				ExpectedMaxRAM: 117,
 			},
 		},
 		{
