371a491a7cc9bc67210d50537082f0acd545fba5
schmikei <keith.schmitt@bluemedora.com>
2022-04-12 17:25:34 -0400

    add // import github.com/open-telemetry/opentelemetry-collector-contrib/receiver/vmwarevcenterreceiver to imports

--- receiver/vmwarevcenterreceiver/config.go
+++ receiver/vmwarevcenterreceiver/config.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package vmwarevcenterreceiver
+package vmwarevcenterreceiver // import github.com/open-telemetry/opentelemetry-collector-contrib/receiver/vmwarevcenterreceiver
 
 import (
 	"errors"
