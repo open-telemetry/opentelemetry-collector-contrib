8fd88dbe7930d086545320e97a5cbb178dc1d0ef
armstrmi <mitchell.armstrong@bluemedora.com>
2022-04-14 09:45:52 -0400

    go mod tidy and added build flags

--- receiver/windowslogreceiver/windowslog.go
+++ receiver/windowslogreceiver/windowslog.go
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build windows
+// +build windows
+
 package windowslogreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/windowslogreceiver"
 
 import (
