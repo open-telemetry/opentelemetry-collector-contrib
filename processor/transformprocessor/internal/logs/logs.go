216eb014557b1f4276df381b3a0ed9ef11a5b819
Anuraag Agrawal <anuraaga@gmail.com>
2022-04-15 03:22:20 +0000

    goporto

--- processor/transformprocessor/internal/logs/logs.go
+++ processor/transformprocessor/internal/logs/logs.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package logs
+package logs // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/transformprocessor/internal/logs"
 
 import (
 	"encoding/hex"
