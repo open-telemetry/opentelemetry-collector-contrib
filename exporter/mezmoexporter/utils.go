6f8404616341da790b44f611eeb075f699c15c0e
Bill Meyer <iambillmeyer@gmail.com>
2022-05-02 10:03:25 -0500

    Initial revision

---
+++ exporter/mezmoexporter/utils.go
@@ -0,0 +1,37 @@
+// Copyright  The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mezmoexporter
+
+import "math/rand"
+
+// truncateString Truncates the given string to a maximum length provided by max.
+func truncateString(s string, max int) string {
+	if len(s) < max {
+		return s
+	}
+
+	return s[:max]
+}
+
+const letters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+// randString Returns a random string of the specified length.
+func randString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
