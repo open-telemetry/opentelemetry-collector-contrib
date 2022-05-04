4eddf6421180a690dfc66df407d9707569a712d3
schmikei <keith.schmitt@bluemedora.com>
2022-05-04 10:35:32 -0400

    fix import order

--- receiver/couchdbreceiver/factory.go
+++ receiver/couchdbreceiver/factory.go
@@ -18,13 +18,14 @@ import (
 	"context"
 	"time"
 
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/couchdbreceiver/internal/metadata"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/config"
 	"go.opentelemetry.io/collector/config/confighttp"
 	"go.opentelemetry.io/collector/config/configtls"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/receiver/scraperhelper"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/couchdbreceiver/internal/metadata"
 )
 
 const (
