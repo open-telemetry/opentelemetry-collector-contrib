f905ec54784732c7c87bcc5b50613f6d2326f991
Travis Illig <tillig@paraesthesia.com>
2022-05-05 15:49:38 -0700

    [processor/tailsamplingprocessor] and support for string invert

--- processor/tailsamplingprocessor/internal/sampling/and_test.go
+++ processor/tailsamplingprocessor/internal/sampling/and_test.go
@@ -78,3 +78,59 @@ func TestAndEvaluatorSampled(t *testing.T) {
 	assert.Equal(t, decision, Sampled)
 
 }
+
+func TestAndEvaluatorStringInvertSampled(t *testing.T) {
+	n1 := NewStringAttributeFilter(zap.NewNop(), "attribute_name", []string{"no_match"}, false, 0, true)
+	n2, err := NewStatusCodeFilter(zap.NewNop(), []string{"ERROR"})
+	if err != nil {
+		t.FailNow()
+	}
+
+	and := NewAnd(zap.NewNop(), []PolicyEvaluator{n1, n2})
+
+	traces := ptrace.NewTraces()
+	rs := traces.ResourceSpans().AppendEmpty()
+	ils := rs.ScopeSpans().AppendEmpty()
+
+	span := ils.Spans().AppendEmpty()
+	span.Attributes().InsertString("attribute_name", "attribute_value")
+	span.Status().SetCode(ptrace.StatusCodeError)
+	span.SetTraceID(pcommon.NewTraceID([16]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}))
+	span.SetSpanID(pcommon.NewSpanID([8]byte{1, 2, 3, 4, 5, 6, 7, 8}))
+
+	trace := &TraceData{
+		ReceivedBatches: []ptrace.Traces{traces},
+	}
+	decision, err := and.Evaluate(traceID, trace)
+	require.NoError(t, err, "Failed to evaluate and policy: %v", err)
+	assert.Equal(t, decision, Sampled)
+
+}
+
+func TestAndEvaluatorStringInvertNotSampled(t *testing.T) {
+	n1 := NewStringAttributeFilter(zap.NewNop(), "attribute_name", []string{"attribute_value"}, false, 0, true)
+	n2, err := NewStatusCodeFilter(zap.NewNop(), []string{"ERROR"})
+	if err != nil {
+		t.FailNow()
+	}
+
+	and := NewAnd(zap.NewNop(), []PolicyEvaluator{n1, n2})
+
+	traces := ptrace.NewTraces()
+	rs := traces.ResourceSpans().AppendEmpty()
+	ils := rs.ScopeSpans().AppendEmpty()
+
+	span := ils.Spans().AppendEmpty()
+	span.Attributes().InsertString("attribute_name", "attribute_value")
+	span.Status().SetCode(ptrace.StatusCodeError)
+	span.SetTraceID(pcommon.NewTraceID([16]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}))
+	span.SetSpanID(pcommon.NewSpanID([8]byte{1, 2, 3, 4, 5, 6, 7, 8}))
+
+	trace := &TraceData{
+		ReceivedBatches: []ptrace.Traces{traces},
+	}
+	decision, err := and.Evaluate(traceID, trace)
+	require.NoError(t, err, "Failed to evaluate and policy: %v", err)
+	assert.Equal(t, decision, NotSampled)
+
+}
