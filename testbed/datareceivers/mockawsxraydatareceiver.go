35e4968430d56fe9908f6027761da82c1846ee97
Ziqi Zhao <zhaoziqi9146@gmail.com>
2022-05-06 23:48:34 +0800

    fix errcheck failed issue for the files of e2e-test, demos and tools. (#9769)
    * fix issue for examples/demo/server
    * fix errcheck issue for testbed and tracegen
    * fix govet error declaration of 'err' shadows declaration
    Signed-off-by: Ziqi Zhao <zhaoziqi9146@gmail.com>

--- testbed/datareceivers/mockawsxraydatareceiver.go
+++ testbed/datareceivers/mockawsxraydatareceiver.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// nolint:errcheck
 package datareceivers // import "github.com/open-telemetry/opentelemetry-collector-contrib/testbed/datareceivers"
 
 import (
@@ -81,8 +80,7 @@ func (ar *MockAwsXrayDataReceiver) Start(tc consumer.Traces, _ consumer.Metrics,
 }
 
 func (ar *MockAwsXrayDataReceiver) Stop() error {
-	ar.receiver.Shutdown(context.Background())
-	return nil
+	return ar.receiver.Shutdown(context.Background())
 }
 
 func (ar *MockAwsXrayDataReceiver) GenConfigYAMLStr() string {
