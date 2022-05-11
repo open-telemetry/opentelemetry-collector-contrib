2599e98f5f256254815704e35cc9ada6deb88eef
Bill Meyer <iambillmeyer@gmail.com>
2022-05-11 16:26:27 -0500

    Applied make goporto updates

--- exporter/mezmoexporter/exporter.go
+++ exporter/mezmoexporter/exporter.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package mezmoexporter
+package mezmoexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/mezmoexporter"
 
 import (
 	"bytes"
--- exporter/mezmoexporter/utils.go
+++ exporter/mezmoexporter/utils.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package mezmoexporter
+package mezmoexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/mezmoexporter"
 
 // truncateString Truncates the given string to a maximum length provided by max.
 func truncateString(s string, max int) string {
