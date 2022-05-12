1ce9b9e6f17180e1b9b7ed005ea96fd018d38755
Martin Hickey <martin.hickey@ie.ibm.com>
2022-05-12 18:17:13 +0100

    Add allow list for components known but no owners
    Review comment:
    - https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/9751#pullrequestreview-970551351
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>

--- .github/workflows/scripts/check-codeowners.sh
+++ .github/workflows/scripts/check-codeowners.sh
@@ -23,12 +23,14 @@
 set -eu -o pipefail
 
 CODEOWNERS=".github/CODEOWNERS"
+ALLOWLIST=".github/ALLOWLIST"
 
 # Get component folders from the project and checks that they have 
 # an owner in $CODEOWNERS 
 check_code_owner_existence() {
   MODULES=$(find . -type f -name "go.mod" -exec dirname {} \; | sort | grep -E '^./' | cut -c 3-)
   MISSING_COMPONENTS=0
+  ALLOW_LIST_COMPONENTS=0
   for module in ${MODULES}
   do
     if ! grep -q "^$module " "$CODEOWNERS"; then
@@ -37,12 +39,19 @@ check_code_owner_existence() {
       # which accounts for internal/aws/awsutil, internal/aws/k8s etc.
       PREFIX_MODULE_PATH=$(echo $module | cut -d/ -f 1-2)
       if ! grep -wq "^$PREFIX_MODULE_PATH/ " "$CODEOWNERS"; then
-        ((MISSING_COMPONENTS=MISSING_COMPONENTS+1))
-        echo "\"$module\" not included in CODEOWNERS"
+        # Check if it is a known component that is waiting on an owner
+        if grep -wq "$module" "$ALLOWLIST"; then
+          ((ALLOW_LIST_COMPONENTS=ALLOW_LIST_COMPONENTS+1))
+          echo "\"$module\" not included in CODEOWNERS but in the ALLOWLIST"
+        else
+          ((MISSING_COMPONENTS=MISSING_COMPONENTS+1))
+          echo "\"$module\" not included in CODEOWNERS"
+        fi
       fi
     fi
   done
-  echo "there are $MISSING_COMPONENTS components not included in CODEOWNERS"
+  echo "there are $ALLOW_LIST_COMPONENTS components not included in CODEOWNERS but known in the ALLOWLIST"
+  echo "there are $MISSING_COMPONENTS components not included in CODEOWNERS and not known in the ALLOWLIST"
   if [ "$MISSING_COMPONENTS" -gt 0 ]; then
     exit 1
   fi
