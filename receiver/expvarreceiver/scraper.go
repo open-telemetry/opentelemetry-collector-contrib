59df1454d664b1a13cc6b2f32b12f8a56c08eb5e
James Moessis <jmoessis@atlassian.com>
2022-05-18 17:00:57 +1000

    initially define the metric metadata.yml for expvarreceiver

--- receiver/expvarreceiver/scraper.go
+++ receiver/expvarreceiver/scraper.go
@@ -31,6 +31,6 @@ func (e expVarScraper) start(_ context.Context, host component.Host) error {
 }
 
 func (e expVarScraper) scrape(ctx context.Context) (pmetric.Metrics, error) {
-	//TODO implement me
-	panic("implement me")
+	metrics := pmetric.NewMetrics()
+	return metrics, nil
 }
