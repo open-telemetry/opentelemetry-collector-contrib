6a1dc0e016091ea27a4abf1c54b46fec81356967
Nico Stewart <nico.stewart@observiq.com>
2022-05-20 15:30:50 -0400

    Make goporto

--- receiver/aerospikereceiver/internal/model/aerospike.go
+++ receiver/aerospikereceiver/internal/model/aerospike.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package model // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/aerospikereceiver/model"
+package model // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/aerospikereceiver/internal/model"
 
 import (
 	"strconv"
