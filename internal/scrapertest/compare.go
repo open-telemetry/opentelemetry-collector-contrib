b76063c212978c4211bdfe0fa5bfb1dff5a06965
Dmitrii Anoshin <anoshindx@gmail.com>
2022-05-20 11:01:43 -0700

    [scrapetest] Add more details to length mismatch error in CompareMetrics (#10170)

--- internal/scrapertest/compare.go
+++ internal/scrapertest/compare.go
@@ -37,7 +37,8 @@ func CompareMetrics(expected, actual pmetric.Metrics, options ...CompareOption) 
 
 	expectedMetrics, actualMetrics := expected.ResourceMetrics(), actual.ResourceMetrics()
 	if expectedMetrics.Len() != actualMetrics.Len() {
-		return fmt.Errorf("number of resources does not match")
+		return fmt.Errorf("number of resources does not match expected: %d, actual: %d", expectedMetrics.Len(),
+			actualMetrics.Len())
 	}
 
 	numResources := expectedMetrics.Len()
@@ -90,7 +91,8 @@ func CompareResourceMetrics(expected, actual pmetric.ResourceMetrics) error {
 	ailms := actual.ScopeMetrics()
 
 	if eilms.Len() != ailms.Len() {
-		return fmt.Errorf("number of instrumentation libraries does not match")
+		return fmt.Errorf("number of instrumentation libraries does not match expected: %d, actual: %d", eilms.Len(),
+			ailms.Len())
 	}
 
 	eilms.Sort(sortInstrumentationLibrary)
@@ -119,7 +121,7 @@ func CompareResourceMetrics(expected, actual pmetric.ResourceMetrics) error {
 // expected and actual values are clones before options are applied.
 func CompareMetricSlices(expected, actual pmetric.MetricSlice) error {
 	if expected.Len() != actual.Len() {
-		return fmt.Errorf("metric slices not of same length")
+		return fmt.Errorf("number of metrics does not match expected: %d, actual: %d", expected.Len(), actual.Len())
 	}
 
 	expectedByName, actualByName := metricsByName(expected), metricsByName(actual)
@@ -183,7 +185,7 @@ func CompareMetricSlices(expected, actual pmetric.MetricSlice) error {
 // an error if they don't match. The error describes what didn't match.
 func CompareNumberDataPointSlices(expected, actual pmetric.NumberDataPointSlice) error {
 	if expected.Len() != actual.Len() {
-		return fmt.Errorf("length of datapoints don't match")
+		return fmt.Errorf("number of datapoints does not match expected: %d, actual: %d", expected.Len(), actual.Len())
 	}
 
 	numPoints := expected.Len()
