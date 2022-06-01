e594840be02528661318eb93b88859405dc0653d
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2022-06-01 12:37:09 -0600

    Fix lint issues

--- processor/transformprocessor/internal/common/func_truncate_all.go
+++ processor/transformprocessor/internal/common/func_truncate_all.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package common
+package common // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/transformprocessor/internal/common"
 
 import (
 	"fmt"
