6d557a36593477d33905b8b7fd840f84eaed8d6a
Bogdan Drutu <bogdandrutu@gmail.com>
2022-06-09 10:44:33 -0700

    carbonreceiver: small fixes in tests, avoid manually constructing addr, and parsing
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- receiver/carbonreceiver/receiver_test.go
+++ receiver/carbonreceiver/receiver_test.go
@@ -18,7 +18,6 @@ package carbonreceiver
 import (
 	"context"
 	"errors"
-	"fmt"
 	"runtime"
 	"testing"
 	"time"
@@ -164,8 +163,7 @@ func Test_carbonreceiver_New(t *testing.T) {
 }
 
 func Test_carbonreceiver_EndToEnd(t *testing.T) {
-	host := "localhost"
-	port := int(testutil.GetAvailablePort(t))
+	addr := testutil.GetAvailableLocalAddress(t)
 	tests := []struct {
 		name     string
 		configFn func() *Config
@@ -177,7 +175,7 @@ func Test_carbonreceiver_EndToEnd(t *testing.T) {
 				return createDefaultConfig().(*Config)
 			},
 			clientFn: func(t *testing.T) *client.Graphite {
-				c, err := client.NewGraphite(client.TCP, host, port)
+				c, err := client.NewGraphite(client.TCP, addr)
 				require.NoError(t, err)
 				return c
 			},
@@ -190,7 +188,7 @@ func Test_carbonreceiver_EndToEnd(t *testing.T) {
 				return cfg
 			},
 			clientFn: func(t *testing.T) *client.Graphite {
-				c, err := client.NewGraphite(client.UDP, host, port)
+				c, err := client.NewGraphite(client.UDP, addr)
 				require.NoError(t, err)
 				return c
 			},
@@ -199,7 +197,7 @@ func Test_carbonreceiver_EndToEnd(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			cfg := tt.configFn()
-			cfg.Endpoint = fmt.Sprintf("%s:%d", host, port)
+			cfg.Endpoint = addr
 			sink := new(consumertest.MetricsSink)
 			rcv, err := New(componenttest.NewNopReceiverCreateSettings(), *cfg, sink)
 			require.NoError(t, err)
