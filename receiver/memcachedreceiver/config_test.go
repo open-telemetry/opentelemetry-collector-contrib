d3afddf394bd31c25cfc6caf98e784edfcac2066
Dan Jaglowski <jaglows3@gmail.com>
2022-06-13 10:44:17 -0400

    Use require instead of assert

--- receiver/memcachedreceiver/config_test.go
+++ receiver/memcachedreceiver/config_test.go
@@ -18,7 +18,6 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component/componenttest"
 	"go.opentelemetry.io/collector/config"
@@ -27,7 +26,7 @@ import (
 
 func TestLoadConfig(t *testing.T) {
 	factories, err := componenttest.NopFactories()
-	assert.Nil(t, err)
+	require.Nil(t, err)
 
 	factory := NewFactory()
 	factories.Receivers[typeStr] = factory
@@ -35,7 +34,7 @@ func TestLoadConfig(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, cfg)
 
-	assert.Equal(t, len(cfg.Receivers), 1)
+	require.Equal(t, len(cfg.Receivers), 1)
 
-	assert.Equal(t, factory.CreateDefaultConfig(), cfg.Receivers[config.NewComponentID("memcached")])
+	require.Equal(t, factory.CreateDefaultConfig(), cfg.Receivers[config.NewComponentID("memcached")])
 }
