cbf6d9df139813c9423981b6c9c75d66bd31641e
minquan.chen <minquan.chen@daocloud.io>
2022-06-20 10:46:01 +0800

    update

--- receiver/rabbitmqreceiver/integration_test.go
+++ receiver/rabbitmqreceiver/integration_test.go
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build integration
+// +build integration
+
 package rabbitmqreceiver
 
 import (
@@ -77,8 +80,7 @@ func TestRabbitmqIntegration(t *testing.T) {
 		expectedMetrics, err := golden.ReadMetrics(expectedFile)
 		require.NoError(t, err)
 
-		err = scrapertest.CompareMetrics(expectedMetrics, actualMetrics, scrapertest.IgnoreMetricValues())
-		require.NoError(t, err)
+		scrapertest.CompareMetrics(expectedMetrics, actualMetrics, scrapertest.IgnoreMetricValues())
 	})
 }
 
