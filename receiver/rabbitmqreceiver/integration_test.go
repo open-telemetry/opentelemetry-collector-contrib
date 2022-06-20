0d6e445b3c0949f48d4c32c6cd2dbac80c016008
Murphy Chen <minquan.chen@daocloud.io>
2022-06-20 23:30:13 +0800

    Add integration test for rabbitmq receiver (#11049)
    * init
    
    * add integration test for rabbitmq
    
    * add integration test for rabbitmq
    
    * add CHANGELOG
    
    * Apply suggestions from code review
    
    Co-authored-by: Daniel Jaglowski <jaglows3@gmail.com>
    
    * revert unrelated file
    
    * mock message push and get
    
    * mock message push and get
    
    * update
    
    * update
    
    Co-authored-by: Daniel Jaglowski <jaglows3@gmail.com>

---
+++ receiver/rabbitmqreceiver/integration_test.go
@@ -0,0 +1,104 @@
+// Copyright  The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build integration
+// +build integration
+
+package rabbitmqreceiver
+
+import (
+	"context"
+	"fmt"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/internal/scrapertest"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/internal/scrapertest/golden"
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+
+	"go.opentelemetry.io/collector/component/componenttest"
+	"go.opentelemetry.io/collector/consumer/consumertest"
+)
+
+var (
+	containerRequest3_9 = testcontainers.ContainerRequest{
+		FromDockerfile: testcontainers.FromDockerfile{
+			Context:    filepath.Join("testdata", "integration"),
+			Dockerfile: "Dockerfile.rabbitmq.3_9",
+		},
+		ExposedPorts: []string{"15672:15672"},
+		Hostname:     "localhost",
+		WaitingFor: wait.ForListeningPort("15672").
+			WithStartupTimeout(2 * time.Minute),
+	}
+)
+
+func TestRabbitmqIntegration(t *testing.T) {
+	t.Run("Running rabbitmq 3.9", func(t *testing.T) {
+		t.Parallel()
+		container := getContainer(t, containerRequest3_9)
+		defer func() {
+			require.NoError(t, container.Terminate(context.Background()))
+		}()
+		hostname, err := container.Host(context.Background())
+		require.NoError(t, err)
+
+		f := NewFactory()
+		cfg := f.CreateDefaultConfig().(*Config)
+		cfg.Endpoint = fmt.Sprintf("http://%s:15672", hostname)
+		cfg.Username = "otelu"
+		cfg.Password = "otelp"
+
+		consumer := new(consumertest.MetricsSink)
+		settings := componenttest.NewNopReceiverCreateSettings()
+		rcvr, err := f.CreateMetricsReceiver(context.Background(), settings, cfg, consumer)
+		require.NoError(t, err, "failed creating metrics receiver")
+
+		require.NoError(t, rcvr.Start(context.Background(), componenttest.NewNopHost()))
+		require.Eventuallyf(t, func() bool {
+			return len(consumer.AllMetrics()) > 0
+		}, 2*time.Minute, 1*time.Second, "failed to receive more than 0 metrics")
+		require.NoError(t, rcvr.Shutdown(context.Background()))
+
+		actualMetrics := consumer.AllMetrics()[0]
+
+		expectedFile := filepath.Join("testdata", "integration", "expected.3_9.json")
+		expectedMetrics, err := golden.ReadMetrics(expectedFile)
+		require.NoError(t, err)
+
+		scrapertest.CompareMetrics(expectedMetrics, actualMetrics, scrapertest.IgnoreMetricValues())
+	})
+}
+
+func getContainer(t *testing.T, req testcontainers.ContainerRequest) testcontainers.Container {
+	require.NoError(t, req.Validate())
+	container, err := testcontainers.GenericContainer(
+		context.Background(),
+		testcontainers.GenericContainerRequest{
+			ContainerRequest: req,
+			Started:          true,
+		})
+	require.NoError(t, err)
+
+	code, err := container.Exec(context.Background(), []string{"./setup.sh"})
+	require.NoError(t, err)
+	require.Equal(t, 0, code)
+
+	err = container.Start(context.Background())
+	require.NoError(t, err)
+	return container
+}
