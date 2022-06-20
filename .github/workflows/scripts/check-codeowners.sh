33586d33809138583ee8c4668f4a310e887365ce
Daniel Jaglowski <jaglows3@gmail.com>
2022-06-20 09:47:48 -0400

    [chore] Clarify output of check-codeowners (#11081)
    * [chore] Clarify output of check-codeowners
    
    * Clarify failure summary
    
    * Only print summary of excepted components if necessary

--- .github/workflows/scripts/check-codeowners.sh
+++ .github/workflows/scripts/check-codeowners.sh
@@ -25,7 +25,7 @@ set -eu -o pipefail
 CODEOWNERS=".github/CODEOWNERS"
 ALLOWLIST=".github/ALLOWLIST"
 
-# Get component folders from the project and checks that they have 
+# Get component folders from the project and checks that they have
 # an owner in $CODEOWNERS
 check_code_owner_existence() {
   MODULES=$(find . -type f -name "go.mod" -exec dirname {} \; | sort | grep -E '^./' | cut -c 3-)
@@ -33,10 +33,10 @@ check_code_owner_existence() {
   ALLOW_LIST_COMPONENTS=0
   for module in ${MODULES}
   do
-    # For a component path exact match, need to add '/ ' to end of module as 
+    # For a component path exact match, need to add '/ ' to end of module as
     # each line in the CODEOWNERS file is of the format:
     # <component_path_relative_from_project_root>/<min_1_space><owner_1><space><owner_2><space>..<owner_n>
-    # This is because the path separator at end is dropped while searching for 
+    # This is because the path separator at end is dropped while searching for
     # modules and there is at least 1 space separating the path from the owners.
     if ! grep -q "^$module/ " "$CODEOWNERS"; then
       # If there is not an exact match to component path, there might be a parent folder
@@ -48,17 +48,21 @@ check_code_owner_existence() {
         # Check if it is a known component that is waiting on an owner
         if grep -wq "$module" "$ALLOWLIST"; then
           ((ALLOW_LIST_COMPONENTS=ALLOW_LIST_COMPONENTS+1))
-          echo "\"$module\" not included in CODEOWNERS but in the ALLOWLIST"
+          echo "pass: \"$module\" not included in CODEOWNERS but in the ALLOWLIST"
         else
           ((MISSING_COMPONENTS=MISSING_COMPONENTS+1))
-          echo "\"$module\" not included in CODEOWNERS"
+          echo "FAIL: \"$module\" not included in CODEOWNERS"
         fi
       fi
     fi
   done
-  echo "there are $ALLOW_LIST_COMPONENTS components not included in CODEOWNERS but known in the ALLOWLIST"
-  echo "there are $MISSING_COMPONENTS components not included in CODEOWNERS and not known in the ALLOWLIST"
+  if [ "$ALLOW_LIST_COMPONENTS" -gt 0 ]; then
+    echo "---"
+    echo "pass: there are $ALLOW_LIST_COMPONENTS components not included in CODEOWNERS but known in the ALLOWLIST"
+  fi
   if [ "$MISSING_COMPONENTS" -gt 0 ]; then
+    echo "---"
+    echo "FAIL: there are $MISSING_COMPONENTS components not included in CODEOWNERS and not known in the ALLOWLIST"
     exit 1
   fi
 }
