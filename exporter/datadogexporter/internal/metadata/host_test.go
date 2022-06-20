d68986fdeb06b126c59f89885df8509b64767055
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2022-06-20 18:53:52 +0200

    [exporter/datadog] Add `exporter.datadog.hostname.preview` feature flag (#10926)
    * [exporter/datadog] Add `exporter.datadog.hostname.preview` feature flag
    
    * Add changelog entry
    
    * Fix test panic
    
    * Address linter error
    
    * Add additional warning abut `host_metadata::hostname_source`

--- exporter/datadogexporter/internal/metadata/host_test.go
+++ exporter/datadogexporter/internal/metadata/host_test.go
@@ -16,13 +16,18 @@ package metadata
 
 import (
 	"context"
+	"errors"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component/componenttest"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"go.uber.org/zap/zaptest/observer"
 
+	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/datadogexporter/internal/metadata/provider"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/datadogexporter/internal/utils/cache"
 )
 
@@ -55,4 +60,106 @@ func TestHost(t *testing.T) {
 	osHostname, err := os.Hostname()
 	require.NoError(t, err)
 	assert.Contains(t, host, osHostname)
+}
+
+var _ provider.HostnameProvider = (*ErrorHostnameProvider)(nil)
+
+type ErrorHostnameProvider string
+
+func (p ErrorHostnameProvider) Hostname(context.Context) (string, error) {
+	return "", errors.New(string(p))
+}
+
+func TestWarnProvider(t *testing.T) {
+	tests := []struct {
+		name            string
+		curProvider     provider.HostnameProvider
+		previewProvider provider.HostnameProvider
+
+		expectedLogs []observer.LoggedEntry
+		hostname     string
+		err          string
+	}{
+		{
+			name:            "current provider fails",
+			curProvider:     ErrorHostnameProvider("errorCurrentHostname"),
+			previewProvider: provider.Config("previewHostname"),
+			err:             "errorCurrentHostname",
+		},
+		{
+			name:            "preview provider fails",
+			curProvider:     provider.Config("currentHostname"),
+			previewProvider: ErrorHostnameProvider("errorPreviewHostname"),
+			hostname:        "currentHostname",
+			expectedLogs: []observer.LoggedEntry{
+				{
+					Entry: zapcore.Entry{
+						Level:   zap.WarnLevel,
+						Message: previewHostnameFailedLogMessage,
+					},
+					Context: []zapcore.Field{
+						{
+							Key:       "error",
+							Type:      zapcore.ErrorType,
+							Interface: errors.New("errorPreviewHostname"),
+						},
+					},
+				},
+			},
+		},
+		{
+			name:            "preview provider and current provider match",
+			curProvider:     provider.Config("hostname"),
+			previewProvider: provider.Config("hostname"),
+			hostname:        "hostname",
+		},
+		{
+			name:            "preview provider and current provider don't match",
+			curProvider:     provider.Config("currentHostname"),
+			previewProvider: provider.Config("previewHostname"),
+			hostname:        "currentHostname",
+			expectedLogs: []observer.LoggedEntry{
+				{
+					Entry: zapcore.Entry{
+						Level:   zap.WarnLevel,
+						Message: defaultHostnameChangeLogMessage,
+					},
+					Context: []zapcore.Field{
+						{
+							Key:    "current default hostname",
+							Type:   zapcore.StringType,
+							String: "currentHostname",
+						},
+						{
+							Key:    "future default hostname",
+							Type:   zapcore.StringType,
+							String: "previewHostname",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, testInstance := range tests {
+		t.Run(testInstance.name, func(t *testing.T) {
+			core, observed := observer.New(zapcore.DebugLevel)
+			provider := &warnProvider{
+				logger:          zap.New(core),
+				curProvider:     testInstance.curProvider,
+				previewProvider: testInstance.previewProvider,
+			}
+
+			hostname, err := provider.Hostname(context.Background())
+			if err != nil || testInstance.err != "" {
+				assert.EqualError(t, err, testInstance.err)
+			} else {
+				assert.Equal(t, testInstance.hostname, hostname)
+			}
+			assert.ElementsMatch(t,
+				testInstance.expectedLogs,
+				observed.AllUntimed(),
+			)
+		})
+	}
 }
