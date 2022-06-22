1a2c8858262ff9dbd9f6635ce15970c10ecc64cc
DiptoChakrabarty <diptochuck123@gmail.com>
2022-06-22 12:34:38 +0530

    fix rebase err

--- exporter/loadbalancingexporter/log_exporter_test.go
+++ exporter/loadbalancingexporter/log_exporter_test.go
@@ -12,10 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-<<<<<<< HEAD
-// nolint:errcheck,gocritic
-=======
->>>>>>> add error checks for loadbalancing exporters
 package loadbalancingexporter
 
 import (
