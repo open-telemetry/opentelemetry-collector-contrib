c6f1bf948b71a80db53b6c383c63732f1f1de542
Dmitrii Anoshin <anoshindx@gmail.com>
2022-06-22 18:34:40 -0700

    [extension/storage] Skip flaky tests on Windows (#11455)
    Had to disable tests from all the packages that import "github.com/open-telemetry/opentelemetry-collector-contrib/extension/storage/dbstorage" directly or indirectly

--- cmd/configschema/docsgen/docsgen/cli_test.go
+++ cmd/configschema/docsgen/docsgen/cli_test.go
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Skip tests on Windows temporarily, see https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/11451
+//go:build !windows
+// +build !windows
+
 package docsgen
 
 import (
