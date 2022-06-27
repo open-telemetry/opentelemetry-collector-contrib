40ad00268cd5a44515baca82a0ed482787154755
Mikołaj Świątek <mswiatek@sumologic.com>
2022-06-27 12:24:03 +0200

    [receiver/filelog] Read lost files first each interval in file input

--- pkg/stanza/internal/fileconsumer/roller.go
+++ pkg/stanza/internal/fileconsumer/roller.go
@@ -17,6 +17,7 @@ package fileconsumer // import "github.com/open-telemetry/opentelemetry-collecto
 import "context"
 
 type roller interface {
+	readLostFiles(context.Context, []*Reader)
 	roll(context.Context, []*Reader)
 	cleanup()
 }
