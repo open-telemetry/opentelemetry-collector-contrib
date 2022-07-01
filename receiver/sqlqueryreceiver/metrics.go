43408ecc393bac5366ae2635c3a5abc5f7a942e1
Corey Smith <coreysmi@amazon.com>
2022-07-01 12:10:11 -0400

    add timestamp to generated metrics

--- receiver/sqlqueryreceiver/metrics.go
+++ receiver/sqlqueryreceiver/metrics.go
@@ -17,7 +17,9 @@ package sqlqueryreceiver // import "github.com/open-telemetry/opentelemetry-coll
 import (
 	"fmt"
 	"strconv"
+	"time"
 
+	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/pmetric"
 )
 
@@ -27,6 +29,9 @@ func rowToMetric(row metricRow, cfg MetricCfg, dest pmetric.Metric) error {
 	dest.SetUnit(cfg.Unit)
 	dataPointSlice := setMetricFields(cfg, dest)
 	dataPoint := dataPointSlice.AppendEmpty()
+	dataPoint.SetTimestamp(pcommon.NewTimestampFromTime(time.Now()))
+	dataPoint.SetStartTimestamp(pcommon.NewTimestampFromTime(time.Now()))
+	dataPoint.SetTimestamp(pcommon.NewTimestampFromTime(time.Now()))
 	value, found := row[cfg.ValueColumn]
 	if !found {
 		return fmt.Errorf("rowToMetric: value_column '%s' not found in result set", cfg.ValueColumn)
