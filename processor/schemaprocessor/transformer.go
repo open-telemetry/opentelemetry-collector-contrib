58256c161db677d0230f0456085049f100c4b7a9
Sean Marciniak <30928402+MovieStoreGuy@users.noreply.github.com>
2022-07-07 16:03:26 +0930

    [Schema Processor] complete implementation squashed

--- processor/schemaprocessor/transformer.go
+++ processor/schemaprocessor/transformer.go
@@ -13,45 +13,145 @@ import (
 	"go.opentelemetry.io/collector/pdata/ptrace"
 	"go.opentelemetry.io/collector/processor"
 	"go.uber.org/zap"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/schemaprocessor/internal/translation"
 )
 
 type transformer struct {
-	targets []string
-	log     *zap.Logger
+	telemetry component.TelemetrySettings
+	config    *Config
+
+	log *zap.Logger
+
+	manager translation.Manager
 }
 
-func newTransformer(
-	_ context.Context,
-	conf component.Config,
-	set processor.Settings,
-) (*transformer, error) {
+func newTransformer(_ context.Context, conf component.Config, set processor.Settings) (*transformer, error) {
 	cfg, ok := conf.(*Config)
 	if !ok {
 		return nil, errors.New("invalid configuration provided")
 	}
+
+	m, err := translation.NewManager(
+		cfg.Targets,
+		set.Logger.Named("schema-manager"),
+	)
+	if err != nil {
+		return nil, err
+	}
+
 	return &transformer{
-		log:     set.Logger,
-		targets: cfg.Targets,
+		config:    cfg,
+		telemetry: set.TelemetrySettings,
+		log:       set.Logger,
+		manager:   m,
 	}, nil
 }
 
-func (t transformer) processLogs(_ context.Context, ld plog.Logs) (plog.Logs, error) {
+func (t transformer) processLogs(ctx context.Context, ld plog.Logs) (plog.Logs, error) {
+	for rl := 0; rl < ld.ResourceLogs().Len(); rl++ {
+		rLog := ld.ResourceLogs().At(rl)
+		resourceSchemaURL := rLog.SchemaUrl()
+		err := t.manager.
+			RequestTranslation(ctx, resourceSchemaURL).
+			ApplyAllResourceChanges(ctx, rLog, resourceSchemaURL)
+		if err != nil {
+			return plog.Logs{}, err
+		}
+		for sl := 0; sl < rLog.ScopeLogs().Len(); sl++ {
+			log := rLog.ScopeLogs().At(sl)
+			logSchemaURL := log.SchemaUrl()
+			if logSchemaURL == "" {
+				logSchemaURL = resourceSchemaURL
+			}
+
+			err := t.manager.
+				RequestTranslation(ctx, logSchemaURL).
+				ApplyScopeLogChanges(ctx, log, logSchemaURL)
+			if err != nil {
+				return plog.Logs{}, err
+			}
+		}
+	}
 	return ld, nil
 }
 
-func (t transformer) processMetrics(_ context.Context, md pmetric.Metrics) (pmetric.Metrics, error) {
+func (t transformer) processMetrics(ctx context.Context, md pmetric.Metrics) (pmetric.Metrics, error) {
+	for rm := 0; rm < md.ResourceMetrics().Len(); rm++ {
+		rMetric := md.ResourceMetrics().At(rm)
+		resourceSchemaURL := rMetric.SchemaUrl()
+		err := t.manager.
+			RequestTranslation(ctx, resourceSchemaURL).
+			ApplyAllResourceChanges(ctx, rMetric, resourceSchemaURL)
+		if err != nil {
+			return pmetric.Metrics{}, err
+		}
+		for sm := 0; sm < rMetric.ScopeMetrics().Len(); sm++ {
+			metric := rMetric.ScopeMetrics().At(sm)
+			metricSchemaURL := metric.SchemaUrl()
+			if metricSchemaURL == "" {
+				metricSchemaURL = resourceSchemaURL
+			}
+			err := t.manager.
+				RequestTranslation(ctx, metricSchemaURL).
+				ApplyScopeMetricChanges(ctx, metric, metricSchemaURL)
+			if err != nil {
+				return pmetric.Metrics{}, err
+			}
+		}
+	}
 	return md, nil
 }
 
-func (t transformer) processTraces(_ context.Context, td ptrace.Traces) (ptrace.Traces, error) {
+func (t transformer) processTraces(ctx context.Context, td ptrace.Traces) (ptrace.Traces, error) {
+	for rt := 0; rt < td.ResourceSpans().Len(); rt++ {
+		rTrace := td.ResourceSpans().At(rt)
+		// todo(ankit) do i need to check if this is empty?
+		resourceSchemaURL := rTrace.SchemaUrl()
+		err := t.manager.
+			RequestTranslation(ctx, resourceSchemaURL).
+			ApplyAllResourceChanges(ctx, rTrace, resourceSchemaURL)
+		if err != nil {
+			return ptrace.Traces{}, err
+		}
+		for ss := 0; ss < rTrace.ScopeSpans().Len(); ss++ {
+			span := rTrace.ScopeSpans().At(ss)
+			spanSchemaURL := span.SchemaUrl()
+			if spanSchemaURL == "" {
+				spanSchemaURL = resourceSchemaURL
+			}
+			err := t.manager.
+				RequestTranslation(ctx, spanSchemaURL).
+				ApplyScopeSpanChanges(ctx, span, spanSchemaURL)
+			if err != nil {
+				return ptrace.Traces{}, err
+			}
+		}
+	}
 	return td, nil
 }
 
 // start will load the remote file definition if it isn't already cached
 // and resolve the schema translation file
-func (t *transformer) start(_ context.Context, _ component.Host) error {
-	for _, target := range t.targets {
-		t.log.Info("Fetching remote schema url", zap.String("schema-url", target))
+func (t *transformer) start(ctx context.Context, host component.Host) error {
+	var providers []translation.Provider
+	// Check for additional extensions that can be checked first before
+	// perfomring the http request
+	// TODO(MovieStoreGuy): Check for storage extensions
+
+	client, err := t.config.ToClient(ctx, host, t.telemetry)
+	if err != nil {
+		return err
 	}
+
+	if err := t.manager.SetProviders(append(providers, translation.NewHTTPProvider(client))...); err != nil {
+		return err
+	}
+	go func(ctx context.Context) {
+		for _, schemaURL := range t.config.Prefetch {
+			_ = t.manager.RequestTranslation(ctx, schemaURL)
+		}
+	}(ctx)
+
 	return nil
 }
--- processor/schemaprocessor/transformer_test.go
+++ processor/schemaprocessor/transformer_test.go
@@ -5,23 +5,38 @@ package schemaprocessor
 
 import (
 	"context"
+	"embed"
 	_ "embed"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/component/componenttest"
+	"go.opentelemetry.io/collector/config/configtelemetry"
 	"go.opentelemetry.io/collector/pdata/plog"
 	"go.opentelemetry.io/collector/pdata/pmetric"
 	"go.opentelemetry.io/collector/pdata/ptrace"
+	"go.opentelemetry.io/collector/pipeline"
 	"go.opentelemetry.io/collector/processor"
+	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/metric/noop"
 	"go.uber.org/zap/zaptest"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/pdatatest/plogtest"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/pdatatest/pmetrictest"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/pdatatest/ptracetest"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/schemaprocessor/internal/translation"
 )
 
 func newTestTransformer(t *testing.T) *transformer {
 	trans, err := newTransformer(context.Background(), newDefaultConfiguration(), processor.Settings{
 		TelemetrySettings: component.TelemetrySettings{
 			Logger: zaptest.NewLogger(t),
+			LeveledMeterProvider: func(_ configtelemetry.Level) metric.MeterProvider {
+				return noop.MeterProvider{}
+			},
 		},
 	})
 	require.NoError(t, err, "Must not error when creating default transformer")
@@ -32,7 +47,149 @@ func TestTransformerStart(t *testing.T) {
 	t.Parallel()
 
 	trans := newTestTransformer(t)
-	assert.NoError(t, trans.start(context.Background(), nil))
+	assert.NoError(t, trans.start(context.Background(), componenttest.NewNopHost()))
+}
+
+//go:embed testdata
+var f embed.FS
+
+func plogsFromJSON(t *testing.T, path string) plog.Logs {
+	t.Helper()
+	unmarshaler := plog.JSONUnmarshaler{}
+	signalJSON, err := f.ReadFile(path)
+	require.NoError(t, err)
+	inSignals, err := unmarshaler.UnmarshalLogs(signalJSON)
+	require.NoError(t, err)
+	require.NotNil(t, inSignals.ResourceLogs())
+	require.NotEqual(t, 0, inSignals.LogRecordCount())
+	return inSignals
+}
+
+func ptracesFromJSON(t *testing.T, path string) ptrace.Traces {
+	t.Helper()
+	unmarshaler := ptrace.JSONUnmarshaler{}
+	signalJSON, err := f.ReadFile(path)
+	require.NoError(t, err)
+	inSignals, err := unmarshaler.UnmarshalTraces(signalJSON)
+	require.NoError(t, err)
+	require.NotNil(t, inSignals.ResourceSpans())
+	require.NotEqual(t, 0, inSignals.SpanCount())
+	return inSignals
+}
+
+func pmetricsFromJSON(t *testing.T, path string) pmetric.Metrics {
+	t.Helper()
+	unmarshaler := pmetric.JSONUnmarshaler{}
+	signalJSON, err := f.ReadFile(path)
+	require.NoError(t, err)
+	inSignals, err := unmarshaler.UnmarshalMetrics(signalJSON)
+	require.NoError(t, err)
+	require.NotNil(t, inSignals.ResourceMetrics())
+	require.NotEqual(t, 0, inSignals.MetricCount())
+	return inSignals
+}
+
+func buildTestTransformer(t *testing.T, targetURL string) *transformer {
+	t.Helper()
+	defaultConfig := newDefaultConfiguration()
+	castedConfig := defaultConfig.(*Config)
+	castedConfig.Targets = []string{targetURL}
+	transform, err := newTransformer(context.Background(), castedConfig, processor.Settings{
+		TelemetrySettings: component.TelemetrySettings{
+			Logger: zaptest.NewLogger(t),
+		},
+	})
+	require.NoError(t, err, "Must not error when creating transformer")
+	err = transform.manager.SetProviders(translation.NewTestProvider(&f))
+	require.NoError(t, err)
+	return transform
+}
+
+func TestTransformerSchemaBySections(t *testing.T) {
+	tests := []struct {
+		name     string
+		section  string
+		dataType pipeline.Signal
+	}{
+		{
+			// todo(ankit) do i need to test all data types here?
+			name:     "all_logs",
+			section:  "all",
+			dataType: pipeline.SignalLogs,
+		},
+		{
+			// todo(ankit) do i need to test all data types here?
+			name:     "resources_logs",
+			section:  "resources",
+			dataType: pipeline.SignalLogs,
+		},
+		{
+			name:     "spans",
+			section:  "spans",
+			dataType: pipeline.SignalTraces,
+		},
+		{
+			name:     "span_events_rename_spans",
+			section:  "span_events_rename_spans",
+			dataType: pipeline.SignalTraces,
+		},
+		{
+			name:     "span_events_rename_attributes",
+			section:  "span_events_rename_attributes",
+			dataType: pipeline.SignalTraces,
+		},
+		{
+			name:     "metrics_rename_metrics",
+			section:  "metrics_rename_metrics",
+			dataType: pipeline.SignalMetrics,
+		},
+		{
+			name:     "metrics_rename_attributes",
+			section:  "metrics_rename_attributes",
+			dataType: pipeline.SignalMetrics,
+		},
+		{
+			name:     "logs_rename_attributes",
+			section:  "logs_rename_attributes",
+			dataType: pipeline.SignalLogs,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			transformerTarget := fmt.Sprintf("https://example.com/testdata/schema_sections/%s/1.0.0", tc.section)
+			transform := buildTestTransformer(t, transformerTarget)
+			inDataPath := fmt.Sprintf("testdata/schema_sections/%s/%s_in.json", tc.section, tc.dataType)
+			outDataPath := fmt.Sprintf("testdata/schema_sections/%s/%s_out.json", tc.section, tc.dataType)
+			switch tc.dataType {
+			case pipeline.SignalLogs:
+				inLogs := plogsFromJSON(t, inDataPath)
+				expected := plogsFromJSON(t, outDataPath)
+
+				logs, err := transform.processLogs(context.Background(), inLogs)
+				require.NoError(t, err)
+				require.NoError(t, plogtest.CompareLogs(expected, logs), "Must match the expected values")
+			case pipeline.SignalMetrics:
+				inMetrics := pmetricsFromJSON(t, inDataPath)
+				expected := pmetricsFromJSON(t, outDataPath)
+
+				metrics, err := transform.processMetrics(context.Background(), inMetrics)
+				require.NoError(t, err)
+				require.NoError(t, pmetrictest.CompareMetrics(expected, metrics), "Must match the expected values")
+			case pipeline.SignalTraces:
+				inTraces := ptracesFromJSON(t, inDataPath)
+				expected := ptracesFromJSON(t, outDataPath)
+
+				traces, err := transform.processTraces(context.Background(), inTraces)
+				require.NoError(t, err)
+				require.NoError(t, ptracetest.CompareTraces(expected, traces), "Must match the expected values")
+			default:
+				require.FailNow(t, "unrecognized data type")
+				return
+			}
+
+		})
+
+	}
 }
 
 func TestTransformerProcessing(t *testing.T) {
@@ -85,4 +242,316 @@ func TestTransformerProcessing(t *testing.T) {
 		assert.NoError(t, err, "Must not error when processing metrics")
 		assert.Equal(t, in, out, "Must return the same data (subject to change)")
 	})
+}
+
+type SchemaUsed int
+
+const (
+	ResourceSchemaVersionUsed = iota + 1
+	ScopeSchemaVersionUsed
+	NoopSchemaUsed
+)
+
+// returns a test log with no schema versions set
+func generateLogForTest() plog.Logs {
+	in := plog.NewLogs()
+	in.ResourceLogs().AppendEmpty()
+	in.ResourceLogs().At(0).ScopeLogs().AppendEmpty()
+	l := in.ResourceLogs().At(0).ScopeLogs().At(0).LogRecords().AppendEmpty()
+	l.Attributes().PutStr("input", "test")
+	return in
+}
+
+//go:embed testdata/testschemas
+var testdataFiles embed.FS
+
+// case 1: resource schema set, scope schema not set, use resource schema
+// case 2: resource schema not set, scope schema set, use scope schema inside
+// case 3: resource schema set, scope schema set, use scope schema
+// case 4: resource schema not set, scope schema not set, noop translation
+func TestTransformerScopeLogSchemaPrecedence(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name            string
+		input           func() plog.Logs
+		whichSchemaUsed SchemaUsed
+		wantErr         assert.ErrorAssertionFunc
+	}{
+		{
+			name: "resourcesetscopeunset",
+			input: func() plog.Logs {
+				log := generateLogForTest()
+				log.ResourceLogs().At(0).SetSchemaUrl("https://example.com/testdata/testschemas/schemaprecedence/1.0.0")
+				return log
+			},
+			whichSchemaUsed: ResourceSchemaVersionUsed,
+			wantErr:         assert.NoError,
+		},
+		{
+			name: "resourceunsetscopeset",
+			input: func() plog.Logs {
+				log := generateLogForTest()
+				log.ResourceLogs().At(0).ScopeLogs().At(0).SetSchemaUrl("https://example.com/testdata/testschemas/schemaprecedence/1.1.0")
+				return log
+			},
+			whichSchemaUsed: ScopeSchemaVersionUsed,
+			wantErr:         assert.NoError,
+		},
+		{
+			name: "resourcesetscopeset",
+			input: func() plog.Logs {
+				log := generateLogForTest()
+				log.ResourceLogs().At(0).SetSchemaUrl("https://example.com/testdata/testschemas/schemaprecedence/1.0.0")
+				log.ResourceLogs().At(0).ScopeLogs().At(0).SetSchemaUrl("https://example.com/testdata/testschemas/schemaprecedence/1.1.0")
+
+				return log
+			},
+			whichSchemaUsed: ScopeSchemaVersionUsed,
+			wantErr:         assert.NoError,
+		},
+		{
+			name: "resourceunsetscopeunset",
+			input: func() plog.Logs {
+				log := generateLogForTest()
+				return log
+			},
+			//want: "https://example.com/testdata/testschemas/schemaprecedence/1.0.0",
+			whichSchemaUsed: NoopSchemaUsed,
+			wantErr:         assert.NoError,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			defaultConfig := newDefaultConfiguration()
+			castedConfig := defaultConfig.(*Config)
+			castedConfig.Targets = []string{"https://example.com/testdata/testschemas/schemaprecedence/1.2.0"}
+			transform, err := newTransformer(context.Background(), defaultConfig, processor.Settings{
+				TelemetrySettings: component.TelemetrySettings{
+					Logger: zaptest.NewLogger(t),
+				},
+			})
+			require.NoError(t, err, "Must not error when creating transformer")
+
+			err = transform.manager.SetProviders(translation.NewTestProvider(&testdataFiles))
+			require.NoError(t, err)
+			got, err := transform.processLogs(context.Background(), tt.input())
+			if !tt.wantErr(t, err, fmt.Sprintf("processLogs(%v)", tt.input())) {
+				return
+			}
+			targetLog := got.ResourceLogs().At(0).ScopeLogs().At(0).LogRecords().At(0)
+			assert.Equal(t, 1, targetLog.Attributes().Len())
+			_, usedResource := targetLog.Attributes().Get("one_one_zero_output")
+			_, usedScope := targetLog.Attributes().Get("one_two_zero_output")
+			_, usedNoop := targetLog.Attributes().Get("input")
+			switch tt.whichSchemaUsed {
+			case ResourceSchemaVersionUsed:
+				assert.True(t, usedResource, "processLogs(%v) not using correct schema,, attributes present: %v", tt.name, targetLog.Attributes().AsRaw())
+			case ScopeSchemaVersionUsed:
+				assert.True(t, usedScope, "processLogs(%v) not using correct schema, attributes present: %v", tt.name, targetLog.Attributes().AsRaw())
+			case NoopSchemaUsed:
+				assert.True(t, usedNoop, "processLogs(%v) not using correct schema,, attributes present: %v", tt.name, targetLog.Attributes().AsRaw())
+			}
+		})
+	}
+}
+
+// returns a test trace with no schema versions set
+func generateTraceForTest() ptrace.Traces {
+	in := ptrace.NewTraces()
+	in.ResourceSpans().AppendEmpty()
+	in.ResourceSpans().At(0).ScopeSpans().AppendEmpty()
+	l := in.ResourceSpans().At(0).ScopeSpans().At(0).Spans().AppendEmpty()
+	l.Attributes().PutStr("input", "test")
+	return in
+}
+
+// literally the exact same tests as above
+// case 1: resource schema set, scope schema not set, use resource schema
+// case 2: resource schema not set, scope schema set, use scope schema inside
+// case 3: resource schema set, scope schema set, use scope schema
+// case 4: resource schema not set, scope schema not set, noop translation
+func TestTransformerScopeTraceSchemaPrecedence(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name            string
+		input           func() ptrace.Traces
+		whichSchemaUsed SchemaUsed
+		wantErr         assert.ErrorAssertionFunc
+	}{
+		{
+			name: "resourcesetscopeunset",
+			input: func() ptrace.Traces {
+				trace := generateTraceForTest()
+				trace.ResourceSpans().At(0).SetSchemaUrl("https://example.com/testdata/testschemas/schemaprecedence/1.0.0")
+				return trace
+			},
+			whichSchemaUsed: ResourceSchemaVersionUsed,
+			wantErr:         assert.NoError,
+		},
+		{
+			name: "resourceunsetscopeset",
+			input: func() ptrace.Traces {
+				trace := generateTraceForTest()
+				trace.ResourceSpans().At(0).ScopeSpans().At(0).SetSchemaUrl("https://example.com/testdata/testschemas/schemaprecedence/1.1.0")
+				return trace
+			},
+			whichSchemaUsed: ScopeSchemaVersionUsed,
+			wantErr:         assert.NoError,
+		},
+		{
+			name: "resourcesetscopeset",
+			input: func() ptrace.Traces {
+				trace := generateTraceForTest()
+				trace.ResourceSpans().At(0).SetSchemaUrl("https://example.com/testdata/testschemas/schemaprecedence/1.0.0")
+				trace.ResourceSpans().At(0).ScopeSpans().At(0).SetSchemaUrl("https://example.com/testdata/testschemas/schemaprecedence/1.1.0")
+
+				return trace
+			},
+			whichSchemaUsed: ScopeSchemaVersionUsed,
+			wantErr:         assert.NoError,
+		},
+		{
+			name: "resourceunsetscopeunset",
+			input: func() ptrace.Traces {
+				trace := generateTraceForTest()
+				return trace
+			},
+			//want: "https://example.com/testdata/testschemas/schemaprecedence/1.0.0",
+			whichSchemaUsed: NoopSchemaUsed,
+			wantErr:         assert.NoError,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			defaultConfig := newDefaultConfiguration()
+			castedConfig := defaultConfig.(*Config)
+			castedConfig.Targets = []string{"https://example.com/testdata/testschemas/schemaprecedence/1.2.0"}
+			transform, err := newTransformer(context.Background(), defaultConfig, processor.Settings{
+				TelemetrySettings: component.TelemetrySettings{
+					Logger: zaptest.NewLogger(t),
+				},
+			})
+			require.NoError(t, err, "Must not error when creating transformer")
+
+			err = transform.manager.SetProviders(translation.NewTestProvider(&testdataFiles))
+			require.NoError(t, err)
+			got, err := transform.processTraces(context.Background(), tt.input())
+			if !tt.wantErr(t, err, fmt.Sprintf("processTraces(%v)", tt.input())) {
+				return
+			}
+			targetTrace := got.ResourceSpans().At(0).ScopeSpans().At(0).Spans().At(0)
+			assert.Equal(t, 1, targetTrace.Attributes().Len())
+			_, usedResource := targetTrace.Attributes().Get("one_one_zero_output")
+			_, usedScope := targetTrace.Attributes().Get("one_two_zero_output")
+			_, usedNoop := targetTrace.Attributes().Get("input")
+			switch tt.whichSchemaUsed {
+			case ResourceSchemaVersionUsed:
+				assert.True(t, usedResource, "processTraces(%v) not using correct schema,, attributes present: %v", tt.name, targetTrace.Attributes().AsRaw())
+			case ScopeSchemaVersionUsed:
+				assert.True(t, usedScope, "processTraces(%v) not using correct schema, attributes present: %v", tt.name, targetTrace.Attributes().AsRaw())
+			case NoopSchemaUsed:
+				assert.True(t, usedNoop, "processTraces(%v) not using correct schema,, attributes present: %v", tt.name, targetTrace.Attributes().AsRaw())
+			}
+		})
+	}
+}
+
+// returns a test metric with no schema versions set
+func generateMetricForTest() pmetric.Metrics {
+	in := pmetric.NewMetrics()
+	in.ResourceMetrics().AppendEmpty()
+	in.ResourceMetrics().At(0).ScopeMetrics().AppendEmpty()
+	l := in.ResourceMetrics().At(0).ScopeMetrics().At(0).Metrics().AppendEmpty()
+	l.SetEmptySum().DataPoints().AppendEmpty().Attributes().PutStr("input", "test")
+	return in
+}
+
+// case 1: resource schema set, scope schema not set, use resource schema
+// case 2: resource schema not set, scope schema set, use scope schema inside
+// case 3: resource schema set, scope schema set, use scope schema
+// case 4: resource schema not set, scope schema not set, noop translation
+func TestTransformerScopeMetricSchemaPrecedence(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name            string
+		input           func() pmetric.Metrics
+		whichSchemaUsed SchemaUsed
+		wantErr         assert.ErrorAssertionFunc
+	}{
+		{
+			name: "resourcesetscopeunset",
+			input: func() pmetric.Metrics {
+				metric := generateMetricForTest()
+				metric.ResourceMetrics().At(0).SetSchemaUrl("https://example.com/testdata/testschemas/schemaprecedence/1.0.0")
+				return metric
+			},
+			whichSchemaUsed: ResourceSchemaVersionUsed,
+			wantErr:         assert.NoError,
+		},
+		{
+			name: "resourceunsetscopeset",
+			input: func() pmetric.Metrics {
+				metric := generateMetricForTest()
+				metric.ResourceMetrics().At(0).ScopeMetrics().At(0).SetSchemaUrl("https://example.com/testdata/testschemas/schemaprecedence/1.1.0")
+				return metric
+			},
+			whichSchemaUsed: ScopeSchemaVersionUsed,
+			wantErr:         assert.NoError,
+		},
+		{
+			name: "resourcesetscopeset",
+			input: func() pmetric.Metrics {
+				metric := generateMetricForTest()
+				metric.ResourceMetrics().At(0).SetSchemaUrl("https://example.com/testdata/testschemas/schemaprecedence/1.0.0")
+				metric.ResourceMetrics().At(0).ScopeMetrics().At(0).SetSchemaUrl("https://example.com/testdata/testschemas/schemaprecedence/1.1.0")
+
+				return metric
+			},
+			whichSchemaUsed: ScopeSchemaVersionUsed,
+			wantErr:         assert.NoError,
+		},
+		{
+			name: "resourceunsetscopeunset",
+			input: func() pmetric.Metrics {
+				metric := generateMetricForTest()
+				return metric
+			},
+			//want: "https://example.com/testdata/testschemas/schemaprecedence/1.0.0",
+			whichSchemaUsed: NoopSchemaUsed,
+			wantErr:         assert.NoError,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			defaultConfig := newDefaultConfiguration()
+			castedConfig := defaultConfig.(*Config)
+			castedConfig.Targets = []string{"https://example.com/testdata/testschemas/schemaprecedence/1.2.0"}
+			transform, err := newTransformer(context.Background(), defaultConfig, processor.Settings{
+				TelemetrySettings: component.TelemetrySettings{
+					Logger: zaptest.NewLogger(t),
+				},
+			})
+			require.NoError(t, err, "Must not error when creating transformer")
+
+			err = transform.manager.SetProviders(translation.NewTestProvider(&testdataFiles))
+			require.NoError(t, err)
+			got, err := transform.processMetrics(context.Background(), tt.input())
+			if !tt.wantErr(t, err, fmt.Sprintf("processMetrics(%v)", tt.input())) {
+				return
+			}
+			targetMetric := got.ResourceMetrics().At(0).ScopeMetrics().At(0).Metrics().At(0).Sum().DataPoints().At(0)
+			assert.Equal(t, 1, targetMetric.Attributes().Len())
+			_, usedResource := targetMetric.Attributes().Get("one_one_zero_output")
+			_, usedScope := targetMetric.Attributes().Get("one_two_zero_output")
+			_, usedNoop := targetMetric.Attributes().Get("input")
+			switch tt.whichSchemaUsed {
+			case ResourceSchemaVersionUsed:
+				assert.True(t, usedResource, "processMetrics(%v) not using correct schema,, attributes present: %v", tt.name, targetMetric.Attributes().AsRaw())
+			case ScopeSchemaVersionUsed:
+				assert.True(t, usedScope, "processMetrics(%v) not using correct schema, attributes present: %v", tt.name, targetMetric.Attributes().AsRaw())
+			case NoopSchemaUsed:
+				assert.True(t, usedNoop, "processMetrics(%v) not using correct schema,, attributes present: %v", tt.name, targetMetric.Attributes().AsRaw())
+			}
+		})
+	}
 }
