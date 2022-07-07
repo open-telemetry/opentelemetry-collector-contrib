fd8a73e269b35019b14bd6e6a0a693a91389de01
Sean Marciniak <30928402+MovieStoreGuy@users.noreply.github.com>
2022-07-07 16:06:02 +0930

    [schema processor] Additional healper packages
    In order to help validate the works of the schema transformer in a
    future PR, these additional packages add in functionality to verify /
    abstract efforts.

---
+++ processor/schemaprocessor/internal/fixture/parallel.go
@@ -0,0 +1,56 @@
+// Copyright  The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fixture
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/schemaprocessor/internal/race"
+)
+
+// ParallelRaceCompute checks to see if the race detector is running before
+// testing the function concurrently
+func ParallelRaceCompute(tb testing.TB, concurrency int, fn func() error) {
+	tb.Helper()
+	if !race.Enabled {
+		tb.Skip(
+			"This test requires the Race Detector to be enabled.",
+			"Please run again with -race to run this test.",
+		)
+		return
+	}
+	require.NotNil(tb, fn, "Must have a valid function")
+
+	var (
+		start = make(chan struct{})
+		wg    sync.WaitGroup
+	)
+	for i := 0; i < concurrency; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			<-start
+			assert.NoError(tb, fn(), "Must not error when executing function")
+		}()
+	}
+	close(start)
+
+	wg.Wait()
+}
