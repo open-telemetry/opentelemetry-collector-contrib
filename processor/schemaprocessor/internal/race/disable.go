f502fb6fda53e2c3a80cf0dbf0d9be8d9f61f167
Sean Marciniak <30928402+MovieStoreGuy@users.noreply.github.com>
2022-07-07 16:06:02 +0930

    [schema processor] Additional healper packages
    In order to help validate the works of the schema transformer in a
    future PR, these additional packages add in functionality to verify /
    abstract efforts.

--- processor/schemaprocessor/internal/race/disable.go
+++ processor/schemaprocessor/internal/race/disable.go
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 //go:build !race
+// +build !race
 
 package race // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/schemaprocessor/internal/race"
 
