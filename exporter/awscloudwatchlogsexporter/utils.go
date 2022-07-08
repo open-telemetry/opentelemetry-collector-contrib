181c26659b19d57db31bf937efac0df3bd2c516f
John D <john@boostchicken.dev>
2022-07-08 06:08:21 -0700

    Fixing import
    Signed-off-by: John Dorman <john@boostchicken.dev>

--- exporter/awscloudwatchlogsexporter/utils.go
+++ exporter/awscloudwatchlogsexporter/utils.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package awscloudwatchlogsexporter
+package awscloudwatchlogsexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/awscloudwatchlogsexporter"
 
 import (
 	"strings"
