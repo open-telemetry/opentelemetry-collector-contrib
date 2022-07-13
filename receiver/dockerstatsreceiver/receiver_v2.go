c36a27f8b68d7c979f7ddffb9977304203dbc746
James Moessis <jmoessis@atlassian.com>
2022-07-13 15:52:10 +1000

    make goporto

--- receiver/dockerstatsreceiver/receiver_v2.go
+++ receiver/dockerstatsreceiver/receiver_v2.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package dockerstatsreceiver
+package dockerstatsreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/dockerstatsreceiver"
 
 import (
 	"context"
