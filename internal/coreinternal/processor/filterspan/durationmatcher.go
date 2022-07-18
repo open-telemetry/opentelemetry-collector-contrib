db423af55cce7797103b95c1b89f1884856529e0
John D <john@boostchicken.dev>
2022-07-18 10:45:03 -0700

    goporto
    Signed-off-by: John D <john@boostchicken.dev>

--- internal/coreinternal/processor/filterspan/durationmatcher.go
+++ internal/coreinternal/processor/filterspan/durationmatcher.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package filterspan
+package filterspan // import "github.com/open-telemetry/opentelemetry-collector-contrib/internal/coreinternal/processor/filterspan"
 
 import (
 	"go.opentelemetry.io/collector/pdata/ptrace"
