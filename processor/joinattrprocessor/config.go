a82210a4875293046e46b5fdf794239d4200ec2c
edenkoveshi <edenk3012@gmail.com>
2022-07-20 17:39:49 +0300

    Added new processor. still needs testing and documentation

---
+++ processor/joinattrprocessor/config.go
@@ -0,0 +1,35 @@
+// Copyright  OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package joinattrprocessor // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/joinattrprocessor"
+
+import (
+	"go.opentelemetry.io/collector/config"
+)
+
+type Config struct {
+	config.ProcessorSettings `mapstructure:",squash"`
+
+	// JoinAttributes are the attributes to be joined
+	JoinAttributes []string `mapstructure:"join_attributes"`
+
+	// Attribute containing the final value
+	TargetAttribute string `mapstructure:"target_attribute"`
+
+	// Attributes will be the joined using the separator
+	Separator string `mapstructure:"separator"`
+
+	// Whether to override an existing attribute or not. Default is false.
+	Override bool `mapstructure:"override"`
+}
---
+++ processor/joinattrprocessor/factory.go
@@ -0,0 +1,71 @@
+// Copyright  OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package joinattrprocessor // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/joinattrprocessor"
+
+import (
+	"context"
+	"fmt"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/config"
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/processor/processorhelper"
+)
+
+const (
+	// The value of "type" key in configuration.
+	typeStr = "join_attributes"
+	// The stability level of the exporter.
+	stability = component.StabilityLevelBeta
+)
+
+// NewFactory creates a factory for the processor processor.
+func NewFactory() component.ProcessorFactory {
+	return component.NewProcessorFactory(
+		typeStr,
+		createDefaultConfig,
+		component.WithTracesProcessorAndStabilityLevel(createTracesProcessor, stability),
+	)
+}
+
+func createDefaultConfig() config.Processor {
+	return &Config{
+		ProcessorSettings: config.NewProcessorSettings(config.NewComponentID(typeStr)),
+	}
+}
+
+// createTracesProcessor creates an instance of processor for processing traces
+func createTracesProcessor(
+	ctx context.Context,
+	params component.ProcessorCreateSettings,
+	cfg config.Processor,
+	next consumer.Traces,
+) (component.TracesProcessor, error) {
+	oCfg := cfg.(*Config)
+
+	processor, err := newJoinAttrProcessor(ctx, oCfg)
+	if err != nil {
+		// TODO: Placeholder for an error metric in the next PR
+		return nil, fmt.Errorf("error creating a JoinAttributes processor: %w", err)
+	}
+
+	return processorhelper.NewTracesProcessor(
+		cfg,
+		next,
+		processor.processTraces,
+		processorhelper.WithCapabilities(processor.Capabilities()),
+		processorhelper.WithStart(processor.Start),
+		processorhelper.WithShutdown(processor.Shutdown))
+}
