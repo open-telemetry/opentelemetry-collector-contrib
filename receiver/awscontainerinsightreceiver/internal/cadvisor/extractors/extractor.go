cc4bcc2d20f505857c5b15e9aec81ff4bb157f75
Pavan Sai Vasireddy (Vasi) <pvasir@amazon.com>
2022-07-21 09:37:19 -0700

    use structured logger

--- receiver/awscontainerinsightreceiver/internal/cadvisor/extractors/extractor.go
+++ receiver/awscontainerinsightreceiver/internal/cadvisor/extractors/extractor.go
@@ -116,7 +116,7 @@ func (c *CAdvisorMetric) Merge(src *CAdvisorMetric) {
 	for k, v := range src.fields {
 		if _, ok := c.fields[k]; ok {
 			c.logger.Debug(fmt.Sprintf("metric being merged has conflict in fields, src: %v, dest: %v \n", *src, *c))
-			c.logger.Debug(fmt.Sprintf("metric being merged has conflict in fields, src: %q, dest: %q \n", src.cgroupPath, c.cgroupPath))
+			c.logger.Debug("metric being merged has conflict in fields", zap.String("src", src.cgroupPath), zap.String("dest", c.cgroupPath))
 			if c.tags[ci.Timestamp] < src.tags[ci.Timestamp] {
 				continue
 			}
