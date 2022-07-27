c92684f9f4dc6e1a47b45294d72fd622fc4aed43
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2022-07-27 11:43:18 -0600

    run make goporto

--- pkg/telemetryquerylanguage/functions/tqlotel/func_keep_keys.go
+++ pkg/telemetryquerylanguage/functions/tqlotel/func_keep_keys.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package tqlotel // import "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/telemetryquerylanguage/functions/otel"
+package tqlotel // import "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/telemetryquerylanguage/functions/tqlotel"
 
 import (
 	"go.opentelemetry.io/collector/pdata/pcommon"
