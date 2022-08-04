8f46950d9860d864e99cb20f3cde048f2a9a4607
dwelch-spike <dwelch@aerospike.com>
2022-08-04 16:04:50 -0700

    task: add back bad endpoint scraper_test

--- receiver/aerospikereceiver/scraper_test.go
+++ receiver/aerospikereceiver/scraper_test.go
@@ -23,13 +23,48 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/component/componenttest"
+	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/pmetric"
 	"go.uber.org/zap"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/internal/scrapertest"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/aerospikereceiver/internal/metadata"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/aerospikereceiver/mocks"
 )
+
+func TestNewAerospikeReceiver_BadEndpoint(t *testing.T) {
+	testCases := []struct {
+		name     string
+		endpoint string
+		errMsg   string
+	}{
+		{
+			name:     "no port",
+			endpoint: "localhost",
+			errMsg:   "missing port in address",
+		},
+		{
+			name:     "no address",
+			endpoint: "",
+			errMsg:   "missing port in address",
+		},
+	}
+
+	cs, err := consumer.NewMetrics(func(ctx context.Context, ld pmetric.Metrics) error { return nil })
+	require.NoError(t, err)
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			cfg := &Config{Endpoint: tc.endpoint}
+			receiver, err := newAerospikeReceiver(component.ReceiverCreateSettings{}, cfg, cs)
+			require.ErrorContains(t, err, tc.errMsg)
+			require.Nil(t, receiver)
+		})
+	}
+}
 
 func TestScrape_CollectClusterMetrics(t *testing.T) {
 	t.Parallel()
