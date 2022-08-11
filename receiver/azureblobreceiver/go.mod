20f8cb6dd6fbb1d9635d6e0d0335cc34bea92af8
Eugene <eugene.fedor@gmail.com>
2022-08-11 14:58:55 -0700

    tests

---
+++ receiver/azureblobreceiver/go.mod
@@ -0,0 +1,66 @@
+module github.com/open-telemetry/opentelemetry-collector-contrib/receiver/azureblobreceiver
+
+go 1.18
+
+require (
+	github.com/Azure/azure-event-hubs-go/v3 v3.3.18
+	github.com/Azure/azure-sdk-for-go/sdk/storage/azblob v0.3.0
+	github.com/open-telemetry/opentelemetry-collector-contrib/internal/sharedcomponent v0.57.2
+	github.com/stretchr/testify v1.8.0
+	go.opentelemetry.io/collector v0.58.0
+	go.opentelemetry.io/collector/pdata v0.58.0
+	go.uber.org/zap v1.22.0
+)
+
+require (
+	github.com/Azure/azure-amqp-common-go/v3 v3.2.3 // indirect
+	github.com/Azure/azure-sdk-for-go v51.1.0+incompatible // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v0.22.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/internal v0.9.1 // indirect
+	github.com/Azure/go-amqp v0.17.0 // indirect
+	github.com/Azure/go-autorest v14.2.0+incompatible // indirect
+	github.com/Azure/go-autorest/autorest v0.11.18 // indirect
+	github.com/Azure/go-autorest/autorest/adal v0.9.13 // indirect
+	github.com/Azure/go-autorest/autorest/date v0.3.0 // indirect
+	github.com/Azure/go-autorest/autorest/to v0.4.0 // indirect
+	github.com/Azure/go-autorest/autorest/validation v0.3.1 // indirect
+	github.com/Azure/go-autorest/logger v0.2.1 // indirect
+	github.com/Azure/go-autorest/tracing v0.6.0 // indirect
+	github.com/benbjohnson/clock v1.3.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/devigned/tab v0.1.1 // indirect
+	github.com/form3tech-oss/jwt-go v3.2.2+incompatible // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/jpillora/backoff v0.0.0-20180909062703-3050d21c67d7 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/knadh/koanf v1.4.2 // indirect
+	github.com/kr/text v0.2.0 // indirect
+)
+
+require (
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.6.1 // indirect
+	github.com/stretchr/objx v0.4.0 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	go.opentelemetry.io/otel v1.9.0 // indirect
+	go.opentelemetry.io/otel/metric v0.31.0 // indirect
+	go.opentelemetry.io/otel/trace v1.9.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	golang.org/x/crypto v0.0.0-20220511200225-c6db032c6c88 // indirect
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20211208223120-3a66f561d7aa // indirect
+	google.golang.org/grpc v1.48.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+replace github.com/open-telemetry/opentelemetry-collector-contrib/internal/sharedcomponent => ../../internal/sharedcomponent
---
+++ receiver/azureblobreceiver/receiver.go
@@ -0,0 +1,125 @@
+// Copyright OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package azureblobreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/azureblobreceiver"
+
+import (
+	"context"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/obsreport"
+	"go.opentelemetry.io/collector/pdata/plog"
+	"go.opentelemetry.io/collector/pdata/ptrace"
+	"go.uber.org/zap"
+)
+
+type LogsDataConsumer interface {
+	ConsumeLogsJSON(ctx context.Context, json []byte) error
+	SetNextLogsConsumer(nextLogsConsumer consumer.Logs)
+}
+
+type TracesDataConsumer interface {
+	ConsumeTracesJSON(ctx context.Context, json []byte) error
+	SetNextTracesConsumer(nextracesConsumer consumer.Traces)
+}
+
+type blobReceiver struct {
+	blobEventHandler   BlobEventHandler
+	logger             *zap.Logger
+	logsUnmarshaler    plog.Unmarshaler
+	tracesUnmarshaler  ptrace.Unmarshaler
+	nextLogsConsumer   consumer.Logs
+	nextTracesConsumer consumer.Traces
+	obsrecv            *obsreport.Receiver
+}
+
+func (b *blobReceiver) Start(ctx context.Context, host component.Host) error {
+
+	b.blobEventHandler.SetLogsDataConsumer(b)
+	b.blobEventHandler.SetTracesDataConsumer(b)
+
+	err := b.blobEventHandler.Run(ctx)
+
+	return err
+}
+
+func (b *blobReceiver) Shutdown(ctx context.Context) error {
+	b.blobEventHandler.Close(ctx)
+
+	return nil
+}
+func (b *blobReceiver) SetNextLogsConsumer(nextLogsConsumer consumer.Logs) {
+	b.nextLogsConsumer = nextLogsConsumer
+}
+
+func (b *blobReceiver) SetNextTracesConsumer(nextTracesConsumer consumer.Traces) {
+	b.nextTracesConsumer = nextTracesConsumer
+}
+
+func (b *blobReceiver) ConsumeLogsJSON(ctx context.Context, json []byte) error {
+
+	if b.nextLogsConsumer == nil {
+		return nil
+	}
+
+	logsContext := b.obsrecv.StartLogsOp(ctx)
+
+	logs, err := b.logsUnmarshaler.UnmarshalLogs(json)
+	if err == nil {
+		err = b.nextLogsConsumer.ConsumeLogs(logsContext, logs)
+	} else {
+		b.logger.Error(err.Error())
+	}
+
+	b.obsrecv.EndLogsOp(logsContext, typeStr, 1, err)
+
+	return err
+}
+
+func (b *blobReceiver) ConsumeTracesJSON(ctx context.Context, json []byte) error {
+	if b.nextTracesConsumer == nil {
+		return nil
+	}
+
+	tracesContext := b.obsrecv.StartTracesOp(ctx)
+
+	traces, err := b.tracesUnmarshaler.UnmarshalTraces(json)
+	if err == nil {
+		err = b.nextTracesConsumer.ConsumeTraces(tracesContext, traces)
+	} else {
+		b.logger.Error(err.Error())
+	}
+
+	b.obsrecv.EndTracesOp(tracesContext, typeStr, 1, err)
+
+	return err
+}
+
+// Returns a new instance of the log receiver
+func newReceiver(config Config, set component.ReceiverCreateSettings, blobEventHandler BlobEventHandler) (component.Receiver, error) {
+	blobReceiver := &blobReceiver{
+		blobEventHandler:  blobEventHandler,
+		logger:            set.Logger,
+		logsUnmarshaler:   plog.NewJSONUnmarshaler(),
+		tracesUnmarshaler: ptrace.NewJSONUnmarshaler(),
+		obsrecv: obsreport.NewReceiver(obsreport.ReceiverSettings{
+			ReceiverID:             config.ID(),
+			Transport:              "event",
+			ReceiverCreateSettings: set,
+		}),
+	}
+
+	return blobReceiver, nil
+}
