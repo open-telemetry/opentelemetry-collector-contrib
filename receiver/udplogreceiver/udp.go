acfcfbe4f9805434d522c95e67aee0a74e667395
Daniel Jaglowski <jaglows3@gmail.com>
2022-08-15 11:25:52 -0400

    [receiver/tcp|udplog] Migrate config unmarshaling from yaml to mapstructure (#13294)
    * [receiver/tcp|udp] Migrate config unmarshaling from yaml to mapstructure
    
    pkg/stanza historically used yaml for configuration unmarshaling.
    The process of migrating to mapstructure was begun long ago but
    was never completed. This PR switches tcplog and udplog underlying operators
    to use mapstructure unmarshaling.

--- receiver/udplogreceiver/udp.go
+++ receiver/udplogreceiver/udp.go
@@ -17,7 +17,6 @@ package udplogreceiver // import "github.com/open-telemetry/opentelemetry-collec
 import (
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/config"
-	"gopkg.in/yaml.v2"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/adapter"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/operator"
@@ -50,7 +49,7 @@ func (f ReceiverType) CreateDefaultConfig() config.Receiver {
 			ReceiverSettings: config.NewReceiverSettings(config.NewComponentID(typeStr)),
 			Operators:        adapter.OperatorConfigs{},
 		},
-		Input: adapter.InputConfig{},
+		Config: *udp.NewConfig("udp_input"),
 	}
 }
 
@@ -61,19 +60,12 @@ func (f ReceiverType) BaseConfig(cfg config.Receiver) adapter.BaseConfig {
 
 // UDPLogConfig defines configuration for the udp receiver
 type UDPLogConfig struct {
+	udp.Config         `mapstructure:",squash"`
 	adapter.BaseConfig `mapstructure:",squash"`
-	Input              adapter.InputConfig `mapstructure:",remain"`
 }
 
 // DecodeInputConfig unmarshals the input operator
 func (f ReceiverType) DecodeInputConfig(cfg config.Receiver) (*operator.Config, error) {
 	logConfig := cfg.(*UDPLogConfig)
-	yamlBytes, _ := yaml.Marshal(logConfig.Input)
-	inputCfg := udp.NewConfig("udp_input")
-
-	if err := yaml.Unmarshal(yamlBytes, &inputCfg); err != nil {
-		return nil, err
-	}
-
-	return &operator.Config{Builder: inputCfg}, nil
+	return &operator.Config{Builder: &logConfig.Config}, nil
 }
