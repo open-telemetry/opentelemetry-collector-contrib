39f955134408195aaab2842d34aed97cde0a234a
Joshua MacDonald <jmacd@lightstep.com>
2022-08-18 15:46:15 -0700

    move the old opencensus metrics test utils out of the way

--- internal/coreinternal/metricstestutil/ocmetricstestutil/metricsutil.go
+++ internal/coreinternal/metricstestutil/ocmetricstestutil/metricsutil.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package metricstestutil // import "github.com/open-telemetry/opentelemetry-collector-contrib/internal/coreinternal/metricstestutil"
+package ocmetricstestutil // import "github.com/open-telemetry/opentelemetry-collector-contrib/internal/coreinternal/metricstestutil/ocmetricstestutil"
 
 import (
 	"time"
