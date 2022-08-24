dab0f16cc6ca3f1db5ab25b4055c6e4bb0b860dc
Martin Hickey <martin.hickey@ie.ibm.com>
2022-08-24 16:05:26 +0100

    Add Instana exporter overall structure (#13452)
    * Add Instana exporter overall structure
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>
    Co-authored-by: Martin Hickey <martin.hickey@ie.ibm.com>
    Co-authored-by: Cedric Ziel <cedric@cedric-ziel.com>
    Co-authored-by: Willian Carvalho <willianc@pm.me>
    
    * Update after reviews
    
    Review comments:
    - https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/13452#pullrequestreview-1080778360
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>
    
    * Update after review
    
    Review comments:
    - https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/13452#issuecomment-1222832369
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>
    
    * Integrate exporter into the components
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>
    
    * Update changelog
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>
    
    * Add porto changes
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>
    
    * unit tests
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>
    
    * Update after review
    
    Review comments:
    - https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/13452#discussion_r952745336
    - https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/13452#discussion_r952748104
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>
    Co-authored-by: Cedric Ziel <cedric@cedric-ziel.com>
    Co-authored-by: Willian Carvalho <willianc@pm.me>

---
+++ exporter/instanaexporter/config.go
@@ -0,0 +1,54 @@
+// Copyright 2022, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package instanaexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/instanaexporter"
+
+import (
+	"errors"
+	"strings"
+
+	"go.opentelemetry.io/collector/config"
+	"go.opentelemetry.io/collector/config/confighttp"
+)
+
+// Config defines configuration for the Instana exporter
+type Config struct {
+	config.ExporterSettings `mapstructure:",squash"` // squash ensures fields are correctly decoded in embedded struct
+
+	Endpoint string `mapstructure:"endpoint"`
+
+	AgentKey string `mapstructure:"agent_key"`
+
+	confighttp.HTTPClientSettings `mapstructure:",squash"`
+}
+
+var _ config.Exporter = (*Config)(nil)
+
+// Validate checks if the exporter configuration is valid
+func (cfg *Config) Validate() error {
+
+	if cfg.Endpoint == "" {
+		return errors.New("no Instana endpoint set")
+	}
+
+	if cfg.AgentKey == "" {
+		return errors.New("no Instana agent key set")
+	}
+
+	if !(strings.HasPrefix(cfg.Endpoint, "http://") || strings.HasPrefix(cfg.Endpoint, "https://")) {
+		return errors.New("endpoint must start with http:// or https://")
+	}
+
+	return nil
+}
