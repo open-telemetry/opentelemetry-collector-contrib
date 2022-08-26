a64bfe702109a406349e7b64b925ef42942a2ae2
Bogdan Drutu <bogdandrutu@gmail.com>
2022-08-26 12:13:04 -0700

    Reduce number of allocation while recording metrics with opencensus
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- exporter/loadbalancingexporter/trace_exporter.go
+++ exporter/loadbalancingexporter/trace_exporter.go
@@ -120,14 +120,17 @@ func (e *traceExporterImp) consumeTrace(ctx context.Context, td ptrace.Traces) e
 		start := time.Now()
 		err = te.ConsumeTraces(ctx, td)
 		duration := time.Since(start)
-		ctx, _ = tag.New(ctx, tag.Upsert(tag.MustNewKey("endpoint"), endpoint))
 
 		if err == nil {
-			sCtx, _ := tag.New(ctx, tag.Upsert(tag.MustNewKey("success"), "true"))
-			stats.Record(sCtx, mBackendLatency.M(duration.Milliseconds()))
+			_ = stats.RecordWithTags(
+				ctx,
+				[]tag.Mutator{tag.Upsert(endpointTagKey, endpoint), successTrueMutator},
+				mBackendLatency.M(duration.Milliseconds()))
 		} else {
-			fCtx, _ := tag.New(ctx, tag.Upsert(tag.MustNewKey("success"), "false"))
-			stats.Record(fCtx, mBackendLatency.M(duration.Milliseconds()))
+			_ = stats.RecordWithTags(
+				ctx,
+				[]tag.Mutator{tag.Upsert(endpointTagKey, endpoint), successFalseMutator},
+				mBackendLatency.M(duration.Milliseconds()))
 		}
 	}
 	return err
