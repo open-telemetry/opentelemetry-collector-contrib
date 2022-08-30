19938c8ddbbeef22b3901f70529bbb36564e37ce
Martin Hickey <martin.hickey@ie.ibm.com>
2022-08-30 20:56:12 +0100

    [exporter/instana] Add implementation (#13620)
    * Add Instana exporter implementation
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>
    
    * Add unit tests
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>
    
    * Add changelog
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>
    
    * Add more unit tests
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>
    
    * Update fater review
    
    Review comments:
    - https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/13620#pullrequestreview-1085527736
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>
    
    * Update after review
    
    Review comments:
    
    - https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/13620#discussion_r957596160
    - https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/13620#discussion_r957603426
    - https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/13620#discussion_r957603426
    - https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/13620#discussion_r957609409
    - https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/13620#discussion_r957610214
    - https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/13620#discussion_r957612370
    - https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/13620#discussion_r957613538
    - https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/13620#discussion_r957626028
    - https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/13620#discussion_r957629599
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>

---
+++ exporter/instanaexporter/exporter.go
@@ -0,0 +1,137 @@
+// Copyright 2022, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package instanaexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/instanaexporter"
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"net/http"
+	"runtime"
+	"strings"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/config"
+	"go.opentelemetry.io/collector/consumer/consumererror"
+	"go.opentelemetry.io/collector/pdata/ptrace"
+	"go.uber.org/zap"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/instanaexporter/internal/backend"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/instanaexporter/internal/converter"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/instanaexporter/internal/converter/model"
+)
+
+type instanaExporter struct {
+	config    *Config
+	client    *http.Client
+	settings  component.TelemetrySettings
+	userAgent string
+}
+
+func (e *instanaExporter) start(_ context.Context, host component.Host) error {
+	client, err := e.config.HTTPClientSettings.ToClient(host, e.settings)
+	if err != nil {
+		return err
+	}
+	e.client = client
+	return nil
+}
+
+func (e *instanaExporter) pushConvertedTraces(ctx context.Context, td ptrace.Traces) error {
+	converter := converter.NewConvertAllConverter(e.settings.Logger)
+	spans := make([]model.Span, 0)
+
+	hostID := ""
+	resourceSpans := td.ResourceSpans()
+	for i := 0; i < resourceSpans.Len(); i++ {
+		resSpan := resourceSpans.At(i)
+
+		resource := resSpan.Resource()
+
+		hostIDAttr, ok := resource.Attributes().Get(backend.AttributeInstanaHostID)
+		if ok {
+			hostID = hostIDAttr.StringVal()
+		}
+
+		ilSpans := resSpan.ScopeSpans()
+		for j := 0; j < ilSpans.Len(); j++ {
+			converterBundle := converter.ConvertSpans(resource.Attributes(), ilSpans.At(j).Spans())
+
+			spans = append(spans, converterBundle.Spans...)
+		}
+	}
+
+	bundle := model.Bundle{Spans: spans}
+	if len(bundle.Spans) == 0 {
+		// skip exporting, nothing to do
+		return nil
+	}
+
+	req, err := bundle.Marshal()
+	if err != nil {
+		return consumererror.NewPermanent(err)
+	}
+
+	headers := map[string]string{
+		backend.HeaderKey:  e.config.AgentKey,
+		backend.HeaderHost: hostID,
+		// Used only by the Instana agent and can be set to "0" for the exporter
+		backend.HeaderTime: "0",
+	}
+
+	return e.export(ctx, e.config.Endpoint, headers, req)
+}
+
+func newInstanaExporter(cfg config.Exporter, set component.ExporterCreateSettings) *instanaExporter {
+	iCfg := cfg.(*Config)
+	userAgent := fmt.Sprintf("%s/%s (%s/%s)", set.BuildInfo.Description, set.BuildInfo.Version, runtime.GOOS, runtime.GOARCH)
+	return &instanaExporter{
+		config:    iCfg,
+		settings:  set.TelemetrySettings,
+		userAgent: userAgent,
+	}
+}
+
+func (e *instanaExporter) export(ctx context.Context, url string, header map[string]string, request []byte) error {
+	url = strings.TrimSuffix(url, "/") + "/bundle"
+	e.settings.Logger.Debug("Preparing to make HTTP request", zap.String("url", url))
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, bytes.NewReader(request))
+	if err != nil {
+		return consumererror.NewPermanent(err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("User-Agent", e.userAgent)
+
+	for name, value := range header {
+		req.Header.Set(name, value)
+	}
+
+	resp, err := e.client.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to send a request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 400 && resp.StatusCode <= 499 {
+		return consumererror.NewPermanent(fmt.Errorf("error when sending payload to %s: %s",
+			url, resp.Status))
+	}
+	if resp.StatusCode >= 500 && resp.StatusCode <= 599 {
+		return fmt.Errorf("error when sending payload to %s: %s", url, resp.Status)
+	}
+
+	return nil
+}
