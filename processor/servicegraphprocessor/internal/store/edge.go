bdecf6c5e4e8ef4462fbac15f58fee0bcdd1717f
Mario Rodriguez <mariorvinas@gmail.com>
2022-09-01 12:42:17 +0200

    Use time.After

--- processor/servicegraphprocessor/internal/store/edge.go
+++ processor/servicegraphprocessor/internal/store/edge.go
@@ -40,14 +40,14 @@ type Edge struct {
 	Dimensions map[string]string
 
 	// expiration is the time at which the Edge expires, expressed as Unix time
-	expiration int64
+	expiration time.Time
 }
 
 func newEdge(key string, ttl time.Duration) *Edge {
 	return &Edge{
 		key:        key,
 		Dimensions: make(map[string]string),
-		expiration: time.Now().Add(ttl).Unix(),
+		expiration: time.Now().Add(ttl),
 	}
 }
 
@@ -58,5 +58,5 @@ func (e *Edge) isComplete() bool {
 }
 
 func (e *Edge) isExpired() bool {
-	return time.Now().Unix() >= e.expiration
+	return time.Now().After(e.expiration)
 }
