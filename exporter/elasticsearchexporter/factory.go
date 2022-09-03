c47978a96e433f5a0b12ee907ae0541e12290566
jian.tan <jian.tan@daocloud.io>
2022-09-03 09:45:30 +0800

    fix lint
    Signed-off-by: jian.tan <jian.tan@daocloud.io>

--- exporter/elasticsearchexporter/factory.go
+++ exporter/elasticsearchexporter/factory.go
@@ -100,6 +100,6 @@ func createTracesExporter(ctx context.Context,
 	if err != nil {
 		return nil, fmt.Errorf("cannot configure Elasticsearch traces exporter: %w", err)
 	}
-	return exporterhelper.NewTracesExporterWithContext(ctx, set, cfg, exporter.pushTraceData,
+	return exporterhelper.NewTracesExporter(ctx, set, cfg, exporter.pushTraceData,
 		exporterhelper.WithShutdown(exporter.Shutdown))
 }
