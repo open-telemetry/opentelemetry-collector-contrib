e9e54b96bb297fe398ad7b6f6b4b3eafb4618a46
Bogdan Drutu <bogdandrutu@gmail.com>
2022-09-07 16:40:43 -0700

    [chore] prometheusreceiver: refactor tests (#13945)
    * Use correct API to create labels, require to be sorted.
    * Avoid manual equality/error checks, use assert.
    
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>
    
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- receiver/prometheusreceiver/internal/transaction_test.go
+++ receiver/prometheusreceiver/internal/transaction_test.go
@@ -23,6 +23,7 @@ import (
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/model/exemplar"
 	"github.com/prometheus/prometheus/model/labels"
+	"github.com/prometheus/prometheus/model/metadata"
 	"github.com/prometheus/prometheus/scrape"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -33,139 +34,125 @@ import (
 	"go.opentelemetry.io/collector/pdata/pcommon"
 )
 
-func Test_transaction_pdata(t *testing.T) {
-	// discoveredLabels contain labels prior to any processing
-	discoveredLabels := labels.New(
-		labels.Label{
-			Name:  model.AddressLabel,
-			Value: "address:8080",
-		},
-		labels.Label{
-			Name:  model.MetricNameLabel,
-			Value: "foo",
-		},
-		labels.Label{
-			Name:  model.SchemeLabel,
-			Value: "http",
-		},
-	)
-	// processedLabels contain label values after processing (e.g. relabeling)
-	processedLabels := labels.New(
-		labels.Label{
-			Name:  model.InstanceLabel,
-			Value: "localhost:8080",
-		},
-	)
+var (
+	target = scrape.NewTarget(
+		// processedLabels contain label values after processing (e.g. relabeling)
+		labels.FromMap(map[string]string{
+			model.InstanceLabel: "localhost:8080",
+		}),
+		// discoveredLabels contain labels prior to any processing
+		labels.FromMap(map[string]string{
+			model.AddressLabel:    "address:8080",
+			model.MetricNameLabel: "foo",
+			model.SchemeLabel:     "http",
+		}),
+		nil)
 
-	target := scrape.NewTarget(processedLabels, discoveredLabels, nil)
-	scrapeCtx := scrape.ContextWithTarget(context.Background(), target)
-	scrapeCtx = scrape.ContextWithMetricMetadataStore(scrapeCtx, noopMetricMetadataStore{})
+	scrapeCtx = scrape.ContextWithMetricMetadataStore(
+		scrape.ContextWithTarget(context.Background(), target),
+		testMetadataStore(map[string]scrape.MetricMetadata{}))
+)
 
-	t.Run("Commit Without Adding", func(t *testing.T) {
-		nomc := consumertest.NewNop()
-		tr := newTransaction(scrapeCtx, nil, true, nil, nomc, nil, componenttest.NewNopReceiverCreateSettings(), nopObsRecv())
-		if got := tr.Commit(); got != nil {
-			t.Errorf("expecting nil from Commit() but got err %v", got)
-		}
+func TestTransactionCommitWithoutAdding(t *testing.T) {
+	nomc := consumertest.NewNop()
+	tr := newTransaction(scrapeCtx, nil, true, nil, nomc, nil, componenttest.NewNopReceiverCreateSettings(), nopObsRecv())
+	assert.NoError(t, tr.Commit())
+}
+
+func TestTransactionRollbackDoesNothing(t *testing.T) {
+	nomc := consumertest.NewNop()
+	tr := newTransaction(scrapeCtx, nil, true, nil, nomc, nil, componenttest.NewNopReceiverCreateSettings(), nopObsRecv())
+	assert.NoError(t, tr.Rollback())
+}
+
+func TestTransactionUpdateMetadataDoesNothing(t *testing.T) {
+	nomc := consumertest.NewNop()
+	tr := newTransaction(scrapeCtx, nil, true, nil, nomc, nil, componenttest.NewNopReceiverCreateSettings(), nopObsRecv())
+	_, err := tr.UpdateMetadata(0, labels.New(), metadata.Metadata{})
+	assert.NoError(t, err)
+}
+
+func TestTransactionAppendNoTarget(t *testing.T) {
+	badLabels := labels.FromStrings(model.MetricNameLabel, "foo")
+	nomc := consumertest.NewNop()
+	tr := newTransaction(scrapeCtx, nil, true, nil, nomc, nil, componenttest.NewNopReceiverCreateSettings(), nopObsRecv())
+	_, err := tr.Append(0, badLabels, time.Now().Unix()*1000, 1.0)
+	assert.Error(t, err)
+}
+
+func TestTransactionAppendNoMetricName(t *testing.T) {
+	jobNotFoundLb := labels.FromMap(map[string]string{
+		model.InstanceLabel: "localhost:8080",
+		model.JobLabel:      "test2",
 	})
+	nomc := consumertest.NewNop()
+	tr := newTransaction(scrapeCtx, nil, true, nil, nomc, nil, componenttest.NewNopReceiverCreateSettings(), nopObsRecv())
+	_, err := tr.Append(0, jobNotFoundLb, time.Now().Unix()*1000, 1.0)
+	assert.Error(t, err)
+}
 
-	t.Run("Rollback does nothing", func(t *testing.T) {
-		nomc := consumertest.NewNop()
-		tr := newTransaction(scrapeCtx, nil, true, nil, nomc, nil, componenttest.NewNopReceiverCreateSettings(), nopObsRecv())
-		if got := tr.Rollback(); got != nil {
-			t.Errorf("expecting nil from Rollback() but got err %v", got)
-		}
+func TestTransactionAppend(t *testing.T) {
+	goodLabels := labels.FromMap(map[string]string{
+		model.InstanceLabel:   "localhost:8080",
+		model.JobLabel:        "test",
+		model.MetricNameLabel: "foo",
 	})
+	sink := new(consumertest.MetricsSink)
+	tr := newTransaction(scrapeCtx, nil, true, nil, sink, nil, componenttest.NewNopReceiverCreateSettings(), nopObsRecv())
+	_, err := tr.Append(0, goodLabels, time.Now().Unix()*1000, 1.0)
+	assert.NoError(t, err)
+	tr.metricBuilder.startTime = 1.0 // set to a non-zero value
+	assert.NoError(t, tr.Commit())
+	expectedResource := CreateResource("test", "localhost:8080", labels.FromStrings(model.SchemeLabel, "http"))
+	mds := sink.AllMetrics()
+	require.Len(t, mds, 1)
+	gotResource := mds[0].ResourceMetrics().At(0).Resource()
+	require.Equal(t, expectedResource, gotResource)
+}
 
-	badLabels := labels.Labels([]labels.Label{{Name: "foo", Value: "bar"}})
-	t.Run("Add One No Target", func(t *testing.T) {
-		nomc := consumertest.NewNop()
-		tr := newTransaction(scrapeCtx, nil, true, nil, nomc, nil, componenttest.NewNopReceiverCreateSettings(), nopObsRecv())
-		if _, got := tr.Append(0, badLabels, time.Now().Unix()*1000, 1.0); got == nil {
-			t.Errorf("expecting error from Add() but got nil")
-		}
+func TestTransactionCommitErrorWhenNoStarTime(t *testing.T) {
+	goodLabels := labels.FromMap(map[string]string{
+		model.InstanceLabel:   "localhost:8080",
+		model.JobLabel:        "test",
+		model.MetricNameLabel: "foo",
 	})
+	sink := new(consumertest.MetricsSink)
+	tr := newTransaction(scrapeCtx, nil, true, nil, sink, nil, componenttest.NewNopReceiverCreateSettings(), nopObsRecv())
+	_, err := tr.Append(0, goodLabels, time.Now().Unix()*1000, 1.0)
+	assert.NoError(t, err)
+	tr.metricBuilder.startTime = 0 // zero value means the start time metric is missing
+	assert.ErrorIs(t, tr.Commit(), errNoStartTimeMetrics)
+}
 
-	jobNotFoundLb := labels.Labels([]labels.Label{
-		{Name: "instance", Value: "localhost:8080"},
-		{Name: "job", Value: "test2"},
-		{Name: "foo", Value: "bar"}})
-	t.Run("Add One Job not found", func(t *testing.T) {
-		nomc := consumertest.NewNop()
-		tr := newTransaction(scrapeCtx, nil, true, nil, nomc, nil, componenttest.NewNopReceiverCreateSettings(), nopObsRecv())
-		if _, got := tr.Append(0, jobNotFoundLb, time.Now().Unix()*1000, 1.0); got == nil {
-			t.Errorf("expecting error from Add() but got nil")
-		}
-	})
-
-	goodLabels := labels.Labels([]labels.Label{{Name: "instance", Value: "localhost:8080"},
-		{Name: "job", Value: "test"},
-		{Name: "__name__", Value: "foo"}})
-	t.Run("Add One Good", func(t *testing.T) {
-		sink := new(consumertest.MetricsSink)
-		tr := newTransaction(scrapeCtx, nil, true, nil, sink, nil, componenttest.NewNopReceiverCreateSettings(), nopObsRecv())
-		if _, got := tr.Append(0, goodLabels, time.Now().Unix()*1000, 1.0); got != nil {
-			t.Errorf("expecting error == nil from Add() but got: %v\n", got)
-		}
-		tr.metricBuilder.startTime = 1.0 // set to a non-zero value
-		if got := tr.Commit(); got != nil {
-			t.Errorf("expecting nil from Commit() but got err %v", got)
-		}
-		l := []labels.Label{{Name: "__scheme__", Value: "http"}}
-		expectedNodeResource := CreateResource("test", "localhost:8080", l)
-		mds := sink.AllMetrics()
-		if len(mds) != 1 {
-			t.Fatalf("wanted one batch, got %v\n", sink.AllMetrics())
-		}
-		gotNodeResource := mds[0].ResourceMetrics().At(0).Resource()
-		require.Equal(t, expectedNodeResource, gotNodeResource, "Resources do not match")
-		// TODO: re-enable this when handle unspecified OC type
-		// assert.Len(t, ocmds[0].Metrics, 1)
-	})
-
-	t.Run("Error when start time is zero", func(t *testing.T) {
-		sink := new(consumertest.MetricsSink)
-		tr := newTransaction(scrapeCtx, nil, true, nil, sink, nil, componenttest.NewNopReceiverCreateSettings(), nopObsRecv())
-		if _, got := tr.Append(0, goodLabels, time.Now().Unix()*1000, 1.0); got != nil {
-			t.Errorf("expecting error == nil from Add() but got: %v\n", got)
-		}
-		tr.metricBuilder.startTime = 0 // zero value means the start time metric is missing
-		got := tr.Commit()
-		if got == nil {
-			t.Error("expecting error from Commit() but got nil")
-		} else if got.Error() != errNoStartTimeMetrics.Error() {
-			t.Errorf("expected error %q but got %q", errNoStartTimeMetrics, got)
-		}
-	})
-
-	// Test append exemplar method with exemplars
+func TestTransactionAppendExemplar(t *testing.T) {
 	var ed exemplar.Exemplar
 	ed.Ts = 1660233371385
 	ed.Value = 0.012
-	ed.Labels = []labels.Label{{Name: "instance", Value: "localhost:8080"},
-		{Name: "job", Value: "test"},
-		{Name: "trace_id", Value: "0000a5bc3defg93h"},
-		{Name: "span_id", Value: "0000a5bc3defg93h"}}
-	labels := labels.Labels([]labels.Label{{Name: "instance", Value: "localhost:8080"},
-		{Name: "job", Value: "test"},
-		{Name: "__name__", Value: "foo_request_duration"}})
-	t.Run("Test append exemplar method with exemplars", func(t *testing.T) {
-		sink := new(consumertest.MetricsSink)
-		tr := newTransaction(scrapeCtx, nil, true, nil, sink, nil, componenttest.NewNopReceiverCreateSettings(), nopObsRecv())
-		if _, got := tr.Append(0, labels, ed.Ts, 0.012); got != nil {
-			t.Errorf("expecting error == nil from Add() but got: %v\n", got)
-		}
-		if _, got := tr.AppendExemplar(0, labels, ed); got != nil {
-			t.Errorf("expecting error == nil from AppendExemplar() but got: %v\n", got)
-		}
-		fn := normalizeMetricName(labels.Get(model.MetricNameLabel))
-		gk := tr.metricBuilder.families[fn].getGroupKey(labels)
-		expected := tr.metricBuilder.families[fn].groups[gk].exemplars[ed.Value]
-		assert.Equal(t, expected.Timestamp(), pcommon.NewTimestampFromTime(time.UnixMilli(ed.Ts)))
-		assert.Equal(t, expected.DoubleVal(), ed.Value)
-		assert.Equal(t, expected.TraceID().HexString(), getTraceID("0000a5bc3defg93h"))
-		assert.Equal(t, expected.SpanID().HexString(), getSpanID("0000a5bc3defg93h"))
+	ed.Labels = labels.FromMap(map[string]string{
+		model.InstanceLabel: "localhost:8080",
+		model.JobLabel:      "test",
+		"trace_id":          "0000a5bc3defg93h",
+		"span_id":           "0000a5bc3defg93h",
 	})
+	lbls := labels.FromMap(map[string]string{
+		model.InstanceLabel:   "localhost:8080",
+		model.JobLabel:        "test",
+		model.MetricNameLabel: "foo_request_duration"})
+
+	sink := new(consumertest.MetricsSink)
+	tr := newTransaction(scrapeCtx, nil, true, nil, sink, nil, componenttest.NewNopReceiverCreateSettings(), nopObsRecv())
+
+	_, err := tr.Append(0, lbls, ed.Ts, 0.012)
+	assert.NoError(t, err)
+	_, err = tr.AppendExemplar(0, lbls, ed)
+	assert.NoError(t, err)
+	fn := normalizeMetricName(lbls.Get(model.MetricNameLabel))
+	gk := tr.metricBuilder.families[fn].getGroupKey(lbls)
+	expected := tr.metricBuilder.families[fn].groups[gk].exemplars[ed.Value]
+	assert.Equal(t, expected.Timestamp(), pcommon.NewTimestampFromTime(time.UnixMilli(ed.Ts)))
+	assert.Equal(t, expected.DoubleVal(), ed.Value)
+	assert.Equal(t, expected.TraceID().HexString(), getTraceID("0000a5bc3defg93h"))
+	assert.Equal(t, expected.SpanID().HexString(), getSpanID("0000a5bc3defg93h"))
 }
 
 func getTraceID(trace string) string {
@@ -189,12 +176,3 @@ func nopObsRecv() *obsreport.Receiver {
 		ReceiverCreateSettings: componenttest.NewNopReceiverCreateSettings(),
 	})
 }
-
-type noopMetricMetadataStore struct{}
-
-func (noopMetricMetadataStore) ListMetadata() []scrape.MetricMetadata { return nil }
-func (noopMetricMetadataStore) GetMetadata(metric string) (scrape.MetricMetadata, bool) {
-	return scrape.MetricMetadata{}, false
-}
-func (noopMetricMetadataStore) SizeMetadata() int   { return 0 }
-func (noopMetricMetadataStore) LengthMetadata() int { return 0 }
