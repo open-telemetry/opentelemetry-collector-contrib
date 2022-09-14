3b536c24b64adea64b660b22a82cffb47f8bc690
Dmitrii Anoshin <anoshindx@gmail.com>
2022-09-14 10:17:27 -0700

    [chore] Replace one missed UpsertEmptyBytes occurrence (#14123)
    [chore] Replace missing UpsertEmptyBytes occurrence

--- receiver/solacereceiver/unmarshaller.go
+++ receiver/solacereceiver/unmarshaller.go
@@ -431,7 +431,7 @@ func (u *solaceMessageUnmarshallerV1) insertUserProperty(toMap pcommon.Map, key 
 	case *model_v1.SpanData_UserPropertyValue_DoubleValue:
 		toMap.UpsertDouble(k, v.DoubleValue)
 	case *model_v1.SpanData_UserPropertyValue_ByteArrayValue:
-		toMap.UpsertEmptyBytes(k).FromRaw(v.ByteArrayValue)
+		toMap.UpsertEmpty(k).SetEmptyBytesVal().FromRaw(v.ByteArrayValue)
 	case *model_v1.SpanData_UserPropertyValue_FloatValue:
 		toMap.UpsertDouble(k, float64(v.FloatValue))
 	case *model_v1.SpanData_UserPropertyValue_Int8Value:
