9b2df71e83a8f50f761ac0904dae5a1cb8e3c735
Daniel Jaglowski <jaglows3@gmail.com>
2022-09-20 12:03:24 -0400

    [pkg/stanza] Add config unmarshal tests for tcp/udp/syslog inputs (#14154)

---
+++ pkg/stanza/operator/input/udp/config_test.go
@@ -0,0 +1,51 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package udp
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/operator/helper"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/operator/helper/operatortest"
+)
+
+func TestUnmarshal(t *testing.T) {
+	operatortest.ConfigUnmarshalTests{
+		DefaultConfig: NewConfig(),
+		TestsFile:     filepath.Join(".", "testdata", "config.yaml"),
+		Tests: []operatortest.ConfigUnmarshalTest{
+			{
+				Name:      "default",
+				ExpectErr: false,
+				Expect:    NewConfig(),
+			},
+			{
+				Name:      "all",
+				ExpectErr: false,
+				Expect: func() *Config {
+					cfg := NewConfig()
+					cfg.ListenAddress = "10.0.0.1:9000"
+					cfg.AddAttributes = true
+					cfg.Encoding = helper.NewEncodingConfig()
+					cfg.Encoding.Encoding = "utf-8"
+					cfg.Multiline = helper.NewMultilineConfig()
+					cfg.Multiline.LineStartPattern = "ABC"
+					return cfg
+				}(),
+			},
+		},
+	}.Run(t)
+}
