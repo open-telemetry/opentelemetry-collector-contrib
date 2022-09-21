d17bf6c2971e6f8638c4d058948ce4a3db5aed2a
Brandon Johnson <brandon.johnson@bluemedora.com>
2022-09-21 12:53:23 -0400

    [receiver/mongodbatlas] Add missing audit log fields (#14344)
    Add missing audit log fields

--- receiver/mongodbatlasreceiver/log_decode_test.go
+++ receiver/mongodbatlasreceiver/log_decode_test.go
@@ -144,7 +144,7 @@ func TestDecode5_0(t *testing.T) {
 			ID:        22944,
 			Context:   "conn35107",
 			Message:   "Connection ended",
-			Attributes: map[string]interface{}{
+			Attributes: map[string]any{
 				"remote":          "192.168.248.2:52066",
 				"uuid":            "d3f4641a-14ca-4a24-b5bb-7d7b391a02e7",
 				"connectionId":    float64(35107),
@@ -160,7 +160,7 @@ func TestDecode5_0(t *testing.T) {
 			ID:        22944,
 			Context:   "conn35109",
 			Message:   "Connection ended",
-			Attributes: map[string]interface{}{
+			Attributes: map[string]any{
 				"remote":          "192.168.248.2:52070",
 				"uuid":            "dcdb08ac-981d-41ea-9d6b-f85fe0475bd1",
 				"connectionId":    float64(35109),
@@ -176,8 +176,8 @@ func TestDecode5_0(t *testing.T) {
 			ID:        20997,
 			Context:   "conn9957",
 			Message:   "Refreshed RWC defaults",
-			Attributes: map[string]interface{}{
-				"newDefaults": map[string]interface{}{},
+			Attributes: map[string]any{
+				"newDefaults": map[string]any{},
 			},
 		},
 	}, entries)
@@ -207,7 +207,7 @@ func TestDecode5_0InvalidLog(t *testing.T) {
 			ID:        22944,
 			Context:   "conn35107",
 			Message:   "Connection ended",
-			Attributes: map[string]interface{}{
+			Attributes: map[string]any{
 				"remote":          "192.168.248.2:52066",
 				"uuid":            "d3f4641a-14ca-4a24-b5bb-7d7b391a02e7",
 				"connectionId":    float64(35107),
@@ -227,6 +227,14 @@ func strp(s string) *string {
 	return &s
 }
 
+func intp(i int) *int {
+	return &i
+}
+
+func boolp(b bool) *bool {
+	return &b
+}
+
 func TestDecodeAudit4_2(t *testing.T) {
 	b, err := os.ReadFile(filepath.Join("testdata", "logs", "sample-payloads", "4.2_audit.log"))
 	require.NoError(t, err)
@@ -241,74 +249,137 @@ func TestDecodeAudit4_2(t *testing.T) {
 	entries, err := decodeAuditJSON(zippedBuffer)
 	require.NoError(t, err)
 
-	t.Logf("%#v", entries)
-
 	require.Equal(t, []model.AuditLog{
 		{
-			AuthType: "",
-			Timestamp: model.LogTimestamp{
-				Date: "2022-09-16T01:38:20.034+0000",
+			Type:      "authenticate",
+			Timestamp: model.LogTimestamp{Date: "2022-09-16T01:38:20.034+0000"},
+			Local:     model.Address{IP: strp("127.0.0.1"), Port: intp(27017)},
+			Remote:    model.Address{IP: strp("127.0.0.1"), Port: intp(50722)},
+			Users: []model.AuditUser{
+				{
+					Database: "admin",
+					User:     "mms-automation",
+				},
 			},
-			ID: model.ID{
-				Binary: "",
-				Type:   "",
-			},
-			Local: model.Address{
-				IP:   "127.0.0.1",
-				Port: 27017,
-			},
-			Remote: model.Address{
-				IP:   "127.0.0.1",
-				Port: 50722,
+			Roles: []model.AuditRole{
+				{
+					Database: "admin",
+					Role:     "clusterAdmin",
+				},
+				{
+					Database: "admin",
+					Role:     "backup",
+				},
+				{
+					Database: "admin",
+					Role:     "dbAdminAnyDatabase",
+				},
+				{
+					Database: "admin",
+					Role:     "restore",
+				},
+				{
+					Database: "admin",
+					Role:     "userAdminAnyDatabase",
+				},
+				{
+					Database: "admin",
+					Role:     "readWriteAnyDatabase",
+				},
 			},
 			Result: 0,
-			Param: model.Param{
-				User:      "mms-automation",
-				Database:  "admin",
-				Mechanism: "SCRAM-SHA-1",
+			Param: map[string]any{
+				"db":        "admin",
+				"mechanism": "SCRAM-SHA-1",
+				"user":      "mms-automation",
 			},
 		},
 		{
-			AuthType: "",
-			Timestamp: model.LogTimestamp{
-				Date: "2022-09-16T02:37:38.714+0000",
-			}, ID: model.ID{
-				Binary: "",
-				Type:   "",
-			}, Local: model.Address{
-				IP:   "192.168.248.5",
-				Port: 27017,
-			}, Remote: model.Address{
-				IP:   "192.168.248.6",
-				Port: 43714,
-			}, Result: 0,
-			Param: model.Param{
-				User:      "mms-automation",
-				Database:  "admin",
-				Mechanism: "SCRAM-SHA-1",
+			Type:      "authenticate",
+			Timestamp: model.LogTimestamp{Date: "2022-09-16T02:37:38.714+0000"},
+			Local:     model.Address{IP: strp("192.168.248.5"), Port: intp(27017)},
+			Remote:    model.Address{IP: strp("192.168.248.6"), Port: intp(43714)},
+			Users: []model.AuditUser{
+				{
+					Database: "admin",
+					User:     "mms-automation",
+				},
+			},
+			Roles: []model.AuditRole{
+				{
+					Database: "admin",
+					Role:     "clusterAdmin",
+				},
+				{
+					Database: "admin",
+					Role:     "backup",
+				},
+				{
+					Database: "admin",
+					Role:     "dbAdminAnyDatabase",
+				},
+				{
+					Database: "admin",
+					Role:     "restore",
+				},
+				{
+					Database: "admin",
+					Role:     "userAdminAnyDatabase",
+				},
+				{
+					Database: "admin",
+					Role:     "readWriteAnyDatabase",
+				},
+			},
+			Result: 0,
+			Param: map[string]any{
+				"db":        "admin",
+				"mechanism": "SCRAM-SHA-1",
+				"user":      "mms-automation",
 			},
 		},
 		{
-			AuthType: "",
-			Timestamp: model.LogTimestamp{
-				Date: "2022-09-16T02:38:20.030+0000",
-			}, ID: model.ID{
-				Binary: "",
-				Type:   "",
+			Type:      "authenticate",
+			Timestamp: model.LogTimestamp{Date: "2022-09-16T02:38:20.030+0000"},
+			Local:     model.Address{IP: strp("127.0.0.1"), Port: intp(27017)},
+			Remote:    model.Address{IP: strp("127.0.0.1"), Port: intp(52216)},
+			Users: []model.AuditUser{
+				{
+					Database: "admin",
+					User:     "mms-automation",
+				},
 			},
-			Local: model.Address{
-				IP:   "127.0.0.1",
-				Port: 27017,
-			},
-			Remote: model.Address{
-				IP:   "127.0.0.1",
-				Port: 52216,
+			Roles: []model.AuditRole{
+				{
+					Database: "admin",
+					Role:     "clusterAdmin",
+				},
+				{
+					Database: "admin",
+					Role:     "backup",
+				},
+				{
+					Database: "admin",
+					Role:     "dbAdminAnyDatabase",
+				},
+				{
+					Database: "admin",
+					Role:     "restore",
+				},
+				{
+					Database: "admin",
+					Role:     "userAdminAnyDatabase",
+				},
+				{
+					Database: "admin",
+					Role:     "readWriteAnyDatabase",
+				},
 			},
 			Result: 0,
-			Param: model.Param{
-				User:      "mms-automation",
-				Database:  "admin",
-				Mechanism: "SCRAM-SHA-1",
+			Param: map[string]any{
+				"db":        "admin",
+				"mechanism": "SCRAM-SHA-1",
+				"user":      "mms-automation",
 			},
 		},
 	}, entries)
@@ -330,27 +401,47 @@ func TestDecodeAudit4_2InvalidLog(t *testing.T) {
 
 	require.Equal(t, []model.AuditLog{
 		{
-			AuthType: "",
-			Timestamp: model.LogTimestamp{
-				Date: "2022-09-16T01:38:20.034+0000",
+			Type:      "authenticate",
+			Timestamp: model.LogTimestamp{Date: "2022-09-16T01:38:20.034+0000"},
+			Local:     model.Address{IP: strp("127.0.0.1"), Port: intp(27017)},
+			Remote:    model.Address{IP: strp("127.0.0.1"), Port: intp(50722)},
+			Users: []model.AuditUser{
+				{
+					Database: "admin",
+					User:     "mms-automation",
+				},
 			},
-			ID: model.ID{
-				Binary: "",
-				Type:   "",
-			},
-			Local: model.Address{
-				IP:   "127.0.0.1",
-				Port: 27017,
-			},
-			Remote: model.Address{
-				IP:   "127.0.0.1",
-				Port: 50722,
+			Roles: []model.AuditRole{
+				{
+					Database: "admin",
+					Role:     "clusterAdmin",
+				},
+				{
+					Database: "admin",
+					Role:     "backup",
+				},
+				{
+					Database: "admin",
+					Role:     "dbAdminAnyDatabase",
+				},
+				{
+					Database: "admin",
+					Role:     "restore",
+				},
+				{
+					Database: "admin",
+					Role:     "userAdminAnyDatabase",
+				},
+				{
+					Database: "admin",
+					Role:     "readWriteAnyDatabase",
+				},
 			},
 			Result: 0,
-			Param: model.Param{
-				User:      "mms-automation",
-				Database:  "admin",
-				Mechanism: "SCRAM-SHA-1",
+			Param: map[string]any{
+				"db":        "admin",
+				"mechanism": "SCRAM-SHA-1",
+				"user":      "mms-automation",
 			},
 		},
 	}, entries)
@@ -370,74 +461,85 @@ func TestDecodeAudit5_0(t *testing.T) {
 	entries, err := decodeAuditJSON(zippedBuffer)
 	require.NoError(t, err)
 
-	t.Logf("%#v", entries)
-
 	require.Equal(t, []model.AuditLog{
 		{
-			AuthType: "",
-			Timestamp: model.LogTimestamp{
-				Date: "2022-09-15T23:56:28.043+00:00",
+			Type:      "clientMetadata",
+			Timestamp: model.LogTimestamp{Date: "2022-09-15T23:56:28.043+00:00"},
+			ID:        &model.ID{Binary: "KXMtAMh9TOOSl9aQBW1Zkg==", Type: "04"},
+			Local:     model.Address{IP: strp("192.168.248.2"), Port: intp(27017)},
+			Remote:    model.Address{IP: strp("192.168.248.2"), Port: intp(34736)},
+			Users:     []model.AuditUser{},
+			Roles:     []model.AuditRole{},
+			Result:    0,
+			Param: map[string]any{
+				"clientMetadata": map[string]any{
+					"application": map[string]any{
+						"name": "MongoDB Automation Agent v12.3.4.7674 (git: 4c7df3ac1d15ef3269d44aa38b17376ca00147eb)",
+					},
+					"driver": map[string]any{
+						"name":    "mongo-go-driver",
+						"version": "v1.7.2+prerelease",
+					},
+					"os": map[string]any{
+						"architecture": "amd64",
+						"type":         "linux",
+					},
+					"platform": "go1.18.2",
+				},
+				"localEndpoint": map[string]any{
+					"ip":   "192.168.248.2",
+					"port": float64(27017),
+				},
 			},
-			ID: model.ID{
-				Binary: "KXMtAMh9TOOSl9aQBW1Zkg==",
-				Type:   "04",
+		},
+		{
+			Type:      "clientMetadata",
+			Timestamp: model.LogTimestamp{Date: "2022-09-15T23:56:28.055+00:00"},
+			ID:        &model.ID{Binary: "pWSwWRZvR9CgNsvcDYhiwg==", Type: "04"},
+			Local:     model.Address{IP: strp("192.168.248.2"), Port: intp(27017)},
+			Remote:    model.Address{IP: strp("192.168.248.2"), Port: intp(34740)},
+			Users:     []model.AuditUser{},
+			Roles:     []model.AuditRole{},
+			Result:    0,
+			Param: map[string]any{
+				"clientMetadata": map[string]any{
+					"application": map[string]any{
+						"name": "MongoDB Automation Agent v12.3.4.7674 (git: 4c7df3ac1d15ef3269d44aa38b17376ca00147eb)",
+					},
+					"driver": map[string]any{
+						"name":    "mongo-go-driver",
+						"version": "v1.7.2+prerelease",
+					},
+					"os": map[string]any{
+						"architecture": "amd64",
+						"type":         "linux",
+					},
+					"platform": "go1.18.2",
+				},
+				"localEndpoint": map[string]any{
+					"ip":   "192.168.248.2",
+					"port": float64(27017),
+				},
 			},
-			Local: model.Address{
-				IP:   "192.168.248.2",
-				Port: 27017,
-			}, Remote: model.Address{
-				IP:   "192.168.248.2",
-				Port: 34736,
-			},
-			Result: 0,
-			Param: model.Param{
-				User:      "",
-				Database:  "",
-				Mechanism: "",
-			},
-		}, {
-			AuthType: "",
-			Timestamp: model.LogTimestamp{
-				Date: "2022-09-15T23:56:28.055+00:00",
-			},
-			ID: model.ID{
-				Binary: "pWSwWRZvR9CgNsvcDYhiwg==",
-				Type:   "04",
-			},
-			Local: model.Address{
-				IP:   "192.168.248.2",
-				Port: 27017,
-			},
-			Remote: model.Address{
-				IP:   "192.168.248.2",
-				Port: 34740,
-			},
-			Result: 0,
-			Param: model.Param{
-				User:      "",
-				Database:  "",
-				Mechanism: "",
-			},
-		}, {
-			AuthType: "",
-			Timestamp: model.LogTimestamp{
-				Date: "2022-09-15T23:56:28.071+00:00",
-			},
-			ID: model.ID{
-				Binary: "pWSwWRZvR9CgNsvcDYhiwg==",
-				Type:   "04",
-			}, Local: model.Address{
-				IP:   "192.168.248.2",
-				Port: 27017,
-			}, Remote: model.Address{
-				IP:   "192.168.248.2",
-				Port: 34740,
-			},
-			Result: 0,
-			Param: model.Param{
-				User:      "",
-				Database:  "",
-				Mechanism: "",
+		},
+		{
+			Type:      "logout",
+			Timestamp: model.LogTimestamp{Date: "2022-09-15T23:56:28.071+00:00"},
+			ID:        &model.ID{Binary: "pWSwWRZvR9CgNsvcDYhiwg==", Type: "04"},
+			Local:     model.Address{IP: strp("192.168.248.2"), Port: intp(27017)},
+			Remote:    model.Address{IP: strp("192.168.248.2"), Port: intp(34740)},
+			Users:     []model.AuditUser{},
+			Roles:     []model.AuditRole{},
+			Result:    0,
+			Param: map[string]any{
+				"initialUsers": []any{
+					map[string]any{
+						"db":   "local",
+						"user": "__system",
+					},
+				},
+				"reason":       "Client has disconnected",
+				"updatedUsers": []any{},
 			},
 		},
 	}, entries)
@@ -459,26 +561,33 @@ func TestDecodeAudit5_0InvalidLog(t *testing.T) {
 
 	require.Equal(t, []model.AuditLog{
 		{
-			AuthType: "",
-			Timestamp: model.LogTimestamp{
-				Date: "2022-09-15T23:56:28.043+00:00",
-			},
-			ID: model.ID{
-				Binary: "KXMtAMh9TOOSl9aQBW1Zkg==",
-				Type:   "04",
-			},
-			Local: model.Address{
-				IP:   "192.168.248.2",
-				Port: 27017,
-			}, Remote: model.Address{
-				IP:   "192.168.248.2",
-				Port: 34736,
-			},
-			Result: 0,
-			Param: model.Param{
-				User:      "",
-				Database:  "",
-				Mechanism: "",
+			Type:      "clientMetadata",
+			Timestamp: model.LogTimestamp{Date: "2022-09-15T23:56:28.043+00:00"},
+			ID:        &model.ID{Binary: "KXMtAMh9TOOSl9aQBW1Zkg==", Type: "04"},
+			Local:     model.Address{IP: strp("192.168.248.2"), Port: intp(27017)},
+			Remote:    model.Address{IP: strp("192.168.248.2"), Port: intp(34736)},
+			Users:     []model.AuditUser{},
+			Roles:     []model.AuditRole{},
+			Result:    0,
+			Param: map[string]any{
+				"clientMetadata": map[string]any{
+					"application": map[string]any{
+						"name": "MongoDB Automation Agent v12.3.4.7674 (git: 4c7df3ac1d15ef3269d44aa38b17376ca00147eb)",
+					},
+					"driver": map[string]any{
+						"name":    "mongo-go-driver",
+						"version": "v1.7.2+prerelease",
+					},
+					"os": map[string]any{
+						"architecture": "amd64",
+						"type":         "linux",
+					},
+					"platform": "go1.18.2",
+				},
+				"localEndpoint": map[string]any{
+					"ip":   "192.168.248.2",
+					"port": float64(27017),
+				},
 			},
 		},
 	}, entries)
