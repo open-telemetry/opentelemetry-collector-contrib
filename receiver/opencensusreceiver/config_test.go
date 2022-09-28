7a77dacbcee087ec470609d8be318e8b0dfeca98
Ruslan Kovalov <ruslan.kovalov@grafana.com>
2022-09-28 12:01:38 +0200

    [chore] Change receiver config tests to unmarshal config only for that component. (part3)

--- receiver/opencensusreceiver/config_test.go
+++ receiver/opencensusreceiver/config_test.go
@@ -21,135 +21,142 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"go.opentelemetry.io/collector/component/componenttest"
 	"go.opentelemetry.io/collector/config"
 	"go.opentelemetry.io/collector/config/configgrpc"
 	"go.opentelemetry.io/collector/config/confignet"
 	"go.opentelemetry.io/collector/config/configtls"
-	"go.opentelemetry.io/collector/service/servicetest"
+	"go.opentelemetry.io/collector/confmap/confmaptest"
 )
 
 func TestLoadConfig(t *testing.T) {
-	factories, err := componenttest.NopFactories()
-	assert.NoError(t, err)
+	t.Parallel()
 
-	factory := NewFactory()
-	factories.Receivers[typeStr] = factory
-	cfg, err := servicetest.LoadConfigAndValidate(filepath.Join("testdata", "config.yaml"), factories)
-
+	cm, err := confmaptest.LoadConf(filepath.Join("testdata", "config.yaml"))
 	require.NoError(t, err)
-	require.NotNil(t, cfg)
 
-	assert.Equal(t, len(cfg.Receivers), 7)
-
-	r0 := cfg.Receivers[config.NewComponentID(typeStr)]
-	assert.Equal(t, r0, factory.CreateDefaultConfig())
-
-	r1 := cfg.Receivers[config.NewComponentIDWithName(typeStr, "customname")].(*Config)
-	assert.Equal(t, r1,
-		&Config{
-			ReceiverSettings: config.NewReceiverSettings(config.NewComponentIDWithName(typeStr, "customname")),
-			GRPCServerSettings: configgrpc.GRPCServerSettings{
-				NetAddr: confignet.NetAddr{
-					Endpoint:  "0.0.0.0:9090",
-					Transport: "tcp",
-				},
-				ReadBufferSize: 512 * 1024,
-			},
-		})
-
-	r2 := cfg.Receivers[config.NewComponentIDWithName(typeStr, "keepalive")].(*Config)
-	assert.Equal(t, r2,
-		&Config{
-			ReceiverSettings: config.NewReceiverSettings(config.NewComponentIDWithName(typeStr, "keepalive")),
-			GRPCServerSettings: configgrpc.GRPCServerSettings{
-				NetAddr: confignet.NetAddr{
-					Endpoint:  "0.0.0.0:55678",
-					Transport: "tcp",
-				},
-				ReadBufferSize: 512 * 1024,
-				Keepalive: &configgrpc.KeepaliveServerConfig{
-					ServerParameters: &configgrpc.KeepaliveServerParameters{
-						MaxConnectionIdle:     11 * time.Second,
-						MaxConnectionAge:      12 * time.Second,
-						MaxConnectionAgeGrace: 13 * time.Second,
-						Time:                  30 * time.Second,
-						Timeout:               5 * time.Second,
+	tests := []struct {
+		id       config.ComponentID
+		expected config.Receiver
+	}{
+		{
+			id: config.NewComponentIDWithName(typeStr, "customname"),
+			expected: &Config{
+				ReceiverSettings: config.NewReceiverSettings(config.NewComponentID(typeStr)),
+				GRPCServerSettings: configgrpc.GRPCServerSettings{
+					NetAddr: confignet.NetAddr{
+						Endpoint:  "0.0.0.0:9090",
+						Transport: "tcp",
 					},
-					EnforcementPolicy: &configgrpc.KeepaliveEnforcementPolicy{
-						MinTime:             10 * time.Second,
-						PermitWithoutStream: true,
-					},
+					ReadBufferSize: 512 * 1024,
 				},
 			},
-		})
-
-	r3 := cfg.Receivers[config.NewComponentIDWithName(typeStr, "msg-size-conc-connect-max-idle")].(*Config)
-	assert.Equal(t, r3,
-		&Config{
-			ReceiverSettings: config.NewReceiverSettings(config.NewComponentIDWithName(typeStr, "msg-size-conc-connect-max-idle")),
-			GRPCServerSettings: configgrpc.GRPCServerSettings{
-				NetAddr: confignet.NetAddr{
-					Endpoint:  "0.0.0.0:55678",
-					Transport: "tcp",
-				},
-				MaxRecvMsgSizeMiB:    32,
-				MaxConcurrentStreams: 16,
-				ReadBufferSize:       1024,
-				WriteBufferSize:      1024,
-				Keepalive: &configgrpc.KeepaliveServerConfig{
-					ServerParameters: &configgrpc.KeepaliveServerParameters{
-						MaxConnectionIdle: 10 * time.Second,
+		},
+		{
+			id: config.NewComponentIDWithName(typeStr, "keepalive"),
+			expected: &Config{
+				ReceiverSettings: config.NewReceiverSettings(config.NewComponentID(typeStr)),
+				GRPCServerSettings: configgrpc.GRPCServerSettings{
+					NetAddr: confignet.NetAddr{
+						Endpoint:  "0.0.0.0:55678",
+						Transport: "tcp",
+					},
+					ReadBufferSize: 512 * 1024,
+					Keepalive: &configgrpc.KeepaliveServerConfig{
+						ServerParameters: &configgrpc.KeepaliveServerParameters{
+							MaxConnectionIdle:     11 * time.Second,
+							MaxConnectionAge:      12 * time.Second,
+							MaxConnectionAgeGrace: 13 * time.Second,
+							Time:                  30 * time.Second,
+							Timeout:               5 * time.Second,
+						},
+						EnforcementPolicy: &configgrpc.KeepaliveEnforcementPolicy{
+							MinTime:             10 * time.Second,
+							PermitWithoutStream: true,
+						},
 					},
 				},
 			},
-		})
-
-	// TODO(ccaraman): Once the config loader checks for the files existence, this test may fail and require
-	// 	use of fake cert/key for test purposes.
-	r4 := cfg.Receivers[config.NewComponentIDWithName(typeStr, "tlscredentials")].(*Config)
-	assert.Equal(t, r4,
-		&Config{
-			ReceiverSettings: config.NewReceiverSettings(config.NewComponentIDWithName(typeStr, "tlscredentials")),
-			GRPCServerSettings: configgrpc.GRPCServerSettings{
-				NetAddr: confignet.NetAddr{
-					Endpoint:  "0.0.0.0:55678",
-					Transport: "tcp",
-				},
-				ReadBufferSize: 512 * 1024,
-				TLSSetting: &configtls.TLSServerSetting{
-					TLSSetting: configtls.TLSSetting{
-						CertFile: "test.crt",
-						KeyFile:  "test.key",
+		},
+		{
+			id: config.NewComponentIDWithName(typeStr, "msg-size-conc-connect-max-idle"),
+			expected: &Config{
+				ReceiverSettings: config.NewReceiverSettings(config.NewComponentID(typeStr)),
+				GRPCServerSettings: configgrpc.GRPCServerSettings{
+					NetAddr: confignet.NetAddr{
+						Endpoint:  "0.0.0.0:55678",
+						Transport: "tcp",
+					},
+					MaxRecvMsgSizeMiB:    32,
+					MaxConcurrentStreams: 16,
+					ReadBufferSize:       1024,
+					WriteBufferSize:      1024,
+					Keepalive: &configgrpc.KeepaliveServerConfig{
+						ServerParameters: &configgrpc.KeepaliveServerParameters{
+							MaxConnectionIdle: 10 * time.Second,
+						},
 					},
 				},
 			},
-		})
-
-	r5 := cfg.Receivers[config.NewComponentIDWithName(typeStr, "cors")].(*Config)
-	assert.Equal(t, r5,
-		&Config{
-			ReceiverSettings: config.NewReceiverSettings(config.NewComponentIDWithName(typeStr, "cors")),
-			GRPCServerSettings: configgrpc.GRPCServerSettings{
-				NetAddr: confignet.NetAddr{
-					Endpoint:  "0.0.0.0:55678",
-					Transport: "tcp",
+		},
+		{
+			id: config.NewComponentIDWithName(typeStr, "tlscredentials"),
+			expected: &Config{
+				ReceiverSettings: config.NewReceiverSettings(config.NewComponentID(typeStr)),
+				GRPCServerSettings: configgrpc.GRPCServerSettings{
+					NetAddr: confignet.NetAddr{
+						Endpoint:  "0.0.0.0:55678",
+						Transport: "tcp",
+					},
+					ReadBufferSize: 512 * 1024,
+					TLSSetting: &configtls.TLSServerSetting{
+						TLSSetting: configtls.TLSSetting{
+							CertFile: "test.crt",
+							KeyFile:  "test.key",
+						},
+					},
 				},
-				ReadBufferSize: 512 * 1024,
 			},
-			CorsOrigins: []string{"https://*.test.com", "https://test.com"},
-		})
-
-	r6 := cfg.Receivers[config.NewComponentIDWithName(typeStr, "uds")].(*Config)
-	assert.Equal(t, r6,
-		&Config{
-			ReceiverSettings: config.NewReceiverSettings(config.NewComponentIDWithName(typeStr, "uds")),
-			GRPCServerSettings: configgrpc.GRPCServerSettings{
-				NetAddr: confignet.NetAddr{
-					Endpoint:  "/tmp/opencensus.sock",
-					Transport: "unix",
+		},
+		{
+			id: config.NewComponentIDWithName(typeStr, "cors"),
+			expected: &Config{
+				ReceiverSettings: config.NewReceiverSettings(config.NewComponentID(typeStr)),
+				GRPCServerSettings: configgrpc.GRPCServerSettings{
+					NetAddr: confignet.NetAddr{
+						Endpoint:  "0.0.0.0:55678",
+						Transport: "tcp",
+					},
+					ReadBufferSize: 512 * 1024,
 				},
-				ReadBufferSize: 512 * 1024,
+				CorsOrigins: []string{"https://*.test.com", "https://test.com"},
 			},
+		},
+		{
+			id: config.NewComponentIDWithName(typeStr, "uds"),
+			expected: &Config{
+				ReceiverSettings: config.NewReceiverSettings(config.NewComponentID(typeStr)),
+				GRPCServerSettings: configgrpc.GRPCServerSettings{
+					NetAddr: confignet.NetAddr{
+						Endpoint:  "/tmp/opencensus.sock",
+						Transport: "unix",
+					},
+					ReadBufferSize: 512 * 1024,
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.id.String(), func(t *testing.T) {
+			factory := NewFactory()
+			cfg := factory.CreateDefaultConfig()
+
+			sub, err := cm.Sub(tt.id.String())
+			require.NoError(t, err)
+			require.NoError(t, config.UnmarshalReceiver(sub, cfg))
+
+			assert.NoError(t, cfg.Validate())
+			assert.Equal(t, tt.expected, cfg)
 		})
+	}
 }
