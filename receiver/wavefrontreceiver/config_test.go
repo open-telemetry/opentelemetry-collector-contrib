146af87a9b5eba5168853cd37aaed10211249bd4
Ruslan Kovalov <ruslan.kovalov@grafana.com>
2022-09-28 18:51:58 +0200

    [chore] Change receiver config tests to unmarshal config only for that component. (part5) (#14563)
    - [x] splunkhecreceiver
    - [x] sqlqueryreceiver
    - [x] sqlserverreceiver
    - [x] statsdreceiver
    - [x] syslogreceiver
    - [x] tcplogreceiver
    - [x] udplogreceiver
    - [x] vcenterreceiver
    - [x] wavefrontreceiver
    - [x] windowsperfcountersreceiver
    - [x] zipkinreceiver
    - [x] zookeeperreceiver

--- receiver/wavefrontreceiver/config_test.go
+++ receiver/wavefrontreceiver/config_test.go
@@ -21,37 +21,49 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"go.opentelemetry.io/collector/component/componenttest"
 	"go.opentelemetry.io/collector/config"
 	"go.opentelemetry.io/collector/config/confignet"
-	"go.opentelemetry.io/collector/service/servicetest"
+	"go.opentelemetry.io/collector/confmap/confmaptest"
 )
 
 func TestLoadConfig(t *testing.T) {
-	factories, err := componenttest.NopFactories()
-	assert.Nil(t, err)
+	t.Parallel()
 
-	factory := NewFactory()
-	factories.Receivers[typeStr] = factory
-	cfg, err := servicetest.LoadConfigAndValidate(filepath.Join("testdata", "config.yaml"), factories)
-
+	cm, err := confmaptest.LoadConf(filepath.Join("testdata", "config.yaml"))
 	require.NoError(t, err)
-	require.NotNil(t, cfg)
 
-	assert.Equal(t, len(cfg.Receivers), 2)
-
-	r0 := cfg.Receivers[config.NewComponentID(typeStr)]
-	assert.Equal(t, factory.CreateDefaultConfig(), r0)
-
-	r1 := cfg.Receivers[config.NewComponentIDWithName(typeStr, "allsettings")].(*Config)
-	assert.Equal(t,
-		&Config{
-			ReceiverSettings: config.NewReceiverSettings(config.NewComponentIDWithName(typeStr, "allsettings")),
-			TCPAddr: confignet.TCPAddr{
-				Endpoint: "localhost:8080",
-			},
-			TCPIdleTimeout:      5 * time.Second,
-			ExtractCollectdTags: true,
+	tests := []struct {
+		id       config.ComponentID
+		expected config.Receiver
+	}{
+		{
+			id:       config.NewComponentID(typeStr),
+			expected: createDefaultConfig(),
 		},
-		r1)
+		{
+			id: config.NewComponentIDWithName(typeStr, "allsettings"),
+			expected: &Config{
+				ReceiverSettings: config.NewReceiverSettings(config.NewComponentID(typeStr)),
+				TCPAddr: confignet.TCPAddr{
+					Endpoint: "localhost:8080",
+				},
+				TCPIdleTimeout:      5 * time.Second,
+				ExtractCollectdTags: true,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.id.String(), func(t *testing.T) {
+			factory := NewFactory()
+			cfg := factory.CreateDefaultConfig()
+
+			sub, err := cm.Sub(tt.id.String())
+			require.NoError(t, err)
+			require.NoError(t, config.UnmarshalReceiver(sub, cfg))
+
+			assert.NoError(t, cfg.Validate())
+			assert.Equal(t, tt.expected, cfg)
+		})
+	}
 }
