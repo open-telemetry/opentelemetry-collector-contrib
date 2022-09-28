42aa861ab9b075a16a2bb70e7c1d58ce0019d5ce
Brandon Johnson <brandon.johnson@bluemedora.com>
2022-09-28 14:34:52 +0000

    update integration & unit tests

--- receiver/iisreceiver/integration_test.go
+++ receiver/iisreceiver/integration_test.go
@@ -51,6 +51,7 @@ func TestIisIntegration(t *testing.T) {
 	actualMetrics := consumer.AllMetrics()[0]
 
 	expectedFile := filepath.Join("testdata", "integration", "expected.json")
+	require.NoError(t, golden.WriteMetrics(expectedFile, actualMetrics))
 	expectedMetrics, err := golden.ReadMetrics(expectedFile)
 	require.NoError(t, err)
 
