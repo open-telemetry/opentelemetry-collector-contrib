e9b3f289a45ce37567dc9cb7a11e547ca28be5a9
Bogdan Drutu <bogdandrutu@gmail.com>
2022-09-30 10:27:01 -0700

    [chore] Remove usage of MoveAndAppend in tailsamplingprocessor
    Also improves number of total allocations, avoiding unnecessary slices and ptrace.Traces instances
    
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- processor/tailsamplingprocessor/internal/sampling/numeric_tag_filter_test.go
+++ processor/tailsamplingprocessor/internal/sampling/numeric_tag_filter_test.go
@@ -76,7 +76,6 @@ func TestNumericTagFilter(t *testing.T) {
 }
 
 func newTraceIntAttrs(nodeAttrs map[string]interface{}, spanAttrKey string, spanAttrValue int64) *TraceData {
-	var traceBatches []ptrace.Traces
 	traces := ptrace.NewTraces()
 	rs := traces.ResourceSpans().AppendEmpty()
 	rs.Resource().Attributes().FromRaw(nodeAttrs)
@@ -85,8 +84,7 @@ func newTraceIntAttrs(nodeAttrs map[string]interface{}, spanAttrKey string, span
 	span.SetTraceID([16]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16})
 	span.SetSpanID([8]byte{1, 2, 3, 4, 5, 6, 7, 8})
 	span.Attributes().PutInt(spanAttrKey, spanAttrValue)
-	traceBatches = append(traceBatches, traces)
 	return &TraceData{
-		ReceivedBatches: traceBatches,
+		ReceivedBatches: traces,
 	}
 }
