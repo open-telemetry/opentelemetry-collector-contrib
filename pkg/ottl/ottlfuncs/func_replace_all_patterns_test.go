c5067d22551cd595f30bb11cacb75947a9908249
Ziqi Zhao <zhaoziqi9146@gmail.com>
2022-10-06 23:49:32 +0800

    [pkg/telemetryquerylanguage] add replace_key_all_patterns function (#12991)
    * [pkg/telemetryquerylanguage] enhance replace_all_patterns function

--- pkg/ottl/ottlfuncs/func_replace_all_patterns_test.go
+++ pkg/ottl/ottlfuncs/func_replace_all_patterns_test.go
@@ -42,6 +42,7 @@ func Test_replaceAllPatterns(t *testing.T) {
 	tests := []struct {
 		name        string
 		target      ottl.GetSetter[pcommon.Map]
+		mode        string
 		pattern     string
 		replacement string
 		want        func(pcommon.Map)
@@ -49,6 +50,7 @@ func Test_replaceAllPatterns(t *testing.T) {
 		{
 			name:        "replace only matches",
 			target:      target,
+			mode:        modeValue,
 			pattern:     "hello",
 			replacement: "hello {universe}",
 			want: func(expectedMap pcommon.Map) {
@@ -60,6 +62,7 @@ func Test_replaceAllPatterns(t *testing.T) {
 		{
 			name:        "no matches",
 			target:      target,
+			mode:        modeValue,
 			pattern:     "nothing",
 			replacement: "nothing {matches}",
 			want: func(expectedMap pcommon.Map) {
@@ -71,6 +74,7 @@ func Test_replaceAllPatterns(t *testing.T) {
 		{
 			name:        "multiple regex match",
 			target:      target,
+			mode:        modeValue,
 			pattern:     `world[^\s]*(\s?)`,
 			replacement: "**** ",
 			want: func(expectedMap pcommon.Map) {
@@ -79,13 +83,52 @@ func Test_replaceAllPatterns(t *testing.T) {
 				expectedMap.PutString("test3", "goodbye **** and **** ")
 			},
 		},
+		{
+			name:        "replace only matches",
+			target:      target,
+			mode:        modeKey,
+			pattern:     "test2",
+			replacement: "foo",
+			want: func(expectedMap pcommon.Map) {
+				expectedMap.Clear()
+				expectedMap.PutString("test", "hello world")
+				expectedMap.PutString("foo", "hello")
+				expectedMap.PutString("test3", "goodbye world1 and world2")
+			},
+		},
+		{
+			name:        "no matches",
+			target:      target,
+			mode:        modeKey,
+			pattern:     "nothing",
+			replacement: "nothing {matches}",
+			want: func(expectedMap pcommon.Map) {
+				expectedMap.Clear()
+				expectedMap.PutString("test", "hello world")
+				expectedMap.PutString("test2", "hello")
+				expectedMap.PutString("test3", "goodbye world1 and world2")
+			},
+		},
+		{
+			name:        "multiple regex match",
+			target:      target,
+			mode:        modeKey,
+			pattern:     `test`,
+			replacement: "test.",
+			want: func(expectedMap pcommon.Map) {
+				expectedMap.Clear()
+				expectedMap.PutString("test.", "hello world")
+				expectedMap.PutString("test.2", "hello")
+				expectedMap.PutString("test.3", "goodbye world1 and world2")
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			scenarioMap := pcommon.NewMap()
 			input.CopyTo(scenarioMap)
 
-			exprFunc, err := ReplaceAllPatterns[pcommon.Map](tt.target, tt.pattern, tt.replacement)
+			exprFunc, err := ReplaceAllPatterns[pcommon.Map](tt.target, tt.mode, tt.pattern, tt.replacement)
 			require.NoError(t, err)
 			exprFunc(scenarioMap)
 
@@ -109,7 +152,7 @@ func Test_replaceAllPatterns_bad_input(t *testing.T) {
 		},
 	}
 
-	exprFunc, err := ReplaceAllPatterns[interface{}](target, "regexpattern", "{replacement}")
+	exprFunc, err := ReplaceAllPatterns[interface{}](target, modeValue, "regexpattern", "{replacement}")
 	assert.Nil(t, err)
 
 	exprFunc(input)
@@ -127,7 +170,7 @@ func Test_replaceAllPatterns_get_nil(t *testing.T) {
 		},
 	}
 
-	exprFunc, err := ReplaceAllPatterns[interface{}](target, "regexp", "{anything}")
+	exprFunc, err := ReplaceAllPatterns[interface{}](target, modeValue, "regexp", "{anything}")
 	require.NoError(t, err)
 	exprFunc(nil)
 }
@@ -144,8 +187,25 @@ func Test_replaceAllPatterns_invalid_pattern(t *testing.T) {
 	}
 
 	invalidRegexPattern := "*"
-	exprFunc, err := ReplaceAllPatterns[interface{}](target, invalidRegexPattern, "{anything}")
+	exprFunc, err := ReplaceAllPatterns[interface{}](target, modeValue, invalidRegexPattern, "{anything}")
 	require.Error(t, err)
 	assert.ErrorContains(t, err, "error parsing regexp:")
 	assert.Nil(t, exprFunc)
+}
+
+func Test_replaceAllPatterns_invalid_model(t *testing.T) {
+	target := &ottl.StandardGetSetter[interface{}]{
+		Getter: func(ctx interface{}) interface{} {
+			t.Errorf("nothing should be received in this scenario")
+			return nil
+		},
+		Setter: func(ctx interface{}, val interface{}) {
+			t.Errorf("nothing should be set in this scenario")
+		},
+	}
+
+	invalidMode := "invalid"
+	exprFunc, err := ReplaceAllPatterns[interface{}](target, invalidMode, "regex", "{anything}")
+	assert.Nil(t, exprFunc)
+	assert.Contains(t, err.Error(), "invalid mode")
 }
