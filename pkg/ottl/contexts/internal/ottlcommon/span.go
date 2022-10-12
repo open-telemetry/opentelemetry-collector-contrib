eb0322929434a30453a51a0725de675b5293bb9e
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2022-10-12 12:38:23 -0600

    ran make goporto

--- pkg/ottl/contexts/internal/ottlcommon/span.go
+++ pkg/ottl/contexts/internal/ottlcommon/span.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package ottlcommon // import "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/internal/ottlcommon"
+package ottlcommon // import "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/internal/ottlcommon"
 
 import (
 	"encoding/hex"
