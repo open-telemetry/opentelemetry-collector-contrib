adecf223414f4508dfea52a34edf901744363307
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2022-10-12 21:09:02 -0600

    [pkg/ottl] Change ParseStatements to return slice of pointers (#14911)
    * Change ParseStatements to return slice of pointers
    
    * Add changelog entry

--- processor/transformprocessor/internal/traces/processor.go
+++ processor/transformprocessor/internal/traces/processor.go
@@ -25,7 +25,7 @@ import (
 )
 
 type Processor struct {
-	statements []ottl.Statement[ottltraces.TransformContext]
+	statements []*ottl.Statement[ottltraces.TransformContext]
 }
 
 func NewProcessor(statements []string, functions map[string]interface{}, settings component.TelemetrySettings) (*Processor, error) {
