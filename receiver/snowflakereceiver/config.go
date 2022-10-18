176dd528031504d3a4cad7080020ea26b8a54df3
shalper2 <shalpern@splunk.com>
2022-10-18 15:18:19 -0500

    goporto

--- receiver/snowflakereceiver/config.go
+++ receiver/snowflakereceiver/config.go
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package snowflakereceiver
+package snowflakereceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/snowflakereceiver"
