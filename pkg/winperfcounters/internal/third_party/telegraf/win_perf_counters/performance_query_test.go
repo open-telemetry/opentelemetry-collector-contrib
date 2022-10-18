f90624ed0b402982230a573998bf0d8c08106aa6
Brandon Johnson <binaryfissiongames@gmail.com>
2022-10-18 19:56:34 -0400

    add test for array func

--- pkg/winperfcounters/internal/third_party/telegraf/win_perf_counters/performance_query_test.go
+++ pkg/winperfcounters/internal/third_party/telegraf/win_perf_counters/performance_query_test.go
@@ -11,12 +11,26 @@ import (
 )
 
 func TestUTF16PtrToString(t *testing.T) {
-	testPtr := (*uint16)(&utf16.Encode([]rune("Hello World\000"))[0])
-	strOut := UTF16PtrToString(testPtr)
-	require.Equal(t, "Hello World", strOut)
+	t.Run("String 'Hello World'", func(t *testing.T) {
+		testPtr := (*uint16)(&utf16.Encode([]rune("Hello World\000"))[0])
+		strOut := UTF16PtrToString(testPtr)
+		require.Equal(t, "Hello World", strOut)
+	})
+
+	t.Run("nil pointer", func(t *testing.T) {
+		strOut := UTF16PtrToString(nil)
+		require.Equal(t, "", strOut)
+	})
 }
 
-func TestUTF16PtrToString_nil(t *testing.T) {
-	strOut := UTF16PtrToString(nil)
-	require.Equal(t, "", strOut)
+func TestUTF16ToStringArray(t *testing.T) {
+	testStr := "First String\000Second String\000Final String\000\000"
+	testStrUTF16 := utf16.Encode([]rune(testStr))
+
+	strs := UTF16ToStringArray(testStrUTF16)
+	require.Equal(t, []string{
+		"First String",
+		"Second String",
+		"Final String",
+	}, strs)
 }
