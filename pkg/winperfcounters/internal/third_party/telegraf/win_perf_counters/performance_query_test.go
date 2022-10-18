bd5e78dfa29341c70b482607b5b6a5954a87d313
Brandon Johnson <binaryfissiongames@gmail.com>
2022-10-18 17:48:32 -0400

    winperfcounters pkg: avoid checkptr errors

---
+++ pkg/winperfcounters/internal/third_party/telegraf/win_perf_counters/performance_query_test.go
@@ -0,0 +1,19 @@
+package win_perf_counters
+
+import (
+	"testing"
+	"unicode/utf16"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestUTF16PtrToString(t *testing.T) {
+	testPtr := (*uint16)(&utf16.Encode([]rune("Hello World\000"))[0])
+	strOut := UTF16PtrToString(testPtr)
+	require.Equal(t, "Hello World", strOut)
+}
+
+func TestUTF16PtrToString_nil(t *testing.T) {
+	strOut := UTF16PtrToString(nil)
+	require.Equal(t, "", strOut)
+}
