05e23cd280dfb6cbdc282aac223abc9d4fda2ef4
Leighton Chen <lechen@microsoft.com>
2022-11-03 10:30:35 -0700

    [release/v1.14.x-0.35bx] Prepare release 1.14.0/0.35b0 (#1428)

--- instrumentation/opentelemetry-instrumentation-logging/src/opentelemetry/instrumentation/logging/version.py
+++ instrumentation/opentelemetry-instrumentation-logging/src/opentelemetry/instrumentation/logging/version.py
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.35b0.dev"
+__version__ = "0.35b0"
 
 _instruments = tuple()
