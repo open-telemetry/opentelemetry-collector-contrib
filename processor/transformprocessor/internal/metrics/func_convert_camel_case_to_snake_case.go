2d2f51b852638757da1c01a315eefb22d8d2c1ef
John Clark <clark.john.d@outlook.com>
2022-11-03 18:39:31 +0000

    Add initial func
    Signed-off-by: John Clark <clark.john.d@outlook.com>

---
+++ processor/transformprocessor/internal/metrics/func_convert_camel_case_to_snake_case.go
@@ -0,0 +1,94 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package metrics // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/transformprocessor/internal/metrics"
+
+import (
+	"strings"
+	"unicode"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/ottldatapoints"
+)
+
+func convertCamelCaseToSnakeCase(stringAggTemp string, monotonic bool) (ottl.ExprFunc[ottldatapoints.TransformContext], error) {
+	return func(ctx ottldatapoints.TransformContext) (interface{}, error) {
+		metric := ctx.GetMetric()
+
+		s := metric.Name()
+
+		if s == "" {
+			return nil, nil
+		}
+		if len(s) == 1 {
+			metric.SetName(strings.ToLower(s))
+			return nil, nil
+		}
+		source := []rune(s)
+		dist := strings.Builder{}
+		dist.Grow(len(s) + len(s)/3)
+		skipNext := false
+
+		for i := 0; i < len(source); i++ {
+			cur := source[i]
+
+			switch cur {
+			case '-', '_':
+				dist.WriteRune('_')
+				skipNext = true
+				continue
+			}
+			if unicode.IsLower(cur) || unicode.IsDigit(cur) {
+				dist.WriteRune(cur)
+				continue
+			}
+
+			if i == 0 {
+				dist.WriteRune(unicode.ToLower(cur))
+				continue
+			}
+
+			last := source[i-1]
+
+			if (!unicode.IsLetter(last)) || unicode.IsLower(last) {
+				if skipNext {
+					skipNext = false
+				} else {
+					dist.WriteRune('_')
+				}
+				dist.WriteRune(unicode.ToLower(cur))
+				continue
+			}
+
+			if i < len(source)-1 {
+				next := source[i+1]
+				if unicode.IsLower(next) {
+					if skipNext {
+						skipNext = false
+					} else {
+						dist.WriteRune('_')
+					}
+					dist.WriteRune(unicode.ToLower(cur))
+					continue
+				}
+			}
+
+			dist.WriteRune(unicode.ToLower(cur))
+		}
+
+		metric.SetName(dist.String())
+
+		return nil, nil
+	}, nil
+}
--- processor/transformprocessor/internal/metrics/functions.go
+++ processor/transformprocessor/internal/metrics/functions.go
@@ -25,6 +25,7 @@ var registry = map[string]interface{}{
 	"convert_gauge_to_sum":             convertGaugeToSum,
 	"convert_summary_sum_val_to_sum":   convertSummarySumValToSum,
 	"convert_summary_count_val_to_sum": convertSummaryCountValToSum,
+	"convert_camel_case_to_snake_case": convertCamelCaseToSnakeCase,
 }
 
 func init() {
