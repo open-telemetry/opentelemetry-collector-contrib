bfc10074e9f452f8d2f7869335dd03fff6c8f40c
John Clark <clark.john.d@outlook.com>
2022-11-08 23:08:42 +0000

    Add logs tests
    Signed-off-by: John Clark <clark.john.d@outlook.com>

--- processor/transformprocessor/internal/logs/processor_test.go
+++ processor/transformprocessor/internal/logs/processor_test.go
@@ -193,6 +193,30 @@ func TestProcess(t *testing.T) {
 			statement: `set(attributes["test"], Split(attributes["not_exist"], "|"))`,
 			want:      func(td plog.Logs) {},
 		},
+		{
+			statement: `set(attributes["test"], ConvertCase(name, "lower")) where name == "operationA""`,
+			want: func(td plog.Logs) {
+				td.ResourceLogs().At(0).ScopeLogs().At(0).LogRecords().At(0).Attributes().PutStr("test", "operationa")
+			},
+		},
+		{
+			statement: `set(attributes["test"], ConvertCase(name, "upper")) where name == "operationA"`,
+			want: func(td plog.Logs) {
+				td.ResourceLogs().At(0).ScopeLogs().At(0).LogRecords().At(0).Attributes().PutStr("test", "OPERATIONA")
+			},
+		},
+		{
+			statement: `set(attributes["test"], ConvertCase(name, "snake")) where name == "operationA"`,
+			want: func(td plog.Logs) {
+				td.ResourceLogs().At(0).ScopeLogs().At(0).LogRecords().At(0).Attributes().PutStr("test", "operation_a")
+			},
+		},
+		{
+			statement: `set(attributes["test"], ConvertCase(name, "camel")) where name == "operationA"`,
+			want: func(td plog.Logs) {
+				td.ResourceLogs().At(0).ScopeLogs().At(0).LogRecords().At(0).Attributes().PutStr("test", "OperationA")
+			},
+		},
 	}
 
 	for _, tt := range tests {
