8223d03f981451f167ab9d4bb5abf798d031ca76
Khyati Gandhi <khyatigandhi6@gmail.com>
2022-11-21 13:35:31 +0530

    Add Logicmonitor Exporter overall structure

---
+++ exporter/logicmonitorexporter/config_test.go
@@ -0,0 +1,108 @@
+// Copyright  The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package logicmonitorexporter
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/collector/config/confighttp"
+)
+
+func TestConfigValidation(t *testing.T) {
+	testcases := []struct {
+		name         string
+		cfg          *Config
+		wantErr      bool
+		errorMessage string
+	}{
+		{
+			name: "empty endpoint",
+			cfg: &Config{
+				HTTPClientSettings: confighttp.HTTPClientSettings{
+					Endpoint: "",
+				},
+				LogBatchingInterval: 200 * time.Millisecond,
+			},
+			wantErr:      true,
+			errorMessage: "Endpoint should not be empty",
+		},
+		{
+			name: "missing http scheme",
+			cfg: &Config{
+				HTTPClientSettings: confighttp.HTTPClientSettings{
+					Endpoint: "test.com/dummy",
+				},
+				LogBatchingInterval: 200 * time.Millisecond,
+			},
+			wantErr:      true,
+			errorMessage: "Endpoint must be valid",
+		},
+		{
+			name: "invalid endpoint format",
+			cfg: &Config{
+				HTTPClientSettings: confighttp.HTTPClientSettings{
+					Endpoint: "invalid.com@#$%",
+				},
+				LogBatchingInterval: 200 * time.Millisecond,
+			},
+			wantErr:      true,
+			errorMessage: "Endpoint must be valid",
+		},
+		{
+			name: "minimum batching interval",
+			cfg: &Config{
+				HTTPClientSettings: confighttp.HTTPClientSettings{
+					Endpoint: "http://validurl.com/rest",
+				},
+				LogBatchingInterval: 20 * time.Millisecond,
+			},
+			wantErr:      true,
+			errorMessage: "Minimum log batching interval should be 30ms",
+		},
+		{
+			name: "valid config",
+			cfg: &Config{
+				HTTPClientSettings: confighttp.HTTPClientSettings{
+					Endpoint: "http://validurl.com/rest",
+				},
+				LogBatchingInterval: 200 * time.Millisecond,
+			},
+			wantErr:      false,
+			errorMessage: "",
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.cfg.Validate()
+			if (err != nil) != tc.wantErr {
+				t.Errorf("config validation failed: error = %v, wantErr %v", err, tc.wantErr)
+				return
+			}
+			if tc.wantErr {
+				assert.Error(t, err)
+				if len(tc.errorMessage) != 0 {
+					assert.Equal(t, errors.New(tc.errorMessage), err, "Error messages must match")
+				}
+				return
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
---
+++ exporter/logicmonitorexporter/factory.go
@@ -0,0 +1,98 @@
+// Copyright  The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package logicmonitorexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/logicmonitorexporter"
+
+import (
+	"context"
+	"time"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/config"
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/exporter/exporterhelper"
+)
+
+const (
+	// The value of "type" key in configuration.
+	typeStr = "logicmonitor"
+	// The stability level of the exporter.
+	stability = component.StabilityLevelBeta
+	// Default values for batching of logs
+	defaultLogBatchingEnabled  = true
+	defaultLogBatchingInterval = 10 * time.Second
+)
+
+// NewFactory creates a LogicMonitor exporter factory
+func NewFactory() component.ExporterFactory {
+	return component.NewExporterFactory(
+		typeStr,
+		createDefaultConfig,
+		component.WithTracesExporter(createTracesExporter, stability),
+		component.WithLogsExporter(createLogsExporter, stability),
+	)
+}
+
+func createDefaultConfig() component.ExporterConfig {
+	return &Config{
+		ExporterSettings:    config.NewExporterSettings(component.NewID(typeStr)),
+		LogBatchingEnabled:  defaultLogBatchingEnabled,
+		LogBatchingInterval: defaultLogBatchingInterval,
+	}
+}
+
+func createTracesExporter(
+	ctx context.Context,
+	set component.ExporterCreateSettings,
+	c component.ExporterConfig,
+) (component.TracesExporter, error) {
+	cfg := c.(*Config)
+	// TODO: Lines commented out until implementation is available
+	// lmexpCfg, err := newTracesExporter(cfg, set)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	var pushConvertedTraces consumer.ConsumeTracesFunc
+
+	return exporterhelper.NewTracesExporter(
+		ctx,
+		set,
+		cfg,
+		// TODO: Lines commented out until implementation is available
+		// lmexpCfg.pushTraces,
+		pushConvertedTraces,
+		exporterhelper.WithCapabilities(consumer.Capabilities{MutatesData: false}),
+		exporterhelper.WithRetry(cfg.RetrySettings),
+		exporterhelper.WithQueue(cfg.QueueSettings))
+}
+
+func createLogsExporter(ctx context.Context, set component.ExporterCreateSettings, cfg component.ExporterConfig) (component.LogsExporter, error) {
+	// TODO: Lines commented out until implementation is available
+	// lmexpCfg, err := newLogsExporter(cfg, set.Logger)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	c := cfg.(*Config)
+	var pushConvertedLogs consumer.ConsumeLogsFunc
+	return exporterhelper.NewLogsExporter(
+		ctx,
+		set,
+		cfg,
+		// TODO: Lines commented out until implementation is available
+		// lmexpCfg.PushLogData,
+		pushConvertedLogs,
+		exporterhelper.WithQueue(c.QueueSettings),
+		exporterhelper.WithRetry(c.RetrySettings),
+	)
+}
