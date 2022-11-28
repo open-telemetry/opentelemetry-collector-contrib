d1defec3899abb907ff71998f3e46245a7d871e8
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2022-11-28 16:36:19 -0700

    Apply feedback

--- pkg/ottl/ottlfuncs/func_parse_json.go
+++ pkg/ottl/ottlfuncs/func_parse_json.go
@@ -16,14 +16,21 @@ package ottlfuncs // import "github.com/open-telemetry/opentelemetry-collector-c
 
 import (
 	"context"
-	"fmt"
-
 	jsoniter "github.com/json-iterator/go"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl"
 )
 
+// ParseJSON factory function returns a `pcommon.Map` struct that is a result of parsing the target string as JSON
+// Each JSON type is converted into a `pdata.Value` using the following map:
+//
+//	JSON boolean -> bool
+//	JSON number  -> float64
+//	JSON string  -> string
+//	JSON null    -> nil
+//	JSON arrays  -> pdata.SliceValue
+//	JSON objects -> map[string]any
 func ParseJSON[K any](target ottl.Getter[K]) (ottl.ExprFunc[K], error) {
 	return func(ctx context.Context, tCtx K) (interface{}, error) {
 		targetVal, err := target.Get(ctx, tCtx)
@@ -38,52 +45,14 @@ func ParseJSON[K any](target ottl.Getter[K]) (ottl.ExprFunc[K], error) {
 			}
 			result := pcommon.NewMap()
 			for k, v := range parsedValue {
-				attrVal := pcommon.NewValueEmpty()
-				err = setValue(attrVal, v)
+				attrVal := result.PutEmpty(k)
+				err = attrVal.FromRaw(v)
 				if err != nil {
 					return pcommon.Map{}, err
 				}
-				attrVal.CopyTo(result.PutEmpty(k))
 			}
 			return result, nil
 		}
 		return nil, nil
 	}, nil
-}
-
-func setValue(value pcommon.Value, val interface{}) error {
-	switch v := val.(type) {
-	case string:
-		value.SetStr(v)
-	case bool:
-		value.SetBool(v)
-	case float64:
-		value.SetDouble(v)
-	case nil:
-	case []interface{}:
-		emptySlice := value.SetEmptySlice()
-		err := setSlice(emptySlice, v)
-		if err != nil {
-			return err
-		}
-	case map[string]interface{}:
-		err := value.SetEmptyMap().FromRaw(v)
-		if err != nil {
-			return err
-		}
-	default:
-		return fmt.Errorf("unknown type, %T", v)
-	}
-	return nil
-}
-
-func setSlice(slice pcommon.Slice, value []interface{}) error {
-	for _, item := range value {
-		emptyValue := slice.AppendEmpty()
-		err := setValue(emptyValue, item)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
 }
