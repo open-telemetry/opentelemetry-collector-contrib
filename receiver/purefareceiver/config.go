2ebcc031dca97e850f5e30e1aed113198580ae76
Diego Amaral <diego-ii@live.com>
2022-11-28 16:49:01 -0300

    [receiver/purefa] Bootstrap component (#14969)

---
+++ receiver/purefareceiver/config.go
@@ -0,0 +1,56 @@
+// Copyright 2022 The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package purefareceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/purefareceiver"
+
+import (
+	"time"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/config"
+	"go.opentelemetry.io/collector/config/configauth"
+	"go.opentelemetry.io/collector/config/confighttp"
+)
+
+var _ component.ReceiverConfig = (*Config)(nil)
+
+// Config relating to Array Metric Scraper.
+type Config struct {
+	config.ReceiverSettings       `mapstructure:",squash"` // squash ensures fields are correctly decoded in embedded struct
+	confighttp.HTTPClientSettings `mapstructure:",squash"`
+
+	// Settings contains settings for the individual scrapers
+	Settings *Settings `mapstructure:"settings"`
+
+	// Arrays represents the list of arrays to query
+	Arrays []Array `mapstructure:"arrays"`
+}
+
+type Array struct {
+	Address string                    `mapstructure:"address"`
+	Auth    configauth.Authentication `mapstructure:"auth"`
+}
+
+type Settings struct {
+	ReloadIntervals *ReloadIntervals `mapstructure:"reload_intervals"`
+}
+
+type ReloadIntervals struct {
+	Array time.Duration `mapstructure:"array"`
+}
+
+func (c *Config) Validate() error {
+	// TODO(dgoscn): perform config validation
+	return nil
+}
