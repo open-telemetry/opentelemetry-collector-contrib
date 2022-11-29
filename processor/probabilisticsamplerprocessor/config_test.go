bb5a0a2551a89b3725285d54f73ed947a76a3ab4
Antoine Toulme <antoine@lunar-ocean.com>
2022-11-29 09:15:06 -0800

    [processor/probabilisticsampler] Log sampling (#14920)
    Add logs support
    
    Co-authored-by: harshit-splunk <hvaghani@splunk.com>

--- processor/probabilisticsamplerprocessor/config_test.go
+++ processor/probabilisticsamplerprocessor/config_test.go
@@ -21,13 +21,14 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/component/componenttest"
 	"go.opentelemetry.io/collector/config"
 	"go.opentelemetry.io/collector/confmap/confmaptest"
+	"go.opentelemetry.io/collector/service/servicetest"
 )
 
 func TestLoadConfig(t *testing.T) {
 	t.Parallel()
-
 	tests := []struct {
 		id       component.ID
 		expected component.Config
@@ -38,11 +39,19 @@ func TestLoadConfig(t *testing.T) {
 				ProcessorSettings:  config.NewProcessorSettings(component.NewID(typeStr)),
 				SamplingPercentage: 15.3,
 				HashSeed:           22,
+				AttributeSource:    "traceID",
 			},
 		},
 		{
-			id:       component.NewIDWithName(typeStr, "empty"),
-			expected: createDefaultConfig(),
+			id: component.NewIDWithName(typeStr, "logs"),
+			expected: &Config{
+				ProcessorSettings:  config.NewProcessorSettings(component.NewIDWithName(typeStr, "logs")),
+				SamplingPercentage: 15.3,
+				HashSeed:           22,
+				AttributeSource:    "record",
+				FromAttribute:      "foo",
+				SamplingPriority:   "bar",
+			},
 		},
 	}
 
@@ -50,16 +59,30 @@ func TestLoadConfig(t *testing.T) {
 		t.Run(tt.id.String(), func(t *testing.T) {
 			cm, err := confmaptest.LoadConf(filepath.Join("testdata", "config.yaml"))
 			require.NoError(t, err)
+			processors, err := cm.Sub("processors")
+			require.NoError(t, err)
 
 			factory := NewFactory()
 			cfg := factory.CreateDefaultConfig()
 
-			sub, err := cm.Sub(tt.id.String())
+			sub, err := processors.Sub(tt.id.String())
 			require.NoError(t, err)
 			require.NoError(t, component.UnmarshalConfig(sub, cfg))
+			cfg.SetIDName(tt.id.Name())
 
 			assert.NoError(t, component.ValidateConfig(cfg))
 			assert.Equal(t, tt.expected, cfg)
 		})
 	}
+}
+
+func TestLoadInvalidConfig(t *testing.T) {
+	factories, err := componenttest.NopFactories()
+	require.NoError(t, err)
+
+	factory := NewFactory()
+	factories.Processors[typeStr] = factory
+
+	_, err = servicetest.LoadConfigAndValidate(filepath.Join("testdata", "invalid.yaml"), factories)
+	require.ErrorContains(t, err, "negative sampling rate: -15.30")
 }
