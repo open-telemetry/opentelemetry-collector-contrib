5b552efe32e6e9d751d726e3b7ce6b90b9fbbd5a
Willian Carvalho <willian.carvalho@ibm.com>
2022-11-29 21:17:53 +0100

    [exporter/instana] Add tests for explicit CA file (#14750)
    * Added new config option ca_file
    
    * added extra tests
    
    * added chloggen file
    
    * updated README.md
    
    * provided right config name for the test
    
    * applied requested changes
    
    * using the collector CA utilities instead of our own
    
    * Updated README.md
    
    * removed unnecessary tls config
    
    * removed changelog
    
    * fixed deprecated method call
    
    * removed line between imported packages

--- exporter/instanaexporter/README.md
+++ exporter/instanaexporter/README.md
@@ -17,8 +17,10 @@ The following exporter configuration parameters are supported.
 |----------------|-------------|
 | endpoint | The Instana backend endpoint that the Exporter connects to. It depends on your region and how it is hosted. It starts with ``https://serverless-`` for SaaS. Otherwise, it starts with ``https://``. It corresponds to the Instana environment variable ``INSTANA_ENDPOINT_URL`` |
 | agent_key      | Your Instana Agent key. The same agent key can be used for host agents and serverless monitoring. It corresponds to the Instana environment variable ``INSTANA_AGENT_KEY`` |
+| tls/ca_file    | [Optional] Certificate authority file for an Instana backend connection where the backend uses a self signed certificate. |
 
 > These parameters match the Instana Serverless Monitoring environment variables and can be found [here](https://www.ibm.com/docs/en/instana-observability/current?topic=references-environment-variables#serverless-monitoring).
+
 
 ### Sample Configuration
 
@@ -57,9 +59,10 @@ exporters:
   logging:
     loglevel: debug
   instana:
-    loglevel: debug
     endpoint: ${INSTANA_ENDPOINT_URL}
     agent_key: ${INSTANA_AGENT_KEY}
+    tls:
+      ca_file: someCA.pem # Optional. Certificate authority file for Instana backend connection.
 
 service:
   pipelines:
--- exporter/instanaexporter/exporter_test.go
+++ exporter/instanaexporter/exporter_test.go
@@ -16,8 +16,11 @@ package instanaexporter
 
 import (
 	"context"
+	"encoding/base64"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -25,6 +28,7 @@ import (
 	"go.opentelemetry.io/collector/component/componenttest"
 	"go.opentelemetry.io/collector/config"
 	"go.opentelemetry.io/collector/config/confighttp"
+	"go.opentelemetry.io/collector/config/configtls"
 	"go.opentelemetry.io/collector/pdata/ptrace"
 )
 
@@ -58,4 +62,81 @@ func newTestTraces() ptrace.Traces {
 	span.SetTraceID([16]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4})
 	span.SetSpanID([8]byte{0, 0, 0, 0, 1, 2, 3, 4})
 	return traces
+}
+
+func TestSelfSignedBackend(t *testing.T) {
+	var err error
+	caFile := "testdata/ca.crt"
+	handler := http.NewServeMux()
+	handler.HandleFunc("/bundle", func(w http.ResponseWriter, r *http.Request) {
+		_, err = io.WriteString(w, "Hello from CA self signed server")
+
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	server := httptest.NewTLSServer(handler)
+	defer server.Close()
+
+	s := base64.StdEncoding.EncodeToString(server.Certificate().Raw)
+	wholeCert := "-----BEGIN CERTIFICATE-----\n" + s + "\n-----END CERTIFICATE-----"
+
+	err = os.WriteFile(caFile, []byte(wholeCert), os.FileMode(0600))
+	defer func() {
+		assert.NoError(t, os.Remove(caFile))
+	}()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Starts the exporter to test the HTTP client request
+
+	cfg := Config{
+		AgentKey: "key11",
+		HTTPClientSettings: confighttp.HTTPClientSettings{
+			Endpoint: server.URL,
+			TLSSetting: configtls.TLSClientSetting{
+				TLSSetting: configtls.TLSSetting{
+					CAFile: caFile,
+				},
+			},
+		},
+		Endpoint:         server.URL,
+		ExporterSettings: config.NewExporterSettings(component.NewIDWithName(typeStr, "valid")),
+	}
+
+	ctx := context.Background()
+
+	instanaExporter := newInstanaExporter(&cfg, componenttest.NewNopExporterCreateSettings())
+	err = instanaExporter.start(ctx, componenttest.NewNopHost())
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.NoError(t, instanaExporter.export(ctx, server.URL, make(map[string]string), []byte{}))
+}
+
+func TestSelfSignedBackendCAFileNotFound(t *testing.T) {
+	cfg := Config{
+		AgentKey: "key11",
+		HTTPClientSettings: confighttp.HTTPClientSettings{
+			Endpoint: "",
+			TLSSetting: configtls.TLSClientSetting{
+				TLSSetting: configtls.TLSSetting{
+					CAFile: "ca_file_not_found.pem",
+				},
+			},
+		},
+		Endpoint:         "",
+		ExporterSettings: config.NewExporterSettings(component.NewIDWithName(typeStr, "valid")),
+	}
+
+	ctx := context.Background()
+
+	instanaExporter := newInstanaExporter(&cfg, componenttest.NewNopExporterCreateSettings())
+
+	assert.Error(t, instanaExporter.start(ctx, componenttest.NewNopHost()), "expect not to find the ca file")
 }
