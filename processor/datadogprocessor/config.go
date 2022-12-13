20119b1b007df6b79c68088949089ea9531f7136
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2022-12-13 12:02:48 +0200

    [processor/datadog]: add datadogprocessor (#16853)
    * processor: add datadogprocessor
    
    This change adds the processor described in #15689. It is the initial PR
    containing the structure and implementation.
    
    * Address PR comments
    
    * go mod tidy
    
    * Ensure Shutdown can be called even if Start fails
    
    * Use component.ID
    
    * Address PR comments
    
    * Update linting errors
    
    * make generate-gh-issue-templates

---
+++ processor/datadogprocessor/config.go
@@ -0,0 +1,35 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package datadogprocessor // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/datadogprocessor"
+
+import (
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/config"
+)
+
+// Config defines the configuration options for datadogprocessor.
+type Config struct {
+	config.ProcessorSettings `mapstructure:",squash"` // squash ensures fields are correctly decoded in embedded struct
+
+	// MetricsExporter specifies the name of the metrics exporter to be used when
+	// exporting stats metrics.
+	MetricsExporter component.ID `mapstructure:"metrics_exporter"`
+}
+
+func createDefaultConfig() component.Config {
+	return &Config{
+		MetricsExporter: component.NewID(component.Type("datadog")),
+	}
+}
