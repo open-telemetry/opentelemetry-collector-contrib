894219d4c96b915a275a4a29de7986966f99c739
Naama Bendalak <naama@Naamas-MacBook-Pro.local>
2022-12-27 12:46:41 +0200

    enabled proccessmetrics for MacOS and added a custom simple config file to use.

--- receiver/hostmetricsreceiver/internal/scraper/processscraper/process_scraper_others.go
+++ receiver/hostmetricsreceiver/internal/scraper/processscraper/process_scraper_others.go
@@ -21,17 +21,40 @@ import (
 	"github.com/shirou/gopsutil/v3/cpu"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/hostmetricsreceiver/internal/scraper/processscraper/ucal"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/hostmetricsreceiver/internal/scraper/processscraper/internal/metadata"
 )
 
-func (s *scraper) recordCPUTimeMetric(now pcommon.Timestamp, cpuTime *cpu.TimesStat) {}
-
-func (s *scraper) recordCPUUtilization(now pcommon.Timestamp, cpuUtilization ucal.CPUUtilization) {}
+func (s *scraper) recordCPUTimeMetric(now pcommon.Timestamp, cpuTime *cpu.TimesStat) {
+	s.mb.RecordProcessCPUTimeDataPoint(now, cpuTime.User, metadata.AttributeStateUser)
+	s.mb.RecordProcessCPUTimeDataPoint(now, cpuTime.System, metadata.AttributeStateSystem)
+	s.mb.RecordProcessCPUTimeDataPoint(now, cpuTime.Iowait, metadata.AttributeStateWait)
+}
 
 func getProcessExecutable(processHandle) (*executableMetadata, error) {
-	return nil, nil
+	// attempting to get the process name 
+	name, err := proc.Name()
+	if err != nil {
+		return nil, err
+	}
+
+	// attampting to get the process path
+	exe, err := proc.Exe()
+	if err != nil {
+		return nil, err
+	}
+
+	// managed to get the info
+	executable := &executableMetadata{name: name, path: exe}
+	return executable, nil
 }
 
 func getProcessCommand(processHandle) (*commandMetadata, error) {
-	return nil, nil
+	cmdline, err := proc.Cmdline()
+	if err != nil {
+		return nil, err
+	}
+
+	command := &commandMetadata{command: cmdline, commandLine: cmdline}
+	return command, nil
+
 }
