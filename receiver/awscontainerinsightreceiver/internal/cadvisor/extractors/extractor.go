99c7283f2332cdb708bff32679e69acc856ff0ab
Khanh Nguyen <khanhntd@amazon.com>
2022-12-30 14:19:03 -0500

    Using GoImport to fix linter

--- receiver/awscontainerinsightreceiver/internal/cadvisor/extractors/extractor.go
+++ receiver/awscontainerinsightreceiver/internal/cadvisor/extractors/extractor.go
@@ -19,9 +19,10 @@ import (
 	"time"
 
 	cinfo "github.com/google/cadvisor/info/v1"
+	"go.uber.org/zap"
+
 	ci "github.com/open-telemetry/opentelemetry-collector-contrib/internal/aws/containerinsight"
 	awsmetrics "github.com/open-telemetry/opentelemetry-collector-contrib/internal/aws/metrics"
-	"go.uber.org/zap"
 )
 
 func GetStats(info *cinfo.ContainerInfo) *cinfo.ContainerStats {
