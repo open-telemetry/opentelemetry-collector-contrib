1504142fc01fd955b37d649eef264448fca347f8
Antoine Toulme <antoine@lunar-ocean.com>
2023-01-02 11:09:38 -0800

    [exporter/signalfxexporter] Add all HTTP client settings to the configuration (#16837)
    Expose all HTTP client settings to the exporter configuration so they may be set in addition to the existing flags.

--- exporter/signalfxexporter/dpclient.go
+++ exporter/signalfxexporter/dpclient.go
@@ -148,27 +148,6 @@ func (s *sfxDPClient) pushMetricsDataForToken(ctx context.Context, sfxDataPoints
 	return 0, nil
 }
 
-func buildHeaders(config *Config) map[string]string {
-	headers := map[string]string{
-		"Connection":   "keep-alive",
-		"Content-Type": "application/x-protobuf",
-		"User-Agent":   "OpenTelemetry-Collector SignalFx Exporter/v0.0.1",
-	}
-
-	if config.AccessToken != "" {
-		headers[splunk.SFxAccessTokenHeader] = config.AccessToken
-	}
-
-	// Add any custom headers from the config. They will override the pre-defined
-	// ones above in case of conflict, but, not the content encoding one since
-	// the latter one is defined according to the payload.
-	for k, v := range config.Headers {
-		headers[k] = v
-	}
-
-	return headers
-}
-
 func (s *sfxDPClient) encodeBody(dps []*sfxpb.DataPoint) (bodyReader io.Reader, compressed bool, err error) {
 	msg := sfxpb.DataPointUploadMessage{
 		Datapoints: dps,
--- exporter/signalfxexporter/factory.go
+++ exporter/signalfxexporter/factory.go
@@ -21,6 +21,7 @@ import (
 	"time"
 
 	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/config/confighttp"
 	"go.opentelemetry.io/collector/confmap"
 	"go.opentelemetry.io/collector/exporter"
 	"go.opentelemetry.io/collector/exporter/exporterhelper"
@@ -56,9 +57,9 @@ func NewFactory() exporter.Factory {
 
 func createDefaultConfig() component.Config {
 	return &Config{
-		TimeoutSettings: exporterhelper.TimeoutSettings{Timeout: defaultHTTPTimeout},
-		RetrySettings:   exporterhelper.NewDefaultRetrySettings(),
-		QueueSettings:   exporterhelper.NewDefaultQueueSettings(),
+		RetrySettings:      exporterhelper.NewDefaultRetrySettings(),
+		QueueSettings:      exporterhelper.NewDefaultQueueSettings(),
+		HTTPClientSettings: confighttp.HTTPClientSettings{Timeout: defaultHTTPTimeout},
 		AccessTokenPassthroughConfig: splunk.AccessTokenPassthroughConfig{
 			AccessTokenPassthrough: true,
 		},
@@ -77,17 +78,17 @@ func createTracesExporter(
 	cfg := eCfg.(*Config)
 	corrCfg := cfg.Correlation
 
-	if corrCfg.Endpoint == "" {
+	if corrCfg.HTTPClientSettings.Endpoint == "" {
 		apiURL, err := cfg.getAPIURL()
 		if err != nil {
 			return nil, fmt.Errorf("unable to create API URL: %w", err)
 		}
-		corrCfg.Endpoint = apiURL.String()
+		corrCfg.HTTPClientSettings.Endpoint = apiURL.String()
 	}
 	if cfg.AccessToken == "" {
 		return nil, errors.New("access_token is required")
 	}
-	set.Logger.Info("Correlation tracking enabled", zap.String("endpoint", corrCfg.Endpoint))
+	set.Logger.Info("Correlation tracking enabled", zap.String("endpoint", corrCfg.HTTPClientSettings.Endpoint))
 	tracker := correlation.NewTracker(corrCfg, cfg.AccessToken, set)
 
 	return exporterhelper.NewTracesExporter(
@@ -112,7 +113,7 @@ func createMetricsExporter(
 		return nil, err
 	}
 
-	exp, err := newSignalFxExporter(cfg, set.Logger)
+	exp, err := newSignalFxExporter(cfg, set)
 	if err != nil {
 		return nil, err
 	}
@@ -125,7 +126,8 @@ func createMetricsExporter(
 		// explicitly disable since we rely on http.Client timeout logic.
 		exporterhelper.WithTimeout(exporterhelper.TimeoutSettings{Timeout: 0}),
 		exporterhelper.WithRetry(cfg.RetrySettings),
-		exporterhelper.WithQueue(cfg.QueueSettings))
+		exporterhelper.WithQueue(cfg.QueueSettings),
+		exporterhelper.WithStart(exp.start))
 
 	if err != nil {
 		return nil, err
@@ -189,7 +191,7 @@ func createLogsExporter(
 ) (exporter.Logs, error) {
 	expCfg := cfg.(*Config)
 
-	exp, err := newEventExporter(expCfg, set.Logger)
+	exp, err := newEventExporter(expCfg, set)
 	if err != nil {
 		return nil, err
 	}
@@ -202,7 +204,8 @@ func createLogsExporter(
 		// explicitly disable since we rely on http.Client timeout logic.
 		exporterhelper.WithTimeout(exporterhelper.TimeoutSettings{Timeout: 0}),
 		exporterhelper.WithRetry(expCfg.RetrySettings),
-		exporterhelper.WithQueue(expCfg.QueueSettings))
+		exporterhelper.WithQueue(expCfg.QueueSettings),
+		exporterhelper.WithStart(exp.startLogs))
 
 	if err != nil {
 		return nil, err
