5d24df4f9ad93ba2652c322185019e684829a608
Antoine Toulme <antoine@lunar-ocean.com>
2023-01-05 18:38:00 -0800

    add more checks to guarantee precedence

--- exporter/signalfxexporter/exporter.go
+++ exporter/signalfxexporter/exporter.go
@@ -230,9 +230,12 @@ func (se *signalfxExporter) startLogs(_ context.Context, host component.Host) er
 func (se *signalfxExporter) createClient(host component.Host) (*http.Client, error) {
 	se.config.HTTPClientSettings.TLSSetting = se.config.IngestTLSSettings
 
-	if se.config.MaxConnections != 0 {
-		se.logger.Warn("You are using the deprecated `max_connections` option that will be removed soon; use `max_idle_conns` and `max_idle_conns_per_host` instead: https://github.com/open-telemetry/opentelemetry-collector-contrib/tree/main/exporter/signalfxexporter#advanced-configuration")
+	if se.config.HTTPClientSettings.MaxIdleConns == nil && se.config.MaxConnections != 0 {
+		se.logger.Warn("You are using the deprecated `max_connections` option that will be removed soon; use `max_idle_conns` instead: https://github.com/open-telemetry/opentelemetry-collector-contrib/tree/main/exporter/signalfxexporter#advanced-configuration")
 		se.config.HTTPClientSettings.MaxIdleConns = &se.config.MaxConnections
+	}
+	if se.config.HTTPClientSettings.MaxIdleConnsPerHost == nil && se.config.MaxConnections != 0 {
+		se.logger.Warn("You are using the deprecated `max_connections` option that will be removed soon; use `max_idle_conns_per_host` instead: https://github.com/open-telemetry/opentelemetry-collector-contrib/tree/main/exporter/signalfxexporter#advanced-configuration")
 		se.config.HTTPClientSettings.MaxIdleConnsPerHost = &se.config.MaxConnections
 	}
 	if se.config.HTTPClientSettings.IdleConnTimeout == nil {
