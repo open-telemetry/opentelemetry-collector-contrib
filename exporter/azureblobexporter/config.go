0803ed58df8c2d50bd855dfbb4afe21bb16bc2e9
Eugene Fedorenko <eugene.fedor@gmail.com>
2023-01-06 16:36:56 -0800

    Update exporter/azureblobexporter/config.go
    Co-authored-by: Antoine Toulme <antoine@toulme.name>

--- exporter/azureblobexporter/config.go
+++ exporter/azureblobexporter/config.go
@@ -15,7 +15,7 @@
 package azureblobexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/azureblobexporter"
 
 // Config defines configuration for Azure Monitor
-type Config struct {
+type config struct {
 	// Azure Blob Storage connection key,
 	// which can be found in the Azure Blob Storage resource on the Azure Portal. (no default)
 	ConnectionString string `mapstructure:"connection_string"`
