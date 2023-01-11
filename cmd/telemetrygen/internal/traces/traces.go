5839afd569b389d564fb3e76d338f15d3c9c82be
Stanley Liu <stanley.liu@datadoghq.com>
2023-01-11 11:59:34 -0500

    Move tracegen functionality to telemetrygen traces subcommand (#16358)
    - Copies tracegen functionality to an internal package in telemetrygen
    - Moves telemetrygen config from internal to main package in order to import internal traces package
    - Copies existing Github actions from tracegen for telemetrygen
    - Marks tracegen module as deprecated
    
    Link to tracking Issue: #9597
    
    Co-authored-by: Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>

--- cmd/telemetrygen/internal/traces/traces.go
+++ cmd/telemetrygen/internal/traces/traces.go
@@ -13,11 +13,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
+package traces
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"time"
 
@@ -32,26 +31,16 @@ import (
 	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
-
-	"github.com/open-telemetry/opentelemetry-collector-contrib/cmd/tracegen/internal/tracegen"
 )
 
-func main() {
-	fs := flag.CommandLine
-	cfg := new(tracegen.Config)
-	cfg.Flags(fs)
-	flag.Parse()
-
+func Start(cfg *Config) error {
 	logger, err := zap.NewDevelopment()
 	if err != nil {
-		panic(fmt.Sprintf("failed to obtain logger: %v", err))
+		return fmt.Errorf("failed to obtain logger: %w", err)
 	}
 	grpcZap.ReplaceGrpcLoggerV2(logger.WithOptions(
 		zap.AddCallerSkip(3),
 	))
-
-	// Deprecation warning
-	logger.Warn("tracegen has been deprecated, use github.com/open-telemetry/opentelemetry-collector-contrib/cmd/telemetrygen instead")
 
 	grpcExpOpt := []otlptracegrpc.Option{
 		otlptracegrpc.WithEndpoint(cfg.Endpoint),
@@ -85,24 +74,32 @@ func main() {
 
 	if err != nil {
 		logger.Error("failed to obtain OTLP exporter", zap.Error(err))
-		return
+		return err
 	}
 	defer func() {
 		logger.Info("stopping the exporter")
-		if err = exp.Shutdown(context.Background()); err != nil {
-			logger.Error("failed to stop the exporter", zap.Error(err))
+		if tempError := exp.Shutdown(context.Background()); tempError != nil {
+			logger.Error("failed to stop the exporter", zap.Error(tempError))
+			err = tempError
 			return
 		}
 	}()
+	if err != nil {
+		return err
+	}
 
 	ssp := sdktrace.NewBatchSpanProcessor(exp, sdktrace.WithBatchTimeout(time.Second))
 	defer func() {
 		logger.Info("stop the batch span processor")
-		if err := ssp.Shutdown(context.Background()); err != nil {
+		if tempError := ssp.Shutdown(context.Background()); tempError != nil {
 			logger.Error("failed to stop the batch span processor", zap.Error(err))
+			err = tempError
 			return
 		}
 	}()
+	if err != nil {
+		return err
+	}
 
 	var attributes []attribute.KeyValue
 	// may be overridden by `-otlp-attributes service.name="foo"`
@@ -121,7 +118,10 @@ func main() {
 	tracerProvider.RegisterSpanProcessor(ssp)
 	otel.SetTracerProvider(tracerProvider)
 
-	if err := tracegen.Run(cfg, logger); err != nil {
+	if err = Run(cfg, logger); err != nil {
 		logger.Error("failed to stop the exporter", zap.Error(err))
+		return err
 	}
+
+	return nil
 }
