a55a2c1391eb945c909ca1337e21497ba674cf54
Alex Burck <aburck@imo-online.com>
2023-01-16 19:53:14 -0600

    Add license and fmt

--- processor/resourcedetectionprocessor/internal/aws/lambda/lambda.go
+++ processor/resourcedetectionprocessor/internal/aws/lambda/lambda.go
@@ -1,3 +1,17 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package lambda // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/resourcedetectionprocessor/internal/aws/lambda"
 
 import (
@@ -16,17 +30,17 @@ const (
 	TypeStr = "lambda"
 
 	// Environment variables that are set when running on AWS Lambda.
-	awsRegionEnvVar = "AWS_REGION"
-	awsLambdaFunctionNameEnvVar = "AWS_LAMBDA_FUNCTION_NAME"
-	awsLambdaFunctionVersionEnvVar = "AWS_LAMBDA_FUNCTION_VERSION"
-	awsLambdaFunctionMemorySizeEnvVar = "AWS_LAMBDA_FUNCTION_MEMORY_SIZE"
-	awsLambdaLogGroupNameEnvVar = "AWS_LAMBDA_LOG_GROUP_NAME"
-	awsLambdaLogStreamNameEnvVar = "AWS_LAMBDA_LOG_STREAM_NAME"
+	// awsRegionEnvVar                   = "AWS_REGION"
+	// awsLambdaFunctionNameEnvVar       = "AWS_LAMBDA_FUNCTION_NAME"
+	// awsLambdaFunctionVersionEnvVar    = "AWS_LAMBDA_FUNCTION_VERSION"
+	// awsLambdaFunctionMemorySizeEnvVar = "AWS_LAMBDA_FUNCTION_MEMORY_SIZE"
+	// awsLambdaLogGroupNameEnvVar       = "AWS_LAMBDA_LOG_GROUP_NAME"
+	// awsLambdaLogStreamNameEnvVar      = "AWS_LAMBDA_LOG_STREAM_NAME"
 )
 
 var _ internal.Detector = (*Detector)(nil)
 
-type Detector struct{
+type Detector struct {
 	logger *zap.Logger
 }
 
