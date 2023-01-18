fc167d96bcf1441b665fcbb17558cf8825eaeea1
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2023-01-18 15:18:52 -0700

    [pkg/ottl] Add tmp path (#16994)
    We've had discussion recently around how to handle functions and converters (previously factory functions) that share the same functionality (see #16571)
    
    This PR suggests a different approach that should reduce the need to chain converters/functions.
    
    Before we chained converters together because OTTL had no place to store data except for the telemetry payload itself. While attributes could be used, it results in the need for cleanup and potentially unwanted transformations based on condition resolution. This PR introduces the concept of tmp to the logs contexts (and future contexts if we like the solution) that statements can use as a "staging" location for complex transformations.
    
    Before to handle situations like the one in #14938 we would have to chain together functions. Pretending we had KeepKeys converter the statement would look like
    
    merge_maps(attributes, KeepKeys(ParseJSON(body), ["log_type"]), "upsert").
    
    These types of statements are tricky to write and can difficult to comprehend, especially for new users. Each Converter we add on increased the burden. Adding a single extra function, like Flatten, really makes a difference: merge_maps(attributes, Flatten(KeepKeys(ParseJSON(body), ["log_type"]), "."), "upsert")
    
    Co-authored-by: Evan Bradley <github@evanbradley.org>

--- pkg/ottl/contexts/ottlmetric/metrics.go
+++ pkg/ottl/contexts/ottlmetric/metrics.go
@@ -15,6 +15,7 @@
 package ottlmetric // import "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/ottlmetric"
 
 import (
+	"context"
 	"fmt"
 
 	"go.opentelemetry.io/collector/component"
@@ -33,6 +34,7 @@ type TransformContext struct {
 	metric               pmetric.Metric
 	instrumentationScope pcommon.InstrumentationScope
 	resource             pcommon.Resource
+	cache                pcommon.Map
 }
 
 func NewTransformContext(metric pmetric.Metric, instrumentationScope pcommon.InstrumentationScope, resource pcommon.Resource) TransformContext {
@@ -40,6 +42,7 @@ func NewTransformContext(metric pmetric.Metric, instrumentationScope pcommon.Ins
 		metric:               metric,
 		instrumentationScope: instrumentationScope,
 		resource:             resource,
+		cache:                pcommon.NewMap(),
 	}
 }
 
@@ -53,6 +56,10 @@ func (tCtx TransformContext) GetInstrumentationScope() pcommon.InstrumentationSc
 
 func (tCtx TransformContext) GetResource() pcommon.Resource {
 	return tCtx.resource
+}
+
+func (tCtx TransformContext) getCache() pcommon.Map {
+	return tCtx.cache
 }
 
 func NewParser(functions map[string]interface{}, telemetrySettings component.TelemetrySettings) ottl.Parser[TransformContext] {
@@ -80,11 +87,43 @@ func parsePath(val *ottl.Path) (ottl.GetSetter[TransformContext], error) {
 
 func newPathGetSetter(path []ottl.Field) (ottl.GetSetter[TransformContext], error) {
 	switch path[0].Name {
+	case "cache":
+		mapKey := path[0].MapKey
+		if mapKey == nil {
+			return accessCache(), nil
+		}
+		return accessCacheKey(mapKey), nil
 	case "resource":
 		return ottlcommon.ResourcePathGetSetter[TransformContext](path[1:])
 	case "instrumentation_scope":
 		return ottlcommon.ScopePathGetSetter[TransformContext](path[1:])
 	default:
 		return ottlcommon.MetricPathGetSetter[TransformContext](path)
+	}
+}
+
+func accessCache() ottl.StandardGetSetter[TransformContext] {
+	return ottl.StandardGetSetter[TransformContext]{
+		Getter: func(ctx context.Context, tCtx TransformContext) (interface{}, error) {
+			return tCtx.getCache(), nil
+		},
+		Setter: func(ctx context.Context, tCtx TransformContext, val interface{}) error {
+			if m, ok := val.(pcommon.Map); ok {
+				m.CopyTo(tCtx.getCache())
+			}
+			return nil
+		},
+	}
+}
+
+func accessCacheKey(mapKey *string) ottl.StandardGetSetter[TransformContext] {
+	return ottl.StandardGetSetter[TransformContext]{
+		Getter: func(ctx context.Context, tCtx TransformContext) (interface{}, error) {
+			return ottlcommon.GetMapValue(tCtx.getCache(), *mapKey), nil
+		},
+		Setter: func(ctx context.Context, tCtx TransformContext, val interface{}) error {
+			ottlcommon.SetMapValue(tCtx.getCache(), *mapKey, val)
+			return nil
+		},
 	}
 }
