9c2efdedcc290bd152a4ceed46894f792776f02b
Peter Deng <yundeng@ebay.com>
2023-02-06 20:02:56 +0800

    refactor newMultilineSplitterFactory

--- pkg/stanza/fileconsumer/splitter_factory.go
+++ pkg/stanza/fileconsumer/splitter_factory.go
@@ -32,14 +32,11 @@ type multilineSplitterFactory struct {
 
 var _ splitterFactory = (*multilineSplitterFactory)(nil)
 
-func newMultilineSplitterFactory(
-	encoding helper.EncodingConfig,
-	flusher helper.FlusherConfig,
-	multiline helper.MultilineConfig) *multilineSplitterFactory {
+func newMultilineSplitterFactory(splitter helper.SplitterConfig) *multilineSplitterFactory {
 	return &multilineSplitterFactory{
-		EncodingConfig: encoding,
-		Flusher:        flusher,
-		Multiline:      multiline,
+		EncodingConfig: splitter.EncodingConfig,
+		Flusher:        splitter.Flusher,
+		Multiline:      splitter.Multiline,
 	}
 
 }
