b1d8352fdfc919d2a9f0c080e382f9c7400258d5
Peter Deng <yundeng@ebay.com>
2023-02-07 10:22:44 +0800

    embed SplitterConfig in multilineSplitterFactory

--- pkg/stanza/fileconsumer/splitter_factory.go
+++ pkg/stanza/fileconsumer/splitter_factory.go
@@ -25,18 +25,14 @@ type splitterFactory interface {
 }
 
 type multilineSplitterFactory struct {
-	EncodingConfig helper.EncodingConfig
-	Flusher        helper.FlusherConfig
-	Multiline      helper.MultilineConfig
+	helper.SplitterConfig
 }
 
 var _ splitterFactory = (*multilineSplitterFactory)(nil)
 
 func newMultilineSplitterFactory(splitter helper.SplitterConfig) *multilineSplitterFactory {
 	return &multilineSplitterFactory{
-		EncodingConfig: splitter.EncodingConfig,
-		Flusher:        splitter.Flusher,
-		Multiline:      splitter.Multiline,
+		SplitterConfig: splitter,
 	}
 
 }
