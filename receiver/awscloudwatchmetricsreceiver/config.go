0d0507c139cdf54a440488bce0acb5b7e461b299
Lewis Robbins <lewis.robbins2@gmail.com>
2023-02-12 14:47:01 +0000

    added validate function

--- receiver/awscloudwatchmetricsreceiver/config.go
+++ receiver/awscloudwatchmetricsreceiver/config.go
@@ -13,3 +13,63 @@
 // limitations under the License.
 
 package awscloudwatchmetricsreceiver
+
+import (
+	"errors"
+	"fmt"
+	"net/url"
+	"time"
+)
+
+var (
+	defaultPollInterval = time.Minute
+)
+
+// Config is the overall config structure for the awscloudwatchmetricsreceiver
+type Config struct {
+	Region       string         `mapstructure:"region"`
+	Profile      string         `mapstructure:"profile"`
+	IMDSEndpoint string         `mapstructure:"imds_endpoint"`
+	PollInterval time.Duration  `mapstructure:"poll_interval"`
+	Metrics      *MetricsConfig `mapstructure:"metrics"`
+}
+
+// MetricsConfig is the configuration for the metrics part of the receiver
+type MetricsConfig struct {
+	Names []*NamesConfig `mapstructure:"named"`
+}
+
+// NamesConfig is the configuration for the metric namespace and metric names
+// https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/cloudwatch_concepts.html
+type NamesConfig struct {
+	Namespace   string    `mapstructure:"namespace"`
+	MetricNames []*string `mapstructure:"metric_names"`
+}
+
+var (
+	errNoMetricsConfigured = errors.New("no metrics configured")
+	errNoRegion            = errors.New("no region was specified")
+	errInvalidPollInterval = errors.New("poll interval is incorrect, it must be a duration greater than one second")
+)
+
+func (cfg *Config) Validate() error {
+	if cfg.Region == "" {
+		return errNoRegion
+	}
+
+	if cfg.IMDSEndpoint != "" {
+		_, err := url.ParseRequestURI(cfg.IMDSEndpoint)
+		if err != nil {
+			return fmt.Errorf("unable to parse URI for imds_endpoint: %w", err)
+		}
+	}
+
+	if cfg.PollInterval < time.Second {
+		return errInvalidPollInterval
+	}
+	return nil
+}
+
+func (cfg *Config) ValidateMetrics() error {
+	return nil
+}
--- receiver/awscloudwatchmetricsreceiver/receiver.go
+++ receiver/awscloudwatchmetricsreceiver/receiver.go
@@ -13,26 +13,3 @@
 // limitations under the License.
 
 package awscloudwatchmetricsreceiver
-
-import (
-	"errors"
-	"time"
-)
-
-var (
-	defaultPollInterval = time.Minute
-)
-
-// Config is the overall config structure for the awscloudwatchmetricsreceiver
-type Config struct {
-	Region       string        `mapstructure:"region"`
-	Profile      string        `mapstructure:"profile"`
-	IMDSEndpoint string        `mapstructure:"imds_endpoint"`
-	PollInterval time.Duration `mapstructure:"poll_interval"`
-}
-
-var (
-	errNoLogsConfigured    = errors.New("no metrics configured")
-	errNoRegion            = errors.New("no region was specified")
-	errInvalidPollInterval = errors.New("poll interval is incorrect, it must be a duration greater than one second")
-)
