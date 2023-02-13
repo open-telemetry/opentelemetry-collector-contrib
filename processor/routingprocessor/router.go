56b65e500596a9c0eec781956032311656173c0a
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2023-02-13 12:21:49 -0700

    Keep original logic

--- processor/routingprocessor/router.go
+++ processor/routingprocessor/router.go
@@ -132,8 +132,7 @@ func (r *router[E, K]) registerRouteExporters(available map[component.ID]compone
 func (r *router[E, K]) getStatementFrom(item RoutingTableItem) (*ottl.Statement[K], error) {
 	var statement *ottl.Statement[K]
 	if item.Statement != "" {
-		var err error
-		statement, err = r.parser.ParseStatement(item.Statement)
+		statement, err := r.parser.ParseStatement(item.Statement)
 		if err != nil {
 			return statement, err
 		}
