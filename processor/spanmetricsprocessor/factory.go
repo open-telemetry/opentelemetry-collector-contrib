44c9eb8f03e63b889adbe46ab3d54a4b7b5fffb9
Dan Jaglowski <jaglows3@gmail.com>
2023-02-13 11:53:07 -0500

    Port spanmetrics processor to spanmetrics connector

--- processor/spanmetricsprocessor/factory.go
+++ processor/spanmetricsprocessor/factory.go
@@ -20,6 +20,7 @@ import (
 
 	"github.com/tilinna/clock"
 	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/connector"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/processor"
 )
@@ -28,7 +29,8 @@ const (
 	// The value of "type" key in configuration.
 	typeStr = "spanmetrics"
 	// The stability level of the processor.
-	stability = component.StabilityLevelDevelopment
+	stability          = component.StabilityLevelDevelopment
+	connectorStability = component.StabilityLevelDevelopment
 )
 
 // NewFactory creates a factory for the spanmetrics processor.
@@ -37,6 +39,15 @@ func NewFactory() processor.Factory {
 		typeStr,
 		createDefaultConfig,
 		processor.WithTraces(createTracesProcessor, stability),
+	)
+}
+
+// NewConnectorFactory creates a factory for the spanmetrics connector.
+func NewConnectorFactory() connector.Factory {
+	return connector.NewFactory(
+		typeStr,
+		createDefaultConfig,
+		connector.WithTracesToMetrics(createTracesToMetricsConnector, connectorStability),
 	)
 }
 
@@ -50,5 +61,23 @@ func createDefaultConfig() component.Config {
 }
 
 func createTracesProcessor(ctx context.Context, params processor.CreateSettings, cfg component.Config, nextConsumer consumer.Traces) (processor.Traces, error) {
-	return newProcessor(params.Logger, cfg, nextConsumer, clock.FromContext(ctx).NewTicker(cfg.(*Config).MetricsFlushInterval))
+	p, err := newProcessor(params.Logger, cfg, metricsTicker(ctx, cfg))
+	if err != nil {
+		return nil, err
+	}
+	p.tracesConsumer = nextConsumer
+	return p, nil
+}
+
+func createTracesToMetricsConnector(ctx context.Context, params connector.CreateSettings, cfg component.Config, nextConsumer consumer.Metrics) (connector.Traces, error) {
+	c, err := newProcessor(params.Logger, cfg, metricsTicker(ctx, cfg))
+	if err != nil {
+		return nil, err
+	}
+	c.metricsConsumer = nextConsumer
+	return c, nil
+}
+
+func metricsTicker(ctx context.Context, cfg component.Config) *clock.Ticker {
+	return clock.FromContext(ctx).NewTicker(cfg.(*Config).MetricsFlushInterval)
 }
