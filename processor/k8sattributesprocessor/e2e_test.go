b25b52e0a36fefe796519fa30d5da14a40057715
Ziqi Zhao <zhaoziqi9146@gmail.com>
2023-02-14 16:32:26 +0800

    add back tag
    Signed-off-by: Ziqi Zhao <zhaoziqi9146@gmail.com>

--- processor/k8sattributesprocessor/e2e_test.go
+++ processor/k8sattributesprocessor/e2e_test.go
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build e2e
+// +build e2e
+
 package k8sattributesprocessor
 
 import (
