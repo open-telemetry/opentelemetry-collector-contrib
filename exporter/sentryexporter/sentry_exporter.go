078be9f247904679e932d11110429cb807bd655f
fadeltd <fadel.trivandi@bukalapak.com>
2023-02-16 13:12:45 +0700

    update dsn spacing

--- exporter/sentryexporter/sentry_exporter.go
+++ exporter/sentryexporter/sentry_exporter.go
@@ -480,7 +480,7 @@ func CreateSentryExporter(config *Config, set exporter.CreateSettings) (exporter
 	transport := newSentryTransport()
 
 	clientOptions := sentry.ClientOptions{
-		Dsn: config.DSN,
+		Dsn: 		 config.DSN,
 		Environment: config.ENV,
 	}
 
