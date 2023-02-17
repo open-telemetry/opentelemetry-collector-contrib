456d81c3b62c59b884b92699556b030cef51d5ad
Ziqi Zhao <zhaoziqi9146@gmail.com>
2023-02-17 18:46:46 +0800

    [pkg/telemetrygen] add support exporting log data
    Signed-off-by: Ziqi Zhao <zhaoziqi9146@gmail.com>

---
+++ cmd/telemetrygen/internal/logs/logs.go
@@ -0,0 +1,97 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package logs
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"go.opentelemetry.io/collector/pdata/plog/plogotlp"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	"go.uber.org/atomic"
+	"go.uber.org/zap"
+	"golang.org/x/time/rate"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/cmd/telemetrygen/internal/common"
+)
+
+// Start starts the log telemetry generator
+func Start(cfg *Config) error {
+	logger, err := common.CreateLogger()
+	if err != nil {
+		return err
+	}
+
+	// only support grpc in insecure mode
+	clientConn, err := grpc.DialContext(context.TODO(), cfg.Endpoint, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return err
+	}
+	exporter := plogotlp.NewGRPCClient(clientConn)
+
+	if err = Run(cfg, exporter, logger); err != nil {
+		logger.Error("failed to stop the exporter", zap.Error(err))
+		return err
+	}
+
+	return nil
+}
+
+// Run executes the test scenario.
+func Run(c *Config, exp plogotlp.GRPCClient, logger *zap.Logger) error {
+	if c.TotalDuration > 0 {
+		c.NumLogs = 0
+	} else if c.NumLogs <= 0 {
+		return fmt.Errorf("either `logs` or `duration` must be greater than 0")
+	}
+
+	limit := rate.Limit(c.Rate)
+	if c.Rate == 0 {
+		limit = rate.Inf
+		logger.Info("generation of logs isn't being throttled")
+	} else {
+		logger.Info("generation of logs is limited", zap.Float64("per-second", float64(limit)))
+	}
+
+	wg := sync.WaitGroup{}
+	running := atomic.NewBool(true)
+	res := resource.NewWithAttributes(semconv.SchemaURL, c.GetAttributes()...)
+
+	for i := 0; i < c.WorkerCount; i++ {
+		wg.Add(1)
+		w := worker{
+			numLogs:        c.NumLogs,
+			limitPerSecond: limit,
+			totalDuration:  c.TotalDuration,
+			running:        running,
+			wg:             &wg,
+			logger:         logger.With(zap.Int("worker", i)),
+			index:          i,
+		}
+
+		go w.simulateLogs(res, exp)
+	}
+	if c.TotalDuration > 0 {
+		time.Sleep(c.TotalDuration)
+		running.Store(false)
+	}
+	wg.Wait()
+	return nil
+}
