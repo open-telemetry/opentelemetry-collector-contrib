4423b13abc06957cad8d74bbaea9b410d7be4bf5
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2023-02-24 14:14:29 -0700

    [processor/transform] Update transformprocessor to log errors (#18859)
    * Update transformprocessor to log errors
    
    * Add changelog
    
    * fix lint

--- processor/transformprocessor/internal/traces/processor.go
+++ processor/transformprocessor/internal/traces/processor.go
@@ -20,6 +20,7 @@ import (
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/pdata/ptrace"
+	"go.uber.org/zap"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/transformprocessor/internal/common"
@@ -27,6 +28,7 @@ import (
 
 type Processor struct {
 	contexts []consumer.Traces
+	logger   *zap.Logger
 }
 
 func NewProcessor(contextStatements []common.ContextStatements, errorMode ottl.ErrorMode, settings component.TelemetrySettings) (*Processor, error) {
@@ -46,6 +48,7 @@ func NewProcessor(contextStatements []common.ContextStatements, errorMode ottl.E
 
 	return &Processor{
 		contexts: contexts,
+		logger:   settings.Logger,
 	}, nil
 }
 
@@ -53,6 +56,7 @@ func (p *Processor) ProcessTraces(ctx context.Context, td ptrace.Traces) (ptrace
 	for _, c := range p.contexts {
 		err := c.ConsumeTraces(ctx, td)
 		if err != nil {
+			p.logger.Error("failed processing traces", zap.Error(err))
 			return td, err
 		}
 	}
