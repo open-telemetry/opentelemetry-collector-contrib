4853ffa0b40bb9544d9844fa1c5e010d23384954
Andrzej Stencel <astencel@sumologic.com>
2023-02-24 01:43:42 +0100

    docs(receiver/hostmetrics/process): update descriptions of OS-specific metrics (#18051)
    The two metrics are Linux-only (not supported on Windows)
    but there was no mention of this in the documentation:
    - process.open_file_descriptors
    - process.context_switches
    I have added "This metric is only available on Linux"
    similar to what other Linux-only metrics have.

--- receiver/hostmetricsreceiver/internal/scraper/processscraper/internal/metadata/generated_metrics.go
+++ receiver/hostmetricsreceiver/internal/scraper/processscraper/internal/metadata/generated_metrics.go
@@ -727,7 +727,7 @@ type metricProcessPagingFaults struct {
 // init fills process.paging.faults metric with initial data.
 func (m *metricProcessPagingFaults) init() {
 	m.data.SetName("process.paging.faults")
-	m.data.SetDescription("Number of page faults the process has made. This metric is only available on Linux.")
+	m.data.SetDescription("Number of page faults the process has made.")
 	m.data.SetUnit("{faults}")
 	m.data.SetEmptySum()
 	m.data.Sum().SetIsMonotonic(true)
@@ -780,7 +780,7 @@ type metricProcessSignalsPending struct {
 // init fills process.signals_pending metric with initial data.
 func (m *metricProcessSignalsPending) init() {
 	m.data.SetName("process.signals_pending")
-	m.data.SetDescription("Number of pending signals for the process. This metric is only available on Linux.")
+	m.data.SetDescription("Number of pending signals for the process.")
 	m.data.SetUnit("{signals}")
 	m.data.SetEmptySum()
 	m.data.Sum().SetIsMonotonic(false)
