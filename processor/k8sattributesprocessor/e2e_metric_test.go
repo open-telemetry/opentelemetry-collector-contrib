2648e8cd29397eb26677a77d8bc9f698701c1239
Ziqi Zhao <zhaoziqi9146@gmail.com>
2023-02-25 17:18:12 +0800

    fix bugs
    Signed-off-by: Ziqi Zhao <zhaoziqi9146@gmail.com>

--- processor/k8sattributesprocessor/e2e_metric_test.go
+++ processor/k8sattributesprocessor/e2e_metric_test.go
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build e2e && metric
+// +build e2e,metric
+
 package k8sattributesprocessor
 
 import (
