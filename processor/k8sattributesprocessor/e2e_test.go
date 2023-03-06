a9e0cc803ad2d77e0066c1eb440b18b0ac92bd45
Ziqi Zhao <zhaoziqi9146@gmail.com>
2023-03-06 12:38:40 +0800

    [processor/k8sattributes] e2e support log data (#19190)
    Signed-off-by: Ziqi Zhao <zhaoziqi9146@gmail.com>

--- processor/k8sattributesprocessor/e2e_test.go
+++ processor/k8sattributesprocessor/e2e_test.go
@@ -26,6 +26,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/plog"
 	"go.opentelemetry.io/collector/pdata/pmetric"
 	"go.opentelemetry.io/collector/pdata/ptrace"
 	"go.uber.org/multierr"
@@ -350,6 +351,141 @@ func scanMetricForAttributes(t *testing.T, expectedService string, kvs map[strin
 				continue
 			}
 			err = resourceHasAttributes(resourceMetrics.Resource(), kvs)
+		}
+	}
+	assert.NoError(t, err)
+
+}
+
+func TestLogE2E(t *testing.T) {
+	tcs := []struct {
+		name    string
+		service string
+		attrs   map[string]*expectedValue
+	}{
+		{
+			name:    "job",
+			service: "test-job",
+			attrs: map[string]*expectedValue{
+				"k8s.pod.name":                newExpectedValue(regex, "telemetrygen-logs-job-[a-z0-9]*"),
+				"k8s.pod.ip":                  newExpectedValue(exist, ""),
+				"k8s.pod.uid":                 newExpectedValue(exist, ""),
+				"k8s.pod.start_time":          newExpectedValue(exist, ""),
+				"k8s.node.name":               newExpectedValue(exist, ""),
+				"k8s.namespace.name":          newExpectedValue(equal, "default"),
+				"k8s.job.name":                newExpectedValue(equal, "telemetrygen-logs-job"),
+				"k8s.job.uid":                 newExpectedValue(exist, ""),
+				"k8s.annotations.workload":    newExpectedValue(equal, "job"),
+				"k8s.labels.app":              newExpectedValue(equal, "telemetrygen-logs"),
+				"k8s.container.name":          newExpectedValue(equal, "telemetrygen"),
+				"k8s.container.restart_count": newExpectedValue(equal, "0"),
+				"container.image.name":        newExpectedValue(equal, "ghcr.io/open-telemetry/opentelemetry-collector-contrib/telemetrygen"),
+				"container.image.tag":         newExpectedValue(equal, "latest"),
+				"container.id":                newExpectedValue(exist, ""),
+			},
+		},
+		{
+			name:    "statefulset",
+			service: "test-statefulset",
+			attrs: map[string]*expectedValue{
+				"k8s.pod.name":                newExpectedValue(equal, "telemetrygen-logs-statefulset-0"),
+				"k8s.pod.ip":                  newExpectedValue(exist, ""),
+				"k8s.pod.uid":                 newExpectedValue(exist, ""),
+				"k8s.pod.start_time":          newExpectedValue(exist, ""),
+				"k8s.node.name":               newExpectedValue(exist, ""),
+				"k8s.namespace.name":          newExpectedValue(equal, "default"),
+				"k8s.statefulset.name":        newExpectedValue(equal, "telemetrygen-logs-statefulset"),
+				"k8s.statefulset.uid":         newExpectedValue(exist, ""),
+				"k8s.annotations.workload":    newExpectedValue(equal, "statefulset"),
+				"k8s.labels.app":              newExpectedValue(equal, "telemetrygen-logs-statefulset"),
+				"k8s.container.name":          newExpectedValue(equal, "telemetrygen"),
+				"k8s.container.restart_count": newExpectedValue(equal, "0"),
+				"container.image.name":        newExpectedValue(equal, "ghcr.io/open-telemetry/opentelemetry-collector-contrib/telemetrygen"),
+				"container.image.tag":         newExpectedValue(equal, "latest"),
+				"container.id":                newExpectedValue(exist, ""),
+			},
+		},
+		{
+			name:    "deployment",
+			service: "test-deployment",
+			attrs: map[string]*expectedValue{
+				"k8s.pod.name":                newExpectedValue(regex, "telemetrygen-logs-deployment-[a-z0-9]*-[a-z0-9]*"),
+				"k8s.pod.ip":                  newExpectedValue(exist, ""),
+				"k8s.pod.uid":                 newExpectedValue(exist, ""),
+				"k8s.pod.start_time":          newExpectedValue(exist, ""),
+				"k8s.node.name":               newExpectedValue(exist, ""),
+				"k8s.namespace.name":          newExpectedValue(equal, "default"),
+				"k8s.deployment.name":         newExpectedValue(equal, "telemetrygen-logs-deployment"),
+				"k8s.replicaset.name":         newExpectedValue(regex, "telemetrygen-logs-deployment-[a-z0-9]*"),
+				"k8s.replicaset.uid":          newExpectedValue(exist, ""),
+				"k8s.annotations.workload":    newExpectedValue(equal, "deployment"),
+				"k8s.labels.app":              newExpectedValue(equal, "telemetrygen-logs-deployment"),
+				"k8s.container.name":          newExpectedValue(equal, "telemetrygen"),
+				"k8s.container.restart_count": newExpectedValue(equal, "0"),
+				"container.image.name":        newExpectedValue(equal, "ghcr.io/open-telemetry/opentelemetry-collector-contrib/telemetrygen"),
+				"container.image.tag":         newExpectedValue(equal, "latest"),
+				"container.id":                newExpectedValue(exist, ""),
+			},
+		},
+		{
+			name:    "daemonset",
+			service: "test-daemonset",
+			attrs: map[string]*expectedValue{
+				"k8s.pod.name":                newExpectedValue(regex, "telemetrygen-logs-daemonset-[a-z0-9]*"),
+				"k8s.pod.ip":                  newExpectedValue(exist, ""),
+				"k8s.pod.uid":                 newExpectedValue(exist, ""),
+				"k8s.pod.start_time":          newExpectedValue(exist, ""),
+				"k8s.node.name":               newExpectedValue(exist, ""),
+				"k8s.namespace.name":          newExpectedValue(equal, "default"),
+				"k8s.daemonset.name":          newExpectedValue(equal, "telemetrygen-logs-daemonset"),
+				"k8s.daemonset.uid":           newExpectedValue(exist, ""),
+				"k8s.annotations.workload":    newExpectedValue(equal, "daemonset"),
+				"k8s.labels.app":              newExpectedValue(equal, "telemetrygen-logs-daemonset"),
+				"k8s.container.name":          newExpectedValue(equal, "telemetrygen"),
+				"k8s.container.restart_count": newExpectedValue(equal, "0"),
+				"container.image.name":        newExpectedValue(equal, "ghcr.io/open-telemetry/opentelemetry-collector-contrib/telemetrygen"),
+				"container.image.tag":         newExpectedValue(equal, "latest"),
+				"container.id":                newExpectedValue(exist, ""),
+			},
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			scanLogForAttributes(t, tc.service, tc.attrs)
+		})
+	}
+}
+
+func scanLogForAttributes(t *testing.T, expectedService string, kvs map[string]*expectedValue) {
+
+	f, _ := os.Open("./testdata/log.json")
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	const maxCapacity int = 8388608
+	buf := make([]byte, maxCapacity)
+	scanner.Buffer(buf, maxCapacity)
+
+	unmarshaler := plog.JSONUnmarshaler{}
+	var err error
+	for scanner.Scan() {
+		logs, serr := unmarshaler.UnmarshalLogs(scanner.Bytes())
+		if serr != nil {
+			continue
+		}
+
+		len := logs.ResourceLogs().Len()
+		for i := 0; i < len; i++ {
+			resourceLogs := logs.ResourceLogs().At(i)
+
+			service, exist := resourceLogs.Resource().Attributes().Get("service.name")
+			assert.Equal(t, true, exist, "log do not has 'service.name' attribute in resource")
+
+			if service.AsString() != expectedService {
+				continue
+			}
+			err = resourceHasAttributes(resourceLogs.Resource(), kvs)
 		}
 	}
 	assert.NoError(t, err)
