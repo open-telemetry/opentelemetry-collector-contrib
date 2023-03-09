38eeb142522bf20a0bd6d94e0ea7ce1342d7e181
Ruslan Kovalov <ruslan.kovalov@grafana.com>
2023-03-09 17:21:54 +0100

    fixup! [connectors/spanmetrics] Use enum type to configure histogram units.

--- connector/spanmetricsconnector/internal/metrics/unit.go
+++ connector/spanmetricsconnector/internal/metrics/unit.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package metrics
+package metrics // import "github.com/open-telemetry/opentelemetry-collector-contrib/connector/spanmetricsconnector/internal/metrics"
 
 import (
 	"encoding"
