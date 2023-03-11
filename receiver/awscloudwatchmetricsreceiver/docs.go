693b9ad90dc8434c7f24da052192c955676fb2da
Lewis Robbins <lewis.robbins2@gmail.com>
2023-03-11 16:29:32 +0000

    Chore: run make goporto

--- receiver/awscloudwatchmetricsreceiver/docs.go
+++ receiver/awscloudwatchmetricsreceiver/docs.go
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package awscloudwatchmetricsreceiver
+package awscloudwatchmetricsreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/awscloudwatchmetricsreceiver"
