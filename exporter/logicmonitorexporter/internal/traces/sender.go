0206f0252234346b316ae6ff2934955c3fce0de5
Avadhut Pisal <avadhutpisal47@gmail.com>
2023-03-14 17:23:48 +0530

    fix goproto check

--- exporter/logicmonitorexporter/internal/traces/sender.go
+++ exporter/logicmonitorexporter/internal/traces/sender.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package traces
+package traces // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/logicmonitorexporter/internal/traces"
 
 import (
 	"context"
