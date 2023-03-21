c04ce30784985bd9c29c44e1c6d5c833aa5217d6
Alex Boten <aboten@lightstep.com>
2023-03-21 07:41:13 -0700

    [processor/resource] update to use generated status (#20083)
    This updates the metadata for the processor to include stability information used to generate the status table.
    
    Signed-off-by: Alex Boten <aboten@lightstep.com>

--- processor/resourceprocessor/doc.go
+++ processor/resourceprocessor/doc.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:generate mdatagen metadata.yaml
+
 // Package resourceprocessor implements a processor for
 // applying changes on resource attributes.
 package resourceprocessor // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/resourceprocessor"
