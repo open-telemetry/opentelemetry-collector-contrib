20c4594b4b1adbd968030ce4dd0881603d679f5b
pszkamruk-splunk <75434853+pszkamruk-splunk@users.noreply.github.com>
2023-03-22 16:04:27 +0100

    Revert "Hec exporter tests" (#5)

--- exporter/splunkhecexporter/test/config/config_data.py
+++
@@ -1,6 +0,0 @@
-# indexes
-EVENT_INDEX_1 = "main"
-EVENT_INDEX_2 = "sck-otel"
-EVENT_INDEX_FILE_LOG = "file_logs"
-METRICS_INDEX_HOST_METRICS = "metrics"
-METRICS_INDEX_METRICS_ENDPOINT = "sck-metrics"
