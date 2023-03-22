3840c951550df64566911d22735de05e72685772
Alex Boten <aboten@lightstep.com>
2023-03-22 16:43:04 -0700

    [receiver/httpcheck] update to use generated status
    This updates the metadata for the receiver to include stability information used to generate the status table.
    
    Signed-off-by: Alex Boten <aboten@lightstep.com>

--- receiver/httpcheckreceiver/factory.go
+++ receiver/httpcheckreceiver/factory.go
@@ -29,8 +29,7 @@ import (
 )
 
 const (
-	typeStr   = "httpcheck"
-	stability = component.StabilityLevelDevelopment
+	typeStr = "httpcheck"
 )
 
 var errConfigNotHTTPCheck = errors.New("config was not a HTTP check receiver config")
@@ -40,7 +39,7 @@ func NewFactory() receiver.Factory {
 	return receiver.NewFactory(
 		typeStr,
 		createDefaultConfig,
-		receiver.WithMetrics(createMetricsReceiver, stability))
+		receiver.WithMetrics(createMetricsReceiver, metadata.Stability))
 }
 
 func createDefaultConfig() component.Config {
