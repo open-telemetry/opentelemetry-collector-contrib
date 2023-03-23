2fe5b00267f1999a86b964fcd35b575641e7947a
Pablo Collins <pcollins@splunk.com>
2023-03-23 16:45:12 -0400

    [receiver/file] add capability to replay metrics in time (#19754)
     This change adds the ability to replay metrics at the same rate that they were originally received. (I also have a change to update the timestamps to current values but left that out for now to keep the diff smaller.)
    
    Link to tracking Issue: #14638

--- receiver/filereceiver/file_reader_test.go
+++ receiver/filereceiver/file_reader_test.go
@@ -19,19 +19,19 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/consumer/consumertest"
 	"go.opentelemetry.io/collector/pdata/pmetric"
-	"go.uber.org/zap"
 )
 
 func TestFileReader_Readline(t *testing.T) {
 	tc := testConsumer{}
 	f, err := os.Open(filepath.Join("testdata", "metrics.json"))
 	require.NoError(t, err)
-	fr := newFileReader(zap.NewNop(), &tc, f)
+	fr := newFileReader(&tc, f, newReplayTimer(0))
 	err = fr.readLine(context.Background())
 	require.NoError(t, err)
 	assert.Equal(t, 1, len(tc.consumed))
@@ -47,12 +47,36 @@ func TestFileReader_Readline(t *testing.T) {
 func TestFileReader_Cancellation(t *testing.T) {
 	fr := fileReader{
 		consumer:     consumertest.NewNop(),
-		logger:       zap.NewNop(),
 		stringReader: blockingStringReader{},
 	}
 	ctx, cancel := context.WithCancel(context.Background())
-	go fr.readAll(ctx)
+	go func() {
+		_ = fr.readAll(ctx)
+	}()
 	cancel()
+}
+
+func TestFileReader_ReadAll(t *testing.T) {
+	tc := testConsumer{}
+	f, err := os.Open(filepath.Join("testdata", "metrics.json"))
+	require.NoError(t, err)
+	sleeper := &fakeSleeper{}
+	rt := &replayTimer{
+		throttle:  2,
+		sleepFunc: sleeper.fakeSleep,
+	}
+	fr := newFileReader(&tc, f, rt)
+	err = fr.readAll(context.Background())
+	require.NoError(t, err)
+	const expectedSleeps = 10
+	assert.Len(t, sleeper.durations, expectedSleeps)
+	assert.EqualValues(t, 0, sleeper.durations[0])
+	for i := 1; i < expectedSleeps; i++ {
+		expected := time.Second * 4
+		actual := sleeper.durations[i]
+		delta := time.Millisecond * 10
+		assert.InDelta(t, float64(expected), float64(actual), float64(delta))
+	}
 }
 
 type blockingStringReader struct {
