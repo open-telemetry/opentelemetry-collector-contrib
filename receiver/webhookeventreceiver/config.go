b0365e7a7e9fdd76ac98f2a3064de9fbef0001f1
shalper2 <shalpern@splunk.com>
2023-03-29 16:41:10 -0500

    eventReceiver first pass

--- receiver/webhookeventreceiver/config.go
+++ receiver/webhookeventreceiver/config.go
@@ -4,10 +4,31 @@
 package webhookeventreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/webhookeventreceiver"
 
 import (
+	"errors"
+
 	"go.opentelemetry.io/collector/config/confighttp"
+	"go.uber.org/multierr"
+)
+
+var (
+    errMissingEndpointFromConfig = errors.New("Missing receiver server endpoint from config.")
 )
 
 // Config defines configuration for the Generic Webhook receiver.
 type Config struct {
-	confighttp.HTTPServerSettings `mapstructure:",squash"` // squash ensures fields are correctly decoded in embedded struct
+	confighttp.HTTPServerSettings `mapstructure:",squash"`      // squash ensures fields are correctly decoded in embedded struct
+    ReadTimeout  string              `mapstructure:"read_timeout"`      // wait time for reading request headers in ms. Default is twenty seconds.
+    WriteTimeout string             `mapstructure:"write_timeout"`      // wait time for writing request response in ms. Default is twenty seconds.
+    Path         string           `mapstructure:"path"`        // path for data collection. Default is <host>:<port>/services/collector
+    HealthPath   string           `mapstructure:"health_path"` // path for health check api. Default is /services/collector/health
+}
+
+func (cfg *Config) Validate() error {
+    var errs error
+
+    if cfg.HTTPServerSettings.Endpoint == "" {
+        errs = multierr.Append(errs, errMissingEndpointFromConfig)
+    }
+
+    return errs
 }
--- receiver/webhookeventreceiver/receiver.go
+++ receiver/webhookeventreceiver/receiver.go
@@ -4,12 +4,93 @@
 package webhookeventreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/webhookeventreceiver"
 
 import (
+	"context"
 	"errors"
+	"net/http"
+	"sync"
+	"time"
 
+	"github.com/docker/docker/api/server/router/session"
+	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/obsreport"
 	"go.opentelemetry.io/collector/receiver"
+	"go.uber.org/zap"
 )
 
-func newLogsReceiver(_ receiver.CreateSettings, _ Config, _ consumer.Logs) (receiver.Logs, error) {
-	return nil, errors.New("unimplemented")
+var (
+    errNilLogsConsumer = errors.New("Missing a logs consumer")
+    errMissingEndpoint     = errors.New("Missing a receiver endpoint")
+)
+
+type eventReceiver struct {
+    settings    receiver.CreateSettings
+    cfg         *Config
+    logConsumer consumer.Logs
+    server      *http.Server
+    shutdownWG  sync.WaitGroup
+    obsrecv     *obsreport.Receiver
+    logger      *zap.Logger
+}
+
+func (er *eventReceiver) Start(ctx context.Context, host component.Host) error {
+    return nil
+}
+
+func (er *eventReceiver) Shutdown(ctx context.Context) error {
+    return nil
+}
+
+func newLogsReceiver(params receiver.CreateSettings, cfg Config, consumer consumer.Logs) (receiver.Logs, error) {
+    if consumer == nil {
+        return nil, errNilLogsConsumer
+    }
+    
+    if cfg.Endpoint == "" {
+        return nil, errMissingEndpoint
+    }
+
+    transport := "http"
+    if cfg.TLSSetting != nil {
+        transport = "https"
+    }
+
+    obsrecv, err := obsreport.NewReceiver(obsreport.ReceiverSettings{
+        ReceiverID: params.ID,
+        Transport: transport,
+        ReceiverCreateSettings: params,
+    })
+    
+    if err != nil {
+        return nil, err 
+    }
+
+    readTimeout, err := time.ParseDuration(cfg.ReadTimeout+"ms")
+    if err != nil {
+        return nil, err 
+    }
+
+    writeTimeout, err := time.ParseDuration(cfg.WriteTimeout + "ms")
+    if err != nil {
+        return nil, err 
+    }
+
+    // create the server
+    server := &http.Server{
+        Addr: cfg.Endpoint,
+        ReadHeaderTimeout: readTimeout,
+        WriteTimeout: writeTimeout,
+    }
+    
+    // create eventReceiver instance
+    er := &eventReceiver{
+        settings: params,
+        cfg: &cfg,
+        logConsumer: consumer,
+        server: server,
+        obsrecv: obsrecv,
+        logger: params.Logger,
+    }
+
+    return er, nil
 }
