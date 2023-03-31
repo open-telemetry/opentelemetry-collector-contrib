132944695c48c4d746b3c33575e06c981c8a4b24
matianjun1 <mtj334510983@163.com>
2023-03-31 13:23:58 +0800

    update flatten config tests

--- pkg/stanza/operator/transformer/flatten/config_test.go
+++ pkg/stanza/operator/transformer/flatten/config_test.go
@@ -28,7 +28,7 @@ func TestUnmarshal(t *testing.T) {
 		TestsFile:     filepath.Join(".", "testdata", "config.yaml"),
 		Tests: []operatortest.ConfigUnmarshalTest{
 			{
-				Name: "flatten_one_level",
+				Name: "flatten_body_one_level",
 				Expect: func() *Config {
 					cfg := NewConfig()
 					cfg.Field = entry.NewBodyField("nested")
@@ -37,7 +37,7 @@ func TestUnmarshal(t *testing.T) {
 				ExpectErr: false,
 			},
 			{
-				Name: "flatten_second_level",
+				Name: "flatten_body_second_level",
 				Expect: func() *Config {
 					cfg := NewConfig()
 					cfg.Field = entry.NewBodyField("nested", "secondlevel")
@@ -46,19 +46,37 @@ func TestUnmarshal(t *testing.T) {
 				ExpectErr: false,
 			},
 			{
-				Name: "flatten_resource",
+				Name: "flatten_resource_one_level",
 				Expect: func() *Config {
 					cfg := NewConfig()
-					cfg.Field = entry.NewResourceField("secondlevel")
+					cfg.Field = entry.NewResourceField("nested")
 					return cfg
 				}(),
 				ExpectErr: false,
 			},
 			{
-				Name: "flatten_attributes",
+				Name: "flatten_resource_second_level",
 				Expect: func() *Config {
 					cfg := NewConfig()
-					cfg.Field = entry.NewAttributeField("secondlevel")
+					cfg.Field = entry.NewResourceField("nested", "secondlevel")
+					return cfg
+				}(),
+				ExpectErr: false,
+			},
+			{
+				Name: "flatten_attributes_one_level",
+				Expect: func() *Config {
+					cfg := NewConfig()
+					cfg.Field = entry.NewAttributeField("nested")
+					return cfg
+				}(),
+				ExpectErr: false,
+			},
+			{
+				Name: "flatten_attributes_second_level",
+				Expect: func() *Config {
+					cfg := NewConfig()
+					cfg.Field = entry.NewAttributeField("nested", "secondlevel")
 					return cfg
 				}(),
 				ExpectErr: false,
