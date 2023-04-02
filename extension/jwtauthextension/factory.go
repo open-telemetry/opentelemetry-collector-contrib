1e1e84f944775a0d3491ee575430343f1383afd8
StarpTech <deusdustin@gmail.com>
2023-04-02 11:35:08 +0200

    chore: improve

--- extension/jwtauthextension/factory.go
+++ extension/jwtauthextension/factory.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package jwtauthextension // import "github.com/open-telemetry/opentelemetry-collector-contrib/extension/oidcauthextension"
+package jwtauthextension // import "github.com/open-telemetry/opentelemetry-collector-contrib/extension/jwtauthextension"
 
 import (
 	"context"
