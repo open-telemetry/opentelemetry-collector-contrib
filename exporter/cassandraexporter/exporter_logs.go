7ccdc4b19a6071067d0cdd8105c9c6f63c57563a
Emre <emre.stress@gmail.com>
2023-04-04 20:13:54 +0300

    Update exporter/cassandraexporter/exporter_logs.go
    Co-authored-by: Antoine Toulme <antoine@toulme.name>

--- exporter/cassandraexporter/exporter_logs.go
+++ exporter/cassandraexporter/exporter_logs.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package cassandraexporter
+package cassandraexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/cassandraexporter"
 
 import (
 	"context"
