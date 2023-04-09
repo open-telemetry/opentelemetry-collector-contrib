30448d8d5402d7ad08be9ccff83a68076c2764da
tamir-michaeli <tamir.michaeli@logz.io>
2023-04-09 18:27:49 +0300

    removed unneeded validation

--- exporter/logzioexporter/exporter.go
+++ exporter/logzioexporter/exporter.go
@@ -81,9 +81,7 @@ func newLogzioTracesExporter(config *Config, set exporter.CreateSettings) (expor
 	if err != nil {
 		return nil, err
 	}
-	if err = config.Validate(); err != nil {
-		return nil, err
-	}
+
 	exporter.config.HTTPClientSettings.Endpoint, err = generateEndpoint(config)
 	if err != nil {
 		return nil, err
@@ -104,9 +102,6 @@ func newLogzioTracesExporter(config *Config, set exporter.CreateSettings) (expor
 func newLogzioLogsExporter(config *Config, set exporter.CreateSettings) (exporter.Logs, error) {
 	exporter, err := newLogzioExporter(config, set)
 	if err != nil {
-		return nil, err
-	}
-	if err = config.Validate(); err != nil {
 		return nil, err
 	}
 	exporter.config.HTTPClientSettings.Endpoint, err = generateEndpoint(config)
