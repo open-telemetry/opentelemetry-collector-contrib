0c344271d2838132e0be6ed11ee0402feb36b6d4
Jiekun <jiekun.dev@gmail.com>
2023-04-13 13:33:44 +0800

    [feature: OTTL sampling] Added OTTL impl struct

--- processor/tailsamplingprocessor/internal/sampling/ottl.go
+++ processor/tailsamplingprocessor/internal/sampling/ottl.go
@@ -1,1 +1,40 @@
-package sampling
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sampling // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/tailsamplingprocessor/internal/sampling"
+
+import (
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.uber.org/zap"
+)
+
+type ottlQueryFilter struct {
+	Queries []string
+	logger  *zap.Logger
+}
+
+var _ PolicyEvaluator = (*ottlQueryFilter)(nil)
+
+// NewOTTLQueryFilter looks at the trace data and returns a corresponding SamplingDecision.
+func NewOTTLQueryFilter(logger *zap.Logger, queries []string) PolicyEvaluator {
+	return &ottlQueryFilter{
+		Queries: queries,
+		logger:  logger,
+	}
+}
+
+func (oqf *ottlQueryFilter) Evaluate(_ pcommon.TraceID, trace *TraceData) (Decision, error) {
+	oqf.logger.Debug("Evaluating spans with OTTL query filter")
+	return Sampled, nil
+}
