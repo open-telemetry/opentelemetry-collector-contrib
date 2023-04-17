d2efc76d123f58d3fdc17897f5515cce78ab17f5
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2023-04-17 11:52:30 -0600

    Fix lint

--- pkg/ottl/contexts/internal/map_test.go
+++ pkg/ottl/contexts/internal/map_test.go
@@ -16,11 +16,13 @@ package internal
 
 import (
 	"fmt"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/ottltest"
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	"go.opentelemetry.io/collector/pdata/pcommon"
-	"testing"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/ottltest"
 )
 
 func Test_GetMapValue_Invalid(t *testing.T) {
