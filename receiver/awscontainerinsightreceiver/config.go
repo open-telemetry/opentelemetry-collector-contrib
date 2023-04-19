49b2e4c2d704a88706b3d73a90237a4909d8861f
Kaushik Surya <kausyas@amazon.com>
2023-04-19 11:13:27 -0400

    [receiver/awscontainerinsightreceiver] Minor comment update

--- receiver/awscontainerinsightreceiver/config.go
+++ receiver/awscontainerinsightreceiver/config.go
@@ -40,7 +40,7 @@ type Config struct {
 	// The default value is false
 	AddFullPodNameMetricLabel bool `mapstructure:"add_full_pod_name_metric_label"`
 
-	// The "LeaderLockName" is an optional attribute to override the name of the locking resource (e.g. config map) used during the leader
+	// LeaderLockName is an optional attribute to override the name of the locking resource (e.g. config map) used during the leader
 	// election process for EKS Container Insights. The elected leader is responsible for scraping cluster level metrics.
 	// The default value is "otel-container-insight-clusterleader".
 	LeaderLockName string `mapstructure:"leader_lock_name"`
