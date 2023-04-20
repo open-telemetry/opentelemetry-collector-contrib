604424531ec7a9c3a98d431aa49a04a12fc4c416
Caleb Hurshman <caleb@hurshman.com>
2023-04-20 11:01:42 -0400

    whatever goporto is

--- receiver/apachesparkreceiver/doc.go
+++ receiver/apachesparkreceiver/doc.go
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package apachesparkreceiver
+package apachesparkreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/apachesparkreceiver"
