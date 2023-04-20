f66dc66b4725a6cfacec5d1d9064e467bf6a8129
Irina Marchuk <mar4ukira@gmail.com>
2023-04-20 09:35:08 +0100

    [chore] lokireceiver update to use use generated status

--- receiver/lokireceiver/factory.go
+++ receiver/lokireceiver/factory.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:generate mdatagen metadata.yaml
+
 package lokireceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/lokireceiver"
 
 import (
