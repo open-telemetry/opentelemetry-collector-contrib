9607a92257a5d957d939e21bb84bdccbf3bee309
Guillaume GILL <gillg@users.noreply.github.com>
2023-04-22 00:34:39 +0200

    Fix lint

--- pkg/stanza/fileconsumer/attributes.go
+++ pkg/stanza/fileconsumer/attributes.go
@@ -16,7 +16,7 @@ package fileconsumer // import "github.com/open-telemetry/opentelemetry-collecto
 
 import (
 	"runtime"
-	
+
 	"path/filepath"
 
 	"go.uber.org/multierr"
@@ -42,7 +42,7 @@ func resolveFileAttributes(path string) (*FileAttributes, error) {
 	var symErr error
 	// Dirty solution, waiting for this permanent fix https://github.com/golang/go/issues/39786
 	// EvalSymlinks on windows is partially working depending on the way you use Symlinks and Junctions
-    	if runtime.GOOS != "windows" {
+ 	if runtime.GOOS != "windows" {
 		resolved, symErr = filepath.EvalSymlinks(path)
  	} else {
 		resolved = path
