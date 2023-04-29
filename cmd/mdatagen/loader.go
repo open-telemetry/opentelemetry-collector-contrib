eecccc91a74b3bacf4f909c068321f96452d472e
Fred Thomsen <fred.thomsen@sciencelogic.com>
2023-04-29 02:08:49 -0400

    [cmd/mdatagen] support map and slice attrs (#18487)
    Add support for map and slice attributes
    
    Add support for map and slice resource and datapoints attributes in
    order to fulfill #18272.

--- cmd/mdatagen/loader.go
+++ cmd/mdatagen/loader.go
@@ -65,6 +65,10 @@ func (mvt *ValueType) UnmarshalText(text []byte) error {
 		mvt.ValueType = pcommon.ValueTypeBool
 	case "bytes":
 		mvt.ValueType = pcommon.ValueTypeBytes
+	case "slice":
+		mvt.ValueType = pcommon.ValueTypeSlice
+	case "map":
+		mvt.ValueType = pcommon.ValueTypeMap
 	default:
 		return fmt.Errorf("invalid type: %q", vtStr)
 	}
@@ -89,6 +93,10 @@ func (mvt ValueType) Primitive() string {
 		return "bool"
 	case pcommon.ValueTypeBytes:
 		return "[]byte"
+	case pcommon.ValueTypeSlice:
+		return "[]any"
+	case pcommon.ValueTypeMap:
+		return "map[string]any"
 	default:
 		return ""
 	}
@@ -105,9 +113,9 @@ func (mvt ValueType) TestValue() string {
 	case pcommon.ValueTypeBool:
 		return "true"
 	case pcommon.ValueTypeMap:
-		return `pcommon.NewMap()`
+		return `map[string]any{"onek": "onev", "twok": "twov"}`
 	case pcommon.ValueTypeSlice:
-		return `pcommon.NewSlice()`
+		return `[]any{"one", "two"}`
 	}
 	return ""
 }
