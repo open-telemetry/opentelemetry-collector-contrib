4bc9ee03c49e06871edc53a26ebd71646fb41532
Antoine Toulme <antoine@lunar-ocean.com>
2023-05-02 02:26:42 -0700

    [chore] Add status type metadata to parquet exporter (#21386)

--- exporter/parquetexporter/doc.go
+++ exporter/parquetexporter/doc.go
@@ -12,5 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:generate mdatagen metadata.yaml
+
 // Package parquetexporter implements an exporter that writes data to Parquet files.
 package parquetexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/parquetexporter"
