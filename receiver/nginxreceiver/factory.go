fe18b71919df7a31c824cd0a6e3216b46affd6bc
BominRahmani <bominrahmani@gmail.com>
2023-05-06 15:23:23 -0400

    [chore][nginx] changed stability -> metadata.stability

--- receiver/nginxreceiver/factory.go
+++ receiver/nginxreceiver/factory.go
@@ -29,7 +29,6 @@ import (
 
 const (
 	typeStr   = "nginx"
-	stability = component.StabilityLevelBeta
 )
 
 // NewFactory creates a factory for nginx receiver.
@@ -37,7 +36,7 @@ func NewFactory() receiver.Factory {
 	return receiver.NewFactory(
 		typeStr,
 		createDefaultConfig,
-		receiver.WithMetrics(createMetricsReceiver, stability))
+		receiver.WithMetrics(createMetricsReceiver, metadata.MetricsStability))
 }
 
 func createDefaultConfig() component.Config {
