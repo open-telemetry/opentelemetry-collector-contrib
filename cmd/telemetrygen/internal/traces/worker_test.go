6799111ad3dc6b00d8cc5932d50e47044d233b5d
Stanley Liu <stanley.liu@datadoghq.com>
2023-05-08 15:05:50 -0400

    Increase test duration by max sleep between traces

--- cmd/telemetrygen/internal/traces/worker_test.go
+++ cmd/telemetrygen/internal/traces/worker_test.go
@@ -92,7 +92,7 @@ func TestUnthrottled(t *testing.T) {
 
 	cfg := &Config{
 		Config: common.Config{
-			TotalDuration: 50 * time.Millisecond,
+			TotalDuration: 50 * time.Millisecond * 40,
 			WorkerCount:   1,
 		},
 	}
