9a7521fb7eb9b9a1d9ba685d0d8d6dffa61f09b5
BominRahmani <bominrahmani@gmail.com>
2023-05-09 09:54:35 -0400

    [chore][receiver/windowseventlog] fixed lint

--- receiver/windowseventlogreceiver/doc.go
+++ receiver/windowseventlogreceiver/doc.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 //go:generate mdatagen metadata.yaml
 
 // Package stanzareceiver implements a receiver that can be used by the
