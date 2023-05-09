68f8dd3def1f0f79463afec9b0e574a0a69e037c
BominRahmani <bominrahmani@gmail.com>
2023-05-09 13:59:25 -0400

    [chore][receiver/sqlquery] create metadata

--- receiver/sqlqueryreceiver/doc.go
+++ receiver/sqlqueryreceiver/doc.go
@@ -14,4 +14,4 @@
 
 //go:generate mdatagen metadata.yaml
 
-package sqlqueryreceiver// import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/sqlqueryreceiver"
+package sqlqueryreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/sqlqueryreceiver"
