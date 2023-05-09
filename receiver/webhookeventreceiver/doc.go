f666827113524e49739a653650da6f4c939fb51b
BominRahmani <bominrahmani@gmail.com>
2023-05-09 10:42:52 -0400

    [chore][receiver/webhook] lint fix

--- receiver/webhookeventreceiver/doc.go
+++ receiver/webhookeventreceiver/doc.go
@@ -14,4 +14,4 @@
 
 //go:generate mdatagen metadata.yaml
 
-package webhookeventreceiver// import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/webhookeventreceiver"
+package webhookeventreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/webhookeventreceiver"
