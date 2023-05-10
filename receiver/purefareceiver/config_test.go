d1aeb8b70fac9820dc9e668ae28c690440efc27b
BominRahmani <bominrahmani@gmail.com>
2023-05-10 15:29:44 -0400

    [chore][receiver/purefa] create metadata

--- receiver/purefareceiver/config_test.go
+++ receiver/purefareceiver/config_test.go
@@ -2,8 +2,6 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
 //       http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -23,6 +21,8 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/confmap/confmaptest"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/purefareceiver/internal/metadata"
 )
 
 func TestLoadConfig(t *testing.T) {
@@ -36,7 +36,7 @@ func TestLoadConfig(t *testing.T) {
 		expected component.Config
 	}{
 		{
-			id: component.NewID(typeStr),
+			id: component.NewID(metadata.Type),
 			expected: &Config{
 				Settings: &Settings{
 					ReloadIntervals: &ReloadIntervals{
