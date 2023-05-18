61a0cead138b7df629e26e86e6de73b0ead4b625
Alex Boten <aboten@lightstep.com>
2023-05-18 15:00:32 -0700

    [exporter/azuredataexplorer] use generated status header
        This PR updates the component to generate the status table using mdatagen.
    
        Linked issue: #21213
    
    Signed-off-by: Alex Boten <aboten@lightstep.com>

--- exporter/azuredataexplorerexporter/doc.go
+++ exporter/azuredataexplorerexporter/doc.go
@@ -1,4 +1,6 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+//go:generate mdatagen metadata.yaml
+
 package azuredataexplorerexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/azuredataexplorerexporter"
