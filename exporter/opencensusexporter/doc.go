1c91a4c4f6a39c142743ce43133c991638684065
Alex Boten <aboten@lightstep.com>
2023-05-19 15:55:50 -0700

    [exporter/opencensus] use generated status header
    This PR updates the component to generate the status table using mdatagen.
    
    Linked issue: #21213
    
    Signed-off-by: Alex Boten <aboten@lightstep.com>

--- exporter/opencensusexporter/doc.go
+++ exporter/opencensusexporter/doc.go
@@ -1,5 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+//go:generate mdatagen metadata.yaml
+
 // Package opencensusexporter exports data to an OpenCensus agent.
 package opencensusexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/opencensusexporter"
