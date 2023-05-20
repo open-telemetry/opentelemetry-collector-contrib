190beea5093af7c7a09173f64847b6e4f1b42205
Alex Boten <aboten@lightstep.com>
2023-05-19 17:14:24 -0700

    [exporter/jaeger] use generated status header
    This PR updates the component to generate the status table using mdatagen.
    
    Linked issue: #21213
    
    Signed-off-by: Alex Boten <aboten@lightstep.com>

--- exporter/jaegerexporter/doc.go
+++ exporter/jaegerexporter/doc.go
@@ -1,5 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+//go:generate mdatagen metadata.yaml
+
 // Package jaegerexporter sends trace data to a Jaeger Collector gRPC endpoint.
 package jaegerexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/jaegerexporter"
