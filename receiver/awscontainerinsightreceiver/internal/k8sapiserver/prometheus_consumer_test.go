9c4df9e5447d2d89187315ffd63f379abf120d50
Chad Patel <patchad@amazon.com>
2023-05-22 17:00:15 -0500

    add 6 more k8sapiserver metrics to container insights plugin

--- receiver/awscontainerinsightreceiver/internal/k8sapiserver/prometheus_consumer_test.go
+++ receiver/awscontainerinsightreceiver/internal/k8sapiserver/prometheus_consumer_test.go
@@ -55,8 +55,8 @@ func (m mockNextConsumer) ConsumeMetrics(ctx context.Context, md pmetric.Metrics
 	assert.NotEmpty(m.t, value.Str())
 
 	value, found = md.ResourceMetrics().At(0).Resource().Attributes().Get("Timestamp")
-	assert.True(m.t, found)
-	assert.NotEmpty(m.t, value.Str())
+	assert.False(m.t, found)
+	assert.Empty(m.t, value.Str())
 
 	value, found = md.ResourceMetrics().At(0).Resource().Attributes().Get("Sources")
 	assert.True(m.t, found)
@@ -65,6 +65,15 @@ func (m mockNextConsumer) ConsumeMetrics(ctx context.Context, md pmetric.Metrics
 	value, found = md.ResourceMetrics().At(0).Resource().Attributes().Get("NodeName")
 	assert.True(m.t, found)
 	assert.Equal(m.t, "test-node", value.Str())
+
+	assert.Equal(m.t, len(defaultResourceToType), md.ResourceMetrics().Len())
+	assert.Equal(m.t, 1, md.ResourceMetrics().At(0).ScopeMetrics().At(0).Metrics().Len())
+
+	metric1 := md.ResourceMetrics().At(0).ScopeMetrics().At(0).Metrics().At(0)
+	assert.Equal(m.t, "apiserver_storage_objects", metric1.Name())
+	assert.Equal(m.t, 123.4, metric1.Gauge().DataPoints().At(0).DoubleValue())
+
+	assert.Equal(m.t, 1, md.MetricCount())
 
 	return nil
 }
@@ -76,12 +85,28 @@ func TestPrometheusConsumeMetrics(t *testing.T) {
 	}
 
 	consumer := newPrometheusConsumer(zap.NewNop(), nextConsumer, "test-cluster", "test-node")
+	consumer.metricsToResource["invalid-metrics-to-resource"] = "invalid"
 
 	cap := consumer.Capabilities()
 	assert.True(t, cap.MutatesData)
 
 	metrics := pmetric.NewMetrics()
-	metrics.ResourceMetrics().AppendEmpty()
+	metrics.ResourceMetrics().AppendEmpty().ScopeMetrics().AppendEmpty().Metrics().AppendEmpty()
+	metric1 := metrics.ResourceMetrics().At(0).ScopeMetrics().At(0).Metrics().At(0)
+	metric1.SetName("apiserver_storage_objects")
+	metric1.SetEmptyGauge().DataPoints().AppendEmpty().SetDoubleValue(123.4)
+
+	metrics.ResourceMetrics().At(0).ScopeMetrics().At(0).Metrics().AppendEmpty()
+	metric2 := metrics.ResourceMetrics().At(0).ScopeMetrics().At(0).Metrics().At(1)
+	metric2.SetName("some_excluded_metric")
+	metric2.SetEmptyGauge().DataPoints().AppendEmpty().SetDoubleValue(456.7)
+
+	metrics.ResourceMetrics().At(0).ScopeMetrics().At(0).Metrics().AppendEmpty()
+	metric3 := metrics.ResourceMetrics().At(0).ScopeMetrics().At(0).Metrics().At(1)
+	metric3.SetName("invalid-metrics-to-resource")
+	metric3.SetEmptyGauge().DataPoints().AppendEmpty().SetDoubleValue(99.9)
+
+	assert.Equal(t, 3, metrics.MetricCount())
 
 	result := consumer.ConsumeMetrics(context.TODO(), metrics)
 	assert.NoError(t, result)
