c6f119a3580069a7b1c5493525b36ff7f92be440
Alex Boten <aboten@lightstep.com>
2023-05-24 14:52:23 -0700

    [processor/metricsgeneration] use generated status header (#22745)
    This PR updates the component to generate the status table using mdatagen.
    
    Linked issue: #21213
    
    Signed-off-by: Alex Boten <aboten@lightstep.com>

--- processor/metricsgenerationprocessor/doc.go
+++ processor/metricsgenerationprocessor/doc.go
@@ -1,6 +1,8 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+//go:generate mdatagen metadata.yaml
+
 // package metricsgenerationprocessor implements a processor which calculates
 // a new metric from existing metrics.
 package metricsgenerationprocessor // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/metricsgenerationprocessor"
