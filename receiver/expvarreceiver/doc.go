3744b55dfd342e7b7c54676148334822e0caf3a0
Alex Boten <aboten@lightstep.com>
2023-05-25 12:54:22 -0700

    [receiver/expvar] use generated status header (#22765)
    This PR updates the component to generate the status table using mdatagen.
    
    Linked issue: #21213
    
    Signed-off-by: Alex Boten <aboten@lightstep.com>

--- receiver/expvarreceiver/doc.go
+++ receiver/expvarreceiver/doc.go
@@ -1,6 +1,6 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package expvarreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/expvarreceiver"
-
 //go:generate mdatagen metadata.yaml
+
+package expvarreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/expvarreceiver"
