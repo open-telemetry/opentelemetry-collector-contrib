1c55b158ef5f8cd8e8c22b062e31340721c22a1a
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2023-05-25 12:05:40 -0600

    Add bridge from filterlog to filterottl

--- internal/filter/filterottl/bridge.go
+++ internal/filter/filterottl/bridge.go
@@ -14,6 +14,7 @@ import (
 	"github.com/open-telemetry/opentelemetry-collector-contrib/internal/filter/filterconfig"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/internal/filter/filterset"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/ottllog"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/ottlspan"
 )
 
@@ -25,6 +26,8 @@ const (
 	scopeVersionStaticStatement       = `instrumentation_scope.version == "%v"`
 	attributesStaticStatement         = `attributes["%v"] == %v`
 	resourceAttributesStaticStatement = `resource.attributes["%v"] == %v`
+	bodyStaticStatement               = `body == "%v"`
+	severityTextStaticStatement       = `severity_text == "%v"`
 
 	serviceNameRegexStatement        = `IsMatch(resource.attributes["service.name"], "%v")`
 	spanNameRegexStatement           = `IsMatch(name, "%v")`
@@ -33,7 +36,52 @@ const (
 	scopeVersionRegexStatement       = `IsMatch(instrumentation_scope.version, "%v")`
 	attributesRegexStatement         = `IsMatch(attributes["%v"], "%v")`
 	resourceAttributesRegexStatement = `IsMatch(resource.attributes["%v"], "%v")`
+	bodyRegexStatement               = `IsMatch(body, "%v")`
+	severityTextRegexStatement       = `IsMatch(severity_text, "%v")`
+
+	// Boolean expression for existing severity number matching
+	// a -> lr.SeverityNumber() == plog.SeverityNumberUnspecified
+	// b -> snm.matchUndefined
+	// c -> lr.SeverityNumber() >= snm.minSeverityNumber
+	// (a AND b) OR ( NOT a AND c)
+	//  a  b  c  X
+	//  0  0  0  0
+	//  0  0  1  1
+	//  0  1  0  0
+	//  0  1  1  1
+	//  1  0  0  0
+	//  1  0  1  0
+	//  1  1  0  1
+	//  1  1  1  1
+	severityNumberStatement = `((severity_number == SEVERITY_NUMBER_UNSPECIFIED and %v) or (severity_number != SEVERITY_NUMBER_UNSPECIFIED and severity_number >= %d))`
 )
+
+func NewLogSkipExprBridge(mc *filterconfig.MatchConfig) (expr.BoolExpr[ottllog.TransformContext], error) {
+	statements := make([]string, 0, 2)
+	if mc.Include != nil {
+		if err := mc.Include.ValidateForLogs(); err != nil {
+			return nil, err
+		}
+		statement, err := createStatement(*mc.Include)
+		if err != nil {
+			return nil, err
+		}
+		statements = append(statements, fmt.Sprintf("not (%v)", statement))
+	}
+
+	if mc.Exclude != nil {
+		if err := mc.Exclude.ValidateForLogs(); err != nil {
+			return nil, err
+		}
+		statement, err := createStatement(*mc.Exclude)
+		if err != nil {
+			return nil, err
+		}
+		statements = append(statements, fmt.Sprintf("%v", statement))
+	}
+
+	return NewBoolExprForLog(statements, StandardLogFuncs(), ottl.PropagateError, component.TelemetrySettings{Logger: zap.NewNop()})
+}
 
 func NewSpanSkipExprBridge(mc *filterconfig.MatchConfig) (expr.BoolExpr[ottlspan.TransformContext], error) {
 	statements := make([]string, 0, 2)
@@ -57,7 +105,7 @@ func NewSpanSkipExprBridge(mc *filterconfig.MatchConfig) (expr.BoolExpr[ottlspan
 }
 
 func createStatement(mp filterconfig.MatchProperties) (string, error) {
-	serviceNameConditions, spanNameConditions, spanKindConditions, scopeNameConditions, scopeVersionConditions, attributeConditions, resourceAttributeConditions, err := createConditions(mp)
+	serviceNameConditions, spanNameConditions, spanKindConditions, scopeNameConditions, scopeVersionConditions, attributeConditions, resourceAttributeConditions, bodyConditions, severityTextConditions, severityNumberCondition, err := createConditions(mp)
 	if err != nil {
 		return "", err
 	}
@@ -109,13 +157,32 @@ func createStatement(mp filterconfig.MatchProperties) (string, error) {
 	if resourceAttributeConditions != nil {
 		conditions = append(conditions, fmt.Sprintf("%v", strings.Join(resourceAttributeConditions, " and ")))
 	}
+	if bodyConditions != nil {
+		if len(bodyConditions) > 1 {
+			format = "(%v)"
+		} else {
+			format = "%v"
+		}
+		conditions = append(conditions, fmt.Sprintf(format, strings.Join(bodyConditions, " or ")))
+	}
+	if severityTextConditions != nil {
+		if len(severityTextConditions) > 1 {
+			format = "(%v)"
+		} else {
+			format = "%v"
+		}
+		conditions = append(conditions, fmt.Sprintf(format, strings.Join(severityTextConditions, " or ")))
+	}
+	if severityNumberCondition != nil {
+		conditions = append(conditions, *severityNumberCondition)
+	}
 	return strings.Join(conditions, " and "), nil
 }
 
-func createConditions(mp filterconfig.MatchProperties) ([]string, []string, []string, []string, []string, []string, []string, error) {
-	serviceNameStatement, spanNameStatement, spanKindStatement, scopeNameStatement, scopeVersionStatement, attrStatement, resourceAttrStatement, err := createStatementTemplates(mp.MatchType)
+func createConditions(mp filterconfig.MatchProperties) ([]string, []string, []string, []string, []string, []string, []string, []string, []string, *string, error) {
+	serviceNameStatement, spanNameStatement, spanKindStatement, scopeNameStatement, scopeVersionStatement, attrStatement, resourceAttrStatement, bodyStatement, severityTextStatement, err := createStatementTemplates(mp.MatchType)
 	if err != nil {
-		return nil, nil, nil, nil, nil, nil, nil, err
+		return nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, err
 	}
 
 	serviceNameConditions := createBasicConditions(serviceNameStatement, mp.Services)
@@ -124,25 +191,28 @@ func createConditions(mp filterconfig.MatchProperties) ([]string, []string, []st
 	scopeNameConditions, scopeVersionConditions := createLibraryConditions(scopeNameStatement, scopeVersionStatement, mp.Libraries)
 	attributeConditions := createAttributeConditions(attrStatement, mp.Attributes, mp.MatchType)
 	resourceAttributeConditions := createAttributeConditions(resourceAttrStatement, mp.Resources, mp.MatchType)
+	bodyConditions := createBasicConditions(bodyStatement, mp.LogBodies)
+	severityTextConditions := createBasicConditions(severityTextStatement, mp.LogSeverityTexts)
+	severityNumberCondition := createSeverityNumberConditions(mp.LogSeverityNumber)
 
-	return serviceNameConditions, spanNameConditions, spanKindConditions, scopeNameConditions, scopeVersionConditions, attributeConditions, resourceAttributeConditions, nil
+	return serviceNameConditions, spanNameConditions, spanKindConditions, scopeNameConditions, scopeVersionConditions, attributeConditions, resourceAttributeConditions, bodyConditions, severityTextConditions, severityNumberCondition, nil
 }
 
-func createStatementTemplates(matchType filterset.MatchType) (string, string, string, string, string, string, string, error) {
+func createStatementTemplates(matchType filterset.MatchType) (string, string, string, string, string, string, string, string, string, error) {
 	switch matchType {
 	case filterset.Strict:
-		return serviceNameStaticStatement, spanNameStaticStatement, spanKindStaticStatement, scopeNameStaticStatement, scopeVersionStaticStatement, attributesStaticStatement, resourceAttributesStaticStatement, nil
+		return serviceNameStaticStatement, spanNameStaticStatement, spanKindStaticStatement, scopeNameStaticStatement, scopeVersionStaticStatement, attributesStaticStatement, resourceAttributesStaticStatement, bodyStaticStatement, severityTextStaticStatement, nil
 	case filterset.Regexp:
-		return serviceNameRegexStatement, spanNameRegexStatement, spanKindRegexStatement, scopeNameRegexStatement, scopeVersionRegexStatement, attributesRegexStatement, resourceAttributesRegexStatement, nil
+		return serviceNameRegexStatement, spanNameRegexStatement, spanKindRegexStatement, scopeNameRegexStatement, scopeVersionRegexStatement, attributesRegexStatement, resourceAttributesRegexStatement, bodyRegexStatement, severityTextRegexStatement, nil
 	default:
-		return "", "", "", "", "", "", "", filterset.NewUnrecognizedMatchTypeError(matchType)
+		return "", "", "", "", "", "", "", "", "", filterset.NewUnrecognizedMatchTypeError(matchType)
 	}
 }
 
 func createBasicConditions(template string, input []string) []string {
 	var conditions []string
-	for _, serviceName := range input {
-		conditions = append(conditions, fmt.Sprintf(template, serviceName))
+	for _, i := range input {
+		conditions = append(conditions, fmt.Sprintf(template, i))
 	}
 	return conditions
 }
@@ -180,4 +250,12 @@ func convertAttribute(value any) string {
 	default:
 		return fmt.Sprintf(`%v`, val)
 	}
+}
+
+func createSeverityNumberConditions(severityNumberProperties *filterconfig.LogSeverityNumberMatchProperties) *string {
+	if severityNumberProperties == nil {
+		return nil
+	}
+	severityNumberCondition := fmt.Sprintf(severityNumberStatement, severityNumberProperties.MatchUndefined, severityNumberProperties.Min)
+	return &severityNumberCondition
 }
