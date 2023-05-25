bfc8c0901bb78768a91fa9b25fa65bba70bbde06
Alex Boten <aboten@lightstep.com>
2023-05-24 20:29:57 -0700

    [processor/routing] use generated status header (#22753)
    This PR updates the component to generate the status table using mdatagen.
    
    Linked issue: #21213
    
    Signed-off-by: Alex Boten <aboten@lightstep.com>

--- processor/routingprocessor/logs.go
+++ processor/routingprocessor/logs.go
@@ -20,6 +20,7 @@ import (
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/ottllog"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/routingprocessor/internal/common"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/routingprocessor/internal/metadata"
 )
 
 var _ processor.Logs = (*logProcessor)(nil)
@@ -44,7 +45,7 @@ func newLogProcessor(settings component.TelemetrySettings, config component.Conf
 
 	meter := settings.MeterProvider.Meter(scopeName + nameSep + "logs")
 	nonRoutedLogRecordsCounter, err := meter.Int64Counter(
-		typeStr+metricSep+processorKey+metricSep+nonRoutedLogRecordsKey,
+		metadata.Type+metricSep+processorKey+metricSep+nonRoutedLogRecordsKey,
 		metric.WithDescription("Number of log records that were not routed to some or all exporters"),
 	)
 	if err != nil {
