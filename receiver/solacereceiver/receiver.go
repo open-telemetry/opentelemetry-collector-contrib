fa6d1e1c7060426d1a962078a7c180a36f6411b4
Michael Cardy <contact@mcardy.com>
2023-05-30 21:11:13 -0400

    [receiver/solacereceiver]: Updates to Solace Receiver for Solace PubSub+ Event Broker 10.4.1 (#22809)
    * Added new unmarshaller for egress spans
    
    SOL-88023
    
    * Refactored unmarshaller separating out receive and egress, added egress
    
    SOL-86715
    
    * Set the send span name to <endpoint> send
    
    SOL-88033
    
    * Updates for latest architecture changes
    
    Use send.outcome instead of send_operation.outcome
    
    Use (anonymous) in send span name when given an anonymous topic endpoint or queue
    
    SOL-88025
    
    * Don't error log each trace
    
    SOL-92755
    
    * Increment span count based on number of received spans, not span messages
    
    SOL-92890
    
    * Fixed reference error causing failing unit test
    
    * Fixed failing unit tests after merge
    
    * Added changelog
    
    * Fixed make lint errors
    
    New copyright too

--- receiver/solacereceiver/receiver.go
+++ receiver/solacereceiver/receiver.go
@@ -247,7 +247,7 @@ flowControlLoop:
 			}
 		} else {
 			// no forward error
-			s.metrics.recordReportedSpans()
+			s.metrics.recordReportedSpans(int64(traces.SpanCount()))
 			break flowControlLoop
 		}
 	}
