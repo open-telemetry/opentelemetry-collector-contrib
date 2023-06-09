cab5ab9803ae0260bbda7142aa6992e2b4546c23
Bomin Rahmani <38274348+BominRahmani@users.noreply.github.com>
2023-06-09 12:08:48 -0400

    [chore] Sort golden metric file slices (#23068)

--- internal/coreinternal/golden/sort_metrics_test.go
+++ internal/coreinternal/golden/sort_metrics_test.go
@@ -4,8 +4,10 @@
 package golden
 
 import (
+	"path/filepath"
 	"testing"
 
+	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 )
 
@@ -40,5 +42,19 @@ func TestSortAttributes(t *testing.T) {
 			t.Errorf("Incorrect key at index %d. Expected: %s, Actual: %s", i, key, actualKeys[i])
 		}
 	}
+
+}
+
+func TestSortMetricsResourceAndScope(t *testing.T) {
+	beforePath := filepath.Join("testdata", "sort-metrics", "before.yaml")
+	afterPath := filepath.Join("testdata", "sort-metrics", "after.yaml")
+	before, err := ReadMetrics(beforePath)
+	require.NoError(t, err)
+	sortResources(before)
+	sortScopes(before)
+	sortMetricDataPointSlices(before)
+	after, err := ReadMetrics(afterPath)
+	require.NoError(t, err)
+	require.Equal(t, before, after)
 
 }
