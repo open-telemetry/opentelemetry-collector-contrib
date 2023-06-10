1545e6e5d46fe926967e8c8064aacb3ac97e797f
shalper2 <shalpern@splunk.com>
2023-06-10 09:23:38 -0500

    gci'd

--- receiver/webhookeventreceiver/receiver.go
+++ receiver/webhookeventreceiver/receiver.go
@@ -133,12 +133,12 @@ func (er *eventReceiver) Start(_ context.Context, host component.Host) error {
 
 // Shutdown function manages receiver shutdown tasks. part of the receiver.Logs interface.
 func (er *eventReceiver) Shutdown(_ context.Context) error {
-    // server must exist to be closed.
-    if er.server == nil {
-	    return nil
-    }
+	// server must exist to be closed.
+	if er.server == nil {
+		return nil
+	}
 
-    err := er.server.Close()
+	err := er.server.Close()
 	er.shutdownWG.Wait()
 	return err
 }
