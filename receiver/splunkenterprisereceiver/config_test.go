5f72958651f0291fc98c7bcdef45a39d0a1700e9
shalper2 <shalpern@splunk.com>
2023-06-15 11:09:38 -0500

    added license and package to test files

--- receiver/splunkenterprisereceiver/config_test.go
+++ receiver/splunkenterprisereceiver/config_test.go
@@ -2,3 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 package splunkenterprisereceiver
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package splunkenterprisereceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/splunkenterprisereceiver"
