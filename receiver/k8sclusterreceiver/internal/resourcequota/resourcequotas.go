7216fdb611a08312d369040f05b8b3b2c3561514
Antoine Toulme <antoine@lunar-ocean.com>
2023-06-15 17:36:21 -0700

    wip

--- receiver/k8sclusterreceiver/internal/resourcequota/resourcequotas.go
+++ receiver/k8sclusterreceiver/internal/resourcequota/resourcequotas.go
@@ -5,50 +5,30 @@ package resourcequota // import "github.com/open-telemetry/opentelemetry-collect
 
 import (
 	"strings"
+	"time"
 
-	agentmetricspb "github.com/census-instrumentation/opencensus-proto/gen-go/agent/metrics/v1"
-	metricspb "github.com/census-instrumentation/opencensus-proto/gen-go/metrics/v1"
-	resourcepb "github.com/census-instrumentation/opencensus-proto/gen-go/resource/v1"
-	conventions "go.opentelemetry.io/collector/semconv/v1.6.1"
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/pmetric"
+	"go.opentelemetry.io/collector/receiver"
 	corev1 "k8s.io/api/core/v1"
 
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/k8sclusterreceiver/internal/constants"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/k8sclusterreceiver/internal/utils"
+	imetadata "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/k8sclusterreceiver/internal/resourcequota/internal/metadata"
 )
 
-var resourceQuotaHardLimitMetric = &metricspb.MetricDescriptor{
-	Name: "k8s.resource_quota.hard_limit",
-	Description: "The upper limit for a particular resource in a specific namespace." +
-		" Will only be sent if a quota is specified. CPU requests/limits will be sent as millicores",
-	Type: metricspb.MetricDescriptor_GAUGE_INT64,
-	LabelKeys: []*metricspb.LabelKey{{
-		Key: "resource",
-	}},
-}
-
-var resourceQuotaUsedMetric = &metricspb.MetricDescriptor{
-	Name: "k8s.resource_quota.used",
-	Description: "The usage for a particular resource in a specific namespace." +
-		" Will only be sent if a quota is specified. CPU requests/limits will be sent as millicores",
-	Type: metricspb.MetricDescriptor_GAUGE_INT64,
-	LabelKeys: []*metricspb.LabelKey{{
-		Key: "resource",
-	}},
-}
-
-func GetMetrics(rq *corev1.ResourceQuota) []*agentmetricspb.ExportMetricsServiceRequest {
-	var metrics []*metricspb.Metric
+func GetMetrics(set receiver.CreateSettings, rq *corev1.ResourceQuota) pmetric.Metrics {
+	mb := imetadata.NewMetricsBuilder(imetadata.DefaultMetricsBuilderConfig(), set)
+	ts := pcommon.NewTimestampFromTime(time.Now())
 
 	for _, t := range []struct {
-		metric *metricspb.MetricDescriptor
-		rl     corev1.ResourceList
+		recordFn func(ts pcommon.Timestamp, val int64, resource string)
+		rl       corev1.ResourceList
 	}{
 		{
-			resourceQuotaHardLimitMetric,
+			mb.RecordK8sResourceQuotaHardLimitDataPoint,
 			rq.Status.Hard,
 		},
 		{
-			resourceQuotaUsedMetric,
+			mb.RecordK8sResourceQuotaUsedDataPoint,
 			rq.Status.Used,
 		},
 	} {
@@ -58,33 +38,10 @@ func GetMetrics(rq *corev1.ResourceQuota) []*agentmetricspb.ExportMetricsService
 			if strings.HasSuffix(string(k), ".cpu") {
 				val = v.MilliValue()
 			}
-
-			metrics = append(metrics,
-				&metricspb.Metric{
-					MetricDescriptor: t.metric,
-					Timeseries: []*metricspb.TimeSeries{
-						utils.GetInt64TimeSeriesWithLabels(val, []*metricspb.LabelValue{{Value: string(k), HasValue: true}}),
-					},
-				},
-			)
+			mb.RecordK8sResourceQuotaUsedDataPoint(ts, val, string(k))
+			mb.RecordK8sResourceQuotaHardLimitDataPoint(ts, val, string(k))
 		}
 	}
 
-	return []*agentmetricspb.ExportMetricsServiceRequest{
-		{
-			Resource: getResource(rq),
-			Metrics:  metrics,
-		},
-	}
-}
-
-func getResource(rq *corev1.ResourceQuota) *resourcepb.Resource {
-	return &resourcepb.Resource{
-		Type: constants.K8sType,
-		Labels: map[string]string{
-			constants.K8sKeyResourceQuotaUID:      string(rq.UID),
-			constants.K8sKeyResourceQuotaName:     rq.Name,
-			conventions.AttributeK8SNamespaceName: rq.Namespace,
-		},
-	}
+	return mb.Emit(imetadata.WithK8sResourcequotaUID(string(rq.UID)), imetadata.WithK8sResourcequotaName(rq.Name), imetadata.WithK8sNamespaceName(rq.Namespace))
 }
