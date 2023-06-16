5cfcdeb8d71d21d9a29f6525e1a0a9ccec1df6c8
Dmitrii Anoshin <anoshindx@gmail.com>
2023-06-15 17:59:51 -0700

    [receiver/k8s_cluster] Do not store unused data in the k8s API cache (#23417)
    This change removes unused k8s informer data from the cache to reduce RAM utilization.
    
    Tried it on a cluster with 40 nodes and 1000 pods, and it gave up to 30% reduction in RAM usage.

---
+++ receiver/k8sclusterreceiver/informer_transform.go
@@ -0,0 +1,31 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package k8sclusterreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/k8sclusterreceiver"
+
+import (
+	appsv1 "k8s.io/api/apps/v1"
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/k8sclusterreceiver/internal/jobs"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/k8sclusterreceiver/internal/node"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/k8sclusterreceiver/internal/pod"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/k8sclusterreceiver/internal/replicaset"
+)
+
+// transformObject transforms the k8s object by removing the data that is not utilized by the receiver.
+// Only highly utilized objects are transformed here while others are kept as is.
+func transformObject(object interface{}) (interface{}, error) {
+	switch o := object.(type) {
+	case *corev1.Pod:
+		return pod.Transform(o), nil
+	case *corev1.Node:
+		return node.Transform(o), nil
+	case *appsv1.ReplicaSet:
+		return replicaset.Transform(o), nil
+	case *batchv1.Job:
+		return jobs.Transform(o), nil
+	}
+	return object, nil
+}
