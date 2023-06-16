abafee3b5eaba35bd0c71e7f754ace327cc2d9a1
Antoine Toulme <antoine@lunar-ocean.com>
2023-06-15 18:08:31 -0700

    wip

--- receiver/k8sclusterreceiver/internal/resourcequota/resourcequotas.go
+++ receiver/k8sclusterreceiver/internal/resourcequota/resourcequotas.go
@@ -19,28 +19,20 @@ func GetMetrics(set receiver.CreateSettings, rq *corev1.ResourceQuota) pmetric.M
 	mb := imetadata.NewMetricsBuilder(imetadata.DefaultMetricsBuilderConfig(), set)
 	ts := pcommon.NewTimestampFromTime(time.Now())
 
-	for _, t := range []struct {
-		recordFn func(ts pcommon.Timestamp, val int64, resource string)
-		rl       corev1.ResourceList
-	}{
-		{
-			mb.RecordK8sResourceQuotaHardLimitDataPoint,
-			rq.Status.Hard,
-		},
-		{
-			mb.RecordK8sResourceQuotaUsedDataPoint,
-			rq.Status.Used,
-		},
-	} {
-		for k, v := range t.rl {
-
-			val := v.Value()
-			if strings.HasSuffix(string(k), ".cpu") {
-				val = v.MilliValue()
-			}
-			mb.RecordK8sResourceQuotaUsedDataPoint(ts, val, string(k))
-			mb.RecordK8sResourceQuotaHardLimitDataPoint(ts, val, string(k))
+	for k, v := range rq.Status.Hard {
+		val := v.Value()
+		if strings.HasSuffix(string(k), ".cpu") {
+			val = v.MilliValue()
 		}
+		mb.RecordK8sResourceQuotaHardLimitDataPoint(ts, val, string(k))
+	}
+
+	for k, v := range rq.Status.Used {
+		val := v.Value()
+		if strings.HasSuffix(string(k), ".cpu") {
+			val = v.MilliValue()
+		}
+		mb.RecordK8sResourceQuotaUsedDataPoint(ts, val, string(k))
 	}
 
 	return mb.Emit(imetadata.WithK8sResourcequotaUID(string(rq.UID)), imetadata.WithK8sResourcequotaName(rq.Name), imetadata.WithK8sNamespaceName(rq.Namespace), imetadata.WithOpencensusResourcetype("k8s"))
