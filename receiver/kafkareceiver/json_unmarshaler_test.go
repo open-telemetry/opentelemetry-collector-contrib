5cda1a48a4d29875b3d43e21245869d7c9176a8a
Caleb Hurshman <caleb@hurshman.com>
2023-06-26 13:21:00 -0400

    switch to jsoniter, add test

--- receiver/kafkareceiver/json_unmarshaler_test.go
+++ receiver/kafkareceiver/json_unmarshaler_test.go
@@ -7,10 +7,25 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/collector/pdata/plog"
 )
 
 func TestNewJSONUnmarshaler(t *testing.T) {
 	t.Parallel()
 	um := newJSONLogsUnmarshaler()
 	assert.Equal(t, "json", um.Encoding())
+}
+
+func TestPlogReturnType(t *testing.T) {
+	t.Parallel()
+	um := newJSONLogsUnmarshaler()
+	json := `{"example": "example valid json to test that the unmarshaler is correctly returning a plog value"}`
+
+	unmarshaledJson, err := um.Unmarshal([]byte(json))
+
+	assert.NoError(t, err)
+	assert.Nil(t, err)
+
+	var expectedType plog.Logs
+	assert.IsType(t, expectedType, unmarshaledJson)
 }
