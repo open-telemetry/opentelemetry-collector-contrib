3d9917470b36875f78c5463ed11f66e991ecf8ad
Mitchell Gale <Mitchell.Gale@improving.com>
2023-06-26 11:59:06 -0700

    added link for OS config.go

--- exporter/opensearchexporter/config.go
+++ exporter/opensearchexporter/config.go
@@ -1,4 +1,4 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package opensearchexporter
+package opensearchexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/opensearchexporter"
