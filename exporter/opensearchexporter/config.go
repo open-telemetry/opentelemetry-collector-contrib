62dd060e32ff880fe646fd0292dafb7157151b58
Mitchell Gale <Mitchell.Gale@improving.com>
2023-06-27 11:22:10 -0700

    Made with go mod changes and adding factory.go, and adding config to config.go

--- exporter/opensearchexporter/config.go
+++ exporter/opensearchexporter/config.go
@@ -2,3 +2,24 @@
 // SPDX-License-Identifier: Apache-2.0
 
 package opensearchexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/opensearchexporter"
+
+const (
+	defaultOpenSearchEnvName = "OPENSEARCH_URL"
+)
+
+type Config struct {
+
+	//// Endpoints holds the OpenSearch URLs the exporter should send events to.
+	////
+	//// OPENSEARCH_URL environment variable is not set.
+	//Endpoints []string `mapstructure:"endpoints"`
+	//
+	//// NumWorkers configures the number of workers publishing bulk requests.
+	//NumWorkers int `mapstructure:"num_workers"`
+	//
+	//HTTPClientSettings `mapstructure:",squash"`
+	//Retry              RetrySettings `mapstructure:"retry"`
+	//Flush              FlushSettings `mapstructure:"flush"`
+	//Namespace          string        `mapstructure:"namespace"`
+	//Dataset            string        `mapstructure:"dataset"`
+}
