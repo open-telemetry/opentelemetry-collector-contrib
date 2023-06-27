d57adc04c281468d74b3372d7f4a7d258b734347
Daniel Jaglowski <jaglows3@gmail.com>
2023-06-27 10:57:22 -0400

    Revert "[pkg/stanza/fileconsumer] Fix issue where buffer size could cause incorrect fingerprint update (#23183)"
    This reverts commit fbe16e156cd90e11cbe5b70c59a83d94dc923cf4.

--- pkg/stanza/fileconsumer/config.go
+++ pkg/stanza/fileconsumer/config.go
@@ -19,7 +19,6 @@ import (
 const (
 	defaultMaxLogSize         = 1024 * 1024
 	defaultMaxConcurrentFiles = 1024
-	defaultBufSize            = 16 * 1024
 )
 
 var allowFileDeletion = featuregate.GlobalRegistry().MustRegister(
@@ -149,7 +148,6 @@ func (c Config) buildManager(logger *zap.SugaredLogger, emit EmitFunc, factory s
 			readerConfig: &readerConfig{
 				fingerprintSize: int(c.FingerprintSize),
 				maxLogSize:      int(c.MaxLogSize),
-				bufferSize:      defaultBufSize,
 				emit:            emit,
 			},
 			fromBeginning:   startAtBeginning,
