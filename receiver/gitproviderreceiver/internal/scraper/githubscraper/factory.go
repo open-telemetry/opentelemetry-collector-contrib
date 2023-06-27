5ca95757250130fef191358c732b2a2eda231531
Adriel Perkins <adrielp@liatrio.com>
2023-06-27 13:30:04 -0400

    chore: run make goporto to fix ci failure

--- receiver/gitproviderreceiver/internal/scraper/githubscraper/factory.go
+++ receiver/gitproviderreceiver/internal/scraper/githubscraper/factory.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package githubscraper // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/githubscraperreceiver/internal/githubscraper"
+package githubscraper // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/gitproviderreceiver/internal/scraper/githubscraper"
 
 import (
 	"context"
