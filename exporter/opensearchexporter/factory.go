a078ad61e1e6860eb241769f877873d31e965d61
Mitchell Gale <Mitchell.Gale@improving.com>
2023-06-28 14:34:27 -0700

    Removed commented out code. Added @Aneurysm9 as code owner.

--- exporter/opensearchexporter/factory.go
+++ exporter/opensearchexporter/factory.go
@@ -7,19 +7,7 @@ import (
 	"go.opentelemetry.io/collector/exporter"
 )
 
-//  const (
-//	// The value of "type" key in configuration.
-//	typeStr = "opensearch"
-//	// The stability level of the exporter.
-//	stability = component.StabilityLevelDevelopment
-//  )
-
 // NewFactory creates a factory for OpenSearch exporter.
 func NewFactory() exporter.Factory {
-	//  return exporter.NewFactory(
-	//	typeStr,
-	//	createDefaultConfig,
-	//	exporter.WithTraces(createTracesExporter, stability),
-	//  )
 	return nil
 }
