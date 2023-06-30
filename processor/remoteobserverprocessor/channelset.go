2fd6dbfeec7ba4f9625e5051d36f0bbdd8d8b5f0
Antoine Toulme <antoine@lunar-ocean.com>
2023-06-29 21:04:09 -0700

    Rename websocketprocessor to remoteobserverprocessor (#23857)
    **Description:**
    Rename websocketprocessor to remoteobserverprocessor
    
    **Link to tracking Issue:**
    Fixes #23856

--- processor/remoteobserverprocessor/channelset.go
+++ processor/remoteobserverprocessor/channelset.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package websocketprocessor // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/websocketprocessor"
+package remoteobserverprocessor // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/remoteobserverprocessor"
 
 import "sync"
 
