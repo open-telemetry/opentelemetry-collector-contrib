46d03a09bb7b137c59a032f93133c6ef0c8c1bca
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2023-06-30 10:41:50 -0600

    [internal/filter] filtermetric to filterottl bridge (#23141)
    **Description:**
    This PR adds a bridge between `filtermetric.NewSkipExpr` and
    `filterottl.NewBoolExprForMetric` behind a feature gate. With the
    feature gate enabled, any component using `filtermetric.NewSkipExpr`
    will start using OTTL behind the scenes.
    
    In addition, the filterprocessor's implementation of `newSkipResExpr`,
    which is a skip expression for resources, is bridged to
    `filterottl.NewBoolExprForResource`. Since this implementation exists
    only for filtering metrics, the same feature gate is used for both.
    
    While investigating the existing `internal/filtermetric` uses with
    filterprocessor and attributesprocessor I found that:
    - The attributes processor does not support Expressions or Resource
    Attributes. The readme claims that it does, but the implementation does
    not support it (and there are no tests). This bridge DOES NOT rectify
    that.
    - The filterprocessor allows filtering by resource attributes,
    expressions, and metric name. Unlike it's implementation for filtering
    spans and logs, the filterprocessor handles filtering spans at the
    Resource level loop. This is the most performant solution, and how OTTL
    likes to think about the problem, but it results in a different pattern
    than filtering spans and logs. This bridge DOES NOT attempt to move the
    resource implementation into `internal/filtermetric`.
    
    **Link to tracking Issue:**
    Related to
    https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/18643
    Related to
    https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/18642
    
    Depends on:
    - [x]
    https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/23142
    
    **Testing:**
    Added tests comparing the output of the existing config and the bridge.

--- internal/filter/filterottl/functions_test.go
+++ internal/filter/filterottl/functions_test.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package common // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/filterprocessor/internal/common"
+package filterottl
 
 import (
 	"context"
