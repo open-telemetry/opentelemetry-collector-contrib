fb3d9c71502479329e8d3b66bdd2f251eec4cdb9
David Ashpole <dashpole@google.com>
2023-07-13 16:59:51 +0000

    prometheus exporters: Add add_metric_suffixes configuration option, which can disable the addition of type and unit suffixes.

--- exporter/prometheusremotewriteexporter/config.go
+++ exporter/prometheusremotewriteexporter/config.go
@@ -42,6 +42,9 @@ type Config struct {
 
 	// CreatedMetric allows customizing creation of _created metrics
 	CreatedMetric *CreatedMetric `mapstructure:"export_created_metric,omitempty"`
+
+	// AddMetricSuffixes, controls whether unit and type suffixes are added to metrics on export
+	AddMetricSuffixes bool `mapstructure:"add_metric_suffixes,omitempty"`
 }
 
 type CreatedMetric struct {
