221c08c90bb06f27536e3c14be55dd177dc85d12
Curtis Robert <crobert@splunk.com>
2023-07-21 15:05:32 -0700

    Move re-used functionality into separate method

--- receiver/k8sclusterreceiver/internal/clusterresourcequota/clusterresourcequotas.go
+++ receiver/k8sclusterreceiver/internal/clusterresourcequota/clusterresourcequotas.go
@@ -4,6 +4,8 @@
 package clusterresourcequota // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/k8sclusterreceiver/internal/clusterresourcequota"
 
 import (
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	"strings"
 	"time"
 
@@ -20,42 +22,34 @@ func GetMetrics(set receiver.CreateSettings, crq *quotav1.ClusterResourceQuota) 
 	ts := pcommon.NewTimestampFromTime(time.Now())
 
 	for k, v := range crq.Status.Total.Hard {
-		val := v.Value()
-		if strings.HasSuffix(string(k), ".cpu") {
-			val = v.MilliValue()
-		}
-
+		val := extractValue(k, v)
 		mbphase.RecordOpenshiftClusterquotaLimitDataPoint(ts, val, string(k))
 	}
 
 	for k, v := range crq.Status.Total.Used {
-		val := v.Value()
-		if strings.HasSuffix(string(k), ".cpu") {
-			val = v.MilliValue()
-		}
-
+		val := extractValue(k, v)
 		mbphase.RecordOpenshiftClusterquotaUsedDataPoint(ts, val, string(k))
 	}
 
 	for _, ns := range crq.Status.Namespaces {
 		for k, v := range ns.Status.Hard {
-			val := v.Value()
-			if strings.HasSuffix(string(k), ".cpu") {
-				val = v.MilliValue()
-			}
-
+			val := extractValue(k, v)
 			mbphase.RecordOpenshiftAppliedclusterquotaLimitDataPoint(ts, val, ns.Namespace, string(k))
 		}
 
 		for k, v := range ns.Status.Used {
-			val := v.Value()
-			if strings.HasSuffix(string(k), ".cpu") {
-				val = v.MilliValue()
-			}
-
+			val := extractValue(k, v)
 			mbphase.RecordOpenshiftAppliedclusterquotaUsedDataPoint(ts, val, ns.Namespace, string(k))
 		}
 	}
 
 	return mbphase.Emit(imetadataphase.WithOpenshiftClusterquotaName(crq.Name), imetadataphase.WithOpenshiftClusterquotaUID(string(crq.UID)), imetadataphase.WithOpencensusResourcetype("k8s"))
+}
+
+func extractValue(k v1.ResourceName, v resource.Quantity) int64 {
+	val := v.Value()
+	if strings.HasSuffix(string(k), ".cpu") {
+		val = v.MilliValue()
+	}
+	return val
 }
