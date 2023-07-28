c8b7a404409aa561079c6497bf2930d417992616
fchikwekwe <faithchikwekwe01@gmail.com>
2023-07-28 12:09:09 -0400

    chore: fix imports to comply with linter

--- pkg/ottl/math_test.go
+++ pkg/ottl/math_test.go
@@ -10,11 +10,12 @@ import (
 	"testing"
 	"time"
 
-	"github.com/open-telemetry/opentelemetry-collector-contrib/internal/coreinternal/timeutils"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/ottltest"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component/componenttest"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/internal/coreinternal/timeutils"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/ottltest"
 )
 
 func mathParsePath(val *Path) (GetSetter[interface{}], error) {
