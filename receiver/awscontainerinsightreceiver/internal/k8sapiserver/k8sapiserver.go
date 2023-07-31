1b6dcadb659d87986ce5e99ca1c09fcafe82644b
Hyunsoo Kim <hsookim@amazon.com>
2023-07-31 18:26:35 -0400

    fix wrong metric type prefix

--- receiver/awscontainerinsightreceiver/internal/k8sapiserver/k8sapiserver.go
+++ receiver/awscontainerinsightreceiver/internal/k8sapiserver/k8sapiserver.go
@@ -241,9 +241,9 @@ func (k *K8sAPIServer) getStatefulSetMetrics(clusterName, timestampNs string) []
 	statefulSets := k.leaderElection.statefulSetClient.StatefulSetInfos()
 	for _, statefulSet := range statefulSets {
 		fields := map[string]interface{}{
-			ci.MetricName(ci.TypeClusterDeployment, ci.SpecReplicas):   statefulSet.Spec.Replicas,            // statefulset_spec_replicas
-			ci.MetricName(ci.TypeClusterDeployment, ci.StatusReplicas): statefulSet.Status.Replicas,          // statefulset_status_replicas
-			ci.StatusReplicasAvailable:                                 statefulSet.Status.AvailableReplicas, // status_replicas_available
+			ci.MetricName(ci.TypeClusterStatefulSet, ci.SpecReplicas):   statefulSet.Spec.Replicas,            // statefulset_spec_replicas
+			ci.MetricName(ci.TypeClusterStatefulSet, ci.StatusReplicas): statefulSet.Status.Replicas,          // statefulset_status_replicas
+			ci.StatusReplicasAvailable:                                  statefulSet.Status.AvailableReplicas, // status_replicas_available
 		}
 		attributes := map[string]string{
 			ci.ClusterNameKey: clusterName,
@@ -268,8 +268,8 @@ func (k *K8sAPIServer) getReplicaSetMetrics(clusterName, timestampNs string) []p
 	replicaSets := k.leaderElection.replicaSetClient.ReplicaSetInfos()
 	for _, replicaSet := range replicaSets {
 		fields := map[string]interface{}{
-			ci.MetricName(ci.TypeClusterDeployment, ci.SpecReplicas):   replicaSet.Spec.Replicas,            // replicaset_spec_replicas
-			ci.MetricName(ci.TypeClusterDeployment, ci.StatusReplicas): replicaSet.Status.Replicas,          // replicaset_status_replicas
+			ci.MetricName(ci.TypeClusterReplicaSet, ci.SpecReplicas):   replicaSet.Spec.Replicas,            // replicaset_spec_replicas
+			ci.MetricName(ci.TypeClusterReplicaSet, ci.StatusReplicas): replicaSet.Status.Replicas,          // replicaset_status_replicas
 			ci.StatusReplicasAvailable:                                 replicaSet.Status.AvailableReplicas, // status_replicas_available
 		}
 		attributes := map[string]string{
