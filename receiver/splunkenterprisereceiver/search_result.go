4c6ef57335b1ada9f5755b3373c8d333325b6b49
shalper2 <shalpern@splunk.com>
2023-08-02 10:09:05 -0600

    added component logic

---
+++ receiver/splunkenterprisereceiver/search_result.go
@@ -0,0 +1,39 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package splunkenterprisereceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/splunkenterprisereceiver"
+
+// metric name and its associated search as a key value pair
+var searchDict = map[string]string{
+	`SplunkLicenseIndexUsageSearch`: `search=search index=_internal source=*license_usage.log type="Usage"| fields idx, b| eval indexname = if(len(idx)=0 OR isnull(idx),"(UNKNOWN)",idx)| stats sum(b) as b by indexname| eval GB=round(b/1024/1024/1024, 3)| fields indexname, GB`,
+}
+
+var apiDict = map[string]string{
+	`SplunkIndexerThroughput`: `/services/server/introspection/indexer?output_mode=json`,
+}
+
+type searchResponse struct {
+	search string
+	Jobid  *string `xml:"sid"`
+	Return int
+	Fields []*Field `xml:"result>field"`
+}
+
+type Field struct {
+	FieldName string `xml:"k,attr"`
+	Value     string `xml:"value>text"`
+}
+
+// '/services/server/introspection/indexer'
+type indexThroughput struct {
+	Entries []IdxTEntry `json:"entry"`
+}
+
+type IdxTEntry struct {
+	Content IdxTContent `json:"content"`
+}
+
+type IdxTContent struct {
+	Status string  `json:"status"`
+	AvgKb  float64 `json:"average_KBps"`
+}
