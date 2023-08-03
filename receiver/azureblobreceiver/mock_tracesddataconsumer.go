bad08bf0d9b0f15734ec82d74581ac29f8bded1a
Antoine Toulme <antoine@lunar-ocean.com>
2023-08-03 09:41:56 -0700

    add license headers

--- receiver/azureblobreceiver/mock_tracesddataconsumer.go
+++ receiver/azureblobreceiver/mock_tracesddataconsumer.go
@@ -1,3 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
 package azureblobreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/azureblobreceiver"
 
 import (
