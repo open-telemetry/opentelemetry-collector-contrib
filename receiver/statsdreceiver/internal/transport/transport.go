1cfb743db04ecc182786da89dfe6579567f007fb
Juan Manuel Perez <jperezflorez@newrelic.com>
2023-08-03 13:02:37 +0200

    fix linter license

--- receiver/statsdreceiver/internal/transport/transport.go
+++ receiver/statsdreceiver/internal/transport/transport.go
@@ -1,3 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
 package transport // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/statsdreceiver/internal/transport"
 
 // Transport is a set of constants of the transport supported by this receiver.
