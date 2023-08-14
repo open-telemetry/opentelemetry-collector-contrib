79476a88b1c28b8396215e6a9f2b39c0427f4044
schmikei <keith.schmitt@bluemedora.com>
2023-08-14 15:54:12 -0400

    add package comment

--- receiver/vcenterreceiver/internal/metadata/custom.go
+++ receiver/vcenterreceiver/internal/metadata/custom.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package metadata
+package metadata // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/vcenterreceiver/internal/metadata"
 
 import (
 	"go.opentelemetry.io/collector/pdata/pcommon"
