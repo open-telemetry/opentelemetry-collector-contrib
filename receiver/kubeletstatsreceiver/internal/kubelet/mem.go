e9f8810a5765807cbc8e02fa2f57167921637e34
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2023-08-15 14:13:56 -0600

    Add new memory.utilization metrics

--- receiver/kubeletstatsreceiver/internal/kubelet/mem.go
+++ receiver/kubeletstatsreceiver/internal/kubelet/mem.go
@@ -4,13 +4,15 @@
 package kubelet // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/kubeletstatsreceiver/internal/kubelet"
 
 import (
+	"math"
+
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	stats "k8s.io/kubelet/pkg/apis/stats/v1alpha1"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/kubeletstatsreceiver/internal/metadata"
 )
 
-func addMemoryMetrics(mb *metadata.MetricsBuilder, memoryMetrics metadata.MemoryMetrics, s *stats.MemoryStats, currentTime pcommon.Timestamp) {
+func addMemoryMetrics(mb *metadata.MetricsBuilder, memoryMetrics metadata.MemoryMetrics, s *stats.MemoryStats, currentTime pcommon.Timestamp, r resources) {
 	if s == nil {
 		return
 	}
@@ -21,4 +23,15 @@ func addMemoryMetrics(mb *metadata.MetricsBuilder, memoryMetrics metadata.Memory
 	recordIntDataPoint(mb, memoryMetrics.WorkingSet, s.WorkingSetBytes, currentTime)
 	recordIntDataPoint(mb, memoryMetrics.PageFaults, s.PageFaults, currentTime)
 	recordIntDataPoint(mb, memoryMetrics.MajorPageFaults, s.MajorPageFaults, currentTime)
+
+	if s.UsageBytes != nil {
+		if r.memoryLimit > 0 {
+			value := math.Min(float64(*s.UsageBytes)/float64(r.memoryLimit), 1)
+			memoryMetrics.LimitUtilization(mb, currentTime, value)
+		}
+		if r.memoryRequest > 0 {
+			value := float64(*s.UsageBytes) / float64(r.memoryRequest)
+			memoryMetrics.RequestUtilization(mb, currentTime, value)
+		}
+	}
 }
