476e5bc506bf9bf45f58aa22647bbaae98a527ae
Dan Jaglowski <jaglows3@gmail.com>
2023-08-15 16:23:50 -0400

    Additional simplification

--- pkg/stanza/fileconsumer/reader_factory.go
+++ pkg/stanza/fileconsumer/reader_factory.go
@@ -4,7 +4,6 @@
 package fileconsumer // import "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/fileconsumer"
 
 import (
-	"bufio"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -29,126 +28,93 @@ type readerFactory struct {
 }
 
 func (f *readerFactory) newReader(file *os.File, fp *fingerprint.Fingerprint) (*reader, error) {
-	return readerBuilder{
-		readerFactory: f,
-		file:          file,
-		readerMetadata: &readerMetadata{
-			Fingerprint:    fp,
-			FileAttributes: map[string]any{},
-		},
-	}.build()
-}
-
-func (f *readerFactory) newFromMetadata(m *readerMetadata) (*reader, error) {
-	return readerBuilder{
-		readerFactory:  f,
-		readerMetadata: m,
-	}.build()
+	return f.build(file, &readerMetadata{
+		Fingerprint:    fp,
+		FileAttributes: map[string]any{},
+	})
 }
 
 // copy creates a deep copy of a reader
 func (f *readerFactory) copy(old *reader, newFile *os.File) (*reader, error) {
-	return readerBuilder{
-		readerFactory: f,
-		file:          newFile,
-		splitFunc:     old.lineSplitFunc,
-		readerMetadata: &readerMetadata{
-			Fingerprint:     old.Fingerprint.Copy(),
-			Offset:          old.Offset,
-			FileAttributes:  util.MapCopy(old.FileAttributes),
-			HeaderFinalized: old.HeaderFinalized,
-		},
-	}.build()
+	return f.build(newFile, &readerMetadata{
+		Fingerprint:     old.Fingerprint.Copy(),
+		Offset:          old.Offset,
+		FileAttributes:  util.MapCopy(old.FileAttributes),
+		HeaderFinalized: old.HeaderFinalized,
+	})
 }
 
 func (f *readerFactory) newFingerprint(file *os.File) (*fingerprint.Fingerprint, error) {
 	return fingerprint.New(file, f.readerConfig.fingerprintSize)
 }
 
-type readerBuilder struct {
-	*readerFactory
-	file           *os.File
-	readerMetadata *readerMetadata
-	splitFunc      bufio.SplitFunc
-}
-
-func (b readerBuilder) build() (r *reader, err error) {
+func (f *readerFactory) build(file *os.File, m *readerMetadata) (r *reader, err error) {
 	r = &reader{
-		readerConfig:   b.readerConfig,
-		readerMetadata: b.readerMetadata,
+		readerConfig:   f.readerConfig,
+		readerMetadata: m,
+		file:           file,
+		SugaredLogger:  f.SugaredLogger.With("path", file.Name()),
+		decoder:        helper.NewDecoder(f.encoding),
 	}
 
-	if b.splitFunc != nil {
-		r.lineSplitFunc = b.splitFunc
-	} else {
-		r.lineSplitFunc, err = b.splitterFactory.Build(b.readerConfig.maxLogSize)
-		if err != nil {
+	if !f.fromBeginning {
+		if err = r.offsetToEnd(); err != nil {
 			return nil, err
 		}
 	}
 
-	r.decoder = helper.NewDecoder(b.encoding)
+	r.lineSplitFunc, err = f.splitterFactory.Build(f.readerConfig.maxLogSize)
+	if err != nil {
+		return nil, err
+	}
 
-	if b.headerConfig == nil || b.readerMetadata.HeaderFinalized {
+	if f.headerConfig == nil || m.HeaderFinalized {
 		r.splitFunc = r.lineSplitFunc
-		r.processFunc = b.readerConfig.emit
+		r.processFunc = f.readerConfig.emit
 	} else {
-		r.splitFunc = b.headerConfig.SplitFunc
-		r.headerReader, err = header.NewReader(b.SugaredLogger, *b.headerConfig)
+		r.splitFunc = f.headerConfig.SplitFunc
+		r.headerReader, err = header.NewReader(f.SugaredLogger, *f.headerConfig)
 		if err != nil {
 			return nil, err
 		}
 		r.processFunc = r.headerReader.Process
 	}
 
-	if b.file == nil {
-		return r, nil
-	}
-
-	r.file = b.file
-	r.SugaredLogger = b.SugaredLogger.With("path", b.file.Name())
-
 	// Resolve file name and path attributes
-	resolved := b.file.Name()
+	resolved := file.Name()
 
-	// Dirty solution, waiting for this permanent fix https://github.com/golang/go/issues/39786
+	// Dirty solution, waiting for this permanent fix https://githuf.com/golang/go/issues/39786
 	// EvalSymlinks on windows is partially working depending on the way you use Symlinks and Junctions
 	if runtime.GOOS != "windows" {
-		resolved, err = filepath.EvalSymlinks(b.file.Name())
+		resolved, err = filepath.EvalSymlinks(file.Name())
 		if err != nil {
-			b.Errorf("resolve symlinks: %w", err)
+			f.Errorf("resolve symlinks: %w", err)
 		}
 	}
 	abs, err := filepath.Abs(resolved)
 	if err != nil {
-		b.Errorf("resolve abs: %w", err)
+		f.Errorf("resolve abs: %w", err)
 	}
 
-	if b.readerConfig.includeFileName {
-		r.FileAttributes[logFileName] = filepath.Base(b.file.Name())
+	if f.readerConfig.includeFileName {
+		r.FileAttributes[logFileName] = filepath.Base(file.Name())
 	} else if r.FileAttributes[logFileName] != nil {
 		delete(r.FileAttributes, logFileName)
 	}
-	if b.readerConfig.includeFilePath {
-		r.FileAttributes[logFilePath] = b.file.Name()
+	if f.readerConfig.includeFilePath {
+		r.FileAttributes[logFilePath] = file.Name()
 	} else if r.FileAttributes[logFilePath] != nil {
 		delete(r.FileAttributes, logFilePath)
 	}
-	if b.readerConfig.includeFileNameResolved {
+	if f.readerConfig.includeFileNameResolved {
 		r.FileAttributes[logFileNameResolved] = filepath.Base(abs)
 	} else if r.FileAttributes[logFileNameResolved] != nil {
 		delete(r.FileAttributes, logFileNameResolved)
 	}
-	if b.readerConfig.includeFilePathResolved {
+	if f.readerConfig.includeFilePathResolved {
 		r.FileAttributes[logFilePathResolved] = abs
 	} else if r.FileAttributes[logFilePathResolved] != nil {
 		delete(r.FileAttributes, logFilePathResolved)
-	}
-
-	if !b.fromBeginning {
-		if err = r.offsetToEnd(); err != nil {
-			return nil, err
-		}
 	}
 
 	return r, nil
