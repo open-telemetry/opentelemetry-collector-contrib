7e27336e390d3bb04dab8499d720db7dcc4ed377
Alex Boten <aboten@lightstep.com>
2023-08-18 12:04:55 -0700

    run goporto
    Signed-off-by: Alex Boten <aboten@lightstep.com>

--- extension/healthcheckextension/internal/healthcheck/handler.go
+++ extension/healthcheckextension/internal/healthcheck/handler.go
@@ -16,7 +16,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package healthcheck
+package healthcheck // import "github.com/open-telemetry/opentelemetry-collector-contrib/extension/healthcheckextension/internal/healthcheck"
 
 import (
 	"encoding/json"
