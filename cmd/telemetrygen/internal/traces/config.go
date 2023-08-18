8cff6dcc5c9a2b14085f894e7b36247f36fc9510
Ziqi Zhao <zhaoziqi9146@gmail.com>
2023-08-18 22:10:08 +0800

    change default value to 0
    Signed-off-by: Ziqi Zhao <zhaoziqi9146@gmail.com>

--- cmd/telemetrygen/internal/traces/config.go
+++ cmd/telemetrygen/internal/traces/config.go
@@ -26,7 +26,7 @@ func (c *Config) Flags(fs *pflag.FlagSet) {
 	fs.IntVar(&c.NumTraces, "traces", 1, "Number of traces to generate in each worker (ignored if duration is provided)")
 	fs.BoolVar(&c.PropagateContext, "marshal", false, "Whether to marshal trace context via HTTP headers")
 	fs.StringVar(&c.ServiceName, "service", "telemetrygen", "Service name to use")
-	fs.StringVar(&c.StatusCode, "status-code", "", "Status code to use for the spans, one of (Unset, Error, Ok) or the equivalent integer (0,1,2)")
+	fs.StringVar(&c.StatusCode, "status-code", "0", "Status code to use for the spans, one of (Unset, Error, Ok) or the equivalent integer (0,1,2)")
 	fs.BoolVar(&c.Batch, "batch", true, "Whether to batch traces")
 	fs.IntVar(&c.LoadSize, "size", 0, "Desired minimum size in MB of string data for each trace generated. This can be used to test traces with large payloads, i.e. when testing the OTLP receiver endpoint max receive size.")
 }
