3d37c21f544fa4eac9e37e53d8e0e27c4665b186
Daniel Jaglowski <jaglows3@gmail.com>
2023-08-22 11:56:54 -0400

    [pkg/stanza] Move encoding concerns to dedicated package (#26019)

--- pkg/stanza/operator/input/udp/udp.go
+++ pkg/stanza/operator/input/udp/udp.go
@@ -15,6 +15,7 @@ import (
 	"go.uber.org/zap"
 	"golang.org/x/text/encoding"
 
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/decoder"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/operator"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/operator/helper"
 )
@@ -83,7 +84,7 @@ func (c Config) Build(logger *zap.SugaredLogger) (operator.Operator, error) {
 		return nil, fmt.Errorf("failed to resolve listen_address: %w", err)
 	}
 
-	enc, err := helper.LookupEncoding(c.Encoding)
+	enc, err := decoder.LookupEncoding(c.Encoding)
 	if err != nil {
 		return nil, err
 	}
@@ -151,7 +152,7 @@ func (u *Input) goHandleMessages(ctx context.Context) {
 	go func() {
 		defer u.wg.Done()
 
-		decoder := helper.NewDecoder(u.encoding)
+		dec := decoder.New(u.encoding)
 		buf := make([]byte, 0, MaxUDPSize)
 		for {
 			message, remoteAddr, err := u.readMessage()
@@ -167,7 +168,7 @@ func (u *Input) goHandleMessages(ctx context.Context) {
 
 			if u.OneLogPerPacket {
 				log := truncateMaxLog(message)
-				u.handleMessage(ctx, remoteAddr, decoder, log)
+				u.handleMessage(ctx, remoteAddr, dec, log)
 				continue
 			}
 
@@ -177,7 +178,7 @@ func (u *Input) goHandleMessages(ctx context.Context) {
 			scanner.Split(u.splitFunc)
 
 			for scanner.Scan() {
-				u.handleMessage(ctx, remoteAddr, decoder, scanner.Bytes())
+				u.handleMessage(ctx, remoteAddr, dec, scanner.Bytes())
 			}
 			if err := scanner.Err(); err != nil {
 				u.Errorw("Scanner error", zap.Error(err))
@@ -198,8 +199,8 @@ func truncateMaxLog(data []byte) (token []byte) {
 	return data
 }
 
-func (u *Input) handleMessage(ctx context.Context, remoteAddr net.Addr, decoder *helper.Decoder, log []byte) {
-	decoded, err := decoder.Decode(log)
+func (u *Input) handleMessage(ctx context.Context, remoteAddr net.Addr, dec *decoder.Decoder, log []byte) {
+	decoded, err := dec.Decode(log)
 	if err != nil {
 		u.Errorw("Failed to decode data", zap.Error(err))
 		return
