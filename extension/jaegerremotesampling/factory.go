0d315e732ba8337caf2be6f256d1cbbb7a11f026
Benedikt Bongartz <bongartz@klimlive.de>
2023-08-25 10:59:39 +0200

    [extension/jaegerremotesampling] register featuregate
    Signed-off-by: Benedikt Bongartz <bongartz@klimlive.de>

--- extension/jaegerremotesampling/factory.go
+++ extension/jaegerremotesampling/factory.go
@@ -5,18 +5,26 @@ package jaegerremotesampling // import "github.com/open-telemetry/opentelemetry-
 
 import (
 	"context"
+	"sync"
 
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/config/configgrpc"
 	"go.opentelemetry.io/collector/config/confighttp"
 	"go.opentelemetry.io/collector/config/confignet"
 	"go.opentelemetry.io/collector/extension"
+	"go.opentelemetry.io/collector/featuregate"
+	"go.uber.org/zap"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/jaegerremotesampling/internal/metadata"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/jaegerremotesampling/jaegerremotesamplingdeprecated"
 )
 
 // NewFactory creates a factory for the jaeger remote sampling extension.
 func NewFactory() extension.Factory {
+	if !protoGate.IsEnabled() {
+		return jaegerremotesamplingdeprecated.NewFactory()
+	}
+
 	return extension.NewFactory(
 		metadata.Type,
 		createDefaultConfig,
@@ -39,6 +47,24 @@ func createDefaultConfig() component.Config {
 		Source: Source{},
 	}
 }
+
+var once sync.Once
+
+func logDeprecation(logger *zap.Logger) {
+	once.Do(func() {
+		logger.Warn("jaegerremotesampling extension will deprecate Thrift-gen and replace it with Proto-gen to be compatbible to jaeger 1.42.0 and higher. See https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/18485 for more details.")
+	})
+}
+
+const protoInsteadOfThrift = "extension.jaegerremotesampling.replaceThriftWithProto"
+
+var protoGate = featuregate.GlobalRegistry().MustRegister(
+	protoInsteadOfThrift,
+	featuregate.StageBeta,
+	featuregate.WithRegisterDescription(
+		"When enabled, the jaegerremotesampling will use Proto-gen over Thrift-gen.",
+	),
+)
 
 func createExtension(_ context.Context, set extension.CreateSettings, cfg component.Config) (extension.Extension, error) {
 	return newExtension(cfg.(*Config), set.TelemetrySettings), nil
