379755df9be7beca424efbf085ce7560de2fb8b3
shalper2 <shalpern@splunk.com>
2023-09-01 09:11:25 -0500

    chipping away at client_test

--- receiver/splunkenterprisereceiver/client_test.go
+++ receiver/splunkenterprisereceiver/client_test.go
@@ -13,16 +13,16 @@ import (
 	"testing"
 	"time"
 
+	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/basicauthextension"
 	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/component/componenttest"
 	"go.opentelemetry.io/collector/config/confighttp"
+	"go.opentelemetry.io/collector/config/configopaque"
 	"go.opentelemetry.io/collector/receiver/scraperhelper"
 )
 
 func TestClientCreation(t *testing.T) {
-	// create a client from an example config
-	client := newSplunkEntClient(&Config{
-		Username:          "admin",
-		Password:          "securityFirst",
+    cfg := &Config {
 		MaxSearchWaitTime: 11 * time.Second,
 		HTTPClientSettings: confighttp.HTTPClientSettings{
 			Endpoint: "https://localhost:8089",
@@ -31,7 +31,11 @@ func TestClientCreation(t *testing.T) {
 			CollectionInterval: 10 * time.Second,
 			InitialDelay:       1 * time.Second,
 		},
-	})
+	}
+
+	// create a client from an example config
+	client, err := newSplunkEntClient(cfg, componenttest.NewNopHost(), componenttest.NewNopTelemetrySettings())
+    require.NoError(t, err)
 
 	testEndpoint, _ := url.Parse("https://localhost:8089")
 
@@ -46,10 +50,7 @@ func TestClientCreation(t *testing.T) {
 // test functionality of createRequest which is used for building metrics out of
 // ad-hoc searches
 func TestClientCreateRequest(t *testing.T) {
-	// create a client from an example config
-	client := newSplunkEntClient(&Config{
-		Username:          "admin",
-		Password:          "securityFirst",
+    cfg := &Config {
 		MaxSearchWaitTime: 11 * time.Second,
 		HTTPClientSettings: confighttp.HTTPClientSettings{
 			Endpoint: "https://localhost:8089",
@@ -58,14 +59,20 @@ func TestClientCreateRequest(t *testing.T) {
 			CollectionInterval: 10 * time.Second,
 			InitialDelay:       1 * time.Second,
 		},
-	})
+	}
+
+	// create a client from an example config
+	client, err := newSplunkEntClient(cfg, componenttest.NewNopHost(), componenttest.NewNopTelemetrySettings())
+
+
+    require.NoError(t, err)
 
 	testJobID := "123"
 
 	tests := []struct {
 		desc     string
 		sr       *searchResponse
-		client   splunkEntClient
+		client   *splunkEntClient
 		expected *http.Request
 	}{
 		{
@@ -122,9 +129,16 @@ func TestClientCreateRequest(t *testing.T) {
 
 // createAPIRequest creates a request for api calls i.e. to introspection endpoint
 func TestAPIRequestCreate(t *testing.T) {
-	client := newSplunkEntClient(&Config{
-		Username:          "admin",
-		Password:          "securityFirst",
+    authExtension := basicauthextension.Config{
+        ClientAuth: &basicauthextension.ClientAuthSettings{
+            Username: "admin",
+            Password: configopaque.String("SecurityFirst!"),
+        },
+    }
+
+    authComponent := basicauthextension.New
+
+    cfg := &Config {
 		MaxSearchWaitTime: 11 * time.Second,
 		HTTPClientSettings: confighttp.HTTPClientSettings{
 			Endpoint: "https://localhost:8089",
@@ -133,15 +147,22 @@ func TestAPIRequestCreate(t *testing.T) {
 			CollectionInterval: 10 * time.Second,
 			InitialDelay:       1 * time.Second,
 		},
-	})
+	}
+
+	client, err := newSplunkEntClient(cfg, componenttest.NewNopHost(), componenttest.NewNopTelemetrySettings())
+    require.NoError(t, err)
 
 	ctx := context.Background()
 	req, err := client.createAPIRequest(ctx, "/test/endpoint")
 	require.NoError(t, err)
 
+    // build the expected request
+    authString := fmt.Sprintf("%s:%s", "admin", "SecurityFirst!")
+	auth64 := base64.StdEncoding.EncodeToString([]byte(authString))
+	expectedBasicAuth := fmt.Sprintf("Basic %s", auth64)
 	expectedURL := client.endpoint.String() + "/test/endpoint"
 	expected, _ := http.NewRequest(http.MethodGet, expectedURL, nil)
-	expected.Header.Add("Authorization", client.basicAuth)
+	expected.Header.Add("Authorization", expectedBasicAuth)
 	expected.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
 	require.Equal(t, expected.URL, req.URL)
