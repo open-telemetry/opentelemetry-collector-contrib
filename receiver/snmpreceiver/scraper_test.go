open-telemetry/opentelemetry-collector-contrib

commit ba8cfceb16c4dc0bebbcdf0411f80db4bb5e3000

[receiver/snmp] Add column and scalar OID metrics to resources that have scalar OID attributes (#25174)
**Description:** <Describe what has changed.>
- Added a new scalar_oid field to ResourceAttribtueConfigs to allow
Scalar OIDs to be used as resource attributes (on Column OID metrics)
- Added support for (Scalar OID only) resource attributes on Scalar OID
metrics as well.

**Link to tracking Issue:** 

https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/23373

**Testing:** 
- Valid and invalid configs using a scalar_oid resource attribute on
both scalar and column oid metrics
- Adding indexed metrics to resources with scalar_oid resource
attributes
- Adding scalar metrics to resources with scalar_oid resource attributes

**Documentation:** <Describe the documentation added.>
- Description of new scalar_oid field in ResourceAttributeConfig and
comments on new functions
- Description of new ResourceAttributes field on Scalar OID metrics

receiver/snmpreceiver/scraper.go
@@ -23,12 +23,14 @@ var (
 	// Error messages
 	errMsgBadValueType                   = `returned metric SNMP data type for OID '%s' is not supported`
 	errMsgIndexedAttributesBadValueType  = `returned attribute SNMP data type for OID '%s' from column OID '%s' is not supported`
+	errMsgScalarAttributesBadValueType   = `returned attribute SNMP data type for OID '%s' is not supported`
 	errMsgOIDAttributeEmptyValue         = `not creating indexed metric '%s' datapoint: %w`
 	errMsgAttributeEmptyValue            = `metric OID attribute value is blank`
 	errMsgResourceAttributeEmptyValue    = `related resource attribute value is blank`
 	errMsgOIDResourceAttributeEmptyValue = `not creating indexed metric '%s' or resource: %w`
 	errMsgScalarOIDProcessing            = `problem processing scalar metric data for OID '%s': %w`
 	errMsgIndexedMetricOIDProcessing     = `problem processing indexed metric data for OID '%s' from column OID '%s': %w`
+	errMsgScalarAttributeOIDProcessing   = `problem processing scalar attribute data from scalar OID '%s': %w`
 	errMsgIndexedAttributeOIDProcessing  = `problem processing indexed attribute data for OID '%s' from column OID '%s': %w`
 )
 
@@ -102,16 +104,12 @@ func (s *snmpScraper) scrapeScalarMetrics(
 	if len(scalarData) == 0 {
 		return
 	}
-
-	// Create general resource if we're about to make scalar metrics
-	resource := metricHelper.getResource(generalResourceKey)
-	if resource == nil {
-		metricHelper.createResource(generalResourceKey, map[string]string{})
-	}
+	// Retrieve scalar OID SNMP data for resource attributes
+	scalarResourceAttributes := s.scrapeScalarResourceAttributes(configHelper.getResourceAttributeScalarOIDs(), scraperErrors)
 
 	// For each piece of SNMP data, attempt to create the necessary OTEL structures (resources/metrics/datapoints)
 	for _, data := range scalarData {
-		if err := s.scalarDataToMetric(data, metricHelper, configHelper); err != nil {
+		if err := s.scalarDataToMetric(data, metricHelper, configHelper, scalarResourceAttributes); err != nil {
 			scraperErrors.AddPartial(1, fmt.Errorf(errMsgScalarOIDProcessing, data.oid, err))
 		}
 	}
@@ -137,11 +135,14 @@ func (s *snmpScraper) scrapeIndexedMetrics(
 	// Retrieve column OID SNMP indexed data for resource attributes
 	columnOIDIndexedResourceAttributeValues := s.scrapeIndexedAttributes(configHelper.getResourceAttributeColumnOIDs(), scraperErrors)
 
+	// Retrieve scalar OID SNMP data for resource attributes
+	columnOIDScalarOIDResourceAttributeValues := s.scrapeScalarResourceAttributes(configHelper.getResourceAttributeScalarOIDs(), scraperErrors)
+
 	// Retrieve all SNMP indexed data from column metric OIDs
 	indexedData := s.client.GetIndexedData(metricColumnOIDs, scraperErrors)
 	// For each piece of SNMP data, attempt to create the necessary OTEL structures (resources/metrics/datapoints)
 	for _, data := range indexedData {
-		if err := s.indexedDataToMetric(data, metricHelper, configHelper, columnOIDIndexedAttributeValues, columnOIDIndexedResourceAttributeValues); err != nil {
+		if err := s.indexedDataToMetric(data, metricHelper, configHelper, columnOIDIndexedAttributeValues, columnOIDIndexedResourceAttributeValues, columnOIDScalarOIDResourceAttributeValues); err != nil {
 			scraperErrors.AddPartial(1, fmt.Errorf(errMsgIndexedMetricOIDProcessing, data.oid, data.columnOID, err))
 		}
 	}
@@ -153,6 +154,7 @@ func (s *snmpScraper) scalarDataToMetric(
 	data SNMPData,
 	metricHelper *otelMetricHelper,
 	configHelper *configHelper,
+	scalarResourceAttributes map[string]string,
 ) error {
 	// Get the related metric name for this SNMP indexed data
 	metricName := configHelper.getMetricName(data.oid)
@@ -161,7 +163,30 @@ func (s *snmpScraper) scalarDataToMetric(
 	// the metric config's attribute values.
 	dataPointAttributes := getScalarDataPointAttributes(configHelper, data.oid)
 
-	return addMetricDataPointToResource(data, metricHelper, configHelper, metricName, generalResourceKey, dataPointAttributes)
+	// Get resource attributes
+	resourceAttributes, err := getResourceAttributes(configHelper, data.oid, "0", map[string]indexedAttributeValues{}, scalarResourceAttributes)
+	if err != nil {
+		return fmt.Errorf(errMsgOIDResourceAttributeEmptyValue, metricName, err)
+	}
+
+	// Create a resource key using all of the relevant resource attribute names
+	resourceAttributeNames := configHelper.getResourceAttributeNames(data.oid)
+
+	var resourceKey string
+	if len(resourceAttributeNames) > 0 {
+		resourceKey = getResourceKey(resourceAttributeNames, "")
+	} else {
+		// Create general resource if we don't have any resource attributes
+		resourceKey = generalResourceKey
+	}
+
+	// Create a new resource if needed
+	resource := metricHelper.getResource(resourceKey)
+	if resource == nil {
+		metricHelper.createResource(resourceKey, resourceAttributes)
+	}
+
+	return addMetricDataPointToResource(data, metricHelper, configHelper, metricName, resourceKey, dataPointAttributes)
 }
 
 // indexedDataToMetric will take one piece of column OID SNMP indexed metric data and turn it
@@ -173,6 +198,7 @@ func (s *snmpScraper) indexedDataToMetric(
 	configHelper *configHelper,
 	columnOIDIndexedAttributeValues map[string]indexedAttributeValues,
 	columnOIDIndexedResourceAttributeValues map[string]indexedAttributeValues,
+	columnOIDScalarResourceAttributeValues map[string]string,
 ) error {
 	// Get the related metric name for this SNMP indexed data
 	metricName := configHelper.getMetricName(data.columnOID)
@@ -186,7 +212,7 @@ func (s *snmpScraper) indexedDataToMetric(
 	}
 
 	// Get resource attributes
-	resourceAttributes, err := getResourceAttributes(configHelper, data.columnOID, indexString, columnOIDIndexedResourceAttributeValues)
+	resourceAttributes, err := getResourceAttributes(configHelper, data.columnOID, indexString, columnOIDIndexedResourceAttributeValues, columnOIDScalarResourceAttributeValues)
 	if err != nil {
 		return fmt.Errorf(errMsgOIDResourceAttributeEmptyValue, metricName, err)
 	}
@@ -194,7 +220,14 @@ func (s *snmpScraper) indexedDataToMetric(
 	// Create a resource key using all of the relevant resource attribute names along
 	// with the row index of the SNMP data
 	resourceAttributeNames := configHelper.getResourceAttributeNames(data.columnOID)
-	resourceKey := getResourceKey(resourceAttributeNames, indexString)
+
+	var resourceKey string
+	// If we only have scalar resource attributes, we don't need multiple resources
+	if len(resourceAttributes) == len(columnOIDScalarResourceAttributeValues) {
+		resourceKey = getResourceKey(resourceAttributeNames, "")
+	} else {
+		resourceKey = getResourceKey(resourceAttributeNames, indexString)
+	}
 
 	// Create a new resource if needed
 	resource := metricHelper.getResource(resourceKey)
@@ -307,12 +340,14 @@ func getResourceAttributes(
 	columnOID string,
 	indexString string,
 	columnOIDIndexedResourceAttributeValues map[string]indexedAttributeValues,
+	columnOIDScalarResourceAttributeValues map[string]string,
 ) (map[string]string, error) {
 	resourceAttributes := map[string]string{}
 
 	for _, attributeName := range configHelper.getResourceAttributeNames(columnOID) {
 		prefix := configHelper.getResourceAttributeConfigIndexedValuePrefix(attributeName)
 		oid := configHelper.getResourceAttributeConfigOID(attributeName)
+		scalarOid := configHelper.getResourceAttributeConfigScalarOID(attributeName)
 		switch {
 		case prefix != "":
 			resourceAttributes[attributeName] = prefix + indexString
@@ -324,12 +359,66 @@ func getResourceAttributes(
 			}
 
 			resourceAttributes[attributeName] = attributeValue
+		case scalarOid != "":
+			resourceAttributes[attributeName] = columnOIDScalarResourceAttributeValues[scalarOid]
 		default:
 			return nil, errors.New(errMsgResourceAttributeEmptyValue)
 		}
 	}
 
 	return resourceAttributes, nil
+}
+
+// scrapeScalarResourceAttributes retrieves all SNMP data from resource attribute
+// config scalar OIDs and stores the returned data for later use by metrics
+func (s *snmpScraper) scrapeScalarResourceAttributes(
+	scalarOIDs []string,
+	scraperErrors *scrapererror.ScrapeErrors,
+) map[string]string {
+	scalarOIDAttributeValues := make(map[string]string, len(scalarOIDs))
+
+	// If no scalar OID resource attribute configs, nothing else to do
+	if len(scalarOIDs) == 0 {
+		return scalarOIDAttributeValues
+	}
+
+	// Retrieve all SNMP data from scalar resource attribute OIDs
+	scalarData := s.client.GetScalarData(scalarOIDs, scraperErrors)
+
+	// For each piece of SNMP data, store the necessary info to help create resources later if needed
+	for _, data := range scalarData {
+		if err := scalarDataToResourceAttribute(data, scalarOIDAttributeValues); err != nil {
+			scraperErrors.AddPartial(1, fmt.Errorf(errMsgScalarAttributeOIDProcessing, data.oid, err))
+		}
+	}
+	return scalarOIDAttributeValues
+}
+
+// scalarDataToResourceAttribute provides a function which will take one piece of scalar OID SNMP data
+// (for a resource attribute) and store it in a map for later use
+func scalarDataToResourceAttribute(
+	data SNMPData,
+	scalarOIDAttributeValues map[string]string,
+) error {
+	// Get the string value of the SNMP data for the {resource} attribute value
+	var stringValue string
+	// Not explicitly checking these casts as this should be made safe in the client
+	switch data.valueType {
+	case notSupportedVal:
+		return fmt.Errorf(errMsgScalarAttributesBadValueType, data.oid)
+	case stringVal:
+		stringValue = data.value.(string)
+	case integerVal:
+		stringValue = strconv.FormatInt(data.value.(int64), 10)
+	case floatVal:
+		stringValue = strconv.FormatFloat(data.value.(float64), 'f', 2, 64)
+	}
+
+	// Store the {resource} attribute value in a map using the scalar OID as a key.
+	// This way we can match metrics to this data through the {resource} attribute config.
+	scalarOIDAttributeValues[data.oid] = stringValue
+
+	return nil
 }
 
 // scrapeIndexedAttributes retrieves all SNMP data from attribute (or resource attribute)

.chloggen/feat_snmpreceiver-resource-attribute-updates.yaml
@@ -1,0 +1,27 @@
+# Use this changelog template to create an entry for release notes.
+
+# One of 'breaking', 'deprecation', 'new_component', 'enhancement', 'bug_fix'
+change_type: enhancement
+
+# The name of the component, or a single word describing the area of concern, (e.g. filelogreceiver)
+component: snmpreceiver
+
+# A brief description of the change.  Surround your text with quotes ("") if it needs to start with a backtick (`).
+note: Support scalar OID resource attributes
+
+# Mandatory: One or more tracking issues related to the change. You can use the PR number here if no issue exists.
+issues: [23373]
+
+# (Optional) One or more lines of additional information to render under the primary note.
+# These lines will be padded with 2 spaces and then inserted directly into the document.
+# Use pipe (|) for multiline entries.
+subtext: Add column and scalar OID metrics to resources that have scalar OID attributes
+
+# If your change doesn't affect end users or the exported elements of any package,
+# you should instead start your pull request title with [chore] or use the "Skip Changelog" label.
+# Optional: The change log or logs in which this entry should be included.
+# e.g. '[user]' or '[user, api]'
+# Include 'user' if the change is relevant to end users.
+# Include 'api' if there is a change to a library API.
+# Default: '[user]'
+change_logs: [user]

receiver/snmpreceiver/config_helper.go
@@ -4,6 +4,7 @@
 package snmpreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/snmpreceiver"
 
 import (
+	"sort"
 	"strings"
 )
 
@@ -13,6 +14,7 @@ type configHelper struct {
 	metricScalarOIDs            []string
 	metricColumnOIDs            []string
 	attributeColumnOIDs         []string
+	resourceAttributeScalarOIDs []string
 	resourceAttributeColumnOIDs []string
 	metricNamesByOID            map[string]string
 	metricAttributesByOID       map[string][]Attribute
@@ -26,6 +28,7 @@ func newConfigHelper(cfg *Config) *configHelper {
 		metricScalarOIDs:            []string{},
 		metricColumnOIDs:            []string{},
 		attributeColumnOIDs:         []string{},
+		resourceAttributeScalarOIDs: []string{},
 		resourceAttributeColumnOIDs: []string{},
 		metricNamesByOID:            map[string]string{},
 		metricAttributesByOID:       map[string][]Attribute{},
@@ -46,6 +49,7 @@ func newConfigHelper(cfg *Config) *configHelper {
 			ch.metricScalarOIDs = append(ch.metricScalarOIDs, oid.OID)
 			ch.metricNamesByOID[oid.OID] = name
 			ch.metricAttributesByOID[oid.OID] = oid.Attributes
+			ch.resourceAttributesByOID[oid.OID] = oid.ResourceAttributes
 		}
 
 		for i, oid := range metricCfg.ColumnOIDs {
@@ -77,20 +81,35 @@ func newConfigHelper(cfg *Config) *configHelper {
 		ch.attributeColumnOIDs = append(ch.attributeColumnOIDs, attributeCfg.OID)
 	}
 
-	// Find all resource attribute column OIDs
+	// Find all resource attribute scalar and column OIDs
 	for name, resourceAttributeCfg := range cfg.ResourceAttributes {
-		if resourceAttributeCfg.OID == "" {
+		if resourceAttributeCfg.ScalarOID != "" {
+			// Data is returned by the client with '.' prefix on the OIDs.
+			// Making sure the prefix exists here in the configs so we can match it up with returned data later
+			if !strings.HasPrefix(resourceAttributeCfg.ScalarOID, ".") {
+				resourceAttributeCfg.ScalarOID = "." + resourceAttributeCfg.ScalarOID
+				cfg.ResourceAttributes[name] = resourceAttributeCfg
+			}
+			ch.resourceAttributeScalarOIDs = append(ch.resourceAttributeScalarOIDs, resourceAttributeCfg.ScalarOID)
 			continue
 		}
-
-		// Data is returned by the client with '.' prefix on the OIDs.
-		// Making sure the prefix exists here in the configs so we can match it up with returned data later
-		if !strings.HasPrefix(resourceAttributeCfg.OID, ".") {
-			resourceAttributeCfg.OID = "." + resourceAttributeCfg.OID
-			cfg.ResourceAttributes[name] = resourceAttributeCfg
+		if resourceAttributeCfg.OID != "" {
+			// Data is returned by the client with '.' prefix on the OIDs.
+			// Making sure the prefix exists here in the configs so we can match it up with returned data later
+			if !strings.HasPrefix(resourceAttributeCfg.OID, ".") {
+				resourceAttributeCfg.OID = "." + resourceAttributeCfg.OID
+				cfg.ResourceAttributes[name] = resourceAttributeCfg
+			}
+			ch.resourceAttributeColumnOIDs = append(ch.resourceAttributeColumnOIDs, resourceAttributeCfg.OID)
 		}
-		ch.resourceAttributeColumnOIDs = append(ch.resourceAttributeColumnOIDs, resourceAttributeCfg.OID)
 	}
+
+	// We expect these []string to be sorted later (i.e. mocks and resourceKey)
+	sort.Strings(ch.metricScalarOIDs)
+	sort.Strings(ch.metricColumnOIDs)
+	sort.Strings(ch.attributeColumnOIDs)
+	sort.Strings(ch.resourceAttributeScalarOIDs)
+	sort.Strings(ch.resourceAttributeColumnOIDs)
 
 	return &ch
 }
@@ -108,6 +127,11 @@ func (h configHelper) getMetricColumnOIDs() []string {
 // getAttributeColumnOIDs returns all of the attribute column OIDs in the attribute configs
 func (h configHelper) getAttributeColumnOIDs() []string {
 	return h.attributeColumnOIDs
+}
+
+// getResourceAttributeScalarOIDs returns all of the resource attribute scalar OIDs in the resource attribute configs
+func (h configHelper) getResourceAttributeScalarOIDs() []string {
+	return h.resourceAttributeScalarOIDs
 }
 
 // getResourceAttributeColumnOIDs returns all of the resource attribute column OIDs in the resource attribute configs
@@ -173,6 +197,16 @@ func (h configHelper) getResourceAttributeConfigOID(name string) string {
 	}
 
 	return attrConfig.OID
+}
+
+// getResourceAttributeConfigScalarOID returns the scalar OID of a resource attribute config
+func (h configHelper) getResourceAttributeConfigScalarOID(name string) string {
+	attrConfig := h.cfg.ResourceAttributes[name]
+	if attrConfig == nil {
+		return ""
+	}
+
+	return attrConfig.ScalarOID
 }
 
 // getMetricConfigAttributes returns the metric config attributes for a given OID

commit 06c7b3dd93a92ec86ecd14008c3598ef9ce38991

[receiver/snmpreceiver] fix scalar RA # check  (#26363)
**Description:** 
Fixed a bug introduced by
[#25174](https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/25174)

There was check that was supposed to ensure that all resource attributes
added to a metric were scalar, but it was really checking if the number
of resource attributes on a metric was the same as the total number of
possible scalar resource attributes defined in the config. This check
has been updated to explicitly verify that all of the resource
attributes on the metric are scalar.

Bug examples:
- A column metric indexed by a column attribute has two scalar resource
attributes with different names but the same oid value. They are the
only two scalar RAs defined in the config. Since the oids are added to a
map whose length is checked, they are seen as one value and the
comparison fails so many resources are created instead of one.
- A column metric indexed by a column attribute has only one scalar
resource attribute when two possible attributes are defined in the
config. The check fails and many resources are created instead of one.

**Testing:** 
- Test that a metric with one scalar resource attribute when two are
available still creates only one resource
- First bug does not feel necessary to unit test, it has been tested
locally and is fixed.

**Documentation:**
- N/A

.chloggen/fix-snmp-scalar-ra-check.yaml
@@ -1,0 +1,27 @@
+# Use this changelog template to create an entry for release notes.
+
+# One of 'breaking', 'deprecation', 'new_component', 'enhancement', 'bug_fix'
+change_type: bug_fix
+
+# The name of the component, or a single word describing the area of concern, (e.g. filelogreceiver)
+component: snmpreceiver
+
+# A brief description of the change.  Surround your text with quotes ("") if it needs to start with a backtick (`).
+note: Fix how to determine how many RAs on a metric are scalar
+
+# Mandatory: One or more tracking issues related to the change. You can use the PR number here if no issue exists.
+issues: [26363]
+
+# (Optional) One or more lines of additional information to render under the primary note.
+# These lines will be padded with 2 spaces and then inserted directly into the document.
+# Use pipe (|) for multiline entries.
+subtext: We now create the proper number of resources for configurations where a resource uses fewer than the available number of scalar resource attribtues.
+
+# If your change doesn't affect end users or the exported elements of any package,
+# you should instead start your pull request title with [chore] or use the "Skip Changelog" label.
+# Optional: The change log or logs in which this entry should be included.
+# e.g. '[user]' or '[user, api]'
+# Include 'user' if the change is relevant to end users.
+# Include 'api' if there is a change to a library API.
+# Default: '[user]'
+change_logs: [user]

receiver/snmpreceiver/scraper.go
@@ -221,9 +221,16 @@ func (s *snmpScraper) indexedDataToMetric(
 	// with the row index of the SNMP data
 	resourceAttributeNames := configHelper.getResourceAttributeNames(data.columnOID)
 
+	// Check how many of the resource attributes on this metric are scalar
+	var numScalarResourceAttributes int
+	for name := range resourceAttributes {
+		if s.cfg.ResourceAttributes[name].ScalarOID != "" {
+			numScalarResourceAttributes++
+		}
+	}
 	var resourceKey string
-	// If we only have scalar resource attributes, we don't need multiple resources
-	if len(resourceAttributes) == len(columnOIDScalarResourceAttributeValues) {
+	// If the only resource attributes on this metric are scalar, we don't need multiple resources
+	if len(resourceAttributes) == numScalarResourceAttributes {
 		resourceKey = getResourceKey(resourceAttributeNames, "")
 	} else {
 		resourceKey = getResourceKey(resourceAttributeNames, indexString)

receiver/snmpreceiver/scraper_test.go
@@ -2982,6 +2982,103 @@ func TestScrape(t *testing.T) {
 				require.NoError(t, err)
 			},
 		},
+		{
+			desc: "Metric does not use all available scalar RAs and still behaves properly (near copy of (24)) (29)",
+			testFunc: func(t *testing.T) {
+				mockClient := new(MockClient)
+				scalarRA1 := SNMPData{
+					oid:       ".5.0",
+					value:     "scalar",
+					valueType: stringVal,
+				}
+				scalarRA2 := SNMPData{
+					oid:       ".6.0",
+					value:     "also scalar",
+					valueType: stringVal,
+				}
+				coidAttr11 := SNMPData{
+					columnOID: ".1",
+					oid:       ".1.1",
+					value:     "string1",
+					valueType: stringVal,
+				}
+				coidAttr12 := SNMPData{
+					columnOID: ".1",
+					oid:       ".1.2",
+					value:     "string2",
+					valueType: stringVal,
+				}
+				coid21 := SNMPData{
+					columnOID: ".2",
+					oid:       ".2.1",
+					value:     int64(3),
+					valueType: integerVal,
+				}
+				coid22 := SNMPData{
+					columnOID: ".2",
+					oid:       ".2.2",
+					value:     int64(4),
+					valueType: integerVal,
+				}
+				mockClient.On("Connect").Return(nil)
+				mockClient.On("Close").Return(nil)
+				mockClient.On("GetScalarData", []string{".5.0", ".6.0"}, mock.Anything).Return([]SNMPData{scalarRA1, scalarRA2}).Once()
+				mockClient.On("GetIndexedData", []string{".1"}, mock.Anything).Return([]SNMPData{coidAttr11, coidAttr12}).Once()
+				mockClient.On("GetIndexedData", []string{".2"}, mock.Anything).Return([]SNMPData{coid21, coid22}).Once()
+				scraper := &snmpScraper{
+					cfg: &Config{
+						ResourceAttributes: map[string]*ResourceAttributeConfig{
+							"rattr1": {
+								ScalarOID: ".5.0",
+							},
+							"rattr2": {
+								ScalarOID: ".6.0",
+							},
+						},
+						Attributes: map[string]*AttributeConfig{
+							"attr1": {
+								OID: ".1",
+							},
+						},
+						Metrics: map[string]*MetricConfig{
+							"metric1": {
+								Description: "test description",
+								Unit:        "By",
+								Gauge: &GaugeMetric{
+									ValueType: "int",
+								},
+								ColumnOIDs: []ColumnOID{
+									{
+										OID:                ".2",
+										ResourceAttributes: []string{"rattr1"},
+										Attributes: []Attribute{
+											{
+												Name: "attr1",
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+					settings: receivertest.NewNopCreateSettings(),
+					client:   mockClient,
+					logger:   zap.NewNop(),
+				}
+
+				expectedMetricGen := func(t *testing.T) pmetric.Metrics {
+					goldenPath := filepath.Join("testdata", "expected_metrics", "29_metric_does_not_use_all_available_scalar_RAs_golden.yaml")
+					expectedMetrics, err := golden.ReadMetrics(goldenPath)
+					require.NoError(t, err)
+					return expectedMetrics
+				}
+				expectedMetrics := expectedMetricGen(t)
+				metrics, err := scraper.scrape(context.Background())
+				require.NoError(t, err)
+				err = pmetrictest.CompareMetrics(expectedMetrics, metrics, pmetrictest.IgnoreTimestamp())
+				require.NoError(t, err)
+			},
+		},
 	}
 
 	for _, tc := range testCases {

receiver/snmpreceiver/testdata/expected_metrics/29_metric_does_not_use_all_available_scalar_RAs_golden.yaml
@@ -1,0 +1,28 @@
+resourceMetrics:
+  - resource:
+      attributes:
+        - key: rattr1
+          value:
+            stringValue: scalar
+    scopeMetrics:
+      - metrics:
+          - description: test description
+            gauge:
+              dataPoints:
+                - asInt: "3"
+                  attributes:
+                    - key: attr1
+                      value:
+                        stringValue: string1
+                  timeUnixNano: "1651783494931319000"
+                - asInt: "4"
+                  attributes:
+                    - key: attr1
+                      value:
+                        stringValue: string2
+                  timeUnixNano: "1651783494931319000"
+            name: metric1
+            unit: By
+        scope:
+          name: otelcol/snmpreceiver
+          version: latest