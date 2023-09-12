530db5c1066a3daa11f9fbcf4cde7e3333b60c69
Mackenzie <63265430+mackjmr@users.noreply.github.com>
2023-09-12 15:56:27 +0200

    Add Daemonset => Gateway Setup (#4376)
    * Add Daemonset => Gateway Setup
    
    * increase limits
    
    * reduce replicas to 3
    
    * reduce batch size to avoid 413
    
    * decrease send_batch_max_size
    
    * disable logs staging
    
    * reset mem
    
    * increase batch size
    
    * reincrease to 1000 for testing
    
    * reset send_batch_size
    
    * remove unnecessary config

--- ci/scripts/ci-deploy-ds-gateway.sh
+++ ci/scripts/ci-deploy-ds-gateway.sh
@@ -12,15 +12,24 @@ set -x
 install_collector() {
 	# Set the namespace and release name
 	release_name="opentelemetry-collector"
-	namespace="otel-gateway"
+	release_name_gateway="opentelemetry-collector-gateway"
+	namespace="otel-ds-gateway"
 
 	# if repo already exists, helm 3+ will skip
 	helm --debug repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
 
 	# --install will run `helm install` if not already present.
+    # gateway with DD exporter
+	helm --debug upgrade "${release_name_gateway}" -n "${namespace}" open-telemetry/opentelemetry-collector --install \
+		-f ./ci/values.yaml \
+		-f ./ci/values-ds-gateway.yaml \
+		--set-string image.tag="otelcolcontrib-v$CI_COMMIT_SHORT_SHA" \
+		--set-string image.repository="601427279990.dkr.ecr.us-east-1.amazonaws.com/otel-collector-contrib"
+
+    # daemonset with otlp exporter
 	helm --debug upgrade "${release_name}" -n "${namespace}" open-telemetry/opentelemetry-collector --install \
 		-f ./ci/values.yaml \
-		-f ./ci/values-gateway.yaml \
+		-f ./ci/values-otlp-col.yaml \
 		--set-string image.tag="otelcolcontrib-v$CI_COMMIT_SHORT_SHA" \
 		--set-string image.repository="601427279990.dkr.ecr.us-east-1.amazonaws.com/otel-collector-contrib"
 
--- ci/scripts/ci-deploy-staging.sh
+++ ci/scripts/ci-deploy-staging.sh
@@ -8,11 +8,11 @@
 set -euo pipefail
 IFS=$'\n\t'
 set -x
+namespace=$NAMESPACE
+values=$VALUES
 
 install_collector() {
-	# Set the namespace and release name
 	release_name="opentelemetry-collector"
-	namespace="otel-staging"
 
 	# if repo already exists, helm 3+ will skip
 	helm --debug repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
@@ -20,7 +20,7 @@ install_collector() {
 	# --install will run `helm install` if not already present.
 	helm --debug upgrade "${release_name}" -n "${namespace}" open-telemetry/opentelemetry-collector --install \
 		-f ./ci/values.yaml \
-		-f ./ci/values-staging.yaml \
+		-f "${values}" \
 		--set-string image.tag="otelcolcontrib-v$CI_COMMIT_SHORT_SHA" \
 		--set-string image.repository="601427279990.dkr.ecr.us-east-1.amazonaws.com/otel-collector-contrib"
 
