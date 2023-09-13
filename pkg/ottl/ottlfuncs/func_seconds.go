2a27159bdb1ef0c2c3e69b667694acd5ea22b17d
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2023-09-13 15:03:29 -0600

    [pkg/ottl] ottlarg struct tag  cleanup (#26669)
    **Description:** 
    Removes all instances of `ottlarg`. Followup to
    https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/25705
    
    **Link to tracking Issue:** <Issue number if applicable>
    
    https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/24874

--- pkg/ottl/ottlfuncs/func_seconds.go
+++ pkg/ottl/ottlfuncs/func_seconds.go
@@ -11,7 +11,7 @@ import (
 )
 
 type SecondsArguments[K any] struct {
-	Duration ottl.DurationGetter[K] `ottlarg:"0"`
+	Duration ottl.DurationGetter[K]
 }
 
 func NewSecondsFactory[K any]() ottl.Factory[K] {
