d9a52012805fc36a12e1d7bd271cdc17a1c1dd23
Cláudio Bastos <claudiobastos@gmail.com>
2023-09-21 07:16:55 +0000

    [exporter/loadbalancing] allow metrics routing (#26378)
    Closes #25858
    
    **Description:** Add metrics exporter that will balance its metrics
    considering new routing choices
    
    **Link to tracking Issue:** #25858
    
    **Testing:** Add tests for new routing choices and metrics exporter
    
    ---------
    
    Signed-off-by: Claudio B <claudiobastos@gmail.com>
    Co-authored-by: Juraci Paixão Kröhling <juraci.github@kroehling.de>

--- exporter/loadbalancingexporter/factory.go
+++ exporter/loadbalancingexporter/factory.go
@@ -25,6 +25,7 @@ func NewFactory() exporter.Factory {
 		createDefaultConfig,
 		exporter.WithTraces(createTracesExporter, metadata.TracesStability),
 		exporter.WithLogs(createLogsExporter, metadata.LogsStability),
+		exporter.WithMetrics(createMetricsExporter, metadata.MetricsStability),
 	)
 }
 
@@ -45,4 +46,8 @@ func createTracesExporter(_ context.Context, params exporter.CreateSettings, cfg
 
 func createLogsExporter(_ context.Context, params exporter.CreateSettings, cfg component.Config) (exporter.Logs, error) {
 	return newLogsExporter(params, cfg)
+}
+
+func createMetricsExporter(_ context.Context, params exporter.CreateSettings, cfg component.Config) (exporter.Metrics, error) {
+	return newMetricsExporter(params, cfg)
 }
