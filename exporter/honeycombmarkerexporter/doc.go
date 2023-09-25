8a1393f6b0938dd6b915ad6c9dae878c32e4c31e
fchikwekwe <faithchikwekwe01@gmail.com>
2023-09-25 11:55:32 -0400

    feat: add generated files for component

---
+++ exporter/honeycombmarkerexporter/doc.go
@@ -0,0 +1,7 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+//go:generate mdatagen metadata.yaml
+
+// Package honeycombmarkerexporter exports marker data to Honeycomb.
+package honeycombexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/honeycombmarkerexporter"
