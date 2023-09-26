eac1a3a8272b81481958d29a54039a76c883afac
Meenal Mathur <meenalmathur23@gmail.com>
2023-09-26 14:43:24 +0530

    Resource attribute added in kafka metrics (#34)

--- receiver/kafkametricsreceiver/consumer_scraper.go
+++ receiver/kafkametricsreceiver/consumer_scraper.go
@@ -161,6 +161,13 @@ func (s *consumerScraper) scrape(context.Context) (pmetric.Metrics, error) {
 		}
 	}
 
+	// add resource attributes
+	rb := s.mb.NewResourceBuilder()
+	rb.SetRuntimeMetricsKafka("true")
+	s.mb.EmitForResource(
+		metadata.WithResource(rb.Emit()),
+	)
+
 	return s.mb.Emit(), scrapeError
 }
 
