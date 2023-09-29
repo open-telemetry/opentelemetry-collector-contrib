09a44385e8c6018dfaee45a594b2ef430da75860
fchikwekwe <faithchikwekwe01@gmail.com>
2023-09-29 11:35:00 -0400

    fix: remove presets and allow configurable markers and rules

--- exporter/honeycombmarkerexporter/config.go
+++ exporter/honeycombmarkerexporter/config.go
@@ -11,13 +11,41 @@ import (
 
 // Config defines configuration for the Honeycomb Marker exporter.
 type Config struct {
-	APIKey  string  `mapstructure:"api_key"`
-	APIURL  string  `mapstructure:"api_url"`
-	Presets presets `mapstructure:"presets"`
+	// APIKey is the authentication token associated with the Honeycomb account.
+	APIKey string `mapstructure:"api_key"`
+
+	// API URL to use (defaults to https://api.honeycomb.io)
+	APIURL string `mapstructure:"api_url"`
+
+	// Markers is the list of markers to create
+	Markers []marker `mapstructure:"markers"`
 }
 
-type presets struct {
-	K8sEvents bool `mapstructure:"k8s_events"`
+type marker struct {
+	// MarkerType defines the type of marker.  Markers with the same type appear in Honeycomb with the same color
+	MarkerType string `mapstructure:"type"`
+
+	// MarkerColor is the color of the marker. Will only be used if the MarkerType does not already exist.
+	MarkerColor string `mapstructure:"color"`
+
+	// MessageField is the attribute that will be used as the message.
+	// If necessary the value will be converted to a string.
+	MessageField string `mapstructure:"message_field"`
+
+	// UrlField is the attribute that will be used as the url.
+	// If necessary the value will be converted to a string.
+	UrlField string `mapstructure:"url_field"`
+
+	// Rules are the OTTL rules that determine when a piece of telemetry should be turned into a Marker
+	Rules Rules `mapstructure:"rules"`
+}
+
+type Rules struct {
+	// ResourceConditions is the list of ottlresource conditions that determine a match
+	ResourceConditions []string `mapstructure:"resource_conditions"`
+
+	// LogConditions is the list of ottllog conditions that determine a match
+	LogConditions []string `mapstructure:"log_conditions"`
 }
 
 func (cfg *Config) Validate() error {
