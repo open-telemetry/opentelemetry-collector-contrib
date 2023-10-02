219bc57e34525092f3edb47a82704ccb434658d9
Dominik Rosiek <drosiek@sumologic.com>
2023-10-02 10:46:44 +0200

    chore: make goporto
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- exporter/sumologicexporter/internal/observability/observability.go
+++ exporter/sumologicexporter/internal/observability/observability.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package observability
+package observability // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/sumologicexporter/internal/observability"
 
 import (
 	"context"
