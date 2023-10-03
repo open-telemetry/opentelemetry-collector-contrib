73315bc3e4a93d2e014ffea30e74a88e6b3a69be
Curtis Robert <crobert@splunk.com>
2023-10-03 16:46:41 -0700

    Fix CICD failures
    - Lint fix
    - Test fix: Move accessory methods into utils helper for testability.

--- processor/resourcedetectionprocessor/internal/aws/eks/detector.go
+++ processor/resourcedetectionprocessor/internal/aws/eks/detector.go
@@ -39,6 +39,8 @@ const (
 
 type detectorUtils interface {
 	getConfigMap(ctx context.Context, namespace string, name string) (map[string]string, error)
+	getClusterName() (string, error)
+	getClusterNameTagFromReservations([]types.Reservation) string
 }
 
 type eksDetectorUtils struct {
@@ -85,50 +87,9 @@ func (d *detector) Detect(ctx context.Context) (resource pcommon.Resource, schem
 	// The error is unhandled because we want to return successfully detected resources
 	// regardless of an error. The caller will properly handle any error hit while getting
 	// the cluster name.
-	clusterName, err := d.getClusterName()
+	clusterName, err := d.utils.getClusterName()
 	d.rb.SetK8sClusterName(clusterName)
 	return d.rb.Emit(), conventions.SchemaURL, err
-}
-
-func (d *detector) getClusterName() (string, error) {
-	cfg, err := config.LoadDefaultConfig(context.TODO())
-	if err != nil {
-		return "", err
-	}
-
-	ec2IMS := imds.NewFromConfig(cfg)
-	regionOutput, err := ec2IMS.GetRegion(context.TODO(), &imds.GetRegionInput{})
-	if err != nil {
-		return "", err
-	}
-	cfg.Region = regionOutput.Region
-
-	client := ec2.NewFromConfig(cfg)
-	describeInstances, err := client.DescribeInstances(context.TODO(), &ec2.DescribeInstancesInput{})
-	if err != nil {
-		return "", err
-	}
-
-	clusterName := d.getClusterNameTagFromReservations(describeInstances.Reservations)
-
-	if len(clusterName) == 0 {
-		return clusterName, fmt.Errorf("Failed to detect EKS cluster name. No tag for cluster name found on EC2 instance.")
-	}
-	return clusterName, nil
-}
-
-func (d *detector) getClusterNameTagFromReservations(reservations []types.Reservation) string {
-	for _, reservation := range reservations {
-		for _, instance := range reservation.Instances {
-			for _, tag := range instance.Tags {
-				if *tag.Key == clusterNameAwsEksTag || *tag.Key == clusterNameEksTag {
-					return *tag.Value
-				}
-			}
-		}
-	}
-
-	return ""
 }
 
 func isEKS(ctx context.Context, utils detectorUtils) (bool, error) {
@@ -167,4 +128,45 @@ func (e eksDetectorUtils) getConfigMap(ctx context.Context, namespace string, na
 		return nil, fmt.Errorf("failed to retrieve ConfigMap %s/%s: %w", namespace, name, err)
 	}
 	return cm.Data, nil
+}
+
+func (e eksDetectorUtils) getClusterName() (string, error) {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		return "", err
+	}
+
+	ec2IMS := imds.NewFromConfig(cfg)
+	regionOutput, err := ec2IMS.GetRegion(context.TODO(), &imds.GetRegionInput{})
+	if err != nil {
+		return "", err
+	}
+	cfg.Region = regionOutput.Region
+
+	client := ec2.NewFromConfig(cfg)
+	describeInstances, err := client.DescribeInstances(context.TODO(), &ec2.DescribeInstancesInput{})
+	if err != nil {
+		return "", err
+	}
+
+	clusterName := e.getClusterNameTagFromReservations(describeInstances.Reservations)
+
+	if len(clusterName) == 0 {
+		return clusterName, fmt.Errorf("Failed to detect EKS cluster name. No tag for cluster name found on EC2 instance")
+	}
+	return clusterName, nil
+}
+
+func (e eksDetectorUtils) getClusterNameTagFromReservations(reservations []types.Reservation) string {
+	for _, reservation := range reservations {
+		for _, instance := range reservation.Instances {
+			for _, tag := range instance.Tags {
+				if *tag.Key == clusterNameAwsEksTag || *tag.Key == clusterNameEksTag {
+					return *tag.Value
+				}
+			}
+		}
+	}
+
+	return ""
 }
--- processor/resourcedetectionprocessor/internal/aws/eks/detector_test.go
+++ processor/resourcedetectionprocessor/internal/aws/eks/detector_test.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
@@ -16,6 +17,11 @@ import (
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/resourcedetectionprocessor/internal/aws/eks/internal/metadata"
 )
 
+const (
+	clusterNameKey = "k8s.cluster.name"
+	clusterName    = "my-cluster"
+)
+
 type MockDetectorUtils struct {
 	mock.Mock
 }
@@ -23,6 +29,15 @@ type MockDetectorUtils struct {
 func (detectorUtils *MockDetectorUtils) getConfigMap(_ context.Context, namespace string, name string) (map[string]string, error) {
 	args := detectorUtils.Called(namespace, name)
 	return args.Get(0).(map[string]string), args.Error(1)
+}
+
+func (detectorUtils *MockDetectorUtils) getClusterName() (string, error) {
+	var reservations []types.Reservation
+	return detectorUtils.getClusterNameTagFromReservations(reservations), nil
+}
+
+func (detectorUtils *MockDetectorUtils) getClusterNameTagFromReservations(_ []types.Reservation) string {
+	return clusterName
 }
 
 func TestNewDetector(t *testing.T) {
@@ -38,7 +53,7 @@ func TestEKS(t *testing.T) {
 	ctx := context.Background()
 
 	t.Setenv("KUBERNETES_SERVICE_HOST", "localhost")
-	detectorUtils.On("getConfigMap", authConfigmapNS, authConfigmapName).Return(map[string]string{"cluster.name": "my-cluster"}, nil)
+	detectorUtils.On("getConfigMap", authConfigmapNS, authConfigmapName).Return(map[string]string{clusterNameKey: clusterName}, nil)
 	// Call EKS Resource detector to detect resources
 	eksResourceDetector := &detector{utils: detectorUtils, err: nil, rb: metadata.NewResourceBuilder(metadata.DefaultResourceAttributesConfig())}
 	res, _, err := eksResourceDetector.Detect(ctx)
@@ -47,6 +62,7 @@ func TestEKS(t *testing.T) {
 	assert.Equal(t, map[string]interface{}{
 		"cloud.provider": "aws",
 		"cloud.platform": "aws_eks",
+		clusterNameKey:   clusterName,
 	}, res.Attributes().AsRaw(), "Resource object returned is incorrect")
 }
 
