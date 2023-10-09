90328eb5b3e861e73f07db61bff9f2d9e38b1c17
Vihas Splunk <vhiteshbhai@splunk.com>
2023-10-09 16:05:42 +0530

    fix: add header

--- extension/encoding/jsonencodingextension/config.go
+++ extension/encoding/jsonencodingextension/config.go
@@ -1,3 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
 package jsonencodingextension // import "github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding/jsonencodingextension"
 
 type Config struct{}
