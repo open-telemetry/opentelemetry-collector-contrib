5dc7abb982020794da6b2b1493a98cb1abcbdf05
dao-jun <daojun@apache.org>
2023-10-11 05:34:06 +0800

    fix license header.

--- extension/encoding/jaegerencodingextension/buffer_test.go
+++ extension/encoding/jaegerencodingextension/buffer_test.go
@@ -1,4 +1,7 @@
-package jaegerencodingextension
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package jaegerencodingextension // import "github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding/jaegerencodingextension"
 
 import (
 	"fmt"
