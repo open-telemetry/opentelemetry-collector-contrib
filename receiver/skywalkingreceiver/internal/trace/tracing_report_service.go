ac9a655d27bf41cb72818fcba01e08f71fec3b52
maokitty <lwc1029872701@gmail.com>
2023-10-10 19:41:36 +0800

    add obsreport of skywalkingreceiver

--- receiver/skywalkingreceiver/internal/trace/tracing_report_service.go
+++ receiver/skywalkingreceiver/internal/trace/tracing_report_service.go
@@ -69,7 +69,7 @@ func (r *Receiver) Collect(stream agent.TraceSegmentReportService_CollectServer)
 			return err
 		}
 
-		err = consumeTraces(stream.Context(), segmentObject, r.nextConsumer)
+		err = consumeTraces(stream.Context(), segmentObject, r.nextConsumer, r.grpcObsrecv)
 		if err != nil {
 			return stream.SendAndClose(&common.Commands{})
 		}
@@ -83,7 +83,7 @@ func (r *Receiver) CollectInSync(ctx context.Context, segments *agent.SegmentCol
 		if err != nil {
 			fmt.Printf("cannot marshal segemnt from sync, %v", err)
 		}
-		err = consumeTraces(ctx, segment, r.nextConsumer)
+		err = consumeTraces(ctx, segment, r.nextConsumer, r.grpcObsrecv)
 		if err != nil {
 			fmt.Printf("cannot consume traces, %v", err)
 		}
@@ -92,12 +92,15 @@ func (r *Receiver) CollectInSync(ctx context.Context, segments *agent.SegmentCol
 	return &common.Commands{}, nil
 }
 
-func consumeTraces(ctx context.Context, segment *agent.SegmentObject, consumer consumer.Traces) error {
+func consumeTraces(ctx context.Context, segment *agent.SegmentObject, consumer consumer.Traces, obsreport  *receiverhelper.ObsReport) error {
 	if segment == nil {
 		return nil
 	}
 	ptd := SkywalkingToTraces(segment)
-	return consumer.ConsumeTraces(ctx, ptd)
+	obsreport.StartTracesOp(ctx)
+	err := consumer.ConsumeTraces(ctx, ptd)
+	obsreport.EndTracesOp(ctx,"protobuf",ptd.SpanCount(), err)
+	return err
 }
 
 func (r *Receiver) HTTPHandler(rsp http.ResponseWriter, req *http.Request) {
@@ -114,7 +117,7 @@ func (r *Receiver) HTTPHandler(rsp http.ResponseWriter, req *http.Request) {
 	}
 
 	for _, segment := range data {
-		err = consumeTraces(req.Context(), segment, r.nextConsumer)
+		err = consumeTraces(req.Context(), segment, r.nextConsumer, r.httpObsrecv)
 		if err != nil {
 			fmt.Printf("cannot consume traces, %v", err)
 		}
