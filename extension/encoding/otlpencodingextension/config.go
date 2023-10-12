8eb56c82fdcb93503c071a7c4b4fa404fd38ed24
dao-jun <daojun@apache.org>
2023-10-13 01:51:25 +0800

    run make goproto

--- extension/encoding/otlpencodingextension/config.go
+++ extension/encoding/otlpencodingextension/config.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package otlpencodingextension // import "otlpencodingextension"
+package otlpencodingextension // import "github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding/otlpencodingextension"
 import (
 	"fmt"
 
--- extension/encoding/zipkinencodingextension/config.go
+++ extension/encoding/zipkinencodingextension/config.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package zipkinencodingextension // import "zipkinencodingextension"
+package zipkinencodingextension // import "github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding/zipkinencodingextension"
 import (
 	"fmt"
 
