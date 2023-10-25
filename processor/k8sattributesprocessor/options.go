37a972f7f7f5163868c75f6e0b79dea27a7de2c8
haoqixu <hq.xu0o0@gmail.com>
2023-10-26 00:09:18 +0800

    update as suggested

--- processor/k8sattributesprocessor/options.go
+++ processor/k8sattributesprocessor/options.go
@@ -4,9 +4,9 @@
 package k8sattributesprocessor // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/k8sattributesprocessor"
 
 import (
+	"fmt"
 	"os"
 	"regexp"
-	"strings"
 
 	conventions "go.opentelemetry.io/collector/semconv/v1.6.1"
 	"k8s.io/apimachinery/pkg/selection"
@@ -212,7 +212,7 @@ func extractFieldRules(fieldType string, fields ...FieldExtractConfig) ([]kube.F
 
 		if name == "" && a.Key != "" {
 			// name for KeyRegex case is set at extraction time/runtime, skipped here
-			name = strings.Join([]string{"k8s", a.From, fieldType, a.Key}, ".")
+			name = fmt.Sprintf("k8s.%v.%v.%v", a.From, fieldType, a.Key)
 		}
 
 		var r *regexp.Regexp
