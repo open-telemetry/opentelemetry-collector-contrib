b30eea0c80e828076349edc6851abbad092eb343
Vihas Splunk <vhiteshbhai@splunk.com>
2023-10-26 01:24:05 +0530

    chore: add support for json codec in jaeger

--- extension/encoding/jaegerencodingextension/config.go
+++ extension/encoding/jaegerencodingextension/config.go
@@ -10,6 +10,7 @@ type JaegerProtocol string
 
 const (
 	JaegerProtocolProtobuf JaegerProtocol = "protobuf"
+	JaegerProtocolJson     JaegerProtocol = "json"
 )
 
 type Config struct {
@@ -19,6 +20,7 @@ type Config struct {
 func (c *Config) Validate() error {
 	switch c.Protocol {
 	case JaegerProtocolProtobuf:
+	case JaegerProtocolJson:
 	default:
 		return fmt.Errorf("invalid protocol %q", c.Protocol)
 	}
