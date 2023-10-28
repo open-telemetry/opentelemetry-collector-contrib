0d27a442930846b7de03b3771970479e0d96ecd8
Juraj Michalek <juraj.michalek@matillion.com>
2023-10-28 22:34:15 +0200

    chore: add missing license to the file

--- pkg/translator/prometheusremotewrite/otlp_to_openmetrics_metadata.go
+++ pkg/translator/prometheusremotewrite/otlp_to_openmetrics_metadata.go
@@ -1,3 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
 package prometheusremotewrite // import "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/translator/prometheusremotewrite"
 
 import (
