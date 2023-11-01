3e30dd0b899884085695f4964bb15e531948735f
Ishmeet <ishmeetsinghis@gmail.com>
2023-11-01 18:25:15 +0530

    made ut and lint happy

--- receiver/sflowreceiver/doc.go
+++ receiver/sflowreceiver/doc.go
@@ -1,3 +1,5 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
 //go:generate mdatagen metadata.yaml
 
 package sflowreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/sflowreceiver"
--- receiver/sflowreceiver/receiver_test.go
+++ receiver/sflowreceiver/receiver_test.go
@@ -1,17 +1,5 @@
 // Copyright The OpenTelemetry Authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//	http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License. language governing permissions and
-// limitations under the License.
+// SPDX-License-Identifier: Apache-2.0
 package sflowreceiver
 
 import (
@@ -104,7 +92,8 @@ func Test_sflowreceiverlogs_Start(t *testing.T) {
 			time.Sleep(1 * time.Second)
 			<-tt.args.ctx.Done()
 
-			err := s.Shutdown(tt.args.ctx)
+			ctx, _ := context.WithTimeout(context.Background(), 50*time.Millisecond)
+			err := s.Shutdown(ctx)
 			assert.Nil(t, err)
 		})
 	}
