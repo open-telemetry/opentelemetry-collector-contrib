open-telemetry/opentelemetry-collector-contrib

commit 5ab2db614bd8d67219a4e79fd65422a38876399a

Remove retry_on_failure from the googlecloud exporters (#25900)
**Description:**

In
https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/19203,
we disabled retry_on_failure by default because retry is now handled by
the google cloud client libraries.

However, many users still enable it because the error message encourages
users to enable retry_on_failure. This is causing problems, especially
with metrics because retrying a batch of metrics produces confusing
errors (like out-of-order or duplicate timeseries errors) and spams
users logs more than necessary.

This PR removes the retry_on_failure config from the GMP and googlecloud
exporters.

exporter/googlecloudexporter/go.mod
@@ -4,7 +4,6 @@ go 1.20
 
 require (
 	github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/collector v0.42.0
-	github.com/cenkalti/backoff/v4 v4.2.1
 	github.com/stretchr/testify v1.8.4
 	go.opentelemetry.io/collector/component v0.83.0
 	go.opentelemetry.io/collector/confmap v0.83.0
@@ -22,6 +21,7 @@ require (
 	cloud.google.com/go/trace v1.10.1 // indirect
 	github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/trace v1.18.0 // indirect
 	github.com/GoogleCloudPlatform/opentelemetry-operations-go/internal/resourcemapping v0.42.0 // indirect
+	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-logr/logr v1.2.4 // indirect

exporter/googlemanagedprometheusexporter/go.mod
@@ -5,7 +5,6 @@ go 1.20
 require (
 	github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/collector v0.42.0
 	github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/collector/googlemanagedprometheus v0.42.0
-	github.com/cenkalti/backoff/v4 v4.2.1
 	github.com/stretchr/testify v1.8.4
 	go.opentelemetry.io/collector v0.83.0
 	go.opentelemetry.io/collector/component v0.83.0
@@ -25,6 +24,7 @@ require (
 	github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/trace v1.18.0 // indirect
 	github.com/GoogleCloudPlatform/opentelemetry-operations-go/internal/resourcemapping v0.42.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect

.chloggen/remove-gcp-retryonfailure.yaml
@@ -1,0 +1,27 @@
+# Use this changelog template to create an entry for release notes.
+
+# One of 'breaking', 'deprecation', 'new_component', 'enhancement', 'bug_fix'
+change_type: breaking
+
+# The name of the component, or a single word describing the area of concern, (e.g. filelogreceiver)
+component: googlecloudexporter
+
+# A brief description of the change.  Surround your text with quotes ("") if it needs to start with a backtick (`).
+note: remove retry_on_failure from the googlecloud exporter. The exporter itself handles retries, and retrying can cause issues.
+
+# Mandatory: One or more tracking issues related to the change. You can use the PR number here if no issue exists.
+issues: [57233]
+
+# (Optional) One or more lines of additional information to render under the primary note.
+# These lines will be padded with 2 spaces and then inserted directly into the document.
+# Use pipe (|) for multiline entries.
+subtext:
+
+# If your change doesn't affect end users or the exported elements of any package,
+# you should instead start your pull request title with [chore] or use the "Skip Changelog" label.
+# Optional: The change log or logs in which this entry should be included.
+# e.g. '[user]' or '[user, api]'
+# Include 'user' if the change is relevant to end users.
+# Include 'api' if there is a change to a library API.
+# Default: '[user]'
+change_logs: [user, api]

exporter/googlecloudexporter/config.go
@@ -17,7 +17,6 @@ type Config struct {
 	// Timeout for all API calls. If not set, defaults to 12 seconds.
 	exporterhelper.TimeoutSettings `mapstructure:",squash"` // squash ensures fields are correctly decoded in embedded struct.
 	exporterhelper.QueueSettings   `mapstructure:"sending_queue"`
-	exporterhelper.RetrySettings   `mapstructure:"retry_on_failure"`
 }
 
 func (cfg *Config) Validate() error {

exporter/googlecloudexporter/config_test.go
@@ -9,7 +9,6 @@ import (
 	"time"
 
 	"github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/collector"
-	"github.com/cenkalti/backoff/v4"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component"
@@ -64,14 +63,6 @@ func TestLoadConfig(t *testing.T) {
 						UseInsecure: true,
 					},
 				},
-			},
-			RetrySettings: exporterhelper.RetrySettings{
-				Enabled:             true,
-				InitialInterval:     10 * time.Second,
-				MaxInterval:         1 * time.Minute,
-				MaxElapsedTime:      10 * time.Minute,
-				RandomizationFactor: backoff.DefaultRandomizationFactor,
-				Multiplier:          backoff.DefaultMultiplier,
 			},
 			QueueSettings: exporterhelper.QueueSettings{
 				Enabled:      true,

exporter/googlecloudexporter/factory.go
@@ -43,11 +43,8 @@ func NewFactory() exporter.Factory {
 
 // createDefaultConfig creates the default configuration for exporter.
 func createDefaultConfig() component.Config {
-	retrySettings := exporterhelper.NewDefaultRetrySettings()
-	retrySettings.Enabled = false
 	return &Config{
 		TimeoutSettings: exporterhelper.TimeoutSettings{Timeout: defaultTimeout},
-		RetrySettings:   retrySettings,
 		QueueSettings:   exporterhelper.NewDefaultQueueSettings(),
 		Config:          collector.DefaultConfig(),
 	}
@@ -71,8 +68,7 @@ func createLogsExporter(
 		// Disable exporterhelper Timeout, since we are using a custom mechanism
 		// within exporter itself
 		exporterhelper.WithTimeout(exporterhelper.TimeoutSettings{Timeout: 0}),
-		exporterhelper.WithQueue(eCfg.QueueSettings),
-		exporterhelper.WithRetry(eCfg.RetrySettings))
+		exporterhelper.WithQueue(eCfg.QueueSettings))
 }
 
 // createTracesExporter creates a trace exporter based on this config.
@@ -94,8 +90,7 @@ func createTracesExporter(
 		// Disable exporterhelper Timeout, since we are using a custom mechanism
 		// within exporter itself
 		exporterhelper.WithTimeout(exporterhelper.TimeoutSettings{Timeout: 0}),
-		exporterhelper.WithQueue(eCfg.QueueSettings),
-		exporterhelper.WithRetry(eCfg.RetrySettings))
+		exporterhelper.WithQueue(eCfg.QueueSettings))
 }
 
 // createMetricsExporter creates a metrics exporter based on this config.
@@ -117,6 +112,5 @@ func createMetricsExporter(
 		// Disable exporterhelper Timeout, since we are using a custom mechanism
 		// within exporter itself
 		exporterhelper.WithTimeout(exporterhelper.TimeoutSettings{Timeout: 0}),
-		exporterhelper.WithQueue(eCfg.QueueSettings),
-		exporterhelper.WithRetry(eCfg.RetrySettings))
+		exporterhelper.WithQueue(eCfg.QueueSettings))
 }

exporter/googlecloudexporter/testdata/config.yaml
@@ -7,11 +7,6 @@ googlecloud/customname:
     enabled: true
     num_consumers: 2
     queue_size: 10
-  retry_on_failure:
-    enabled: true
-    initial_interval: 10s
-    max_interval: 60s
-    max_elapsed_time: 10m
   metric:
     prefix: prefix
     skip_create_descriptor: true

exporter/googlecloudexporter/testdata/legacyconfig.yaml
@@ -1,45 +1,0 @@
-receivers:
-  nop:
-
-processors:
-  nop:
-
-exporters:
-  googlecloud:
-  googlecloud/customname:
-    project: my-project
-    endpoint: test-endpoint
-    user_agent: opentelemetry-collector-contrib {{version}}
-    use_insecure: true
-    timeout: 20s
-    resource_mappings:
-      - source_type: source.resource1
-        target_type: target-resource1
-        label_mappings:
-          - source_key: contrib.opencensus.io/exporter/googlecloud/project_id
-            target_key: project_id
-            optional: true
-          - source_key: source.label1
-            target_key: target_label_1
-      - source_type: source.resource2
-        target_type: target-resource2
-    sending_queue:
-      enabled: true
-      num_consumers: 2
-      queue_size: 10
-    retry_on_failure:
-      enabled: true
-      initial_interval: 10s
-      max_interval: 60s
-      max_elapsed_time: 10m
-    metric:
-      prefix: prefix
-      skip_create_descriptor: true
-
-service:
-  pipelines:
-    traces:
-        receivers: [nop]
-        processors: [nop]
-        exporters: [googlecloud]
-

commit 1bfbb1832d67384f6eff8dfddda720f1b19c7ffb

Retract googlecloud exporter releases that don't have logging (#28884)
**Description:**

Logging was broken after
https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/25900
(released in v0.84.0). It is fixed by
https://github.com/open-telemetry/opentelemetry-collector/pull/8792,
which will be released in v0.89.0. This will help with any distributions
that include the googlecloud exporter components.

exporter/googlecloudexporter/go.mod
@@ -79,6 +79,11 @@ require (
 )
 
 retract (
+	v0.88.0
+	v0.87.0
+	v0.86.0
+	v0.85.0
+	v0.84.0
 	v0.76.2
 	v0.76.1
 	v0.65.0

exporter/googlemanagedprometheusexporter/go.mod
@@ -124,6 +124,11 @@ require (
 )
 
 retract (
+	v0.88.0
+	v0.87.0
+	v0.86.0
+	v0.85.0
+	v0.84.0
 	v0.76.2
 	v0.76.1
 	v0.65.0