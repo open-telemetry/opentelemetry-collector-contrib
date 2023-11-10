68182eeb632224fe2dddd05ae9a5e162d4569874
Alex Boten <alex@boten.ca>
2023-11-10 13:01:32 -0800

    Update receiver/filelogreceiver/doc.go

--- receiver/filelogreceiver/doc.go
+++ receiver/filelogreceiver/doc.go
@@ -4,5 +4,5 @@
 //go:generate mdatagen metadata.yaml
 
 // Package filelogreceiver implements a receiver that can be used by the
-// Opentelemetry collector to receive logs using the stanza log agent
+// OpenTelemetry collector to receive logs using the stanza log agent
 package filelogreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/filelogreceiver"
