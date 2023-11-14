1cdf013ee2a6e3380a39a0073bc95fd65647b451
fchikwekwe <faithchikwekwe01@gmail.com>
2023-11-14 15:33:01 -0500

    fix: remove whitespace

--- cmd/otelcontribcol/exporters_test.go
+++ cmd/otelcontribcol/exporters_test.go
@@ -16,7 +16,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component"
-
 	"go.opentelemetry.io/collector/config/configgrpc"
 	"go.opentelemetry.io/collector/config/configopaque"
 	"go.opentelemetry.io/collector/exporter"
