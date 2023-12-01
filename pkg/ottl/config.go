9b0d627666da38ca3a446ae8a769799e6fd502e3
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2023-11-30 17:07:42 -0700

    run make goporto

--- pkg/ottl/config.go
+++ pkg/ottl/config.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package ottl
+package ottl // import "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl"
 
 import (
 	"fmt"
