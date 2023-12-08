2f4965fc88b1699f506322963ca9c93090a138a9
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2023-12-07 20:25:32 -0700

    Add more tests

--- pkg/ottl/functions_test.go
+++ pkg/ottl/functions_test.go
@@ -2185,6 +2185,80 @@ func Test_newPath(t *testing.T) {
 	assert.Nil(t, p.nextPath)
 }
 
+func Test_baseKey_String(t *testing.T) {
+	bp := baseKey{
+		s: ottltest.Strp("test"),
+	}
+	assert.Equal(t, "test", *bp.String())
+}
+
+func Test_baseKey_Int(t *testing.T) {
+	bp := baseKey{
+		i: ottltest.Intp(1),
+	}
+	assert.Equal(t, int64(1), *bp.Int())
+}
+
+func Test_baseKey_Next(t *testing.T) {
+	bp := baseKey{
+		nextKey: &baseKey{},
+	}
+	next := bp.Next()
+	assert.NotNil(t, next)
+	assert.Nil(t, next.Next())
+}
+
+func Test_baseKey_isComplete(t *testing.T) {
+	tests := []struct {
+		name          string
+		p             baseKey
+		expectedError bool
+	}{
+		{
+			name: "fetched no next",
+			p: baseKey{
+				fetched: true,
+			},
+		},
+		{
+			name: "fetched with next",
+			p: baseKey{
+				fetched: true,
+				nextKey: &baseKey{
+					fetched: true,
+				},
+			},
+		},
+		{
+			name: "not fetched no next",
+			p: baseKey{
+				fetched: false,
+			},
+			expectedError: true,
+		},
+		{
+			name: "not fetched with next",
+			p: baseKey{
+				fetched: true,
+				nextKey: &baseKey{
+					fetched: false,
+				},
+			},
+			expectedError: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.p.isComplete()
+			if tt.expectedError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
 func Test_newKey(t *testing.T) {
 	keys := []Key{
 		{
