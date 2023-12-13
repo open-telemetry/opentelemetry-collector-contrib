c326bc25d8996345c20e5d591277ffd9796beb49
Dan Jaglowski <jaglows3@gmail.com>
2023-12-13 08:19:50 -0800

    [chore] Expose namedpipe config on all platforms

--- receiver/namedpipereceiver/namedpipe_test.go
+++ receiver/namedpipereceiver/namedpipe_test.go
@@ -1,6 +1,9 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+//go:build linux
+// +build linux
+
 package namedpipereceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/namedpipereceiver"
 
 import (
