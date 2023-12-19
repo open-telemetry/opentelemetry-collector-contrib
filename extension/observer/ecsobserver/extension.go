cebc4389e919b91087a718b979bef59651f4325c
Antoine Toulme <antoine@lunar-ocean.com>
2023-12-19 09:16:04 -0800

    [chore] use ReportComponentStatus (#29907)

--- extension/observer/ecsobserver/extension.go
+++ extension/observer/ecsobserver/extension.go
@@ -15,31 +15,31 @@ var _ extension.Extension = (*ecsObserver)(nil)
 
 // ecsObserver implements component.ServiceExtension interface.
 type ecsObserver struct {
-	logger *zap.Logger
-	sd     *serviceDiscovery
+	telemetrySettings component.TelemetrySettings
+	sd                *serviceDiscovery
 
 	// for Shutdown
 	cancel func()
 }
 
 // Start runs the service discovery in background
-func (e *ecsObserver) Start(_ context.Context, host component.Host) error {
-	e.logger.Info("Starting ECSDiscovery")
+func (e *ecsObserver) Start(_ context.Context, _ component.Host) error {
+	e.telemetrySettings.Logger.Info("Starting ECSDiscovery")
 	// Ignore the ctx parameter as it is not for long running operation
 	ctx, cancel := context.WithCancel(context.Background())
 	e.cancel = cancel
 	go func() {
 		if err := e.sd.runAndWriteFile(ctx); err != nil {
-			e.logger.Error("ECSDiscovery stopped by error", zap.Error(err))
+			e.telemetrySettings.Logger.Error("ECSDiscovery stopped by error", zap.Error(err))
 			// Stop the collector
-			host.ReportFatalError(err)
+			_ = e.telemetrySettings.ReportComponentStatus(component.NewFatalErrorEvent(err))
 		}
 	}()
 	return nil
 }
 
 func (e *ecsObserver) Shutdown(_ context.Context) error {
-	e.logger.Info("Stopping ECSDiscovery")
+	e.telemetrySettings.Logger.Info("Stopping ECSDiscovery")
 	e.cancel()
 	return nil
 }
