925f1bcd50cd0fc465534b48aaa79fbca7149b56
Antoine Toulme <antoine@lunar-ocean.com>
2024-01-11 18:46:24 -0800

    [chore] fix invalid comment

--- exporter/splunkhecexporter/batchperscope_test.go
+++ exporter/splunkhecexporter/batchperscope_test.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package splunkhecexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/batchperresourceattr"
+package splunkhecexporter
 
 import (
 	"context"
