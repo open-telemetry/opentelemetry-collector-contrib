3e8ad65b125fd429fd8f822851654c6c3b2bb431
Joshua MacDonald <josh.macdonald@gmail.com>
2024-01-24 15:09:04 -0800

    lint

--- receiver/otelarrowreceiver/internal/metadata/generated_status.go
+++ receiver/otelarrowreceiver/internal/metadata/generated_status.go
@@ -4,11 +4,21 @@ package metadata
 
 import (
 	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/trace"
 )
 
 const (
 	Type             = "otelarrow"
-	MetricsStability = component.StabilityLevelDevelopment
 	TracesStability  = component.StabilityLevelDevelopment
+	MetricsStability = component.StabilityLevelDevelopment
 	LogsStability    = component.StabilityLevelDevelopment
 )
+
+func Meter(settings component.TelemetrySettings) metric.Meter {
+	return settings.MeterProvider.Meter("otelcol/otelarrowreceiver")
+}
+
+func Tracer(settings component.TelemetrySettings) trace.Tracer {
+	return settings.TracerProvider.Tracer("otelcol/otelarrowreceiver")
+}
