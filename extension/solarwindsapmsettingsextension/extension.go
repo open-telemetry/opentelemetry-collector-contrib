f4a7f6189888a906503e64719368f53112bfe0ad
Jerry Leung <tingfung.leung@solarwinds.com>
2024-01-24 20:38:53 -0800

    Added extension function layout

--- extension/solarwindsapmsettingsextension/extension.go
+++ extension/solarwindsapmsettingsextension/extension.go
@@ -5,16 +5,31 @@ package solarwindsapmsettingsextension // import "github.com/open-telemetry/open
 
 import (
 	"context"
+	"crypto/tls"
+	"errors"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"time"
 
+	"github.com/solarwindscloud/apm-proto/go/collectorpb"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/extension"
 	"go.uber.org/zap"
+)
+
+const (
+	RawOutputFile   = "/tmp/solarwinds-apm-settings-raw"
+	JSONOutputFile  = "/tmp/solarwinds-apm-settings.json"
+	MinimumInterval = "5s"
+	MaximumInterval = "60s"
 )
 
 type solarwindsapmSettingsExtension struct {
 	logger *zap.Logger
 	config *Config
 	cancel context.CancelFunc
+	conn   *grpc.ClientConn
+	client collectorpb.TraceCollectorClient
 }
 
 func newSolarwindsApmSettingsExtension(extensionCfg *Config, logger *zap.Logger) (extension.Extension, error) {
@@ -25,13 +40,63 @@ func newSolarwindsApmSettingsExtension(extensionCfg *Config, logger *zap.Logger)
 	return settingsExtension, nil
 }
 
-func (extension *solarwindsapmSettingsExtension) Start(_ context.Context, _ component.Host) error {
+func (extension *solarwindsapmSettingsExtension) Start(ctx context.Context, _ component.Host) error {
 	extension.logger.Debug("Starting up solarwinds apm settings extension")
-	_, extension.cancel = context.WithCancel(context.Background())
+	ctx = context.Background()
+	ctx, extension.cancel = context.WithCancel(ctx)
+
+	var err error
+	extension.conn, err = grpc.Dial(extension.config.Endpoint, grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{})))
+	if err != nil {
+		return errors.New("Failed to dial: " + err.Error())
+	} else {
+		extension.logger.Info("Dailed to " + extension.config.Endpoint)
+	}
+	extension.client = collectorpb.NewTraceCollectorClient(extension.conn)
+
+	task(extension)
+
+	// setup lightweight thread to refresh
+	interval, _ := time.ParseDuration(extension.config.Interval)
+	go func() {
+		ticker := time.NewTicker(interval)
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ticker.C:
+				task(extension)
+			case <-ctx.Done():
+				extension.logger.Info("Received ctx.Done() from ticker")
+				return
+			}
+		}
+	}()
+
 	return nil
 }
 
 func (extension *solarwindsapmSettingsExtension) Shutdown(_ context.Context) error {
 	extension.logger.Debug("Shutting down solarwinds apm settings extension")
+	err := extension.conn.Close()
+	if err != nil {
+		return errors.New("Failed to close the gRPC connection to solarwinds APM collector " + err.Error())
+	}
 	return nil
+}
+
+func task(extension *solarwindsapmSettingsExtension) {
+	if validateSolarwindsApmSettingsExtensionConfiguration(extension.config, extension.logger) {
+		refresh(extension)
+	} else {
+		extension.logger.Warn("No refresh due to invalid config value")
+	}
+}
+
+func validateSolarwindsApmSettingsExtensionConfiguration(extensionCfg *Config, logger *zap.Logger) bool {
+	// Concrete implementation will be available in next PR
+	return false
+}
+
+func refresh(extension *solarwindsapmSettingsExtension) {
+	// Concrete implementation will be available in next PR
 }
--- extension/solarwindsapmsettingsextension/go.mod
+++ extension/solarwindsapmsettingsextension/go.mod
@@ -3,12 +3,15 @@ module github.com/open-telemetry/opentelemetry-collector-contrib/extension/solar
 go 1.20
 
 require (
+	github.com/solarwindscloud/apm-proto v0.0.0-20231107001908-432e697887b6
 	github.com/stretchr/testify v1.8.4
 	go.opentelemetry.io/collector/component v0.93.0
+	go.opentelemetry.io/collector/confmap v0.93.0
 	go.opentelemetry.io/collector/extension v0.93.0
 	go.opentelemetry.io/otel/metric v1.22.0
 	go.opentelemetry.io/otel/trace v1.22.0
 	go.uber.org/zap v1.26.0
+	google.golang.org/grpc v1.60.1
 )
 
 require (
@@ -38,7 +41,6 @@ require (
 	github.com/prometheus/statsd_exporter v0.22.7 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/collector/config/configtelemetry v0.93.0 // indirect
-	go.opentelemetry.io/collector/confmap v0.93.0 // indirect
 	go.opentelemetry.io/collector/featuregate v1.0.1 // indirect
 	go.opentelemetry.io/collector/pdata v1.0.1 // indirect
 	go.opentelemetry.io/otel v1.22.0 // indirect
@@ -50,7 +52,6 @@ require (
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20231002182017-d307bd883b97 // indirect
-	google.golang.org/grpc v1.60.1 // indirect
 	google.golang.org/protobuf v1.32.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
