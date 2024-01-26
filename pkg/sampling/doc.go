c8c196d676abd8d032c8fbaa2060ef7cea49969e
Joshua MacDonald <josh.macdonald@gmail.com>
2024-01-26 06:15:06 -0800

    Add higher-level example in doc comment

--- pkg/sampling/doc.go
+++ pkg/sampling/doc.go
@@ -29,6 +29,61 @@
 //   - [Randomness]: Randomness used for sampling decisions.
 //   - [Threshold.Probability]: a float64 in the range [MinSamplingProbability, 1.0].
 //
+// # Example use-case
+//
+// To configure a consistent tail sampler in an OpenTelemetry
+// Collector using a fixed probability for all traces in an
+// "equalizing" arrangement, where the effect og sampling is
+// conditioned on how much sampling has already taken place, use the
+// following pseudocode.
+//
+//	func Setup() {
+//		// Get a fixed probability value from the configuration, in
+//		// the range (0, 1].
+//		probability := *FLAG_probability
+//
+//		// Calculate the sampling threshold from probability using 3
+//		// hex digits of precision.
+//		fixedThreshold, err = ProbabilityToThresholdWithPrecision(probability, 3)
+//		if err != nil {
+//			// error case: Probability is not valid.
+//		}
+//	}
+//
+//	func MakeDecision(tracestate string, tid TraceID) bool {
+//		// Parse the incoming tracestate
+//		ts, err := NewW3CTraceState(tracestate)
+//		if err != nil {
+//			// error case: Tracestate is ill-formed.
+//		}
+//		// For an absolute probability sample, we check the incoming
+//		// tracestate to see whether it was already sampled enough.
+//		if ts.OTelValue().HasTValue() {
+//			// If the incoming tracestate was already sampled at
+//			// least as much as our threshold implies, then its
+//			// (rejection) threshold is higher.  If so, then no
+//			// further sampling is called for.
+//			if ThresholdGreater(ts.OTelValue().TValueThreshold(), fixedThreshold) {
+//				return true
+//			}
+//		}
+//		var rnd Randomness
+//		// If the R-value is present, use it.  If not, rely on TraceID
+//		// randomness.  Note that OTLP v1.1.0 introduces a new Span flag
+//		// to convey trace randomness correctly, and if the context has
+//		// neither the randomness bit set or the R-value set, we need a
+//		// fallback, which can be to synthesize an R-value or to assume
+//		// the TraceID has sufficient randomness.  This detail is left
+//		// out of scope.
+//		if ts.OTelValue().HasRValue() {
+//			rnd = ts.OTelValue().RValueRandomness()
+//		} else {
+//			rnd = TraceIDToRandomness(tid)
+//		}
+//
+//		return fixedThreshold.ShouldSample(rnd)
+//	}
+//
 // [W3C tracecontext specification]: https://www.w3.org/TR/trace-context/#tracestate-header
 // [Tracestate handling]: https://opentelemetry.io/docs/specs/otel/trace/tracestate-handling/
 package sampling // import "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/sampling"
