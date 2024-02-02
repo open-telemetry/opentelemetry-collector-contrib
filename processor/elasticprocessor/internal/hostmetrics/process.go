8edbe8e87e08edceb4b1d26ee4850f9eda5d914f
ishleenk17 <ishleen.kaur@elastic.co>
2024-02-02 19:26:26 +0530

    Updated processor scrapper

--- processor/elasticprocessor/internal/hostmetrics/process.go
+++ processor/elasticprocessor/internal/hostmetrics/process.go
@@ -7,7 +7,8 @@ import (
 
 func addProcessMetrics(metrics pmetric.MetricSlice, dataset string) error {
 	var timestamp pcommon.Timestamp
-	var threads int64
+	var threads, memUsage int64
+	var memUtil float64
 
 	for i := 0; i < metrics.Len(); i++ {
 		metric := metrics.At(i)
@@ -15,8 +16,18 @@ func addProcessMetrics(metrics pmetric.MetricSlice, dataset string) error {
 			dp := metric.Sum().DataPoints().At(0)
 			timestamp = dp.Timestamp()
 			threads = dp.IntValue()
+		} else if metric.Name() == "process.memory.utilization" {
+			dp := metric.Gauge().DataPoints().At(0)
+			timestamp = dp.Timestamp()
+			memUtil = dp.DoubleValue()
+		} else if metric.Name() == "process.memory.usage" {
+			dp := metric.Sum().DataPoints().At(0)
+			timestamp = dp.Timestamp()
+			memUsage = dp.IntValue()
 		}
 	}
+
+	memUtilPct := memUtil / 100
 
 	addMetrics(metrics, dataset,
 		metric{
@@ -24,6 +35,18 @@ func addProcessMetrics(metrics pmetric.MetricSlice, dataset string) error {
 			name:      "system.process.num_threads",
 			timestamp: timestamp,
 			intValue:  &threads,
+		},
+		metric{
+			dataType:    Gauge,
+			name:        "system.process.memory.rss.pct",
+			timestamp:   timestamp,
+			doubleValue: &memUtilPct,
+		},
+		metric{
+			dataType:  Sum,
+			name:      "system.process.memory.rss.bytes",
+			timestamp: timestamp,
+			intValue:  &memUsage,
 		},
 	)
 
