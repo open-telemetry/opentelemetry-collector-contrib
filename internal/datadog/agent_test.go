570a0dc377a75ec886f35f6000acfd744eb8b22c
Yang Song <yang.song@datadoghq.com>
2024-02-05 11:01:11 -0500

    Fix another typo

--- internal/datadog/agent_test.go
+++ internal/datadog/agent_test.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package datadog // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/datadog"
+package datadog // import "github.com/open-telemetry/opentelemetry-collector-contrib/internal/datadog"
 
 import (
 	"context"
