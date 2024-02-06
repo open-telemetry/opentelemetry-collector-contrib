339c7bad55dad3584f46282f0cd4e0a4c58dcac8
andrefrco <andre.frco@gmail.com>
2024-02-05 21:22:37 -0300

    Add copyright to the client_test file
    Signed-off-by: andrefrco <andre.frco@gmail.com>

--- extension/storage/dbstorage/client_test.go
+++ extension/storage/dbstorage/client_test.go
@@ -1,4 +1,7 @@
-package dbstorage
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package dbstorage // import "github.com/open-telemetry/opentelemetry-collector-contrib/extension/storage/dbstorage"
 
 import (
 	"context"
