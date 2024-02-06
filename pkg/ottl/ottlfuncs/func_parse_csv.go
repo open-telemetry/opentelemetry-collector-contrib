bda815f98586652757d54fd42f6e8c52a0d7b5a6
Brandon Johnson <brandon.johnson@observiq.com>
2024-02-05 23:19:45 -0500

    make gci

--- pkg/ottl/ottlfuncs/func_parse_csv.go
+++ pkg/ottl/ottlfuncs/func_parse_csv.go
@@ -8,8 +8,9 @@ import (
 	"io"
 	"strings"
 
-	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl"
 	"go.opentelemetry.io/collector/pdata/pcommon"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl"
 )
 
 const (
