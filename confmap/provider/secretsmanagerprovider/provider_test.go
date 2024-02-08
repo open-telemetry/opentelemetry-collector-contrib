11611661f7e30a9ac877d79ec92d6ddfabc0dfe1
Antoine Toulme <antoine@lunar-ocean.com>
2024-02-07 21:52:57 -0800

    fix header

--- confmap/provider/secretsmanagerprovider/provider_test.go
+++ confmap/provider/secretsmanagerprovider/provider_test.go
@@ -1,30 +1,20 @@
 // Copyright The OpenTelemetry Authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// SPDX-License-Identifier: Apache-2.0
 
 package secretsmanagerprovider
 
 import (
 	"context"
 	"encoding/json"
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
-	"github.com/stretchr/testify/assert"
-	"go.opentelemetry.io/collector/confmap"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/collector/confmap"
 )
 
 // Create a provider mocking s3provider works in normal cases
