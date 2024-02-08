f9fca1bf8ecf4e2eab2bd808b0a7ac87b7cb8ef2
Alex Boten <aboten@lightstep.com>
2024-02-07 16:00:14 -0800

    make generate

--- receiver/otelarrowreceiver/internal/metadata/generated_status.go
+++ receiver/otelarrowreceiver/internal/metadata/generated_status.go
@@ -8,8 +8,11 @@ import (
 	"go.opentelemetry.io/otel/trace"
 )
 
+var (
+	Type = component.MustNewType("otelarrow")
+)
+
 const (
-	Type             = "otelarrow"
 	TracesStability  = component.StabilityLevelDevelopment
 	MetricsStability = component.StabilityLevelDevelopment
 	LogsStability    = component.StabilityLevelDevelopment
