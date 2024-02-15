dc7d68fecbb440a9ca9cf4e5568189e2b7feb898
Alex Boten <aboten@lightstep.com>
2024-02-15 12:12:17 -0800

    [chore] more lint fixes
    Signed-off-by: Alex Boten <aboten@lightstep.com>

--- receiver/rabbitmqreceiver/scraper_test.go
+++ receiver/rabbitmqreceiver/scraper_test.go
@@ -84,22 +84,22 @@ func TestScaperScrape(t *testing.T) {
 	}{
 		{
 			desc: "Nil client",
-			setupMockClient: func(t *testing.T) client {
+			setupMockClient: func(*testing.T) client {
 				return nil
 			},
-			expectedMetricGen: func(t *testing.T) pmetric.Metrics {
+			expectedMetricGen: func(*testing.T) pmetric.Metrics {
 				return pmetric.NewMetrics()
 			},
 			expectedErr: errClientNotInit,
 		},
 		{
 			desc: "API Call Failure",
-			setupMockClient: func(t *testing.T) client {
+			setupMockClient: func(*testing.T) client {
 				mockClient := mocks.MockClient{}
 				mockClient.On("GetQueues", mock.Anything).Return(nil, errors.New("some api error"))
 				return &mockClient
 			},
-			expectedMetricGen: func(t *testing.T) pmetric.Metrics {
+			expectedMetricGen: func(*testing.T) pmetric.Metrics {
 
 				return pmetric.NewMetrics()
 			},
