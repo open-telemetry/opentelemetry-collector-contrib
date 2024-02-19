a379343a773878aaef9a63baf625b287cb8b24a8
sh0rez <me@shorez.de>
2024-02-19 12:33:06 +0100

    identity: remove mdatagen
    left by accident, no need here

--- internal/exp/metrics/identity/doc.go
+++ internal/exp/metrics/identity/doc.go
@@ -1,8 +1,6 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-//go:generate mdatagen metadata.yaml
-
 // identity types for metrics and sample streams.
 //
 // Use the `Of*(T) -> I` functions to obtain a unique, comparable (==) and
