7745b7e7bc75a859018143b990ccfb049fd41b5a
sh0rez <me@shorez.de>
2024-02-20 12:05:44 +0100

    metrics/staleness: synchronization
    adds mutex-sync to the staleness struct.
    this is especially important for item expiration, which modifies the pq
    and map serially. intermittent changes by other routines would lead to
    data inconsistencies here.

--- internal/exp/metrics/staleness/staleness.go
+++ internal/exp/metrics/staleness/staleness.go
@@ -4,6 +4,7 @@
 package staleness
 
 import (
+	"sync"
 	"time"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/internal/exp/metrics/identity"
@@ -30,32 +31,62 @@ type Map[T any] interface {
 type Staleness[T any] struct {
 	max time.Duration
 
-	Map[T]
-	pq PriorityQueue
+	items Map[T]
+	pq    PriorityQueue
+
+	mtx sync.RWMutex
 }
 
 func NewStaleness[T any](max time.Duration, newMap Map[T]) *Staleness[T] {
 	return &Staleness[T]{
 		max: max,
-		Map: newMap,
-		pq:  NewPriorityQueue(),
+
+		items: newMap,
+		pq:    NewPriorityQueue(),
+
+		mtx: sync.RWMutex{},
 	}
 }
 
-func (s *Staleness[T]) Store(id identity.Stream, value T) {
-	s.pq.Update(id, nowFunc())
-	s.Map.Store(id, value)
+func (s *Staleness[T]) Load(id identity.Stream) (T, bool) {
+	s.mtx.RLock()
+	defer s.mtx.RUnlock()
+	return s.items.Load(id)
 }
 
-func (s *Staleness[T]) ExpireOldEntries() {
-	now := nowFunc()
+func (s *Staleness[T]) Store(id identity.Stream, v T) {
+	s.mtx.Lock()
+	s.pq.Update(id, time.Now())
+	s.items.Store(id, v)
+	s.mtx.Unlock()
 
+}
+
+func (s *Staleness[T]) Delete(id identity.Stream) {
+	s.mtx.Lock()
+	s.items.Delete(id)
+	s.mtx.Unlock()
+}
+
+func (s *Staleness[T]) Items() func(yield func(identity.Stream, T) bool) bool {
+	return func(yield func(identity.Stream, T) bool) bool {
+		s.mtx.RLock()
+		defer s.mtx.RUnlock()
+		return s.items.Items()(yield)
+	}
+}
+
+func (s *Staleness[T]) expireOldItems() {
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+
+	now := nowFunc()
 	for {
 		_, ts := s.pq.Peek()
 		if now.Sub(ts) < s.max {
 			break
 		}
 		id, _ := s.pq.Pop()
-		s.Map.Delete(id)
+		s.items.Delete(id)
 	}
 }
