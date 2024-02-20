c53a0867bea36a221dd269419b83dd9e970704a5
Adrian Astley <adastley@gmail.com>
2024-02-20 14:18:17 -0500

    [processor/interval] Implement the main logic

--- processor/intervalprocessor/config.go
+++ processor/intervalprocessor/config.go
@@ -4,15 +4,24 @@
 package intervalprocessor // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/intervalprocessor"
 
 import (
+	"errors"
 	"time"
 
 	"go.opentelemetry.io/collector/component"
+)
+
+var (
+	ErrInvalidIntervalValue     = errors.New("invalid interval value")
+	ErrInvalidMaxStatenessValue = errors.New("invalid max_stateless value")
 )
 
 var _ component.Config = (*Config)(nil)
 
 // Config defines the configuration for the processor.
 type Config struct {
+	// Interval is the time
+	Interval time.Duration `mapstructure:"interval"`
+
 	// MaxStaleness is the total time a state entry will live past the time it was last seen. Set to 0 to retain state indefinitely.
 	MaxStaleness time.Duration `mapstructure:"max_staleness"`
 }
@@ -20,5 +29,13 @@ type Config struct {
 // Validate checks whether the input configuration has all of the required fields for the processor.
 // An error is returned if there are any invalid inputs.
 func (config *Config) Validate() error {
+	if config.Interval <= 0 {
+		return ErrInvalidIntervalValue
+	}
+
+	if config.MaxStaleness < 0 {
+		return ErrInvalidMaxStatenessValue
+	}
+
 	return nil
 }
--- processor/intervalprocessor/processor.go
+++ processor/intervalprocessor/processor.go
@@ -5,13 +5,20 @@ package intervalprocessor // import "github.com/open-telemetry/opentelemetry-col
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"sync"
 	"time"
 
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/pmetric"
 	"go.opentelemetry.io/collector/processor"
 	"go.uber.org/zap"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/internal/exp/metrics/identity"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/intervalprocessor/internal/metrics"
 )
 
 var _ processor.Metrics = (*Processor)(nil)
@@ -19,9 +26,16 @@ var _ processor.Metrics = (*Processor)(nil)
 type Processor struct {
 	ctx    context.Context
 	cancel context.CancelFunc
-	log    *zap.Logger
+	logger *zap.Logger
 
-	maxStaleness time.Duration
+	stateLock     sync.Mutex
+	numbers       map[identity.Stream]metrics.StreamDataPoint[pmetric.NumberDataPoint]
+	histograms    map[identity.Stream]metrics.StreamDataPoint[pmetric.HistogramDataPoint]
+	expHistograms map[identity.Stream]metrics.StreamDataPoint[pmetric.ExponentialHistogramDataPoint]
+
+	interval       time.Duration
+	maxStaleness   time.Duration
+	intervalTicker *time.Ticker
 
 	nextConsumer consumer.Metrics
 }
@@ -30,19 +44,32 @@ func newProcessor(config *Config, log *zap.Logger, nextConsumer consumer.Metrics
 	ctx, cancel := context.WithCancel(context.Background())
 
 	return &Processor{
-		ctx:          ctx,
-		cancel:       cancel,
-		log:          log,
+		ctx:    ctx,
+		cancel: cancel,
+		logger: log,
+
+		stateLock:     sync.Mutex{},
+		numbers:       map[identity.Stream]metrics.StreamDataPoint[pmetric.NumberDataPoint]{},
+		histograms:    map[identity.Stream]metrics.StreamDataPoint[pmetric.HistogramDataPoint]{},
+		expHistograms: map[identity.Stream]metrics.StreamDataPoint[pmetric.ExponentialHistogramDataPoint]{},
+
+		interval:     config.Interval,
 		maxStaleness: config.MaxStaleness,
+
 		nextConsumer: nextConsumer,
 	}
 }
 
 func (p *Processor) Start(_ context.Context, _ component.Host) error {
+	p.intervalTicker = time.NewTicker(p.interval)
+	go p.exportMetricsLoop()
 	return nil
 }
 
 func (p *Processor) Shutdown(_ context.Context) error {
+	if p.intervalTicker != nil {
+		p.intervalTicker.Stop()
+	}
 	p.cancel()
 	return nil
 }
@@ -52,5 +79,209 @@ func (p *Processor) Capabilities() consumer.Capabilities {
 }
 
 func (p *Processor) ConsumeMetrics(ctx context.Context, md pmetric.Metrics) error {
-	return p.nextConsumer.ConsumeMetrics(ctx, md)
+	var errs error
+
+	p.stateLock.Lock()
+	defer p.stateLock.Unlock()
+
+	md.ResourceMetrics().RemoveIf(func(rm pmetric.ResourceMetrics) bool {
+		rm.ScopeMetrics().RemoveIf(func(sm pmetric.ScopeMetrics) bool {
+			sm.Metrics().RemoveIf(func(m pmetric.Metric) bool {
+				switch m.Type() {
+				case pmetric.MetricTypeGauge, pmetric.MetricTypeSummary:
+					return false
+				case pmetric.MetricTypeSum:
+					sum := m.Sum()
+
+					if !sum.IsMonotonic() {
+						return false
+					}
+
+					if sum.AggregationTemporality() != pmetric.AggregationTemporalityCumulative {
+						return false
+					}
+
+					aggregateDataPoints(sum.DataPoints(), p.numbers, rm.Resource(), rm.SchemaUrl(), sm.Scope(), sm.SchemaUrl(), m)
+					return true
+				case pmetric.MetricTypeHistogram:
+					histogram := m.Histogram()
+
+					if histogram.AggregationTemporality() != pmetric.AggregationTemporalityCumulative {
+						return false
+					}
+
+					aggregateDataPoints(histogram.DataPoints(), p.histograms, rm.Resource(), rm.SchemaUrl(), sm.Scope(), sm.SchemaUrl(), m)
+					return true
+				case pmetric.MetricTypeExponentialHistogram:
+					expHistogram := m.ExponentialHistogram()
+
+					if expHistogram.AggregationTemporality() != pmetric.AggregationTemporalityCumulative {
+						return false
+					}
+
+					aggregateDataPoints(expHistogram.DataPoints(), p.expHistograms, rm.Resource(), rm.SchemaUrl(), sm.Scope(), sm.SchemaUrl(), m)
+					return true
+				default:
+					errs = errors.Join(fmt.Errorf("invalid MetricType %d", m.Type()))
+					return false
+				}
+			})
+			return sm.Metrics().Len() == 0
+		})
+		return rm.ScopeMetrics().Len() == 0
+	})
+
+	if err := p.nextConsumer.ConsumeMetrics(ctx, md); err != nil {
+		errs = errors.Join(errs, err)
+	}
+
+	return errs
+}
+
+func aggregateDataPoints[DPS metrics.DataPointSlice[DP], DP metrics.DataPoint[DP]](dataPoints DPS, state map[identity.Stream]metrics.StreamDataPoint[DP], res pcommon.Resource, resSchemaURL string, scope pcommon.InstrumentationScope, scopeSchemaURL string, m pmetric.Metric) {
+	metric := metrics.From(res, resSchemaURL, scope, scopeSchemaURL, m)
+	metricID := metric.Identity()
+
+	now := time.Now()
+
+	for i := 0; i < dataPoints.Len(); i++ {
+		dp := dataPoints.At(i)
+
+		streamDataPointID := metrics.StreamDataPointIdentity(metricID, dp)
+
+		existing, ok := state[streamDataPointID]
+		if !ok {
+			state[streamDataPointID] = metrics.StreamDataPoint[DP]{
+				Metric:      metric,
+				DataPoint:   dp,
+				LastUpdated: now,
+			}
+			continue
+		}
+
+		// Check if the datapoint is newer
+		if dp.Timestamp().AsTime().After(existing.DataPoint.Timestamp().AsTime()) {
+			state[streamDataPointID] = metrics.StreamDataPoint[DP]{
+				Metric:      metric,
+				DataPoint:   dp,
+				LastUpdated: now,
+			}
+			continue
+		}
+
+		// Otherwise, we leave existing as-is
+	}
+}
+
+func (p *Processor) exportMetricsLoop() {
+	for {
+		select {
+		case <-p.ctx.Done():
+			return
+		case <-p.intervalTicker.C:
+			p.expireOldMetrics()
+			p.exportMetrics()
+		}
+	}
+}
+
+func (p *Processor) exportMetrics() {
+	p.stateLock.Lock()
+	defer p.stateLock.Unlock()
+
+	// We have to generate our own metrics slice to send to the nextConsumer
+	md := pmetric.NewMetrics()
+
+	// We want to avoid generating duplicate ResourceMetrics, ScopeMetrics, and Metrics
+	// So we use lookups to only generate what we need
+	rmLookup := map[identity.Resource]pmetric.ResourceMetrics{}
+	smLookup := map[identity.Scope]pmetric.ScopeMetrics{}
+	mLookup := map[identity.Metric]pmetric.Metric{}
+
+	for dataID, dp := range p.numbers {
+		m := getOrCreateMetric(dataID, dp.Metric, md, rmLookup, smLookup, mLookup)
+
+		sum := m.Sum()
+		numDP := sum.DataPoints().AppendEmpty()
+		dp.DataPoint.CopyTo(numDP)
+	}
+
+	for dataID, dp := range p.histograms {
+		m := getOrCreateMetric(dataID, dp.Metric, md, rmLookup, smLookup, mLookup)
+
+		histogram := m.Histogram()
+		histogramDP := histogram.DataPoints().AppendEmpty()
+		dp.DataPoint.CopyTo(histogramDP)
+	}
+
+	for dataID, dp := range p.expHistograms {
+		m := getOrCreateMetric(dataID, dp.Metric, md, rmLookup, smLookup, mLookup)
+
+		expHistogram := m.ExponentialHistogram()
+		expHistogramDP := expHistogram.DataPoints().AppendEmpty()
+		dp.DataPoint.CopyTo(expHistogramDP)
+	}
+
+	if err := p.nextConsumer.ConsumeMetrics(p.ctx, md); err != nil {
+		p.logger.Error("Metrics export failed", zap.Error(err))
+	}
+}
+
+func getOrCreateMetric(
+	streamID identity.Stream, metricRef metrics.Metric,
+	md pmetric.Metrics,
+	rmLookup map[identity.Resource]pmetric.ResourceMetrics,
+	smLookup map[identity.Scope]pmetric.ScopeMetrics,
+	mLookup map[identity.Metric]pmetric.Metric,
+) pmetric.Metric {
+	// Find the ResourceMetrics
+	rm, ok := rmLookup[streamID.Metric().Scope().Resource()]
+	if !ok {
+		// We need to create it
+		rm = md.ResourceMetrics().AppendEmpty()
+		metricRef.CopyToResourceMetric(rm)
+	}
+
+	// Find the ScopeMetrics
+	sm, ok := smLookup[streamID.Metric().Scope()]
+	if !ok {
+		sm = rm.ScopeMetrics().AppendEmpty()
+		metricRef.CopyToScopeMetric(sm)
+	}
+
+	m, ok := mLookup[streamID.Metric()]
+	if !ok {
+		m = sm.Metrics().AppendEmpty()
+		metricRef.CopyToPMetric(m)
+	}
+
+	return m
+}
+
+func (p *Processor) expireOldMetrics() {
+	// Fast-out
+	if p.maxStaleness == 0 {
+		return
+	}
+
+	p.stateLock.Lock()
+	defer p.stateLock.Unlock()
+
+	now := time.Now()
+
+	for key, dp := range p.numbers {
+		if dp.LastUpdated.Add(p.maxStaleness).Before(now) {
+			delete(p.numbers, key)
+		}
+	}
+	for key, dp := range p.histograms {
+		if dp.LastUpdated.Add(p.maxStaleness).Before(now) {
+			delete(p.numbers, key)
+		}
+	}
+	for key, dp := range p.expHistograms {
+		if dp.LastUpdated.Add(p.maxStaleness).Before(now) {
+			delete(p.numbers, key)
+		}
+	}
 }
