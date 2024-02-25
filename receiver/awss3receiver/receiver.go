7faf81ed47773c0e032602490408a9b99b98f870
Adam Charrett <adcharre@synamedia.com>
2024-02-25 18:15:34 +0000

    Initial commit to introduce awss3receiver

---
+++ receiver/awss3receiver/receiver.go
@@ -0,0 +1,26 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package awss3receiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/awss3receiver"
+
+import (
+	"context"
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/consumer"
+	"go.uber.org/zap"
+)
+
+type awss3Receiver struct {
+}
+
+func newAWSS3TraceReceiver(cfg *Config, traces consumer.Traces, logger *zap.Logger) (*awss3Receiver, error) {
+	return &awss3Receiver{}, nil
+}
+
+func (r *awss3Receiver) Start(ctx context.Context, _ component.Host) error {
+	return nil
+}
+
+func (r *awss3Receiver) Shutdown(_ context.Context) error {
+	return nil
+}
