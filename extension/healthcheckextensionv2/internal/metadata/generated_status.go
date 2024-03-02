5df8a783439dae3c8034895c6f54d21a037250e8
Matthew Wear <matthew.wear@gmail.com>
2024-03-01 16:39:18 -0800

    Readme fusion

--- extension/healthcheckextensionv2/internal/metadata/generated_status.go
+++ extension/healthcheckextensionv2/internal/metadata/generated_status.go
@@ -8,8 +8,11 @@ import (
 	"go.opentelemetry.io/otel/trace"
 )
 
+var (
+	Type = component.MustNewType("healthcheckv2")
+)
+
 const (
-	Type               = "healthcheckv2"
 	ExtensionStability = component.StabilityLevelDevelopment
 )
 
