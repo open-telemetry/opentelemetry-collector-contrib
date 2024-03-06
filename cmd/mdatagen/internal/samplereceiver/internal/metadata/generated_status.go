bde317d1b332ff86fefa18a957307e20c353a495
Dmitrii Anoshin <anoshindx@gmail.com>
2024-03-05 17:14:46 -0800

    [chore] Migrate the "generate" make target to mdatagen from core repo (#31609)
    This change migrates `generate` make target from using the deprecated
    `cmd/mdatagen` in this repository to mdategen defined in core
    repository.
    
    To avoid breaking changes for the end users, we keep the scope names
    used in this repo as before. This required defining them explicitly in
    metadata.yaml files. We can update them after
    https://github.com/open-telemetry/opentelemetry-collector/issues/9494
    and
    https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/21469
    are resolved.
    
    Taking the opportunity that the scope names can be explicitly defined,
    this PR also updates missing scope names for extensions with
    inconsistent package names e.g.: `awsproxy` and `jaegerremotesampling`.
    It's not a breaking change because the generated meter and tracer are
    not being used yet.
    
    This change unblocks
    https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/30495

--- cmd/mdatagen/internal/samplereceiver/internal/metadata/generated_status.go
+++ cmd/mdatagen/internal/samplereceiver/internal/metadata/generated_status.go
@@ -19,9 +19,9 @@ const (
 )
 
 func Meter(settings component.TelemetrySettings) metric.Meter {
-	return settings.MeterProvider.Meter("otelcol/samplereceiver")
+	return settings.MeterProvider.Meter("github.com/open-telemetry/opentelemetry-collector-contrib/cmd/mdatagen/internal/samplereceiver")
 }
 
 func Tracer(settings component.TelemetrySettings) trace.Tracer {
-	return settings.TracerProvider.Tracer("otelcol/samplereceiver")
+	return settings.TracerProvider.Tracer("github.com/open-telemetry/opentelemetry-collector-contrib/cmd/mdatagen/internal/samplereceiver")
 }
