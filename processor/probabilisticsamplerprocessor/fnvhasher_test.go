e903382e1f2868638ef34d0352ce7b380130748c
Andrew Glaude <andrew.glaude@datadoghq.com>
2024-03-08 14:53:07 -0500

    Use encoding/binary to convert hashing seed for better performance

---
+++ processor/probabilisticsamplerprocessor/fnvhasher_test.go
@@ -0,0 +1,13 @@
+package probabilisticsamplerprocessor
+
+import (
+	"testing"
+)
+
+func BenchmarkSeedConversion(b *testing.B) {
+	val := uint32(0x3024001) // Just a random 32 bit int
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		i32tob(val)
+	}
+}
