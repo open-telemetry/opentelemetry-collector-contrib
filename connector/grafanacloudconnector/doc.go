e9f2260cb381239d620eb4e83d2c4ce3eb7e1cd8
Robbie Lankford <robert.lankford@grafana.com>
2024-03-12 18:18:58 -0700

    make goporto

--- connector/grafanacloudconnector/doc.go
+++ connector/grafanacloudconnector/doc.go
@@ -3,4 +3,4 @@
 
 //go:generate mdatagen metadata.yaml
 
-package grafanacloudconnector
+package grafanacloudconnector // import "github.com/open-telemetry/opentelemetry-collector-contrib/connector/grafanacloudconnector"
--- connector/grafanacloudconnector/host_metrics.go
+++ connector/grafanacloudconnector/host_metrics.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package grafanacloudconnector
+package grafanacloudconnector // import "github.com/open-telemetry/opentelemetry-collector-contrib/connector/grafanacloudconnector"
 
 import (
 	"sync"
