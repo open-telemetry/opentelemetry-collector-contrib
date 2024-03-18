175a96da361a1c709671bf4981622087c27107ef
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2024-03-18 10:27:59 -0600

    Remove use of deprecated scraperhelper structs/funcs

--- receiver/gitproviderreceiver/config_test.go
+++ receiver/gitproviderreceiver/config_test.go
@@ -43,7 +43,7 @@ func TestLoadConfig(t *testing.T) {
 
 	r1 := cfg.Receivers[component.NewIDWithName(metadata.Type, "customname")].(*Config)
 	expectedConfig := &Config{
-		ScraperControllerSettings: scraperhelper.ScraperControllerSettings{
+		ControllerConfig: scraperhelper.ControllerConfig{
 			CollectionInterval: 30 * time.Second,
 			InitialDelay:       1 * time.Second,
 		},
@@ -79,7 +79,7 @@ func TestLoadInvalidConfig_InvalidScraperKey(t *testing.T) {
 
 func TestConfig_Unmarshal(t *testing.T) {
 	type fields struct {
-		ScraperControllerSettings scraperhelper.ScraperControllerSettings
+		ScraperControllerSettings scraperhelper.ControllerConfig
 		Scrapers                  map[string]internal.Config
 		MetricsBuilderConfig      metadata.MetricsBuilderConfig
 	}
@@ -104,9 +104,9 @@ func TestConfig_Unmarshal(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			cfg := &Config{
-				ScraperControllerSettings: test.fields.ScraperControllerSettings,
-				Scrapers:                  test.fields.Scrapers,
-				MetricsBuilderConfig:      test.fields.MetricsBuilderConfig,
+				ControllerConfig:     test.fields.ControllerConfig,
+				Scrapers:             test.fields.Scrapers,
+				MetricsBuilderConfig: test.fields.MetricsBuilderConfig,
 			}
 			if err := cfg.Unmarshal(test.args.componentParser); (err != nil) != test.wantErr {
 				t.Errorf("Config.Unmarshal() error = %v, wantErr %v", err, test.wantErr)
