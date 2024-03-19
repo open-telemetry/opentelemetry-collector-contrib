785ed35f55cbb6c7e259a0091e1e858cff7a54cc
Richard Mitchell <18526695+rimitchell@users.noreply.github.com>
2024-03-19 09:54:31 -0700

    doc: Fix missed word change

--- processor/resourcedetectionprocessor/internal/azure/config.go
+++ processor/resourcedetectionprocessor/internal/azure/config.go
@@ -8,7 +8,7 @@ import (
 )
 
 type Config struct {
-	// Tags is a list of regex's to match ec2 instance tag keys that users want
+	// Tags is a list of regex's to match azure instance tag keys that users want
 	// to add as resource attributes to processed data
 	Tags               []string                          `mapstructure:"tags"`
 	ResourceAttributes metadata.ResourceAttributesConfig `mapstructure:"resource_attributes"`
