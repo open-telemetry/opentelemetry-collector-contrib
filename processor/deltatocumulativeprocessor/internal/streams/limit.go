21699d8503fe38f1a1da17d6ebbcfab4f8930854
sh0rez <me@shorez.de>
2024-03-25 11:51:01 +0100

    streams: permit write to existing at limit

--- processor/deltatocumulativeprocessor/internal/streams/limit.go
+++ processor/deltatocumulativeprocessor/internal/streams/limit.go
@@ -23,7 +23,9 @@ type LimitMap[T any] struct {
 }
 
 func (m LimitMap[T]) Store(id identity.Stream, v T) error {
-	if m.Map.Len() <= m.Max {
+	_, ok := m.Map.Load(id)
+	avail := m.Map.Len() < m.Max
+	if ok || avail {
 		return m.Map.Store(id, v)
 	}
 
