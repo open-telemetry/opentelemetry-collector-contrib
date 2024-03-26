cbfba1a31064e6bc2d798b29e2a76917891d0b83
Casey Wilson <casey.wilson@liatrio.com>
2024-03-26 06:27:18 -0400

    fix: refactored metric names to be clearer

--- receiver/gitproviderreceiver/internal/scraper/githubscraper/github_scraper.go
+++ receiver/gitproviderreceiver/internal/scraper/githubscraper/github_scraper.go
@@ -136,7 +136,7 @@ func (ghs *githubScraper) scrape(ctx context.Context) (pmetric.Metrics, error) {
 
 					age := getAge(pr.CreatedAt, pr.MergedAt)
 
-					ghs.mb.RecordGitRepositoryPullRequestMergedTimeDataPoint(now, age, name, pr.HeadRefName)
+					ghs.mb.RecordGitRepositoryPullRequestTimeToMergeDataPoint(now, age, name, pr.HeadRefName)
 
 				} else {
 					open++
@@ -148,13 +148,13 @@ func (ghs *githubScraper) scrape(ctx context.Context) (pmetric.Metrics, error) {
 					if pr.Reviews.TotalCount > 0 {
 						age := getAge(pr.CreatedAt, pr.Reviews.Nodes[0].CreatedAt)
 
-						ghs.mb.RecordGitRepositoryPullRequestApprovedTimeDataPoint(now, age, name, pr.HeadRefName)
+						ghs.mb.RecordGitRepositoryPullRequestTimeToApprovalDataPoint(now, age, name, pr.HeadRefName)
 					}
 				}
 			}
 
-			ghs.mb.RecordGitRepositoryPullRequestOpenCountDataPoint(now, int64(open), name)
-			ghs.mb.RecordGitRepositoryPullRequestMergedCountDataPoint(now, int64(merged), name)
+			ghs.mb.RecordGitRepositoryPullRequestCountDataPoint(now, int64(open), metadata.AttributePullRequestStateOpen, name)
+			ghs.mb.RecordGitRepositoryPullRequestCountDataPoint(now, int64(open), metadata.AttributePullRequestStateMerged, name)
 		}()
 	}
 
