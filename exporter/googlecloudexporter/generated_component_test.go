6d0294a9a7f464375dbf88766883f763d385c600
Mike Dame <mikedame@google.com>
2024-03-27 18:53:16 +0000

    Re-disable lifecycle but keep shutdown tests

--- exporter/googlecloudexporter/generated_component_test.go
+++ exporter/googlecloudexporter/generated_component_test.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component"
-	"go.opentelemetry.io/collector/component/componenttest"
 	"go.opentelemetry.io/collector/confmap/confmaptest"
 	"go.opentelemetry.io/collector/exporter"
 	"go.opentelemetry.io/collector/exporter/exportertest"
@@ -60,46 +59,6 @@ func TestComponentLifecycle(t *testing.T) {
 		t.Run(test.name+"-shutdown", func(t *testing.T) {
 			c, err := test.createFn(context.Background(), exportertest.NewNopCreateSettings(), cfg)
 			require.NoError(t, err)
-			err = c.Shutdown(context.Background())
-			require.NoError(t, err)
-		})
-		t.Run(test.name+"-lifecycle", func(t *testing.T) {
-			c, err := test.createFn(context.Background(), exportertest.NewNopCreateSettings(), cfg)
-			require.NoError(t, err)
-			host := componenttest.NewNopHost()
-			err = c.Start(context.Background(), host)
-			require.NoError(t, err)
-			require.NotPanics(t, func() {
-				switch test.name {
-				case "logs":
-					e, ok := c.(exporter.Logs)
-					require.True(t, ok)
-					logs := generateLifecycleTestLogs()
-					if !e.Capabilities().MutatesData {
-						logs.MarkReadOnly()
-					}
-					err = e.ConsumeLogs(context.Background(), logs)
-				case "metrics":
-					e, ok := c.(exporter.Metrics)
-					require.True(t, ok)
-					metrics := generateLifecycleTestMetrics()
-					if !e.Capabilities().MutatesData {
-						metrics.MarkReadOnly()
-					}
-					err = e.ConsumeMetrics(context.Background(), metrics)
-				case "traces":
-					e, ok := c.(exporter.Traces)
-					require.True(t, ok)
-					traces := generateLifecycleTestTraces()
-					if !e.Capabilities().MutatesData {
-						traces.MarkReadOnly()
-					}
-					err = e.ConsumeTraces(context.Background(), traces)
-				}
-			})
-
-			require.NoError(t, err)
-
 			err = c.Shutdown(context.Background())
 			require.NoError(t, err)
 		})
