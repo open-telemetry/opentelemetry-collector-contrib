23514c6b3f29c003d20036315ce71ea0112702ee
Arve Knudsen <arve.knudsen@gmail.com>
2024-03-28 10:35:12 +0100

    Benchmark FromMetrics and PrometheusConverter.FromMetrics separately
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/translator/prometheusremotewrite/metrics_to_prw_test.go
+++ pkg/translator/prometheusremotewrite/metrics_to_prw_test.go
@@ -35,11 +35,46 @@ func BenchmarkFromMetrics(b *testing.B) {
 											payload := createExportRequest(resourceAttributeCount, histogramCount, nonHistogramCount, labelsPerMetric, exemplarsPerSeries)
 
 											for i := 0; i < b.N; i++ {
-												converter := NewPrometheusConverter()
-												err := converter.FromMetrics(payload.Metrics(), Settings{})
+												tsMap, err := FromMetrics(payload.Metrics(), Settings{})
 												require.NoError(b, err)
-												ts := converter.TimeSeries()
-												require.NotNil(b, ts)
+												require.NotNil(b, tsMap)
+											}
+										})
+									}
+								})
+							}
+						})
+					}
+				})
+			}
+		})
+	}
+}
+
+func BenchmarkPrometheusConverter_FromMetrics(b *testing.B) {
+	for _, resourceAttributeCount := range []int{0, 5, 50} {
+		b.Run(fmt.Sprintf("resource attribute count: %v", resourceAttributeCount), func(b *testing.B) {
+			for _, histogramCount := range []int{0, 1000} {
+				b.Run(fmt.Sprintf("histogram count: %v", histogramCount), func(b *testing.B) {
+					nonHistogramCounts := []int{0, 1000}
+
+					if resourceAttributeCount == 0 && histogramCount == 0 {
+						// Don't bother running a scenario where we'll generate no series.
+						nonHistogramCounts = []int{1000}
+					}
+
+					for _, nonHistogramCount := range nonHistogramCounts {
+						b.Run(fmt.Sprintf("non-histogram count: %v", nonHistogramCount), func(b *testing.B) {
+							for _, labelsPerMetric := range []int{2, 20} {
+								b.Run(fmt.Sprintf("labels per metric: %v", labelsPerMetric), func(b *testing.B) {
+									for _, exemplarsPerSeries := range []int{0, 5, 10} {
+										b.Run(fmt.Sprintf("exemplars per series: %v", exemplarsPerSeries), func(b *testing.B) {
+											payload := createExportRequest(resourceAttributeCount, histogramCount, nonHistogramCount, labelsPerMetric, exemplarsPerSeries)
+
+											for i := 0; i < b.N; i++ {
+												converter := NewPrometheusConverter()
+												require.NoError(b, converter.FromMetrics(payload.Metrics(), Settings{}))
+												require.NotNil(b, converter.TimeSeries())
 											}
 										})
 									}
