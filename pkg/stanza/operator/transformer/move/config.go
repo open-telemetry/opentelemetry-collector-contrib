e45bde549637cea4c8283fe8185bf6e248538475
Daniel Jaglowski <jaglows3@gmail.com>
2024-04-03 14:08:33 -0500

    [chore][pkg/stanza] Cleanup move operator files (#32064)
    Contributes to #32058

--- pkg/stanza/operator/transformer/move/config.go
+++ pkg/stanza/operator/transformer/move/config.go
@@ -4,7 +4,6 @@
 package move // import "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/operator/transformer/move"
 
 import (
-	"context"
 	"fmt"
 
 	"go.uber.org/zap"
@@ -55,25 +54,4 @@ func (c Config) Build(logger *zap.SugaredLogger) (operator.Operator, error) {
 		From:                c.From,
 		To:                  c.To,
 	}, nil
-}
-
-// Transformer is an operator that moves a field's value to a new field
-type Transformer struct {
-	helper.TransformerOperator
-	From entry.Field
-	To   entry.Field
-}
-
-// Process will process an entry with a move transformation.
-func (p *Transformer) Process(ctx context.Context, entry *entry.Entry) error {
-	return p.ProcessWith(ctx, entry, p.Transform)
-}
-
-// Transform will apply the move operation to an entry
-func (p *Transformer) Transform(e *entry.Entry) error {
-	val, exist := p.From.Delete(e)
-	if !exist {
-		return fmt.Errorf("move: field does not exist: %s", p.From.String())
-	}
-	return p.To.Set(e, val)
 }
