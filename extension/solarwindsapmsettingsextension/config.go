e79e2be68784178b821f308409d7ac9c316cf0de
Jerry Leung <tingfung.leung@solarwinds.com>
2024-04-03 15:35:54 -0700

    nits

--- extension/solarwindsapmsettingsextension/config.go
+++ extension/solarwindsapmsettingsextension/config.go
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 package solarwindsapmsettingsextension // import "github.com/open-telemetry/opentelemetry-collector-contrib/extension/solarwindsapmsettingsextension"
+
 import "time"
 
 type Config struct {
