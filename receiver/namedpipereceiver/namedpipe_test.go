b79d14d2a597952801226baa6db0953e62ddba12
Curtis Robert <crobert@splunk.com>
2024-04-05 15:43:44 -0700

    Use context with cancel

--- receiver/namedpipereceiver/namedpipe_test.go
+++ receiver/namedpipereceiver/namedpipe_test.go
@@ -60,10 +60,13 @@ func TestReadPipe(t *testing.T) {
 	converter.Start()
 	defer converter.Stop()
 
-	rcvr, err := f.CreateLogsReceiver(context.Background(), receivertest.NewNopCreateSettings(), cfg, sink)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	rcvr, err := f.CreateLogsReceiver(ctx, receivertest.NewNopCreateSettings(), cfg, sink)
 	require.NoError(t, err, "failed to create receiver")
-	require.NoError(t, rcvr.Start(context.Background(), componenttest.NewNopHost()))
-	//defer require.NoError(t, rcvr.Shutdown(context.Background()))
+	require.NoError(t, rcvr.Start(ctx, componenttest.NewNopHost()))
+	defer require.NoError(t, rcvr.Shutdown(ctx))
 
 	pipe, err := os.OpenFile("/tmp/pipe", os.O_WRONLY, 0o600)
 	require.NoError(t, err, "failed to open pipe")
