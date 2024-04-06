f51c7b2d20e0ceb956a93f0fbe96bdf56815a4f1
Adam Charrett <adcharre@synamedia.com>
2024-04-06 22:08:39 +0100

    rename awss3Receiver to awss3TraceReceiver

--- receiver/awss3receiver/receiver.go
+++ receiver/awss3receiver/receiver.go
@@ -12,19 +12,19 @@ import (
 	"strings"
 )
 
-type awss3Receiver struct {
+type awss3TraceReceiver struct {
 	s3Reader *s3Reader
 	consumer consumer.Traces
 	logger   *zap.Logger
 	cancel   context.CancelFunc
 }
 
-func newAWSS3TraceReceiver(cfg *Config, traces consumer.Traces, logger *zap.Logger) (*awss3Receiver, error) {
+func newAWSS3TraceReceiver(cfg *Config, traces consumer.Traces, logger *zap.Logger) (*awss3TraceReceiver, error) {
 	reader, err := newS3Reader(cfg)
 	if err != nil {
 		return nil, err
 	}
-	return &awss3Receiver{
+	return &awss3TraceReceiver{
 		s3Reader: reader,
 		consumer: traces,
 		logger:   logger,
@@ -32,7 +32,7 @@ func newAWSS3TraceReceiver(cfg *Config, traces consumer.Traces, logger *zap.Logg
 	}, nil
 }
 
-func (r *awss3Receiver) Start(ctx context.Context, _ component.Host) error {
+func (r *awss3TraceReceiver) Start(ctx context.Context, _ component.Host) error {
 	ctx, r.cancel = context.WithCancel(ctx)
 	go func() {
 		_ = r.s3Reader.readAll(ctx, "traces", r.receiveBytes)
@@ -40,14 +40,14 @@ func (r *awss3Receiver) Start(ctx context.Context, _ component.Host) error {
 	return nil
 }
 
-func (r *awss3Receiver) Shutdown(_ context.Context) error {
+func (r *awss3TraceReceiver) Shutdown(_ context.Context) error {
 	if r.cancel != nil {
 		r.cancel()
 	}
 	return nil
 }
 
-func (r *awss3Receiver) receiveBytes(ctx context.Context, key string, data []byte) error {
+func (r *awss3TraceReceiver) receiveBytes(ctx context.Context, key string, data []byte) error {
 	var unmarshaler ptrace.Unmarshaler
 	if strings.HasSuffix(key, ".json") {
 		unmarshaler = &ptrace.JSONUnmarshaler{}
