bbb0a53c05e886f9259008c359efd9aa8841eb9c
Antoine Toulme <antoine@lunar-ocean.com>
2024-04-12 14:50:31 -0700

    fix lint

--- receiver/prometheusreceiver/metrics_receiver.go
+++ receiver/prometheusreceiver/metrics_receiver.go
@@ -310,7 +310,7 @@ func (r *pReceiver) initPrometheusComponents(ctx context.Context, logger log.Log
 		},
 	}
 
-        // for testing only
+	// for testing only
 	if r.skipOffsetting {
 		optsValue := reflect.ValueOf(opts).Elem()
 		field := optsValue.FieldByName("skipOffsetting")
