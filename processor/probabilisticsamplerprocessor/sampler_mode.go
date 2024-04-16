e33b28de4bac4cf4971c51dce5b482b2adbc66cc
Joshua MacDonald <josh.macdonald@gmail.com>
2024-04-16 14:44:58 -0700

    goporto

--- processor/probabilisticsamplerprocessor/sampler_mode.go
+++ processor/probabilisticsamplerprocessor/sampler_mode.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package probabilisticsamplerprocessor
+package probabilisticsamplerprocessor // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/probabilisticsamplerprocessor"
 
 import (
 	"context"
