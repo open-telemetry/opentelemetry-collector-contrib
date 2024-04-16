97db72a5db6848f98f4d9b07b935a0d303a17050
Brandon Johnson <brandon.johnson@observiq.com>
2024-04-16 10:18:06 -0400

    [cmd/opampsupervisor] Fix early exits not shutting down client/server in bootstrapping (#31944)
    **Description:**
    * Fix early exits not shutting down client/server in bootstrapping
    
    **Link to tracking Issue:** Closes #31943
    
    **Testing:**
    I tested manually adding errors into the code. There isn't a good,
    consistent way to add e.g. the timeout error as-is.
    
    ---------
    
    Co-authored-by: Daniel Jaglowski <jaglows3@gmail.com>

--- cmd/opampsupervisor/supervisor/supervisor.go
+++ cmd/opampsupervisor/supervisor/supervisor.go
@@ -287,6 +287,12 @@ func (s *Supervisor) getBootstrapInfo() (err error) {
 		return err
 	}
 
+	defer func() {
+		if stopErr := srv.Stop(context.Background()); stopErr != nil {
+			err = errors.Join(err, fmt.Errorf("error when stopping the opamp server: %w", stopErr))
+		}
+	}()
+
 	cmd, err := commander.NewCommander(
 		s.logger,
 		s.config.Agent,
@@ -300,6 +306,12 @@ func (s *Supervisor) getBootstrapInfo() (err error) {
 		return err
 	}
 
+	defer func() {
+		if stopErr := cmd.Stop(context.Background()); stopErr != nil {
+			err = errors.Join(err, fmt.Errorf("error when stopping the collector: %w", stopErr))
+		}
+	}()
+
 	select {
 	// TODO make timeout configurable
 	case <-time.After(3 * time.Second):
@@ -309,20 +321,8 @@ func (s *Supervisor) getBootstrapInfo() (err error) {
 			return errors.New("collector's OpAMP client never connected to the Supervisor")
 		}
 	case err = <-done:
-		if err != nil {
-			return err
-		}
-	}
-
-	if err = cmd.Stop(context.Background()); err != nil {
 		return err
 	}
-
-	if err = srv.Stop(context.Background()); err != nil {
-		return err
-	}
-
-	return nil
 }
 
 func (s *Supervisor) Capabilities() protobufs.AgentCapabilities {
