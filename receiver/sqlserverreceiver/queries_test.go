8defea25f01a67f46341e47812f1adb24f055152
Curtis Robert <crobert@splunk.com>
2024-04-18 10:47:07 -0700

    [chore][receiver/sqlserver] Fix newlines when testing on Windows

--- receiver/sqlserverreceiver/queries_test.go
+++ receiver/sqlserverreceiver/queries_test.go
@@ -6,19 +6,17 @@ package sqlserverreceiver
 import (
 	"os"
 	"path"
-	"runtime"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestQueryIODBWithoutInstanceName(t *testing.T) {
-	if runtime.GOOS == "windows" {
-		t.Skip("Test is failing on Windows, see https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/32519")
-	}
-
-	expected, err := os.ReadFile(path.Join("./testdata", "databaseIOQueryWithoutInstanceName.txt"))
+	expected_bytes, err := os.ReadFile(path.Join("./testdata", "databaseIOQueryWithoutInstanceName.txt"))
 	require.NoError(t, err)
+	// Replace all will fix newlines when testing on Windows
+	expected := strings.ReplaceAll(string(expected_bytes[:]), "\r\n", "\n")
 
 	actual := getSQLServerDatabaseIOQuery("")
 
@@ -26,12 +24,10 @@ func TestQueryIODBWithoutInstanceName(t *testing.T) {
 }
 
 func TestQueryIODBWithInstanceName(t *testing.T) {
-	if runtime.GOOS == "windows" {
-		t.Skip("Test is failing on Windows, see https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/32519")
-	}
-
-	expected, err := os.ReadFile(path.Join("./testdata", "databaseIOQueryWithInstanceName.txt"))
+	expected_bytes, err := os.ReadFile(path.Join("./testdata", "databaseIOQueryWithInstanceName.txt"))
 	require.NoError(t, err)
+	// Replace all will fix newlines when testing on Windows
+	expected := strings.ReplaceAll(string(expected_bytes[:]), "\r\n", "\n")
 
 	actual := getSQLServerDatabaseIOQuery("instanceName")
 
