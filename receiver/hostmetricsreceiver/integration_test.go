4f0e38a75beb0acbe07609729a64ad3223d87206
Curtis Robert <crobert@splunk.com>
2024-04-18 13:44:45 -0700

    Skip another host metrics integration test

--- receiver/hostmetricsreceiver/integration_test.go
+++ receiver/hostmetricsreceiver/integration_test.go
@@ -90,6 +90,8 @@ func Test_ProcessScrapeWithCustomRootPath(t *testing.T) {
 }
 
 func Test_ProcessScrapeWithBadRootPathAndEnvVar(t *testing.T) {
+	t.Skip("TODO: Skipping for now due to https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/32536")
+
 	expectedFile := filepath.Join("testdata", "e2e", "expected_process_separate_proc.yaml")
 	t.Setenv("HOST_PROC", filepath.Join("testdata", "e2e", "proc"))
 	scraperinttest.NewIntegrationTest(
