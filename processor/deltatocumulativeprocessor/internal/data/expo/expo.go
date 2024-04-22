885e39a9b12b9a82652f4786923b9bbda9e1871f
sh0rez <me@shorez.de>
2024-04-22 17:51:45 +0200

    expo: TestAbsolute

--- processor/deltatocumulativeprocessor/internal/data/expo/expo.go
+++ processor/deltatocumulativeprocessor/internal/data/expo/expo.go
@@ -6,3 +6,40 @@ type (
 	DataPoint = pmetric.ExponentialHistogramDataPoint
 	Buckets   = pmetric.ExponentialHistogramDataPointBuckets
 )
+
+func Abs(buckets buckets) Absolute {
+	return Absolute{buckets: buckets}
+}
+
+type buckets = Buckets
+
+// Absolute addresses bucket counts using an absolute scale, such that it is
+// interoperable with [Scale].
+//
+// It spans from [[Absolute.Lower]:[Absolute.Upper]]
+type Absolute struct {
+	buckets
+}
+
+// Abs returns the value at absolute index 'at'
+func (a Absolute) Abs(at int) uint64 {
+	if i, ok := a.idx(at); ok {
+		return a.BucketCounts().At(i)
+	}
+	return 0
+}
+
+// Upper returns the minimal index outside the set, such that every i < Upper
+func (a Absolute) Upper() int {
+	return a.BucketCounts().Len() + int(a.Offset())
+}
+
+// Lower returns the minimal index inside the set, such that every i >= Lower
+func (a Absolute) Lower() int {
+	return int(a.Offset())
+}
+
+func (a Absolute) idx(at int) (int, bool) {
+	idx := at - a.Lower()
+	return idx, idx >= 0 && idx < a.BucketCounts().Len()
+}
---
+++ processor/deltatocumulativeprocessor/internal/data/expo/expo_test.go
@@ -0,0 +1,47 @@
+package expo_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/matryer/is"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/deltatocumulativeprocessor/internal/data/expo"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/deltatocumulativeprocessor/internal/data/expo/expotest"
+)
+
+func TestAbsolute(t *testing.T) {
+	is := is.NewRelaxed(t)
+
+	bs := expotest.Bins{ø, 1, 2, 3, 4, 5, ø, ø}.Into()
+	abs := expo.Abs(bs)
+
+	lo, up := abs.Lower(), abs.Upper()
+	is.Equal(-2, lo) // lower bound
+	is.Equal(3, up)  // upper bound
+
+	for i := lo; i < up; i++ {
+		got := abs.Abs(i)
+		is.Equal(bs.BucketCounts().At(i+2), got)
+	}
+}
+
+func ExampleAbsolute() {
+	nums := []float64{0.4, 2.3, 2.4, 4.5}
+
+	bs := expotest.Observe0(nums...)
+	abs := expo.Abs(bs)
+
+	fmt.Printf("spans from %d:%d\n", abs.Lower(), abs.Upper())
+
+	s := expo.Scale(0)
+	for _, n := range nums {
+		fmt.Printf("%.1f belongs to bucket %2d: %d\n", n, s.Idx(n), abs.Abs(s.Idx(n)))
+	}
+
+	// Output:
+	// spans from -2:3
+	// 0.4 belongs to bucket -2: 1
+	// 2.3 belongs to bucket  1: 2
+	// 2.4 belongs to bucket  1: 2
+	// 4.5 belongs to bucket  2: 1
+}
