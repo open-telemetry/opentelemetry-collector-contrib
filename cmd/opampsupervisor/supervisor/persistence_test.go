8ec682999b9a4ae1de46493dde146f0354386c11
Brandon Johnson <brandon.johnson@observiq.com>
2024-04-22 14:40:52 -0400

    test set instance ID

--- cmd/opampsupervisor/supervisor/persistence_test.go
+++ cmd/opampsupervisor/supervisor/persistence_test.go
@@ -9,16 +9,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestNewPersistentState(t *testing.T) {
-	f := filepath.Join(t.TempDir(), "state.yaml")
-	state, err := createNewPersistentState(f)
-	require.NoError(t, err)
-
-	// instance ID should be populated
-	require.NotEqual(t, ulid.ULID{}, state.InstanceID)
-	require.FileExists(t, f)
-}
-
 func TestCreateOrLoadPersistentState(t *testing.T) {
 	t.Run("Creates a new state file if it does not exist", func(t *testing.T) {
 		f := filepath.Join(t.TempDir(), "state.yaml")
@@ -44,6 +34,28 @@ func TestCreateOrLoadPersistentState(t *testing.T) {
 		require.FileExists(t, f)
 	})
 
+}
+
+func TestPersistentState_SetInstanceID(t *testing.T) {
+	f := filepath.Join(t.TempDir(), "state.yaml")
+	state, err := createNewPersistentState(f)
+	require.NoError(t, err)
+
+	// instance ID should be populated
+	require.NotEqual(t, ulid.ULID{}, state.InstanceID)
+	require.FileExists(t, f)
+
+	newULID := ulid.MustParse("01HW3GS9NWD840C5C2BZS3KYPW")
+	err = state.SetInstanceID(newULID)
+	require.NoError(t, err)
+
+	require.Equal(t, newULID, state.InstanceID)
+
+	// Test that loading the state after setting the instance ID has the new instance ID
+	loadedState, err := loadPersistentState(f)
+	require.NoError(t, err)
+
+	require.Equal(t, newULID, loadedState.InstanceID)
 }
 
 func TestGenerateNewULID(t *testing.T) {
