6b587f7d2cf65b470800cc8892b15dcb41f166b7
Alex Boten <223565+codeboten@users.noreply.github.com>
2024-04-22 06:28:34 -0700

    few more modules that need skipping
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>

--- receiver/k8sobjectsreceiver/generated_package_test.go
+++ receiver/k8sobjectsreceiver/generated_package_test.go
@@ -4,10 +4,8 @@ package k8sobjectsreceiver
 
 import (
 	"testing"
-
-	"go.uber.org/goleak"
 )
 
 func TestMain(m *testing.M) {
-	goleak.VerifyTestMain(m)
+	// skipping goleak test as per metadata.yml configuration
 }
