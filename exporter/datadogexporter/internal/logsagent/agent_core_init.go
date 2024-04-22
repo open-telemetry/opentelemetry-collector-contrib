80df01ac183c2fe66f407d29e3d12d58efdeaed0
Stanley Liu <stanley.liu@datadoghq.com>
2024-04-22 16:20:43 -0400

    Change build endpoints

--- exporter/datadogexporter/internal/logsagent/agent_core_init.go
+++ exporter/datadogexporter/internal/logsagent/agent_core_init.go
@@ -41,8 +41,8 @@ func (a *Agent) SetupPipeline(
 // buildEndpoints builds endpoints for the logs agent
 func buildEndpoints(coreConfig pkgconfigmodel.Reader) (*config.Endpoints, error) {
 	httpConnectivity := config.HTTPConnectivityFailure
-	if endpoints, err := config.BuildHTTPEndpointsWithVectorOverride(coreConfig, intakeTrackType, config.AgentJSONIntakeProtocol, config.DefaultIntakeOrigin); err == nil {
+	if endpoints, err := config.BuildHTTPEndpoints(coreConfig, intakeTrackType, config.AgentJSONIntakeProtocol, config.DefaultIntakeOrigin); err == nil {
 		httpConnectivity = http.CheckConnectivity(endpoints.Main, coreConfig)
 	}
-	return config.BuildEndpointsWithVectorOverride(coreConfig, httpConnectivity, intakeTrackType, config.AgentJSONIntakeProtocol, config.DefaultIntakeOrigin)
+	return config.BuildEndpoints(coreConfig, httpConnectivity, intakeTrackType, config.AgentJSONIntakeProtocol, config.DefaultIntakeOrigin)
 }
