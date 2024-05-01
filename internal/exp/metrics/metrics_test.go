07e436cec2df50d5b958a1afc74fafb4099e9a53
Adrian Astley <adastley@gmail.com>
2024-05-01 08:38:00 -0400

    [internal/exp/metrics]: Add functions to merge metrics

---
+++ internal/exp/metrics/metrics_test.go
@@ -0,0 +1,42 @@
+package metrics_test
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/internal/exp/metrics"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/golden"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/pdatatest/pmetrictest"
+)
+
+func TestMergeMetrics(t *testing.T) {
+	t.Parallel()
+
+	testCases := []string{
+		"basic_merge",
+	}
+
+	for _, tc := range testCases {
+		testName := tc
+
+		t.Run(testName, func(t *testing.T) {
+			t.Parallel()
+
+			dir := filepath.Join("testdata", testName)
+
+			mdA, err := golden.ReadMetrics(filepath.Join(dir, "a.yaml"))
+			require.NoError(t, err)
+
+			mdB, err := golden.ReadMetrics(filepath.Join(dir, "b.yaml"))
+			require.NoError(t, err)
+
+			expectedOutput, err := golden.ReadMetrics(filepath.Join(dir, "output.yaml"))
+			require.NoError(t, err)
+
+			output := metrics.Merge(mdA, mdB)
+			require.NoError(t, pmetrictest.CompareMetrics(expectedOutput, output))
+		})
+	}
+}
