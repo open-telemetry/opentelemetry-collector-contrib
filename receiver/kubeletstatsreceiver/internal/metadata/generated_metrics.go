077c70cc6292f726f925f9c499f4644d610039ae
Greg Dallavalle <greg.dallavalle@lightstep.com>
2024-05-06 12:13:21 -0500

    make generate

--- receiver/kubeletstatsreceiver/internal/metadata/generated_metrics.go
+++ receiver/kubeletstatsreceiver/internal/metadata/generated_metrics.go
@@ -2978,6 +2978,18 @@ func NewMetricsBuilder(mbc MetricsBuilderConfig, settings receiver.CreateSetting
 	if mbc.ResourceAttributes.ContainerID.MetricsExclude != nil {
 		mb.resourceAttributeExcludeFilter["container.id"] = filter.CreateFilter(mbc.ResourceAttributes.ContainerID.MetricsExclude)
 	}
+	if mbc.ResourceAttributes.CsiDriver.MetricsInclude != nil {
+		mb.resourceAttributeIncludeFilter["csi.driver"] = filter.CreateFilter(mbc.ResourceAttributes.CsiDriver.MetricsInclude)
+	}
+	if mbc.ResourceAttributes.CsiDriver.MetricsExclude != nil {
+		mb.resourceAttributeExcludeFilter["csi.driver"] = filter.CreateFilter(mbc.ResourceAttributes.CsiDriver.MetricsExclude)
+	}
+	if mbc.ResourceAttributes.CsiVolumeHandle.MetricsInclude != nil {
+		mb.resourceAttributeIncludeFilter["csi.volume.handle"] = filter.CreateFilter(mbc.ResourceAttributes.CsiVolumeHandle.MetricsInclude)
+	}
+	if mbc.ResourceAttributes.CsiVolumeHandle.MetricsExclude != nil {
+		mb.resourceAttributeExcludeFilter["csi.volume.handle"] = filter.CreateFilter(mbc.ResourceAttributes.CsiVolumeHandle.MetricsExclude)
+	}
 	if mbc.ResourceAttributes.FsType.MetricsInclude != nil {
 		mb.resourceAttributeIncludeFilter["fs.type"] = filter.CreateFilter(mbc.ResourceAttributes.FsType.MetricsInclude)
 	}
