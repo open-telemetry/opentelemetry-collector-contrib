241167a6af0016944cb09d9893fa7bdda870ff75
Cem Deniz Kabakci <CemDenizKabakci@outlook.com>
2024-05-13 16:47:13 +0200

    [receiver/cloudfoundryreceiver] WIP: Add functionality to fill trace id and span id from RTR Logs
    Co-authored-by: Jose Riguera <jose.riguera@springer.com>

--- receiver/cloudfoundryreceiver/converter.go
+++ receiver/cloudfoundryreceiver/converter.go
@@ -4,12 +4,14 @@
 package cloudfoundryreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/cloudfoundryreceiver"
 
 import (
+	"strings"
 	"time"
 
 	"code.cloudfoundry.org/go-loggregator/rpc/loggregator_v2"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/plog"
 	"go.opentelemetry.io/collector/pdata/pmetric"
+	"go.opentelemetry.io/otel/trace"
 )
 
 const (
@@ -47,9 +49,23 @@ func convertEnvelopeToLogs(envelope *loggregator_v2.Envelope, logSlice plog.LogR
 		// TODO: review log attributes and tags from the envelope
 		log := logSlice.AppendEmpty()
 		log.SetTimestamp(pcommon.Timestamp(envelope.GetTimestamp()))
+		log.SetObservedTimestamp(pcommon.NewTimestampFromTime(startTime))
 		log.Body().SetStr(string(envelope.GetLog().GetPayload()))
-		log.SetSeverityNumber(plog.SeverityNumber(envelope.GetLog().GetType()))
+		switch envelope.GetLog().GetType() {
+		case loggregator_v2.Log_OUT:
+			log.SetSeverityText(plog.SeverityNumberInfo.String())
+			log.SetSeverityNumber(plog.SeverityNumberInfo)
+		case loggregator_v2.Log_ERR:
+			log.SetSeverityText(plog.SeverityNumberError.String())
+			log.SetSeverityNumber(plog.SeverityNumberError)
+		}
+
 		copyEnvelopeAttributes(log.Attributes(), envelope)
+
+		if value, found := log.Attributes().Get("org.cloudfoundry.source_type"); found && value.AsString() == "RTR" {
+			parseLogTracedID(log)
+		}
+
 	}
 }
 
@@ -65,4 +81,36 @@ func copyEnvelopeAttributes(attributes pcommon.Map, envelope *loggregator_v2.Env
 	if envelope.InstanceId != "" {
 		attributes.PutStr(attributeNamePrefix+"instance_id", envelope.InstanceId)
 	}
+}
+
+func parseLogTracedID(log plog.LogRecord) error {
+	s := log.Body().AsString()
+	quoted := false
+	a := strings.FieldsFunc(s, func(r rune) bool {
+		if r == '"' {
+			quoted = !quoted
+		}
+		return !quoted && r == ' '
+	})
+
+	traceIDStr := strings.Split(a[21], ":")[1]
+	traceIDStr = strings.Trim(traceIDStr, "\"")
+
+	spanIDStr := strings.Split(a[22], ":")[1]
+	spanIDStr = strings.Trim(spanIDStr, "\"")
+
+	traceID, err := trace.TraceIDFromHex(traceIDStr)
+	if err != nil {
+		return err
+	}
+
+	spanID, err := trace.SpanIDFromHex(spanIDStr)
+	if err != nil {
+		return err
+	}
+
+	log.SetTraceID([16]byte(traceID))
+	log.SetSpanID([8]byte(spanID))
+
+	return nil
 }
