0355479d7865b3661276a44a7e8297822180bef1
Juraci Paixão Kröhling <juraci@kroehling.de>
2024-05-17 13:53:06 +0200

    goporto
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- processor/tailsamplingprocessor/internal/telemetry/featureflag.go
+++ processor/tailsamplingprocessor/internal/telemetry/featureflag.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package telemetry
+package telemetry // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/tailsamplingprocessor/internal/telemetry"
 
 import "go.opentelemetry.io/collector/featuregate"
 
