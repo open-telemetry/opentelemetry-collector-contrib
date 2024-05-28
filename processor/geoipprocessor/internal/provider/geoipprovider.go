dd89cf6137573038046315555df9177130fd275c
Roger Coll <rogercoll@protonmail.com>
2024-05-28 17:40:32 +0200

    feat: add geoipprovider internal package
    Includes the GeoIPProvider interface, a configuration type and a factory
    interface able to generate providers.

---
+++ processor/geoipprocessor/internal/provider/geoipprovider.go
@@ -0,0 +1,34 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package provider
+
+import (
+	"context"
+	"net"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/processor"
+	"go.opentelemetry.io/otel/attribute"
+)
+
+// GeoIPProvider defines methods for obtaining the geographical location based on the provided IP address.
+type GeoIPProvider interface {
+	// Location returns a set of attributes representing the geographical location for the given IP address. It requires a context for managing request lifetime.
+	Location(context.Context, net.IP) (attribute.Set, error)
+}
+
+// Config is the configuration of a GeoIPProvider.
+// It extends the component.ConfigValidator interface to include validation logic.
+type Config interface {
+	component.ConfigValidator
+}
+
+// GeoIPProviderFactory can create GeoIPProvider instances.
+type GeoIPProviderFactory interface {
+	// CreateDefaultConfig creates the default configuration for the GeoIPProvider.
+	CreateDefaultConfig() Config
+
+	// CreateGeoIPProvider creates a provider based on this config.
+	CreateGeoIPProvider(ctx context.Context, settings processor.CreateSettings, cfg Config) (GeoIPProvider, error)
+}
