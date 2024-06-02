83cd75258ba5f2fd2f699b22b54fe1ee52c4ef07
Pablo Estrada <pabloem@apache.org>
2024-06-02 14:54:53 -0400

    Adding opampsupervisor component base info

---
+++ cmd/opampsupervisor/doc.go
@@ -0,0 +1,9 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+//go:generate mdatagen metadata.yaml
+
+// Package opampsupervisor is an implementation of an Open Agent Management Protocol (OpAMP)
+// 'supervisor' - which is the process that manages local execution of the OpenTelemetry Collector
+// as a local agent in hosts or containers.
+package opampsupervisor // import "github.com/open-telemetry/opentelemetry-collector-contrib/cmd/opampsupervisor
