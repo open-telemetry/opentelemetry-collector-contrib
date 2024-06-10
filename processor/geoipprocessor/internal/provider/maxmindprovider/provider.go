d51ff81cb1e8dd01ce7cd588efd1d7dd68c45a95
Roger Coll <rogercoll@protonmail.com>
2024-06-10 14:45:53 +0200

    chore: run goporto

--- processor/geoipprocessor/internal/provider/maxmindprovider/provider.go
+++ processor/geoipprocessor/internal/provider/maxmindprovider/provider.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package maxmind
+package maxmind // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/geoipprocessor/internal/provider/maxmindprovider"
 
 import (
 	"context"
