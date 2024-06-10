9396cd83e08de3a1d368d799e4b37aa6104fddbf
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2024-06-10 14:06:45 -0600

    [pkg/ottl] Add debug logging (#33274)
    **Description:**
    Adds debug logging to `StatementSequence.Execute` to allow printing the
    state of the `TransformContext` after each statement.
    
    Example logs:
    
    ```
    2024-05-28T16:19:29.198-0600    debug   ottl@v0.101.0/parser.go:265     initial TransformContext        {"kind": "processor", "name": "transform", "pipeline": "logs", "tCtx": {"resource": {"attributes": {}, "dropped_attribute_count": 0}, "instrumentation_scope": {"attributes": {}, "dropped_attribute_count": 0, "name": "", "version": ""}, "log_record": {"attributes": {"log.file.name": "test.log"}, "body": "test", "dropped_attribute_count": 0, "flags": 0, "observed_time_unix_nano": "2024-05-28 22:19:29.098833 +0000 UTC", "severity_number": 0, "severity_text": "", "span_id": "", "time_unix_nano": "1970-01-01 00:00:00 +0000 UTC", "trace_id": ""}, "cache": {}}}
    2024-05-28T16:19:29.199-0600    debug   ottl@v0.101.0/parser.go:268     after transformation    {"kind": "processor", "name": "transform", "pipeline": "logs", "statement": "set(attributes[\"test\"], \"pass\")", "tCtx": {"resource": {"attributes": {}, "dropped_attribute_count": 0}, "instrumentation_scope": {"attributes": {}, "dropped_attribute_count": 0, "name": "", "version": ""}, "log_record": {"attributes": {"log.file.name": "test.log", "test": "pass"}, "body": "test", "dropped_attribute_count": 0, "flags": 0, "observed_time_unix_nano": "2024-05-28 22:19:29.098833 +0000 UTC", "severity_number": 0, "severity_text": "", "span_id": "", "time_unix_nano": "1970-01-01 00:00:00 +0000 UTC", "trace_id": ""}, "cache": {}}}
    ```
    
    Formatted json
    
    ```json
    {
        "kind": "processor",
        "name": "transform",
        "pipeline": "logs",
        "statement": "set(attributes[\"test\"], \"pass\")",
        "tCtx": {
            "resource": {
                "attributes": {},
                "dropped_attribute_count": 0
            },
            "instrumentation_scope": {
                "attributes": {},
                "dropped_attribute_count": 0,
                "name": "",
                "version": ""
            },
            "log_record": {
                "attributes": {
                    "log.file.name": "test.log",
                    "test": "pass"
                },
                "body": "test",
                "dropped_attribute_count": 0,
                "flags": 0,
                "observed_time_unix_nano": "2024-05-28 22:19:29.098833 +0000 UTC",
                "severity_number": 0,
                "severity_text": "",
                "span_id": "",
                "time_unix_nano": "1970-01-01 00:00:00 +0000 UTC",
                "trace_id": ""
            },
            "cache": {}
        }
    }
    ```
    
    **Link to tracking Issue:** <Issue number if applicable>
    Related to
    https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/30932

--- pkg/ottl/contexts/ottlmetric/metrics.go
+++ pkg/ottl/contexts/ottlmetric/metrics.go
@@ -5,19 +5,22 @@ package ottlmetric // import "github.com/open-telemetry/opentelemetry-collector-
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/pmetric"
+	"go.uber.org/zap/zapcore"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/internal"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/internal/logging"
 )
 
-var _ internal.ResourceContext = TransformContext{}
-var _ internal.InstrumentationScopeContext = TransformContext{}
-var _ internal.MetricContext = TransformContext{}
+var _ internal.ResourceContext = (*TransformContext)(nil)
+var _ internal.InstrumentationScopeContext = (*TransformContext)(nil)
+var _ zapcore.ObjectMarshaler = (*TransformContext)(nil)
 
 type TransformContext struct {
 	metric               pmetric.Metric
@@ -25,6 +28,14 @@ type TransformContext struct {
 	instrumentationScope pcommon.InstrumentationScope
 	resource             pcommon.Resource
 	cache                pcommon.Map
+}
+
+func (tCtx TransformContext) MarshalLogObject(encoder zapcore.ObjectEncoder) error {
+	err := encoder.AddObject("resource", logging.Resource(tCtx.resource))
+	err = errors.Join(err, encoder.AddObject("scope", logging.InstrumentationScope(tCtx.instrumentationScope)))
+	err = errors.Join(err, encoder.AddObject("metric", logging.Metric(tCtx.metric)))
+	err = errors.Join(err, encoder.AddObject("cache", logging.Map(tCtx.cache)))
+	return err
 }
 
 type Option func(*ottl.Parser[TransformContext])
