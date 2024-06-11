177fa7e6cd96c60133e04aab56187e541bee2d10
Pablo Estrada <pabloem@apache.org>
2024-06-11 15:49:42 -0400

    fixup

--- cmd/opampsupervisor/doc.go
+++ cmd/opampsupervisor/doc.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-//go:generate go run go.opentelemetry.io/collector/cmd/mdatagen metadata.yaml
+//go:generate mdatagen metadata.yaml
 
 // Package opampsupervisor is an implementation of an Open Agent Management Protocol (OpAMP)
 // 'supervisor' - which is the process that manages local execution of the OpenTelemetry Collector
