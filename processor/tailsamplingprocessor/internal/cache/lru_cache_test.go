bb0a66fb45e8ca2544e1449c5670018549279bb8
James Moessis <jmoessis@atlassian.com>
2024-06-14 15:18:28 +1000

    fix tests

--- processor/tailsamplingprocessor/internal/cache/lru_cache_test.go
+++ processor/tailsamplingprocessor/internal/cache/lru_cache_test.go
@@ -9,16 +9,18 @@ import (
 )
 
 func TestSinglePut(t *testing.T) {
-	c, err := NewLRUDecisionCache(2, func(uint64, bool) {})
+	c, err := NewLRUDecisionCache[int](2)
 	require.NoError(t, err)
 	id, err := traceIDFromHex("12341234123412341234123412341234")
 	require.NoError(t, err)
-	require.NoError(t, c.Put(id))
-	assert.Equal(t, true, c.Get(id))
+	c.Put(id, 123)
+	v, ok := c.Get(id)
+	assert.Equal(t, 123, v)
+	assert.True(t, ok)
 }
 
 func TestExceedsSizeLimit(t *testing.T) {
-	c, err := NewLRUDecisionCache(2, func(uint64, bool) {})
+	c, err := NewLRUDecisionCache[bool](2)
 	require.NoError(t, err)
 	id1, err := traceIDFromHex("12341234123412341234123412341231")
 	require.NoError(t, err)
@@ -27,17 +29,23 @@ func TestExceedsSizeLimit(t *testing.T) {
 	id3, err := traceIDFromHex("12341234123412341234123412341233")
 	require.NoError(t, err)
 
-	require.NoError(t, c.Put(id1))
-	require.NoError(t, c.Put(id2))
-	require.NoError(t, c.Put(id3))
+	c.Put(id1, true)
+	c.Put(id2, true)
+	c.Put(id3, true)
 
-	assert.False(t, c.Get(id1)) // evicted
-	assert.True(t, c.Get(id2))
-	assert.True(t, c.Get(id3))
+	v, ok := c.Get(id1)
+	assert.False(t, v)  // evicted
+	assert.False(t, ok) // evicted
+	v, ok = c.Get(id2)
+	assert.True(t, v)
+	assert.True(t, ok)
+	v, ok = c.Get(id3)
+	assert.True(t, v)
+	assert.True(t, ok)
 }
 
 func TestLeastRecentlyUsedIsEvicted(t *testing.T) {
-	c, err := NewLRUDecisionCache(2, func(uint64, bool) {})
+	c, err := NewLRUDecisionCache[bool](2)
 	require.NoError(t, err)
 	id1, err := traceIDFromHex("12341234123412341234123412341231")
 	require.NoError(t, err)
@@ -46,14 +54,22 @@ func TestLeastRecentlyUsedIsEvicted(t *testing.T) {
 	id3, err := traceIDFromHex("12341234123412341234123412341233")
 	require.NoError(t, err)
 
-	require.NoError(t, c.Put(id1))
-	require.NoError(t, c.Put(id2))
-	assert.Equal(t, true, c.Get(id1)) // use id1
-	require.NoError(t, c.Put(id3))
+	c.Put(id1, true)
+	c.Put(id2, true)
+	v, ok := c.Get(id1) // use id1
+	assert.True(t, true, v)
+	assert.True(t, true, ok)
+	c.Put(id3, true)
 
-	assert.True(t, c.Get(id1))
-	assert.False(t, c.Get(id2)) // evicted
-	assert.True(t, c.Get(id3))
+	v, ok = c.Get(id1)
+	assert.True(t, v)
+	assert.True(t, ok)
+	v, ok = c.Get(id2)
+	assert.False(t, v)  // evicted, returns zero-value
+	assert.False(t, ok) // evicted, not OK
+	v, ok = c.Get(id3)
+	assert.True(t, v)
+	assert.True(t, ok)
 }
 
 func traceIDFromHex(idStr string) (pcommon.TraceID, error) {
--- processor/tailsamplingprocessor/internal/cache/nop_cache_test.go
+++ processor/tailsamplingprocessor/internal/cache/nop_cache_test.go
@@ -7,9 +7,11 @@ import (
 )
 
 func TestNopCache(t *testing.T) {
-	c := NewNopDecisionCache()
+	c := NewNopDecisionCache[bool]()
 	id, err := traceIDFromHex("12341234123412341234123412341234")
 	require.NoError(t, err)
-	require.NoError(t, c.Put(id))
-	assert.False(t, c.Get(id))
+	c.Put(id, true)
+	v, ok := c.Get(id)
+	assert.False(t, v)
+	assert.False(t, ok)
 }
