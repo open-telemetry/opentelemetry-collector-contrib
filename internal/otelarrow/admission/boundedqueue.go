ef4ea8c6090f1dc35a4152d1356cb1da86408399
Joshua MacDonald <josh.macdonald@gmail.com>
2024-06-17 09:49:58 -0700

    goporto

--- internal/otelarrow/admission/boundedqueue.go
+++ internal/otelarrow/admission/boundedqueue.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package admission
+package admission // import "github.com/open-telemetry/opentelemetry-collector-contrib/internal/otelarrow/admission"
 
 import (
 	"context"
--- internal/otelarrow/netstats/grpc.go
+++ internal/otelarrow/netstats/grpc.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package netstats
+package netstats // import "github.com/open-telemetry/opentelemetry-collector-contrib/internal/otelarrow/netstats"
 
 import "google.golang.org/grpc"
 
