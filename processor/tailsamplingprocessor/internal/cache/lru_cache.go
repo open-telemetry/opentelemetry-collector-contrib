131bbf9c6051f4521a83ff2eca587335b6fd6a08
James Moessis <jmoessis@atlassian.com>
2024-06-17 17:53:09 +1000

    porto

--- processor/tailsamplingprocessor/internal/cache/lru_cache.go
+++ processor/tailsamplingprocessor/internal/cache/lru_cache.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package cache
+package cache // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/tailsamplingprocessor/internal/cache"
 
 import (
 	"encoding/binary"
