e9f05b9fe63535e6dabcc58423f5923516ae736a
Evan Bradley <11745660+evan-bradley@users.noreply.github.com>
2024-06-21 08:50:00 -0400

    Update pkg/ottl/contexts/internal/schema.go

--- pkg/ottl/contexts/internal/schema.go
+++ pkg/ottl/contexts/internal/schema.go
@@ -2,8 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 package internal // import "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/internal"
 
-//nolint:gofmt
+//revive:disable:var-naming The methods in this interface are defined by pdata types.
 type SchemaURLItem interface {
 	SchemaUrl() string
 	SetSchemaUrl(v string)
 }
+//revive:enable:var-naming
