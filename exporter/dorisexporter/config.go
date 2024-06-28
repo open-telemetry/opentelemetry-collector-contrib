4f8d132f3b5d00cf8452b80d162931e089f40ade
composer <2789706336@qq.com>
2024-06-29 02:18:02 +0800

    refactor: endpoint and mysql_endpoint

--- exporter/dorisexporter/config.go
+++ exporter/dorisexporter/config.go
@@ -16,11 +16,11 @@ type Config struct {
 	configretry.BackOffConfig      `mapstructure:"retry_on_failure"`
 	exporterhelper.QueueSettings   `mapstructure:"sending_queue"`
 
-	// Endpoint is the http stream load address and mysql protocol tcp address.
-	Endpoint `mapstructure:"endpoint"`
 	// TableNames is the table name for logs, traces and metrics.
 	Table `mapstructure:"table"`
 
+	// Endpoint is the http stream load address.
+	Endpoint string `mapstructure:"endpoint"`
 	// Database is the database name.
 	Database string `mapstructure:"database"`
 	// Username is the authentication username.
@@ -29,17 +29,12 @@ type Config struct {
 	Password string `mapstructure:"password"`
 	// CreateSchema is whether databases and tables are created automatically.
 	CreateSchema bool `mapstructure:"create_schema"`
+	// MySQLEndpoint is the mysql protocol address to create the schema; ignored if create_schema is false.
+	MySQLEndpoint string `mapstructure:"mysql_endpoint"`
 	// Data older than these days will be deleted; ignored if create_schema is false. If set to 0, historical data will not be deleted.
 	HistoryDays int32 `mapstructure:"history_days"`
 	// Timezone is the timezone of the doris.
 	TimeZone string `mapstructure:"timezone"`
-}
-
-type Endpoint struct {
-	// HTTP is the http stream load address.
-	HTTP string `mapstructure:"http"`
-	// TCP is the mysql protocol tcp address; ignored if create_schema is false.
-	TCP string `mapstructure:"tcp"`
 }
 
 type Table struct {
@@ -52,12 +47,12 @@ type Table struct {
 }
 
 func (cfg *Config) Validate() (err error) {
-	if cfg.Endpoint.HTTP == "" {
-		err = errors.Join(err, errors.New("endpoint.http must be specified"))
+	if cfg.Endpoint == "" {
+		err = errors.Join(err, errors.New("endpoint must be specified"))
 	}
 	if cfg.CreateSchema {
-		if cfg.Endpoint.TCP == "" {
-			err = errors.Join(err, errors.New("endpoint.tcp must be specified"))
+		if cfg.MySQLEndpoint == "" {
+			err = errors.Join(err, errors.New("mysql_endpoint must be specified"))
 		}
 
 		if cfg.HistoryDays < 0 {
@@ -68,20 +63,21 @@ func (cfg *Config) Validate() (err error) {
 	return err
 }
 
+const (
+	defaultStart       = -2147483648 // IntMin
+	defaultHistoryDays = 498
+)
+
 func (cfg *Config) startAndHistoryDays() (int32, int32) {
 	if cfg.HistoryDays == 0 {
-		return -2147483648, 498
+		return defaultStart, defaultHistoryDays
 	}
 	if cfg.HistoryDays > 498 {
-		return -cfg.HistoryDays, 498
+		return -cfg.HistoryDays, defaultHistoryDays
 	}
 	return -cfg.HistoryDays, cfg.HistoryDays
 }
 
 func (cfg *Config) timeZone() (*time.Location, error) {
-	return time.LoadLocation(cfg.TimeZone)
-}
-
-func (cfg *Config) Test() (*time.Location, error) {
 	return time.LoadLocation(cfg.TimeZone)
 }
