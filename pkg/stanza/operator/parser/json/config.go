c0c19735013268375c328200ea234e884fc3d5cc
Keval Bhogayata <90185475+bhogayatakb@users.noreply.github.com>
2024-07-01 12:40:23 +0530

    fixed pkg/stanza failed checks (#82)

--- pkg/stanza/operator/parser/json/config.go
+++ pkg/stanza/operator/parser/json/config.go
@@ -4,13 +4,9 @@
 package json // import "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/operator/parser/json"
 
 import (
-	"context"
-	"fmt"
-
 	jsoniter "github.com/json-iterator/go"
 	"go.opentelemetry.io/collector/component"
 
-	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/entry"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/operator"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/operator/helper"
 )
@@ -49,34 +45,4 @@ func (c Config) Build(set component.TelemetrySettings) (operator.Operator, error
 		ParserOperator: parserOperator,
 		json:           jsoniter.ConfigFastest,
 	}, nil
-}
-
-// Parser is an operator that parses JSON.
-type Parser struct {
-	helper.ParserOperator
-	json jsoniter.API
-}
-
-// Process will parse an entry for JSON.
-func (j *Parser) Process(ctx context.Context, entry *entry.Entry) error {
-	return j.ParserOperator.ProcessWith(ctx, entry, j.parse)
-}
-
-// parse will parse a value as JSON.
-func (j *Parser) parse(value interface{}) (interface{}, error) {
-	fmt.Println(value)
-	if j.Flatten == true {
-		fmt.Println("Requested for flatten...!!")
-	}
-	var parsedValue map[string]interface{}
-	switch m := value.(type) {
-	case string:
-		err := j.json.UnmarshalFromString(m, &parsedValue)
-		if err != nil {
-			return nil, err
-		}
-	default:
-		return nil, fmt.Errorf("type %T cannot be parsed as JSON", value)
-	}
-	return parsedValue, nil
 }
