eacdf2178e1e87e43c1e89f83d9c186865ad0b21
Florian Bacher <florian.bacher@dynatrace.com>
2024-07-02 07:26:12 +0200

    fix test
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- pkg/stanza/operator/parser/syslog/parser_test.go
+++ pkg/stanza/operator/parser/syslog/parser_test.go
@@ -85,7 +85,7 @@ func TestSyslogParseRFC5424_Octet_Counting_MessageTooLong(t *testing.T) {
 	cfg := basicConfig()
 	cfg.Protocol = RFC5424
 	cfg.EnableOctetCounting = true
-	cfg.MaxLogSize = 214
+	cfg.MaxOctets = 214
 
 	body := `215 <86>1 2015-08-05T21:58:59.693Z 192.168.2.132 SecureAuth0 23108 ID52020 [SecureAuth@27389 UserHostAddress="192.168.2.132" Realm="SecureAuth0" UserID="Tester2" PEN="27389"] Found the user for retrieving user's profile`
 
