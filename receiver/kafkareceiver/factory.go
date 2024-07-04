ef83c95da04324e3536f9af33d446239f6fcd092
William Dumont <william.dumont@grafana.com>
2024-07-04 13:41:33 +0200

    change topic configuration for consistency with default behavior

--- receiver/kafkareceiver/factory.go
+++ receiver/kafkareceiver/factory.go
@@ -136,8 +136,8 @@ func (f *kafkaReceiverFactory) createTracesReceiver(
 	}
 
 	oCfg := *(cfg.(*Config))
-	if oCfg.Topic == "" {
-		oCfg.Topic = defaultTracesTopic
+	if oCfg.TracesTopic == "" {
+		oCfg.TracesTopic = defaultTracesTopic
 	}
 	unmarshaler := f.tracesUnmarshalers[oCfg.Encoding]
 	if unmarshaler == nil {
@@ -162,8 +162,8 @@ func (f *kafkaReceiverFactory) createMetricsReceiver(
 	}
 
 	oCfg := *(cfg.(*Config))
-	if oCfg.Topic == "" {
-		oCfg.Topic = defaultMetricsTopic
+	if oCfg.MetricsTopic == "" {
+		oCfg.MetricsTopic = defaultMetricsTopic
 	}
 	unmarshaler := f.metricsUnmarshalers[oCfg.Encoding]
 	if unmarshaler == nil {
@@ -188,8 +188,8 @@ func (f *kafkaReceiverFactory) createLogsReceiver(
 	}
 
 	oCfg := *(cfg.(*Config))
-	if oCfg.Topic == "" {
-		oCfg.Topic = defaultLogsTopic
+	if oCfg.LogsTopic == "" {
+		oCfg.LogsTopic = defaultLogsTopic
 	}
 	unmarshaler, err := getLogsUnmarshaler(oCfg.Encoding, f.logsUnmarshalers)
 	if err != nil {
--- receiver/kafkareceiver/kafka_receiver.go
+++ receiver/kafkareceiver/kafka_receiver.go
@@ -102,7 +102,7 @@ func newTracesReceiver(config Config, set receiver.Settings, unmarshaler TracesU
 
 	return &kafkaTracesConsumer{
 		config:            config,
-		topics:            []string{config.Topic},
+		topics:            []string{config.TracesTopic},
 		nextConsumer:      nextConsumer,
 		unmarshaler:       unmarshaler,
 		settings:          set,
@@ -222,7 +222,7 @@ func newMetricsReceiver(config Config, set receiver.Settings, unmarshaler Metric
 
 	return &kafkaMetricsConsumer{
 		config:            config,
-		topics:            []string{config.Topic},
+		topics:            []string{config.MetricsTopic},
 		nextConsumer:      nextConsumer,
 		unmarshaler:       unmarshaler,
 		settings:          set,
@@ -316,7 +316,7 @@ func newLogsReceiver(config Config, set receiver.Settings, unmarshaler LogsUnmar
 
 	return &kafkaLogsConsumer{
 		config:            config,
-		topics:            []string{config.Topic},
+		topics:            []string{config.LogsTopic},
 		nextConsumer:      nextConsumer,
 		unmarshaler:       unmarshaler,
 		settings:          set,
