8a2928b8366269c20d1c24e97a62c88a47d1f8b6
sh0rez <me@shorez.de>
2024-07-09 16:00:35 +0200

    [processor/deltatocumulative]: fixed histograms
    adds aggregation support for fixed-bucket / explicit bounds histograms

---
+++ processor/deltatocumulativeprocessor/internal/data/slices.go
@@ -0,0 +1,18 @@
+package data
+
+type Slice[E any] interface {
+	At(int) E
+	Len() int
+}
+
+func SlicesEqual[E comparable, S Slice[E]](a, b S) bool {
+	if a.Len() != b.Len() {
+		return false
+	}
+	for i := 0; i < a.Len(); i++ {
+		if a.At(i) != b.At(i) {
+			return false
+		}
+	}
+	return true
+}
