832ed2843412828ed84fff7a31d2cc9467e346ff
Abhishek Kothari <abhishek.t@cloudwerx.tech>
2024-07-10 14:41:32 +0530

    Added google cloud monitoring receiver

---
+++ receiver/googlecloudmonitoringreceiver/receiver.go
@@ -0,0 +1,52 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package googlecloudmonitoringreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/googlecloudmonitoringreceiver"
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/pdata/pmetric"
+)
+
+type monitoringReceiver struct {
+	config *Config
+	logger *zap.Logger
+	cancel context.CancelFunc
+}
+
+func newGoogleCloudMonitoringReceiver(cfg *Config, logger *zap.Logger) *monitoringReceiver {
+	return &monitoringReceiver{
+		config: cfg,
+		logger: logger,
+	}
+}
+
+func (m *monitoringReceiver) Scrape(ctx context.Context) (pmetric.Metrics, error) {
+	metrics := pmetric.NewMetrics()
+	m.logger.Debug("Scrape metrics ")
+
+	return metrics, nil
+}
+
+func (m *monitoringReceiver) Start(ctx context.Context, _ component.Host) error {
+	ctx, m.cancel = context.WithCancel(ctx)
+	err := m.initialize(ctx)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *monitoringReceiver) Shutdown(context.Context) error {
+	m.logger.Debug("shutting down googlecloudmonitoringreceiver receiver")
+	return nil
+}
+
+func (m *monitoringReceiver) initialize(ctx context.Context) error {
+	// Implement the logic for handling metrics here.
+	return nil
+}
