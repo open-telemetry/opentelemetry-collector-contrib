ae518de3a8518e46d6aa697c183145a6b996a698
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2024-07-10 08:49:55 +0200

    more graceful shutdown, add some sleeps after server startups
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

--- testbed/testbed/test_case.go
+++ testbed/testbed/test_case.go
@@ -158,6 +158,7 @@ func (tc *TestCase) StartAgent(args ...string) {
 	}()
 
 	tc.WaitFor(tc.LoadGenerator.IsReady, "LoadGenerator isn't ready")
+	tc.Sleep(time.Second)
 }
 
 // StopAgent stops agent process.
@@ -181,6 +182,7 @@ func (tc *TestCase) StopLoad() {
 // StartBackend starts the specified backend type.
 func (tc *TestCase) StartBackend() {
 	require.NoError(tc.t, tc.MockBackend.Start(), "Cannot start backend")
+	tc.Sleep(time.Second)
 }
 
 // StopBackend stops the backend.
