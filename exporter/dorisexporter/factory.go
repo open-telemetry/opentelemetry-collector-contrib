52d3e33e4f31614fc4f18873f93a2db2f7adf187
composer <2789706336@qq.com>
2024-07-13 01:11:33 +0800

    chore: temporarily remove implementation

--- exporter/dorisexporter/factory.go
+++ exporter/dorisexporter/factory.go
@@ -44,41 +44,11 @@ func createDefaultConfig() component.Config {
 }
 
 func createLogsExporter(ctx context.Context, set exporter.Settings, cfg component.Config) (exporter.Logs, error) {
-	c := cfg.(*Config)
-	exporter, err := newLogsExporter(set.Logger, c)
-	if err != nil {
-		return nil, err
-	}
-	return exporterhelper.NewLogsExporter(
-		ctx,
-		set,
-		cfg,
-		exporter.pushLogData,
-		exporterhelper.WithStart(exporter.start),
-		exporterhelper.WithShutdown(exporter.shutdown),
-		exporterhelper.WithTimeout(c.TimeoutSettings),
-		exporterhelper.WithQueue(c.QueueSettings),
-		exporterhelper.WithRetry(c.BackOffConfig),
-	)
+	return nil, nil
 }
 
 func createTracesExporter(ctx context.Context, set exporter.Settings, cfg component.Config) (exporter.Traces, error) {
-	c := cfg.(*Config)
-	exporter, err := newTracesExporter(set.Logger, c)
-	if err != nil {
-		return nil, err
-	}
-	return exporterhelper.NewTracesExporter(
-		ctx,
-		set,
-		cfg,
-		exporter.pushTraceData,
-		exporterhelper.WithStart(exporter.start),
-		exporterhelper.WithShutdown(exporter.shutdown),
-		exporterhelper.WithTimeout(c.TimeoutSettings),
-		exporterhelper.WithQueue(c.QueueSettings),
-		exporterhelper.WithRetry(c.BackOffConfig),
-	)
+	return nil, nil
 }
 
 func createMetricsExporter(ctx context.Context, set exporter.Settings, cfg component.Config) (exporter.Metrics, error) {
