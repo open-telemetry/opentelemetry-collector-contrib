e4b511579cad10c70dcf19142ff80be6e7e7ca55
dlopes7 <davidribeirolopes@gmail.com>
2024-07-14 11:43:15 -0500

    netflowreceiver - factory and config
    According to CONTRIBUTING.md, adding the scaffolding of the netflow receiver for the first PR

---
+++ receiver/netflowreceiver/config.go
@@ -0,0 +1,70 @@
+package netflowreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/netflowreceiver"
+
+import "fmt"
+
+// Config represents the receiver config settings within the collector's config.yaml
+type Config struct {
+	Listeners []ListenerConfig `mapstructure:"listeners"`
+}
+
+type ListenerConfig struct {
+
+	// The scheme defines the type of flow data that the listener will receive
+	// The scheme must be one of sflow, netflow, or flow
+	Scheme string `mapstructure:"scheme"`
+
+	// The hostname or IP address that the listener will bind to
+	Hostname string `mapstructure:"hostname"`
+
+	// The port that the listener will bind to
+	Port int `mapstructure:"port"`
+
+	// The number of sockets that the listener will use
+	Sockets int `mapstructure:"sockets"`
+
+	// The number of workers that the listener will use to decode incoming flow messages
+	// By default it will be two times the number of sockets
+	// Ideally set this to the number of CPU cores
+	Workers int `mapstructure:"workers"`
+
+	// The size of the queue that the listener will use
+	// This is a buffer that will hold flow messages before they are processed by a worker
+	QueueSize int `mapstructure:"queueSize"`
+}
+
+// Validate checks if the receiver configuration is valid
+func (cfg *Config) Validate() error {
+	validSchemes := [3]string{"sflow", "netflow", "flow"}
+
+	for _, listener := range cfg.Listeners {
+
+		validScheme := false
+		for _, scheme := range validSchemes {
+			if listener.Scheme == scheme {
+				validScheme = true
+				break
+			}
+		}
+		if !validScheme {
+			return fmt.Errorf("scheme must be one of sflow, netflow, or flow")
+		}
+
+		if listener.Sockets <= 0 {
+			return fmt.Errorf("sockets must be greater than 0")
+		}
+
+		if listener.Workers <= 0 {
+			return fmt.Errorf("workers must be greater than 0")
+		}
+
+		if listener.QueueSize <= 0 {
+			listener.QueueSize = defaultQueueSize
+		}
+
+		if listener.Port <= 0 {
+			return fmt.Errorf("port must be greater than 0")
+		}
+	}
+
+	return nil
+}
---
+++ receiver/netflowreceiver/factory.go
@@ -0,0 +1,60 @@
+package netflowreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/netflowreceiver"
+
+import (
+	"context"
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/receiver"
+)
+
+var (
+	typeStr = component.MustNewType("netflow")
+)
+
+const (
+	defaultSockets   = 1
+	defaultWorkers   = 2
+	defaultQueueSize = 1_000_000
+)
+
+// NewFactory creates a factory for netflow receiver.
+func NewFactory() receiver.Factory {
+	return receiver.NewFactory(
+		typeStr,
+		createDefaultConfig,
+		receiver.WithLogs(createLogsReceiver, component.StabilityLevelAlpha))
+}
+
+func createDefaultConfig() component.Config {
+	return &Config{
+		Listeners: []ListenerConfig{
+			{
+				Scheme:    "sflow",
+				Port:      6343,
+				Sockets:   defaultSockets,
+				Workers:   defaultWorkers,
+				QueueSize: defaultQueueSize,
+			},
+			{
+				Scheme:    "netflow",
+				Port:      2055,
+				Sockets:   defaultSockets,
+				Workers:   defaultWorkers,
+				QueueSize: defaultQueueSize,
+			},
+		},
+	}
+}
+
+func createLogsReceiver(_ context.Context, params receiver.CreateSettings, cfg component.Config, consumer consumer.Logs) (receiver.Logs, error) {
+	logger := params.Logger
+	conf := cfg.(*Config)
+
+	nr := &netflowReceiver{
+		logger:      logger,
+		logConsumer: consumer,
+		config:      conf,
+	}
+
+	return nr, nil
+}
