5f03ff984b7e83acb5714a6b0f89c3470e579fc0
dlopes7 <davidribeirolopes@gmail.com>
2024-07-17 21:43:17 -0500

    netflowreceiver - add config tests

--- receiver/netflowreceiver/config_test.go
+++ receiver/netflowreceiver/config_test.go
@@ -28,6 +28,41 @@ func TestLoadConfig(t *testing.T) {
 			id:       component.NewIDWithName(metadata.Type, "defaults"),
 			expected: createDefaultConfig(),
 		},
+		{
+			id:       component.NewIDWithName(metadata.Type, "two_listeners"),
+			expected: &Config{
+				Listeners: []ListenerConfig{
+					{
+						Scheme:   "netflow",
+						Port:     2055,
+						Sockets:  1,
+						Workers:  4,
+						QueueSize: 0,
+					},
+					{
+						Scheme:   "sflow",
+						Port:     6443,
+						Sockets:  1,
+						Workers:  2,
+						QueueSize: 1000,
+					},
+			},
+			},
+		},
+		{
+			id: 	  component.NewIDWithName(metadata.Type, "one_listener"),
+			expected: &Config{
+				Listeners: []ListenerConfig{
+					{
+						Scheme:   "netflow",
+						Port:     2055,
+						Sockets:  1,
+						Workers:  1,
+						QueueSize: 0,
+					},
+			},
+			},
+		},
 	}
 
 	for _, tt := range tests {
@@ -43,4 +78,42 @@ func TestLoadConfig(t *testing.T) {
 			assert.Equal(t, tt.expected, cfg)
 		})
 	}
+}
+
+
+func TestInvalidConfig(t *testing.T) {
+	t.Parallel()
+
+	cm, err := confmaptest.LoadConf(filepath.Join("testdata", "config.yaml"))
+	require.NoError(t, err)
+
+	tests := []struct {
+		id  component.ID
+		err string
+	}{
+		{
+			id:  component.NewIDWithName(metadata.Type, "invalid_schema"),
+			err: "scheme must be one of sflow, netflow, or flow",
+		},
+		{
+			id:  component.NewIDWithName(metadata.Type, "invalid_port"),
+			err: "port must be greater than 0",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.id.String(), func(t *testing.T) {
+			factory := NewFactory()
+			cfg := factory.CreateDefaultConfig()
+
+			sub, err := cm.Sub(tt.id.String())
+			require.NoError(t, err)
+			require.NoError(t, sub.Unmarshal(cfg))
+
+			err = component.ValidateConfig(cfg)
+			assert.ErrorContains(t, err, tt.err)
+		})
+	}
+
+
 }
