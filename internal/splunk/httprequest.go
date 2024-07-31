2c1f84f2db7d37a41317507ea0fc05e985d0fd4c
sbylica <sbylica@splunk.com>
2024-07-31 10:13:27 +0000

    .

--- internal/splunk/httprequest.go
+++ internal/splunk/httprequest.go
@@ -10,7 +10,7 @@ import (
 	"strconv"
 	"time"
 	"io"
-    "encoding/json"
+	"encoding/json"
 
 	"go.opentelemetry.io/collector/consumer/consumererror"
 	"go.opentelemetry.io/collector/exporter/exporterhelper"
