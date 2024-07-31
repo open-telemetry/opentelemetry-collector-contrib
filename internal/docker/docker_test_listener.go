6274501d794174d41cb7c19fea1af328dc31e421
Paulo Janotti <pjanotti@splunk.com>
2024-07-31 14:17:32 -0700

    make goporto

--- internal/docker/docker_test_listener.go
+++ internal/docker/docker_test_listener.go
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 //go:build !windows
 
-package docker
+package docker // import "github.com/open-telemetry/opentelemetry-collector-contrib/internal/docker"
 
 import (
 	"net"
