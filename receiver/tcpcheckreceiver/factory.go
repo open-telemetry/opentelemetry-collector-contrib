f154a2763dc2061025759b81e000564164e3aa71
huangyanfeng <huangyanfeng1992@gmail.com>
2024-08-07 14:57:52 +0800

    [receiver/tcpcheck] New receiver
    Introduce a new receiver, TCP Check Receiver.This receiver creates stats by connecting to an TCP server.
    
    Related to #34414
    
    N/A
    
    metadata.yaml, README.
    
    Signed-off-by: huangyanfeng <huangyanfeng1992@gmail.com>

---
+++ receiver/tcpcheckreceiver/factory.go
@@ -0,0 +1,53 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package tcpcheckreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/tcpcheckreceiver"
+
+import (
+	"context"
+	"time"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/receiver"
+	"go.opentelemetry.io/collector/receiver/scraperhelper"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/tcpcheckreceiver/internal/configtcp"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/tcpcheckreceiver/internal/metadata"
+)
+
+// NewFactory creates a factory for tcpcheckreceiver receiver.
+func NewFactory() receiver.Factory {
+	return receiver.NewFactory(
+		metadata.Type,
+		createDefaultConfig,
+		receiver.WithMetrics(createMetricsReceiver, metadata.MetricsStability))
+}
+
+func createDefaultConfig() component.Config {
+	cfg := scraperhelper.NewDefaultControllerConfig()
+	cfg.CollectionInterval = 10 * time.Second
+
+	return &Config{
+		ControllerConfig: cfg,
+		TCPClientSettings: configtcp.TCPClientSettings{
+			Timeout: 10 * time.Second,
+		},
+		MetricsBuilderConfig: metadata.DefaultMetricsBuilderConfig(),
+	}
+}
+
+func createMetricsReceiver(_ context.Context, params receiver.Settings, rConf component.Config, consumer consumer.Metrics) (receiver.Metrics, error) {
+	cfg, ok := rConf.(*Config)
+	if !ok {
+		return nil, errConfigNotTCPCheck
+	}
+
+	tcpCheckScraper := newScraper(cfg, params)
+	scraper, err := scraperhelper.NewScraper(metadata.Type, tcpCheckScraper.scrape, scraperhelper.WithStart(tcpCheckScraper.start))
+	if err != nil {
+		return nil, err
+	}
+
+	return scraperhelper.NewScraperControllerReceiver(&cfg.ControllerConfig, params, consumer, scraperhelper.AddScraper(scraper))
+}
