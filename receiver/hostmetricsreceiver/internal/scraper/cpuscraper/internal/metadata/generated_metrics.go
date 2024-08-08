a8099744cec2ef07e1aef1701df6f0973a848d40
Alex Boten <223565+codeboten@users.noreply.github.com>
2024-08-08 11:14:27 -0700

    [receiver/hostmetrics] update scope name for consistency (#34526)
    Update the scope name for telemetry produced by the hostmetrics
    receiver's scrapers from `otelcol/hostmetricsreceiver/*` to
    `github.com/open-telemetry/opentelemetry-collector-contrib/receiver/hostmetricsreceiver/internal/scraper/*`
    
    Part of
    https://github.com/open-telemetry/opentelemetry-collector/issues/9494
    
    ---------
    
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>

--- receiver/hostmetricsreceiver/internal/scraper/cpuscraper/internal/metadata/generated_metrics.go
+++ receiver/hostmetricsreceiver/internal/scraper/cpuscraper/internal/metadata/generated_metrics.go
@@ -412,7 +412,7 @@ func (mb *MetricsBuilder) EmitForResource(rmo ...ResourceMetricsOption) {
 	rm := pmetric.NewResourceMetrics()
 	rm.SetSchemaUrl(conventions.SchemaURL)
 	ils := rm.ScopeMetrics().AppendEmpty()
-	ils.Scope().SetName("otelcol/hostmetricsreceiver/cpu")
+	ils.Scope().SetName("github.com/open-telemetry/opentelemetry-collector-contrib/receiver/hostmetricsreceiver/internal/scraper/cpuscraper")
 	ils.Scope().SetVersion(mb.buildInfo.Version)
 	ils.Metrics().EnsureCapacity(mb.metricsCapacity)
 	mb.metricSystemCPUFrequency.emit(ils.Metrics())
