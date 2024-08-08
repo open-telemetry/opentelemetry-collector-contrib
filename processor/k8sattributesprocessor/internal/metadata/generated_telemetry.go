37ce11d7831486594eccdda069db72a2b66c7360
Alex Boten <223565+codeboten@users.noreply.github.com>
2024-08-08 13:46:37 -0700

    [processors] updating remaining processor scope names
    Doing this in a single PR as there are no additional changes other than running the generation.
    
    Part of open-telemetry/opentelemetry-collector#9494
    
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>

--- processor/k8sattributesprocessor/internal/metadata/generated_telemetry.go
+++ processor/k8sattributesprocessor/internal/metadata/generated_telemetry.go
@@ -14,11 +14,11 @@ import (
 )
 
 func Meter(settings component.TelemetrySettings) metric.Meter {
-	return settings.MeterProvider.Meter("otelcol/k8sattributes")
+	return settings.MeterProvider.Meter("github.com/open-telemetry/opentelemetry-collector-contrib/processor/k8sattributesprocessor")
 }
 
 func Tracer(settings component.TelemetrySettings) trace.Tracer {
-	return settings.TracerProvider.Tracer("otelcol/k8sattributes")
+	return settings.TracerProvider.Tracer("github.com/open-telemetry/opentelemetry-collector-contrib/processor/k8sattributesprocessor")
 }
 
 // TelemetryBuilder provides an interface for components to report telemetry
