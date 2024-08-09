cf88eb4ab3165865837ec87eb28c95ded1fd26bb
Mike Goldsmith <goldsmith.mike@gmail.com>
2024-08-09 16:45:16 +0100

    [processor/logdedup] Make processor name consistent (#34572)
    **Description:**
    
    Makes the processor name consistent.
    
    **Link to tracking Issue:**
    
    - Closes #34571
    
    **Testing:** 
    
    **Documentation:**

--- processor/logdedupprocessor/factory_test.go
+++ processor/logdedupprocessor/factory_test.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package logdeduplicationprocessor
+package logdedupprocessor
 
 import (
 	"context"
@@ -11,7 +11,7 @@ import (
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/processor"
 
-	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/logdeduplicationprocessor/internal/metadata"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/logdedupprocessor/internal/metadata"
 )
 
 func TestNewProcessorFactory(t *testing.T) {
