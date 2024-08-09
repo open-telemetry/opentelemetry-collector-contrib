80e63f3bc7783810e445580e995860de4d228b18
Alex Boten <223565+codeboten@users.noreply.github.com>
2024-08-09 13:19:34 -0700

    [chore] update core to bring in latest mdatagen (#34586)
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>

--- processor/groupbytraceprocessor/internal/metadata/generated_telemetry.go
+++ processor/groupbytraceprocessor/internal/metadata/generated_telemetry.go
@@ -13,14 +13,12 @@ import (
 	"go.opentelemetry.io/collector/config/configtelemetry"
 )
 
-const ScopeName = "github.com/open-telemetry/opentelemetry-collector-contrib/processor/groupbytraceprocessor"
-
 func Meter(settings component.TelemetrySettings) metric.Meter {
-	return settings.MeterProvider.Meter(ScopeName)
+	return settings.MeterProvider.Meter("github.com/open-telemetry/opentelemetry-collector-contrib/processor/groupbytraceprocessor")
 }
 
 func Tracer(settings component.TelemetrySettings) trace.Tracer {
-	return settings.TracerProvider.Tracer(ScopeName)
+	return settings.TracerProvider.Tracer("github.com/open-telemetry/opentelemetry-collector-contrib/processor/groupbytraceprocessor")
 }
 
 // TelemetryBuilder provides an interface for components to report telemetry
