7e6aa55f50b1429f20d27f86062b8b238ddda06f
Mike Goldsmth <goldsmith.mike@gmail.com>
2024-08-12 12:41:48 +0100

    run mdatagen

--- processor/logdedupprocessor/internal/metadata/generated_status.go
+++ processor/logdedupprocessor/internal/metadata/generated_status.go
@@ -7,7 +7,8 @@ import (
 )
 
 var (
-	Type = component.MustNewType("logdedup")
+	Type      = component.MustNewType("logdedup")
+	ScopeName = "github.com/open-telemetry/opentelemetry-collector-contrib/processor/logdedupprocessor"
 )
 
 const (
