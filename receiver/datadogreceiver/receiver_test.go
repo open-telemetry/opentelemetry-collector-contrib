5a3955dd34e59b2c9636be436181414c9bff2efd
Melchior MOULIN <melchior.moulin@gmail.com>
2024-08-19 11:16:11 +0200

    refacto test

--- receiver/datadogreceiver/receiver_test.go
+++ receiver/datadogreceiver/receiver_test.go
@@ -84,6 +84,12 @@ func TestDatadogServer(t *testing.T) {
 			expectCode:    http.StatusBadRequest,
 			expectContent: "Unable to unmarshal reqs\n",
 		},
+		{
+			name:          "Fake featuresdiscovery",
+			op:            nil, // Content-length: 0.
+			expectCode:    http.StatusBadRequest,
+			expectContent: "Fake featuresdiscovery\n",
+		},
 	} {
 		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
@@ -168,60 +174,6 @@ func TestDatadogMetricsV1_EndToEnd(t *testing.T) {
 	assert.Equal(t, 0.7, metric.Sum().DataPoints().At(0).DoubleValue())
 	expectedEnvironment, _ := metric.Sum().DataPoints().At(0).Attributes().Get("environment")
 	assert.Equal(t, "test", expectedEnvironment.AsString())
-}
-func TestDatadogFakeFeatureDiscovery(t *testing.T) {
-	cfg := createDefaultConfig().(*Config)
-	cfg.Endpoint = "localhost:0" // Using a randomly assigned address
-	dd, err := newDataDogReceiver(
-		cfg,
-		receivertest.NewNopSettings(),
-	)
-	dd.(*datadogReceiver).nextTracesConsumer = consumertest.NewNop()
-	require.NoError(t, err, "Must not error when creating receiver")
-
-	ctx, cancel := context.WithCancel(context.Background())
-	t.Cleanup(cancel)
-
-	require.NoError(t, dd.Start(ctx, componenttest.NewNopHost()))
-	t.Cleanup(func() {
-		require.NoError(t, dd.Shutdown(ctx), "Must not error shutting down")
-	})
-
-	for _, tc := range []struct {
-		name string
-		op   io.Reader
-
-		expectCode    int
-		expectContent string
-	}{
-		{
-			name:          "Fake featuresdiscovery",
-			op:            nil,
-			expectCode:    http.StatusBadRequest,
-			expectContent: "Fake featuresdiscovery\n",
-		},
-	} {
-		tc := tc
-		t.Run(tc.name, func(t *testing.T) {
-			t.Parallel()
-
-			req, err := http.NewRequest(
-				http.MethodPost,
-				fmt.Sprintf("http://%s/v0.7/traces", dd.(*datadogReceiver).address),
-				tc.op,
-			)
-			require.NoError(t, err, "Must not error when creating request")
-
-			resp, err := http.DefaultClient.Do(req)
-			require.NoError(t, err, "Must not error performing request")
-
-			actual, err := io.ReadAll(resp.Body)
-			require.NoError(t, errors.Join(err, resp.Body.Close()), "Must not error when reading body")
-
-			assert.Equal(t, tc.expectContent, string(actual))
-			assert.Equal(t, tc.expectCode, resp.StatusCode, "Must match the expected status code")
-		})
-	}
 }
 func TestDatadogMetricsV2_EndToEnd(t *testing.T) {
 	cfg := createDefaultConfig().(*Config)
