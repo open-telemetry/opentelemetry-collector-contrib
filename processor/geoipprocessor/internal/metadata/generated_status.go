1eed997ccbf14bd73e913482f4b6cfa2bd2887c5
Roger Coll <rogercoll@protonmail.com>
2024-08-21 17:29:52 +0200

    chore: run gotidy

--- processor/geoipprocessor/internal/metadata/generated_status.go
+++ processor/geoipprocessor/internal/metadata/generated_status.go
@@ -7,7 +7,8 @@ import (
 )
 
 var (
-	Type = component.MustNewType("geoip")
+	Type      = component.MustNewType("geoip")
+	ScopeName = "github.com/open-telemetry/opentelemetry-collector-contrib/processor/geoipprocessor"
 )
 
 const (
