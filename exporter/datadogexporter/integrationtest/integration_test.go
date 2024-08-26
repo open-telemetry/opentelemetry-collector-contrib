12c909eca85b9d77d5b30e5e8dd0a03b1f8899ef
Yang Song <yang.song@datadoghq.com>
2024-08-26 12:39:58 -0400

    lint

--- exporter/datadogexporter/integrationtest/integration_test.go
+++ exporter/datadogexporter/integrationtest/integration_test.go
@@ -533,7 +533,7 @@ func TestIntegrationInternalMetrics(t *testing.T) {
 	if runtime.GOOS == "windows" {
 		t.Skip("flaky test on windows https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/34836")
 	}
-	
+
 	// 1. Set up mock Datadog server
 	seriesRec := &testutil.HTTPRequestRecorderWithChan{Pattern: testutil.MetricV2Endpoint, ReqChan: make(chan []byte, 100)}
 	tracesRec := &testutil.HTTPRequestRecorderWithChan{Pattern: testutil.TraceEndpoint, ReqChan: make(chan []byte, 100)}
