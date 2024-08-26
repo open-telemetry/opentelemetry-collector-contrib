96a41ed38d789b4230ad7ab4fe3000981e8c151b
joeyyy09 <menteharshith@gmail.com>
2024-08-26 12:06:07 +0530

    Update tests and documentation
    Signed-off-by: joeyyy09 <menteharshith@gmail.com>

--- receiver/kafkareceiver/unmarshaler_test.go
+++ receiver/kafkareceiver/unmarshaler_test.go
@@ -14,6 +14,7 @@ import (
 func TestDefaultTracesUnMarshaler(t *testing.T) {
 	expectedEncodings := []string{
 		"otlp_proto",
+		"otlp_json",
 		"jaeger_proto",
 		"jaeger_json",
 		"zipkin_proto",
