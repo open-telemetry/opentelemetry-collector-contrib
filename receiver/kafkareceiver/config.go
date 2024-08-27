e102f2a7c11c5d3d33aff2f73b9094db131acfcf
Cody Kaczynski <cxdy@users.noreply.github.com>
2024-08-27 08:24:27 -0400

    docs: clarify max limit (none)
    Co-authored-by: Israel Blancas <iblancasa@gmail.com>

--- receiver/kafkareceiver/config.go
+++ receiver/kafkareceiver/config.go
@@ -83,7 +83,7 @@ type Config struct {
 	MinFetchSize int32 `mapstructure:"min_fetch_size"`
 	// The default bytes per fetch from Kafka (default "1048576")
 	DefaultFetchSize int32 `mapstructure:"default_fetch_size"`
-	// The maximum bytes per fetch from Kafka (default "0")
+	// The maximum bytes per fetch from Kafka (default "0", no limit)
 	MaxFetchSize int32 `mapstructure:"max_fetch_size"`
 }
 
