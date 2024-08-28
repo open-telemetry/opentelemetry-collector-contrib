09192d66ee3d2fb46941756bed6c2958529399ae
Jared Tan <jian.tan@daocloud.io>
2024-08-28 21:58:59 +0800

    make -j2 goporto
    Signed-off-by: Jared Tan <jian.tan@daocloud.io>

--- pkg/pdatautil/attributes.go
+++ pkg/pdatautil/attributes.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package pdatautil
+package pdatautil // import "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/pdatautil"
 
 import "go.opentelemetry.io/collector/pdata/pcommon"
 
