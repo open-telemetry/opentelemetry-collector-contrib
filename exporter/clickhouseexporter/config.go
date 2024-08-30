77771f2fb28ef78b0f44b329b678ed830820df9e
Spencer Torres <contact@spencertorres.com>
2024-08-30 15:49:13 -0400

    rename client info to "otelcol"

--- exporter/clickhouseexporter/config.go
+++ exporter/clickhouseexporter/config.go
@@ -177,7 +177,7 @@ func (cfg *Config) buildDB() (*sql.DB, error) {
 		opts.ClientInfo.Products = []struct {
 			Name    string
 			Version string
-		}{{"otel-exporter", getCollectorVersion()}}
+		}{{"otelcol", getCollectorVersion()}}
 	}
 
 	conn := clickhouse.OpenDB(opts)
