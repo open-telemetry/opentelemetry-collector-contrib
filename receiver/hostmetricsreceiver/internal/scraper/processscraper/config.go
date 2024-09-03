3621e85ff611ed9e305101b817945d33d4d94703
LEACHOURI Khalil <kleachouri.murex@gmail.com>
2024-09-03 12:04:00 +0200

    Add ability to mute errors coming from process scraper mainly due to access rights

--- receiver/hostmetricsreceiver/internal/scraper/processscraper/config.go
+++ receiver/hostmetricsreceiver/internal/scraper/processscraper/config.go
@@ -22,29 +22,38 @@ type Config struct {
 	Include MatchConfig `mapstructure:"include"`
 	Exclude MatchConfig `mapstructure:"exclude"`
 
+	// MuteProcessAllErrors is a flag that will mute all the errors encountered when trying to read metrics of a process.
+	// When this flag is enabled, there is no need to activate any other error suppression flags.
+	MuteProcessAllErrors bool `mapstructure:"mute_process_all_errors,omitempty"`
+
 	// MuteProcessNameError is a flag that will mute the error encountered when trying to read a process name the
 	// collector does not have permission to read.
 	// See https://github.com/open-telemetry/opentelemetry-collector/issues/3004 for more information.
+	// This flag is ignored when MuteProcessAllErrors is set to true as all errors are muted.
 	MuteProcessNameError bool `mapstructure:"mute_process_name_error,omitempty"`
 
 	// MuteProcessIOError is a flag that will mute the error encountered when trying to read IO metrics of a process
 	// the collector does not have permission to read.
+	// This flag is ignored when MuteProcessAllErrors is set to true as all errors are muted.
 	MuteProcessIOError bool `mapstructure:"mute_process_io_error,omitempty"`
 
 	// MuteProcessCgroupError is a flag that will mute the error encountered when trying to read the cgroup of a process
 	// the collector does not have permission to read.
+	// This flag is ignored when MuteProcessAllErrors is set to true as all errors are muted.
 	MuteProcessCgroupError bool `mapstructure:"mute_process_cgroup_error,omitempty"`
 
 	// MuteProcessExeError is a flag that will mute the error encountered when trying to read the executable path of a process
-	// the collector does not have permission to read (Linux)
+	// the collector does not have permission to read (Linux).
+	// This flag is ignored when MuteProcessAllErrors is set to true as all errors are muted.
 	MuteProcessExeError bool `mapstructure:"mute_process_exe_error,omitempty"`
 
 	// MuteProcessUserError is a flag that will mute the error encountered when trying to read uid which
-	// doesn't exist on the system, eg. is owned by user existing in container only
+	// doesn't exist on the system, eg. is owned by user existing in container only.
+	// This flag is ignored when MuteProcessAllErrors is set to true as all errors are muted.
 	MuteProcessUserError bool `mapstructure:"mute_process_user_error,omitempty"`
 
 	// ScrapeProcessDelay is used to indicate the minimum amount of time a process must be running
-	// before metrics are scraped for it.  The default value is 0 seconds (0s)
+	// before metrics are scraped for it.  The default value is 0 seconds (0s).
 	ScrapeProcessDelay time.Duration `mapstructure:"scrape_process_delay"`
 }
 
