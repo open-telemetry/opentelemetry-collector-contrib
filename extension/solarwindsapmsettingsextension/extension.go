a55f64e4f44beb1373760785ebb9a5a3db62ef07
Jerry Leung <tingfung.leung@solarwinds.com>
2024-09-03 10:29:07 -0700

    Improved indentation

--- extension/solarwindsapmsettingsextension/extension.go
+++ extension/solarwindsapmsettingsextension/extension.go
@@ -92,89 +92,90 @@ func (extension *solarwindsapmSettingsExtension) Shutdown(_ context.Context) err
 
 func refresh(extension *solarwindsapmSettingsExtension, filename string) {
 	extension.telemetrySettings.Logger.Info("time to refresh", zap.String("endpoint", extension.config.ClientConfig.Endpoint))
-	if hostname, err := os.Hostname(); err != nil {
+	hostname, err := os.Hostname()
+	if err != nil {
 		extension.telemetrySettings.Logger.Error("unable to call os.Hostname()", zap.Error(err))
-	} else {
-		ctx, cancel := context.WithTimeout(context.Background(), grpcContextDeadline)
-		defer cancel()
+		return
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), grpcContextDeadline)
+	defer cancel()
 
-		request := &collectorpb.SettingsRequest{
-			ApiKey: extension.config.Key,
-			Identity: &collectorpb.HostID{
-				Hostname: hostname,
-			},
-			ClientVersion: "2",
+	request := &collectorpb.SettingsRequest{
+		ApiKey: extension.config.Key,
+		Identity: &collectorpb.HostID{
+			Hostname: hostname,
+		},
+		ClientVersion: "2",
+	}
+	response, err := extension.client.GetSettings(ctx, request)
+	if err != nil {
+		extension.telemetrySettings.Logger.Error("unable to get settings", zap.String("endpoint", extension.config.ClientConfig.Endpoint), zap.Error(err))
+		return
+	}
+	switch result := response.GetResult(); result {
+	case collectorpb.ResultCode_OK:
+		if len(response.GetWarning()) > 0 {
+			extension.telemetrySettings.Logger.Warn("GetSettings succeed", zap.String("result", result.String()), zap.String("warning", response.GetWarning()))
 		}
-		response, err := extension.client.GetSettings(ctx, request)
-		if err != nil {
-			extension.telemetrySettings.Logger.Error("unable to get settings", zap.String("endpoint", extension.config.ClientConfig.Endpoint), zap.Error(err))
-			return
+		var settings []map[string]any
+		for _, item := range response.GetSettings() {
+			setting := make(map[string]any)
+			setting["flags"] = string(item.GetFlags())
+			setting["timestamp"] = item.GetTimestamp()
+			setting["value"] = item.GetValue()
+			arguments := make(map[string]any)
+			if value, ok := item.Arguments["BucketCapacity"]; ok {
+				arguments["BucketCapacity"] = math.Float64frombits(binary.LittleEndian.Uint64(value))
+			}
+			if value, ok := item.Arguments["BucketRate"]; ok {
+				arguments["BucketRate"] = math.Float64frombits(binary.LittleEndian.Uint64(value))
+			}
+			if value, ok := item.Arguments["TriggerRelaxedBucketCapacity"]; ok {
+				arguments["TriggerRelaxedBucketCapacity"] = math.Float64frombits(binary.LittleEndian.Uint64(value))
+			}
+			if value, ok := item.Arguments["TriggerRelaxedBucketRate"]; ok {
+				arguments["TriggerRelaxedBucketRate"] = math.Float64frombits(binary.LittleEndian.Uint64(value))
+			}
+			if value, ok := item.Arguments["TriggerStrictBucketCapacity"]; ok {
+				arguments["TriggerStrictBucketCapacity"] = math.Float64frombits(binary.LittleEndian.Uint64(value))
+			}
+			if value, ok := item.Arguments["TriggerStrictBucketRate"]; ok {
+				arguments["TriggerStrictBucketRate"] = math.Float64frombits(binary.LittleEndian.Uint64(value))
+			}
+			if value, ok := item.Arguments["MetricsFlushInterval"]; ok {
+				arguments["MetricsFlushInterval"] = int32(binary.LittleEndian.Uint32(value))
+			}
+			if value, ok := item.Arguments["MaxTransactions"]; ok {
+				arguments["MaxTransactions"] = int32(binary.LittleEndian.Uint32(value))
+			}
+			if value, ok := item.Arguments["MaxCustomMetrics"]; ok {
+				arguments["MaxCustomMetrics"] = int32(binary.LittleEndian.Uint32(value))
+			}
+			if value, ok := item.Arguments["EventsFlushInterval"]; ok {
+				arguments["EventsFlushInterval"] = int32(binary.LittleEndian.Uint32(value))
+			}
+			if value, ok := item.Arguments["ProfilingInterval"]; ok {
+				arguments["ProfilingInterval"] = int32(binary.LittleEndian.Uint32(value))
+			}
+			setting["arguments"] = arguments
+			setting["ttl"] = item.GetTtl()
+			settings = append(settings, setting)
 		}
-		switch result := response.GetResult(); result {
-		case collectorpb.ResultCode_OK:
-			if len(response.GetWarning()) > 0 {
-				extension.telemetrySettings.Logger.Warn("GetSettings succeed", zap.String("result", result.String()), zap.String("warning", response.GetWarning()))
-			}
-			var settings []map[string]any
-			for _, item := range response.GetSettings() {
-				setting := make(map[string]any)
-				setting["flags"] = string(item.GetFlags())
-				setting["timestamp"] = item.GetTimestamp()
-				setting["value"] = item.GetValue()
-				arguments := make(map[string]any)
-				if value, ok := item.Arguments["BucketCapacity"]; ok {
-					arguments["BucketCapacity"] = math.Float64frombits(binary.LittleEndian.Uint64(value))
-				}
-				if value, ok := item.Arguments["BucketRate"]; ok {
-					arguments["BucketRate"] = math.Float64frombits(binary.LittleEndian.Uint64(value))
-				}
-				if value, ok := item.Arguments["TriggerRelaxedBucketCapacity"]; ok {
-					arguments["TriggerRelaxedBucketCapacity"] = math.Float64frombits(binary.LittleEndian.Uint64(value))
-				}
-				if value, ok := item.Arguments["TriggerRelaxedBucketRate"]; ok {
-					arguments["TriggerRelaxedBucketRate"] = math.Float64frombits(binary.LittleEndian.Uint64(value))
-				}
-				if value, ok := item.Arguments["TriggerStrictBucketCapacity"]; ok {
-					arguments["TriggerStrictBucketCapacity"] = math.Float64frombits(binary.LittleEndian.Uint64(value))
-				}
-				if value, ok := item.Arguments["TriggerStrictBucketRate"]; ok {
-					arguments["TriggerStrictBucketRate"] = math.Float64frombits(binary.LittleEndian.Uint64(value))
-				}
-				if value, ok := item.Arguments["MetricsFlushInterval"]; ok {
-					arguments["MetricsFlushInterval"] = int32(binary.LittleEndian.Uint32(value))
-				}
-				if value, ok := item.Arguments["MaxTransactions"]; ok {
-					arguments["MaxTransactions"] = int32(binary.LittleEndian.Uint32(value))
-				}
-				if value, ok := item.Arguments["MaxCustomMetrics"]; ok {
-					arguments["MaxCustomMetrics"] = int32(binary.LittleEndian.Uint32(value))
-				}
-				if value, ok := item.Arguments["EventsFlushInterval"]; ok {
-					arguments["EventsFlushInterval"] = int32(binary.LittleEndian.Uint32(value))
-				}
-				if value, ok := item.Arguments["ProfilingInterval"]; ok {
-					arguments["ProfilingInterval"] = int32(binary.LittleEndian.Uint32(value))
-				}
-				setting["arguments"] = arguments
-				setting["ttl"] = item.GetTtl()
-				settings = append(settings, setting)
-			}
-			if content, err := json.Marshal(settings); err != nil {
-				extension.telemetrySettings.Logger.Error("unable to marshal setting JSON[] byte from settings", zap.Error(err))
+		if content, err := json.Marshal(settings); err != nil {
+			extension.telemetrySettings.Logger.Error("unable to marshal setting JSON[] byte from settings", zap.Error(err))
+		} else {
+			if err := os.WriteFile(filename, content, 0600); err != nil {
+				extension.telemetrySettings.Logger.Error("unable to write "+filename, zap.Error(err))
 			} else {
-				if err := os.WriteFile(filename, content, 0600); err != nil {
-					extension.telemetrySettings.Logger.Error("unable to write "+filename, zap.Error(err))
+				if len(response.GetWarning()) > 0 {
+					extension.telemetrySettings.Logger.Warn(filename + " is refreshed (soft disabled)")
 				} else {
-					if len(response.GetWarning()) > 0 {
-						extension.telemetrySettings.Logger.Warn(filename + " is refreshed (soft disabled)")
-					} else {
-						extension.telemetrySettings.Logger.Info(filename + " is refreshed")
-					}
-					extension.telemetrySettings.Logger.Info(string(content))
+					extension.telemetrySettings.Logger.Info(filename + " is refreshed")
 				}
+				extension.telemetrySettings.Logger.Info(string(content))
 			}
-		default:
-			extension.telemetrySettings.Logger.Warn("GetSettings failed", zap.String("result", result.String()), zap.String("warning", response.GetWarning()))
 		}
+	default:
+		extension.telemetrySettings.Logger.Warn("GetSettings failed", zap.String("result", result.String()), zap.String("warning", response.GetWarning()))
 	}
 }
