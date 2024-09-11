d673dac6c81dbe4498fe491e98217d94fec8a8d1
Ankit Patel <8731662+ankitpatel96@users.noreply.github.com>
2024-09-11 19:35:57 -0400

    cleanup

--- processor/schemaprocessor/internal/translation/revision_v1.go
+++ processor/schemaprocessor/internal/translation/revision_v1.go
@@ -4,7 +4,6 @@
 package translation // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/schemaprocessor/internal/translation"
 
 import (
-	"go.opentelemetry.io/collector/pdata/ptrace"
 	"go.opentelemetry.io/otel/schema/v1.0/ast"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/schemaprocessor/internal/changelist"
@@ -143,89 +142,4 @@ func newLogsChangelist(logs ast.Logs) *changelist.ChangeList{
 		}
 	}
 	return &changelist.ChangeList{Migrators: values}
-}
-
-func newAttributeChangeSetSliceFromChanges(attrs ast.Attributes) *migrate.AttributeChangeSetSlice {
-	values := make([]*migrate.AttributeChangeSet, 0, 10)
-	for _, at := range attrs.Changes {
-		if renamed := at.RenameAttributes; renamed != nil {
-			values = append(values, migrate.NewAttributeChangeSet(renamed.AttributeMap))
-		}
-	}
-	return migrate.NewAttributeChangeSetSlice(values...)
-}
-
-func newSpanConditionalAttributeSlice(spans ast.Spans) *migrate.ConditionalAttributeSetSlice {
-	values := make([]*migrate.ConditionalAttributeSet, 0, 10)
-	for _, ch := range spans.Changes {
-		if renamed := ch.RenameAttributes; renamed != nil {
-			values = append(values, migrate.NewConditionalAttributeSet(
-				renamed.AttributeMap,
-				renamed.ApplyToSpans...,
-			))
-		}
-	}
-	return migrate.NewConditionalAttributeSetSlice(values...)
-}
-
-func newMetricConditionalSlice(metrics ast.Metrics) *migrate.ConditionalAttributeSetSlice {
-	values := make([]*migrate.ConditionalAttributeSet, 0, 10)
-	for _, ch := range metrics.Changes {
-		if rename := ch.RenameAttributes; rename != nil {
-			values = append(values, migrate.NewConditionalAttributeSet(rename.AttributeMap, rename.ApplyToMetrics...))
-		}
-	}
-	return migrate.NewConditionalAttributeSetSlice(values...)
-}
-
-func newMetricNameSignalSlice(metrics ast.Metrics) *migrate.SignalNameChangeSlice {
-	values := make([]*migrate.SignalNameChange, 0, 10)
-	for _, ch := range metrics.Changes {
-		//todo check nil?
-		values = append(values, migrate.NewSignalNameChange(ch.RenameMetrics))
-	}
-	return migrate.NewSignalNameChangeSlice(values...)
-}
-
-func generateSpanEventsRenameAttributes(ch ast.SpanEventsChange) func(span ptrace.Span) bool{
-	return func(span ptrace.Span) bool {
-		var spanNameMatches, spanEventMatches bool
-		if ch.RenameAttributes.ApplyToSpans == nil || len(ch.RenameAttributes.ApplyToSpans) == 0 {
-			spanNameMatches = true
-		} else {
-			for _, spanName := range ch.RenameAttributes.ApplyToSpans {
-				if span.Name() == string(spanName) {
-					spanNameMatches = true
-					break
-				}
-			}
-		}
-		if ch.RenameAttributes.ApplyToEvents == nil || len(ch.RenameAttributes.ApplyToEvents) == 0 {
-			spanEventMatches = true
-		} else {
-			for _, eventName := range ch.RenameAttributes.ApplyToEvents {
-				for i := 0; i < span.Events().Len(); i++  {
-					spanEvent := span.Events().At(i)
-					if spanEvent.Name() == string(eventName) {
-						spanEventMatches = true
-						break
-					}
-				}
-			}
-		}
-		return spanNameMatches && spanEventMatches
-	}
-}
-
-func newSpanEventsRenameAttributesOnSpanEventEvent(spanEvents ast.SpanEvents) *migrate.ConditionalLambdaAttributeSetSlice[ptrace.Span]{
-	var conditions []*migrate.ConditionalLambdaAttributeSet[ptrace.Span]
-	for _, ch := range spanEvents.Changes {
-		if rename := ch.RenameAttributes; rename != nil {
-			conditions = append(conditions, migrate.NewConditionalLambdaAttributeSet(
-				ch.RenameAttributes.AttributeMap,
-				generateSpanEventsRenameAttributes(ch),
-			))
-		}
-	}
-	return migrate.NewConditionalLambdaAttributeSetSlice(conditions...)
 }
