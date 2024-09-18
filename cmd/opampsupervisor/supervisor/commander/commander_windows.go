7c25bf2d983de93c6f5cc4cd2c2577058c600a5e
Dakota Paasman <122491662+dpaasman00@users.noreply.github.com>
2024-09-18 09:14:31 -0400

    alloc and free console

--- cmd/opampsupervisor/supervisor/commander/commander_windows.go
+++ cmd/opampsupervisor/supervisor/commander/commander_windows.go
@@ -6,25 +6,46 @@
 package commander
 
 import (
+	"fmt"
 	"os"
 	"syscall"
 
 	"golang.org/x/sys/windows"
+	"golang.org/x/sys/windows/svc"
 )
 
 var (
 	kernel32API = windows.NewLazySystemDLL("kernel32.dll")
 
-	ctrlEventProc = kernel32API.NewProc("GenerateConsoleCtrlEvent")
+	ctrlEventProc    = kernel32API.NewProc("GenerateConsoleCtrlEvent")
+	allocConsoleProc = kernel32API.NewProc("AllocConsole")
+	freeConsoleProc  = kernel32API.NewProc("FreeConsole")
 )
 
 func sendShutdownSignal(process *os.Process) error {
 	// signaling with os.Interrupt is not supported on windows systems,
 	// so we need to use the windows API to properly send a graceful shutdown signal.
 	// See: https://learn.microsoft.com/en-us/windows/console/generateconsolectrlevent
+	isService, err := svc.IsWindowsService()
+	if err != nil {
+		return fmt.Errorf("isWindowsService: %w", err)
+	}
+
+	if isService {
+		if err := allocConsole(); err != nil {
+			return fmt.Errorf("allocConsole: %w", err)
+		}
+	}
+
 	r, _, e := ctrlEventProc.Call(syscall.CTRL_BREAK_EVENT, uintptr(process.Pid))
 	if r == 0 {
-		return e
+		return fmt.Errorf("sendShutdownSignal to PID '%d': %w", process.Pid, e)
+	}
+
+	if isService {
+		if err := freeConsole(); err != nil {
+			return fmt.Errorf("freeConsole: %w", err)
+		}
 	}
 
 	return nil
@@ -37,4 +58,24 @@ func sysProcAttrs() *syscall.SysProcAttr {
 	return &syscall.SysProcAttr{
 		CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP,
 	}
+}
+
+func allocConsole() error {
+	// windows services don't get created with a console
+	// need to allocate a console in order to send CTRL_BREAK_EVENT to agent sub process
+	ret, _, err := allocConsoleProc.Call()
+	if ret == 0 {
+		return err
+	}
+	return nil
+}
+
+func freeConsole() error {
+	// windows services don't run with console, so we want to clean it up
+	// only keep console around while it is strictly necessary
+	ret, _, err := freeConsoleProc.Call()
+	if ret == 0 {
+		return err
+	}
+	return nil
 }
