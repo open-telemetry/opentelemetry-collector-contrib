d3982315cea410f9ac011cc335d1402e3f27e1bc
Vihas Makwana <vihas.makwana@elastic.co>
2024-09-20 21:19:58 +0530

    fix: header

--- internal/coreinternal/errorutil/http.go
+++ internal/coreinternal/errorutil/http.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package errorutil
+package errorutil // import "github.com/open-telemetry/opentelemetry-collector-contrib/internal/coreinternal/errorutil"
 
 import (
 	"net/http"
