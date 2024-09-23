f9b62e05eb015d41d212a6b63f0ded703b21b00b
shalper2 <shalpern@splunk.com>
2024-09-23 09:28:31 -0500

    first pass

--- receiver/windowsservicereceiver/factory.go
+++ receiver/windowsservicereceiver/factory.go
@@ -2,3 +2,25 @@
 // SPDX-License-Identifier: Apache-2.0
 
 package windowsservicereceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/windowsservicereceiver"
+
+import (
+	"time"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/receiver/scraperhelper"
+)
+
+const (
+	defaultInterval = 5 * time.Minute
+)
+
+func createDefaultConfig() component.Config {
+	scfg := scraperhelper.NewDefaultControllerConfig()
+	scfg.CollectionInterval = defaultInterval
+
+	return &Config{
+		ControllerConfig: scfg,
+		Services:         nil,
+		MonitorAll:       false,
+	}
+}
