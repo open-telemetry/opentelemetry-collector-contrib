02ee56c740c4b8e839d90126aeeaea713b9aad36
odubajDT <ondrej.dubaj@dynatrace.com>
2024-09-27 14:20:03 +0200

    add tests for metadata
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- internal/metadataproviders/kubeadm/metadata.go
+++ internal/metadataproviders/kubeadm/metadata.go
@@ -40,9 +40,10 @@ func NewProvider(configMapName string, configMapNamespace string, apiConf k8scon
 }
 
 func (k *kubeadmProvider) ClusterName(ctx context.Context) (string, error) {
-	cluster, err := k.kubeadmClient.CoreV1().ConfigMaps(k.configMapNamespace).Get(ctx, k.configMapName, metav1.GetOptions{})
+	configmap, err := k.kubeadmClient.CoreV1().ConfigMaps(k.configMapNamespace).Get(ctx, k.configMapName, metav1.GetOptions{})
 	if err != nil {
 		return "", fmt.Errorf("failed to fetch ConfigMap with name %s and namespace %s from K8s API: %w", k.configMapName, k.configMapNamespace, err)
 	}
-	return cluster.Name, nil
+
+	return configmap.Data["clusterName"], nil
 }
