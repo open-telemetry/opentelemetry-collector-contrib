34680c615a776ff985bd2f4c20e1f9c47f2232ec
odubajDT <ondrej.dubaj@dynatrace.com>
2024-09-27 14:33:32 +0200

    add unit tests
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- processor/resourcedetectionprocessor/internal/kubeadm/config_test.go
+++ processor/resourcedetectionprocessor/internal/kubeadm/config_test.go
@@ -1,1 +1,15 @@
 package kubeadm
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSetCustomEnvVar(t *testing.T) {
+	cfg := CreateDefaultConfig()
+	err := cfg.UpdateDefaults()
+	assert.NoError(t, err)
+	assert.Equal(t, defaultConfigMapName, cfg.configMapName)
+	assert.Equal(t, defaultConfigMapNamespace, cfg.configMapNamespace)
+}
