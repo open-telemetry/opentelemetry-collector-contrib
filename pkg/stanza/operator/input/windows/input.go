e464d20688e8d411ee95ff9f85095a7b82e7b233
Daniel Kuiper <44123852+kuiperda@users.noreply.github.com>
2024-10-01 08:01:35 -0400

    move code producing noisy error

--- pkg/stanza/operator/input/windows/input.go
+++ pkg/stanza/operator/input/windows/input.go
@@ -172,7 +172,7 @@ func (i *Input) Stop() error {
 	return i.stopRemoteSession()
 }
 
-// readOnInterval will read events with respect to the polling interval.
+// readOnInterval will read events with respect to the polling interval until it reaches the end of the channel. 
 func (i *Input) readOnInterval(ctx context.Context) {
 	defer i.wg.Done()
 
@@ -184,30 +184,7 @@ func (i *Input) readOnInterval(ctx context.Context) {
 		case <-ctx.Done():
 			return
 		case <-ticker.C:
-			i.readToEnd(ctx)
-		}
-	}
-}
-
-// readToEnd will read events from the subscription until it reaches the end of the channel.
-func (i *Input) readToEnd(ctx context.Context) {
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		default:
-			if count := i.read(ctx); count == 0 {
-				if i.isRemote() {
-					if err := i.startRemoteSession(); err != nil {
-						i.Logger().Error("Failed to re-establish remote session", zap.String("server", i.remote.Server), zap.Error(err))
-						return
-					}
-					if err := i.subscription.Open(i.startAt, uintptr(i.remoteSessionHandle), i.channel, i.bookmark); err != nil {
-						i.Logger().Error("Failed to re-open subscription for remote server", zap.String("server", i.remote.Server), zap.Error(err))
-					}
-				}
-				return
-			}
+			i.read(ctx)
 		}
 	}
 }
@@ -226,6 +203,14 @@ func (i *Input) read(ctx context.Context) int {
 			}
 			i.Logger().Info("Resubscribing, creating remote subscription")
 			i.subscription = NewRemoteSubscription(i.remote.Server)
+			if err := i.startRemoteSession(); err != nil {
+				i.Logger().Error("Failed to re-establish remote session", zap.String("server", i.remote.Server), zap.Error(err))
+				return 0
+			}
+			if err := i.subscription.Open(i.startAt, uintptr(i.remoteSessionHandle), i.channel, i.bookmark); err != nil {
+				i.Logger().Error("Failed to re-open subscription for remote server", zap.String("server", i.remote.Server), zap.Error(err))
+				return 0
+			}
 		}
 		return 0
 	}
