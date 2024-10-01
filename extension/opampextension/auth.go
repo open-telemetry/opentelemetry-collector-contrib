cc574bfdf76ebe363e75bff802550169a773602b
Brandon Johnson <brandon.johnson@observiq.com>
2024-10-01 03:26:07 -0400

    make goporto

--- extension/opampextension/auth.go
+++ extension/opampextension/auth.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package opampextension
+package opampextension // import "github.com/open-telemetry/opentelemetry-collector-contrib/extension/opampextension"
 
 import (
 	"bytes"
