af7067b936edd333ae39a0d10e447f47c1ababd7
Brandon Johnson <brandon.johnson@observiq.com>
2024-10-01 01:02:17 -0400

    extract tarball

--- cmd/opampsupervisor/e2e_test.go
+++ cmd/opampsupervisor/e2e_test.go
@@ -1778,7 +1778,6 @@ func TestSupervisorUpgradesAgent(t *testing.T) {
 		ServerProvidedAllPackagesHash: []byte{0x03, 0x04},
 	}, ps)
 
-	// TODO: Sample agent description to make sure new agent is running/bootstrapped
 	agentDesc := <-agentDescriptionChan
 	versionFound := false
 	for _, v := range agentDesc.IdentifyingAttributes {
