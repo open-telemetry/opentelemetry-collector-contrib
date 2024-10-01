2f543c16d5db5bc99f383708eaff4812cb411de3
Brandon Johnson <brandon.johnson@observiq.com>
2024-10-01 01:36:46 -0400

    comment grammar

--- cmd/opampsupervisor/supervisor/package_test.go
+++ cmd/opampsupervisor/supervisor/package_test.go
@@ -13,7 +13,7 @@ func TestMain(m *testing.M) {
 
 	goleak.VerifyTestMain(
 		m,
-		// This leaky goroutine it seems to come from using the cosign library.
+		// This leaky goroutine seems to come from the cosign dependency tree.
 		goleak.IgnoreAnyFunction("github.com/syndtr/goleveldb/leveldb.(*DB).mpoolDrain"),
 	)
 }
