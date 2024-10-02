b92ef7d6c73ab4a1893a632411038e94e46497bb
Arthur Silva Sens <arthursens2005@gmail.com>
2024-10-02 19:30:58 -0300

    feat: receiver/prometheusremotewrite - Content-Type negotiation
    Signed-off-by: Arthur Silva Sens <arthursens2005@gmail.com>

--- receiver/prometheusremotewritereceiver/receiver.go
+++ receiver/prometheusremotewritereceiver/receiver.go
@@ -8,12 +8,15 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
+	promconfig "github.com/prometheus/prometheus/config"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/component/componentstatus"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/receiver"
+	"go.uber.org/zap/zapcore"
 )
 
 func newRemoteWriteReceiver(settings receiver.Settings, cfg *Config, nextConsumer consumer.Metrics) (receiver.Metrics, error) {
@@ -64,6 +67,58 @@ func (prw *prometheusRemoteWriteReceiver) Shutdown(ctx context.Context) error {
 	return prw.server.Shutdown(ctx)
 }
 
-func (prw *prometheusRemoteWriteReceiver) handlePRW(_ http.ResponseWriter, _ *http.Request) {
-	fmt.Println("handleWrite called")
+func (prw *prometheusRemoteWriteReceiver) handlePRW(w http.ResponseWriter, req *http.Request) {
+	contentType := req.Header.Get("Content-Type")
+	if contentType == "" {
+		prw.settings.Logger.Warn("message received without Content-Type header, rejecting")
+		http.Error(w, "Content-Type header is required", http.StatusUnsupportedMediaType)
+		return
+	}
+
+	msgType, err := prw.parseProto(contentType)
+	if err != nil {
+		prw.settings.Logger.Warn("Error decoding remote-write request", zapcore.Field{Key: "error", Type: zapcore.ErrorType, Interface: err})
+		http.Error(w, err.Error(), http.StatusUnsupportedMediaType)
+		return
+	}
+	if msgType != promconfig.RemoteWriteProtoMsgV2 {
+		prw.settings.Logger.Warn("message received with unsupported proto version, rejecting")
+		http.Error(w, "Unsupported proto version", http.StatusUnsupportedMediaType)
+		return
+	}
+
+	// After parsing the content-type header, the next step would be to handle content-encoding.
+	// Luckly confighttp's Server has middleware that already decompress the request body for us.
+
+	// The next step in follow up PRs would be to decode the request body.
+}
+
+// parseProto parses the content-type header and returns the version of the remote-write protocol.
+// We can't expect that senders of remote-write v1 will add the "proto=" parameter since it was not
+// a requirement in v1. So, if the parameter is not found, we assume v1.
+func (prw *prometheusRemoteWriteReceiver) parseProto(contentType string) (promconfig.RemoteWriteProtoMsg, error) {
+	contentType = strings.TrimSpace(contentType)
+
+	parts := strings.Split(contentType, ";")
+	if parts[0] != "application/x-protobuf" {
+		return "", fmt.Errorf("expected %q as the first (media) part, got %v content-type", "application/x-protobuf", contentType)
+	}
+
+	for _, part := range parts[1:] {
+		parameter := strings.Split(part, "=")
+		if len(parameter) != 2 {
+			return "", fmt.Errorf("as per https://www.rfc-editor.org/rfc/rfc9110#parameter expected parameters to be key-values, got %v in %v content-type", part, contentType)
+		}
+
+		if strings.TrimSpace(parameter[0]) == "proto" {
+			ret := promconfig.RemoteWriteProtoMsg(parameter[1])
+			if err := ret.Validate(); err != nil {
+				return "", fmt.Errorf("got %v content type; %w", contentType, err)
+			}
+			return ret, nil
+		}
+	}
+
+	// No "proto=" parameter found, assume v1.
+	return promconfig.RemoteWriteProtoMsgV1, nil
 }
