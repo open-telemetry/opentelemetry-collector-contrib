756b4dadc188bf1af043da665f08b606d2e4d2d3
mackjmr <mackenzie.jomard@datadoghq.com>
2024-10-03 17:12:42 +0200

    [chore] [receiver/bigip] Use confighttp.NewDefaultClientConfig instead of manually creating struct
    **Description:**
    This PR makes usage of `NewDefaultClientConfig` instead of manually creating the confighttp.ClientConfig struct.
    
    **Link to tracking Issue:** #35457

--- receiver/bigipreceiver/factory_test.go
+++ receiver/bigipreceiver/factory_test.go
@@ -5,12 +5,14 @@ package bigipreceiver // import "github.com/open-telemetry/opentelemetry-collect
 
 import (
 	"context"
+	"net/http"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/config/confighttp"
+	"go.opentelemetry.io/collector/config/configopaque"
 	"go.opentelemetry.io/collector/consumer/consumertest"
 	"go.opentelemetry.io/collector/receiver/receivertest"
 	"go.opentelemetry.io/collector/receiver/scraperhelper"
@@ -19,6 +21,11 @@ import (
 )
 
 func TestNewFactory(t *testing.T) {
+	defaultMaxIdleConns := http.DefaultTransport.(*http.Transport).MaxIdleConns
+	defaultMaxIdleConnsPerHost := http.DefaultTransport.(*http.Transport).MaxIdleConnsPerHost
+	defaultMaxConnsPerHost := http.DefaultTransport.(*http.Transport).MaxConnsPerHost
+	defaultIdleConnTimeout := http.DefaultTransport.(*http.Transport).IdleConnTimeout
+
 	testCases := []struct {
 		desc     string
 		testFunc func(*testing.T)
@@ -40,8 +47,13 @@ func TestNewFactory(t *testing.T) {
 						CollectionInterval: 10 * time.Second,
 					},
 					ClientConfig: confighttp.ClientConfig{
-						Endpoint: defaultEndpoint,
-						Timeout:  10 * time.Second,
+						Endpoint:            defaultEndpoint,
+						Timeout:             10 * time.Second,
+						MaxIdleConns:        &defaultMaxIdleConns,
+						MaxIdleConnsPerHost: &defaultMaxIdleConnsPerHost,
+						MaxConnsPerHost:     &defaultMaxConnsPerHost,
+						IdleConnTimeout:     &defaultIdleConnTimeout,
+						Headers:             map[string]configopaque.String{},
 					},
 					MetricsBuilderConfig: metadata.DefaultMetricsBuilderConfig(),
 				}
