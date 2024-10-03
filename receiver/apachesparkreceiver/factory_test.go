8f249ac50b9b79d9b43fda20e5f59e90bd5fe9c2
mackjmr <mackenzie.jomard@datadoghq.com>
2024-10-03 15:49:05 +0200

    [chore] [receiver/apachespark] Use NewDefaultClientConfig instead of manually creating struct
    **Description:**
    This PR makes usage of `NewDefaultClientConfig` instead of manually creating the confighttp.ClientConfig struct.
    
    **Link to tracking Issue:** #35457

--- receiver/apachesparkreceiver/factory_test.go
+++ receiver/apachesparkreceiver/factory_test.go
@@ -5,12 +5,14 @@ package apachesparkreceiver
 
 import (
 	"context"
+	"net/http"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/config/confighttp"
+	"go.opentelemetry.io/collector/config/configopaque"
 	"go.opentelemetry.io/collector/consumer/consumertest"
 	"go.opentelemetry.io/collector/receiver/receivertest"
 	"go.opentelemetry.io/collector/receiver/scraperhelper"
@@ -19,6 +21,11 @@ import (
 )
 
 func TestNewFactory(t *testing.T) {
+	defaultMaxIdleConns := http.DefaultTransport.(*http.Transport).MaxIdleConns
+	defaultMaxIdleConnsPerHost := http.DefaultTransport.(*http.Transport).MaxIdleConnsPerHost
+	defaultMaxConnsPerHost := http.DefaultTransport.(*http.Transport).MaxConnsPerHost
+	defaultIdleConnTimeout := http.DefaultTransport.(*http.Transport).IdleConnTimeout
+
 	testCases := []struct {
 		desc     string
 		testFunc func(*testing.T)
@@ -41,7 +48,12 @@ func TestNewFactory(t *testing.T) {
 						InitialDelay:       time.Second,
 					},
 					ClientConfig: confighttp.ClientConfig{
-						Endpoint: defaultEndpoint,
+						Endpoint:            defaultEndpoint,
+						MaxIdleConns:        &defaultMaxIdleConns,
+						MaxIdleConnsPerHost: &defaultMaxIdleConnsPerHost,
+						MaxConnsPerHost:     &defaultMaxConnsPerHost,
+						IdleConnTimeout:     &defaultIdleConnTimeout,
+						Headers:             map[string]configopaque.String{},
 					},
 					MetricsBuilderConfig: metadata.DefaultMetricsBuilderConfig(),
 				}
