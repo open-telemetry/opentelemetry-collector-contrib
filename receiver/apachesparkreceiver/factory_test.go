6a4175c03794d84a8b94fa0af1c5039e357f6036
mackjmr <mackenzie.jomard@datadoghq.com>
2024-10-04 13:35:09 +0200

    feedback

--- receiver/apachesparkreceiver/factory_test.go
+++ receiver/apachesparkreceiver/factory_test.go
@@ -5,14 +5,12 @@ package apachesparkreceiver
 
 import (
 	"context"
-	"net/http"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/config/confighttp"
-	"go.opentelemetry.io/collector/config/configopaque"
 	"go.opentelemetry.io/collector/consumer/consumertest"
 	"go.opentelemetry.io/collector/receiver/receivertest"
 	"go.opentelemetry.io/collector/receiver/scraperhelper"
@@ -21,10 +19,8 @@ import (
 )
 
 func TestNewFactory(t *testing.T) {
-	defaultMaxIdleConns := http.DefaultTransport.(*http.Transport).MaxIdleConns
-	defaultMaxIdleConnsPerHost := http.DefaultTransport.(*http.Transport).MaxIdleConnsPerHost
-	defaultMaxConnsPerHost := http.DefaultTransport.(*http.Transport).MaxConnsPerHost
-	defaultIdleConnTimeout := http.DefaultTransport.(*http.Transport).IdleConnTimeout
+	clientConfig := confighttp.NewDefaultClientConfig()
+	clientConfig.Endpoint = defaultEndpoint
 
 	testCases := []struct {
 		desc     string
@@ -47,14 +43,7 @@ func TestNewFactory(t *testing.T) {
 						CollectionInterval: defaultCollectionInterval,
 						InitialDelay:       time.Second,
 					},
-					ClientConfig: confighttp.ClientConfig{
-						Endpoint:            defaultEndpoint,
-						MaxIdleConns:        &defaultMaxIdleConns,
-						MaxIdleConnsPerHost: &defaultMaxIdleConnsPerHost,
-						MaxConnsPerHost:     &defaultMaxConnsPerHost,
-						IdleConnTimeout:     &defaultIdleConnTimeout,
-						Headers:             map[string]configopaque.String{},
-					},
+					ClientConfig:         clientConfig,
 					MetricsBuilderConfig: metadata.DefaultMetricsBuilderConfig(),
 				}
 
