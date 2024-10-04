aae4800e56e7c137fad5dc45427d6cf8218ea855
Gemene Narcis <naarcis96@gmail.com>
2024-10-04 11:42:03 +0300

    dockerstatsreceiver: migrate to newer semconv version

--- receiver/dockerstatsreceiver/integration_test.go
+++ receiver/dockerstatsreceiver/integration_test.go
@@ -21,7 +21,7 @@ import (
 	"go.opentelemetry.io/collector/pipeline"
 	rcvr "go.opentelemetry.io/collector/receiver"
 	"go.opentelemetry.io/collector/receiver/receivertest"
-	conventions "go.opentelemetry.io/collector/semconv/v1.6.1"
+	conventions "go.opentelemetry.io/collector/semconv/v1.27.0"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zaptest"
 )
