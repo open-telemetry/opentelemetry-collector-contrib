3147bbafa6b364480c61fb707c582f7a6a6aa3c2
Juraj Michalek <juraj.michalek132@gmail.com>
2024-10-11 16:14:36 +0200

    chore: update existing function instead of creating a new one
    Signed-off-by: Juraj Michalek <juraj.michalek132@gmail.com>

--- pkg/translator/prometheusremotewrite/metrics_to_prw_v2_test.go
+++ pkg/translator/prometheusremotewrite/metrics_to_prw_v2_test.go
@@ -4,15 +4,12 @@
 package prometheusremotewrite
 
 import (
-	"fmt"
 	"testing"
 	"time"
 
 	writev2 "github.com/prometheus/prometheus/prompb/io/prometheus/write/v2"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/pdata/pcommon"
-	"go.opentelemetry.io/collector/pdata/pmetric"
-	"go.opentelemetry.io/collector/pdata/pmetric/pmetricotlp"
 )
 
 func TestFromMetricsV2(t *testing.T) {
@@ -26,7 +23,7 @@ func TestFromMetricsV2(t *testing.T) {
 	}
 
 	ts := uint64(time.Now().UnixNano())
-	payload := createExportRequestWithTimestamp(5, 0, 1, 3, 0, pcommon.Timestamp(ts))
+	payload := createExportRequest(5, 0, 1, 3, 0, pcommon.Timestamp(ts))
 	want := func() map[string]*writev2.TimeSeries {
 		return map[string]*writev2.TimeSeries{
 			"0": {
@@ -44,56 +41,4 @@ func TestFromMetricsV2(t *testing.T) {
 	require.Equal(t, wanted, tsMap)
 	require.NotNil(t, symbolsTable)
 
-}
-
-func createExportRequestWithTimestamp(resourceAttributeCount int, histogramCount int, nonHistogramCount int, labelsPerMetric int, exemplarsPerSeries int, timestamp pcommon.Timestamp) pmetricotlp.ExportRequest {
-	request := pmetricotlp.NewExportRequest()
-
-	rm := request.Metrics().ResourceMetrics().AppendEmpty()
-	generateAttributes(rm.Resource().Attributes(), "resource", resourceAttributeCount)
-
-	metrics := rm.ScopeMetrics().AppendEmpty().Metrics()
-
-	for i := 1; i <= histogramCount; i++ {
-		m := metrics.AppendEmpty()
-		m.SetEmptyHistogram()
-		m.SetName(fmt.Sprintf("histogram-%v", i))
-		m.Histogram().SetAggregationTemporality(pmetric.AggregationTemporalityCumulative)
-		h := m.Histogram().DataPoints().AppendEmpty()
-		h.SetTimestamp(timestamp)
-
-		// Set 50 samples, 10 each with values 0.5, 1, 2, 4, and 8
-		h.SetCount(50)
-		h.SetSum(155)
-		h.BucketCounts().FromRaw([]uint64{10, 10, 10, 10, 10, 0})
-		h.ExplicitBounds().FromRaw([]float64{.5, 1, 2, 4, 8, 16}) // Bucket boundaries include the upper limit (ie. each sample is on the upper limit of its bucket)
-
-		generateAttributes(h.Attributes(), "series", labelsPerMetric)
-		generateExemplars(h.Exemplars(), exemplarsPerSeries, timestamp)
-	}
-
-	for i := 1; i <= nonHistogramCount; i++ {
-		m := metrics.AppendEmpty()
-		m.SetEmptySum()
-		m.SetName(fmt.Sprintf("sum-%v", i))
-		m.Sum().SetAggregationTemporality(pmetric.AggregationTemporalityCumulative)
-		point := m.Sum().DataPoints().AppendEmpty()
-		point.SetTimestamp(timestamp)
-		point.SetDoubleValue(1.23)
-		generateAttributes(point.Attributes(), "series", labelsPerMetric)
-		generateExemplars(point.Exemplars(), exemplarsPerSeries, timestamp)
-	}
-
-	for i := 1; i <= nonHistogramCount; i++ {
-		m := metrics.AppendEmpty()
-		m.SetEmptyGauge()
-		m.SetName(fmt.Sprintf("gauge-%v", i))
-		point := m.Gauge().DataPoints().AppendEmpty()
-		point.SetTimestamp(timestamp)
-		point.SetDoubleValue(1.23)
-		generateAttributes(point.Attributes(), "series", labelsPerMetric)
-		generateExemplars(point.Exemplars(), exemplarsPerSeries, timestamp)
-	}
-
-	return request
 }
