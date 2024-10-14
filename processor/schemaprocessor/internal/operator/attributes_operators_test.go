361363f5b6b6f43dad6d8df001b18cc46c290141
Ankit Patel <8731662+ankitpatel96@users.noreply.github.com>
2024-10-14 14:33:18 -0400

    table tests and metrics

--- processor/schemaprocessor/internal/operator/attributes_operators_test.go
+++ processor/schemaprocessor/internal/operator/attributes_operators_test.go
@@ -9,6 +9,7 @@ import (
 	"go.opentelemetry.io/collector/pdata/pmetric"
 	"go.opentelemetry.io/collector/pdata/ptrace"
 
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/schemaprocessor/internal/alias"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/schemaprocessor/internal/migrate"
 )
 
@@ -23,58 +24,95 @@ func TestAttributeOperators(t *testing.T) {
 		SpanEventOperator: SpanEventAttributeOperator{attrChange},
 		ResourceMigrator:  ResourceAttributeOperator{attrChange},
 	}
-	// test for each type of operator within an AllOperator
-	t.Run("MetricOperatorSum", func(t *testing.T) {
-		// todo test for each type of metric
-		metric := pmetric.NewMetric()
-		metric.SetEmptySum().DataPoints().AppendEmpty().Attributes().PutStr("service_version", "1.0.0")
 
-		if err := allOperator.MetricOperator.Do(migrate.StateSelectorApply, metric); err != nil {
-			t.Errorf("unexpected error: %v", err)
-		}
-		val, ok := metric.Sum().DataPoints().At(0).Attributes().Get("service.version")
-		assert.True(t, ok)
-		assert.Equal(t, "1.0.0", val.Str())
-	})
-
-	t.Run("LogOperator", func(t *testing.T) {
-		log := plog.NewLogRecord()
-		log.Attributes().PutStr("service_version", "1.0.0")
-		if err := allOperator.LogOperator.Do(migrate.StateSelectorApply, log); err != nil {
-			t.Errorf("unexpected error: %v", err)
-		}
-		val, ok := log.Attributes().Get("service.version")
-		assert.True(t, ok)
-		assert.Equal(t, "1.0.0", val.Str())
-	})
-	t.Run("SpanOperator", func(t *testing.T) {
-		span := ptrace.NewSpan()
-		span.Attributes().PutStr("service_version", "1.0.0")
-		if err := allOperator.SpanOperator.Do(migrate.StateSelectorApply, span); err != nil {
-			t.Errorf("unexpected error: %v", err)
-		}
-		val, ok := span.Attributes().Get("service.version")
-		assert.True(t, ok)
-		assert.Equal(t, "1.0.0", val.Str())
-	})
-	t.Run("SpanEventOperator", func(t *testing.T) {
-		spanEvent := ptrace.NewSpanEvent()
-		spanEvent.Attributes().PutStr("service_version", "1.0.0")
-		if err := allOperator.SpanEventOperator.Do(migrate.StateSelectorApply, spanEvent); err != nil {
-			t.Errorf("unexpected error: %v", err)
-		}
-		val, ok := spanEvent.Attributes().Get("service.version")
-		assert.True(t, ok)
-		assert.Equal(t, "1.0.0", val.Str())
-	})
-	t.Run("ResourceMigrator", func(t *testing.T) {
-		resource := pcommon.NewResource()
-		resource.Attributes().PutStr("service_version", "1.0.0")
-		if err := allOperator.ResourceMigrator.Do(migrate.StateSelectorApply, resource); err != nil {
-			t.Errorf("unexpected error: %v", err)
-		}
-		val, ok := resource.Attributes().Get("service.version")
-		assert.True(t, ok)
-		assert.Equal(t, "1.0.0", val.Str())
-	})
+	tests := []struct {
+		name string
+		operator func() (alias.Attributed, error)
+	}{
+		{
+			name: "MetricOperatorExponentialHistogram",
+			operator: func() (alias.Attributed, error) {
+				metric := pmetric.NewMetric()
+				metric.SetEmptyExponentialHistogram().DataPoints().AppendEmpty().Attributes().PutStr("service_version", "1.0.0")
+				return metric.ExponentialHistogram().DataPoints().At(0), allOperator.MetricOperator.Do(migrate.StateSelectorApply, metric)
+			},
+		},
+		{
+			name: "MetricOperatorGauge",
+			operator: func() (alias.Attributed, error) {
+				metric := pmetric.NewMetric()
+				metric.SetEmptyGauge().DataPoints().AppendEmpty().Attributes().PutStr("service_version", "1.0.0")
+				return metric.Gauge().DataPoints().At(0), allOperator.MetricOperator.Do(migrate.StateSelectorApply, metric)
+			},
+		},
+		{
+			name: "MetricOperatorHistogram",
+			operator: func() (alias.Attributed, error) {
+				metric := pmetric.NewMetric()
+				metric.SetEmptyHistogram().DataPoints().AppendEmpty().Attributes().PutStr("service_version", "1.0.0")
+				return metric.Histogram().DataPoints().At(0), allOperator.MetricOperator.Do(migrate.StateSelectorApply, metric)
+			},
+		},
+		{
+			name: "MetricOperatorSum",
+			operator: func() (alias.Attributed, error) {
+				metric := pmetric.NewMetric()
+				metric.SetEmptySum().DataPoints().AppendEmpty().Attributes().PutStr("service_version", "1.0.0")
+				return metric.Sum().DataPoints().At(0), allOperator.MetricOperator.Do(migrate.StateSelectorApply, metric)
+			},
+		},
+		{
+			name: "MetricOperatorSummary",
+			operator: func() (alias.Attributed, error) {
+				metric := pmetric.NewMetric()
+				metric.SetEmptySummary().DataPoints().AppendEmpty().Attributes().PutStr("service_version", "1.0.0")
+				return metric.Summary().DataPoints().At(0), allOperator.MetricOperator.Do(migrate.StateSelectorApply, metric)
+			},
+		},
+		{
+			name: "LogOperator",
+			operator: func() (alias.Attributed, error) {
+				log := plog.NewLogRecord()
+				log.Attributes().PutStr("service_version", "1.0.0")
+				return log, allOperator.LogOperator.Do(migrate.StateSelectorApply, log)
+			},
+		},
+		{
+			name: "SpanOperator",
+			operator: func() (alias.Attributed, error) {
+				span := ptrace.NewSpan()
+				span.Attributes().PutStr("service_version", "1.0.0")
+				return span, allOperator.SpanOperator.Do(migrate.StateSelectorApply, span)
+			},
+		},
+		{
+			name: "SpanEventOperator",
+			operator: func() (alias.Attributed, error) {
+				spanEvent := ptrace.NewSpanEvent()
+				spanEvent.Attributes().PutStr("service_version", "1.0.0")
+				return spanEvent, allOperator.SpanEventOperator.Do(migrate.StateSelectorApply, spanEvent)
+			},
+		},
+		{
+			name: "ResourceMigrator",
+			operator: func() (alias.Attributed, error) {
+				resource := pcommon.NewResource()
+				resource.Attributes().PutStr("service_version", "1.0.0")
+				return resource, allOperator.ResourceMigrator.Do(migrate.StateSelectorApply, resource)
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			item, err := tt.operator()
+			if err != nil {
+				t.Errorf("unexpected error: %v", err)
+				return
+			}
+			attrs := item.Attributes()
+			val, ok := attrs.Get("service.version")
+			assert.True(t, ok)
+			assert.Equal(t, "1.0.0", val.Str())
+		})
+	}
 }
