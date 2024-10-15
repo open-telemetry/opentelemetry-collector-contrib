5b1b3b2a26f4c10bdfdea73a2645a1f2d9a21054
Ankit Patel <8731662+ankitpatel96@users.noreply.github.com>
2024-10-15 16:50:03 -0400

    rename migrator

--- processor/schemaprocessor/internal/operator/multi_conditional_attribute_operators.go
+++ processor/schemaprocessor/internal/operator/multi_conditional_attribute_operators.go
@@ -12,7 +12,7 @@ import (
 // SpanEventConditionalAttributeOperator is an operator that powers the [Span Event's rename_attributes] change.
 // [Span Event's rename_attributes]: https://opentelemetry.io/docs/specs/otel/schemas/file_format_v1.1.0/#rename_attributes-transformation-1
 type SpanEventConditionalAttributeOperator struct {
-	migrator migrate.MultiConditionalAttributeSet
+	MultiConditionalAttributeSet migrate.MultiConditionalAttributeSet
 }
 
 func (o SpanEventConditionalAttributeOperator) IsMigrator() {}
@@ -20,7 +20,7 @@ func (o SpanEventConditionalAttributeOperator) IsMigrator() {}
 func (o SpanEventConditionalAttributeOperator) Do(ss migrate.StateSelector, span ptrace.Span) error {
 	for e := 0; e < span.Events().Len(); e++ {
 		event := span.Events().At(e)
-		if err := o.migrator.Do(ss, event.Attributes(),
+		if err := o.MultiConditionalAttributeSet.Do(ss, event.Attributes(),
 			map[string]string{
 				"event.name": event.Name(),
 				"span.name":  span.Name(),
