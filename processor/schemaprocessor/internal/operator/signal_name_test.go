fa6ca80725e4cf992526f64dded16f5938324002
Ankit Patel <8731662+ankitpatel96@users.noreply.github.com>
2024-10-14 21:22:54 -0400

    more operator tests

---
+++ processor/schemaprocessor/internal/operator/signal_name_test.go
@@ -0,0 +1,31 @@
+package operator
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/pdata/pmetric"
+	"go.opentelemetry.io/collector/pdata/ptrace"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/schemaprocessor/internal/migrate"
+)
+
+func TestSpanEventSignalNameChangeOperator(t *testing.T) {
+	s := ptrace.NewSpan()
+	s.Events().AppendEmpty().SetName("event.name")
+	c := SpanEventSignalNameChange{SignalNameChange: migrate.NewSignalNameChange(map[string]string{
+		"event.name": "event_name",
+	})}
+	require.NoError(t, c.Do(migrate.StateSelectorApply, s))
+	require.Equal(t, "event_name", s.Events().At(0).Name())
+}
+
+func TestMetricSignalNameChangeOperator(t *testing.T) {
+	s := pmetric.NewMetric()
+	s.SetName("event.name")
+	c := MetricSignalNameChange{SignalNameChange: migrate.NewSignalNameChange(map[string]string{
+		"event.name": "event_name",
+	})}
+	require.NoError(t, c.Do(migrate.StateSelectorApply, s))
+	require.Equal(t, "event_name", s.Name())
+}
