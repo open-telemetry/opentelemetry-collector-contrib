0986213bd5d7274eed8aa9930a936e84e60c0fe2
Mike Goldsmith <goldsmith.mike@gmail.com>
2024-10-16 11:00:32 +0100

    Update AzureEventsHubReceiver with new resource logs translator (#35357)
    **Description:**
    
    Updates the AzureEventsHubReceiver to use the new azure resource logs
    translator.
    
    Includes the following changes:
    - Add config option to enable the new behaviour, default is off so
    requires users to opt-in
    - Add interface to allow switching over the previous and new translator
    logic
    - Update receiver to use new config option to return the appropriate
    translator
    - Update README with new configuration option
    
    Follow-up PR to adding the translator:
    - #34830 
    
    **Link to tracking Issue:**
    N/A
    
    **Testing:**
    Includes unit test to verify configuration defaults and changes.
    
    **Documentation:**
    N/A
    
    ---------
    
    Co-authored-by: Alex Boten <223565+codeboten@users.noreply.github.com>

--- receiver/azureeventhubreceiver/config_test.go
+++ receiver/azureeventhubreceiver/config_test.go
@@ -35,12 +35,14 @@ func TestLoadConfig(t *testing.T) {
 	assert.Equal(t, "", r0.(*Config).Offset)
 	assert.Equal(t, "", r0.(*Config).Partition)
 	assert.Equal(t, defaultLogFormat, logFormat(r0.(*Config).Format))
+	assert.False(t, r0.(*Config).ApplySemanticConventions)
 
 	r1 := cfg.Receivers[component.NewIDWithName(metadata.Type, "all")]
 	assert.Equal(t, "Endpoint=sb://namespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=superSecret1234=;EntityPath=hubName", r1.(*Config).Connection)
 	assert.Equal(t, "1234-5566", r1.(*Config).Offset)
 	assert.Equal(t, "foo", r1.(*Config).Partition)
 	assert.Equal(t, rawLogFormat, logFormat(r1.(*Config).Format))
+	assert.True(t, r1.(*Config).ApplySemanticConventions)
 }
 
 func TestMissingConnection(t *testing.T) {
