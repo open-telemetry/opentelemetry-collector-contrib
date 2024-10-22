eb9df65b2189fc90be391c7d0cdf274097bf0fa6
Dakota Paasman <122491662+dpaasman00@users.noreply.github.com>
2024-10-22 09:11:44 -0400

    [extension/opamp] Add `os.description` as non-identifying agent attribute (#35816)
    **Description:** <Describe what has changed.>
    <!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
    Ex. Adding a feature - Explain what this achieves.-->
    
    The opamp extension now reports additional information about the host
    machine's operating system, specifically the version. It does so by
    reporting the semantic convention `os.description`(defined
    [here](https://opentelemetry.io/docs/specs/semconv/attributes-registry/os/))
    as a non-identifying attribute in the agent description message.
    
    **Link to tracking Issue:** <Issue number if applicable> Closes #35555
    
    **Testing:** <Describe what testing was performed and which tests were
    added.> Unit tests updated.
    Verified `os.description` attribute shows up with values like 'macOS
    15.0' and 'Ubuntu 20.04.6 LTS'

--- extension/opampextension/opamp_agent_test.go
+++ extension/opampextension/opamp_agent_test.go
@@ -19,6 +19,7 @@ import (
 	"go.opentelemetry.io/collector/confmap/confmaptest"
 	"go.opentelemetry.io/collector/extension/extensiontest"
 	semconv "go.opentelemetry.io/collector/semconv/v1.27.0"
+	"go.uber.org/zap"
 )
 
 func TestNewOpampAgent(t *testing.T) {
@@ -53,6 +54,7 @@ func TestNewOpampAgentAttributes(t *testing.T) {
 func TestCreateAgentDescription(t *testing.T) {
 	hostname, err := os.Hostname()
 	require.NoError(t, err)
+	description := getOSDescription(zap.NewNop())
 
 	serviceName := "otelcol-distrot"
 	serviceVersion := "distro.0"
@@ -76,6 +78,7 @@ func TestCreateAgentDescription(t *testing.T) {
 				NonIdentifyingAttributes: []*protobufs.KeyValue{
 					stringKeyValue(semconv.AttributeHostArch, runtime.GOARCH),
 					stringKeyValue(semconv.AttributeHostName, hostname),
+					stringKeyValue(semconv.AttributeOSDescription, description),
 					stringKeyValue(semconv.AttributeOSType, runtime.GOOS),
 				},
 			},
@@ -99,6 +102,7 @@ func TestCreateAgentDescription(t *testing.T) {
 					stringKeyValue(semconv.AttributeHostArch, runtime.GOARCH),
 					stringKeyValue(semconv.AttributeHostName, hostname),
 					stringKeyValue(semconv.AttributeK8SPodName, "my-very-cool-pod"),
+					stringKeyValue(semconv.AttributeOSDescription, description),
 					stringKeyValue(semconv.AttributeOSType, runtime.GOOS),
 				},
 			},
@@ -119,6 +123,7 @@ func TestCreateAgentDescription(t *testing.T) {
 				NonIdentifyingAttributes: []*protobufs.KeyValue{
 					stringKeyValue(semconv.AttributeHostArch, runtime.GOARCH),
 					stringKeyValue(semconv.AttributeHostName, "override-host"),
+					stringKeyValue(semconv.AttributeOSDescription, description),
 					stringKeyValue(semconv.AttributeOSType, runtime.GOOS),
 				},
 			},
