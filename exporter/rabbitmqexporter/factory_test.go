cef64ce292645b578cc953c779a5068b3f014b4d
Magnus Landerblom <magnus@landerblom.se>
2024-10-22 05:57:35 +0200

    Allow specifying name on the connection to RabbitMQ (#34682)

--- exporter/rabbitmqexporter/factory_test.go
+++ exporter/rabbitmqexporter/factory_test.go
@@ -47,10 +47,30 @@ func TestCreateLogsExporter(t *testing.T) {
 	assert.NotNil(t, te)
 }
 
+func TestCreateMetricsExporterWithConnectionName(t *testing.T) {
+	factory := NewFactory()
+	cfg := factory.CreateDefaultConfig().(*Config)
+	cfg.Connection.Name = "my-conn-name"
+
+	te, err := factory.CreateMetricsExporter(context.Background(), exportertest.NewNopSettings(), cfg)
+	assert.NoError(t, err)
+	assert.NotNil(t, te)
+}
+
 func TestCreateExporterWithCustomRoutingKey(t *testing.T) {
 	factory := NewFactory()
 	cfg := factory.CreateDefaultConfig().(*Config)
 	cfg.Routing.RoutingKey = "custom_routing_key"
+
+	te, err := factory.CreateLogsExporter(context.Background(), exportertest.NewNopSettings(), cfg)
+	assert.NoError(t, err)
+	assert.NotNil(t, te)
+}
+
+func TestCreateExporterWithConnectionName(t *testing.T) {
+	factory := NewFactory()
+	cfg := factory.CreateDefaultConfig().(*Config)
+	cfg.Connection.Name = "my-conn-name"
 
 	te, err := factory.CreateLogsExporter(context.Background(), exportertest.NewNopSettings(), cfg)
 	assert.NoError(t, err)
@@ -63,6 +83,16 @@ func TestCreateExporterWithTLSSettings(t *testing.T) {
 	cfg.Connection.TLSConfig = &configtls.ClientConfig{}
 
 	te, err := factory.CreateLogsExporter(context.Background(), exportertest.NewNopSettings(), cfg)
+	assert.NoError(t, err)
+	assert.NotNil(t, te)
+}
+
+func TestCreateTracesExporterWithConnectionName(t *testing.T) {
+	factory := NewFactory()
+	cfg := factory.CreateDefaultConfig().(*Config)
+	cfg.Connection.Name = "my-conn-name"
+
+	te, err := factory.CreateTracesExporter(context.Background(), exportertest.NewNopSettings(), cfg)
 	assert.NoError(t, err)
 	assert.NotNil(t, te)
 }
