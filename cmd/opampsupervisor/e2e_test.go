eba94eedbaa63e21d64feb674de20795e888ef80
Brandon Johnson <brandon.johnson@observiq.com>
2024-10-23 15:53:09 -0400

    remove "v" prefix from version

--- cmd/opampsupervisor/e2e_test.go
+++ cmd/opampsupervisor/e2e_test.go
@@ -2030,7 +2030,7 @@ func TestSupervisorUpgradesAgent(t *testing.T) {
 			Packages: map[string]*protobufs.PackageAvailable{
 				"": {
 					Type:    protobufs.PackageType_PackageType_TopLevel,
-					Version: "v" + agentVersion,
+					Version: agentVersion,
 					Hash:    []byte{0x01, 0x02},
 					File: &protobufs.DownloadableFile{
 						DownloadUrl: agentURL,
@@ -2053,7 +2053,7 @@ func TestSupervisorUpgradesAgent(t *testing.T) {
 				Name:                 "",
 				AgentHasVersion:      "",
 				AgentHasHash:         nil,
-				ServerOfferedVersion: "v" + agentVersion,
+				ServerOfferedVersion: agentVersion,
 				ServerOfferedHash:    []byte{0x01, 0x02},
 				Status:               protobufs.PackageStatusEnum_PackageStatusEnum_Installing,
 			},
@@ -2066,9 +2066,9 @@ func TestSupervisorUpgradesAgent(t *testing.T) {
 		Packages: map[string]*protobufs.PackageStatus{
 			"": {
 				Name:                 "",
-				AgentHasVersion:      "v" + agentVersion,
+				AgentHasVersion:      agentVersion,
 				AgentHasHash:         []byte{0x01, 0x02},
-				ServerOfferedVersion: "v" + agentVersion,
+				ServerOfferedVersion: agentVersion,
 				ServerOfferedHash:    []byte{0x01, 0x02},
 				Status:               protobufs.PackageStatusEnum_PackageStatusEnum_Installed,
 			},
