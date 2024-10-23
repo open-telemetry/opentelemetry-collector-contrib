2af070600d81d72dc981f5068ef9d7b21b01e42f
Oseme Odigie <oseme.odigie@solace.com>
2024-10-23 15:54:24 -0400

    fixed formatting issues while running make generate

--- exporter/googlecloudexporter/generated_package_test.go
+++ exporter/googlecloudexporter/generated_package_test.go
@@ -3,8 +3,9 @@
 package googlecloudexporter
 
 import (
-	"go.uber.org/goleak"
 	"testing"
+
+	"go.uber.org/goleak"
 )
 
 func TestMain(m *testing.M) {
--- extension/jaegerremotesampling/generated_package_test.go
+++ extension/jaegerremotesampling/generated_package_test.go
@@ -3,8 +3,9 @@
 package jaegerremotesampling
 
 import (
-	"go.uber.org/goleak"
 	"testing"
+
+	"go.uber.org/goleak"
 )
 
 func TestMain(m *testing.M) {
