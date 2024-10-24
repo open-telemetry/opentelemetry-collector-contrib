5b94d4b4c6392238fbd1c181d030b6049746bab2
kaiyan-sheng <kaiyan.sheng@elastic.co>
2024-10-24 15:50:40 -0600

    fix lint

--- receiver/awsfirehosereceiver/factory_test.go
+++ receiver/awsfirehosereceiver/factory_test.go
@@ -7,11 +7,12 @@ import (
 	"context"
 	"testing"
 
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/awsfirehosereceiver/internal/unmarshaler/otlpmetricstream"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component/componenttest"
 	"go.opentelemetry.io/collector/consumer/consumertest"
 	"go.opentelemetry.io/collector/receiver/receivertest"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/awsfirehosereceiver/internal/unmarshaler/otlpmetricstream"
 )
 
 func TestValidConfig(t *testing.T) {
