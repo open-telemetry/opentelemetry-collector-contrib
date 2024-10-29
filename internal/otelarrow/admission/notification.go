9d32b6c8044cd1565a65733a76bf3ac4167f5ca7
Joshua MacDonald <josh.macdonald@gmail.com>
2024-10-29 11:33:56 -0700

    goporto

--- internal/otelarrow/admission/notification.go
+++ internal/otelarrow/admission/notification.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package admission
+package admission // import "github.com/open-telemetry/opentelemetry-collector-contrib/internal/otelarrow/admission"
 
 // notification.N is a minimal Go version of absl::Notification:
 //
