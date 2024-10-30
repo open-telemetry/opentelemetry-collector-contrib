2e60343e3f029ec24f3f3502c5ac764d9cf3f28d
Joshua MacDonald <jmacd@users.noreply.github.com>
2024-10-30 15:00:00 -0700

    (otelarrowreceiver): Fix incorrect OTLP admission control behavior (#36078)

--- receiver/otelarrowreceiver/internal/arrow/arrow_test.go
+++ receiver/otelarrowreceiver/internal/arrow/arrow_test.go
@@ -36,7 +36,6 @@ import (
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/trace"
-	"go.opentelemetry.io/otel/trace/noop"
 	"go.uber.org/mock/gomock"
 	"go.uber.org/zap/zaptest"
 	"golang.org/x/net/http2/hpack"
@@ -50,10 +49,10 @@ import (
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/otelarrowreceiver/internal/arrow/mock"
 )
 
-var noopTraces = noop.NewTracerProvider()
+var noopTelemetry = componenttest.NewNopTelemetrySettings()
 
 func defaultBQ() *admission.BoundedQueue {
-	return admission.NewBoundedQueue(noopTraces, int64(100000), int64(10))
+	return admission.NewBoundedQueue(noopTelemetry, int64(100000), int64(10))
 }
 
 type compareJSONTraces struct{ ptrace.Traces }
@@ -490,10 +489,10 @@ func TestBoundedQueueWithPdataHeaders(t *testing.T) {
 			var bq *admission.BoundedQueue
 			if tt.rejected {
 				ctc.stream.EXPECT().Send(statusOKFor(batch.BatchId)).Times(0)
-				bq = admission.NewBoundedQueue(noopTraces, int64(sizer.TracesSize(td)-100), 10)
+				bq = admission.NewBoundedQueue(noopTelemetry, int64(sizer.TracesSize(td)-100), 10)
 			} else {
 				ctc.stream.EXPECT().Send(statusOKFor(batch.BatchId)).Times(1).Return(nil)
-				bq = admission.NewBoundedQueue(noopTraces, defaultBoundedQueueLimit, 10)
+				bq = admission.NewBoundedQueue(noopTelemetry, defaultBoundedQueueLimit, 10)
 			}
 
 			ctc.start(ctc.newRealConsumer, bq)
