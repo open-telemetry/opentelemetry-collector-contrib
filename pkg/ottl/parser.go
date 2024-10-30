63b53b58be87fb42b303f9d23f9279c89f151554
edmocosta <11836452+edmocosta@users.noreply.github.com>
2024-10-30 15:39:05 +0100

    Move AppendStatementPathsContext to parser.go

--- pkg/ottl/parser.go
+++ pkg/ottl/parser.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/alecthomas/participle/v2"
 	"go.opentelemetry.io/collector/component"
@@ -195,6 +196,33 @@ func (p *Parser[K]) ParseCondition(condition string) (*Condition[K], error) {
 	}, nil
 }
 
+// AppendStatementPathsContext changes the given OTTL statement adding the context name prefix
+// to all context-less paths. No modifications are performed for paths which [Path.Context]
+// value matches any WithPathContextNames value.
+// The context argument must be valid WithPathContextNames value, otherwise an error is returned.
+func (p *Parser[K]) AppendStatementPathsContext(context string, statement string) (string, error) {
+	if _, ok := p.pathContextNames[context]; !ok {
+		return statement, fmt.Errorf(`unknown context "%s" for parser %T, valid options are: %s`, context, p, p.buildPathContextNamesText(""))
+	}
+	parsed, err := parseStatement(statement)
+	if err != nil {
+		return "", err
+	}
+	paths := getParsedStatementPaths(parsed)
+	if len(paths) == 0 {
+		return statement, nil
+	}
+
+	var missingContextOffsets []int
+	for _, it := range paths {
+		if _, ok := p.pathContextNames[it.Context]; !ok {
+			missingContextOffsets = append(missingContextOffsets, it.Pos.Offset)
+		}
+	}
+
+	return writeStatementWithPathsContext(context, statement, missingContextOffsets), nil
+}
+
 var parser = newParser[parsedStatement]()
 var conditionParser = newParser[booleanExpression]()
 
@@ -224,6 +252,29 @@ func parseCondition(raw string) (*booleanExpression, error) {
 	}
 
 	return parsed, nil
+}
+
+func writeStatementWithPathsContext(context string, statement string, offsets []int) string {
+	if len(offsets) == 0 {
+		return statement
+	}
+
+	contextPrefix := context + "."
+	var sb strings.Builder
+	sb.Grow(len(statement) + (len(contextPrefix) * len(offsets)))
+
+	left := 0
+	for i, offset := range offsets {
+		sb.WriteString(statement[left:offset])
+		sb.WriteString(contextPrefix)
+		if i+1 >= len(offsets) {
+			sb.WriteString(statement[offset:])
+		} else {
+			left = offset
+		}
+	}
+
+	return sb.String()
 }
 
 // newParser returns a parser that can be used to read a string into a parsedStatement. An error will be returned if the string
