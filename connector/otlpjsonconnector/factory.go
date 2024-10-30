2326e20569972dc9b46ff8bd4beec1ddb0008469
Khushi Jain <khushi.jain@elastic.co>
2024-10-30 15:38:31 +0530

    Update connector/otlpjsonconnector/factory.go
    Co-authored-by: Daniel Jaglowski <jaglows3@gmail.com>

--- connector/otlpjsonconnector/factory.go
+++ connector/otlpjsonconnector/factory.go
@@ -14,9 +14,9 @@ import (
 	"github.com/open-telemetry/opentelemetry-collector-contrib/connector/otlpjsonconnector/internal/metadata"
 )
 
-var logRegex = regexp.MustCompile(`resourceLogs"\s*:\s*\[`)
-var metricRegex = regexp.MustCompile(`resourceMetrics"\s*:\s*\[`)
-var traceRegex = regexp.MustCompile(`resourceSpans"\s*:\s*\[`)
+var logRegex = regexp.MustCompile(`^\{\s*"resourceLogs"\s*:\s*\[`)
+var metricRegex = regexp.MustCompile(`^\{\s*"resourceMetrics"\s*:\s*\[`)
+var traceRegex = regexp.MustCompile(`^\{\s*"resourceSpans"\s*:\s*\[`)
 
 // NewFactory returns a ConnectorFactory.
 func NewFactory() connector.Factory {
