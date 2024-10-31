64ab5bc0ee2a2ae86b082bd6b40ba9273a93f870
Dakota Paasman <122491662+dpaasman00@users.noreply.github.com>
2024-10-31 09:45:30 -0400

    dont do healthcheck if healthchecker is nil

--- cmd/opampsupervisor/supervisor/supervisor.go
+++ cmd/opampsupervisor/supervisor/supervisor.go
@@ -1095,7 +1095,7 @@ func (s *Supervisor) startHealthCheckTicker() {
 }
 
 func (s *Supervisor) healthCheck() {
-	if !s.commander.IsRunning() {
+	if !s.commander.IsRunning() || s.healthChecker == nil {
 		return
 	}
 
