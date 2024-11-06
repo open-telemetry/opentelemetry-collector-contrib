5d45d87bc401e9404e85a4e0170e1a3620b6808e
whitneygriffith <whitney.griffith16@gmail.com>
2024-11-06 20:10:46 +0000

    update mocks
    Signed-off-by: whitneygriffith <whitney.griffith16@gmail.com>

--- exporter/azuremonitorexporter/traceexporter_test.go
+++ exporter/azuremonitorexporter/traceexporter_test.go
@@ -118,6 +118,7 @@ func TestExporterTraceDataCallbackSingleSpanNoEnvelope(t *testing.T) {
 func getMockTransportChannel() *mockTransportChannel {
 	transportChannelMock := mockTransportChannel{}
 	transportChannelMock.On("Send", mock.Anything)
+	transportChannelMock.On("Flush", mock.Anything)
 	return &transportChannelMock
 }
 
