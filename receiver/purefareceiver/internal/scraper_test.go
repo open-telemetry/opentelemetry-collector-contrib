cf6065e9154c342ba2e6967a61d873e9fc984e97
Christopher Roberts <chrroberts@purestorage.com>
2024-11-07 08:23:12 -0600

    Added Test for Scrape Protocols in Scraper

--- receiver/purefareceiver/internal/scraper_test.go
+++ receiver/purefareceiver/internal/scraper_test.go
@@ -57,6 +57,7 @@ func TestToPrometheusConfig(t *testing.T) {
 	// verify
 	assert.NoError(t, err)
 	assert.Len(t, scCfgs, 1)
+	assert.NotNil(t, scCfgs[0].ScrapeProtocols)
 	assert.EqualValues(t, "purefa", scCfgs[0].Params.Get("namespace"))
 	assert.EqualValues(t, "the-token", scCfgs[0].HTTPClientConfig.BearerToken)
 	assert.True(t, scCfgs[0].HTTPClientConfig.TLSConfig.InsecureSkipVerify)
