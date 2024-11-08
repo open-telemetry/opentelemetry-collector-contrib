f31295eb365a2f2322abb8af5c5bbe75e78ad378
Dan Jaglowski <jaglows3@gmail.com>
2024-11-08 10:56:30 -0500

    [connector/routing] Add ability to route by span context

--- connector/routingconnector/internal/ptraceutil/traces_test.go
+++ connector/routingconnector/internal/ptraceutil/traces_test.go
@@ -80,3 +80,147 @@ func TestMoveResourcesIf(t *testing.T) {
 		})
 	}
 }
+
+func TestMoveSpansWithContextIf(t *testing.T) {
+	testCases := []struct {
+		name       string
+		moveIf     func(ptrace.ResourceSpans, ptrace.ScopeSpans, ptrace.Span) bool
+		from       ptrace.Traces
+		to         ptrace.Traces
+		expectFrom ptrace.Traces
+		expectTo   ptrace.Traces
+	}{
+		{
+			name: "move_none",
+			moveIf: func(_ ptrace.ResourceSpans, _ ptrace.ScopeSpans, _ ptrace.Span) bool {
+				return false
+			},
+			from:       ptraceutiltest.NewTraces("AB", "CD", "EF", "GH"),
+			to:         ptrace.NewTraces(),
+			expectFrom: ptraceutiltest.NewTraces("AB", "CD", "EF", "GH"),
+			expectTo:   ptrace.NewTraces(),
+		},
+		{
+			name: "move_all",
+			moveIf: func(_ ptrace.ResourceSpans, _ ptrace.ScopeSpans, _ ptrace.Span) bool {
+				return true
+			},
+			from:       ptraceutiltest.NewTraces("AB", "CD", "EF", "GH"),
+			to:         ptrace.NewTraces(),
+			expectFrom: ptrace.NewTraces(),
+			expectTo:   ptraceutiltest.NewTraces("AB", "CD", "EF", "GH"),
+		},
+		{
+			name: "move_all_from_one_resource",
+			moveIf: func(rl ptrace.ResourceSpans, _ ptrace.ScopeSpans, _ ptrace.Span) bool {
+				rname, ok := rl.Resource().Attributes().Get("resourceName")
+				return ok && rname.AsString() == "resourceB"
+			},
+			from:       ptraceutiltest.NewTraces("AB", "CD", "EF", "GH"),
+			to:         ptrace.NewTraces(),
+			expectFrom: ptraceutiltest.NewTraces("A", "CD", "EF", "GH"),
+			expectTo:   ptraceutiltest.NewTraces("B", "CD", "EF", "GH"),
+		},
+		{
+			name: "move_all_from_one_scope",
+			moveIf: func(rl ptrace.ResourceSpans, sl ptrace.ScopeSpans, _ ptrace.Span) bool {
+				rname, ok := rl.Resource().Attributes().Get("resourceName")
+				return ok && rname.AsString() == "resourceB" && sl.Scope().Name() == "scopeC"
+			},
+			from: ptraceutiltest.NewTraces("AB", "CD", "EF", "GH"),
+			to:   ptrace.NewTraces(),
+			expectFrom: ptraceutiltest.NewTracesFromOpts(
+				ptraceutiltest.WithResource('A',
+					ptraceutiltest.WithScope('C', ptraceutiltest.WithSpan('E', "GH"), ptraceutiltest.WithSpan('F', "GH")),
+					ptraceutiltest.WithScope('D', ptraceutiltest.WithSpan('E', "GH"), ptraceutiltest.WithSpan('F', "GH")),
+				),
+				ptraceutiltest.WithResource('B',
+					ptraceutiltest.WithScope('D', ptraceutiltest.WithSpan('E', "GH"), ptraceutiltest.WithSpan('F', "GH")),
+				),
+			),
+			expectTo: ptraceutiltest.NewTraces("B", "C", "EF", "GH"),
+		},
+		{
+			name: "move_all_from_one_scope_in_each_resource",
+			moveIf: func(_ ptrace.ResourceSpans, sl ptrace.ScopeSpans, _ ptrace.Span) bool {
+				return sl.Scope().Name() == "scopeD"
+			},
+			from:       ptraceutiltest.NewTraces("AB", "CD", "EF", "GH"),
+			to:         ptrace.NewTraces(),
+			expectFrom: ptraceutiltest.NewTraces("AB", "C", "EF", "GH"),
+			expectTo:   ptraceutiltest.NewTraces("AB", "D", "EF", "GH"),
+		},
+		{
+			name: "move_one",
+			moveIf: func(rl ptrace.ResourceSpans, sl ptrace.ScopeSpans, m ptrace.Span) bool {
+				rname, ok := rl.Resource().Attributes().Get("resourceName")
+				return ok && rname.AsString() == "resourceA" && sl.Scope().Name() == "scopeD" && m.Name() == "spanF"
+			},
+			from: ptraceutiltest.NewTraces("AB", "CD", "EF", "GH"),
+			to:   ptrace.NewTraces(),
+			expectFrom: ptraceutiltest.NewTracesFromOpts(
+				ptraceutiltest.WithResource('A',
+					ptraceutiltest.WithScope('C', ptraceutiltest.WithSpan('E', "GH"), ptraceutiltest.WithSpan('F', "GH")),
+					ptraceutiltest.WithScope('D', ptraceutiltest.WithSpan('E', "GH")),
+				),
+				ptraceutiltest.WithResource('B',
+					ptraceutiltest.WithScope('C', ptraceutiltest.WithSpan('E', "GH"), ptraceutiltest.WithSpan('F', "GH")),
+					ptraceutiltest.WithScope('D', ptraceutiltest.WithSpan('E', "GH"), ptraceutiltest.WithSpan('F', "GH")),
+				),
+			),
+			expectTo: ptraceutiltest.NewTraces("A", "D", "F", "GH"),
+		},
+		{
+			name: "move_one_from_each_scope",
+			moveIf: func(_ ptrace.ResourceSpans, _ ptrace.ScopeSpans, m ptrace.Span) bool {
+				return m.Name() == "spanE"
+			},
+			from:       ptraceutiltest.NewTraces("AB", "CD", "EF", "GH"),
+			to:         ptrace.NewTraces(),
+			expectFrom: ptraceutiltest.NewTraces("AB", "CD", "F", "GH"),
+			expectTo:   ptraceutiltest.NewTraces("AB", "CD", "E", "GH"),
+		},
+		{
+			name: "move_one_from_each_scope_in_one_resource",
+			moveIf: func(rl ptrace.ResourceSpans, _ ptrace.ScopeSpans, m ptrace.Span) bool {
+				rname, ok := rl.Resource().Attributes().Get("resourceName")
+				return ok && rname.AsString() == "resourceB" && m.Name() == "spanE"
+			},
+			from: ptraceutiltest.NewTraces("AB", "CD", "EF", "GH"),
+			to:   ptrace.NewTraces(),
+			expectFrom: ptraceutiltest.NewTracesFromOpts(
+				ptraceutiltest.WithResource('A',
+					ptraceutiltest.WithScope('C', ptraceutiltest.WithSpan('E', "GH"), ptraceutiltest.WithSpan('F', "GH")),
+					ptraceutiltest.WithScope('D', ptraceutiltest.WithSpan('E', "GH"), ptraceutiltest.WithSpan('F', "GH")),
+				),
+				ptraceutiltest.WithResource('B',
+					ptraceutiltest.WithScope('C', ptraceutiltest.WithSpan('F', "GH")),
+					ptraceutiltest.WithScope('D', ptraceutiltest.WithSpan('F', "GH")),
+				),
+			),
+			expectTo: ptraceutiltest.NewTraces("B", "CD", "E", "GH"),
+		},
+		{
+			name: "move_some_to_preexisting",
+			moveIf: func(_ ptrace.ResourceSpans, sl ptrace.ScopeSpans, _ ptrace.Span) bool {
+				return sl.Scope().Name() == "scopeD"
+			},
+			from:       ptraceutiltest.NewTraces("AB", "CD", "EF", "GH"),
+			to:         ptraceutiltest.NewTraces("1", "2", "3", "4"),
+			expectFrom: ptraceutiltest.NewTraces("AB", "C", "EF", "GH"),
+			expectTo: ptraceutiltest.NewTracesFromOpts(
+				ptraceutiltest.WithResource('1', ptraceutiltest.WithScope('2', ptraceutiltest.WithSpan('3', "4"))),
+				ptraceutiltest.WithResource('A', ptraceutiltest.WithScope('D', ptraceutiltest.WithSpan('E', "GH"), ptraceutiltest.WithSpan('F', "GH"))),
+				ptraceutiltest.WithResource('B', ptraceutiltest.WithScope('D', ptraceutiltest.WithSpan('E', "GH"), ptraceutiltest.WithSpan('F', "GH"))),
+			),
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			ptraceutil.MoveSpansWithContextIf(tt.from, tt.to, tt.moveIf)
+			assert.NoError(t, ptracetest.CompareTraces(tt.expectFrom, tt.from), "from not modified as expected")
+			assert.NoError(t, ptracetest.CompareTraces(tt.expectTo, tt.to), "to not as expected")
+		})
+	}
+}
