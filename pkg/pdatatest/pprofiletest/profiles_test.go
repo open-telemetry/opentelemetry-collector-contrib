4aed58052982ef81391401cf10822eafaa343294
odubajDT <ondrej.dubaj@dynatrace.com>
2024-11-12 15:26:50 +0100

    more polishing + unit tests
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- pkg/pdatatest/pprofiletest/profiles_test.go
+++ pkg/pdatatest/pprofiletest/profiles_test.go
@@ -8,7 +8,9 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/pprofile"
+	"go.uber.org/multierr"
 )
 
 func TestCompareProfiles(t *testing.T) {
@@ -280,7 +282,165 @@ func TestCompareProfileLineSlice(t *testing.T) {
 		expected pprofile.LineSlice
 		actual   pprofile.LineSlice
 		err      error
-	}{}
+	}{
+
+		{
+			name: "empty",
+			expected: func() pprofile.LineSlice {
+				l := pprofile.NewLineSlice()
+				return l
+			}(),
+			actual: func() pprofile.LineSlice {
+				l := pprofile.NewLineSlice()
+				return l
+			}(),
+		},
+		{
+			name: "equal",
+			expected: func() pprofile.LineSlice {
+				l := pprofile.NewLineSlice()
+				i1 := l.AppendEmpty()
+				i1.SetFunctionIndex(1)
+				i1.SetLine(3)
+				i1.SetColumn(3)
+				i2 := l.AppendEmpty()
+				i2.SetFunctionIndex(2)
+				i2.SetLine(4)
+				i2.SetColumn(4)
+				return l
+			}(),
+			actual: func() pprofile.LineSlice {
+				l := pprofile.NewLineSlice()
+				i1 := l.AppendEmpty()
+				i1.SetFunctionIndex(1)
+				i1.SetLine(3)
+				i1.SetColumn(3)
+				i2 := l.AppendEmpty()
+				i2.SetFunctionIndex(2)
+				i2.SetLine(4)
+				i2.SetColumn(4)
+				return l
+			}(),
+		},
+		{
+			name: "equal wrong order",
+			expected: func() pprofile.LineSlice {
+				l := pprofile.NewLineSlice()
+				i1 := l.AppendEmpty()
+				i1.SetFunctionIndex(1)
+				i1.SetLine(3)
+				i1.SetColumn(3)
+				i2 := l.AppendEmpty()
+				i2.SetFunctionIndex(2)
+				i2.SetLine(4)
+				i2.SetColumn(4)
+				return l
+			}(),
+			actual: func() pprofile.LineSlice {
+				l := pprofile.NewLineSlice()
+				i2 := l.AppendEmpty()
+				i2.SetFunctionIndex(2)
+				i2.SetLine(4)
+				i2.SetColumn(4)
+				i1 := l.AppendEmpty()
+				i1.SetFunctionIndex(1)
+				i1.SetLine(3)
+				i1.SetColumn(3)
+				return l
+			}(),
+			err: multierr.Combine(
+				errors.New(`lines are out of order: line "functionIndex: 1" expected at index 0, found at index 1`),
+				errors.New(`lines are out of order: line "functionIndex: 2" expected at index 1, found at index 0`),
+			),
+		},
+		{
+			name: "wrong length",
+			expected: func() pprofile.LineSlice {
+				l := pprofile.NewLineSlice()
+				i1 := l.AppendEmpty()
+				i1.SetFunctionIndex(1)
+				i1.SetLine(3)
+				i1.SetColumn(3)
+				return l
+			}(),
+			actual: func() pprofile.LineSlice {
+				l := pprofile.NewLineSlice()
+				i1 := l.AppendEmpty()
+				i1.SetFunctionIndex(1)
+				i1.SetLine(3)
+				i1.SetColumn(3)
+				i2 := l.AppendEmpty()
+				i2.SetFunctionIndex(2)
+				i2.SetLine(4)
+				i2.SetColumn(4)
+				return l
+			}(),
+			err: multierr.Combine(
+				errors.New(`number of lines doesn't match expected: 1, actual: 2`),
+			),
+		},
+		{
+			name: "not equal - does not match expected",
+			expected: func() pprofile.LineSlice {
+				l := pprofile.NewLineSlice()
+				i1 := l.AppendEmpty()
+				i1.SetFunctionIndex(1)
+				i1.SetLine(3)
+				i1.SetColumn(3)
+				i2 := l.AppendEmpty()
+				i2.SetFunctionIndex(2)
+				i2.SetLine(4)
+				i2.SetColumn(4)
+				return l
+			}(),
+			actual: func() pprofile.LineSlice {
+				l := pprofile.NewLineSlice()
+				i1 := l.AppendEmpty()
+				i1.SetFunctionIndex(1)
+				i1.SetLine(3)
+				i1.SetColumn(3)
+				i2 := l.AppendEmpty()
+				i2.SetFunctionIndex(2)
+				i2.SetLine(5)
+				i2.SetColumn(5)
+				return l
+			}(),
+			err: multierr.Combine(
+				errors.New(`line with "functionIndex: 2" does not match expected`),
+			),
+		},
+		{
+			name: "not equal - missing",
+			expected: func() pprofile.LineSlice {
+				l := pprofile.NewLineSlice()
+				i1 := l.AppendEmpty()
+				i1.SetFunctionIndex(1)
+				i1.SetLine(3)
+				i1.SetColumn(3)
+				i2 := l.AppendEmpty()
+				i2.SetFunctionIndex(2)
+				i2.SetLine(4)
+				i2.SetColumn(4)
+				return l
+			}(),
+			actual: func() pprofile.LineSlice {
+				l := pprofile.NewLineSlice()
+				i1 := l.AppendEmpty()
+				i1.SetFunctionIndex(1)
+				i1.SetLine(3)
+				i1.SetColumn(3)
+				i2 := l.AppendEmpty()
+				i2.SetFunctionIndex(3)
+				i2.SetLine(5)
+				i2.SetColumn(5)
+				return l
+			}(),
+			err: multierr.Combine(
+				errors.New(`missing expected line "functionIndex: 2"`),
+				errors.New(`unexpected profile line "functionIndex: 3"`),
+			),
+		},
+	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			assert.Equal(t, test.err, CompareProfileLineSlice(test.expected, test.actual))
@@ -294,7 +454,119 @@ func TestCompareProfileAttributeUnitSlice(t *testing.T) {
 		expected pprofile.AttributeUnitSlice
 		actual   pprofile.AttributeUnitSlice
 		err      error
-	}{}
+	}{
+		{
+			name: "empty",
+			expected: func() pprofile.AttributeUnitSlice {
+				l := pprofile.NewAttributeUnitSlice()
+				return l
+			}(),
+			actual: func() pprofile.AttributeUnitSlice {
+				l := pprofile.NewAttributeUnitSlice()
+				return l
+			}(),
+		},
+		{
+			name: "equal",
+			expected: func() pprofile.AttributeUnitSlice {
+				l := pprofile.NewAttributeUnitSlice()
+				i1 := l.AppendEmpty()
+				i1.SetAttributeKey(2)
+				i1.SetUnit(3)
+				i2 := l.AppendEmpty()
+				i2.SetAttributeKey(4)
+				i2.SetUnit(5)
+				return l
+			}(),
+			actual: func() pprofile.AttributeUnitSlice {
+				l := pprofile.NewAttributeUnitSlice()
+				i1 := l.AppendEmpty()
+				i1.SetAttributeKey(2)
+				i1.SetUnit(3)
+				i2 := l.AppendEmpty()
+				i2.SetAttributeKey(4)
+				i2.SetUnit(5)
+				return l
+			}(),
+		},
+		{
+			name: "equal wrong order",
+			expected: func() pprofile.AttributeUnitSlice {
+				l := pprofile.NewAttributeUnitSlice()
+				i1 := l.AppendEmpty()
+				i1.SetAttributeKey(2)
+				i1.SetUnit(3)
+				i2 := l.AppendEmpty()
+				i2.SetAttributeKey(4)
+				i2.SetUnit(5)
+				return l
+			}(),
+			actual: func() pprofile.AttributeUnitSlice {
+				l := pprofile.NewAttributeUnitSlice()
+				i2 := l.AppendEmpty()
+				i2.SetAttributeKey(4)
+				i2.SetUnit(5)
+				i1 := l.AppendEmpty()
+				i1.SetAttributeKey(2)
+				i1.SetUnit(3)
+				return l
+			}(),
+			err: multierr.Combine(
+				errors.New(`attributeUnits are out of order: attributeUnit "attributeKey: 2" expected at index 0, found at index 1`),
+				errors.New(`attributeUnits are out of order: attributeUnit "attributeKey: 4" expected at index 1, found at index 0`),
+			),
+		},
+		{
+			name: "wrong length",
+			expected: func() pprofile.AttributeUnitSlice {
+				l := pprofile.NewAttributeUnitSlice()
+				i1 := l.AppendEmpty()
+				i1.SetAttributeKey(2)
+				i1.SetUnit(3)
+				return l
+			}(),
+			actual: func() pprofile.AttributeUnitSlice {
+				l := pprofile.NewAttributeUnitSlice()
+				i1 := l.AppendEmpty()
+				i1.SetAttributeKey(2)
+				i1.SetUnit(3)
+				i2 := l.AppendEmpty()
+				i2.SetAttributeKey(4)
+				i2.SetUnit(5)
+				return l
+			}(),
+			err: multierr.Combine(
+				errors.New(`number of attributeUnits doesn't match expected: 1, actual: 2`),
+			),
+		},
+		{
+			name: "not equal",
+			expected: func() pprofile.AttributeUnitSlice {
+				l := pprofile.NewAttributeUnitSlice()
+				i1 := l.AppendEmpty()
+				i1.SetAttributeKey(2)
+				i1.SetUnit(3)
+				i2 := l.AppendEmpty()
+				i2.SetAttributeKey(4)
+				i2.SetUnit(5)
+				return l
+			}(),
+			actual: func() pprofile.AttributeUnitSlice {
+				l := pprofile.NewAttributeUnitSlice()
+				i1 := l.AppendEmpty()
+				i1.SetAttributeKey(2)
+				i1.SetUnit(3)
+				i2 := l.AppendEmpty()
+				i2.SetAttributeKey(6)
+				i2.SetUnit(7)
+				return l
+			}(),
+			err: multierr.Combine(
+				errors.New(`missing expected attributeUnit "attributeKey: 4"`),
+				errors.New(`unexpected profile attributeUnit "attributeKey: 6"`),
+			),
+		},
+	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			assert.Equal(t, test.err, CompareProfileAttributeUnitSlice(test.expected, test.actual))
@@ -308,7 +580,119 @@ func TestCompareProfileLinkSlice(t *testing.T) {
 		expected pprofile.LinkSlice
 		actual   pprofile.LinkSlice
 		err      error
-	}{}
+	}{
+		{
+			name: "empty",
+			expected: func() pprofile.LinkSlice {
+				l := pprofile.NewLinkSlice()
+				return l
+			}(),
+			actual: func() pprofile.LinkSlice {
+				l := pprofile.NewLinkSlice()
+				return l
+			}(),
+		},
+		{
+			name: "equal",
+			expected: func() pprofile.LinkSlice {
+				l := pprofile.NewLinkSlice()
+				i1 := l.AppendEmpty()
+				i1.SetSpanID(pcommon.SpanID([]byte("spanidnn")))
+				i1.SetTraceID(pcommon.TraceID([]byte("traceidnnnnnnnnn")))
+				i2 := l.AppendEmpty()
+				i2.SetSpanID(pcommon.SpanID([]byte("spanidn2")))
+				i2.SetTraceID(pcommon.TraceID([]byte("traceid2nnnnnnnn")))
+				return l
+			}(),
+			actual: func() pprofile.LinkSlice {
+				l := pprofile.NewLinkSlice()
+				i1 := l.AppendEmpty()
+				i1.SetSpanID(pcommon.SpanID([]byte("spanidnn")))
+				i1.SetTraceID(pcommon.TraceID([]byte("traceidnnnnnnnnn")))
+				i2 := l.AppendEmpty()
+				i2.SetSpanID(pcommon.SpanID([]byte("spanidn2")))
+				i2.SetTraceID(pcommon.TraceID([]byte("traceid2nnnnnnnn")))
+				return l
+			}(),
+		},
+		{
+			name: "equal wrong order",
+			expected: func() pprofile.LinkSlice {
+				l := pprofile.NewLinkSlice()
+				i1 := l.AppendEmpty()
+				i1.SetSpanID(pcommon.SpanID([]byte("spanidnn")))
+				i1.SetTraceID(pcommon.TraceID([]byte("traceidnnnnnnnnn")))
+				i2 := l.AppendEmpty()
+				i2.SetSpanID(pcommon.SpanID([]byte("spanidn2")))
+				i2.SetTraceID(pcommon.TraceID([]byte("traceid2nnnnnnnn")))
+				return l
+			}(),
+			actual: func() pprofile.LinkSlice {
+				l := pprofile.NewLinkSlice()
+				i2 := l.AppendEmpty()
+				i2.SetSpanID(pcommon.SpanID([]byte("spanidn2")))
+				i2.SetTraceID(pcommon.TraceID([]byte("traceid2nnnnnnnn")))
+				i1 := l.AppendEmpty()
+				i1.SetSpanID(pcommon.SpanID([]byte("spanidnn")))
+				i1.SetTraceID(pcommon.TraceID([]byte("traceidnnnnnnnnn")))
+				return l
+			}(),
+			err: multierr.Combine(
+				errors.New(`links are out of order: link "spanId: 7370616e69646e6e, traceId: 747261636569646e6e6e6e6e6e6e6e6e" expected at index 0, found at index 1`),
+				errors.New(`links are out of order: link "spanId: 7370616e69646e32, traceId: 74726163656964326e6e6e6e6e6e6e6e" expected at index 1, found at index 0`),
+			),
+		},
+		{
+			name: "wrong length",
+			expected: func() pprofile.LinkSlice {
+				l := pprofile.NewLinkSlice()
+				i1 := l.AppendEmpty()
+				i1.SetSpanID(pcommon.SpanID([]byte("spanidnn")))
+				i1.SetTraceID(pcommon.TraceID([]byte("traceidnnnnnnnnn")))
+				return l
+			}(),
+			actual: func() pprofile.LinkSlice {
+				l := pprofile.NewLinkSlice()
+				i2 := l.AppendEmpty()
+				i2.SetSpanID(pcommon.SpanID([]byte("spanidn2")))
+				i2.SetTraceID(pcommon.TraceID([]byte("traceid2nnnnnnnn")))
+				i1 := l.AppendEmpty()
+				i1.SetSpanID(pcommon.SpanID([]byte("spanidnn")))
+				i1.SetTraceID(pcommon.TraceID([]byte("traceidnnnnnnnnn")))
+				return l
+			}(),
+			err: multierr.Combine(
+				errors.New(`number of links doesn't match expected: 1, actual: 2`),
+			),
+		},
+		{
+			name: "not equal",
+			expected: func() pprofile.LinkSlice {
+				l := pprofile.NewLinkSlice()
+				i1 := l.AppendEmpty()
+				i1.SetSpanID(pcommon.SpanID([]byte("spanidnn")))
+				i1.SetTraceID(pcommon.TraceID([]byte("traceidnnnnnnnnn")))
+				i2 := l.AppendEmpty()
+				i2.SetSpanID(pcommon.SpanID([]byte("spanidn3")))
+				i2.SetTraceID(pcommon.TraceID([]byte("traceid3nnnnnnnn")))
+				return l
+			}(),
+			actual: func() pprofile.LinkSlice {
+				l := pprofile.NewLinkSlice()
+				i2 := l.AppendEmpty()
+				i2.SetSpanID(pcommon.SpanID([]byte("spanidn2")))
+				i2.SetTraceID(pcommon.TraceID([]byte("traceid2nnnnnnnn")))
+				i1 := l.AppendEmpty()
+				i1.SetSpanID(pcommon.SpanID([]byte("spanidnn")))
+				i1.SetTraceID(pcommon.TraceID([]byte("traceidnnnnnnnnn")))
+				return l
+			}(),
+			err: multierr.Combine(
+				errors.New(`missing expected link "spanId: 7370616e69646e33, traceId: 74726163656964336e6e6e6e6e6e6e6e"`),
+				errors.New(`unexpected profile link "spanId: 7370616e69646e32, traceId: 74726163656964326e6e6e6e6e6e6e6e"`),
+			),
+		},
+	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			assert.Equal(t, test.err, CompareProfileLinkSlice(test.expected, test.actual))
