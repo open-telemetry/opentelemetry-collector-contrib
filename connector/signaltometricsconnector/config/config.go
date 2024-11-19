51abe4d70e127722d7f7d4eee30149b80a172ffa
Vishal Raj <vishal.raj@elastic.co>
2024-11-19 18:44:58 +0000

    [connector/signaltometrics] Add boilerplate for the connector

---
+++ connector/signaltometricsconnector/config/config.go
@@ -0,0 +1,10 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package config // import "github.com/open-telemetry/opentelemetry-collector-contrib/connector/signaltometricsconnector/config"
+
+type Config struct{}
+
+func (c *Config) Validate() error {
+	return nil
+}
