e0a843044bdd0549f4eb3d991b5583010fae4271
Anuraj Jain <anurajjain@sharechat.co>
2024-11-25 18:22:36 +0530

    add metadata file and mdatagen doc.go file also add generated files by madatagen - documentation.go custom emitted metrics documentation - generated processor status section in Readme file - test file for generated status and telemetry

---
+++ processor/logratelimitprocessor/doc.go
@@ -0,0 +1,8 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+//go:generate mdatagen metadata.yaml
+
+// Package logratelimitprocessor implements a processor for filtering
+// (dropping) metrics and/or spans by various properties.
+package logratelimitprocessor // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/logratelimitprocessor"
