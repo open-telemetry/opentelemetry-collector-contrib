a9fe242d437909ec1ed65e25aebe138989491c3d
Michal Pristas <michal.pristas@gmail.com>
2024-11-27 12:13:36 +0100

    gofumpt

--- pkg/ottl/ottlfuncs/func_html_strip.go
+++ pkg/ottl/ottlfuncs/func_html_strip.go
@@ -29,9 +29,9 @@ func createHTMLStripFunction[K any](_ ottl.FunctionContext, oArgs ottl.Arguments
 	return htmlStrip(args.HTMLSource), nil
 }
 
-func htmlStrip[K any](HTMLSource ottl.StringGetter[K]) ottl.ExprFunc[K] {
+func htmlStrip[K any](unescapedSource ottl.StringGetter[K]) ottl.ExprFunc[K] {
 	return func(ctx context.Context, tCtx K) (any, error) {
-		htmlString, err := HTMLSource.Get(ctx, tCtx)
+		htmlString, err := unescapedSource.Get(ctx, tCtx)
 		if err != nil {
 			return nil, err
 		}
