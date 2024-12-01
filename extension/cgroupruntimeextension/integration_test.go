8e4ece3d781b8ebcdb640a0709ecab1c865c8c61
Roger Coll <rogercoll@protonmail.com>
2024-12-01 19:21:33 +0100

    ci: run integration tests as root

--- extension/cgroupruntimeextension/integration_test.go
+++ extension/cgroupruntimeextension/integration_test.go
@@ -1,5 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
+//go:build integration && root
+// +build integration,root
 
 package cgroupruntimeextension // import "github.com/open-telemetry/opentelemetry-collector-contrib/extension/cgroupruntimeextension"
 
