1305ff286069ad6d4f6c1e37559fccc1613303a8
Vishal Raj <vishal.raj@elastic.co>
2024-12-04 14:39:11 +0000

    [connector/signaltometrics]Add config validation and custom ottl funcs

---
+++ connector/signaltometricsconnector/internal/customottl/adjustedcount.go
@@ -0,0 +1,41 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package customottl // import "github.com/open-telemetry/opentelemetry-collector-contrib/connector/signaltometricsconnector/internal/customottl"
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/ottlspan"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/sampling"
+)
+
+func NewAdjustedCountFactory() ottl.Factory[ottlspan.TransformContext] {
+	return ottl.NewFactory("AdjustedCount", nil, createAdjustedCountFunction)
+}
+
+func createAdjustedCountFunction(_ ottl.FunctionContext, _ ottl.Arguments) (ottl.ExprFunc[ottlspan.TransformContext], error) {
+	return adjustedCount()
+}
+
+func adjustedCount() (ottl.ExprFunc[ottlspan.TransformContext], error) {
+	return func(_ context.Context, tCtx ottlspan.TransformContext) (any, error) {
+		tracestate := tCtx.GetSpan().TraceState().AsRaw()
+		w3cTraceState, err := sampling.NewW3CTraceState(tracestate)
+		if err != nil {
+			return float64(0), fmt.Errorf("failed to parse w3c tracestate: %v", err)
+		}
+		otTraceState := w3cTraceState.OTelValue()
+		if otTraceState == nil {
+			// If otel trace state is missing, default to 1
+			return float64(1), nil
+		}
+		if len(otTraceState.TValue()) == 0 {
+			// For non-probabilistic sampler OR always sampling threshold, default to 1
+			return float64(1), nil
+		}
+		return otTraceState.AdjustedCount(), nil
+	}, nil
+}
---
+++ connector/signaltometricsconnector/internal/customottl/funcs.go
@@ -0,0 +1,31 @@
+package customottl // import "github.com/open-telemetry/opentelemetry-collector-contrib/connector/signaltometricsconnector/internal/customottl"
+
+import (
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/ottldatapoint"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/ottllog"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/ottlspan"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/ottlfuncs"
+)
+
+func SpanFuncs() map[string]ottl.Factory[ottlspan.TransformContext] {
+	common := commonFuncs[ottlspan.TransformContext]()
+	adjustedCountFactory := NewAdjustedCountFactory()
+	common[adjustedCountFactory.Name()] = adjustedCountFactory
+	return common
+}
+
+func DatapointFuncs() map[string]ottl.Factory[ottldatapoint.TransformContext] {
+	return commonFuncs[ottldatapoint.TransformContext]()
+}
+
+func LogFuncs() map[string]ottl.Factory[ottllog.TransformContext] {
+	return commonFuncs[ottllog.TransformContext]()
+}
+
+func commonFuncs[K any]() map[string]ottl.Factory[K] {
+	getFactory := NewGetFactory[K]()
+	standard := ottlfuncs.StandardFuncs[K]()
+	standard[getFactory.Name()] = getFactory
+	return standard
+}
