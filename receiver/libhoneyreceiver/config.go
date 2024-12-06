1257289b6fc7c21417d63713ebe595acfaf34ebf
Mike Terhar <mike@terhar.com>
2024-12-06 15:46:06 +0000

    add license headers

--- receiver/libhoneyreceiver/config.go
+++ receiver/libhoneyreceiver/config.go
@@ -1,3 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
 package libhoneyreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/libhoneyreceiver"
 
 import (
