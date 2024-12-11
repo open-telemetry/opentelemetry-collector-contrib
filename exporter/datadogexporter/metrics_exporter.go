b265c3bf23e37b9bf9bb2649b34303adc546c6d7
Damian Murphy <spud@canva.com>
2024-12-11 15:22:41 +1030

    datadogexporter: read response body on pushSketches

--- exporter/datadogexporter/metrics_exporter.go
+++ exporter/datadogexporter/metrics_exporter.go
@@ -154,6 +154,10 @@ func (exp *metricsExporter) pushSketches(ctx context.Context, sl sketches.Sketch
 	}
 	defer resp.Body.Close()
 
+	// We must read the full response body from the http request to ensure that connections can be
+	// properly re-used. https://pkg.go.dev/net/http#Client.Do
+	_, err = io.Copy(io.Discard, resp.Body)
+
 	if resp.StatusCode >= 400 {
 		return clientutil.WrapError(fmt.Errorf("error when sending payload to %s: %s", sketches.SketchSeriesEndpoint, resp.Status), resp)
 	}
