56ba22f5e8a8eb75f38ffe7be21c29d17c160057
Damien Mathieu <42@dmathieu.com>
2024-12-12 18:15:39 +0100

    Update exporter/elasticsearchexporter/model_test.go

--- exporter/elasticsearchexporter/model_test.go
+++ exporter/elasticsearchexporter/model_test.go
@@ -1109,7 +1109,7 @@ func TestEncodeLogOtelMode(t *testing.T) {
 // helper function that creates the OTel LogRecord from the test structure
 func createTestOTelLogRecord(t *testing.T, rec OTelRecord) (plog.LogRecord, pcommon.InstrumentationScope, pcommon.Resource) {
 	record := plog.NewLogRecord()
-	record.SetTimestamp(pcommon.Timestamp(uint64(rec.Timestamp.UnixNano())))                 //nolint:gosec // this input is controller from test
+	record.SetTimestamp(pcommon.Timestamp(uint64(rec.Timestamp.UnixNano())))                 //nolint:gosec // this input is controller by tests
 	record.SetObservedTimestamp(pcommon.Timestamp(uint64(rec.ObservedTimestamp.UnixNano()))) //nolint:gosec // this input is controller from test
 
 	record.SetTraceID(pcommon.TraceID(rec.TraceID))
