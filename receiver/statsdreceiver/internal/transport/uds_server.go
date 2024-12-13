365d90d5a3391f80f637cff23a3cec6c959aa72d
Michael Li <li.michael1998@gmail.com>
2024-12-12 22:40:29 -0800

    remove socket deletion

--- receiver/statsdreceiver/internal/transport/uds_server.go
+++ receiver/statsdreceiver/internal/transport/uds_server.go
@@ -22,10 +22,6 @@ func NewUDSServer(transport Transport, socketPath string) (Server, error) {
 		return nil, fmt.Errorf("NewUDSServer with %s: %w", transport.String(), ErrUnsupportedPacketTransport)
 	}
 
-	if err := prepareSocket(socketPath); err != nil {
-		return nil, err
-	}
-
 	conn, err := net.ListenPacket(transport.String(), socketPath)
 	if err != nil {
 		return nil, fmt.Errorf("starting to listen %s socket: %w", transport.String(), err)
@@ -43,18 +39,4 @@ func NewUDSServer(transport Transport, socketPath string) (Server, error) {
 func (u *udsServer) Close() error {
 	os.Remove(u.packetConn.LocalAddr().String())
 	return u.packetConn.Close()
-}
-
-func prepareSocket(socketPath string) error {
-	if _, err := os.Stat(socketPath); err == nil {
-		// File exists, remove it
-		if err = os.Remove(socketPath); err != nil {
-			return fmt.Errorf("failed to remove existing socket file: %w", err)
-		}
-	} else if !os.IsNotExist(err) {
-		// Return any error that's not "file does not exist"
-		return fmt.Errorf("failed to stat socket file: %w", err)
-	}
-
-	return nil
 }
