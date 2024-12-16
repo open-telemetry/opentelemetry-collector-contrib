0a213625036c40e05b302f191f34c0263778fbe8
Damian Murphy <spud@canva.com>
2024-12-16 19:11:32 +1030

    datadogexporter: read response body on pushSketches (#36779)
    <!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
    Ex. Adding a feature - Explain what this achieves.-->
    #### Description
    
    The pushSketches function in the datadogexporter does not read the
    contents of the HTTP response's Body on return.
    
    The Go documentation states that the full response body from the http
    request should be read and closed to ensure that connections can be
    properly re-used: https://pkg.go.dev/net/http#Client.Do
    
    We saw this manifest as a high load of incoming metric traffic through
    the exporter creating a connection per request and essentially killing
    platform infrastructure (proxy, DNS) as a result.
    
    
    <!--Describe what testing was performed and which tests were added.-->
    #### Testing
    
    No additional testing added. Fix is running to great effect in private
    internal fork at extremely high scale.

--- exporter/datadogexporter/metrics_exporter.go
+++ exporter/datadogexporter/metrics_exporter.go
@@ -8,6 +8,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"sync"
 	"time"
@@ -153,6 +154,13 @@ func (exp *metricsExporter) pushSketches(ctx context.Context, sl sketches.Sketch
 		return clientutil.WrapError(fmt.Errorf("failed to do sketches HTTP request: %w", err), resp)
 	}
 	defer resp.Body.Close()
+
+	// We must read the full response body from the http request to ensure that connections can be
+	// properly re-used. https://pkg.go.dev/net/http#Client.Do
+	_, err = io.Copy(io.Discard, resp.Body)
+	if err != nil {
+		return clientutil.WrapError(fmt.Errorf("failed to read response body from sketches HTTP request: %w", err), resp)
+	}
 
 	if resp.StatusCode >= 400 {
 		return clientutil.WrapError(fmt.Errorf("error when sending payload to %s: %s", sketches.SketchSeriesEndpoint, resp.Status), resp)
