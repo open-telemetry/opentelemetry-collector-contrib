07bc32cf799567550fdfd4dd1495ba54e2a2de42
Rutvik Shiyani <88027613+RutvikS-crest@users.noreply.github.com>
2024-12-16 18:51:57 +0530

    remove commented code.

--- receiver/mongodbreceiver/metrics.go
+++ receiver/mongodbreceiver/metrics.go
@@ -157,7 +157,6 @@ func (s *mongodbScraper) recordConnections(now pcommon.Timestamp, doc bson.M, db
 func (s *mongodbScraper) recordFlowControlLagTime(now pcommon.Timestamp, doc bson.M, dbName string, errs *scrapererror.ScrapeErrors) {
 	metricPath := []string{"flowControl", "isLaggedTimeMicros"}
 	metricName := "mongodb.flowcontrol.lag.time"
-	// metricAttributes := fmt.Sprintf("%s", dbName)
 	val, err := collectMetric(doc, metricPath)
 	if err != nil {
 		errs.AddPartial(1, fmt.Errorf(collectMetricWithAttributes, metricName, dbName, err))
