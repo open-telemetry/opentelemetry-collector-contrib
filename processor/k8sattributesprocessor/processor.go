6e25656291f74d0e2958c786ca888ba601c2cc5d
Florian Bacher <florian.bacher@dynatrace.com>
2024-12-20 11:08:24 +0100

    simplify switch statement
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- processor/k8sattributesprocessor/processor.go
+++ processor/k8sattributesprocessor/processor.go
@@ -226,20 +226,17 @@ func (kp *kubernetesprocessor) addContainerAttributes(attrs pcommon.Map, pod *ku
 		if !ok {
 			return
 		}
-	default:
-		switch {
-		// if there is only one container in the pod, we can fall back to that container
-		case len(pod.Containers.ByID) == 1:
-			for _, c := range pod.Containers.ByID {
-				containerSpec = c
-			}
-		case len(pod.Containers.ByName) == 1:
-			for _, c := range pod.Containers.ByName {
-				containerSpec = c
-			}
-		default:
-			return
+	// if there is only one container in the pod, we can fall back to that container
+	case len(pod.Containers.ByID) == 1:
+		for _, c := range pod.Containers.ByID {
+			containerSpec = c
 		}
+	case len(pod.Containers.ByName) == 1:
+		for _, c := range pod.Containers.ByName {
+			containerSpec = c
+		}
+	default:
+		return
 	}
 	if containerSpec.Name != "" {
 		setResourceAttribute(attrs, conventions.AttributeK8SContainerName, containerSpec.Name)
