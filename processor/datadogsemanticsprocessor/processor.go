00dbc570fc89bd98723c622d636cc089bdcb347f
Ibraheem Aboulnaga <ibraheem.aboulnaga@datadoghq.com>
2024-12-20 15:03:16 -0500

    Introduce datadogsemanticsprocessor component with mappings for OTLP->DD trace semantics

---
+++ processor/datadogsemanticsprocessor/processor.go
@@ -0,0 +1,67 @@
+package datadogsemanticsprocessor
+
+import (
+	"context"
+	"fmt"
+	pb "github.com/DataDog/datadog-agent/pkg/proto/pbgo/trace"
+	"github.com/DataDog/datadog-agent/pkg/trace/transform"
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/ptrace"
+	"strings"
+)
+
+func (tp *tracesProcessor) processTraces(ctx context.Context, td ptrace.Traces) (output ptrace.Traces, err error) {
+	rspans := td.ResourceSpans()
+	for i := 0; i < rspans.Len(); i++ {
+		rspan := rspans.At(i)
+		for j := 0; j < rspan.ScopeSpans().Len(); j++ {
+			libspans := rspan.ScopeSpans().At(j)
+			for k := 0; k < libspans.Spans().Len(); k++ {
+				otelspan := libspans.Spans().At(k)
+				var ddspan *pb.Span
+
+				ddspan = transform.OtelSpanToDDSpan(otelspan, rspan.Resource(), libspans.Scope(), tp.agentCfg, []string{""})
+				if tp.overrideIncomingDatadogFields {
+					otelspan.Attributes().RemoveIf(func(k string, v pcommon.Value) bool {
+						return strings.HasPrefix(k, "datadog.")
+					})
+				}
+				if _, ok := otelspan.Attributes().Get("datadog.service"); !ok {
+					otelspan.Attributes().PutStr("datadog.service", ddspan.Service)
+				}
+				if _, ok := otelspan.Attributes().Get("datadog.name"); !ok {
+					otelspan.Attributes().PutStr("datadog.name", ddspan.Name)
+				}
+				if _, ok := otelspan.Attributes().Get("datadog.resource"); !ok {
+					otelspan.Attributes().PutStr("datadog.resource", ddspan.Resource)
+				}
+				if _, ok := otelspan.Attributes().Get("datadog.trace_id"); !ok {
+					otelspan.Attributes().PutStr("datadog.trace_id", fmt.Sprintf("%d", ddspan.TraceID))
+				}
+				if _, ok := otelspan.Attributes().Get("datadog.span_id"); !ok {
+					otelspan.Attributes().PutStr("datadog.span_id", fmt.Sprintf("%d", ddspan.SpanID))
+				}
+				if _, ok := otelspan.Attributes().Get("datadog.parent_id"); !ok {
+					otelspan.Attributes().PutStr("datadog.parent_id", fmt.Sprintf("%d", ddspan.ParentID))
+				}
+				if _, ok := otelspan.Attributes().Get("datadog.type"); !ok {
+					otelspan.Attributes().PutStr("datadog.type", ddspan.Type)
+				}
+				if _, ok := otelspan.Attributes().Get("datadog.meta"); !ok {
+					metaMap := otelspan.Attributes().PutEmptyMap("datadog.meta")
+					for key, value := range ddspan.Meta {
+						metaMap.PutStr(key, value)
+					}
+				}
+				if _, ok := otelspan.Attributes().Get("datadog.metrics"); !ok {
+					metricsMap := otelspan.Attributes().PutEmptyMap("datadog.metrics")
+					for key, value := range ddspan.Metrics {
+						metricsMap.PutDouble(key, value)
+					}
+				}
+			}
+		}
+	}
+
+	return td, err // XXX can the input spans be modified? or do we need to clone them to add the attributes then output that?
+}
---
+++ processor/datadogsemanticsprocessor/processor_test.go
@@ -0,0 +1,351 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package datadogsemanticsprocessor
+
+import (
+	"context"
+	"github.com/DataDog/datadog-agent/pkg/trace/testutil"
+	semconv "go.opentelemetry.io/collector/semconv/v1.6.1"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/component/componentstatus"
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/consumer/consumertest"
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/ptrace"
+	"go.opentelemetry.io/collector/processor"
+	"go.opentelemetry.io/collector/processor/processortest"
+)
+
+func newTestTracesProcessor(cfg component.Config, next consumer.Traces) (processor.Traces, error) {
+	set := processortest.NewNopSettings()
+	return createTracesProcessor(
+		context.Background(),
+		set,
+		cfg,
+		next,
+	)
+}
+
+type multiTest struct {
+	t *testing.T
+
+	tp processor.Traces
+
+	nextTrace *consumertest.TracesSink
+
+	//ddspTrace   *datadogsemanticsprocessor
+}
+
+func newMultiTest(
+	t *testing.T,
+	cfg component.Config,
+	errFunc func(err error),
+) *multiTest {
+	m := &multiTest{
+		t:         t,
+		nextTrace: new(consumertest.TracesSink),
+	}
+
+	tp, err := newTestTracesProcessor(cfg, m.nextTrace)
+	require.NoError(t, err)
+	err = tp.Start(context.Background(), &nopHost{
+		reportFunc: func(event *componentstatus.Event) {
+			errFunc(event.Err())
+		},
+	})
+	if errFunc == nil {
+		assert.NotNil(t, tp)
+		require.NoError(t, err)
+	}
+
+	m.tp = tp
+	return m
+}
+
+func (m *multiTest) testConsume(
+	ctx context.Context,
+	traces ptrace.Traces,
+	errFunc func(err error),
+) {
+	errs := []error{
+		m.tp.ConsumeTraces(ctx, traces),
+	}
+
+	for _, err := range errs {
+		if errFunc != nil {
+			errFunc(err)
+		}
+	}
+}
+
+func (m *multiTest) assertBatchesLen(batchesLen int) {
+	require.Len(m.t, m.nextTrace.AllTraces(), batchesLen)
+}
+
+func (m *multiTest) assertResourceObjectLen(batchNo int) {
+	assert.Equal(m.t, 1, m.nextTrace.AllTraces()[batchNo].ResourceSpans().Len())
+}
+
+func (m *multiTest) assertResourceAttributesLen(batchNo int, attrsLen int) {
+	assert.Equal(m.t, attrsLen, m.nextTrace.AllTraces()[batchNo].ResourceSpans().At(0).Resource().Attributes().Len())
+}
+
+func (m *multiTest) assertResource(batchNum int, resourceFunc func(res pcommon.Resource)) {
+	rss := m.nextTrace.AllTraces()[batchNum].ResourceSpans()
+	r := rss.At(0).Resource()
+
+	if resourceFunc != nil {
+		resourceFunc(r)
+	}
+}
+
+func TestNewProcessor(t *testing.T) {
+	cfg := NewFactory().CreateDefaultConfig()
+
+	newMultiTest(t, cfg, nil)
+}
+
+type generateResourceFunc func(res pcommon.Resource)
+
+func TestNilBatch(t *testing.T) {
+	m := newMultiTest(t, NewFactory().CreateDefaultConfig(), nil)
+	m.testConsume(
+		context.Background(),
+		ptrace.NewTraces(),
+		func(err error) {
+			assert.NoError(t, err)
+		})
+
+	m.assertBatchesLen(1)
+}
+
+func TestBasicTranslation(t *testing.T) {
+	tests := []struct {
+		name                          string
+		overrideIncomingDatadogFields bool
+		in                            []testutil.OTLPResourceSpan
+		fn                            func(*ptrace.Traces)
+	}{
+		{
+			name:                          "complete test",
+			overrideIncomingDatadogFields: false,
+			in: []testutil.OTLPResourceSpan{
+				{
+					LibName:    "libname",
+					LibVersion: "1.2",
+					Attributes: map[string]interface{}{
+						"service.name":                "test-service",
+						"resource.name":               "test-resource",
+						"deployment.environment.name": "spanenv2",
+					},
+					Spans: []*testutil.OTLPSpan{
+						{
+							TraceID:  [16]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
+							SpanID:   [8]byte{0, 1, 2, 3, 4, 5, 6, 7},
+							ParentID: [8]byte{0, 0, 0, 0, 0, 0, 0, 1},
+							Kind:     ptrace.SpanKindServer,
+							Attributes: map[string]interface{}{
+								"operation.name":                "test-operation",
+								semconv.AttributeHTTPStatusCode: 200,
+							},
+						},
+					},
+				},
+			},
+			fn: func(out *ptrace.Traces) {
+				rs := out.ResourceSpans().At(0)
+				span := rs.ScopeSpans().At(0).Spans().At(0)
+				ddservice, _ := span.Attributes().Get("datadog.service")
+				require.Equal(t, "test-service", ddservice.AsString())
+				ddname, _ := span.Attributes().Get("datadog.name")
+				require.Equal(t, "test-operation", ddname.AsString())
+				ddresource, _ := span.Attributes().Get("datadog.resource")
+				require.Equal(t, "test-resource", ddresource.AsString())
+				ddTraceID, _ := span.Attributes().Get("datadog.trace_id")
+				require.Equal(t, "579005069656919567", ddTraceID.AsString())
+				ddSpanID, _ := span.Attributes().Get("datadog.span_id")
+				require.Equal(t, "283686952306183", ddSpanID.AsString())
+				ddParentID, _ := span.Attributes().Get("datadog.parent_id")
+				require.Equal(t, "1", ddParentID.AsString())
+				ddType, _ := span.Attributes().Get("datadog.type")
+				require.Equal(t, "web", ddType.AsString())
+				meta, _ := span.Attributes().Get("datadog.meta")
+				env, _ := meta.Map().Get("env")
+				require.Equal(t, "spanenv2", env.AsString())
+				metrics, _ := span.Attributes().Get("datadog.metrics")
+				statusCode, _ := metrics.Map().Get(semconv.AttributeHTTPStatusCode)
+				require.Equal(t, 200.0, statusCode.Double())
+			},
+		},
+		{
+			name:                          "overrideIncomingDatadogFields",
+			overrideIncomingDatadogFields: true,
+			in: []testutil.OTLPResourceSpan{
+				{
+					LibName:    "libname",
+					LibVersion: "1.2",
+					Attributes: map[string]interface{}{
+						"service.name":                "test-service",
+						"resource.name":               "test-resource",
+						"deployment.environment.name": "spanenv2",
+					},
+					Spans: []*testutil.OTLPSpan{
+						{
+							TraceID:  [16]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
+							SpanID:   [8]byte{0, 1, 2, 3, 4, 5, 6, 7},
+							ParentID: [8]byte{0, 0, 0, 0, 0, 0, 0, 1},
+							Kind:     ptrace.SpanKindServer,
+							Attributes: map[string]interface{}{
+								"datadog.service":               "specified-service",
+								"datadog.resource":              "specified-resource",
+								"datadog.name":                  "specified-operation",
+								"datadog.trace_id":              "123456789",
+								"datadog.span_id":               "987654321",
+								"datadog.parent_id":             "123456789",
+								"datadog.type":                  "specified-type",
+								"datadog.meta":                  map[string]interface{}{"env": "specified-env"},
+								"datadog.metrics":               map[string]interface{}{semconv.AttributeHTTPStatusCode: 500},
+								"operation.name":                "test-operation",
+								semconv.AttributeHTTPStatusCode: 200,
+							},
+						},
+					},
+				},
+			},
+			fn: func(out *ptrace.Traces) {
+				rs := out.ResourceSpans().At(0)
+				span := rs.ScopeSpans().At(0).Spans().At(0)
+				ddservice, _ := span.Attributes().Get("datadog.service")
+				require.Equal(t, "test-service", ddservice.AsString())
+				ddname, _ := span.Attributes().Get("datadog.name")
+				require.Equal(t, "test-operation", ddname.AsString())
+				ddresource, _ := span.Attributes().Get("datadog.resource")
+				require.Equal(t, "test-resource", ddresource.AsString())
+				ddTraceID, _ := span.Attributes().Get("datadog.trace_id")
+				require.Equal(t, "579005069656919567", ddTraceID.AsString())
+				ddSpanID, _ := span.Attributes().Get("datadog.span_id")
+				require.Equal(t, "283686952306183", ddSpanID.AsString())
+				ddParentID, _ := span.Attributes().Get("datadog.parent_id")
+				require.Equal(t, "1", ddParentID.AsString())
+				ddType, _ := span.Attributes().Get("datadog.type")
+				require.Equal(t, "web", ddType.AsString())
+				meta, _ := span.Attributes().Get("datadog.meta")
+				env, _ := meta.Map().Get("env")
+				require.Equal(t, "spanenv2", env.AsString())
+				metrics, _ := span.Attributes().Get("datadog.metrics")
+				statusCode, _ := metrics.Map().Get(semconv.AttributeHTTPStatusCode)
+				require.Equal(t, 200.0, statusCode.Double())
+			},
+		},
+		{
+			name:                          "don't overrideIncomingDatadogFields",
+			overrideIncomingDatadogFields: false,
+			in: []testutil.OTLPResourceSpan{
+				{
+					LibName:    "libname",
+					LibVersion: "1.2",
+					Attributes: map[string]interface{}{
+						"service.name":                "test-service",
+						"resource.name":               "test-resource",
+						"deployment.environment.name": "spanenv2",
+					},
+					Spans: []*testutil.OTLPSpan{
+						{
+							TraceID:  [16]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
+							SpanID:   [8]byte{0, 1, 2, 3, 4, 5, 6, 7},
+							ParentID: [8]byte{0, 0, 0, 0, 0, 0, 0, 1},
+							Kind:     ptrace.SpanKindServer,
+							Attributes: map[string]interface{}{
+								"datadog.service":   "specified-service",
+								"datadog.resource":  "specified-resource",
+								"datadog.name":      "specified-operation",
+								"datadog.trace_id":  "123456789",
+								"datadog.span_id":   "987654321",
+								"datadog.parent_id": "123456789",
+								"datadog.type":      "specified-type",
+								// TODO: Update testutil.NewOTLPTracesRequest to support map[string]interface{},
+								//  then update this test to override meta and metrics.
+								//"datadog.meta":                  map[string]interface{}{"env": "specified-env"},
+								//"datadog.metrics":               map[string]interface{}{semconv.AttributeHTTPStatusCode: 500},
+								"operation.name":                "test-operation",
+								semconv.AttributeHTTPStatusCode: 200,
+							},
+						},
+					},
+				},
+			},
+			fn: func(out *ptrace.Traces) {
+				rs := out.ResourceSpans().At(0)
+				span := rs.ScopeSpans().At(0).Spans().At(0)
+				ddservice, _ := span.Attributes().Get("datadog.service")
+				require.Equal(t, "specified-service", ddservice.AsString())
+				ddname, _ := span.Attributes().Get("datadog.name")
+				require.Equal(t, "specified-operation", ddname.AsString())
+				ddresource, _ := span.Attributes().Get("datadog.resource")
+				require.Equal(t, "specified-resource", ddresource.AsString())
+				ddTraceID, _ := span.Attributes().Get("datadog.trace_id")
+				require.Equal(t, "123456789", ddTraceID.AsString())
+				ddSpanID, _ := span.Attributes().Get("datadog.span_id")
+				require.Equal(t, "987654321", ddSpanID.AsString())
+				ddParentID, _ := span.Attributes().Get("datadog.parent_id")
+				require.Equal(t, "123456789", ddParentID.AsString())
+				ddType, _ := span.Attributes().Get("datadog.type")
+				require.Equal(t, "specified-type", ddType.AsString())
+				// TODO: Update testutil.NewOTLPTracesRequest to support map[string]interface{},
+				//  then update this test to check the values of meta and metrics.
+				//meta, _ := span.Attributes().Get("datadog.meta")
+				//env, _ := meta.Map().Get("env")
+				//require.Equal(t, "specified-env", env.AsString())
+				//metrics, _ := span.Attributes().Get("datadog.metrics")
+				//statusCode, _ := metrics.Map().Get(semconv.AttributeHTTPStatusCode)
+				//require.Equal(t, 500.0, statusCode.Double())
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		m := newMultiTest(
+			t,
+			&Config{
+				OverrideIncomingDatadogFields: tt.overrideIncomingDatadogFields,
+			},
+			nil,
+		)
+
+		traces := testutil.NewOTLPTracesRequest(tt.in)
+
+		m.testConsume(context.Background(),
+			traces.Traces(),
+			nil,
+		)
+
+		tt.fn(&m.nextTrace.AllTraces()[0])
+	}
+}
+
+func TestCapabilities(t *testing.T) {
+	p, err := newTestTracesProcessor(
+		NewFactory().CreateDefaultConfig(),
+		consumertest.NewNop(),
+	)
+	assert.NoError(t, err)
+	caps := p.Capabilities()
+	assert.True(t, caps.MutatesData)
+}
+
+type nopHost struct {
+	reportFunc func(event *componentstatus.Event)
+}
+
+func (nh *nopHost) GetExtensions() map[component.ID]component.Component {
+	return nil
+}
+
+func (nh *nopHost) Report(event *componentstatus.Event) {
+	nh.reportFunc(event)
+}
