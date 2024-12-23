bba41e447b128a62dfaae0a7be0fb594b892500f
Michal Grandys <grandys.michal@gmail.com>
2024-12-23 18:53:59 +0100

    Fixed lint errors, restored skipping integration tests on windows

--- receiver/sqlqueryreceiver/testdata/integration/sqlserver/configure-db.sh
+++ receiver/sqlqueryreceiver/testdata/integration/sqlserver/configure-db.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#
+# Copyright The OpenTelemetry Authors
+# SPDX-License-Identifier: Apache-2.0
+#
+#
+
 
 # Run the SQL script to initialize the database
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P YourStrong!Passw0rd -d master -i /usr/src/app/init.sql
