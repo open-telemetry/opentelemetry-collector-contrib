4c10adcc1a80ea659efa52556cfce0be8e332d5e
odubajDT <ondrej.dubaj@dynatrace.com>
2025-01-02 11:54:40 +0100

    [pkg/ottl] enhance flatten converter to resolve attribute key conflicts
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- pkg/ottl/ottlfuncs/func_flatten_test.go
+++ pkg/ottl/ottlfuncs/func_flatten_test.go
@@ -20,6 +20,7 @@ func Test_flatten(t *testing.T) {
 		prefix   ottl.Optional[string]
 		depth    ottl.Optional[int64]
 		expected map[string]any
+		conflict bool
 	}{
 		{
 			name: "simple",
@@ -30,6 +31,91 @@ func Test_flatten(t *testing.T) {
 			depth:  ottl.Optional[int64]{},
 			expected: map[string]any{
 				"name": "test",
+			},
+		},
+		{
+			name: "conflicting map",
+			target: map[string]any{
+				"address": map[string]any{
+					"street": map[string]any{
+						"house": int64(1234),
+					},
+				},
+				"address.street": map[string]any{
+					"house": int64(1235),
+				},
+			},
+			prefix: ottl.Optional[string]{},
+			depth:  ottl.Optional[int64]{},
+			expected: map[string]any{
+				"address.street.house": int64(1235),
+			},
+		},
+		{
+			name: "conflicting slice",
+			target: map[string]any{
+				"address": map[string]any{
+					"street": []any{"first"},
+					"house":  int64(1234),
+				},
+				"address.street": []any{"second"},
+			},
+			prefix: ottl.Optional[string]{},
+			depth:  ottl.Optional[int64]{},
+			expected: map[string]any{
+				"address.house":    int64(1234),
+				"address.street.0": "second",
+			},
+		},
+		{
+			name: "conflicting map with nested slice",
+			target: map[string]any{
+				"address": map[string]any{
+					"street": "first",
+					"house":  int64(1234),
+				},
+				"address.street": "second",
+				"occupants": []any{
+					"user 1",
+					"user 2",
+				},
+			},
+			prefix: ottl.Optional[string]{},
+			depth:  ottl.Optional[int64]{},
+			expected: map[string]any{
+				"address.street": "second",
+				"address.house":  int64(1234),
+				"occupants.0":    "user 1",
+				"occupants.1":    "user 2",
+			},
+		},
+		{
+			name: "conflicting map with nested slice in conflicting item",
+			target: map[string]any{
+				"address": map[string]any{
+					"street": map[string]any{
+						"number": "first",
+					},
+					"house": int64(1234),
+				},
+				"address.street": map[string]any{
+					"number": []any{"second", "third"},
+				},
+				"address.street.number": "fourth",
+				"occupants": []any{
+					"user 1",
+					"user 2",
+				},
+			},
+			prefix: ottl.Optional[string]{},
+			depth:  ottl.Optional[int64]{},
+			expected: map[string]any{
+				"address.street.number":   "fourth",
+				"address.house":           int64(1234),
+				"address.street.number.0": "second",
+				"address.street.number.1": "third",
+				"occupants.0":             "user 1",
+				"occupants.1":             "user 2",
 			},
 		},
 		{
@@ -61,6 +147,229 @@ func Test_flatten(t *testing.T) {
 				"occupants.0": "user 1",
 				"occupants.1": "user 2",
 			},
+		},
+		{
+			name: "simple - conflict on",
+			target: map[string]any{
+				"name": "test",
+			},
+			prefix: ottl.Optional[string]{},
+			depth:  ottl.Optional[int64]{},
+			expected: map[string]any{
+				"name": "test",
+			},
+			conflict: true,
+		},
+		{
+			name: "nested map - conflict on",
+			target: map[string]any{
+				"address": map[string]any{
+					"street": "first",
+					"house":  int64(1234),
+				},
+			},
+			prefix: ottl.Optional[string]{},
+			depth:  ottl.Optional[int64]{},
+			expected: map[string]any{
+				"address.street": "first",
+				"address.house":  int64(1234),
+			},
+			conflict: true,
+		},
+		{
+			name: "conflicting map - conflict on",
+			target: map[string]any{
+				"address": map[string]any{
+					"street": map[string]any{
+						"house": int64(1234),
+					},
+				},
+				"address.street": map[string]any{
+					"house": int64(1235),
+				},
+			},
+			prefix: ottl.Optional[string]{},
+			depth:  ottl.Optional[int64]{},
+			expected: map[string]any{
+				"address.street.house":   int64(1234),
+				"address.street.house.0": int64(1235),
+			},
+			conflict: true,
+		},
+		{
+			name: "conflicting slice - conflict on",
+			target: map[string]any{
+				"address": map[string]any{
+					"street": []any{"first"},
+					"house":  int64(1234),
+				},
+				"address.street": []any{"second"},
+			},
+			prefix: ottl.Optional[string]{},
+			depth:  ottl.Optional[int64]{},
+			expected: map[string]any{
+				"address.street":   "first",
+				"address.house":    int64(1234),
+				"address.street.0": "second",
+			},
+			conflict: true,
+		},
+		{
+			name: "conflicting map with nested slice - conflict on",
+			target: map[string]any{
+				"address": map[string]any{
+					"street": "first",
+					"house":  int64(1234),
+				},
+				"address.street": "second",
+				"occupants": []any{
+					"user 1",
+					"user 2",
+				},
+			},
+			prefix: ottl.Optional[string]{},
+			depth:  ottl.Optional[int64]{},
+			expected: map[string]any{
+				"address.street":   "first",
+				"address.house":    int64(1234),
+				"address.street.0": "second",
+				"occupants":        "user 1",
+				"occupants.0":      "user 2",
+			},
+			conflict: true,
+		},
+		{
+			name: "conflicting map with nested slice in conflicting item - conflict on",
+			target: map[string]any{
+				"address": map[string]any{
+					"street": map[string]any{
+						"number": "first",
+					},
+					"house": int64(1234),
+				},
+				"address.street": map[string]any{
+					"number": []any{"second", "third"},
+				},
+				"address.street.number": "fourth",
+				"occupants": []any{
+					"user 1",
+					"user 2",
+				},
+			},
+			prefix: ottl.Optional[string]{},
+			depth:  ottl.Optional[int64]{},
+			expected: map[string]any{
+				"address.street.number":   "first",
+				"address.house":           int64(1234),
+				"address.street.number.0": "second",
+				"address.street.number.1": "third",
+				"occupants":               "user 1",
+				"occupants.0":             "user 2",
+				"address.street.number.2": "fourth",
+			},
+			conflict: true,
+		},
+		{
+			name: "nested slice - conflict on",
+			target: map[string]any{
+				"occupants": []any{
+					"user 1",
+					"user 2",
+				},
+			},
+			prefix: ottl.Optional[string]{},
+			depth:  ottl.Optional[int64]{},
+			expected: map[string]any{
+				"occupants":   "user 1",
+				"occupants.0": "user 2",
+			},
+			conflict: true,
+		},
+		{
+			name: "combination - conflict on",
+			target: map[string]any{
+				"name": "test",
+				"address": map[string]any{
+					"street": "first",
+					"house":  int64(1234),
+				},
+				"occupants": []any{
+					"user 1",
+					"user 2",
+				},
+			},
+			prefix: ottl.Optional[string]{},
+			depth:  ottl.Optional[int64]{},
+			expected: map[string]any{
+				"name":           "test",
+				"address.street": "first",
+				"address.house":  int64(1234),
+				"occupants":      "user 1",
+				"occupants.0":    "user 2",
+			},
+			conflict: true,
+		},
+		{
+			name: "deep nesting - conflict on",
+			target: map[string]any{
+				"1": map[string]any{
+					"2": map[string]any{
+						"3": map[string]any{
+							"4": "5",
+						},
+					},
+				},
+			},
+			prefix: ottl.Optional[string]{},
+			depth:  ottl.Optional[int64]{},
+			expected: map[string]any{
+				"1.2.3.4": "5",
+			},
+			conflict: true,
+		},
+		{
+			name: "use prefix - conflict on",
+			target: map[string]any{
+				"name": "test",
+				"address": map[string]any{
+					"street": "first",
+					"house":  int64(1234),
+				},
+				"occupants": []any{
+					"user 1",
+					"user 2",
+				},
+			},
+			prefix: ottl.NewTestingOptional[string]("app"),
+			depth:  ottl.Optional[int64]{},
+			expected: map[string]any{
+				"app.name":           "test",
+				"app.address.street": "first",
+				"app.address.house":  int64(1234),
+				"app.occupants":      "user 1",
+				"app.occupants.0":    "user 2",
+			},
+			conflict: true,
+		},
+		{
+			name: "max depth - conflict on",
+			target: map[string]any{
+				"0": map[string]any{
+					"1": map[string]any{
+						"2": map[string]any{
+							"3": "value",
+						},
+					},
+				},
+			},
+			prefix: ottl.Optional[string]{},
+			depth:  ottl.NewTestingOptional[int64](2),
+			expected: map[string]any{
+				"0.1.2": map[string]any{
+					"3": "value",
+				},
+			},
+			conflict: true,
 		},
 		{
 			name: "combination",
@@ -189,7 +498,7 @@ func Test_flatten(t *testing.T) {
 				},
 			}
 
-			exprFunc, err := flatten[any](target, tt.prefix, tt.depth)
+			exprFunc, err := flatten[any](target, tt.prefix, tt.depth, ottl.NewTestingOptional[bool](tt.conflict))
 			assert.NoError(t, err)
 			_, err = exprFunc(nil, nil)
 			assert.NoError(t, err)
@@ -205,7 +514,7 @@ func Test_flatten_bad_target(t *testing.T) {
 			return 1, nil
 		},
 	}
-	exprFunc, err := flatten[any](target, ottl.Optional[string]{}, ottl.Optional[int64]{})
+	exprFunc, err := flatten[any](target, ottl.Optional[string]{}, ottl.Optional[int64]{}, ottl.NewTestingOptional[bool](false))
 	assert.NoError(t, err)
 	_, err = exprFunc(nil, nil)
 	assert.Error(t, err)
@@ -233,7 +542,7 @@ func Test_flatten_bad_depth(t *testing.T) {
 					return pcommon.NewMap(), nil
 				},
 			}
-			_, err := flatten[any](target, ottl.Optional[string]{}, tt.depth)
+			_, err := flatten[any](target, ottl.Optional[string]{}, tt.depth, ottl.NewTestingOptional[bool](false))
 			assert.Error(t, err)
 		})
 	}
