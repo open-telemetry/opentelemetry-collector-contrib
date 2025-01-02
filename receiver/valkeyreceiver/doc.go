66856f7f20fe2cb5686ee099c94f5bd0e275f288
Roger Coll <rogercoll@protonmail.com>
2025-01-02 11:13:17 +0100

    fix: header licenses

--- receiver/valkeyreceiver/doc.go
+++ receiver/valkeyreceiver/doc.go
@@ -1,6 +1,6 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-//go:generate ../../.tools/mdatagen metadata.yaml
+//go:generate mdatagen metadata.yaml
 
 package valkeyreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/valkeyreceiver"
