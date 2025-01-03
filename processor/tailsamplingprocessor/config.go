7744a21b7fe8b6cb124b1a52a927fa52d63b3e29
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-01-03 09:46:13 -0500

    spelling: greater
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- processor/tailsamplingprocessor/config.go
+++ processor/tailsamplingprocessor/config.go
@@ -227,12 +227,12 @@ type OTTLConditionCfg struct {
 type DecisionCacheConfig struct {
 	// SampledCacheSize specifies the size of the cache that holds the sampled trace IDs.
 	// This value will be the maximum amount of trace IDs that the cache can hold before overwriting previous IDs.
-	// For effective use, this value should be at least an order of magnitude higher than Config.NumTraces.
+	// For effective use, this value should be at least an order of magnitude greater than Config.NumTraces.
 	// If left as default 0, a no-op DecisionCache will be used.
 	SampledCacheSize int `mapstructure:"sampled_cache_size"`
 	// NonSampledCacheSize specifies the size of the cache that holds the non-sampled trace IDs.
 	// This value will be the maximum amount of trace IDs that the cache can hold before overwriting previous IDs.
-	// For effective use, this value should be at least an order of magnitude higher than Config.NumTraces.
+	// For effective use, this value should be at least an order of magnitude greater than Config.NumTraces.
 	// If left as default 0, a no-op DecisionCache will be used.
 	NonSampledCacheSize int `mapstructure:"non_sampled_cache_size"`
 }
