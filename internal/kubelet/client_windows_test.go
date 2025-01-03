826f5c7fa8a6bd66c6e04f2724ab15d5e7059b22
Rick Rossi <dricross@amazon.com>
2025-01-03 16:57:38 -0500

    Fixup internal/kubelet

--- internal/kubelet/client_windows_test.go
+++ internal/kubelet/client_windows_test.go
@@ -9,28 +9,28 @@ package kubelet
 import (
 	"testing"
 
-	"github.com/open-telemetry/opentelemetry-collector-contrib/internal/aws/containerinsight"
-
 	"github.com/stretchr/testify/assert"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/internal/aws/containerinsight"
 )
 
 func TestSAPathInHostProcessContainer(t *testing.T) {
 	// todo: Remove this workaround func when Windows AMIs has containerd 1.7 which solves upstream bug.
 
 	// Test default SA cert and token.
-	assert.Equal(t, svcAcctCACertPath, "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt")
-	assert.Equal(t, svcAcctTokenPath, "/var/run/secrets/kubernetes.io/serviceaccount/token")
+	assert.Equal(t, "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt", svcAcctCACertPath)
+	assert.Equal(t, "/var/run/secrets/kubernetes.io/serviceaccount/token", svcAcctTokenPath)
 
 	// Test SA cert and token when run inside container.
 	t.Setenv(containerinsight.RunInContainer, "True")
 	updateSVCPath()
-	assert.Equal(t, svcAcctCACertPath, "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt")
-	assert.Equal(t, svcAcctTokenPath, "/var/run/secrets/kubernetes.io/serviceaccount/token")
+	assert.Equal(t, "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt", svcAcctCACertPath)
+	assert.Equal(t, "/var/run/secrets/kubernetes.io/serviceaccount/token", svcAcctTokenPath)
 
 	// Test SA cert and token when run inside host process container.
 	t.Setenv(containerinsight.RunAsHostProcessContainer, "True")
 	t.Setenv("CONTAINER_SANDBOX_MOUNT_POINT", "test123456")
 	updateSVCPath()
-	assert.Equal(t, svcAcctCACertPath, "test123456/var/run/secrets/kubernetes.io/serviceaccount/ca.crt")
-	assert.Equal(t, svcAcctTokenPath, "test123456/var/run/secrets/kubernetes.io/serviceaccount/token")
+	assert.Equal(t, "test123456/var/run/secrets/kubernetes.io/serviceaccount/ca.crt", svcAcctCACertPath)
+	assert.Equal(t, "test123456/var/run/secrets/kubernetes.io/serviceaccount/token", svcAcctTokenPath)
 }
--- internal/kubelet/go.mod
+++ internal/kubelet/go.mod
@@ -48,7 +48,8 @@ require (
 	golang.org/x/sys v0.24.0 // indirect
 	golang.org/x/term v0.23.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
-	golang.org/x/time v0.4.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240814211410-ddb44dafa142 // indirect
 	google.golang.org/grpc v1.67.1 // indirect
 	google.golang.org/protobuf v1.35.2 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
