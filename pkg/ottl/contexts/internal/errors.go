265c4e42aff1407ac7d62419f1479fe3b711838c
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-01-03 09:53:58 -0500

    spelling: neither-nor
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- pkg/ottl/contexts/internal/errors.go
+++ pkg/ottl/contexts/internal/errors.go
@@ -6,7 +6,7 @@ package internal // import "github.com/open-telemetry/opentelemetry-collector-co
 import "fmt"
 
 const (
-	DefaultErrorMessage = "segment %q from path %q is not a valid path nor a valid OTTL keyword for the %v context - review %v to see all valid paths"
+	DefaultErrorMessage = "segment %q from path %q is neither a valid path nor a valid OTTL keyword for the %v context - review %v to see all valid paths"
 
 	ResourceContextRef      = "https://github.com/open-telemetry/opentelemetry-collector-contrib/tree/main/pkg/ottl/contexts/ottlresource"
 	InstrumentationScopeRef = "https://github.com/open-telemetry/opentelemetry-collector-contrib/tree/main/pkg/ottl/contexts/ottlscope"
