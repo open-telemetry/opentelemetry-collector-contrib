405b4b81d4e6e26e91238c9a7a3b5327bba5c408
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-01-07 17:58:59 -0500

    spelling: sarama
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- internal/kafka/awsmsk/doc.go
+++ internal/kafka/awsmsk/doc.go
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 // Package msk implements the required IAM auth used by AWS' managed Kafka platform
-// to be used with the Surama kafka producer.
+// to be used with the Sarama kafka producer.
 //
 // Further details on how the SASL connector works can be viewed here:
 //
