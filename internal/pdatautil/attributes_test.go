78ffc65dc12ad3ebef010309e5dc382a8c94dcb8
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-01-07 09:59:43 -0500

    spelling: resourceattribs
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- internal/pdatautil/attributes_test.go
+++ internal/pdatautil/attributes_test.go
@@ -11,8 +11,8 @@ import (
 )
 
 func TestGetDimensionValue(t *testing.T) {
-	resourceattris := pcommon.NewMap()
-	resourceattris.PutStr("service.name", "mock-service-name")
+	resourceattribs := pcommon.NewMap()
+	resourceattribs.PutStr("service.name", "mock-service-name")
 
 	spanattris := pcommon.NewMap()
 	spanattris.PutStr("span.name", "mock-span-name")
@@ -32,7 +32,7 @@ func TestGetDimensionValue(t *testing.T) {
 		{
 			name:             "success get dimension value",
 			dimension:        Dimension{Name: "foo"},
-			attributes:       []pcommon.Map{resourceattris, spanattris, otherattribs},
+			attributes:       []pcommon.Map{resourceattribs, spanattris, otherattribs},
 			wantDimensionVal: "bar",
 		},
 		{
@@ -41,7 +41,7 @@ func TestGetDimensionValue(t *testing.T) {
 				Name:  "foo",
 				Value: &defaultFoo,
 			},
-			attributes:       []pcommon.Map{resourceattris, spanattris},
+			attributes:       []pcommon.Map{resourceattribs, spanattris},
 			wantDimensionVal: "bar",
 		},
 		{
@@ -49,7 +49,7 @@ func TestGetDimensionValue(t *testing.T) {
 			dimension: Dimension{
 				Name: "foo",
 			},
-			attributes:       []pcommon.Map{resourceattris, spanattris},
+			attributes:       []pcommon.Map{resourceattribs, spanattris},
 			wantDimensionVal: "",
 		},
 	}
