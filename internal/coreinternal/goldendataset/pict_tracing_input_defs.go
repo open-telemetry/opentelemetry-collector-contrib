a6b3b8d4170b306330d0a2b148db1fd43a2fb1fa
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-01-07 19:52:28 -0500

    spelling: populate
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- internal/coreinternal/goldendataset/pict_tracing_input_defs.go
+++ internal/coreinternal/goldendataset/pict_tracing_input_defs.go
@@ -48,7 +48,7 @@ const (
 type PICTTracingInputs struct {
 	// Specifies the category of attributes to populate the Resource field with
 	Resource PICTInputResource
-	// Specifies the number and library categories to populte the ScopeSpans field with
+	// Specifies the number and library categories to populate the ScopeSpans field with
 	InstrumentationLibrary PICTInputInstrumentationLibrary
 	// Specifies the relative number of spans to populate the ScopeSpans' Spans field with
 	Spans PICTInputSpans
