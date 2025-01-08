617b0bbdcda99ab4b2b12539f678ee41cab0bb09
Sean Porter <portertech@gmail.com>
2025-01-08 03:08:20 -0800

    [processor/tailsampling] Fixed sampling policy evaluation debug logging batch metrics (#37040)
    #### Description
    
    Currently, the processor always logs (at debug) `sampled=0` and
    `notSampled=0` for every batch processed. This pull-request fixes those
    metrics.
    
    ---------
    
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- processor/tailsamplingprocessor/processor.go
+++ processor/tailsamplingprocessor/processor.go
@@ -404,6 +404,13 @@ func (tsp *tailSamplingSpanProcessor) makeDecision(id pcommon.TraceID, trace *sa
 		finalDecision = sampling.Sampled
 	}
 
+	switch finalDecision {
+	case sampling.Sampled:
+		metrics.decisionSampled++
+	case sampling.NotSampled:
+		metrics.decisionNotSampled++
+	}
+
 	return finalDecision
 }
 
