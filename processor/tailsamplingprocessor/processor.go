49b729d864db2a21edb97f144b4758f599a01215
Sean Porter <portertech@gmail.com>
2025-01-08 03:09:52 -0800

    [processor/tailsampling] Added debug logging to sampling decision caches (#37038)
    #### Description
    
    This pull-request adds debug logging to the sampling decision caches,
    making it a little easier to debug decisions.
    
    "Trace ID is in the sampled cache" and "Trace ID is in the non-sampled
    cache" with the trace ID as a zap field.
    
    ---------
    
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- processor/tailsamplingprocessor/processor.go
+++ processor/tailsamplingprocessor/processor.go
@@ -450,6 +450,7 @@ func (tsp *tailSamplingSpanProcessor) processTraces(resourceSpans ptrace.Resourc
 	for id, spans := range idToSpansAndScope {
 		// If the trace ID is in the sampled cache, short circuit the decision
 		if _, ok := tsp.sampledIDCache.Get(id); ok {
+			tsp.logger.Debug("Trace ID is in the sampled cache", zap.Stringer("id", id))
 			traceTd := ptrace.NewTraces()
 			appendToTraces(traceTd, resourceSpans, spans)
 			tsp.releaseSampledTrace(tsp.ctx, id, traceTd)
@@ -460,6 +461,7 @@ func (tsp *tailSamplingSpanProcessor) processTraces(resourceSpans ptrace.Resourc
 		}
 		// If the trace ID is in the non-sampled cache, short circuit the decision
 		if _, ok := tsp.nonSampledIDCache.Get(id); ok {
+			tsp.logger.Debug("Trace ID is in the non-sampled cache", zap.Stringer("id", id))
 			tsp.telemetry.ProcessorTailSamplingEarlyReleasesFromCacheDecision.
 				Add(tsp.ctx, int64(len(spans)), attrSampledFalse)
 			continue
@@ -553,7 +555,7 @@ func (tsp *tailSamplingSpanProcessor) dropTrace(traceID pcommon.TraceID, deletio
 		tsp.numTracesOnMap.Add(^uint64(0))
 	}
 	if trace == nil {
-		tsp.logger.Debug("Attempt to delete traceID not on table")
+		tsp.logger.Debug("Attempt to delete trace ID not on table", zap.Stringer("id", traceID))
 		return
 	}
 
