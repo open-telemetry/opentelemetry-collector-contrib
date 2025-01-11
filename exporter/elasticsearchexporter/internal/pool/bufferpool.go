a99c3fc1fc4a18985606968c819137cee4528dbf
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2025-01-11 11:44:43 +0100

    make goporto

--- exporter/elasticsearchexporter/internal/pool/bufferpool.go
+++ exporter/elasticsearchexporter/internal/pool/bufferpool.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package pool // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/elasticsearchexporter"
+package pool // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/elasticsearchexporter/internal/pool"
 
 import (
 	"bytes"
