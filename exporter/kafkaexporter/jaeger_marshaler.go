45c15b8e267759c5bab893d07e3c78fd97b88094
Shivanshu Raj Shrivastava <shivanshu1333@gmail.com>
2025-01-15 04:59:43 +0530

    feat: implement size based chunking for jaeger marshaler
    Signed-off-by: Shivanshu Raj Shrivastava <shivanshu1333@gmail.com>

--- exporter/kafkaexporter/jaeger_marshaler.go
+++ exporter/kafkaexporter/jaeger_marshaler.go
@@ -5,6 +5,7 @@ package kafkaexporter // import "github.com/open-telemetry/opentelemetry-collect
 
 import (
 	"bytes"
+	"fmt"
 
 	"github.com/IBM/sarama"
 	"github.com/gogo/protobuf/jsonpb"
@@ -24,32 +25,69 @@ type jaegerMarshaler struct {
 var _ TracesMarshaler = (*jaegerMarshaler)(nil)
 
 func (j jaegerMarshaler) Marshal(traces ptrace.Traces, topic string) ([]*ProducerMessageChunks, error) {
-	// ToDo: implement partitionedByTraceID
+	if j.maxMessageBytes <= 0 {
+		return nil, fmt.Errorf("maxMessageBytes must be positive, got %d", j.maxMessageBytes)
+	}
 
 	batches := jaeger.ProtoFromTraces(traces)
+	if len(batches) == 0 {
+		return []*ProducerMessageChunks{}, nil
+	}
+
 	var messages []*sarama.ProducerMessage
-
-	// ToDo: effectively chunk the spans adhering to j.maxMessageBytes
 	var messageChunks []*ProducerMessageChunks
-
+	var packetSize int
 	var errs error
+
 	for _, batch := range batches {
 		for _, span := range batch.Spans {
 			span.Process = batch.Process
+
 			bts, err := j.marshaler.marshal(span)
 			if err != nil {
-				errs = multierr.Append(errs, err)
+				errs = multierr.Append(errs, fmt.Errorf("failed to marshal span %s: %w", span.SpanID.String(), err))
 				continue
 			}
+
 			key := []byte(span.TraceID.String())
-			messages = append(messages, &sarama.ProducerMessage{
+			msg := &sarama.ProducerMessage{
 				Topic: topic,
 				Value: sarama.ByteEncoder(bts),
 				Key:   sarama.ByteEncoder(key),
-			})
+			}
+
+			// Deriving version from sarama library
+			// https://github.com/IBM/sarama/blob/main/async_producer.go#L454
+			currentMsgSize := msg.ByteSize(2)
+
+			// Check if message itself exceeds size limit
+			if currentMsgSize > j.maxMessageBytes {
+				errs = multierr.Append(errs, fmt.Errorf("span %s exceeds maximum message size: %d > %d",
+					span.SpanID.String(), currentMsgSize, j.maxMessageBytes))
+				continue
+			}
+
+			// Check if adding this message would exceed the chunk size limit
+			if (packetSize + currentMsgSize) <= j.maxMessageBytes {
+				packetSize += currentMsgSize
+				messages = append(messages, msg)
+			} else {
+				// Current chunk is full, create new chunk
+				if len(messages) > 0 {
+					messageChunks = append(messageChunks, &ProducerMessageChunks{messages})
+					messages = make([]*sarama.ProducerMessage, 0, len(messages)) // Preallocate with previous size
+				}
+				messages = append(messages, msg)
+				packetSize = currentMsgSize
+			}
 		}
 	}
-	messageChunks = append(messageChunks, &ProducerMessageChunks{messages})
+
+	// Add final chunk if there are remaining messages
+	if len(messages) > 0 {
+		messageChunks = append(messageChunks, &ProducerMessageChunks{messages})
+	}
+
 	return messageChunks, errs
 }
 
