ac283b165daf24afd0574c760a83226eb639a2e0
Ibraheem Aboulnaga <ibraheem.aboulnaga@datadoghq.com>
2025-01-15 12:58:15 -0500

    wip

--- processor/datadogsemanticsprocessor/processor.go
+++ processor/datadogsemanticsprocessor/processor.go
@@ -6,6 +6,7 @@ package datadogsemanticsprocessor // import "github.com/open-telemetry/opentelem
 import (
 	"context"
 	"fmt"
+	"github.com/DataDog/datadog-agent/pkg/trace/traceutil"
 	"strings"
 
 	pb "github.com/DataDog/datadog-agent/pkg/proto/pbgo/trace"
@@ -18,23 +19,23 @@ func (tp *tracesProcessor) processTraces(ctx context.Context, td ptrace.Traces) 
 	rspans := td.ResourceSpans()
 	for i := 0; i < rspans.Len(); i++ {
 		rspan := rspans.At(i)
+		otelres := rspan.Resource()
 		for j := 0; j < rspan.ScopeSpans().Len(); j++ {
 			libspans := rspan.ScopeSpans().At(j)
 			for k := 0; k < libspans.Spans().Len(); k++ {
 				otelspan := libspans.Spans().At(k)
 				var ddspan *pb.Span
-
-				ddspan = transform.OtelSpanToDDSpan(otelspan, rspan.Resource(), libspans.Scope(), tp.agentCfg, []string{""})
+				ddspan = transform.OtelSpanToDDSpan(otelspan, otelres, libspans.Scope(), tp.agentCfg, []string{""})
 				if tp.overrideIncomingDatadogFields {
 					otelspan.Attributes().RemoveIf(func(k string, v pcommon.Value) bool {
 						return strings.HasPrefix(k, "datadog.")
 					})
 				}
 				if _, ok := otelspan.Attributes().Get("datadog.service"); !ok {
-					otelspan.Attributes().PutStr("datadog.service", ddspan.Service)
+					otelspan.Attributes().PutStr("datadog.service", traceutil.GetOTelService(otelres, true)
 				}
 				if _, ok := otelspan.Attributes().Get("datadog.name"); !ok {
-					otelspan.Attributes().PutStr("datadog.name", ddspan.Name)
+					otelspan.Attributes().PutStr("datadog.name", traceutil.GetOTelAttrValInResAndSpanAttrs(otelspan, otelres, true, "datadog.name"))
 				}
 				if _, ok := otelspan.Attributes().Get("datadog.resource"); !ok {
 					otelspan.Attributes().PutStr("datadog.resource", ddspan.Resource)
