560a80bba851498b52ecb5712f58617d15f41140
Dakota Paasman <122491662+dpaasman00@users.noreply.github.com>
2025-01-22 08:41:06 -0500

    remove duplicate opampclient mock func

--- cmd/opampsupervisor/supervisor/supervisor_test.go
+++ cmd/opampsupervisor/supervisor/supervisor_test.go
@@ -1040,8 +1040,6 @@ func (m mockOpAMPClient) SendCustomMessage(message *protobufs.CustomMessage) (me
 	return msgChan, nil
 }
 
-func (m mockOpAMPClient) SetFlags(_ protobufs.AgentToServerFlags) {}
-
 type mockConn struct {
 	sendFunc func(ctx context.Context, message *protobufs.ServerToAgent) error
 }
