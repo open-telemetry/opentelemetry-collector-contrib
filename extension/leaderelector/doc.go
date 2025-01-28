224e24025d6b2167a7e41dbec8c4ea368789151c
Rakesh Garimella <rakesh.garimella@sap.com>
2025-01-28 09:18:52 +0100

    changes based on review comments

--- extension/leaderelector/doc.go
+++ extension/leaderelector/doc.go
@@ -3,6 +3,4 @@
 
 //go:generate mdatagen metadata.yaml
 
-// Package myextension implements an extension
-
 package leaderelector // import "github.com/open-telemetry/opentelemetry-collector-contrib/extension/leaderelector"
