2dcfecdf2a637fdcfa7fe58f3ad68dd19c3943e8
Chao <chao.weng@appdynamics.com>
2025-02-11 01:01:10 +0800

    Deprecate extensiontest.NewNopSettings

--- extension/opampextension/factory_test.go
+++ extension/opampextension/factory_test.go
@@ -18,7 +18,7 @@ func TestFactory_CreateDefaultConfig(t *testing.T) {
 	assert.Equal(t, createDefaultConfig().(*Config), cfg)
 
 	assert.NoError(t, componenttest.CheckConfigStruct(cfg))
-	ext, err := createExtension(context.Background(), extensiontest.NewNopSettings(), cfg)
+	ext, err := createExtension(context.Background(), extensiontest.NewNopSettingsWithType(extensiontest.NopType), cfg)
 	require.NoError(t, err)
 	require.NotNil(t, ext)
 	require.NoError(t, ext.Shutdown(context.Background()))
@@ -26,7 +26,7 @@ func TestFactory_CreateDefaultConfig(t *testing.T) {
 
 func TestFactory_Create(t *testing.T) {
 	cfg := NewFactory().CreateDefaultConfig()
-	ext, err := createExtension(context.Background(), extensiontest.NewNopSettings(), cfg)
+	ext, err := createExtension(context.Background(), extensiontest.NewNopSettingsWithType(extensiontest.NopType), cfg)
 	require.NoError(t, err)
 	require.NotNil(t, ext)
 	require.NoError(t, ext.Shutdown(context.Background()))
