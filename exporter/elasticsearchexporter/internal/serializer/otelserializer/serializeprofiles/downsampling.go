8421fbab34f150c0bb9717186f6e85d787021f17
Tim Rühsen <tim.ruhsen@elastic.co>
2025-02-13 11:04:26 +0100

    Add SPDX license header

--- exporter/elasticsearchexporter/internal/serializer/otelserializer/serializeprofiles/downsampling.go
+++ exporter/elasticsearchexporter/internal/serializer/otelserializer/serializeprofiles/downsampling.go
@@ -1,4 +1,7 @@
-package serializeprofiles
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package serializeprofiles // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/elasticsearchexporter/internal/serializer/otelserializer/serializeprofiles"
 
 import (
 	"fmt"
