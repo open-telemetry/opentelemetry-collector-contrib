5f9d19982005aaecc7bd6422b4e729e4c96aaad3
Carson Ip <carson.ip@elastic.co>
2025-02-17 14:36:42 +0000

    Disable rate limiting on zero or negative value

--- exporter/elasticsearchexporter/internal/logging/options.go
+++ exporter/elasticsearchexporter/internal/logging/options.go
@@ -14,7 +14,11 @@ import (
 
 // WithRateLimit returns a zap.Option which rate limits messages
 // with approximately the given frequency.
+// interval <= 0 disables any rate limiting.
 func WithRateLimit(interval time.Duration) zap.Option {
+	if interval <= 0 {
+		return zap.WrapCore(func(core zapcore.Core) zapcore.Core { return core })
+	}
 	return zap.WrapCore(func(in zapcore.Core) zapcore.Core {
 		return zapcore.NewSamplerWithOptions(in, interval, 1, math.MaxInt32)
 	})
