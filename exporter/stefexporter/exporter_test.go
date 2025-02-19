de6f83468cca7d84dda11e3afed448e69a91abe5
Pablo Baeyens <pbaeyens31+github@gmail.com>
2025-02-19 11:30:11 +0100

    [chore] Use NewNopSettingsWithType(metadata.Type) on tests (#38030)
    <!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
    Ex. Adding a feature - Explain what this achieves.-->
    #### Description
    
    Tries to fix some of the contrib tests for
    open-telemetry/opentelemetry-collector/pull/12381
    
    <!-- Issue number (e.g. #1234) or full URL to issue, if applicable. -->
    #### Link to tracking issue
    
    Updates open-telemetry/opentelemetry-collector/issues/12221

--- exporter/stefexporter/exporter_test.go
+++ exporter/stefexporter/exporter_test.go
@@ -27,6 +27,7 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
+	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/stefexporter/internal/metadata"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/internal/common/testutil"
 )
 
@@ -147,7 +148,7 @@ func runTest(
 	// Make retries quick. We will be testing failure modes and don't want test to take too long.
 	cfg.RetryConfig.InitialInterval = 10 * time.Millisecond
 
-	set := exportertest.NewNopSettings()
+	set := exportertest.NewNopSettingsWithType(metadata.Type)
 	set.TelemetrySettings.Logger = logger
 
 	exp, err := factory.CreateMetrics(context.Background(), set, cfg)
@@ -298,7 +299,7 @@ func TestStartServerAfterClient(t *testing.T) {
 		TLSSetting: configtls.ClientConfig{Insecure: true},
 	}
 
-	set := exportertest.NewNopSettings()
+	set := exportertest.NewNopSettingsWithType(metadata.Type)
 	set.TelemetrySettings.Logger = logger
 
 	exp := newStefExporter(set.TelemetrySettings, cfg)
