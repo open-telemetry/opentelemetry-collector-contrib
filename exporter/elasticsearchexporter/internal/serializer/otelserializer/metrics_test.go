8c483a684df2be2bf4b41654462310b560dc36f3
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2025-02-20 16:04:04 +0100

    Fix test case

--- exporter/elasticsearchexporter/internal/serializer/otelserializer/metrics_test.go
+++ exporter/elasticsearchexporter/internal/serializer/otelserializer/metrics_test.go
@@ -56,6 +56,6 @@ func TestSerializeMetricsConflict(t *testing.T) {
 		"metrics": map[string]any{
 			"foo": json.Number("42"),
 		},
-		"_metric_names_hash": "fa57b911",
+		"_metric_names_hash": "a9f37ed7",
 	}, result, eventAsJSON)
 }
