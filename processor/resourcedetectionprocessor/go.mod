70a67d850ff4623d9626142fe2f809c4632e5a26
odubajDT <ondrej.dubaj@dynatrace.com>
2025-02-25 07:08:26 +0100

    introduce lib for exponential backoff
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- processor/resourcedetectionprocessor/go.mod
+++ processor/resourcedetectionprocessor/go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/config v1.29.6
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.28
 	github.com/aws/aws-sdk-go-v2/service/ec2 v1.203.1
+	github.com/cenkalti/backoff/v5 v5.0.2
 	github.com/google/go-cmp v0.6.0
 	github.com/hashicorp/consul/api v1.31.2
 	github.com/open-telemetry/opentelemetry-collector-contrib/internal/aws/ecsutil v0.120.1
--- processor/resourcedetectionprocessor/internal/resourcedetection.go
+++ processor/resourcedetectionprocessor/internal/resourcedetection.go
@@ -13,13 +13,12 @@ import (
 	"sync"
 	"time"
 
+	backoff "github.com/cenkalti/backoff/v5"
 	"go.opentelemetry.io/collector/featuregate"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/processor"
 	"go.uber.org/zap"
 )
-
-const MaxRetryInterval = 32 * time.Second
 
 var allowErrorPropagationFeatureGate = featuregate.GlobalRegistry().MustRegister(
 	"processor.resourcedetection.propagateerrors",
@@ -123,13 +122,13 @@ func (p *ResourceProvider) Get(ctx context.Context, client *http.Client) (resour
 		var cancel context.CancelFunc
 		ctx, cancel = context.WithTimeout(ctx, client.Timeout)
 		defer cancel()
-		p.detectResource(ctx)
+		p.detectResource(ctx, client.Timeout)
 	})
 
 	return p.detectedResource.resource, p.detectedResource.schemaURL, p.detectedResource.err
 }
 
-func (p *ResourceProvider) detectResource(ctx context.Context) {
+func (p *ResourceProvider) detectResource(ctx context.Context, timeout time.Duration) {
 	p.detectedResource = &resourceResult{}
 
 	res := pcommon.NewResource()
@@ -141,7 +140,13 @@ func (p *ResourceProvider) detectResource(ctx context.Context) {
 	for i, detector := range p.detectors {
 		resultsChan[i] = make(chan resourceResult)
 		go func(detector Detector) {
-			sleep := 1 * time.Second
+			sleep := backoff.ExponentialBackOff{
+				InitialInterval:     1 * time.Second,
+				RandomizationFactor: 1.5,
+				Multiplier:          2,
+				MaxInterval:         timeout,
+			}
+			sleep.Reset()
 			var err error
 			var r pcommon.Resource
 			var schemaURL string
@@ -152,11 +157,8 @@ func (p *ResourceProvider) detectResource(ctx context.Context) {
 					return
 				}
 				p.logger.Warn("failed to detect resource", zap.Error(err))
-				if sleep < MaxRetryInterval {
-					sleep *= 2
-				}
-				timer := time.NewTimer(sleep)
 
+				timer := time.NewTimer(sleep.NextBackOff())
 				select {
 				case <-timer.C:
 					fmt.Println("Retrying fetching data...")
