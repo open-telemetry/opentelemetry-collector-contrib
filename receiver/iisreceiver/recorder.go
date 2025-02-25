3871dc73f8ce38287e6e218ab77be8632d5e64f0
Robert Mayer <rwlmayer@gmail.com>
2025-02-25 10:28:57 +0100

    added additional application pool state recorder

--- receiver/iisreceiver/recorder.go
+++ receiver/iisreceiver/recorder.go
@@ -103,6 +103,15 @@ var appPoolPerfCounterRecorders = []perfCounterRecorderConf{
 			},
 		},
 	},
+	{
+		object:   "APP_POOL_WAS",
+		instance: "*",
+		recorders: map[string]recordFunc{
+			"Current Application Pool State": func(mb *metadata.MetricsBuilder, ts pcommon.Timestamp, val float64) {
+				mb.RecordIisApplicationPoolStateDataPoint(ts, int64(val))
+			},
+		},
+	},
 }
 
 func recordMaxQueueItemAge(mb *metadata.MetricsBuilder, ts pcommon.Timestamp, val float64) {
