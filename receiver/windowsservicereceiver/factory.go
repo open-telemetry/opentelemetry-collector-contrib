414352922ef77c51d5319b9cec7f427d301ff7f2
Paulo Janotti <pjanotti@splunk.com>
2025-02-27 14:12:50 -0800

    Fix factory

--- receiver/windowsservicereceiver/factory.go
+++ receiver/windowsservicereceiver/factory.go
@@ -20,6 +20,7 @@ func NewFactory() receiver.Factory {
 	return receiver.NewFactory(
 		metadata.Type,
 		createDefaultConfig,
+		receiver.WithMetrics(createMetricsReceiver, metadata.MetricsStability),
 	)
 }
 
