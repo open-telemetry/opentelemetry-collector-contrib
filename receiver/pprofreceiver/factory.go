888f3e2d9a1a4fba8f15174dd22b66c68a46ed16
Antoine Toulme <atoulme@splunk.com>
2025-02-27 20:26:17 -0500

    [receiver/pprof] New pprof receiver

---
+++ receiver/pprofreceiver/factory.go
@@ -0,0 +1,37 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package pprofreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/pprofreceiver"
+
+import (
+	"context"
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/config/confighttp"
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/receiver"
+	"go.opentelemetry.io/collector/receiver/xreceiver"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/pprofreceiver/internal/metadata"
+)
+
+func NewFactory() receiver.Factory {
+	return xreceiver.NewFactory(
+		metadata.Type,
+		createDefaultConfig,
+		xreceiver.WithProfiles(createProfilesReceiver, metadata.ProfilesStability))
+}
+
+func createDefaultConfig() component.Config {
+	return &Config{
+		ClientConfig: confighttp.NewDefaultClientConfig(),
+	}
+}
+
+func createProfilesReceiver(
+	_ context.Context,
+	params receiver.Settings,
+	rConf component.Config,
+	consumer consumer.Metrics,
+) (receiver.Profiles, error) {
+	return nil, errors.New("not implemented")
+}
