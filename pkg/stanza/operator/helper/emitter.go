2760116647477e5f857b54e0bf9f0ecc90feef0a
Andrzej Stencel <andrzej.stencel@elastic.co>
2025-03-03 16:12:21 +0100

    [pkg/stanza] Introduce batching logs in File consumer (#36663)
    #### Description
    
    Modifies the File consumer to emit logs in batches as opposed to sending
    each log individually through the Stanza pipeline and on to the Log
    Emitter.
    
    Here are the changes introduced:
    
    -
    https://github.com/open-telemetry/opentelemetry-collector-contrib/commit/6b4c9fe5d8159a9147dcfd23a8bdb95a0de653c8
    Changed the `Reader::ReadToEnd` method in File consumer to collect the
    tokens scanned from the file into batches. At this point, the Reader
    still emits each token individually, as the `emit.Callback` function
    only accepts a single token.
    -
    https://github.com/open-telemetry/opentelemetry-collector-contrib/commit/c20699520e630255f723835ac8abd156fd663ded
    Changed `emit.Callback` function signature to accept a slice of tokens
    as opposed to a single token, and changed the Reader to emit a batch of
    tokens in one request. At this point, the batches are still split into
    individual tokens inside the `emit` function, because the Stanza
    operators can only process one entry at a time.
    -
    https://github.com/open-telemetry/opentelemetry-collector-contrib/commit/aedda3adabd11fab641154b593ef2256689aa775
    Added `ProcessBatch` method to Stanza operators and used it in the
    `emit` function. At this point, the batch of tokens is translated to a
    batch of entries and passed to Log Emitter as a whole batch. The batch
    is still split in the Log Emitter, which calls `consumeFunc` for each
    entry in a loop.
    -
    https://github.com/open-telemetry/opentelemetry-collector-contrib/commit/13d60543d7fbc75534ab30755a1e7e1ace06abad
    Changed the LogEmitter to add the whole batch to its buffer, as opposed
    to adding entries one by one.
    
    **Slice of entries `[]entry.Entry` vs. slice of pointers
    `[]*entry.Entry`**
    
    I considered whether the `ProcessBatch` method in the `Operator`
    interface should accept a slice of structs `[]entry.Entry` or a slice of
    pointers `[]*entry.Entry`. I ran some tests (similar to
    https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/35454)
    and they showed a 7-10% performance loss when using a slice of structs
    vs. a slice of pointers. That's why I decided to use the slice of
    pointers `[]*entry.Entry`.
    
    #### Link to tracking issue
    
    - Fixes
    https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/35455
    
    #### Testing
    
    No changes in tests. The goal is for the functionality to not change and
    for performance to not decrease.
    
    I have added a new benchmark in a separate PR
    https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/38054
    that should be helpful in assessing the performance impact of this
    change.
    
    #### Documentation
    
    These are internal changes, no user documentation needs changing.

--- pkg/stanza/operator/helper/emitter.go
+++ pkg/stanza/operator/helper/emitter.go
@@ -99,6 +99,31 @@ func (e *LogEmitter) Stop() error {
 	return nil
 }
 
+// ProcessBatch emits the entries to the consumerFunc
+func (e *LogEmitter) ProcessBatch(ctx context.Context, entries []*entry.Entry) error {
+	if oldBatch := e.appendEntries(entries); len(oldBatch) > 0 {
+		e.consumerFunc(ctx, oldBatch)
+	}
+
+	return nil
+}
+
+// appendEntries appends the entry to the current batch. If maxBatchSize is reached, a new batch will be made, and the old batch
+// (which should be flushed) will be returned
+func (e *LogEmitter) appendEntries(entries []*entry.Entry) []*entry.Entry {
+	e.batchMux.Lock()
+	defer e.batchMux.Unlock()
+
+	e.batch = append(e.batch, entries...)
+	if uint(len(e.batch)) >= e.maxBatchSize {
+		var oldBatch []*entry.Entry
+		oldBatch, e.batch = e.batch, make([]*entry.Entry, 0, e.maxBatchSize)
+		return oldBatch
+	}
+
+	return nil
+}
+
 // Process will emit an entry to the output channel
 func (e *LogEmitter) Process(ctx context.Context, ent *entry.Entry) error {
 	if oldBatch := e.appendEntry(ent); len(oldBatch) > 0 {
