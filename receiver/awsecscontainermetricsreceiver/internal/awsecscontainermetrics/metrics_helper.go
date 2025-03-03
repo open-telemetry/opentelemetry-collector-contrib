18e6b640fba3231579bc6f3375c68013ef4f8555
Jon Nangle <jon.nangle@ft.com>
2025-03-03 13:03:14 +0000

    Aggregate io metrics from all devices

--- receiver/awsecscontainermetricsreceiver/internal/awsecscontainermetrics/metrics_helper.go
+++ receiver/awsecscontainermetricsreceiver/internal/awsecscontainermetrics/metrics_helper.go
@@ -110,9 +110,9 @@ func extractStorageUsage(stats *DiskStats) (uint64, uint64) {
 	for _, blockStat := range stats.IoServiceBytesRecursives {
 		switch op := blockStat.Op; op {
 		case "Read":
-			readBytes = aws.Uint64Value(blockStat.Value)
+			readBytes += aws.Uint64Value(blockStat.Value)
 		case "Write":
-			writeBytes = aws.Uint64Value(blockStat.Value)
+			writeBytes += aws.Uint64Value(blockStat.Value)
 		default:
 			// ignoring "Async", "Total", "Sum", etc
 			continue
