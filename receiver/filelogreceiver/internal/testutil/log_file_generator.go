c0e586bf8cf0ec9f400b4f41fd137b09a430eaba
Andrzej Stencel <andrzej.stencel@elastic.co>
2025-03-03 08:22:30 +0100

    make goporto

--- receiver/filelogreceiver/internal/testutil/log_file_generator.go
+++ receiver/filelogreceiver/internal/testutil/log_file_generator.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package testutil
+package testutil // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/filelogreceiver/internal/testutil"
 
 import (
 	"math/rand/v2"
