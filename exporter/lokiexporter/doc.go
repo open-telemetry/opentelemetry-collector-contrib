7aba65c250ff330161b0b6fb03afb7ef5e0cadcd
Antoine Toulme <atoulme@splunk.com>
2025-03-04 14:25:28 -0800

    [exporter/loki] remove code

--- exporter/lokiexporter/doc.go
+++
@@ -1,5 +0,0 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-
-// Package lokiexporter implements an exporter that sends log data to Loki.
-package lokiexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/lokiexporter"
