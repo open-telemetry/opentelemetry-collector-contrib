9533d189766e0ba63d73f6e3b13ade1f8626150b
Cuichen Li <cuichen.li94@gmail.com>
2025-03-04 15:19:43 +0800

    add query to be executed
    Co-authored-by: Chao Weng <chao.weng@appdynamics.com>

--- receiver/sqlserverreceiver/queries.go
+++ receiver/sqlserverreceiver/queries.go
@@ -4,6 +4,7 @@
 package sqlserverreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/sqlserverreceiver"
 
 import (
+	_ "embed"
 	"fmt"
 	"strings"
 )
@@ -335,4 +336,11 @@ func getSQLServerPropertiesQuery(instanceName string) string {
 	}
 
 	return fmt.Sprintf(sqlServerProperties, "")
+}
+
+//go:embed templates/sqlServerQuerySample.tmpl
+var sqlServerQuerySamples string
+
+func getSQLServerQuerySamplesQuery() string {
+	return sqlServerQuerySamples
 }
