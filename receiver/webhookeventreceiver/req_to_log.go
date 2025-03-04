open-telemetry/opentelemetry-service-contrib

[webhookeventreceiver] add option to include headers as attributes

receiver/webhookeventreceiver/req_to_log.go
@@ -5,7 +5,10 @@ package webhookeventreceiver // import "github.com/open-telemetry/opentelemetry-
 
 import (
 	"bufio"
+	"net/http"
+	"net/textproto"
 	"net/url"
+	"strings"
 	"time"
 
 	"go.opentelemetry.io/collector/pdata/pcommon"
@@ -16,8 +19,9 @@ import (
 )
 
 func reqToLog(sc *bufio.Scanner,
+	headers http.Header,
 	query url.Values,
-	_ *Config,
+	config *Config,
 	settings receiver.Settings,
 ) (plog.Logs, int) {
 	// we simply dont split the data passed into scan (i.e. scan the whole thing)
@@ -41,6 +45,9 @@ func reqToLog(sc *bufio.Scanner,
 	scopeLog.Scope().SetVersion(settings.BuildInfo.Version)
 	scopeLog.Scope().Attributes().PutStr("source", settings.ID.String())
 	scopeLog.Scope().Attributes().PutStr("receiver", metadata.Type.String())
+	if config.ConvertHeadersToAttributes {
+		appendHeaders(config, scopeLog, headers)
+	}
 
 	for sc.Scan() {
 		logRecord := scopeLog.LogRecords().AppendEmpty()
@@ -58,5 +65,19 @@ func appendMetadata(resourceLog plog.ResourceLogs, query url.Values) {
 		if query.Get(k) != "" {
 			resourceLog.Resource().Attributes().PutStr(k, query.Get(k))
 		}
+	}
+}
+
+// append headers as attributes
+func appendHeaders(config *Config, scopeLog plog.ScopeLogs, headers http.Header) {
+	for k := range headers {
+		// Skip the required header used for authentication
+		if k == textproto.CanonicalMIMEHeaderKey(config.RequiredHeader.Key) {
+			continue
+		}
+		// store headers with "header" namespace and normalize key to snake_case
+		normalizedHeader := strings.ReplaceAll(k, "-", "_")
+		normalizedHeader = strings.ToLower(normalizedHeader)
+		scopeLog.Scope().Attributes().PutStr("header."+normalizedHeader, strings.Join(headers.Values(k), ";"))
 	}
 }

open-telemetry/opentelemetry-collector-contrib

receiver/webhookeventreceiver/req_to_log.go
@@ -5,7 +5,10 @@ package webhookeventreceiver // import "github.com/open-telemetry/opentelemetry-
 
 import (
 	"bufio"
+	"net/http"
+	"net/textproto"
 	"net/url"
+	"strings"
 	"time"
 
 	"go.opentelemetry.io/collector/pdata/pcommon"
@@ -16,6 +19,7 @@ import (
 )
 
 func reqToLog(sc *bufio.Scanner,
+	headers http.Header,
 	query url.Values,
 	cfg *Config,
 	settings receiver.Settings,
@@ -45,6 +49,9 @@ func reqToLog(sc *bufio.Scanner,
 	scopeLog.Scope().SetVersion(settings.BuildInfo.Version)
 	scopeLog.Scope().Attributes().PutStr("source", settings.ID.String())
 	scopeLog.Scope().Attributes().PutStr("receiver", metadata.Type.String())
+	if cfg.ConvertHeadersToAttributes {
+		appendHeaders(cfg, scopeLog, headers)
+	}
 
 	for sc.Scan() {
 		logRecord := scopeLog.LogRecords().AppendEmpty()
@@ -62,5 +69,19 @@ func appendMetadata(resourceLog plog.ResourceLogs, query url.Values) {
 		if query.Get(k) != "" {
 			resourceLog.Resource().Attributes().PutStr(k, query.Get(k))
 		}
+	}
+}
+
+// append headers as attributes
+func appendHeaders(config *Config, scopeLog plog.ScopeLogs, headers http.Header) {
+	for k := range headers {
+		// Skip the required header used for authentication
+		if k == textproto.CanonicalMIMEHeaderKey(config.RequiredHeader.Key) {
+			continue
+		}
+		// store headers with "header" namespace and normalize key to snake_case
+		normalizedHeader := strings.ReplaceAll(k, "-", "_")
+		normalizedHeader = strings.ToLower(normalizedHeader)
+		scopeLog.Scope().Attributes().PutStr("header."+normalizedHeader, strings.Join(headers.Values(k), ";"))
 	}
 }