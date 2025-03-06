c300c29dfebd7b5a9afcf9fde018c5e9fc313a1e
Florian Bacher <florian.bacher@dynatrace.com>
2025-03-06 13:40:13 +0100

    fix linting
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- pkg/ottl/e2e/e2e_test.go
+++ pkg/ottl/e2e/e2e_test.go
@@ -1374,7 +1374,6 @@ func Test_e2e_ottl_statement_sequence(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-
 			tCtx := constructLogTransformContext()
 
 			for _, statement := range tt.statements {
