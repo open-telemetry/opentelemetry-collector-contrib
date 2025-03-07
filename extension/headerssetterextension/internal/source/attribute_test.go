391a5e56c073c980a9fd3d56bb8a0443bc61b54e
Jener Rasmussen <git@jener.me>
2025-03-07 18:07:16 +0100

    [extension/header_setter] Test expected value for attribute with struct as value

--- extension/headerssetterextension/internal/source/attribute_test.go
+++ extension/headerssetterextension/internal/source/attribute_test.go
@@ -12,7 +12,7 @@ import (
 )
 
 type authData struct {
-	attrs map[string]string
+	attrs map[string]any
 }
 
 func (a authData) GetAttribute(s string) any {
@@ -27,26 +27,42 @@ func (a authData) GetAttributeNames() []string {
 	return keys
 }
 
-func TestAttributeSourceSuccess(t *testing.T) {
+func TestAttributeSourceSuccessString(t *testing.T) {
 	ts := &AttributeSource{Key: "X-Scope-OrgID"}
 	cl := client.FromContext(context.Background())
-	cl.Auth = authData{attrs: map[string]string{"X-Scope-OrgID": "acme"}}
+	cl.Auth = authData{attrs: map[string]any{"X-Scope-OrgID": "acme"}}
 	ctx := client.NewContext(context.Background(), cl)
 
-	header, err := ts.Get(ctx)
+	val, err := ts.Get(ctx)
 
 	assert.NoError(t, err)
-	assert.Equal(t, "acme", header)
+	assert.Equal(t, "acme", val)
+}
+
+func TestAttributeSourceSuccessStruct(t *testing.T) {
+	ts := &AttributeSource{Key: "X-Scope-OrgID"}
+	cl := client.FromContext(context.Background())
+	cl.Auth = authData{attrs: map[string]any{"X-Scope-OrgID": struct {
+		Foo string
+	}{
+		Foo: "bar",
+	}}}
+	ctx := client.NewContext(context.Background(), cl)
+
+	val, err := ts.Get(ctx)
+
+	assert.NoError(t, err)
+	assert.Equal(t, "{\"Foo\":\"bar\"}", val)
 }
 
 func TestAttributeSourceNotFound(t *testing.T) {
 	ts := &AttributeSource{Key: "X-Scope-OrgID"}
 	cl := client.FromContext(context.Background())
-	cl.Auth = authData{attrs: map[string]string{"Not-Scope-OrgID": "acme"}}
+	cl.Auth = authData{attrs: map[string]any{"Not-Scope-OrgID": "acme"}}
 	ctx := client.NewContext(context.Background(), cl)
 
-	header, err := ts.Get(ctx)
+	val, err := ts.Get(ctx)
 
 	assert.NoError(t, err)
-	assert.Empty(t, header)
+	assert.Empty(t, val)
 }
