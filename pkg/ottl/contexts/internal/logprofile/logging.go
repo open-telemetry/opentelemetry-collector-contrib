9eefcf9583a38a29a6932708c90e21253e595884
Tim Rühsen <tim.ruhsen@elastic.co>
2025-03-10 12:25:35 +0100

    Fix goporto

--- pkg/ottl/contexts/internal/logprofile/logging.go
+++ pkg/ottl/contexts/internal/logprofile/logging.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package logprofile // import "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/ottlprofile"
+package logprofile // import "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/internal/logprofile"
 
 import (
 	"encoding/hex"
