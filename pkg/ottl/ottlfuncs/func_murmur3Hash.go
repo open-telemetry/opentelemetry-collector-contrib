c51c28df4f094eae909bedc060011aceeabc7f61
Kaise Cheng <kaise.cheng@elastic.co>
2025-03-11 17:25:08 +0000

    license check

--- pkg/ottl/ottlfuncs/func_murmur3Hash.go
+++ pkg/ottl/ottlfuncs/func_murmur3Hash.go
@@ -1,3 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
 package ottlfuncs // import "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/ottlfuncs"
 
 import (
