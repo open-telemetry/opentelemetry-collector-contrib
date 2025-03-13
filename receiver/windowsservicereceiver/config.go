70cd04241f5d7214811e9adc7dc9a8c6fc546797
shalper2 <shalpern@splunk.com>
2025-03-13 15:52:38 -0500

    first pass at config

--- receiver/windowsservicereceiver/config.go
+++ receiver/windowsservicereceiver/config.go
@@ -3,4 +3,18 @@
 
 package windowsservicereceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/windowsservicereceiver"
 
-type Config struct{}
+import (
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/windowsservicereceiver/internal/metadata"
+	"go.opentelemetry.io/collector/receiver/scraperhelper"
+)
+
+// Config defines configuration for windowsservice receiver.
+type Config struct {
+	scraperhelper.ControllerConfig `mapstructure:",squash"`
+	metadata.MetricsBuilderConfig  `mapstructure:",squash"`
+	IncludeServices                []string `mapstructure:"include_services"` // user provided list of services to monitor with receiver
+	ExcludeServices                []string `mapstructure:"exclude_services"` // user provided list of services to be excluded
+}
+
+// Validate checks the receiver configuration is valid
+func (cfg *Config) Validate() error { return nil }
