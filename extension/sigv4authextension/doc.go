2026aef9780dc16522d64ee27a34fe3ea4d1b577
Pablo Baeyens <pbaeyens31+github@gmail.com>
2025-03-13 17:48:37 +0100

    [chore] Replace extensionauth.Client mentions with protocol-specific mentions (#38601)
    <!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
    Ex. Adding a feature - Explain what this achieves.-->
    #### Description
    
    <!-- Issue number (e.g. #1234) or full URL to issue, if applicable. -->
    
    Follows #38451 and open-telemetry/opentelemetry-collector/pull/12574.
    
    Removes explicit mentions to `extensionauth.Client` and replaces them by
    `extensionauth.HTTPClient` and `extensionauth.GRPCClient`

--- extension/sigv4authextension/doc.go
+++ extension/sigv4authextension/doc.go
@@ -3,7 +3,7 @@
 
 //go:generate mdatagen metadata.yaml
 
-// Package sigv4authextension implements the `extensionauth.Client` interface.
+// Package sigv4authextension implements the `extensionauth.HTTPClient` interface.
 // This extension provides the Sigv4 process of adding authentication information to AWS API requests sent by HTTP.
 // As such, the extension can be used for HTTP based exporters that export to AWS services.
 package sigv4authextension // import "github.com/open-telemetry/opentelemetry-collector-contrib/extension/sigv4authextension"
