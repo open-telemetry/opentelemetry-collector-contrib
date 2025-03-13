1012d257434a733954dcc638c46a25e16933ba59
Tomas Pica <tomas.picadelcanto@grafana.com>
2025-03-13 17:20:37 +0100

    lint license header

--- exporter/faroexporter/exporter.go
+++ exporter/faroexporter/exporter.go
@@ -1,3 +1,4 @@
+// Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
 package faroexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/faroexporter"
