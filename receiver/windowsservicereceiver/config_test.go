04631d3df35d81d5a85ace79b2e44c2a8fb66223
shalper2 <shalpern@splunk.com>
2025-03-20 10:56:35 -0500

    removed unused testing code

--- receiver/windowsservicereceiver/config_test.go
+++ receiver/windowsservicereceiver/config_test.go
@@ -2,10 +2,3 @@
 // SPDX-License-Identifier: Apache-2.0
 
 package windowsservicereceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/windowsservicereceiver"
-
-import (
-	"testing"
-)
-
-func TestValidateConfig(t *testing.T) {
-}
