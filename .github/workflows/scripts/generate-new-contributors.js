fbeaa0e019502c1aa53dd14860792500a6a1a85c
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2025-03-26 16:53:32 +0100

    working script
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

--- .github/workflows/scripts/generate-new-contributors.js
+++ .github/workflows/scripts/generate-new-contributors.js
@@ -1,41 +1,62 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-const simpleGit = require("simple-git");
+const simpleGit = require('simple-git');
+const git = simpleGit("../../../");
 
-const REPO_NAME = "opentelemetry-collector-contrib"
-const REPO_OWNER = "open-telemetry"
-const git = simpleGit("../../../")
+async function getFirstTimeContributors(fromTag, toTag) {
+    try {
+        const fromCommit = await git.revparse(fromTag)
+        const toCommit = await git.revparse(toTag)
+        // Get the list of commits between two tags
+        const releaseCommits = await git.log({
+            from: fromCommit, to: toCommit,
+            format: "oneline"
+        }).then(result => result.all);
 
+        const authors = releaseCommits.map(commit => {
+            return {name: commit.author_name, email: commit.author_email}
+        });
 
-async function main({github, context}) {
-    let fromCommit = await git.revparse("v0.121.0")
-    let toCommit = await git.revparse("v0.122.0")
-    let commits = await git.log({
-        from: fromCommit,
-        to: toCommit,
-        format: "oneline"
-    })
-    console.log(`Got ${commits.all.length} commits since last release.`)
+        const uniqueAuthors = getUniqueCombinations(authors);
+        const allCommits = await git.log({
+            format: "oneline",
+        })
 
-    let commitAuthors = commits.all.map(commit => {
-        return {authorName: commit.author_name, authorEmail: commit.author_email}
-    })
+        const firstTimeContributors = [];
+        for (const author of uniqueAuthors) {
+            console.log(`Handling commits of ${author.name}`)
+            const authorCommits = allCommits.all.filter(commit => {
+                return commit.author_name === author.name && commit.author_email === author.email
+            });
 
-    let firstTimeContributors = commitAuthors.filter(async author => {
-        let firstTimer = await simpleGit("../../../").raw([
-            'rev-list',
-            'HEAD',
-            '--count',
-            '--author=' + author.authorName,
-        ])
-        firstTimer = firstTimer.trim()
+            if (authorCommits.length === 1) {
+                firstTimeContributors.push(author);
+            }
+        }
 
-        return firstTimer === '1'
-    })
-    
-    console.log(firstTimeContributors)
-    console.log("bla")
+        return firstTimeContributors;
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
 
-main({undefined, undefined}).then(r => {console.log("done")})
+getFirstTimeContributors('v0.121.0', 'v0.122.0').then(contributors => {
+    console.log('First-time contributors:', contributors);
+    console.log('Number of first-time contributors: ', contributors.length);
+});
+
+function getUniqueCombinations(data) {
+    const uniqueSet = new Set();
+    const uniqueArray = [];
+
+    data.forEach(item => {
+        const combination = `${item.name}-${item.email}`;
+        if (!uniqueSet.has(combination)) {
+            uniqueSet.add(combination);
+            uniqueArray.push(item);
+        }
+    });
+
+    return uniqueArray;
+}
