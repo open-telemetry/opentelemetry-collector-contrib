f0c970084d34c0118b0ad76c3cbc3c5dbf69d48d
Andrew Wilkins <axw@elastic.co>
2025-03-26 16:57:59 +0800

    [chore] exporter/kafkaexporter: tidy up
    - Move marshaler implementations to internal package
    - Simplify code to obtain a marshaler given an encoding name
    - Make marshalers independent of Sarama (Kafka client)
    - Extract partitioning logic out of marshalers, with the exception
      of Jaeger marshalers
    - Implement a generic Kafka exporter with type parameter
      for signal-specific parts
    - Create more tightly scoped unit tests for encodings, independent of the
      general functionality of the exporter

--- exporter/kafkaexporter/config.go
+++ exporter/kafkaexporter/config.go
@@ -31,11 +31,18 @@ type Config struct {
 	Encoding string `mapstructure:"encoding"`
 
 	// PartitionTracesByID sets the message key of outgoing trace messages to the trace ID.
-	// Please note: does not have any effect on Jaeger encoding exporters since Jaeger exporters include
-	// trace ID as the message key by default.
+	//
+	// NOTE: this does not have any effect for Jaeger encodings. Jaeger encodings always use
+	// use the trace ID for the message key.
 	PartitionTracesByID bool `mapstructure:"partition_traces_by_id"`
 
+	// PartitionMetricsByResourceAttributes controls the partitioning of metrics messages by
+	// resource. If this is true, then the message key will be set to a hash of the resource's
+	// identifying attributes.
 	PartitionMetricsByResourceAttributes bool `mapstructure:"partition_metrics_by_resource_attributes"`
 
+	// PartitionLogsByResourceAttributes controls the partitioning of logs messages by resource.
+	// If this is true, then the message key will be set to a hash of the resource's identifying
+	// attributes.
 	PartitionLogsByResourceAttributes bool `mapstructure:"partition_logs_by_resource_attributes"`
 }
