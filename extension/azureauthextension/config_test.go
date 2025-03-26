816ce57760668920d65dda6b91dc30e515c51a46
Constança Manteigas <113898685+constanca-m@users.noreply.github.com>
2025-03-26 19:16:46 +0100

    New component: azureauthextension (#38991)
    <!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
    Ex. Adding a feature - Explain what this achieves.-->
    #### Description
    
    Adds new component authenticator for Azure.
    
    <!-- Issue number (e.g. #1234) or full URL to issue, if applicable. -->
    #### Link to tracking issue
    Fixes #38908.
    
    <!--Describe what testing was performed and which tests were added.-->
    #### Testing
    
    Unit tests.
    
    <!--Describe the documentation added.-->
    #### Documentation
    
    README file. More details will be added along new PRs.
    
    <!--Please delete paragraphs that you did not use before submitting.-->

---
+++ extension/azureauthextension/config_test.go
@@ -0,0 +1,121 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package azureauthextension
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/confmap/confmaptest"
+	"go.opentelemetry.io/collector/confmap/xconfmap"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/azureauthextension/internal/metadata"
+)
+
+func TestLoadConfig(t *testing.T) {
+	t.Parallel()
+
+	cm, errLoad := confmaptest.LoadConf(filepath.Join("testdata", "config.yaml"))
+	require.NoError(t, errLoad)
+
+	tests := []struct {
+		id          component.ID
+		expected    component.Config
+		expectedErr string
+	}{
+		{
+			id:          component.NewIDWithName(metadata.Type, ""),
+			expectedErr: errEmptyAuthentication.Error() + "\n" + errEmptyScope.Error(),
+		},
+		{
+			id: component.NewIDWithName(metadata.Type, "use_default"),
+			expected: &Config{
+				UseDefault: true,
+				Scope:      "test",
+			},
+		},
+		{
+			id: component.NewIDWithName(metadata.Type, "valid_service_principal_secret"),
+			expected: &Config{
+				Scope: "test",
+				ServicePrincipal: &ServicePrincipal{
+					TenantID:     "test",
+					ClientID:     "test",
+					ClientSecret: "test",
+				},
+			},
+		},
+		{
+			id: component.NewIDWithName(metadata.Type, "valid_service_principal_certificate"),
+			expected: &Config{
+				Scope: "test",
+				ServicePrincipal: &ServicePrincipal{
+					TenantID:              "test",
+					ClientID:              "test",
+					ClientCertificatePath: "test",
+				},
+			},
+		},
+		{
+			id:          component.NewIDWithName(metadata.Type, "service_principal_empty_client_id"),
+			expectedErr: fmt.Sprintf("%s: %s", "service_principal", errEmptyClientID.Error()),
+		},
+		{
+			id:          component.NewIDWithName(metadata.Type, "service_principal_empty_tenant_id"),
+			expectedErr: fmt.Sprintf("%s: %s", "service_principal", errEmptyTenantID.Error()),
+		},
+		{
+			id:          component.NewIDWithName(metadata.Type, "service_principal_empty_client_credential"),
+			expectedErr: fmt.Sprintf("%s: %s", "service_principal", errEmptyClientCredential.Error()),
+		},
+		{
+			id: component.NewIDWithName(metadata.Type, "valid_workload_identity"),
+			expected: &Config{
+				Scope: "test",
+				Workload: &WorkloadIdentity{
+					TenantID:           "test",
+					ClientID:           "test",
+					FederatedTokenFile: "test",
+				},
+			},
+		},
+		{
+			id:          component.NewIDWithName(metadata.Type, "workload_identity_empty_client_id"),
+			expectedErr: fmt.Sprintf("%s: %s", "workload_identity", errEmptyClientID.Error()),
+		},
+		{
+			id:          component.NewIDWithName(metadata.Type, "workload_identity_empty_tenant_id"),
+			expectedErr: fmt.Sprintf("%s: %s", "workload_identity", errEmptyTenantID.Error()),
+		},
+		{
+			id:          component.NewIDWithName(metadata.Type, "workload_identity_empty_federated_token_file"),
+			expectedErr: fmt.Sprintf("%s: %s", "workload_identity", errEmptyFederatedTokenFile.Error()),
+		},
+	}
+
+	for _, tt := range tests {
+		name := strings.ReplaceAll(tt.id.String(), "/", "_")
+		t.Run(name, func(t *testing.T) {
+			factory := NewFactory()
+			cfg := factory.CreateDefaultConfig()
+
+			sub, err := cm.Sub(tt.id.String())
+			require.NoError(t, err)
+			require.NoError(t, sub.Unmarshal(cfg))
+
+			err = xconfmap.Validate(cfg)
+			if tt.expectedErr != "" {
+				require.Error(t, err)
+				require.EqualError(t, err, tt.expectedErr)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tt.expected, cfg)
+			}
+		})
+	}
+}
---
+++ extension/azureauthextension/factory.go
@@ -0,0 +1,32 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package azureauthextension // import "github.com/open-telemetry/opentelemetry-collector-contrib/extension/azureauthextension"
+
+import (
+	"context"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/extension"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/azureauthextension/internal/metadata"
+)
+
+func NewFactory() extension.Factory {
+	return extension.NewFactory(
+		metadata.Type,
+		createDefaultConfig,
+		createExtension,
+		metadata.ExtensionStability,
+	)
+}
+
+func createExtension(_ context.Context, set extension.Settings, cfg component.Config) (extension.Extension, error) {
+	return newAzureAuthenticator(cfg.(*Config), set.Logger), nil
+}
+
+func createDefaultConfig() component.Config {
+	return &Config{
+		UseDefault: false,
+	}
+}
