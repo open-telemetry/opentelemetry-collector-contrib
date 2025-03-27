07c08e563246e2cdc4f2abb7ac5b1ce672337aa1
Tomas Pica <tomas.picadelcanto@grafana.com>
2025-03-27 16:48:31 +0100

    add license header

--- exporter/faroexporter/utils.go
+++ exporter/faroexporter/utils.go
@@ -1,3 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
 package faroexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/faroexporter"
 
 import (
