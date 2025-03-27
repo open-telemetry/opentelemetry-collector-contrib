4a455cd81633ece8dcb45cfc525251f515fc1151
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2025-03-27 09:39:27 +0100

    add commit hashes to list of new contributors to fetch usernames afterwards, prepare workflow
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

--- .github/workflows/scripts/generate-new-contributors.js
+++ .github/workflows/scripts/generate-new-contributors.js
@@ -29,34 +29,43 @@ async function getFirstTimeContributors(fromTag, toTag) {
             format: "oneline"
         }).then(result => result.all);
 
-        const authors = releaseCommits.map(commit => {
-            return {name: commit.author_name, email: commit.author_email}
+        const authorsAndHashes = releaseCommits.map(commit => {
+            return {name: commit.author_name, email: commit.author_email, hash: commit.hash}
         });
 
-        const uniqueAuthors = getUniqueCombinations(authors);
+        const uniqueAuthorsAndHashes = getUniqueCombinations(authorsAndHashes);
         const allCommits = await git.log({
             format: "oneline",
         })
 
-        const firstTimeContributors = [];
-        for (const author of uniqueAuthors) {
-            console.log(`Handling commits of ${author.name}`)
+        const firstTimeContributorsAndHashes = [];
+        for (const item of uniqueAuthorsAndHashes) {
+            console.log(`Handling commits of ${item.name}`)
             const authorCommits = allCommits.all.filter(commit => {
-                return commit.author_name === author.name && commit.author_email === author.email
+                return commit.author_name === item.name && commit.author_email === item.email
             });
 
             if (authorCommits.length === 1) {
-                firstTimeContributors.push(author);
+                firstTimeContributorsAndHashes.push(item);
             }
         }
 
-        return firstTimeContributors;
+        return firstTimeContributorsAndHashes;
     } catch (error) {
         console.error('Error:', error);
     }
 }
 
-getFirstTimeContributors('v0.121.0', 'v0.122.0').then(contributors => {
-    console.log('First-time contributors:', contributors);
-    console.log('Number of first-time contributors: ', contributors.length);
-});
+async function main({ github }) {
+    getFirstTimeContributors('v0.121.0', 'v0.122.0').then(contributors => {
+        console.log('First-time contributors:', contributors);
+        console.log('Number of first-time contributors: ', contributors.length);
+    });
+}
+
+module.exports = async ({ github }) => {
+    await main({ github })
+}
+
+github = undefined
+main({ github })
