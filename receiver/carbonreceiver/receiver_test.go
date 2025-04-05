5377acbf69093fb3e41937ae0f75baa57a12ffe4
Matthieu MOREL <matthieu.morel35@gmail.com>
2025-04-05 10:34:28 +0200

    [chore]: fix testifylint rules (receiver)
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- receiver/carbonreceiver/receiver_test.go
+++ receiver/carbonreceiver/receiver_test.go
@@ -237,7 +237,7 @@ func Test_carbonreceiver_EndToEnd(t *testing.T) {
 			m := mdd[0].ResourceMetrics().At(0).ScopeMetrics().At(0).Metrics().At(0)
 			assert.Equal(t, carbonMetric.Name, m.Name())
 			require.Equal(t, 1, m.Gauge().DataPoints().Len())
-			require.Equal(t, len(recorder.Ended()), len(recorder.Started()))
+			require.Len(t, recorder.Started(), len(recorder.Ended()))
 		})
 	}
 }
