89a6f8a427c87a2e0ff8738c6b33096f27d3b201
Bence Csati <bence.csati@axoflow.com>
2025-04-07 09:04:06 +0200

    chore: use goccy/go-json instead of json-iterator/go
    Signed-off-by: Bence Csati <bence.csati@axoflow.com>

--- receiver/awscloudwatchreceiver/integration_test.go
+++ receiver/awscloudwatchreceiver/integration_test.go
@@ -14,6 +14,7 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs"
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs/types"
+	"github.com/goccy/go-json"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component/componenttest"
