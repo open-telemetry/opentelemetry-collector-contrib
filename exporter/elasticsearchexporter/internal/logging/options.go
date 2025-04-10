025db51ab48e12957c6eed516ceed5c5a8991e89
Carson Ip <carson.ip@elastic.co>
2025-04-10 11:42:24 +0100

    goporto

--- exporter/elasticsearchexporter/internal/logging/options.go
+++ exporter/elasticsearchexporter/internal/logging/options.go
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 // Package logging contains utility functions for logging.
-package logging
+package logging // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/elasticsearchexporter/internal/logging"
 
 import (
 	"math"
