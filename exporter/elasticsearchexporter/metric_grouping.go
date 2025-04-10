aa815afa235c964b2e11c6848ec2fb6baa6eb167
Carson Ip <carson.ip@elastic.co>
2025-04-10 20:24:59 +0100

    Ignore error from FromRaw

--- exporter/elasticsearchexporter/metric_grouping.go
+++ exporter/elasticsearchexporter/metric_grouping.go
@@ -63,7 +63,7 @@ func (h ecsDataPointHasher) hashCombined(resource pcommon.Resource, _ pcommon.In
 	resource.Attributes().CopyTo(merged)
 	// scope attributes are ignored in ECS mode
 	for k, v := range dp.Attributes().All() {
-		merged.PutEmpty(k).FromRaw(v.AsRaw())
+		_ = merged.PutEmpty(k).FromRaw(v.AsRaw())
 	}
 
 	hasher := fnv.New32a()
