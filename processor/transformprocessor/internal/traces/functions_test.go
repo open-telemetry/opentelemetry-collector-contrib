0dcde12a1600b7b20fa92acceda83a856666c2f8
Francisco Valente <1435136+franciscovalentecastro@users.noreply.github.com>
2025-04-17 17:40:33 +0000

    [processor/transform] Create `additional*Funcs` parameters for NewProcessor and *Functions methods.

--- processor/transformprocessor/internal/traces/functions_test.go
+++ processor/transformprocessor/internal/traces/functions_test.go
@@ -4,11 +4,13 @@
 package traces
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/ottlspan"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/ottlspanevent"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/ottlfuncs"
@@ -18,7 +20,36 @@ func Test_SpanFunctions(t *testing.T) {
 	expected := ottlfuncs.StandardFuncs[ottlspan.TransformContext]()
 	isRootSpanFactory := ottlfuncs.NewIsRootSpanFactory()
 	expected[isRootSpanFactory.Name()] = isRootSpanFactory
-	actual := SpanFunctions()
+	actual := SpanFunctions([]ottl.Factory[ottlspan.TransformContext]{})
+	require.Len(t, actual, len(expected))
+	for k := range actual {
+		assert.Contains(t, expected, k)
+	}
+}
+
+type TestSpanFuncArguments[K any] struct{}
+
+func NewTestSpanFuncFactory[K any]() ottl.Factory[K] {
+	return ottl.NewFactory("TestSpanFunc", &TestSpanFuncArguments[K]{}, createTestSpanFunc[K])
+}
+
+func createTestSpanFunc[K any](_ ottl.FunctionContext, _ ottl.Arguments) (ottl.ExprFunc[K], error) {
+	return func(_ context.Context, _ K) (any, error) {
+		return nil, nil
+	}, nil
+}
+
+func Test_SpanFunctions_AdditionalSpanFuncs(t *testing.T) {
+	testSpanFuncFactory := NewTestSpanFuncFactory[ottlspan.TransformContext]()
+
+	expected := ottlfuncs.StandardFuncs[ottlspan.TransformContext]()
+	expected[testSpanFuncFactory.Name()] = testSpanFuncFactory
+	isRootSpanFactory := ottlfuncs.NewIsRootSpanFactory()
+	expected[isRootSpanFactory.Name()] = isRootSpanFactory
+
+	additionalSpanFuncs := []ottl.Factory[ottlspan.TransformContext]{testSpanFuncFactory}
+	actual := SpanFunctions(additionalSpanFuncs)
+
 	require.Len(t, actual, len(expected))
 	for k := range actual {
 		assert.Contains(t, expected, k)
