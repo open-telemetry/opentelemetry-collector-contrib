d5bc2f86d581b23e70149a279e9876865cfd0cce
Peter Nguyen <petern0408@gmail.com>
2025-04-24 08:51:46 -0700

    gofmt

--- exporter/azureblobexporter/exporter_test.go
+++ exporter/azureblobexporter/exporter_test.go
@@ -168,12 +168,12 @@ func TestGenerateBlobNameSerialNumBefore(t *testing.T) {
 
 	c := &Config{
 		BlobNameFormat: &BlobNameFormat{
-			MetricsFormat:  "2006/01/02/metrics_15_04_05.json",
-			LogsFormat:     "2006/01/02/logs_15_04_05.json",
-			TracesFormat:   "2006/01/02/traces_15_04_05",  // no extension
-			SerialNumRange: 10000,
+			MetricsFormat:            "2006/01/02/metrics_15_04_05.json",
+			LogsFormat:               "2006/01/02/logs_15_04_05.json",
+			TracesFormat:             "2006/01/02/traces_15_04_05", // no extension
+			SerialNumRange:           10000,
 			SerialNumBeforeExtension: true,
-			Params:         map[string]string{},
+			Params:                   map[string]string{},
 		},
 	}
 
