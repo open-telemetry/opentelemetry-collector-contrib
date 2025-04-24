e365bf2f999d7e15bb1562b604561fdc74f0f3c9
Tomas Pica <tomas.picadelcanto@grafana.com>
2025-04-24 12:19:25 +0200

    lint / headers

--- receiver/faroreceiver/receiver.go
+++ receiver/faroreceiver/receiver.go
@@ -1,3 +1,4 @@
+// Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
 package faroreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/faroreceiver"
