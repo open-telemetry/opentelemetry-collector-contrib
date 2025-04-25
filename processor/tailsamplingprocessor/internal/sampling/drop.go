6f2862b291b1cae93e7d6edfd47ca89b023eb186
Sean Porter <portertech@gmail.com>
2025-04-25 12:48:10 -0700

    drop policy type
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- processor/tailsamplingprocessor/internal/sampling/drop.go
+++ processor/tailsamplingprocessor/internal/sampling/drop.go
@@ -10,26 +10,27 @@ import (
 	"go.uber.org/zap"
 )
 
-type And struct {
+type Drop struct {
 	// the subpolicy evaluators
 	subpolicies []PolicyEvaluator
 	logger      *zap.Logger
 }
 
-func NewAnd(
+func NewDrop(
 	logger *zap.Logger,
 	subpolicies []PolicyEvaluator,
 ) PolicyEvaluator {
-	return &And{
+	return &Drop{
 		subpolicies: subpolicies,
 		logger:      logger,
 	}
 }
 
 // Evaluate looks at the trace data and returns a corresponding SamplingDecision.
-func (c *And) Evaluate(ctx context.Context, traceID pcommon.TraceID, trace *TraceData) (Decision, error) {
-	// The policy iterates over all sub-policies and returns Sampled if all sub-policies returned a Sampled Decision.
-	// If any subpolicy returns NotSampled or InvertNotSampled, it returns NotSampled Decision.
+func (c *Drop) Evaluate(ctx context.Context, traceID pcommon.TraceID, trace *TraceData) (Decision, error) {
+	// The policy iterates over all sub-policies and returns Dropped if all
+	// sub-policies returned a Sampled Decision. If any subpolicy returns
+	// NotSampled, it returns NotSampled Decision.
 	for _, sub := range c.subpolicies {
 		decision, err := sub.Evaluate(ctx, traceID, trace)
 		if err != nil {
@@ -39,5 +40,5 @@ func (c *And) Evaluate(ctx context.Context, traceID pcommon.TraceID, trace *Trac
 			return NotSampled, nil
 		}
 	}
-	return Sampled, nil
+	return Dropped, nil
 }
