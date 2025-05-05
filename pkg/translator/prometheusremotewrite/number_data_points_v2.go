5851fda35535494a625abf8676d70c360648088e
sujal shah <sujalshah28092004@gmail.com>
2025-05-05 17:43:21 +0530

    WIP
    related to: #33661.

--- pkg/translator/prometheusremotewrite/number_data_points_v2.go
+++ pkg/translator/prometheusremotewrite/number_data_points_v2.go
@@ -5,6 +5,7 @@ package prometheusremotewrite // import "github.com/open-telemetry/opentelemetry
 
 import (
 	"math"
+	"strconv"
 
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/model/timestamp"
@@ -77,6 +78,95 @@ func (c *prometheusConverterV2) addSumNumberDataPoints(dataPoints pmetric.Number
 		}
 		// TODO: properly add exemplars to the TimeSeries
 		c.addSample(sample, lbls)
+	}
+}
+
+// addHistogramDataPoints converts histogram metrics from OTLP to Prometheus remote write format
+func (c *prometheusConverterV2) addHistogramDataPoints(dataPoints pmetric.HistogramDataPointSlice,
+	resource pcommon.Resource, settings Settings, baseName string,
+) {
+	for x := 0; x < dataPoints.Len(); x++ {
+		pt := dataPoints.At(x)
+		timestamp := convertTimeStamp(pt.Timestamp())
+		baseLabels := createAttributes(resource, pt.Attributes(), settings.ExternalLabels, nil, false)
+
+		// If the sum is unset, it indicates the _sum metric point should be omitted
+		if pt.HasSum() {
+			// treat sum as a sample in an individual TimeSeries
+			sum := &writev2.Sample{
+				Value:     pt.Sum(),
+				Timestamp: timestamp,
+			}
+			if pt.Flags().NoRecordedValue() {
+				sum.Value = math.Float64frombits(value.StaleNaN)
+			}
+
+			sumlabels := createLabels(baseName+sumStr, baseLabels)
+			c.addSample(sum, sumlabels)
+		}
+
+		// treat count as a sample in an individual TimeSeries
+		count := &writev2.Sample{
+			Value:     float64(pt.Count()),
+			Timestamp: timestamp,
+		}
+		if pt.Flags().NoRecordedValue() {
+			count.Value = math.Float64frombits(value.StaleNaN)
+		}
+
+		countlabels := createLabels(baseName+countStr, baseLabels)
+		c.addSample(count, countlabels)
+
+		// cumulative count for conversion to cumulative histogram
+		var cumulativeCount uint64
+
+		var bucketBounds []bucketBoundsDataV2
+
+		// process each bound, based on histograms proto definition, # of buckets = # of explicit bounds + 1
+		for i := 0; i < pt.ExplicitBounds().Len() && i < pt.BucketCounts().Len(); i++ {
+			bound := pt.ExplicitBounds().At(i)
+			cumulativeCount += pt.BucketCounts().At(i)
+			bucket := &writev2.Sample{
+				Value:     float64(cumulativeCount),
+				Timestamp: timestamp,
+			}
+			if pt.Flags().NoRecordedValue() {
+				bucket.Value = math.Float64frombits(value.StaleNaN)
+			}
+			boundStr := strconv.FormatFloat(bound, 'f', -1, 64)
+			labels := createLabels(baseName+bucketStr, baseLabels, leStr, boundStr)
+
+			// Add the sample and store the signature for later exemplar lookup
+			c.addSample(bucket, labels)
+			signature := timeSeriesSignature(labels)
+
+			bucketBounds = append(bucketBounds, bucketBoundsDataV2{
+				signature: signature,
+				bound:     bound,
+			})
+		}
+
+		// add le=+Inf bucket
+		infBucket := &writev2.Sample{
+			Timestamp: timestamp,
+		}
+		if pt.Flags().NoRecordedValue() {
+			infBucket.Value = math.Float64frombits(value.StaleNaN)
+		} else {
+			infBucket.Value = float64(pt.Count())
+		}
+		infLabels := createLabels(baseName+bucketStr, baseLabels, leStr, pInfStr)
+
+		// Add the inf bucket sample and store its signature
+		c.addSample(infBucket, infLabels)
+		signature := timeSeriesSignature(infLabels)
+
+		bucketBounds = append(bucketBounds, bucketBoundsDataV2{
+			signature: signature,
+			bound:     math.Inf(1),
+		})
+
+		c.addExemplars(pt, bucketBounds)
 	}
 }
 
