99f6b585af85d7b42843e8b1d6fcadb2a149ecce
Tigran Najaryan <tnajaryan@splunk.com>
2025-05-08 10:10:27 -0400

    [stefexporter] Implement async exporting
    - Introduced 2 new helpers: ConnManager and Sync2Async.
      These can be used by any other async exporters in the
      future. For now they are in stefexpoter internal package,
      but they have no coupling with stefexporter and can be
      moved to a shared package and used by any exporter.
    
    - The previous implementation flushed STEF connection data
      on every export call and waited for acknowledgement.
      This limited the throughput and hurt compression ratios.
      The new implementation eliminates both of these
      limitations. Flushing is now done more rarely and
      acknowledgement waiting is now asynchronous, which allows
      more data to be written to the same STEF/gRPC stream
      while waiting for the ack to be received.

---
+++ exporter/stefexporter/internal/sync2async_test.go
@@ -0,0 +1,97 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package internal
+
+import (
+	"context"
+	"errors"
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+)
+
+func TestSync2Async(t *testing.T) {
+	logger, _ := zap.NewDevelopment()
+
+	var globalID DataID = 0
+	jobs := make(
+		chan struct {
+			data       any
+			dataID     DataID
+			resultChan ResultChan
+		},
+	)
+
+	var asyncMux sync.Mutex
+
+	async := func(
+		ctx context.Context,
+		data any,
+		resultChan ResultChan,
+	) (DataID, error) {
+		asyncMux.Lock()
+		globalID++
+		ackID := globalID
+		asyncMux.Unlock()
+
+		jobs <- struct {
+			data       any
+			dataID     DataID
+			resultChan ResultChan
+		}{
+			data:       data,
+			dataID:     ackID,
+			resultChan: resultChan,
+		}
+
+		return ackID, nil
+	}
+
+	// Perform the jobs in a separate goroutine.
+	go func() {
+		for job := range jobs {
+			var err error
+			if job.data.(int)%10 == 0 {
+				// Make it an error once in a while.
+				err = errors.New("some error")
+			}
+			job.resultChan <- AsyncResult{DataID: job.dataID, Err: err}
+		}
+	}()
+
+	const syncProducers = 100
+	s2a := NewSync2Async(logger, syncProducers, async)
+
+	ctx := context.Background()
+	var wg sync.WaitGroup
+	const countPerProducer = 100
+	const totalCount = syncProducers * countPerProducer
+	for i := 0; i < syncProducers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			data := i
+			for i := 0; i < countPerProducer; i++ {
+				err := s2a.DoSync(ctx, data)
+				if data%10 == 0 {
+					// Must be an error.
+					if err == nil {
+						require.Error(t, err)
+					}
+				} else {
+					// Must not be an error.
+					if err != nil {
+						require.NoError(t, err)
+					}
+				}
+			}
+		}()
+	}
+	wg.Wait()
+	close(jobs)
+
+	require.EqualValues(t, totalCount, globalID)
+}
