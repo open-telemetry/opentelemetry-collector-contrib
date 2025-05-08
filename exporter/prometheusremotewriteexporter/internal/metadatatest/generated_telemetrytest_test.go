27d7ce7f5ffab031ec32b54cf8596c7193715e73
Nikos Angelopoulos <nikolaos.angelopoulos@grafana.com>
2025-05-08 18:47:16 +0200

    fix: wal_test

--- exporter/prometheusremotewriteexporter/internal/metadatatest/generated_telemetrytest_test.go
+++ exporter/prometheusremotewriteexporter/internal/metadatatest/generated_telemetrytest_test.go
@@ -10,6 +10,8 @@ import (
 	"go.opentelemetry.io/otel/sdk/metric/metricdata"
 	"go.opentelemetry.io/otel/sdk/metric/metricdata/metricdatatest"
 
+	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/prometheusremotewriteexporter/internal/metadata"
+
 	"go.opentelemetry.io/collector/component/componenttest"
 )
 
