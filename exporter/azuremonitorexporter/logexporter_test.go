c9f881c50d91f7510b607fc842d685761f135ec4
hgaol <dhangao@hotmail.com>
2025-05-10 10:32:01 +0800

    downgrade semconv version to v1.12.0 in log_to_envelope and logexporter tests

--- exporter/azuremonitorexporter/logexporter_test.go
+++ exporter/azuremonitorexporter/logexporter_test.go
@@ -18,7 +18,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/plog"
-	conventions "go.opentelemetry.io/collector/semconv/v1.27.0"
+	conventions "go.opentelemetry.io/collector/semconv/v1.12.0"
 	"go.uber.org/zap"
 )
 
