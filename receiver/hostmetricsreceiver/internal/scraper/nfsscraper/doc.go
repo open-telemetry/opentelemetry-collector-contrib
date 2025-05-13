1a7852dfba5295b9989edc807b6ba531e007e00a
John Leslie <johnleslie@google.com>
2025-05-13 19:02:50 +0000

    Add skeleton for new hostmetrics scraper: nfsscraper
    nfsscraper will provide metrics for the OS NFS client and server.

---
+++ receiver/hostmetricsreceiver/internal/scraper/nfsscraper/doc.go
@@ -0,0 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+//go:generate mdatagen metadata.yaml
+
+package nfsscraper // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/hostmetricsreceiver/internal/scraper/nfsscraper"
---
+++ receiver/hostmetricsreceiver/internal/scraper/nfsscraper/factory.go
@@ -0,0 +1,45 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package nfsscraper // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/hostmetricsreceiver/internal/scraper/nfsscraper"
+
+import (
+	"context"
+	"errors"
+	"runtime"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/scraper"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/hostmetricsreceiver/internal/scraper/nfsscraper/internal/metadata"
+)
+
+// NewFactory for NFS scraper.
+func NewFactory() scraper.Factory {
+	return scraper.NewFactory(metadata.Type, createDefaultConfig, scraper.WithMetrics(createMetricsScraper, metadata.MetricsStability))
+}
+
+// createDefaultConfig creates the default configuration for the Scraper.
+func createDefaultConfig() component.Config {
+	return &Config{
+		MetricsBuilderConfig: metadata.DefaultMetricsBuilderConfig(),
+	}
+}
+
+// createMetricsScraper creates a resource scraper based on provided config.
+func createMetricsScraper(
+	ctx context.Context,
+	settings scraper.Settings,
+	cfg component.Config,
+) (scraper.Metrics, error) {
+	if runtime.GOOS != "linux" {
+		return nil, errors.New("uptime scraper only available on Linux")
+	}
+
+	nfsScraper := newNfsScraper(ctx, settings, cfg.(*Config))
+
+	return scraper.NewMetrics(
+		nfsScraper.scrape,
+		scraper.WithStart(nfsScraper.start),
+	)
+}
