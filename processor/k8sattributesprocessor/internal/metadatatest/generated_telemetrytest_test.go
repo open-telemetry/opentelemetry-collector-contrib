ee3b3bc21c43aaa9ae43f0c2d19b5edd30c19f6e
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-05-15 09:26:03 +0200

    generate

--- processor/k8sattributesprocessor/internal/metadatatest/generated_telemetrytest_test.go
+++ processor/k8sattributesprocessor/internal/metadatatest/generated_telemetrytest_test.go
@@ -10,6 +10,8 @@ import (
 	"go.opentelemetry.io/otel/sdk/metric/metricdata"
 	"go.opentelemetry.io/otel/sdk/metric/metricdata/metricdatatest"
 
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/k8sattributesprocessor/internal/metadata"
+
 	"go.opentelemetry.io/collector/component/componenttest"
 )
 
