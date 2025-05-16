7cdff38340304d8244e4a1d24ee7b0108650f39b
Nick Pordash <npordash@ebay.com>
2025-05-15 21:17:44 -0700

    remove test-scoped change of `model.NameValidationScheme` from `TestLabelNameLimitConfig` since it is deprecated

--- receiver/prometheusreceiver/metrics_receiver_labels_test.go
+++ receiver/prometheusreceiver/metrics_receiver_labels_test.go
@@ -6,7 +6,6 @@ package prometheusreceiver
 import (
 	"testing"
 
-	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/model/labels"
 	"github.com/prometheus/prometheus/model/relabel"
 	"github.com/stretchr/testify/require"
@@ -250,10 +249,6 @@ test_counter0{label1="value1",label2="value2"} 1
 `
 
 func TestLabelNameLimitConfig(t *testing.T) {
-	scheme := model.NameValidationScheme
-	model.NameValidationScheme = model.UTF8Validation
-	defer func() { model.NameValidationScheme = scheme }()
-
 	targets := []*testData{
 		{
 			name: "target1",
