1e55e96447991509ccfca36427cc17073d012de7
Francisco Valente <1435136+franciscovalentecastro@users.noreply.github.com>
2025-05-23 07:50:09 +0000

    [processor/transform] Create `With*Functions` factory options instead of `WithAdditional*Functions`. Create debug log.

--- processor/transformprocessor/functions_test.go
+++ processor/transformprocessor/functions_test.go
@@ -8,103 +8,49 @@ import (
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl"
 	"github.com/stretchr/testify/assert"
-	"go.uber.org/zap"
 )
 
-var (
-	DefaultFunctions = map[string]ottl.Factory[bool]{
-		"DefaultFunc1": createTestFuncFactory[bool]("DefaultFunc1"),
-		"DefaultFunc2": createTestFuncFactory[bool]("DefaultFunc2"),
-		"DefaultFunc3": createTestFuncFactory[bool]("DefaultFunc3"),
-	}
-)
+func Test_createFunctionsMap_EmptyFunctions(t *testing.T) {
+	functions := []ottl.Factory[bool]{}
+	expected := map[string]ottl.Factory[bool]{}
 
-func Test_mergeAdditionalFunctions_NoAdditionalFunctions(t *testing.T) {
-	defaultFunctions := DefaultFunctions
-	additionalFunctions := []ottl.Factory[bool]{}
+	actual := createFunctionsMap(functions)
 
-	expectedFunctions := map[string]ottl.Factory[bool]{
-		"DefaultFunc1": createTestFuncFactory[bool]("DefaultFunc1"),
-		"DefaultFunc2": createTestFuncFactory[bool]("DefaultFunc2"),
-		"DefaultFunc3": createTestFuncFactory[bool]("DefaultFunc3"),
-	}
-
-	actualFunctions := mergeAdditionalFunctions(defaultFunctions, additionalFunctions, zap.NewNop())
-	assert.Len(t, actualFunctions, len(expectedFunctions))
-	for k, v := range actualFunctions {
-		assert.Equal(t, v.Name(), expectedFunctions[k].Name())
+	for k, v := range actual {
+		assert.Equal(t, v.Name(), expected[k].Name())
 	}
 }
 
-func Test_mergeAdditionalFunctions_AddOneFunction(t *testing.T) {
-	defaultFunctions := DefaultFunctions
-	additionalFunctions := []ottl.Factory[bool]{createTestFuncFactory[bool]("TestFunc1")}
-
-	expectedFunctions := map[string]ottl.Factory[bool]{
+func Test_createFunctionsMap_OneFunction(t *testing.T) {
+	functions := []ottl.Factory[bool]{
+		createTestFuncFactory[bool]("DefaultFunc1"),
+	}
+	expected := map[string]ottl.Factory[bool]{
 		"DefaultFunc1": createTestFuncFactory[bool]("DefaultFunc1"),
-		"DefaultFunc2": createTestFuncFactory[bool]("DefaultFunc2"),
-		"DefaultFunc3": createTestFuncFactory[bool]("DefaultFunc3"),
-		"TestFunc1":    createTestFuncFactory[bool]("TestFunc1"),
 	}
 
-	actualFunctions := mergeAdditionalFunctions(defaultFunctions, additionalFunctions, zap.NewNop())
-	assert.Len(t, actualFunctions, len(expectedFunctions))
-	for k, v := range actualFunctions {
-		assert.Equal(t, v.Name(), expectedFunctions[k].Name())
+	actual := createFunctionsMap(functions)
+
+	for k, v := range actual {
+		assert.Equal(t, v.Name(), expected[k].Name())
 	}
 }
 
-func Test_mergeAdditionalFunctions_AddMultipleFunctions(t *testing.T) {
-	defaultFunctions := DefaultFunctions
-	additionalFunctions := []ottl.Factory[bool]{createTestFuncFactory[bool]("TestFunc1"), createTestFuncFactory[bool]("TestFunc2")}
-
-	expectedFunctions := map[string]ottl.Factory[bool]{
+func Test_createFunctionsMap_MultipleFunctions(t *testing.T) {
+	functions := []ottl.Factory[bool]{
+		createTestFuncFactory[bool]("DefaultFunc1"),
+		createTestFuncFactory[bool]("DefaultFunc2"),
+		createTestFuncFactory[bool]("DefaultFunc3"),
+	}
+	expected := map[string]ottl.Factory[bool]{
 		"DefaultFunc1": createTestFuncFactory[bool]("DefaultFunc1"),
 		"DefaultFunc2": createTestFuncFactory[bool]("DefaultFunc2"),
 		"DefaultFunc3": createTestFuncFactory[bool]("DefaultFunc3"),
-		"TestFunc1":    createTestFuncFactory[bool]("TestFunc1"),
-		"TestFunc2":    createTestFuncFactory[bool]("TestFunc2"),
 	}
 
-	actualFunctions := mergeAdditionalFunctions(defaultFunctions, additionalFunctions, zap.NewNop())
-	assert.Len(t, actualFunctions, len(expectedFunctions))
-	for k, v := range actualFunctions {
-		assert.Equal(t, v.Name(), expectedFunctions[k].Name())
-	}
-}
+	actual := createFunctionsMap(functions)
 
-func Test_mergeAdditionalFunctions_OverrideDefaultFunction(t *testing.T) {
-	defaultFunctions := DefaultFunctions
-	additionalFunctions := []ottl.Factory[bool]{createTestFuncFactory[bool]("TestFunc1"), createTestFuncFactory[bool]("DefaultFunc2")}
-
-	expectedFunctions := map[string]ottl.Factory[bool]{
-		"DefaultFunc1": createTestFuncFactory[bool]("DefaultFunc1"),
-		"DefaultFunc2": createTestFuncFactory[bool]("DefaultFunc2"),
-		"DefaultFunc3": createTestFuncFactory[bool]("DefaultFunc3"),
-		"TestFunc1":    createTestFuncFactory[bool]("TestFunc1"),
-	}
-
-	actualFunctions := mergeAdditionalFunctions(defaultFunctions, additionalFunctions, zap.NewNop())
-	assert.Len(t, actualFunctions, len(expectedFunctions))
-	for k, v := range actualFunctions {
-		assert.Equal(t, v.Name(), expectedFunctions[k].Name())
-	}
-}
-
-func Test_mergeAdditionalFunctions_DuplicateAdditionalFunctions(t *testing.T) {
-	defaultFunctions := DefaultFunctions
-	additionalFunctions := []ottl.Factory[bool]{createTestFuncFactory[bool]("TestFunc2"), createTestFuncFactory[bool]("TestFunc2")}
-
-	expectedFunctions := map[string]ottl.Factory[bool]{
-		"DefaultFunc1": createTestFuncFactory[bool]("DefaultFunc1"),
-		"DefaultFunc2": createTestFuncFactory[bool]("DefaultFunc2"),
-		"DefaultFunc3": createTestFuncFactory[bool]("DefaultFunc3"),
-		"TestFunc2":    createTestFuncFactory[bool]("TestFunc2"),
-	}
-
-	actualFunctions := mergeAdditionalFunctions(defaultFunctions, additionalFunctions, zap.NewNop())
-	assert.Len(t, actualFunctions, len(expectedFunctions))
-	for k, v := range actualFunctions {
-		assert.Equal(t, v.Name(), expectedFunctions[k].Name())
+	for k, v := range actual {
+		assert.Equal(t, v.Name(), expected[k].Name())
 	}
 }
