15c7ff6c5f1f8fec4c47f5565c29f87e63ab8477
Michael Burt <mburt@splunk.com>
2025-05-27 23:42:15 -0600

    use errors.Is to confirm file exists

--- receiver/tlscheckreceiver/scraper.go
+++ receiver/tlscheckreceiver/scraper.go
@@ -84,16 +84,18 @@ func validateEndpoint(endpoint string) error {
 
 func validateFilepath(filePath string) error {
 	cleanPath := filepath.Clean(filePath)
+	if !filepath.IsAbs(cleanPath) {
+		return fmt.Errorf("file path must be absolute: %s", filePath)
+	}
+
 	fileInfo, err := os.Stat(cleanPath)
 	if err != nil {
-		if os.IsNotExist(err) {
+		if errors.Is(err, os.ErrNotExist) {
 			return fmt.Errorf("certificate file does not exist: %s", filePath)
 		}
 		return fmt.Errorf("error accessing certificate file %s: %w", filePath, err)
 	}
-	if !filepath.IsAbs(cleanPath) {
-		return fmt.Errorf("file path must be absolute: %s", filePath)
-	}
+
 	if fileInfo.IsDir() {
 		return fmt.Errorf("path is a directory, not a file: %s", cleanPath)
 	}
