5f2c534dea788948d4aaa0c5b17062087ac16bdc
Juraj Michalek <juraj.michalek132@gmail.com>
2025-06-04 20:40:46 +0200

    chore: prom translation rw2 add support for target_info

--- pkg/translator/prometheusremotewrite/metrics_to_prw_v2.go
+++ pkg/translator/prometheusremotewrite/metrics_to_prw_v2.go
@@ -111,8 +111,7 @@ func (c *prometheusConverterV2) fromMetrics(md pmetric.Metrics, settings Setting
 				}
 			}
 		}
-		// TODO implement
-		// addResourceTargetInfov2(resource, settings, mostRecentTimestamp, c)
+		addResourceTargetInfoV2(resource, settings, mostRecentTimestamp, c)
 	}
 
 	return
