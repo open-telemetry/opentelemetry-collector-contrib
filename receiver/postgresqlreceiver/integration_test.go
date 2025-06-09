03f8ad0aee8a33e067c6f1ac820cd143ebcfac5b
Chao <chao.weng@appdynamics.com>
2025-06-09 17:49:41 +0800

    fix

--- receiver/postgresqlreceiver/integration_test.go
+++ receiver/postgresqlreceiver/integration_test.go
@@ -185,6 +185,7 @@ func TestScrapeLogsFromContainer(t *testing.T) {
 		TopQueryCollection: TopQueryCollection{
 			Enabled: true,
 		},
+		LogsBuilderConfig: metadata.DefaultLogsBuilderConfig(),
 	}
 	clientFactory := newDefaultClientFactory(&cfg)
 
