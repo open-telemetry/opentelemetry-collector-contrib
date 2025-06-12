9c7d9884f8f938c6d86293108b7ca7a073aae432
Juraj Michalek <juraj.michalek132@gmail.com>
2025-06-12 21:49:18 +0200

    chore: finish resolving conflict after merge from main

--- pkg/translator/prometheusremotewrite/helper_v2.go
+++ pkg/translator/prometheusremotewrite/helper_v2.go
@@ -7,11 +7,67 @@ import (
 	"math"
 	"strconv"
 
+	prometheustranslator "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/translator/prometheus"
 	"github.com/prometheus/prometheus/model/value"
 	writev2 "github.com/prometheus/prometheus/prompb/io/prometheus/write/v2"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/pmetric"
 )
+
+// addResourceTargetInfoV2 converts the resource to the target info metric.
+func (c *prometheusConverterV2) addResourceTargetInfoV2(resource pcommon.Resource, settings Settings, timestamp pcommon.Timestamp) {
+	if settings.DisableTargetInfo || timestamp == 0 {
+		return
+	}
+
+	attributes := resource.Attributes()
+	identifyingAttrs := []string{
+		string(conventions.ServiceNamespaceKey),
+		string(conventions.ServiceNameKey),
+		string(conventions.ServiceInstanceIDKey),
+	}
+	nonIdentifyingAttrsCount := attributes.Len()
+	for _, a := range identifyingAttrs {
+		_, haveAttr := attributes.Get(a)
+		if haveAttr {
+			nonIdentifyingAttrsCount--
+		}
+	}
+	if nonIdentifyingAttrsCount == 0 {
+		// If we only have job + instance, then target_info isn't useful, so don't add it.
+		return
+	}
+
+	name := prometheustranslator.TargetInfoMetricName
+	if len(settings.Namespace) > 0 {
+		// TODO what to do with this in case of full utf-8 support?
+		name = settings.Namespace + "_" + name
+	}
+
+	labels := createAttributes(resource, attributes, settings.ExternalLabels, identifyingAttrs, false, model.MetricNameLabel, name)
+	haveIdentifier := false
+	for _, l := range labels {
+		if l.Name == model.JobLabel || l.Name == model.InstanceLabel {
+			haveIdentifier = true
+			break
+		}
+	}
+
+	if !haveIdentifier {
+		// We need at least one identifying label to generate target_info.
+		return
+	}
+
+	sample := &writev2.Sample{
+		Value: float64(1),
+		// convert ns to ms
+		Timestamp: convertTimeStamp(timestamp),
+	}
+	c.addSample(sample, labels, metadata{
+		Type: writev2.Metadata_METRIC_TYPE_GAUGE,
+		Help: "Target metadata",
+	})
+}
 
 func (c *prometheusConverterV2) addHistogramDataPoints(dataPoints pmetric.HistogramDataPointSlice,
 	resource pcommon.Resource, settings Settings, baseName string, metadata metadata,
