f92a5763be13aeb59ecf6f8b47e31d54109191bb
Spencer Torres <contact@spencertorres.com>
2025-06-16 15:30:58 -0400

    [chore] [exporter/clickhouseexporter] full refactor for ClickHouse component (#40536)
    #### Description
    
    This PR intends to reorganize the code of the exporter without making
    any user-facing changes to the behavior or config. The goal is to make
    it more maintainable and revise all previous changes into one cohesive
    component.
    
    Apologies in advance for the many changes, but I wanted this to be
    runnable so we can merge it into main. **Let me know if we need to split
    anything out, or make any changes.**
    
    Notable changes:
    - All `clickhouse-go` database calls now use the library's native Go
    interface instead of the `database/sql` interface. This is more
    efficient and easier to configure + test. There was a lot of overhead in
    converting from `database/sql` to the driver's native structures, so
    this has a performance benefit as well as more idiomatic code.
    - Files have been reorganized so that code is in a more logical and easy
    to find place. (For example, there were some shared functions inside the
    `exporter_logs.go` file, even though it was unrelated to logs)
    - SQL is no longer written inside the `.go` file, and is now using
    `go:embed` to embed it from a directory of files.
    - Moved generic code to the `internal` package to simplify the top level
    package's exports.
    - Optimized insert code for logs/traces to be more CPU efficient (there
    were some redundant calls for converting attributes to `map`, wasting
    CPU).
    
    See testing section below for notes on test changes.
    
    #### Testing
    
    - Mocked database tests were replaced with full integration tests for
    logs, traces, and metrics.
    - Integration test share the same test container, ensuring quicker
    testing (no more starting several containers for simple query tests)
    - Preserved `goleak` checks for integration tests (and unit tests)
    - Integration tests were split into multiple files (these are in
    `*_integration_test.go` files)
    - Unit tests are separate from the integration tests (these are in the
    usual `_test.go` files)
    - Added tests for new functions/cases
    - Removed irrelevant/unhelpful tests

--- exporter/clickhouseexporter/internal/metrics/metrics_model_test.go
+++ exporter/clickhouseexporter/internal/metrics/metrics_model_test.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package internal
+package metrics
 
 import (
 	"testing"
--- exporter/clickhouseexporter/internal/sqltemplates/traces_table.sql
+++ exporter/clickhouseexporter/internal/sqltemplates/traces_table.sql
@@ -1,7 +1,5 @@
--- Default Trace table DDL
-
-CREATE TABLE IF NOT EXISTS otel_traces (
-    Timestamp DateTime64(9) CODEC(Delta, ZSTD(1)),
+CREATE TABLE IF NOT EXISTS "%s"."%s" %s (
+                                     Timestamp DateTime64(9) CODEC(Delta, ZSTD(1)),
     TraceId String CODEC(ZSTD(1)),
     SpanId String CODEC(ZSTD(1)),
     ParentSpanId String CODEC(ZSTD(1)),
@@ -17,15 +15,15 @@ CREATE TABLE IF NOT EXISTS otel_traces (
     StatusCode LowCardinality(String) CODEC(ZSTD(1)),
     StatusMessage String CODEC(ZSTD(1)),
     Events Nested (
-        Timestamp DateTime64(9),
-        Name LowCardinality(String),
-        Attributes Map(LowCardinality(String), String)
+                      Timestamp DateTime64(9),
+    Name LowCardinality(String),
+    Attributes Map(LowCardinality(String), String)
     ) CODEC(ZSTD(1)),
     Links Nested (
-        TraceId String,
-        SpanId String,
-        TraceState String,
-        Attributes Map(LowCardinality(String), String)
+                     TraceId String,
+                     SpanId String,
+                     TraceState String,
+                     Attributes Map(LowCardinality(String), String)
     ) CODEC(ZSTD(1)),
     INDEX idx_trace_id TraceId TYPE bloom_filter(0.001) GRANULARITY 1,
     INDEX idx_res_attr_key mapKeys(ResourceAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
@@ -33,31 +31,8 @@ CREATE TABLE IF NOT EXISTS otel_traces (
     INDEX idx_span_attr_key mapKeys(SpanAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
     INDEX idx_span_attr_value mapValues(SpanAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
     INDEX idx_duration Duration TYPE minmax GRANULARITY 1
-) ENGINE = MergeTree()
-PARTITION BY toDate(Timestamp)
-ORDER BY (ServiceName, SpanName, toDateTime(Timestamp))
-TTL toDate(Timestamp) + toIntervalDay(180)
-SETTINGS index_granularity=8192, ttl_only_drop_parts = 1;
-
-
-CREATE TABLE IF NOT EXISTS otel_traces_trace_id_ts (
-     TraceId String CODEC(ZSTD(1)),
-     Start DateTime CODEC(Delta, ZSTD(1)),
-     End DateTime CODEC(Delta, ZSTD(1)),
-     INDEX idx_trace_id TraceId TYPE bloom_filter(0.01) GRANULARITY 1
-) ENGINE = MergeTree()
-PARTITION BY toDate(Start)
-ORDER BY (TraceId, Start)
-TTL toDate(Start) + toIntervalDay(180)
-SETTINGS index_granularity=8192, ttl_only_drop_parts = 1;
-
-
-CREATE MATERIALIZED VIEW IF NOT EXISTS otel_traces_trace_id_ts_mv
-TO otel_traces_trace_id_ts
-AS SELECT
-    TraceId,
-    min(Timestamp) as Start,
-    max(Timestamp) as End
-FROM otel_traces
-WHERE TraceId != ''
-GROUP BY TraceId;
+    ) ENGINE = %s
+    PARTITION BY toDate(Timestamp)
+    ORDER BY (ServiceName, SpanName, toDateTime(Timestamp))
+    %s
+    SETTINGS index_granularity=8192, ttl_only_drop_parts = 1
