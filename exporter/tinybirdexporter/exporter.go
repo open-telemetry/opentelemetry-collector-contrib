b5ff2826add5514f22c4ece2f8ccddeaed6b49d3
Jordi Vilaseca <jvilaseca@tinybird.co>
2025-06-16 16:11:57 +0200

    [exporter/tinybird] fix linter issues

--- exporter/tinybirdexporter/exporter.go
+++ exporter/tinybirdexporter/exporter.go
@@ -16,22 +16,22 @@ import (
 
 type tinybirdExporter struct{}
 
-func newExporter(cfg component.Config, set exporter.Settings) (*tinybirdExporter, error) {
+func newExporter(_ component.Config, _ exporter.Settings) (*tinybirdExporter, error) {
 	return &tinybirdExporter{}, nil
 }
 
-func (e *tinybirdExporter) start(ctx context.Context, host component.Host) error {
+func (e *tinybirdExporter) start(_ context.Context, _ component.Host) error {
 	return nil
 }
 
-func (e *tinybirdExporter) pushTraces(ctx context.Context, td ptrace.Traces) error {
+func (e *tinybirdExporter) pushTraces(_ context.Context, _ ptrace.Traces) error {
 	return errors.New("this component is under development and traces are not yet supported, see https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/40475 to track development progress")
 }
 
-func (e *tinybirdExporter) pushMetrics(ctx context.Context, md pmetric.Metrics) error {
+func (e *tinybirdExporter) pushMetrics(_ context.Context, _ pmetric.Metrics) error {
 	return errors.New("this component is under development and metrics are not yet supported, see https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/40475 to track development progress")
 }
 
-func (e *tinybirdExporter) pushLogs(ctx context.Context, ld plog.Logs) error {
+func (e *tinybirdExporter) pushLogs(_ context.Context, _ plog.Logs) error {
 	return errors.New("this component is under development and logs are not yet supported, see https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/40475 to track development progress")
 }
