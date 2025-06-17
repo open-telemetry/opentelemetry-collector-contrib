9c19e64539d8002555440d964de73657af06ea90
Justin Voss <jmanvoss1@gmail.com>
2025-06-17 12:24:15 -0500

    add copyright notice

--- internal/sqlquery/driver.go
+++ internal/sqlquery/driver.go
@@ -1,3 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
 package sqlquery // import "github.com/open-telemetry/opentelemetry-collector-contrib/internal/sqlquery"
 
 const (
