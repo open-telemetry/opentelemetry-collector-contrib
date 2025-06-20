e4e85c85ce2450517bef183517c0924d9ae8e348
braydonk <braydonk@google.com>
2025-06-20 12:30:16 +0000

    fix broken test and lint errors

--- pkg/stanza/operator/transformer/remove/transformer_test.go
+++ pkg/stanza/operator/transformer/remove/transformer_test.go
@@ -209,7 +209,8 @@ func TestProcessAndBuild(t *testing.T) {
 			"remove_resource",
 			func() *Config {
 				cfg := NewConfig()
-				cfg.Field.UnmarshalText([]byte("resource"))
+				err := cfg.Field.UnmarshalText([]byte("resource"))
+				require.NoError(t, err, "UnmarshalText failed in test setup code: %v", err)
 				return cfg
 			}(),
 			func() *entry.Entry {
@@ -230,7 +231,8 @@ func TestProcessAndBuild(t *testing.T) {
 			"remove_attributes",
 			func() *Config {
 				cfg := NewConfig()
-				cfg.Field.UnmarshalText([]byte("attributes"))
+				err := cfg.Field.UnmarshalText([]byte("attributes"))
+				require.NoError(t, err, "UnmarshalText failed in test setup code: %v", err)
 				return cfg
 			}(),
 			func() *entry.Entry {
