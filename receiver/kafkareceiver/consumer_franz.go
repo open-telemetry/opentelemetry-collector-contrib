4effc8fb60a4e241332f96b1db0b3aeb1d0e0a7f
Vishal Raj <vishal.raj@elastic.co>
2025-06-26 16:52:39 +0100

    Protect against OnPartitionRevoke too

--- receiver/kafkareceiver/consumer_franz.go
+++ receiver/kafkareceiver/consumer_franz.go
@@ -377,12 +377,14 @@ func (c *franzConsumer) lost(ctx context.Context, _ *kgo.Client,
 	for topic, partitions := range lost {
 		for _, partition := range partitions {
 			tp := topicPartition{topic: topic, partition: partition}
-			pc, ok := c.assignments[tp]
-			// For fatal true, called from OnPartitionLost, we need to ensure
-			// that the assignments exist as OnPartitionLost is called on any
-			// fatal group errors and thus it is possible that this method is
-			// called without the group ever joining and getting assigned.
-			if !fatal || (fatal && ok) {
+			// In some cases, it is possible for the `lost` to be called with
+			// no assignments. So, check if assignments exists first.
+			//
+			// - OnPartitionLost can be called without the group ever joining
+			// and getting assigned.
+			// - OnPartitionRevoked can be called multiple times for cooperative
+			// balancer on topic lost/deleted.
+			if pc, ok := c.assignments[tp]; ok {
 				delete(c.assignments, tp)
 				pc.cancel(errors.New(
 					"stopping processing: partition reassigned or lost",
@@ -392,10 +394,10 @@ func (c *franzConsumer) lost(ctx context.Context, _ *kgo.Client,
 					defer wg.Done()
 					pc.wg.Wait()
 				}()
+				c.telemetryBuilder.KafkaReceiverPartitionClose.Add(context.Background(),
+					1, metric.WithAttributes(attribute.String(attrInstanceName, c.id.Name())),
+				)
 			}
-			c.telemetryBuilder.KafkaReceiverPartitionClose.Add(context.Background(),
-				1, metric.WithAttributes(attribute.String(attrInstanceName, c.id.Name())),
-			)
 		}
 	}
 	if fatal {
