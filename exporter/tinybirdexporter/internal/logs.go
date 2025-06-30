25bf8adb535bb199c2ca1e89fabb86930148dc98
Jordi Vilaseca <jvilaseca@tinybird.co>
2025-06-30 16:53:31 +0200

    [exporter/tinybird] add license to new files

--- exporter/tinybirdexporter/internal/logs.go
+++ exporter/tinybirdexporter/internal/logs.go
@@ -1,3 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
 package internal // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/tinybirdexporter/internal"
 
 import (
