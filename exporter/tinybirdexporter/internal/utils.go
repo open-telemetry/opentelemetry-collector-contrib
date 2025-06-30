a41016f4b6e1fdd521f76c2e7fca151531b1bc9d
Jordi Vilaseca <jvilaseca@tinybird.co>
2025-06-30 14:51:26 +0200

    [exporter/tinybird] implement logs

---
+++ exporter/tinybirdexporter/internal/utils.go
@@ -0,0 +1,24 @@
+package internal // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/tinybirdexporter/internal"
+
+import (
+	"go.opentelemetry.io/collector/pdata/pcommon"
+
+	conventions "go.opentelemetry.io/otel/semconv/v1.27.0"
+)
+
+func getServiceName(resAttr pcommon.Map) string {
+	if v, ok := resAttr.Get(string(conventions.ServiceNameKey)); ok {
+		return v.AsString()
+	}
+
+	return ""
+}
+
+func convertAttributes(attributes pcommon.Map) map[string]string {
+	attrs := make(map[string]string, attributes.Len())
+	attributes.Range(func(k string, v pcommon.Value) bool {
+		attrs[k] = v.AsString()
+		return true
+	})
+	return attrs
+}
