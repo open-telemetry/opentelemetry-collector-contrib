a3622bbe8d7b61bae86436bdd88c6dd9799d6af8
David Ashpole <dashpole@google.com>
2025-07-01 16:57:28 +0000

    add start_time_metric strategy to the metricstarttime processor

--- processor/metricstarttimeprocessor/internal/testhelper/util.go
+++ processor/metricstarttimeprocessor/internal/testhelper/util.go
@@ -172,6 +172,39 @@ func ExponentialHistogramPointNoValue(attributes []*KV, startTimestamp, timestam
 	return hdp
 }
 
+// exponentialHistogramPointSimplified let's you define an exponential
+// histogram with just a few parameters.
+// Scale and ZeroCount are set to the provided values.
+// Positive and negative buckets are generated using the offset and bucketCount
+// parameters by adding buckets from offset in both positive and negative
+// directions. Bucket counts start from 1 and increase by 1 for each bucket.
+// Sum and Count will be proportional to the bucket count.
+func ExponentialHistogramPointSimplified(attributes []*KV, startTimestamp, timestamp pcommon.Timestamp, scale int32, zeroCount uint64, offset int32, bucketCount int) pmetric.ExponentialHistogramDataPoint {
+	hdp := ExponentialHistogramPointRaw(attributes, startTimestamp, timestamp)
+	hdp.SetScale(scale)
+	hdp.SetZeroCount(zeroCount)
+
+	positive := hdp.Positive()
+	positive.SetOffset(offset)
+	positive.BucketCounts().EnsureCapacity(bucketCount)
+	negative := hdp.Negative()
+	negative.SetOffset(offset)
+	negative.BucketCounts().EnsureCapacity(bucketCount)
+
+	var sum float64
+	var count uint64
+	for i := 0; i < bucketCount; i++ {
+		positive.BucketCounts().Append(uint64(i + 1))
+		negative.BucketCounts().Append(uint64(i + 1))
+		count += uint64(i+1) + uint64(i+1)
+		sum += float64(i+1)*10 + float64(i+1)*10.0
+	}
+	hdp.SetCount(count)
+	hdp.SetSum(sum)
+
+	return hdp
+}
+
 func DoublePointRaw(attributes []*KV, startTimestamp, timestamp pcommon.Timestamp) pmetric.NumberDataPoint {
 	ndp := pmetric.NewNumberDataPoint()
 	ndp.SetStartTimestamp(startTimestamp)
