b5c6d1413d261fe200eea519519c1de706e0365e
John Leslie <johnleslie@google.com>
2025-07-02 22:55:39 +0000

    remove util.go because linters complain when its unused on non-linux :/

--- receiver/hostmetricsreceiver/internal/scraper/nfsscraper/nfs_scraper_linux.go
+++ receiver/hostmetricsreceiver/internal/scraper/nfsscraper/nfs_scraper_linux.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -537,4 +538,19 @@ func parseNfsdStats(f io.Reader) (*NfsdStats, error) {
 	}
 
 	return nfsdStats, nil
+}
+
+// parseStringsToUint64s parses a slice of strings into a slice of uint64
+func parseStringsToUint64s(strSlice []string) (*[]uint64, error) {
+	uint64Slice := make([]uint64, 0, len(strSlice))
+
+	for _, s := range strSlice {
+		val, err := strconv.ParseUint(s, 10, 64) // Assuming decimal base and uint64
+		if err != nil {
+			return nil, fmt.Errorf("failed to convert string '%s' to uint64: %w", s, err)
+		}
+		uint64Slice = append(uint64Slice, val)
+	}
+
+	return &uint64Slice, nil
 }
