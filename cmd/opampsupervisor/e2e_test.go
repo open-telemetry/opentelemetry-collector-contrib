3ced0a527b4648dd9678a8bf832f3215b13a6d01
Florian Bacher <florian.bacher@dynatrace.com>
2025-07-07 08:46:49 +0200

    fix failing tests
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- cmd/opampsupervisor/e2e_test.go
+++ cmd/opampsupervisor/e2e_test.go
@@ -45,7 +45,6 @@ import (
 	"github.com/open-telemetry/opamp-go/server/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"go.opentelemetry.io/collector/pdata/ptrace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -54,7 +53,6 @@ import (
 	"github.com/open-telemetry/opentelemetry-collector-contrib/cmd/opampsupervisor/supervisor"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/cmd/opampsupervisor/supervisor/config"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/cmd/opampsupervisor/supervisor/telemetry"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/testbed/testbed"
 )
 
 // getTestModes returns the test modes for the supervisor tests.
