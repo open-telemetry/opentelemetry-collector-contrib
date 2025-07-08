bea72e4f61aa3e6e6c553078fbc74f0465e1cfed
Will Li <cuichen.li94@gmail.com>
2025-07-08 16:00:22 +0800

    [chore][receiver/sqlserver] provide one generic configuration in readme (#40851)
    <!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
    Ex. Adding a feature - Explain what this achieves.-->
    #### Description
    
    we now have many different configurations but the current readme does
    not have one generic readme that user can use directly.
    
    <!-- Issue number (e.g. #1234) or full URL to issue, if applicable. -->
    #### Link to tracking issue
    Fixes
    
    <!--Describe what testing was performed and which tests were added.-->
    #### Testing
    
    <!--Describe the documentation added.-->
    #### Documentation
    
    <!--Please delete paragraphs that you did not use before submitting.-->
    /cc @natalie-arsky
    
    ---------
    
    Co-authored-by: Curtis Robert <crobert@splunk.com>
    Co-authored-by: Chao Weng <19381524+sincejune@users.noreply.github.com>

--- receiver/sqlserverreceiver/config_test.go
+++ receiver/sqlserverreceiver/config_test.go
@@ -6,6 +6,7 @@ package sqlserverreceiver
 import (
 	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
@@ -165,7 +166,14 @@ func TestLoadConfig(t *testing.T) {
 			},
 		}
 		expected.LogsBuilderConfig = metadata.LogsBuilderConfig{
-			Events: metadata.DefaultEventsConfig(),
+			Events: metadata.EventsConfig{
+				DbServerQuerySample: metadata.EventConfig{
+					Enabled: true,
+				},
+				DbServerTopQuery: metadata.EventConfig{
+					Enabled: true,
+				},
+			},
 			ResourceAttributes: metadata.ResourceAttributesConfig{
 				HostName: metadata.ResourceAttributeConfig{
 					Enabled: true,
@@ -192,6 +200,7 @@ func TestLoadConfig(t *testing.T) {
 		expected.LookbackTime = 60
 		expected.TopQueryCount = 200
 		expected.MaxQuerySampleCount = 1000
+		expected.TopQueryCollection.CollectionInterval = 80 * time.Second
 
 		expected.QuerySample = QuerySample{
 			MaxRowsPerQuery: 1450,
