2d7b77d25db8b27e097f6ca6c76ebb9932061c68
Florian Lehner <florian.lehner@elastic.co>
2025-07-11 14:56:41 +0200

    ottl: add profilesample
    The OTel Profiling signal contains multiple submessages to the message Profile. Sample is the first submessage to Profile.
    
    Fixes #40161
    
    Signed-off-by: Florian Lehner <florian.lehner@elastic.co>

---
+++ pkg/ottl/contexts/internal/ctxprofilesample/profilesample.go
@@ -0,0 +1,115 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package ctxprofilesample // import "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/internal/ctxprofilesample"
+
+import (
+	"context"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/internal/ctxerror"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/internal/ctxutil"
+
+	"go.opentelemetry.io/collector/pdata/pprofile"
+)
+
+type ProfileSampleContext interface {
+	GetProfileSample() pprofile.Sample
+}
+
+func PathGetSetter[K ProfileSampleContext](path ottl.Path[K]) (ottl.GetSetter[K], error) {
+	if path == nil {
+		return nil, ctxerror.New("nil", "nil", Name, DocRef)
+	}
+	switch path.Name() {
+	case "locations_start_index":
+		return accessLocationsStartIndex[K](), nil
+	case "locations_length":
+		return accessLocationsLength[K](), nil
+	case "values":
+		return accessValues[K](), nil
+	case "attribute_indices":
+		return accessAttributeIndices[K](), nil
+	case "link_index":
+		return accessLinkIndex[K](), nil
+	case "timestamps_unix_nano":
+		return accessTimestampsUnixNano[K](), nil
+	default:
+		return nil, ctxerror.New(path.Name(), path.String(), Name, DocRef)
+	}
+}
+
+func accessLocationsStartIndex[K ProfileSampleContext]() ottl.StandardGetSetter[K] {
+	return ottl.StandardGetSetter[K]{
+		Getter: func(_ context.Context, tCtx K) (any, error) {
+			return tCtx.GetProfileSample().LocationsStartIndex(), nil
+		},
+		Setter: func(_ context.Context, tCtx K, val any) error {
+			if v, ok := val.(int32); ok {
+				tCtx.GetProfileSample().SetLocationsStartIndex(v)
+			}
+			return nil
+		},
+	}
+}
+
+func accessLocationsLength[K ProfileSampleContext]() ottl.StandardGetSetter[K] {
+	return ottl.StandardGetSetter[K]{
+		Getter: func(_ context.Context, tCtx K) (any, error) {
+			return tCtx.GetProfileSample().LocationsLength(), nil
+		},
+		Setter: func(_ context.Context, tCtx K, val any) error {
+			if v, ok := val.(int32); ok {
+				tCtx.GetProfileSample().SetLocationsLength(v)
+			}
+			return nil
+		},
+	}
+}
+
+func accessValues[K ProfileSampleContext]() ottl.StandardGetSetter[K] {
+	return ottl.StandardGetSetter[K]{
+		Getter: func(_ context.Context, tCtx K) (any, error) {
+			return ctxutil.GetCommonIntSliceValues[int64](tCtx.GetProfileSample().Value()), nil
+		},
+		Setter: func(_ context.Context, tCtx K, val any) error {
+			return ctxutil.SetCommonIntSliceValues[int64](tCtx.GetProfileSample().Value(), val)
+		},
+	}
+}
+
+func accessAttributeIndices[K ProfileSampleContext]() ottl.StandardGetSetter[K] {
+	return ottl.StandardGetSetter[K]{
+		Getter: func(_ context.Context, tCtx K) (any, error) {
+			return ctxutil.GetCommonIntSliceValues[int32](tCtx.GetProfileSample().AttributeIndices()), nil
+		},
+		Setter: func(_ context.Context, tCtx K, val any) error {
+			return ctxutil.SetCommonIntSliceValues[int32](tCtx.GetProfileSample().AttributeIndices(), val)
+		},
+	}
+}
+
+func accessLinkIndex[K ProfileSampleContext]() ottl.StandardGetSetter[K] {
+	return ottl.StandardGetSetter[K]{
+		Getter: func(_ context.Context, tCtx K) (any, error) {
+			return tCtx.GetProfileSample().LinkIndex(), nil
+		},
+		Setter: func(_ context.Context, tCtx K, val any) error {
+			if v, ok := val.(int32); ok {
+				tCtx.GetProfileSample().SetLinkIndex(v)
+			}
+			return nil
+		},
+	}
+}
+
+func accessTimestampsUnixNano[K ProfileSampleContext]() ottl.StandardGetSetter[K] {
+	return ottl.StandardGetSetter[K]{
+		Getter: func(_ context.Context, tCtx K) (any, error) {
+			return ctxutil.GetCommonIntSliceValues[uint64](tCtx.GetProfileSample().TimestampsUnixNano()), nil
+		},
+		Setter: func(_ context.Context, tCtx K, val any) error {
+			return ctxutil.SetCommonIntSliceValues[uint64](tCtx.GetProfileSample().TimestampsUnixNano(), val)
+		},
+	}
+}
