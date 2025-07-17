dee2efbd7d82e35a8964087426f864a27596ac13
Matthieu MOREL <matthieu.morel35@gmail.com>
2025-07-17 08:01:39 +0200

    [chore]: enable httpNoBody rule from go-critic
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- receiver/libhoneyreceiver/receiver.go
+++ receiver/libhoneyreceiver/receiver.go
@@ -137,7 +137,7 @@ func (r *libhoneyReceiver) registerLogConsumer(tc consumer.Logs) {
 
 func (r *libhoneyReceiver) handleAuth(resp http.ResponseWriter, req *http.Request) {
 	authURL := fmt.Sprintf("%s/1/auth", r.cfg.AuthAPI)
-	authReq, err := http.NewRequest(http.MethodGet, authURL, nil)
+	authReq, err := http.NewRequest(http.MethodGet, authURL, http.NoBody)
 	if err != nil {
 		errJSON, _ := json.Marshal(`{"error": "failed to create auth request"}`)
 		writeResponse(resp, "json", http.StatusBadRequest, errJSON)
