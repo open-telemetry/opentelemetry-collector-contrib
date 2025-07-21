c49acf1705188af33a7f7c53fef69fb67b22eaf0
Phil Gore <erog38@gmail.com>
2025-07-21 09:01:29 -0600

    [cmd/telemetrygen] Add ability to ensure uniqueness of timeseries within a time period (#39935)
    #### Description
    
    Adds a new `timeBox` object which has the sole purpose of generating
    unique attributes per second. This allows for telemetrygen to not
    override it's own values when sending to backends.
    
    <!-- Issue number (e.g. #1234) or full URL to issue, if applicable. -->
    #### Link to tracking issue
    Fixes #39933
    
    <!--Describe what testing was performed and which tests were added.-->
    #### Testing
    
    Additional test added to cover new functionality
    
    #### Documentation
    
    New Flag added along with help criteria
    
    ---------
    
    Co-authored-by: Pablo Baeyens <pbaeyens31+github@gmail.com>

--- cmd/telemetrygen/pkg/metrics/worker.go
+++ cmd/telemetrygen/pkg/metrics/worker.go
@@ -24,6 +24,7 @@ type worker struct {
 	aggregationTemporality AggregationTemporality       // Temporality type to use
 	exemplars              []metricdata.Exemplar[int64] // exemplars to attach to the metric
 	numMetrics             int                          // how many metrics the worker has to generate (only when duration==0)
+	enforceUnique          bool                         // if true, the worker will generate unique timeseries
 	totalDuration          time.Duration                // how long to run the test for (overrides `numMetrics`)
 	limitPerSecond         rate.Limit                   // how many metrics per second to generate
 	wg                     *sync.WaitGroup              // notify when done
@@ -82,13 +83,16 @@ var histogramBucketSamples = []struct {
 	},
 }
 
-func (w worker) simulateMetrics(res *resource.Resource, exporter sdkmetric.Exporter, signalAttrs []attribute.KeyValue) {
+func (w worker) simulateMetrics(res *resource.Resource, exporter sdkmetric.Exporter, signalAttrs []attribute.KeyValue, tb *timeBox) {
 	limiter := rate.NewLimiter(w.limitPerSecond, 1)
 
 	startTime := w.clock.Now()
 
 	var i int64
 	for w.running.Load() {
+		if w.enforceUnique {
+			signalAttrs = append(signalAttrs, tb.getAttribute())
+		}
 		var metrics []metricdata.Metrics
 		now := w.clock.Now()
 		if w.aggregationTemporality.AsTemporality() == metricdata.DeltaTemporality {
