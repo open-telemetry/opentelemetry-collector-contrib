3952231f750724edfbf7a9724ef08af60cce069f
Arthur Silva Sens <arthursens2005@gmail.com>
2025-07-23 20:35:45 -0300

    WIP/exporter/prometheus: Implement translation strategies
    Signed-off-by: Arthur Silva Sens <arthursens2005@gmail.com>

--- exporter/prometheusexporter/config.go
+++ exporter/prometheusexporter/config.go
@@ -4,11 +4,13 @@
 package prometheusexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/prometheusexporter"
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/config/confighttp"
+	"go.opentelemetry.io/collector/featuregate"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/resourcetotelemetry"
 )
@@ -36,12 +38,48 @@ type Config struct {
 	EnableOpenMetrics bool `mapstructure:"enable_open_metrics"`
 
 	// AddMetricSuffixes controls whether suffixes are added to metric names. Defaults to true.
+	// Deprecated: Use TranslationStrategy instead when exporter.prometheusexporter.UseTranslationStrategy feature gate is enabled.
 	AddMetricSuffixes bool `mapstructure:"add_metric_suffixes"`
+
+	// TranslationStrategy controls how OTLP metric and attribute names are translated into Prometheus metric and label names.
+	// Only used when exporter.prometheusexporter.UseTranslationStrategy feature gate is enabled.
+	TranslationStrategy translationStrategy `mapstructure:"translation_strategy"`
 }
 
 var _ component.Config = (*Config)(nil)
 
 // Validate checks if the exporter configuration is valid
-func (*Config) Validate() error {
+func (cfg *Config) Validate() error {
+	if translationStrategyFeatureGate.IsEnabled() {
+		switch cfg.TranslationStrategy {
+		case underscoreEscapingWithSuffixes, underscoreEscapingWithoutSuffixes, noUTF8EscapingWithSuffixes, noTranslation:
+		default:
+			return fmt.Errorf("invalid translation_strategy: %s", cfg.TranslationStrategy)
+		}
+	}
 	return nil
 }
+
+type translationStrategy string
+
+const (
+	// underscoreEscapingWithSuffixes fully escapes metric names for classic Prometheus metric name compatibility,
+	// and includes appending type and unit suffixes
+	underscoreEscapingWithSuffixes translationStrategy = "UnderscoreEscapingWithSuffixes"
+
+	// underscoreEscapingWithoutSuffixes escapes special characters to '_', but suffixes won't be attached
+	underscoreEscapingWithoutSuffixes translationStrategy = "UnderscoreEscapingWithoutSuffixes"
+
+	// noUTF8EscapingWithSuffixes disables changing special characters to '_'. Special suffixes like units and '_total' for counters will be attached
+	noUTF8EscapingWithSuffixes translationStrategy = "NoUTF8EscapingWithSuffixes"
+
+	// noTranslation bypasses all metric and label name translation, passing them through unaltered
+	noTranslation translationStrategy = "NoTranslation"
+)
+
+var translationStrategyFeatureGate = featuregate.GlobalRegistry().MustRegister(
+	"exporter.prometheusexporter.UseTranslationStrategy",
+	featuregate.StageAlpha,
+	featuregate.WithRegisterDescription("When enabled, the Prometheus exporter uses the new translation_strategy configuration instead of add_metric_suffixes"),
+	featuregate.WithRegisterReferenceURL("https://github.com/open-telemetry/opentelemetry-specification/pull/4533"),
+)
