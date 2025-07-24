4836f8d873a0f05e99e790285763e7bb71b5bf7f
Patrick Oyarzun <patrick.oyarzun@grafana.com>
2025-07-24 13:38:08 -0400

    add license

--- processor/tailsamplingprocessor/internal/tracelimiter/drop_old_traces_limiter.go
+++ processor/tailsamplingprocessor/internal/tracelimiter/drop_old_traces_limiter.go
@@ -1,3 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
 package tracelimiter // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/tailsamplingprocessor/internal/tracelimiter"
 
 import (
