a686c4d162b460ced6b28acd10b5642ba7a72476
Cuichen Li <cuichen.li94@gmail.com>
2025-07-26 18:26:21 +0800

    add script for init mongodb

---
+++ receiver/mongodbreceiver/init-mongo.js
@@ -0,0 +1,15 @@
+// Create database and collections
+db = db.getSiblingDB('sample_db');
+
+// Create a collection
+db.createCollection('users');
+
+// Create a user with read/write access to the database
+db.createUser({
+  user: 'app_user',
+  pwd: 'app_password',
+  roles: [{
+    role: 'readWrite',
+    db: 'sample_db'
+  }]
+});
