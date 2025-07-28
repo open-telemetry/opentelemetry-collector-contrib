3088e96807aa40573ffb200ea1b7b82bf19923ac
krisztianfekete <git@krisztianfekete.org>
2025-07-28 13:29:33 +0200

    gci format

--- receiver/k8sobjectsreceiver/receiver_test.go
+++ receiver/k8sobjectsreceiver/receiver_test.go
@@ -10,13 +10,11 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/component/componenttest"
 	"go.opentelemetry.io/collector/consumer/consumertest"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/receiver/receivertest"
-
 	apiWatch "k8s.io/apimachinery/pkg/watch"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/k8sleaderelector/k8sleaderelectortest"
