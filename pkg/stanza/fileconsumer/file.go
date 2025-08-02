c75df2702eaa714b7faf1e76e283006383990e73
Vihas Makwana <121151420+VihasMakwana@users.noreply.github.com>
2025-08-03 02:40:41 +0530

    [pkg/stanza][fileconsumer] - finalize archive's implementation (#39901)
    This PR finalizes the archive implementation and adds test case. 
    
    It updates following things:
    1. Updates tracker to track unmatched files as well.
    - Theoretically, we can perform a match against archive with a single
    fingerprint but it is not effecient as we'll need to access disk
    frequently. Refer
    https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/32727#issuecomment-2259215363
    for detailed discussion on this topic.
    - Thus, we first track all unmatched files and then perform match
    against archive to minimise disk access.
    3. Introduces a `handleUnmatchedFiles` helper to create readers once
    archive matching is done.
    
    Relates:
    https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/38056
    
    ---------
    
    Co-authored-by: Andrzej Stencel <andrzej.stencel@elastic.co>

--- pkg/stanza/fileconsumer/file.go
+++ pkg/stanza/fileconsumer/file.go
@@ -228,7 +228,48 @@ func (m *Manager) makeReaders(ctx context.Context, paths []string) {
 			continue
 		}
 
-		m.tracker.Add(r)
+		if r != nil {
+			m.tracker.Add(r)
+			continue
+		}
+		m.tracker.AddUnmatched(file, fp)
+	}
+	m.handleUnmatchedFiles(ctx)
+}
+
+func (m *Manager) handleUnmatchedFiles(ctx context.Context) {
+	// Notes:
+	// 1. fp[i] is the fingerprint of file[i], and matchedMetadata[i] (if present) is the corresponding metadata retrieved from the archive.
+	// 2. If matchedMetadata[i] is not nil, a match for file[i] was found in the archive — use this metadata to create the reader.
+	//    If matchedMetadata[i] is nil, no match was found — create a new reader from scratch.
+	files, fps, matchedMetadata := m.tracker.LookupArchive(ctx)
+
+	for i, md := range matchedMetadata {
+		file := files[i]
+		fp := fps[i]
+
+		var reader *reader.Reader
+		var err error
+
+		if md != nil {
+			reader, err = m.readerFactory.NewReaderFromMetadata(file, md)
+			if m.tracker.Name() != tracker.NoStateTracker {
+				m.set.Logger.Info("File found in archive. Started watching file again", zap.String("path", file.Name()))
+			}
+		} else {
+			if m.tracker.Name() != tracker.NoStateTracker {
+				m.set.Logger.Info("Started watching file", zap.String("path", file.Name()))
+			}
+			reader, err = m.readerFactory.NewReader(file, fp)
+		}
+
+		if err != nil {
+			m.set.Logger.Error("Failed to create reader", zap.Error(err))
+			continue
+		}
+
+		m.telemetryBuilder.FileconsumerOpenFiles.Add(ctx, 1)
+		m.tracker.Add(reader)
 	}
 }
 
@@ -261,19 +302,8 @@ func (m *Manager) newReader(ctx context.Context, file *os.File, fp *fingerprint.
 		return r, nil
 	}
 
-	// When the NoStateTracker is used, this would result in log spam as new
-	// readers are created every scrape interval.
-	if m.tracker.Name() != tracker.NoStateTracker {
-		m.set.Logger.Info("Started watching file", zap.String("path", file.Name()))
-	}
-
-	// If we don't match any previously known files, create a new reader from scratch
-	r, err := m.readerFactory.NewReader(file, fp)
-	if err != nil {
-		return nil, err
-	}
-	m.telemetryBuilder.FileconsumerOpenFiles.Add(ctx, 1)
-	return r, nil
+	// If no previously known files are matched, readers will be created after matching against the archive.
+	return nil, nil
 }
 
 func (m *Manager) instantiateTracker(ctx context.Context, persister operator.Persister) {
