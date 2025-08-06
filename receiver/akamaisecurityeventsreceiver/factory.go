4b6a2067db6ccba2890fd3f1e11f7d81a4f82c4e
Peter Deng <yundeng@ebay.com>
2025-08-06 06:14:03 -0700

    fix ci errors

--- receiver/akamaisecurityeventsreceiver/factory.go
+++ receiver/akamaisecurityeventsreceiver/factory.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package akamaisecurityeventsreceiver
+package akamaisecurityeventsreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/akamaisecurityeventsreceiver"
 
 import (
 	"context"
