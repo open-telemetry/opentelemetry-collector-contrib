37bc380be5049f432ade625c86204a5e99e80090
Pablo Baeyens <pbaeyens31+github@gmail.com>
2025-08-13 16:18:40 +0200

    Update receiver/libhoneyreceiver/config_test.go
    Co-authored-by: Alex Boten <223565+codeboten@users.noreply.github.com>

--- receiver/libhoneyreceiver/config_test.go
+++ receiver/libhoneyreceiver/config_test.go
@@ -35,7 +35,7 @@ func TestCreateDefaultConfig(t *testing.T) {
 	libhoneyCfg, ok := cfg.(*Config)
 	require.True(t, ok, "invalid Config type")
 
-	GetOrInsertDefault(t, &libhoneyCfg.HTTP)
+	getOrInsertDefault(t, &libhoneyCfg.HTTP)
 	assert.Equal(t, "localhost:8080", libhoneyCfg.HTTP.Get().Endpoint)
 	assert.Equal(t, []string{"/events", "/event", "/batch"}, libhoneyCfg.HTTP.Get().TracesURLPaths)
 	assert.Empty(t, libhoneyCfg.AuthAPI)
