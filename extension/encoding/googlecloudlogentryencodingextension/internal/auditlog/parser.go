b8003a87f962210f54aed43c27656c517b9158b4
Constanca Manteigas <constanca.manteigas@elastic.co>
2025-08-15 15:26:39 +0200

    make goporto

--- extension/encoding/googlecloudlogentryencodingextension/internal/auditlog/parser.go
+++ extension/encoding/googlecloudlogentryencodingextension/internal/auditlog/parser.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package auditlog
+package auditlog // import "github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding/googlecloudlogentryencodingextension/internal/auditlog"
 
 import (
 	"errors"
--- extension/encoding/googlecloudlogentryencodingextension/internal/shared/utils.go
+++ extension/encoding/googlecloudlogentryencodingextension/internal/shared/utils.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package shared
+package shared // import "github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding/googlecloudlogentryencodingextension/internal/shared"
 
 import (
 	"fmt"
