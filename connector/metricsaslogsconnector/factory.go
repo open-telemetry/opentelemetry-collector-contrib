0908994122629377610e0f3cfb195e0085d64829
Kyosuke Kariya <Kyosuke.Kariya@morganstanley.com>
2025-08-16 17:29:40 +0900

    feat: first version of metricsaslogs connector

---
+++ connector/metricsaslogsconnector/factory.go
@@ -0,0 +1,32 @@
+package metricsaslogsconnector
+
+import (
+	"context"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/connector"
+	"go.opentelemetry.io/collector/connector/xconnector"
+	"go.opentelemetry.io/collector/consumer"
+)
+
+func NewFactory() connector.Factory {
+	return xconnector.NewFactory(
+		component.MustNewType("metricsaslogs"),
+		createDefaultConfig,
+		xconnector.WithMetricsToLogs(createMetricsToLogs, component.StabilityLevelAlpha),
+	)
+}
+
+func createMetricsToLogs(
+	_ context.Context,
+	set connector.Settings,
+	cfg component.Config,
+	nextConsumer consumer.Logs,
+) (connector.Metrics, error) {
+	c := cfg.(*Config)
+	return &metricsAsLogs{
+		logsConsumer: nextConsumer,
+		config:       c,
+		logger:       set.Logger,
+	}, nil
+}
