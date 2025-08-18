24cdd5f52742fa4a4587a1cd84dea0ab22657de8
adithya-r-nathan <adithya.r.nathan@gmail.com>
2025-08-18 08:54:46 +0530

    Address review feedback: fix formatting, remove helper, add changelog

--- extension/encoding/awslogsencodingextension/extension.go
+++ extension/encoding/awslogsencodingextension/extension.go
@@ -4,181 +4,181 @@
 package awslogsencodingextension // import "github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding/awslogsencodingextension"
 
 import (
-    "bytes"
-    "context"
-    "fmt"
-    "io"
-    "sync"
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"sync"
 
-    "github.com/klauspost/compress/gzip"
-    "go.opentelemetry.io/collector/component"
-    "go.opentelemetry.io/collector/extension"
-    "go.opentelemetry.io/collector/pdata/plog"
+	"github.com/klauspost/compress/gzip"
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/extension"
+	"go.opentelemetry.io/collector/pdata/plog"
 
-    "github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding"
-    awsunmarshaler "github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding/awslogsencodingextension/internal/unmarshaler"
-    cloudtraillog "github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding/awslogsencodingextension/internal/unmarshaler/cloudtraillog"
-    elbaccesslogs "github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding/awslogsencodingextension/internal/unmarshaler/elb-access-log"
-    s3accesslog "github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding/awslogsencodingextension/internal/unmarshaler/s3-access-log"
-    subscriptionfilter "github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding/awslogsencodingextension/internal/unmarshaler/subscription-filter"
-    vpcflowlog "github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding/awslogsencodingextension/internal/unmarshaler/vpc-flow-log"
-    "github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding/awslogsencodingextension/internal/unmarshaler/waf"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding"
+	awsunmarshaler "github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding/awslogsencodingextension/internal/unmarshaler"
+	cloudtraillog "github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding/awslogsencodingextension/internal/unmarshaler/cloudtraillog"
+	elbaccesslogs "github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding/awslogsencodingextension/internal/unmarshaler/elb-access-log"
+	s3accesslog "github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding/awslogsencodingextension/internal/unmarshaler/s3-access-log"
+	subscriptionfilter "github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding/awslogsencodingextension/internal/unmarshaler/subscription-filter"
+	vpcflowlog "github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding/awslogsencodingextension/internal/unmarshaler/vpc-flow-log"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding/awslogsencodingextension/internal/unmarshaler/waf"
 )
 
 const (
-    gzipEncoding    = "gzip"
-    bytesEncoding   = "bytes"
-    parquetEncoding = "parquet"
+	gzipEncoding    = "gzip"
+	bytesEncoding   = "bytes"
+	parquetEncoding = "parquet"
 )
 
 var _ encoding.LogsUnmarshalerExtension = (*encodingExtension)(nil)
 
 type encodingExtension struct {
-    unmarshaler awsunmarshaler.AWSUnmarshaler
-    format      string
-    gzipPool    sync.Pool
-    // if format is VPC, then content can be in parquet or
-    // gzip encoding
-    vpcFormat string
+	unmarshaler awsunmarshaler.AWSUnmarshaler
+	format      string
+	gzipPool    sync.Pool
+	// if format is VPC, then content can be in parquet or
+	// gzip encoding
+	vpcFormat string
 }
 
 func newExtension(cfg *Config, settings extension.Settings) (*encodingExtension, error) {
-    switch cfg.Format {
-    case formatCloudWatchLogsSubscriptionFilter:
-        return &encodingExtension{
-            unmarshaler: subscriptionfilter.NewSubscriptionFilterUnmarshaler(settings.BuildInfo),
-            format:      formatCloudWatchLogsSubscriptionFilter,
-        }, nil
-    case formatVPCFlowLog:
-        unmarshaler, err := vpcflowlog.NewVPCFlowLogUnmarshaler(
-            cfg.VPCFlowLogConfig.FileFormat,
-            settings.BuildInfo,
-            settings.Logger,
-        )
-        return &encodingExtension{
-            unmarshaler: unmarshaler,
-            vpcFormat:   cfg.VPCFlowLogConfig.FileFormat,
-            format:      formatVPCFlowLog,
-        }, err
-    case formatS3AccessLog:
-        return &encodingExtension{
-            unmarshaler: s3accesslog.NewS3AccessLogUnmarshaler(settings.BuildInfo),
-            format:      formatS3AccessLog,
-        }, nil
-    case formatWAFLog:
-        return &encodingExtension{
-            unmarshaler: waf.NewWAFLogUnmarshaler(settings.BuildInfo),
-            format:      formatWAFLog,
-        }, nil
-    case formatCloudTrailLog:
-        return &encodingExtension{
-            unmarshaler: cloudtraillog.NewCloudTrailLogUnmarshaler(settings.BuildInfo),
-            format:      formatCloudTrailLog,
-        }, nil
-    case formatELBAccessLog:
-        return &encodingExtension{
-            unmarshaler: elbaccesslogs.NewELBAccessLogUnmarshaler(
-                settings.BuildInfo,
-                settings.Logger,
-            ),
-            format: formatELBAccessLog,
-        }, nil
-    default:
-        // Format will have been validated by Config.Validate,
-        // so we'll only get here if we haven't handled a valid
-        // format.
-        return nil, fmt.Errorf("unimplemented format %q", cfg.Format)
-    }
+	switch cfg.Format {
+	case formatCloudWatchLogsSubscriptionFilter:
+		return &encodingExtension{
+			unmarshaler: subscriptionfilter.NewSubscriptionFilterUnmarshaler(settings.BuildInfo),
+			format:      formatCloudWatchLogsSubscriptionFilter,
+		}, nil
+	case formatVPCFlowLog:
+		unmarshaler, err := vpcflowlog.NewVPCFlowLogUnmarshaler(
+			cfg.VPCFlowLogConfig.FileFormat,
+			settings.BuildInfo,
+			settings.Logger,
+		)
+		return &encodingExtension{
+			unmarshaler: unmarshaler,
+			vpcFormat:   cfg.VPCFlowLogConfig.FileFormat,
+			format:      formatVPCFlowLog,
+		}, err
+	case formatS3AccessLog:
+		return &encodingExtension{
+			unmarshaler: s3accesslog.NewS3AccessLogUnmarshaler(settings.BuildInfo),
+			format:      formatS3AccessLog,
+		}, nil
+	case formatWAFLog:
+		return &encodingExtension{
+			unmarshaler: waf.NewWAFLogUnmarshaler(settings.BuildInfo),
+			format:      formatWAFLog,
+		}, nil
+	case formatCloudTrailLog:
+		return &encodingExtension{
+			unmarshaler: cloudtraillog.NewCloudTrailLogUnmarshaler(settings.BuildInfo),
+			format:      formatCloudTrailLog,
+		}, nil
+	case formatELBAccessLog:
+		return &encodingExtension{
+			unmarshaler: elbaccesslogs.NewELBAccessLogUnmarshaler(
+				settings.BuildInfo,
+				settings.Logger,
+			),
+			format: formatELBAccessLog,
+		}, nil
+	default:
+		// Format will have been validated by Config.Validate,
+		// so we'll only get here if we haven't handled a valid
+		// format.
+		return nil, fmt.Errorf("unimplemented format %q", cfg.Format)
+	}
 }
 
 func (*encodingExtension) Start(_ context.Context, _ component.Host) error {
-    return nil
+	return nil
 }
 
 func (*encodingExtension) Shutdown(_ context.Context) error {
-    return nil
+	return nil
 }
 
 func (e *encodingExtension) getGzipReader(buf []byte) (io.Reader, error) {
-    var err error
-    gzipReader, ok := e.gzipPool.Get().(*gzip.Reader)
-    if !ok {
-        gzipReader, err = gzip.NewReader(bytes.NewReader(buf))
-    } else {
-        err = gzipReader.Reset(bytes.NewBuffer(buf))
-    }
+	var err error
+	gzipReader, ok := e.gzipPool.Get().(*gzip.Reader)
+	if !ok {
+		gzipReader, err = gzip.NewReader(bytes.NewReader(buf))
+	} else {
+		err = gzipReader.Reset(bytes.NewBuffer(buf))
+	}
 
-    if err != nil {
-        if gzipReader != nil {
-            e.gzipPool.Put(gzipReader)
-        }
-        return nil, fmt.Errorf("failed to decompress content: %w", err)
-    }
+	if err != nil {
+		if gzipReader != nil {
+			e.gzipPool.Put(gzipReader)
+		}
+		return nil, fmt.Errorf("failed to decompress content: %w", err)
+	}
 
-    return gzipReader, nil
+	return gzipReader, nil
 }
 
 // isGzipData checks if the buffer contains gzip-compressed data by examining magic bytes
 func isGzipData(buf []byte) bool {
-    return len(buf) > 2 && buf[0] == 0x1f && buf[1] == 0x8b
+	return len(buf) > 2 && buf[0] == 0x1f && buf[1] == 0x8b
 }
 
 // getReaderForData returns the appropriate reader and encoding type based on data format
 func (e *encodingExtension) getReaderForData(buf []byte) (string, io.Reader, error) {
-    if isGzipData(buf) {
-        reader, err := e.getGzipReader(buf)
-        return gzipEncoding, reader, err
-    }
-    return bytesEncoding, bytes.NewReader(buf), nil
+	if isGzipData(buf) {
+		reader, err := e.getGzipReader(buf)
+		return gzipEncoding, reader, err
+	}
+	return bytesEncoding, bytes.NewReader(buf), nil
 }
 
 func (e *encodingExtension) getReaderFromFormat(buf []byte) (string, io.Reader, error) {
-    switch e.format {
-    case formatWAFLog, formatCloudWatchLogsSubscriptionFilter, formatCloudTrailLog, formatELBAccessLog:
-        return e.getReaderForData(buf)
-        
-    case formatS3AccessLog:
-        return bytesEncoding, bytes.NewReader(buf), nil
-        
-    case formatVPCFlowLog:
-        switch e.vpcFormat {
-        case fileFormatParquet:
-            return parquetEncoding, nil, fmt.Errorf("%q still needs to be implemented", e.vpcFormat)
-        case fileFormatPlainText:
-            return e.getReaderForData(buf)
-        default:
-            // should not be possible
-            return "", nil, fmt.Errorf(
-                "unsupported file fileFormat %q for VPC flow log, expected one of %q",
-                e.vpcFormat,
-                supportedVPCFlowLogFileFormat,
-            )
-        }
-        
-    default:
-        // should not be possible
-        return "", nil, fmt.Errorf("unimplemented: format %q has no reader", e.format)
-    }
+	switch e.format {
+	case formatWAFLog, formatCloudWatchLogsSubscriptionFilter, formatCloudTrailLog, formatELBAccessLog:
+		return e.getReaderForData(buf)
+
+	case formatS3AccessLog:
+		return bytesEncoding, bytes.NewReader(buf), nil
+
+	case formatVPCFlowLog:
+		switch e.vpcFormat {
+		case fileFormatParquet:
+			return parquetEncoding, nil, fmt.Errorf("%q still needs to be implemented", e.vpcFormat)
+		case fileFormatPlainText:
+			return e.getReaderForData(buf)
+		default:
+			// should not be possible
+			return "", nil, fmt.Errorf(
+				"unsupported file fileFormat %q for VPC flow log, expected one of %q",
+				e.vpcFormat,
+				supportedVPCFlowLogFileFormat,
+			)
+		}
+
+	default:
+		// should not be possible
+		return "", nil, fmt.Errorf("unimplemented: format %q has no reader", e.format)
+	}
 }
 
 func (e *encodingExtension) UnmarshalLogs(buf []byte) (plog.Logs, error) {
-    encodingReader, reader, err := e.getReaderFromFormat(buf)
-    if err != nil {
-        return plog.Logs{}, fmt.Errorf("failed to get reader for %q logs: %w", e.format, err)
-    }
+	encodingReader, reader, err := e.getReaderFromFormat(buf)
+	if err != nil {
+		return plog.Logs{}, fmt.Errorf("failed to get reader for %q logs: %w", e.format, err)
+	}
 
-    defer func() {
-        if encodingReader == gzipEncoding {
-            r := reader.(*gzip.Reader)
-            _ = r.Close()
-            e.gzipPool.Put(r)
-        }
-    }()
+	defer func() {
+		if encodingReader == gzipEncoding {
+			r := reader.(*gzip.Reader)
+			_ = r.Close()
+			e.gzipPool.Put(r)
+		}
+	}()
 
-    logs, err := e.unmarshaler.UnmarshalAWSLogs(reader)
-    if err != nil {
-        return plog.Logs{}, fmt.Errorf("failed to unmarshal logs as %q format: %w", e.format, err)
-    }
+	logs, err := e.unmarshaler.UnmarshalAWSLogs(reader)
+	if err != nil {
+		return plog.Logs{}, fmt.Errorf("failed to unmarshal logs as %q format: %w", e.format, err)
+	}
 
-    return logs, nil
+	return logs, nil
 }
--- extension/encoding/awslogsencodingextension/extension_test.go
+++ extension/encoding/awslogsencodingextension/extension_test.go
@@ -4,169 +4,164 @@
 package awslogsencodingextension
 
 import (
-    "bytes"
-    "os"
-    "sync"
-    "testing"
+	"bytes"
+	"os"
+	"sync"
+	"testing"
 
-    "github.com/klauspost/compress/gzip"
-    "github.com/stretchr/testify/assert"
-    "github.com/stretchr/testify/require"
-    "go.opentelemetry.io/collector/component"
-    "go.opentelemetry.io/collector/extension/extensiontest"
+	"github.com/klauspost/compress/gzip"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/extension/extensiontest"
 
-    subscriptionfilter "github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding/awslogsencodingextension/internal/unmarshaler/subscription-filter"
+	subscriptionfilter "github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding/awslogsencodingextension/internal/unmarshaler/subscription-filter"
 )
 
-// Helper function to unmarshal logs and expect no error
-func unmarshalLogsExpectNoError(t *testing.T, e *encodingExtension, buf []byte) {
-    logs, err := e.UnmarshalLogs(buf)
-    require.NoError(t, err)
-    require.NotNil(t, logs)
-}
-
 func TestNew_CloudWatchLogsSubscriptionFilter(t *testing.T) {
-    e, err := newExtension(&Config{Format: formatCloudWatchLogsSubscriptionFilter}, extensiontest.NewNopSettings(extensiontest.NopType))
-    require.NoError(t, err)
-    require.NotNil(t, e)
+	e, err := newExtension(&Config{Format: formatCloudWatchLogsSubscriptionFilter}, extensiontest.NewNopSettings(extensiontest.NopType))
+	require.NoError(t, err)
+	require.NotNil(t, e)
 
-    _, err = e.UnmarshalLogs([]byte("invalid"))
-    require.ErrorContains(t, err, `failed to unmarshal logs as "cloudwatch_logs_subscription_filter" format`)
+	_, err = e.UnmarshalLogs([]byte("invalid"))
+	require.ErrorContains(t, err, `failed to unmarshal logs as "cloudwatch_logs_subscription_filter" format`)
 }
 
 func TestNew_CloudTrailLog(t *testing.T) {
-    e, err := newExtension(&Config{Format: formatCloudTrailLog}, extensiontest.NewNopSettings(extensiontest.NopType))
-    require.NoError(t, err)
-    require.NotNil(t, e)
+	e, err := newExtension(&Config{Format: formatCloudTrailLog}, extensiontest.NewNopSettings(extensiontest.NopType))
+	require.NoError(t, err)
+	require.NotNil(t, e)
 
-    _, err = e.UnmarshalLogs([]byte("invalid"))
-    require.ErrorContains(t, err, `failed to unmarshal logs as "cloudtrail_log" format`)
+	_, err = e.UnmarshalLogs([]byte("invalid"))
+	require.ErrorContains(t, err, `failed to unmarshal logs as "cloudtrail_log" format`)
 }
 
 func TestNew_VPCFlowLog(t *testing.T) {
-    cfg := createDefaultConfig().(*Config)
-    cfg.Format = formatVPCFlowLog
-    e, err := newExtension(cfg, extensiontest.NewNopSettings(extensiontest.NopType))
-    require.NoError(t, err)
-    require.NotNil(t, e)
+	cfg := createDefaultConfig().(*Config)
+	cfg.Format = formatVPCFlowLog
+	e, err := newExtension(cfg, extensiontest.NewNopSettings(extensiontest.NopType))
+	require.NoError(t, err)
+	require.NotNil(t, e)
 
-    // VPC Flow Log unmarshaler handles non-log input gracefully
-    unmarshalLogsExpectNoError(t, e, []byte("some test input"))
+	// VPC Flow Log unmarshaler handles non-log input gracefully
+	logs, err := e.UnmarshalLogs([]byte("some test input"))
+	require.NoError(t, err)
+	require.NotNil(t, logs)
 }
 
 func TestNew_S3AccessLog(t *testing.T) {
-    e, err := newExtension(&Config{Format: formatS3AccessLog}, extensiontest.NewNopSettings(extensiontest.NopType))
-    require.NoError(t, err)
-    require.NotNil(t, e)
+	e, err := newExtension(&Config{Format: formatS3AccessLog}, extensiontest.NewNopSettings(extensiontest.NopType))
+	require.NoError(t, err)
+	require.NotNil(t, e)
 
-    _, err = e.UnmarshalLogs([]byte("invalid"))
-    require.ErrorContains(t, err, `failed to unmarshal logs as "s3_access_log" format`)
+	_, err = e.UnmarshalLogs([]byte("invalid"))
+	require.ErrorContains(t, err, `failed to unmarshal logs as "s3_access_log" format`)
 }
 
 func TestNew_WAFLog(t *testing.T) {
-    e, err := newExtension(&Config{Format: formatWAFLog}, extensiontest.NewNopSettings(extensiontest.NopType))
-    require.NoError(t, err)
-    require.NotNil(t, e)
+	e, err := newExtension(&Config{Format: formatWAFLog}, extensiontest.NewNopSettings(extensiontest.NopType))
+	require.NoError(t, err)
+	require.NotNil(t, e)
 
-    _, err = e.UnmarshalLogs([]byte("invalid"))
-    require.ErrorContains(t, err, `failed to unmarshal logs as "waf_log" format`)
+	_, err = e.UnmarshalLogs([]byte("invalid"))
+	require.ErrorContains(t, err, `failed to unmarshal logs as "waf_log" format`)
 }
 
 func TestNew_ELBAcessLog(t *testing.T) {
-    e, err := newExtension(&Config{Format: formatELBAccessLog}, extensiontest.NewNopSettings(extensiontest.NopType))
-    require.NoError(t, err)
-    require.NotNil(t, e)
+	e, err := newExtension(&Config{Format: formatELBAccessLog}, extensiontest.NewNopSettings(extensiontest.NopType))
+	require.NoError(t, err)
+	require.NotNil(t, e)
 
-    _, err = e.UnmarshalLogs([]byte("invalid"))
-    require.ErrorContains(t, err, `failed to unmarshal logs as "elb_access_log" format`)
+	_, err = e.UnmarshalLogs([]byte("invalid"))
+	require.ErrorContains(t, err, `failed to unmarshal logs as "elb_access_log" format`)
 }
 
 func TestNew_Unimplemented(t *testing.T) {
-    e, err := newExtension(&Config{Format: "invalid"}, extensiontest.NewNopSettings(extensiontest.NopType))
-    require.Error(t, err)
-    require.Nil(t, e)
-    assert.EqualError(t, err, `unimplemented format "invalid"`)
+	e, err := newExtension(&Config{Format: "invalid"}, extensiontest.NewNopSettings(extensiontest.NopType))
+	require.Error(t, err)
+	require.Nil(t, e)
+	assert.EqualError(t, err, `unimplemented format "invalid"`)
 }
 
 func TestGetReaderFromFormat(t *testing.T) {
-    t.Parallel()
+	t.Parallel()
 
-    tests := map[string]struct {
-        format string
-        buf    []byte
-    }{
-        "non_gzip_data_waf_log": {
-            format: formatWAFLog,
-            buf:    []byte("invalid"),
-        },
-        "valid_gzip_reader": {
-            format: formatWAFLog,
-            buf: func() []byte {
-                var buf bytes.Buffer
-                gz := gzip.NewWriter(&buf)
-                _, err := gz.Write([]byte("valid"))
-                require.NoError(t, err)
-                _ = gz.Close()
-                return buf.Bytes()
-            }(),
-        },
-        "valid_bytes_reader": {
-            format: formatS3AccessLog,
-            buf:    []byte("valid"),
-        },
-    }
+	tests := map[string]struct {
+		format string
+		buf    []byte
+	}{
+		"non_gzip_data_waf_log": {
+			format: formatWAFLog,
+			buf:    []byte("invalid"),
+		},
+		"valid_gzip_reader": {
+			format: formatWAFLog,
+			buf: func() []byte {
+				var buf bytes.Buffer
+				gz := gzip.NewWriter(&buf)
+				_, err := gz.Write([]byte("valid"))
+				require.NoError(t, err)
+				_ = gz.Close()
+				return buf.Bytes()
+			}(),
+		},
+		"valid_bytes_reader": {
+			format: formatS3AccessLog,
+			buf:    []byte("valid"),
+		},
+	}
 
-    for name, test := range tests {
-        t.Run(name, func(t *testing.T) {
-            e := &encodingExtension{format: test.format}
-            _, reader, err := e.getReaderFromFormat(test.buf)
-            require.NoError(t, err)
-            require.NotNil(t, reader)
-        })
-    }
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			e := &encodingExtension{format: test.format}
+			_, reader, err := e.getReaderFromFormat(test.buf)
+			require.NoError(t, err)
+			require.NotNil(t, reader)
+		})
+	}
 }
 
 // readAndCompressLogFile reads the data inside it, compresses it
 // and returns a GZIP reader for it.
 func readAndCompressLogFile(t *testing.T, file string) []byte {
-    data, err := os.ReadFile(file)
-    require.NoError(t, err)
-    var compressedData bytes.Buffer
-    gzipWriter := gzip.NewWriter(&compressedData)
-    _, err = gzipWriter.Write(data)
-    require.NoError(t, err)
-    err = gzipWriter.Close()
-    require.NoError(t, err)
-    return compressedData.Bytes()
+	data, err := os.ReadFile(file)
+	require.NoError(t, err)
+	var compressedData bytes.Buffer
+	gzipWriter := gzip.NewWriter(&compressedData)
+	_, err = gzipWriter.Write(data)
+	require.NoError(t, err)
+	err = gzipWriter.Close()
+	require.NoError(t, err)
+	return compressedData.Bytes()
 }
 
 func TestConcurrentGzipReaderUsage(t *testing.T) {
-    // Create an encoding extension for cloudwatch format to test the
-    // gzip reader and check that it works as expected for non concurrent
-    // and concurrent usage
-    ext := &encodingExtension{
-        unmarshaler: subscriptionfilter.NewSubscriptionFilterUnmarshaler(component.BuildInfo{}),
-        format:      formatCloudWatchLogsSubscriptionFilter,
-        gzipPool:    sync.Pool{},
-    }
+	// Create an encoding extension for cloudwatch format to test the
+	// gzip reader and check that it works as expected for non concurrent
+	// and concurrent usage
+	ext := &encodingExtension{
+		unmarshaler: subscriptionfilter.NewSubscriptionFilterUnmarshaler(component.BuildInfo{}),
+		format:      formatCloudWatchLogsSubscriptionFilter,
+		gzipPool:    sync.Pool{},
+	}
 
-    cloudwatchData := readAndCompressLogFile(t, "testdata/cloudwatch_log.json")
-    testUnmarshall := func() {
-        _, err := ext.UnmarshalLogs(cloudwatchData)
-        require.NoError(t, err)
-    }
+	cloudwatchData := readAndCompressLogFile(t, "testdata/cloudwatch_log.json")
+	testUnmarshall := func() {
+		_, err := ext.UnmarshalLogs(cloudwatchData)
+		require.NoError(t, err)
+	}
 
-    // non concurrent
-    testUnmarshall()
-    // concurrent usage
-    concurrent := 20
-    wg := sync.WaitGroup{}
-    for i := 0; i < concurrent; i++ {
-        wg.Add(1)
-        go func() {
-            defer wg.Done()
-            testUnmarshall()
-        }()
-    }
-    wg.Wait()
+	// non concurrent
+	testUnmarshall()
+	// concurrent usage
+	concurrent := 20
+	wg := sync.WaitGroup{}
+	for i := 0; i < concurrent; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			testUnmarshall()
+		}()
+	}
+	wg.Wait()
 }
