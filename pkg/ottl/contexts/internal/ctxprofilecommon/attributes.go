ec31f289fdc6cdda6b99e5f0f4816fce051b1232
Florian Lehner <florian.lehner@elastic.co>
2025-08-20 14:38:44 +0200

    fixup: drop custom implementation
    Signed-off-by: Florian Lehner <florian.lehner@elastic.co>

--- pkg/ottl/contexts/internal/ctxprofilecommon/attributes.go
+++ pkg/ottl/contexts/internal/ctxprofilecommon/attributes.go
@@ -21,7 +21,7 @@ type ProfileAttributeContext interface {
 func AccessAttributes[K ProfileAttributeContext]() ottl.StandardGetSetter[K] {
 	return ottl.StandardGetSetter[K]{
 		Getter: func(_ context.Context, tCtx K) (any, error) {
-			return fromAttributeIndices(tCtx.GetProfilesDictionary().AttributeTable(), tCtx), nil
+			return pprofile.FromAttributeIndices(tCtx.GetProfilesDictionary().AttributeTable(), tCtx), nil
 		},
 		Setter: func(_ context.Context, tCtx K, val any) error {
 			m, err := ctxutil.GetMap(val)
@@ -29,7 +29,7 @@ func AccessAttributes[K ProfileAttributeContext]() ottl.StandardGetSetter[K] {
 				return err
 			}
 			for k, v := range m.All() {
-				if err := putAttribute(tCtx.GetProfilesDictionary().AttributeTable(), tCtx, k, v); err != nil {
+				if err := pprofile.PutAttribute(tCtx.GetProfilesDictionary().AttributeTable(), tCtx, k, v); err != nil {
 					return err
 				}
 			}
@@ -41,7 +41,8 @@ func AccessAttributes[K ProfileAttributeContext]() ottl.StandardGetSetter[K] {
 func AccessAttributesKey[K ProfileAttributeContext](key []ottl.Key[K]) ottl.StandardGetSetter[K] {
 	return ottl.StandardGetSetter[K]{
 		Getter: func(ctx context.Context, tCtx K) (any, error) {
-			return ctxutil.GetMapValue[K](ctx, tCtx, fromAttributeIndices(tCtx.GetProfilesDictionary().AttributeTable(), tCtx), key)
+			return ctxutil.GetMapValue[K](ctx, tCtx,
+				pprofile.FromAttributeIndices(tCtx.GetProfilesDictionary().AttributeTable(), tCtx), key)
 		},
 		Setter: func(ctx context.Context, tCtx K, val any) error {
 			newKey, err := ctxutil.GetMapKeyName(ctx, tCtx, key[0])
@@ -52,7 +53,7 @@ func AccessAttributesKey[K ProfileAttributeContext](key []ottl.Key[K]) ottl.Stan
 			if err := ctxutil.SetIndexableValue[K](ctx, tCtx, v, val, key[1:]); err != nil {
 				return err
 			}
-			return putAttribute(tCtx.GetProfilesDictionary().AttributeTable(), tCtx, *newKey, v)
+			return pprofile.PutAttribute(tCtx.GetProfilesDictionary().AttributeTable(), tCtx, *newKey, v)
 		},
 	}
 }
@@ -73,54 +74,4 @@ func getAttributeValue[K ProfileAttributeContext](tCtx K, key string) pcommon.Va
 	}
 
 	return pcommon.NewValueEmpty()
-}
-
-// fromAttributeIndices creates a pcommon.Map from the attribute indices in the provided context
-func fromAttributeIndices(attributeTable pprofile.AttributeTableSlice, ctx ProfileAttributeContext) pcommon.Map {
-	m := pcommon.NewMap()
-	indices := ctx.AttributeIndices().AsRaw()
-
-	for _, tableIndex := range indices {
-		if int(tableIndex) < attributeTable.Len() {
-			attr := attributeTable.At(int(tableIndex))
-			attr.Value().CopyTo(m.PutEmpty(attr.Key()))
-		}
-	}
-
-	return m
-}
-
-// putAttribute adds or updates an attribute in the attribute table and updates the context's attribute indices
-func putAttribute(attributeTable pprofile.AttributeTableSlice, ctx ProfileAttributeContext, key string, value pcommon.Value) error {
-	// First, check if the attribute already exists in the context's indices
-	indices := ctx.AttributeIndices()
-	existingIndex := -1
-
-	for i := 0; i < indices.Len(); i++ {
-		tableIndex := indices.At(i)
-		if int(tableIndex) < attributeTable.Len() {
-			attr := attributeTable.At(int(tableIndex))
-			if attr.Key() == key {
-				existingIndex = int(tableIndex)
-				break
-			}
-		}
-	}
-
-	if existingIndex >= 0 {
-		// Update existing attribute
-		attr := attributeTable.At(existingIndex)
-		value.CopyTo(attr.Value())
-	} else {
-		// Add new attribute to the table
-		newAttr := attributeTable.AppendEmpty()
-		newAttr.SetKey(key)
-		value.CopyTo(newAttr.Value())
-
-		// Add the new index to the context's attribute indices
-		newIndex := int32(attributeTable.Len() - 1)
-		indices.Append(newIndex)
-	}
-
-	return nil
 }
