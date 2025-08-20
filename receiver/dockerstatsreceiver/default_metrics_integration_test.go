515b9e0cd7708dc24a8baa79a14666b94bcdeeba
Paulo Janotti <pjanotti@splunk.com>
2025-08-19 19:01:55 -0700

    Set expected data to match a container started on the test

--- receiver/dockerstatsreceiver/default_metrics_integration_test.go
+++ receiver/dockerstatsreceiver/default_metrics_integration_test.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/internal/coreinternal/scraperinttest"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/pdatatest/pmetrictest"
+	"github.com/testcontainers/testcontainers-go"
 )
 
 func TestIntegration(t *testing.T) {
@@ -19,9 +20,21 @@ func TestIntegration(t *testing.T) {
 		t.Skip("Skipping test on Darwin GH runners: test requires Docker service")
 	}
 
+	// Start a docker container to ensure container metrics are available
 	scraperinttest.NewIntegrationTest(
 		NewFactory(),
+		scraperinttest.WithContainerRequest(
+			testcontainers.ContainerRequest{
+				Image: "alpine:latest",
+				Name:  "dockerstatsreceiver-test",
+			},
+		),
 		scraperinttest.WithCompareOptions(
+			pmetrictest.IgnoreResourceAttributeValue("container.hostname"),
+			pmetrictest.IgnoreResourceAttributeValue("container.id"),
+			pmetrictest.IgnoreResourceAttributeValue("container.image.name"),
+			pmetrictest.IgnoreResourceAttributeValue("container.name"),
+			pmetrictest.IgnoreResourceAttributeValue("container.runtime"),
 			pmetrictest.IgnoreMetricValues(),
 			pmetrictest.IgnoreMetricDataPointsOrder(),
 			pmetrictest.IgnoreStartTimestamp(),
