b18d003c2eb8000f79e0a4423c20ed291a7f5867
Jonathan Coates <git@squiddev.cc>
2025-08-24 15:06:08 +0100

    [systemdreceiver] Scraping unit status

--- receiver/systemdreceiver/factory.go
+++ receiver/systemdreceiver/factory.go
@@ -5,12 +5,22 @@ package systemdreceiver // import "github.com/open-telemetry/opentelemetry-colle
 
 import (
 	"context"
+	"errors"
+	"runtime"
+	"time"
 
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/receiver"
+	"go.opentelemetry.io/collector/scraper"
+	"go.opentelemetry.io/collector/scraper/scraperhelper"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/systemdreceiver/internal/metadata"
+)
+
+var (
+	errConfigNotValid = errors.New("config not valid for the systemd receiver")
+	errNonLinux       = errors.New("systemd receiver is only supported on Linux")
 )
 
 // NewFactory creates a factory for systemd receiver.
@@ -22,14 +32,32 @@ func NewFactory() receiver.Factory {
 }
 
 func createDefaultConfig() component.Config {
-	return &Config{}
+	cfg := scraperhelper.NewDefaultControllerConfig()
+	cfg.CollectionInterval = 60 * time.Second
+
+	return &Config{
+		ControllerConfig:     cfg,
+		MetricsBuilderConfig: metadata.DefaultMetricsBuilderConfig(),
+		Scope:                "system",
+		Units:                []string{"*.service"},
+	}
 }
 
-func createMetricsReceiver(
-	_ context.Context,
-	_ receiver.Settings,
-	_ component.Config,
-	_ consumer.Metrics,
-) (receiver.Metrics, error) {
-	return systemdReceiver{}, nil
+func createMetricsReceiver(_ context.Context, params receiver.Settings, rConf component.Config, consumer consumer.Metrics) (receiver.Metrics, error) {
+	if runtime.GOOS != "linux" {
+		return nil, errNonLinux
+	}
+
+	cfg, ok := rConf.(*Config)
+	if !ok {
+		return nil, errConfigNotValid
+	}
+
+	systemdScraper := newScraper(cfg, params)
+	s, err := scraper.NewMetrics(systemdScraper.scrape, scraper.WithStart(systemdScraper.start), scraper.WithShutdown(systemdScraper.shutdown))
+	if err != nil {
+		return nil, err
+	}
+
+	return scraperhelper.NewMetricsController(&cfg.ControllerConfig, params, consumer, scraperhelper.AddScraper(metadata.Type, s))
 }
