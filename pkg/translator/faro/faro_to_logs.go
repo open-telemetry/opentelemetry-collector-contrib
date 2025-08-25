e78e581ff2a915a34aca5f9afe16c13c79482b30
Irina Marchuk <mar4ukira@gmail.com>
2025-08-25 09:05:42 +0100

    [translator/faro] Add Timestamp, ObservedTimestamp, SpanId, and TraceId fields to Log Record (#40800)
    <!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
    Ex. Adding a feature - Explain what this achieves.-->
    #### Description
    When converting telemetry data from Faro format into OTLP format, the
    fields Timestamp, ObservedTimestamp, SpanId, and TraceId were not set.
    This PR aims to set those fields to the Log Record 
    
    <!--Describe what testing was performed and which tests were added.-->
    #### Testing
    Unit tests were updated

--- pkg/translator/faro/faro_to_logs.go
+++ pkg/translator/faro/faro_to_logs.go
@@ -5,6 +5,8 @@ package faro // import "github.com/open-telemetry/opentelemetry-collector-contri
 
 import (
 	"context"
+	"encoding/hex"
+	"fmt"
 	"regexp"
 	"strconv"
 	"time"
@@ -12,6 +14,7 @@ import (
 	"github.com/go-logfmt/logfmt"
 	faroTypes "github.com/grafana/faro/pkg/go"
 	"github.com/zeebo/xxh3"
+	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/plog"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
@@ -20,10 +23,11 @@ import (
 )
 
 type kvTime struct {
-	kv   *keyVal
-	ts   time.Time
-	kind faroTypes.Kind
-	hash uint64
+	kv    *keyVal
+	ts    time.Time
+	kind  faroTypes.Kind
+	hash  uint64
+	trace faroTypes.TraceContext
 }
 
 var (
@@ -75,31 +79,35 @@ func TranslateToLogs(ctx context.Context, payload faroTypes.Payload) (plog.Logs,
 
 	for _, logItem := range payload.Logs {
 		kvList = append(kvList, &kvTime{
-			kv:   logToKeyVal(logItem),
-			ts:   logItem.Timestamp,
-			kind: faroTypes.KindLog,
+			kv:    logToKeyVal(logItem),
+			ts:    logItem.Timestamp,
+			kind:  faroTypes.KindLog,
+			trace: logItem.Trace,
 		})
 	}
 	for _, exception := range payload.Exceptions {
 		kvList = append(kvList, &kvTime{
-			kv:   exceptionToKeyVal(exception),
-			ts:   exception.Timestamp,
-			kind: faroTypes.KindException,
-			hash: xxh3.HashString(drainExceptionValue(exception.Value)),
+			kv:    exceptionToKeyVal(exception),
+			ts:    exception.Timestamp,
+			kind:  faroTypes.KindException,
+			hash:  xxh3.HashString(drainExceptionValue(exception.Value)),
+			trace: exception.Trace,
 		})
 	}
 	for _, measurement := range payload.Measurements {
 		kvList = append(kvList, &kvTime{
-			kv:   measurementToKeyVal(measurement),
-			ts:   measurement.Timestamp,
-			kind: faroTypes.KindMeasurement,
+			kv:    measurementToKeyVal(measurement),
+			ts:    measurement.Timestamp,
+			kind:  faroTypes.KindMeasurement,
+			trace: measurement.Trace,
 		})
 	}
 	for _, event := range payload.Events {
 		kvList = append(kvList, &kvTime{
-			kv:   eventToKeyVal(event),
-			ts:   event.Timestamp,
-			kind: faroTypes.KindEvent,
+			kv:    eventToKeyVal(event),
+			ts:    event.Timestamp,
+			kind:  faroTypes.KindEvent,
+			trace: event.Trace,
 		})
 	}
 	span.SetAttributes(attribute.Int("count", len(kvList)))
@@ -134,6 +142,37 @@ func TranslateToLogs(ctx context.Context, payload faroTypes.Payload) (plog.Logs,
 		logRecord.Attributes().PutStr(faroKind, string(i.kind))
 		if (i.kind == faroTypes.KindException) && (i.hash != 0) {
 			logRecord.Attributes().PutStr(faroExceptionHash, strconv.FormatUint(i.hash, 10))
+		}
+		observedTimestamp := pcommon.NewTimestampFromTime(time.Now())
+		logRecord.SetObservedTimestamp(observedTimestamp)
+		if !i.ts.IsZero() {
+			logRecord.SetTimestamp(pcommon.NewTimestampFromTime(i.ts))
+		}
+
+		spanID := i.trace.SpanID
+		if spanID != "" {
+			var spanIDBytes [8]byte
+			byteSlice, err := hex.DecodeString(spanID)
+			// If there is an error, we skip the log record
+			if err != nil {
+				errs = multierr.Append(errs, fmt.Errorf("failed to decode spanID=%s: %w", spanID, err))
+				continue
+			}
+			copy(spanIDBytes[:], byteSlice)
+			logRecord.SetSpanID(spanIDBytes)
+		}
+
+		traceID := i.trace.TraceID
+		if traceID != "" {
+			var traceIDBytes [16]byte
+			byteSlice, err := hex.DecodeString(traceID)
+			// If there is an error, we skip the log record
+			if err != nil {
+				errs = multierr.Append(errs, fmt.Errorf("failed to decode traceID=%s: %w", traceID, err))
+				continue
+			}
+			copy(traceIDBytes[:], byteSlice)
+			logRecord.SetTraceID(traceIDBytes)
 		}
 	}
 	return logs, errs
