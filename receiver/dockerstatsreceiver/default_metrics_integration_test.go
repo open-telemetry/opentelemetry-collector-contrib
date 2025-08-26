7f8b126d419d31c94e142e3003080b87176ddf54
Paulo Janotti <pjanotti@splunk.com>
2025-08-26 10:38:27 -0700

    Revert "[chore] Skip test while investigating flakiness"
    This reverts commit 23233b679843861fc083beccd4d2c9d437b9354e.

--- receiver/dockerstatsreceiver/default_metrics_integration_test.go
+++ receiver/dockerstatsreceiver/default_metrics_integration_test.go
@@ -7,6 +7,7 @@ package dockerstatsreceiver
 
 import (
 	"os"
+	"runtime"
 	"testing"
 
 	"github.com/testcontainers/testcontainers-go"
@@ -16,8 +17,8 @@ import (
 )
 
 func TestIntegration(t *testing.T) {
-	if os.Getenv("GITHUB_ACTIONS") == "true" {
-		t.Skip("Skipping test on GH runners: until flakiness is investigated")
+	if runtime.GOOS == "darwin" && os.Getenv("GITHUB_ACTIONS") == "true" {
+		t.Skip("Skipping test on Darwin GH runners: test requires Docker service")
 	}
 
 	// Start a docker container to ensure container metrics are available
