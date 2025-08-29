1781e2fc78123910c1b0992ac20ce02f9669008c
Juraj Michálek <juraj.michalek132@gmail.com>
2025-08-29 12:04:59 +0200

    [chore] telemetry gen update metric type doc string (#42344)
    <!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
    Ex. Adding a feature - Explain what this achieves.-->
    #### Description
    
    Updated documentation string for metric type in telemetry gen to reflect
    histogram type also supported
    
    https://github.com/jmichalek132/opentelemetry-collector-contrib/blob/f6df0c0039ff0062c450e360747b67407bf74aae/cmd/telemetrygen/pkg/metrics/metrics_types.go#L30
    
    <!-- Issue number (e.g. #1234) or full URL to issue, if applicable. -->
    #### Link to tracking issue
    Fixes
    
    <!--Describe what testing was performed and which tests were added.-->
    #### Testing
    
    <!--Describe the documentation added.-->
    #### Documentation
    
    <!--Please delete paragraphs that you did not use before submitting.-->

--- cmd/telemetrygen/pkg/metrics/config.go
+++ cmd/telemetrygen/pkg/metrics/config.go
@@ -46,7 +46,7 @@ func (c *Config) Flags(fs *pflag.FlagSet) {
 	fs.StringVar(&c.TraceID, "trace-id", c.TraceID, "TraceID to use as exemplar")
 	fs.StringVar(&c.SpanID, "span-id", c.SpanID, "SpanID to use as exemplar")
 
-	fs.Var(&c.MetricType, "metric-type", "Metric type enum. must be one of 'Gauge' or 'Sum'")
+	fs.Var(&c.MetricType, "metric-type", "Metric type enum. must be one of 'Gauge', 'Sum' or 'Histogram'")
 	fs.Var(&c.AggregationTemporality, "aggregation-temporality", "aggregation-temporality for metrics. Must be one of 'delta' or 'cumulative'")
 	fs.BoolVar(&c.EnforceUniqueTimeseries, "unique-timeseries", c.EnforceUniqueTimeseries, "Enforce unique timeseries within unique-timeseries-timelimit, performance impacting")
 	fs.DurationVar(&c.UniqueTimelimit, "unique-timeseries-duration", c.UniqueTimelimit, "Time limit for unique timeseries generation, timeseries generated within this time will be unique")
