dd005c56db6e3fd0a8be0b44352824eb45a2aec2
Israel Blancas <iblancasa@gmail.com>
2025-08-29 03:19:04 +0200

    Expose some metrics for the opamp supervisor (#40166)
    Co-authored-by: Evan Bradley <11745660+evan-bradley@users.noreply.github.com>

--- cmd/opampsupervisor/supervisor/supervisor_test.go
+++ cmd/opampsupervisor/supervisor/supervisor_test.go
@@ -32,10 +32,12 @@ import (
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/config/confighttp"
 	"go.opentelemetry.io/collector/pdata/plog"
+	"go.opentelemetry.io/otel/sdk/metric"
 	"go.uber.org/zap"
 	"google.golang.org/protobuf/proto"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/cmd/opampsupervisor/supervisor/config"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/cmd/opampsupervisor/supervisor/telemetry"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/testbed/testbed"
 )
 
@@ -750,6 +752,13 @@ service:
 
 		configStorageDir := t.TempDir()
 
+		mp := metric.NewMeterProvider()
+		metrics, err := telemetry.NewMetrics(mp)
+		require.NoError(t, err)
+		defer func() {
+			_ = mp.Shutdown(t.Context())
+		}()
+
 		s := Supervisor{
 			telemetrySettings: newNopTelemetrySettings(),
 			pidProvider:       defaultPIDProvider{},
@@ -768,6 +777,7 @@ service:
 			cfgState:                       &atomic.Value{},
 			customMessageToServer:          make(chan *protobufs.CustomMessage, 10),
 			doneChan:                       make(chan struct{}),
+			metrics:                        metrics,
 		}
 
 		require.NoError(t, s.createTemplates())
@@ -1210,6 +1220,12 @@ func Test_handleAgentOpAMPMessage(t *testing.T) {
 		}
 
 		testUUID := uuid.MustParse("018fee23-4a51-7303-a441-73faed7d9deb")
+		mp := metric.NewMeterProvider()
+		metrics, err := telemetry.NewMetrics(mp)
+		require.NoError(t, err)
+		defer func() {
+			_ = mp.Shutdown(t.Context())
+		}()
 		s := Supervisor{
 			telemetrySettings:              newNopTelemetrySettings(),
 			pidProvider:                    defaultPIDProvider{},
@@ -1224,6 +1240,7 @@ func Test_handleAgentOpAMPMessage(t *testing.T) {
 			doneChan:                       make(chan struct{}),
 			agentReadyChan:                 make(chan struct{}),
 			agentReady:                     atomic.Bool{},
+			metrics:                        metrics,
 		}
 
 		s.handleAgentOpAMPMessage(&mockConn{}, &protobufs.AgentToServer{
