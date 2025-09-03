5f2ddda5df27bd3d4927d45d2aa31a64b987cda2
Pablo Baeyens <pbaeyens31+github@gmail.com>
2025-09-03 16:12:48 +0200

    Add references to Github issues

--- connector/spanmetricsconnector/connector_test.go
+++ connector/spanmetricsconnector/connector_test.go
@@ -905,6 +905,7 @@ func TestConsumeTraces(t *testing.T) {
 
 				// Trigger flush.
 				mockClock.Advance(time.Nanosecond)
+				// TODO: Remove time.Sleep call, see https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/42461
 				time.Sleep(10 * time.Millisecond)
 				require.Eventually(t, func() bool {
 					return len(mcon.AllMetrics()) > 0
--- extension/k8sleaderelector/extension_test.go
+++ extension/k8sleaderelector/extension_test.go
@@ -103,6 +103,7 @@ func TestExtension_WithDelay(t *testing.T) {
 
 	// Simulate a delay of setting up callbacks after the leader has been elected.
 	expectedLeaseDurationSeconds := ptr.To(int32(15))
+	// TODO: Remove time.Sleep below, see https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/42460
 	time.Sleep(100 * time.Millisecond)
 	require.Eventually(t, func() bool {
 		lease, err := fakeClient.CoordinationV1().Leases("default").Get(ctx, "foo", metav1.GetOptions{})
