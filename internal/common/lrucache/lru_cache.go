1359f4c795b98a0c947985374941a0a051742a7c
Florian Bacher <florian.bacher@dynatrace.com>
2025-09-04 10:29:40 +0200

    add license header
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- internal/common/lrucache/lru_cache.go
+++ internal/common/lrucache/lru_cache.go
@@ -1,3 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
 package lrucache // import "github.com/open-telemetry/opentelemetry-collector-contrib/internal/common/lrucache"
 
 import (
