aea4013718b1176f607586df757238fef6665797
Paulo Dias <paulodias.gm@gmail.com>
2025-09-05 11:20:01 +0100

    fix: update metadata.yaml to avoid error of status must be empty for subcomponents
    Signed-off-by: Paulo Dias <paulodias.gm@gmail.com>

--- processor/resourcedetectionprocessor/internal/hetzner/internal/metadata/generated_config.go
+++ processor/resourcedetectionprocessor/internal/hetzner/internal/metadata/generated_config.go
@@ -25,7 +25,7 @@ func (rac *ResourceAttributeConfig) Unmarshal(parser *confmap.Conf) error {
 	return nil
 }
 
-// ResourceAttributesConfig provides config for resourcedetectionprocessor/hetzner resource attributes.
+// ResourceAttributesConfig provides config for hetznerdetector resource attributes.
 type ResourceAttributesConfig struct {
 	CloudAvailabilityZone ResourceAttributeConfig `mapstructure:"cloud.availability_zone"`
 	CloudProvider         ResourceAttributeConfig `mapstructure:"cloud.provider"`
