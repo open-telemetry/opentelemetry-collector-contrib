fd23378c9d41ca109d5d2edf173d68931c9c9f13
Arthur Silva Sens <arthursens2005@gmail.com>
2025-09-09 18:29:37 -0300

    Address comments
    Signed-off-by: Arthur Silva Sens <arthursens2005@gmail.com>

--- pkg/translator/prometheusremotewrite/histograms_v2_test.go
+++ pkg/translator/prometheusremotewrite/histograms_v2_test.go
@@ -742,7 +742,8 @@ func TestPrometheusConverterV2_addExponentialHistogramDataPoints(t *testing.T) {
 			}
 			converter := newPrometheusConverterV2(Settings{})
 			metricNamer := otlptranslator.MetricNamer{WithMetricSuffixes: true}
-			metricName, _ := metricNamer.Build(prom.TranslatorMetricFromOtelMetric(metric))
+			metricName, err := metricNamer.Build(prom.TranslatorMetricFromOtelMetric(metric))
+			require.NoError(t, err)
 			require.NoError(t, converter.addExponentialHistogramDataPoints(
 				metric.ExponentialHistogram().DataPoints(),
 				pcommon.NewResource(),
