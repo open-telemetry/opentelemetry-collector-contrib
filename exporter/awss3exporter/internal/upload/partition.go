c45016257d4adc3a1ac2feeb4b6fe4cbac08d0a0
Gene Hynson <ghynson@montecarlodata.com>
2025-09-12 10:38:13 -0700

    fix: fmt

--- exporter/awss3exporter/internal/upload/partition.go
+++ exporter/awss3exporter/internal/upload/partition.go
@@ -71,7 +71,7 @@ func (pki *PartitionKeyBuilder) bucketKeyPrefix(ts time.Time, overridePrefix str
 		pathParts = append(pathParts, prefix)
 	}
   
-  location := pki.PartitionTimeLocation
+    location := pki.PartitionTimeLocation
 	if location == nil {
 		location = time.Local
 	}
