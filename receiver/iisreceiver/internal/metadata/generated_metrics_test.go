49cfc3dedb6b877fb8cb8dd46fddc0b6c12e99e5
Paulo Janotti <pjanotti@splunk.com>
2025-09-13 23:48:53 -0700

    [chore][doc] Update `iis.application_pool.state` metric description (#42649)
    Update `iis.application_pool.state` metric description per installed
    counter information of IIS 10.
    
    Fix #42586

--- receiver/iisreceiver/internal/metadata/generated_metrics_test.go
+++ receiver/iisreceiver/internal/metadata/generated_metrics_test.go
@@ -154,7 +154,7 @@ func TestMetricsBuilder(t *testing.T) {
 					validatedMetrics["iis.application_pool.state"] = true
 					assert.Equal(t, pmetric.MetricTypeGauge, ms.At(i).Type())
 					assert.Equal(t, 1, ms.At(i).Gauge().DataPoints().Len())
-					assert.Equal(t, "The current state of the application pool. (0 - Starting, 1 - Started, 2 - Stopping, 3 - Stopped, 4 - Unknown)", ms.At(i).Description())
+					assert.Equal(t, "The current status of the application pool (1 - Uninitialized, 2 - Initialized, 3 - Running, 4 - Disabling, 5 - Disabled, 6 - Shutdown Pending, 7 - Delete Pending).", ms.At(i).Description())
 					assert.Equal(t, "{state}", ms.At(i).Unit())
 					dp := ms.At(i).Gauge().DataPoints().At(0)
 					assert.Equal(t, start, dp.StartTimestamp())
