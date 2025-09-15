9c8476657d20f33b79f2f0a967100a3bfb89764c
tanushangural <tangural@newrelic.com>
2025-09-15 20:15:56 +0530

    remove dups file

--- receiver/newrelicsqlserverreceiver/factory.go
+++ receiver/newrelicsqlserverreceiver/factory.go
@@ -5,14 +5,13 @@ package newrelicsqlserverreceiver // import "github.com/open-telemetry/opentelem
 
 import (
 	"context"
+	"time"
 
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer"
-	"go.opentelemetry.io/collector/pdata/pmetric"
 	"go.opentelemetry.io/collector/receiver"
 	"go.opentelemetry.io/collector/scraper"
 	"go.opentelemetry.io/collector/scraper/scraperhelper"
-	"go.uber.org/zap"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/newrelicsqlserverreceiver/internal/metadata"
 )
@@ -26,7 +25,19 @@ func NewFactory() receiver.Factory {
 }
 
 func createDefaultConfig() component.Config {
-	return DefaultConfig()
+	cfg := scraperhelper.NewDefaultControllerConfig()
+	cfg.CollectionInterval = 10 * time.Second
+
+	return &Config{
+		ControllerConfig:     cfg,
+		Hostname:             "localhost",
+		Port:                 "1433",
+		Username:             "",
+		Password:             "",
+		EnableBufferMetrics:  true,
+		MaxConcurrentWorkers: 5,
+		Timeout:              30 * time.Second,
+	}
 }
 
 func createMetricsReceiver(
@@ -37,17 +48,11 @@ func createMetricsReceiver(
 ) (receiver.Metrics, error) {
 	cfg := rConf.(*Config)
 
-	// Create the main SQL Server scraper
-	sqlServerScraper, err := newSQLServerScraper(params, cfg)
-	if err != nil {
-		return nil, err
-	}
-
-	// Create scraper with lifecycle methods
+	ns := newSqlServerScraper(params, cfg)
 	s, err := scraper.NewMetrics(
-		sqlServerScraper.scrape,
-		scraper.WithStart(sqlServerScraper.start),
-		scraper.WithShutdown(sqlServerScraper.shutdown))
+		ns.scrape,
+		scraper.WithStart(ns.start),
+		scraper.WithShutdown(ns.shutdown))
 	if err != nil {
 		return nil, err
 	}
@@ -56,93 +61,4 @@ func createMetricsReceiver(
 		&cfg.ControllerConfig, params, consumer,
 		scraperhelper.AddScraper(metadata.Type, s),
 	)
-}
-
-// sqlServerScraper is the main scraper that orchestrates all SQL Server metric collection
-type sqlServerScraper struct {
-	connection         *SQLConnection
-	coreMetricsScraper *coreMetricsScraper
-	logger             *zap.Logger
-	config             *Config
-}
-
-// newSQLServerScraper creates a new SQL Server scraper instance
-func newSQLServerScraper(params receiver.Settings, config *Config) (*sqlServerScraper, error) {
-	logger := params.Logger
-
-	// Create SQL connection - we'll initialize it in start() method
-	var connection *SQLConnection
-
-	// Create core metrics scraper
-	coreMetricsScraper := newCoreMetricsScraper(connection, config, logger)
-
-	return &sqlServerScraper{
-		connection:         connection,
-		coreMetricsScraper: coreMetricsScraper,
-		logger:             logger,
-		config:             config,
-	}, nil
-}
-
-// start initializes the scraper and establishes database connections
-func (s *sqlServerScraper) start(ctx context.Context, _ component.Host) error {
-	s.logger.Info("Starting New Relic SQL Server receiver")
-
-	// Create and test the connection
-	connection, err := NewSQLConnection(ctx, s.config, s.logger)
-	if err != nil {
-		s.logger.Error("Failed to connect to SQL Server", zap.Error(err))
-		return err
-	}
-	s.connection = connection
-
-	// Test the connection with ping
-	if err := s.connection.Ping(ctx); err != nil {
-		s.logger.Error("Failed to ping SQL Server", zap.Error(err))
-		return err
-	}
-
-	s.logger.Info("Successfully connected to SQL Server",
-		zap.String("hostname", s.config.Hostname),
-		zap.String("port", s.config.Port),
-		zap.String("instance", s.config.Instance))
-
-	return nil
-}
-
-// shutdown closes the database connection and cleans up resources
-func (s *sqlServerScraper) shutdown(ctx context.Context) error {
-	s.logger.Info("Shutting down New Relic SQL Server receiver")
-
-	if s.connection != nil {
-		s.connection.Close()
-	}
-
-	return nil
-}
-
-// scrape collects metrics from SQL Server
-func (s *sqlServerScraper) scrape(ctx context.Context) (pmetric.Metrics, error) {
-	s.logger.Debug("Scraping SQL Server metrics")
-
-	// Create metrics container
-	metrics := pmetric.NewMetrics()
-
-	// Collect core metrics
-	coreMetrics, err := s.coreMetricsScraper.scrape(ctx)
-	if err != nil {
-		s.logger.Error("Failed to collect core metrics", zap.Error(err))
-		return metrics, err
-	}
-
-	// Merge core metrics
-	coreMetrics.ResourceMetrics().MoveAndAppendTo(metrics.ResourceMetrics())
-
-	// Note: Query performance monitoring metrics are handled by a separate scraper in the controller
-	// The scraperhelper.NewMetricsController will handle multiple scrapers if we add them
-
-	s.logger.Debug("Successfully scraped SQL Server metrics",
-		zap.Int("resource_metrics_count", metrics.ResourceMetrics().Len()))
-
-	return metrics, nil
 }
--- receiver/newrelicsqlserverreceiver/go.mod
+++ receiver/newrelicsqlserverreceiver/go.mod
@@ -1,26 +1,14 @@
-module github.com/open-telemetry/opentelemetry-collector-contrib
+module github.com/open-telemetry/opentelemetry-collector-contrib/receiver/newrelicsqlserverreceiver
 
-// NOTE:
-// This go.mod is NOT used to build any official binary.
-// To see the builder manifests used for official binaries,
-// check https://github.com/open-telemetry/opentelemetry-collector-releases
-//
-// For the OpenTelemetry Collector Contrib distribution specifically, see
-// https://github.com/open-telemetry/opentelemetry-collector-releases/tree/main/distributions/otelcol-contrib
+go 1.24.0
 
-go 1.24
-
-retract (
-	v0.76.2
-	v0.76.1
-	v0.65.0
-	v0.37.0 // Contains dependencies on v0.36.0 components, which should have been updated to v0.37.0.
-)
+toolchain go1.24.3
 
 require (
 	github.com/jmoiron/sqlx v1.4.0
-	github.com/microsoft/go-mssqldb v1.9.3
+	github.com/microsoft/go-mssqldb v1.7.2
 	github.com/stretchr/testify v1.11.1
+	github.com/testcontainers/testcontainers-go v0.38.0
 	go.opentelemetry.io/collector/component v1.41.0
 	go.opentelemetry.io/collector/confmap v1.41.0
 	go.opentelemetry.io/collector/consumer v1.41.0
@@ -35,13 +23,13 @@ require (
 
 require (
 	dario.cat/mergo v1.0.1 // indirect
-	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.18.0 // indirect
-	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.10.1 // indirect
-	github.com/Azure/azure-sdk-for-go/sdk/internal v1.11.1 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.9.1 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.5.1 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/internal v1.5.1 // indirect
 	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
-	github.com/AzureAD/microsoft-authentication-library-for-go v1.4.2 // indirect
+	github.com/AzureAD/microsoft-authentication-library-for-go v1.2.1 // indirect
 	github.com/Microsoft/go-winio v0.6.2 // indirect
-	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/containerd/errdefs v1.0.0 // indirect
 	github.com/containerd/errdefs/pkg v0.3.0 // indirect
 	github.com/containerd/log v0.1.0 // indirect
@@ -60,7 +48,7 @@ require (
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.0 // indirect
 	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
 	github.com/golang-sql/sqlexp v0.1.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
@@ -93,7 +81,6 @@ require (
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/shirou/gopsutil/v4 v4.25.5 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
-	github.com/testcontainers/testcontainers-go v0.38.0 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
@@ -110,18 +97,20 @@ require (
 	go.opentelemetry.io/contrib/bridges/otelzap v0.12.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.49.0 // indirect
 	go.opentelemetry.io/otel v1.38.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.38.0 // indirect
 	go.opentelemetry.io/otel/log v0.14.0 // indirect
 	go.opentelemetry.io/otel/metric v1.38.0 // indirect
 	go.opentelemetry.io/otel/sdk v1.38.0 // indirect
 	go.opentelemetry.io/otel/sdk/metric v1.38.0 // indirect
 	go.opentelemetry.io/otel/trace v1.38.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.8.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
-	golang.org/x/crypto v0.39.0 // indirect
-	golang.org/x/net v0.41.0 // indirect
+	golang.org/x/crypto v0.41.0 // indirect
+	golang.org/x/net v0.43.0 // indirect
 	golang.org/x/sys v0.35.0 // indirect
-	golang.org/x/text v0.26.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250707201910-8d1bb00bc6a7 // indirect
+	golang.org/x/text v0.28.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250825161204-c5933d9347a5 // indirect
 	google.golang.org/grpc v1.75.0 // indirect
 	google.golang.org/protobuf v1.36.8 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
