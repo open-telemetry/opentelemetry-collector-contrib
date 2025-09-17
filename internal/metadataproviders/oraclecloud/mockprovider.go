d9f4d2d1517092534adba72f0f144d5463d8f8d4
Dave Cadwallader <dave.cadwallader@oracle.com>
2025-09-17 10:21:26 -0700

    add missing vanity imports

--- internal/metadataproviders/oraclecloud/mockprovider.go
+++ internal/metadataproviders/oraclecloud/mockprovider.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package oraclecloud 
+package oraclecloud // import "github.com/open-telemetry/opentelemetry-collector-contrib/internal/metadataproviders/oraclecloud"
 
 import (
 	"context"
