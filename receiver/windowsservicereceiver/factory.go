b5341a6c5bb6cb0b51d49176918eaf84deb71243
merdzikdominik <dominik.merdzik@onet.pl>
2025-09-21 12:52:48 +0200

    Update factory.go
    (1) Errors are collected and returned collectively at the end (errsCh channel + multierr.Append).
    
    (2) Scrape is parallel (errgroup + concurrency-limiting semaphore).
    
    (3) Status is cast directly from svc.status.State with range validation.
    
    (4) Removed an unnecessary, unused bool from mapStartTypeToAttr; start has a clean signature.
    
    (5) Method names indicate the mutation: updateService, updateStatus, updateConfig.

--- receiver/windowsservicereceiver/factory.go
+++ receiver/windowsservicereceiver/factory.go
@@ -1,9 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-//go:build windows
-
-package windowsservicereceiver
+package windowsservicereceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/windowsservicereceiver"
 
 import (
 	"time"
