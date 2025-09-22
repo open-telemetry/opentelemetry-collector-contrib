927b023a4c3ed88f9bfff824a158c2a50c882dfe
merdzikdominik <dominik.merdzik@onet.pl>
2025-09-22 23:21:07 +0200

    Update factory_others.go

--- receiver/windowsservicereceiver/factory_others.go
+++ receiver/windowsservicereceiver/factory_others.go
@@ -7,15 +7,38 @@ package windowsservicereceiver // import "github.com/open-telemetry/opentelemetr
 import (
 	"context"
 	"errors"
+	"time"
 
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/receiver"
+	"go.opentelemetry.io/collector/scraper/scraperhelper"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/windowsservicereceiver/internal/metadata"
 )
 
 var errUnsupportedOS = errors.New("windowsservicereceiver: supported only on Windows")
 
-func createMetricsReceiver(
+func NewFactory() receiver.Factory {
+	return receiver.NewFactory(
+		metadata.Type,
+		createDefaultConfig,
+		receiver.WithMetrics(createUnsupported, metadata.MetricsStability),
+	)
+}
+
+func createDefaultConfig() component.Config {
+	return &Config{
+		ControllerConfig: scraperhelper.ControllerConfig{
+			CollectionInterval: 1 * time.Minute,
+		},
+		MetricsBuilderConfig: metadata.DefaultMetricsBuilderConfig(),
+		IncludeServices:      nil,
+		ExcludeServices:      nil,
+	}
+}
+
+func createUnsupported(
 	_ context.Context,
 	_ receiver.Settings,
 	_ component.Config,
