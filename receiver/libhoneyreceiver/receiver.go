06c733d3442a2d29b69c0ac3c1ce35e87c79e281
Mike Terhar <mike@terhar.com>
2025-09-24 14:29:48 +0000

    add debug log

--- receiver/libhoneyreceiver/receiver.go
+++ receiver/libhoneyreceiver/receiver.go
@@ -135,6 +135,7 @@ func (r *libhoneyReceiver) registerLogConsumer(tc consumer.Logs) {
 }
 
 func (r *libhoneyReceiver) handleAuth(resp http.ResponseWriter, req *http.Request) {
+	r.settings.Logger.Debug("handling auth request", zap.String("auth_api", r.cfg.AuthAPI))
 	authURL := fmt.Sprintf("%s/1/auth", r.cfg.AuthAPI)
 	authReq, err := http.NewRequest(http.MethodGet, authURL, http.NoBody)
 	if err != nil {
