cb39d56909866c0bd4e0a738b0256f8f061a5c0b
Keith Schmitt <32067685+schmikei@users.noreply.github.com>
2025-09-24 21:41:56 -0400

    [processor/unroll]: upstream unroll processor (#42500)
    <!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
    Ex. Adding a feature - Explain what this achieves.-->
    #### Description
    
    Adds a new log processor component that will take a log body of slices
    and expand them into multiple log entries within the ScopeLogs.
    
    This is a component we made in the [Bindplane Distro of the
    Collector](https://github.com/observIQ/bindplane-otel-collector/tree/main).
    We initially solved it for logs without proper parsing extensions. This
    pairs quite well with the
    [transformprocessor](https://github.com/open-telemetry/opentelemetry-collector-contrib/tree/main/processor/transformprocessor)
    to
    [Split](https://github.com/open-telemetry/opentelemetry-collector-contrib/blob/0c2a3255d7fb3eb3ec25f2eb2c16e644d9684ad8/pkg/ottl/ottlfuncs/README.md#split)
    and unroll say based off improperly formatted newlines depending on the
    receiver sending the data.
    
    <!-- Issue number (e.g. #1234) or full URL to issue, if applicable. -->
    #### Link to tracking issue
    Fixes #42491
    
    <!--Describe what testing was performed and which tests were added.-->
    #### Testing
    
    Believe to be properly unit tested, however its been a second since I've
    added a component so happy to add some more tests where they are
    applicable.
    
    <!--Describe the documentation added.-->
    #### Documentation
    
    <!--Please delete paragraphs that you did not use before submitting.-->

---
+++ processor/unrollprocessor/processor_test.go
@@ -0,0 +1,94 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package unrollprocessor
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/consumer/consumertest"
+	"go.opentelemetry.io/collector/pdata/plog"
+	"go.opentelemetry.io/collector/processor/processortest"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/golden"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/pdatatest/plogtest"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/unrollprocessor/internal/metadata"
+)
+
+func BenchmarkUnroll(b *testing.B) {
+	unrollProcessor := &unrollProcessor{
+		cfg: createDefaultConfig().(*Config),
+	}
+	testLogs := createTestResourceLogs()
+
+	for n := 0; n < b.N; n++ {
+		_, _ = unrollProcessor.ProcessLogs(b.Context(), testLogs)
+	}
+}
+
+func createTestResourceLogs() plog.Logs {
+	rl := plog.NewLogs()
+	for i := 0; i < 10; i++ {
+		resourceLog := rl.ResourceLogs().AppendEmpty()
+		for j := 0; j < 10; j++ {
+			scopeLogs := resourceLog.ScopeLogs().AppendEmpty()
+			_ = scopeLogs.LogRecords().AppendEmpty().Body().SetEmptySlice().FromRaw([]any{1, 2, 3, 4, 5, 6, 7})
+		}
+	}
+	return rl
+}
+
+func TestProcessor(t *testing.T) {
+	for _, test := range []struct {
+		name      string
+		recursive bool
+	}{
+		{
+			name: "nop",
+		},
+		{
+			name: "simple",
+		},
+		{
+			name: "mixed_slice_types",
+		},
+		{
+			name: "some_not_slices",
+		},
+		{
+			name: "recursive_false",
+		},
+		{
+			name:      "recursive_true",
+			recursive: true,
+		},
+		{
+			name: "empty",
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			input, err := golden.ReadLogs(filepath.Join("testdata", test.name, "input.yaml"))
+			require.NoError(t, err)
+			expected, err := golden.ReadLogs(filepath.Join("testdata", test.name, "expected.yaml"))
+			require.NoError(t, err)
+
+			f := NewFactory()
+			cfg := f.CreateDefaultConfig().(*Config)
+			cfg.Recursive = test.recursive
+			set := processortest.NewNopSettings(metadata.Type)
+			sink := &consumertest.LogsSink{}
+			p, err := f.CreateLogs(t.Context(), set, cfg, sink)
+			require.NoError(t, err)
+
+			err = p.ConsumeLogs(t.Context(), input)
+			require.NoError(t, err)
+
+			actual := sink.AllLogs()
+			require.Len(t, actual, 1)
+
+			require.NoError(t, plogtest.CompareLogs(expected, actual[0]))
+		})
+	}
+}
