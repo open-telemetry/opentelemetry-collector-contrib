a18811c42b996a93cee43f462b6903bb9266874b
Yaten Dhingra <129659514+yaten2302@users.noreply.github.com>
2025-09-26 21:41:29 +0530

    fix: role_arn support in AWSCloudWatch and AWSEmf exporters (#42541)
    <!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
    Ex. Adding a feature - Explain what this achieves.-->
    #### Description
    
    This PR attempts to fix re-add the support of `role_arn` in
    AWSCloudWatch and AWSEmf exporters.
    The support was present in aws-go-sdk-v1, but broke while moving to v2.
    
    <!-- Issue number (e.g. #1234) or full URL to issue, if applicable. -->
    #### Link to tracking issue
    Fixes #42115
    
    ---------
    
    Signed-off-by: Yaten <yaten598@gmail.com>
    Co-authored-by: Antoine Toulme <atoulme@splunk.com>

--- exporter/awscloudwatchlogsexporter/exporter.go
+++ exporter/awscloudwatchlogsexporter/exporter.go
@@ -50,7 +50,6 @@ func newCwLogsPusher(ctx context.Context, expConfig *Config, params exp.Settings
 	if expConfig == nil {
 		return nil, errors.New("awscloudwatchlogs exporter config is nil")
 	}
-
 	expConfig.logger = params.Logger
 
 	awsConfig, err := awsutil.GetAWSConfig(ctx, params.Logger, &expConfig.AWSSessionSettings)
