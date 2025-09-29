c6b3ef03a365a72b4b6665a9d6d6cf998b2123e8
Paulo Janotti <pjanotti@splunk.com>
2025-09-28 17:31:17 -0700

    Revert "Bogus change to trigger scoped-test"
    This reverts commit a184a7859fff7d573cd93fa41f78ba91e5a15f1a.

--- receiver/hostmetricsreceiver/factory_test.go
+++ receiver/hostmetricsreceiver/factory_test.go
@@ -20,7 +20,6 @@ import (
 var creationSet = receivertest.NewNopSettings(metadata.Type)
 
 func TestCreateDefaultConfig(t *testing.T) {
-	// Bogus change just to trigger `scoped-test`: must be reverted before merging corresponding PR.
 	factory := NewFactory()
 	cfg := factory.CreateDefaultConfig()
 	assert.NotNil(t, cfg, "failed to create default config")
