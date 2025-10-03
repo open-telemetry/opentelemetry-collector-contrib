120d289c8adfb6716bc345e17dc8e72c8d6fa7b2
Florian Bacher <florian.bacher@dynatrace.com>
2025-10-03 06:54:13 +0200

    add missing license headers
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- receiver/k8sobjectsreceiver/observer/config.go
+++ receiver/k8sobjectsreceiver/observer/config.go
@@ -1,3 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
 package observer // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/k8sobjectsreceiver/observer"
 
 import (
