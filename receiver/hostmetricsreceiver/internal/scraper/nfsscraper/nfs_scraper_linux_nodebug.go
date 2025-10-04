0381a130f98eefc8bf2135600c1456bd2264651b
John Leslie <johnleslie@google.com>
2025-10-04 16:57:54 +0000

    add //nolint:unused

--- receiver/hostmetricsreceiver/internal/scraper/nfsscraper/nfs_scraper_linux_nodebug.go
+++ receiver/hostmetricsreceiver/internal/scraper/nfsscraper/nfs_scraper_linux_nodebug.go
@@ -5,6 +5,7 @@
 
 package nfsscraper // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/hostmetricsreceiver/internal/scraper/nfsscraper"
 
+//nolint:unused
 func debugLine(_, _ string) {
 	// This is a no-op function that will be compiled in when the 'debug' build tag is not used.
 	// go compiler will inline / optimize this call out
