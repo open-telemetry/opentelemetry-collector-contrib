536948d602a90075809c88393689cde70f06cc49
John Leslie <johnleslie@google.com>
2025-10-04 17:14:29 +0000

    add //go:build linux

--- receiver/hostmetricsreceiver/internal/scraper/nfsscraper/nfs_scraper_linux.go
+++ receiver/hostmetricsreceiver/internal/scraper/nfsscraper/nfs_scraper_linux.go
@@ -1,6 +1,8 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+//go:build linux
+
 package nfsscraper // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/hostmetricsreceiver/internal/scraper/nfsscraper"
 
 import (
