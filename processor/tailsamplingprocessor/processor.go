1deb987a9103a36e86304fb801c2199c2fecca6c
Patrick Oyarzun <patrick.oyarzun@grafana.com>
2025-10-07 22:30:54 -0400

    Use a test controller instead of synctest

--- processor/tailsamplingprocessor/processor.go
+++ processor/tailsamplingprocessor/processor.go
@@ -56,7 +56,7 @@ type tailSamplingSpanProcessor struct {
 	nextConsumer       consumer.Traces
 	policies           []*policy
 	idToTrace          sync.Map
-	policyTicker       timeutils.TTicker
+	policyTicker       *timeutils.PolicyTicker
 	tickerFrequency    time.Duration
 	decisionBatcher    idbatcher.Batcher
 	sampledIDCache     cache.Cache[bool]
@@ -173,24 +173,10 @@ func newTracesProcessor(ctx context.Context, set processor.Settings, nextConsume
 	return tsp, nil
 }
 
-// withDecisionBatcher sets the batcher used to batch trace IDs for policy evaluation.
-func withDecisionBatcher(batcher idbatcher.Batcher) Option {
-	return func(tsp *tailSamplingSpanProcessor) {
-		tsp.decisionBatcher = batcher
-	}
-}
-
 // withPolicies sets the sampling policies to be used by the processor.
 func withPolicies(policies []*policy) Option {
 	return func(tsp *tailSamplingSpanProcessor) {
 		tsp.policies = policies
-	}
-}
-
-// withTickerFrequency sets the frequency at which the processor will evaluate the sampling policies.
-func withTickerFrequency(frequency time.Duration) Option {
-	return func(tsp *tailSamplingSpanProcessor) {
-		tsp.tickerFrequency = frequency
 	}
 }
 
