6bec6b3473587a3d7a78dc34291d917f85cae4d6
stfreed <stfreed@starbucks.com>
2025-10-08 10:55:50 -0400

    fix: add license header comments to go files

--- receiver/redfishreceiver/config.go
+++ receiver/redfishreceiver/config.go
@@ -1,3 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
 package redfishreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/redfishreceiver"
 
 import (
