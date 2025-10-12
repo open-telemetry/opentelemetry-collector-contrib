12a3e1477bb1bb2332261620fa55175c460ccef3
Nicolas <69248573+niwoerner@users.noreply.github.com>
2025-10-12 18:27:51 +0200

    [receiver/gitlab] add span attributes (#42162)
    #### Description
    This PR adds attributes to the created spans by the GitLab receiver. It
    sets applicable semconv attributes + defines additional candidates for
    new `CICD` and `VCS` semconv attributes which will eventually be added
    in the future. (see this
    [item](https://github.com/open-telemetry/semantic-conventions/issues/1749#issue-2791360850)
    for more details)
    
    Once this PR is merged, the GitLab receiver should be ready to be
    promoted to alpha. -> Will address
    [this](https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/41592)
    issue.
    
    #### Link to tracking issue
    
    https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/35207
    
    #### Testing
    Added unit tests and performed manual testing
    <img width="2549" height="971" alt="gitlab-receiver-span-attrs"
    src="https://github.com/user-attachments/assets/d7ded24a-c460-4fb4-abbc-fb5b8c6568f9"
    />
    
    #### Documentation
    Updated README with new configuration options

--- receiver/gitlabreceiver/traces_event_handling_test.go
+++ receiver/gitlabreceiver/traces_event_handling_test.go
@@ -11,6 +11,7 @@ import (
 	gitlab "gitlab.com/gitlab-org/api/client-go"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/ptrace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.34.0"
 )
 
 // Helper function to parse a PipelineEvent from JSON for the remaining tests
@@ -61,6 +62,12 @@ func TestHandlePipeline(t *testing.T) {
 			expectError:     true,
 			expectedErrText: "invalid finishedAt timestamp: time is empty",
 			spanCount:       0,
+		},
+		{
+			name:        "pipeline_with_minimal_fields",
+			jsonEvent:   minimalValidPipelineWebhookEvent,
+			expectError: false,
+			spanCount:   1,
 		},
 	}
 
@@ -379,6 +386,341 @@ func TestParseGitlabTime(t *testing.T) {
 			} else {
 				require.NoError(t, err, "did not expect error for time string: %s", tt.timeStr)
 				require.False(t, time.IsZero(), "expected non-zero time value")
+			}
+		})
+	}
+}
+
+func TestIncludeUserAttributes(t *testing.T) {
+	tests := []struct {
+		name                  string
+		includeUserAttributes bool
+		expectUserAttrs       bool
+	}{
+		{
+			name:                  "user details excluded by default",
+			includeUserAttributes: false,
+			expectUserAttrs:       false,
+		},
+		{
+			name:                  "user details included when enabled",
+			includeUserAttributes: true,
+			expectUserAttrs:       true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			receiver := setupGitlabTracesReceiver(t)
+			receiver.cfg.WebHook.IncludeUserAttributes = tt.includeUserAttributes
+
+			var pipelineEvent gitlab.PipelineEvent
+			err := json.Unmarshal([]byte(validPipelineWebhookEvent), &pipelineEvent)
+			require.NoError(t, err)
+
+			attrs := pcommon.NewMap()
+			receiver.setResourceAttributes(attrs, &pipelineEvent)
+
+			_, hasAuthorName := attrs.Get(AttributeVCSRefHeadRevisionAuthorName)
+			_, hasAuthorEmail := attrs.Get(AttributeVCSRefHeadRevisionAuthorEmail)
+			_, hasCommitMessage := attrs.Get(AttributeVCSRefHeadRevisionMessage)
+			_, hasActorID := attrs.Get(AttributeCICDPipelineRunActorID)
+			_, hasActorUsername := attrs.Get(AttributeGitLabPipelineRunActorUsername)
+			_, hasActorName := attrs.Get(AttributeCICDPipelineRunActorName)
+
+			if tt.expectUserAttrs {
+				require.True(t, hasAuthorName, "expected author name attribute")
+				require.True(t, hasAuthorEmail, "expected author email attribute")
+				require.True(t, hasCommitMessage, "expected commit message attribute")
+				require.True(t, hasActorID, "expected actor ID attribute")
+				require.True(t, hasActorUsername, "expected actor username attribute")
+				require.True(t, hasActorName, "expected actor name attribute")
+			} else {
+				require.False(t, hasAuthorName, "unexpected author name attribute")
+				require.False(t, hasAuthorEmail, "unexpected author email attribute")
+				require.False(t, hasCommitMessage, "unexpected commit message attribute")
+				require.False(t, hasActorID, "unexpected actor ID attribute")
+				require.False(t, hasActorUsername, "unexpected actor username attribute")
+				require.False(t, hasActorName, "unexpected actor name attribute")
+			}
+
+			// Non-sensitive attributes should always be present
+			_, hasServiceName := attrs.Get("service.name")
+			require.True(t, hasServiceName, "service.name should always be present")
+		})
+	}
+}
+
+func TestSetAttributes(t *testing.T) {
+	receiver := setupGitlabTracesReceiver(t)
+	receiver.cfg.WebHook.IncludeUserAttributes = true
+
+	var pipelineEvent gitlab.PipelineEvent
+	err := json.Unmarshal([]byte(validPipelineWebhookEvent), &pipelineEvent)
+	require.NoError(t, err)
+
+	attrs := pcommon.NewMap()
+	receiver.setResourceAttributes(attrs, &pipelineEvent)
+
+	// VCS
+	vcsProvider, _ := attrs.Get("vcs.provider.name")
+	require.Equal(t, "gitlab", vcsProvider.Str())
+
+	repoName, _ := attrs.Get("vcs.repository.name")
+	require.Equal(t, "my-project", repoName.Str())
+
+	repoURL, _ := attrs.Get("vcs.repository.url.full")
+	require.Equal(t, "https://gitlab.example.com/test/project", repoURL.Str())
+
+	visibility, _ := attrs.Get(AttributeVCSRepositoryVisibility)
+	require.Equal(t, "private", visibility.Str())
+
+	namespace, _ := attrs.Get(AttributeGitLabProjectNamespace)
+	require.Equal(t, "test", namespace.Str())
+
+	defaultBranch, _ := attrs.Get(AttributeVCSRepositoryRefDefault)
+	require.Equal(t, "main", defaultBranch.Str())
+
+	// Pipeline
+	pipelineAttrs := pcommon.NewMap()
+	glPipeline := &glPipeline{&pipelineEvent}
+	glPipeline.setAttributes(pipelineAttrs)
+
+	pipelineSource, _ := pipelineAttrs.Get(AttributeGitLabPipelineSource)
+	require.Equal(t, "push", pipelineSource.Str())
+
+	// Job
+	if len(pipelineEvent.Builds) > 0 {
+		job := &glPipelineJob{
+			event:  &glJobEvent{},
+			jobURL: "https://example.com/job/1",
+		}
+		buildData, _ := json.Marshal(pipelineEvent.Builds[0])
+		err := json.Unmarshal(buildData, job.event)
+		require.NoError(t, err)
+
+		jobAttrs := pcommon.NewMap()
+		job.setAttributes(jobAttrs)
+
+		queuedDuration, _ := jobAttrs.Get(AttributeGitLabJobQueuedDuration)
+		require.Equal(t, 60.5, queuedDuration.Double())
+
+		allowFailure, _ := jobAttrs.Get(AttributeGitLabJobAllowFailure)
+		require.False(t, allowFailure.Bool())
+
+		runnerType, _ := jobAttrs.Get(AttributeCICDWorkerType)
+		require.Equal(t, "instance_type", runnerType.Str())
+
+		isShared, _ := jobAttrs.Get(AttributeCICDWorkerShared)
+		require.True(t, isShared.Bool())
+
+		tags, _ := jobAttrs.Get(AttributeCICDWorkerTags)
+		require.Equal(t, 2, tags.Slice().Len())
+		require.Equal(t, "docker", tags.Slice().At(0).Str())
+		require.Equal(t, "linux", tags.Slice().At(1).Str())
+	}
+}
+
+func TestMultiPipeline(t *testing.T) {
+	var pipelineEvent gitlab.PipelineEvent
+	err := json.Unmarshal([]byte(validMultiPipelineWebhookEvent), &pipelineEvent)
+	require.NoError(t, err)
+
+	pipelineAttrs := pcommon.NewMap()
+	glPipeline := &glPipeline{&pipelineEvent}
+	glPipeline.setAttributes(pipelineAttrs)
+
+	// Pipeline source
+	pipelineSource, _ := pipelineAttrs.Get(AttributeGitLabPipelineSource)
+	require.Equal(t, "parent_pipeline", pipelineSource.Str())
+
+	// Parent pipeline
+	parentProjectID, _ := pipelineAttrs.Get(AttributeGitLabPipelineSourcePipelineProjectID)
+	require.Equal(t, int64(123), parentProjectID.Int())
+
+	parentPipelineID, _ := pipelineAttrs.Get(AttributeGitLabPipelineSourcePipelineID)
+	require.Equal(t, int64(1), parentPipelineID.Int())
+
+	parentJobID, _ := pipelineAttrs.Get(AttributeGitLabPipelineSourcePipelineJobID)
+	require.Equal(t, int64(99), parentJobID.Int())
+
+	parentProjectNamespace, _ := pipelineAttrs.Get(AttributeGitLabPipelineSourcePipelineProjectNamespace)
+	require.Equal(t, "test/parent-project", parentProjectNamespace.Str())
+
+	parentProjectURL, _ := pipelineAttrs.Get(AttributeGitLabPipelineSourcePipelineProjectURL)
+	require.Equal(t, "https://gitlab.example.com/test/parent-project", parentProjectURL.Str())
+}
+
+func TestPipelineWithMissingOptionalFields(t *testing.T) {
+	receiver, pipeline, _, _ := setupTestPipelineFromJSON(t, minimalValidPipelineWebhookEvent)
+	receiver.cfg.WebHook.IncludeUserAttributes = true
+
+	traces, err := receiver.handlePipeline(pipeline)
+	require.NoError(t, err)
+
+	require.Equal(t, 1, traces.SpanCount())
+
+	resource := traces.ResourceSpans().At(0).Resource()
+	attrs := resource.Attributes()
+
+	// Required fields should be present
+	serviceName, found := attrs.Get(string(semconv.ServiceNameKey))
+	require.True(t, found)
+	require.Equal(t, "test/project", serviceName.Str())
+
+	// Optional fields should be present but may be empty
+	pipelineName, found := attrs.Get(string(semconv.CICDPipelineNameKey))
+	require.True(t, found)
+	require.Empty(t, pipelineName.Str())
+
+	repoName, found := attrs.Get(string(semconv.VCSRepositoryNameKey))
+	require.True(t, found)
+	require.Empty(t, repoName.Str())
+
+	// Timestamp should not be present since it's nil
+	_, found = attrs.Get(AttributeVCSRefHeadRevisionTimestamp)
+	require.False(t, found)
+
+	// User attributes with include_user_attributes=true but user is nil
+	_, found = attrs.Get(AttributeCICDPipelineRunActorID)
+	require.False(t, found)
+
+	// Author fields should be empty strings
+	authorName, found := attrs.Get(AttributeVCSRefHeadRevisionAuthorName)
+	require.True(t, found)
+	require.Empty(t, authorName.Str())
+
+	authorEmail, found := attrs.Get(AttributeVCSRefHeadRevisionAuthorEmail)
+	require.True(t, found)
+	require.Empty(t, authorEmail.Str())
+
+	// Merge request ID should not create attributes when 0
+	_, found = attrs.Get(string(semconv.VCSChangeIDKey))
+	require.False(t, found)
+}
+
+func TestEnvironmentAttributes(t *testing.T) {
+	var pipelineEvent gitlab.PipelineEvent
+	err := json.Unmarshal([]byte(validPipelineWithEnvironmentEvent), &pipelineEvent)
+	require.NoError(t, err)
+
+	stagingJob := &glPipelineJob{
+		event:  &glJobEvent{},
+		jobURL: "https://example.com/job/10",
+	}
+
+	buildData, _ := json.Marshal(pipelineEvent.Builds[0])
+	err = json.Unmarshal(buildData, stagingJob.event)
+	require.NoError(t, err)
+
+	stagingAttrs := pcommon.NewMap()
+	stagingJob.setAttributes(stagingAttrs)
+
+	envName, _ := stagingAttrs.Get(AttributeCICDTaskEnvironmentName)
+	require.Equal(t, "staging", envName.Str())
+
+	deploymentTier, _ := stagingAttrs.Get(AttributeGitLabEnvironmentDeploymentTier)
+	require.Equal(t, "staging", deploymentTier.Str())
+
+	envAction, _ := stagingAttrs.Get(AttributeGitLabEnvironmentAction)
+	require.Equal(t, "start", envAction.Str())
+
+	prodJob := &glPipelineJob{
+		event:  &glJobEvent{},
+		jobURL: "https://example.com/job/11",
+	}
+	buildData, _ = json.Marshal(pipelineEvent.Builds[1])
+	err = json.Unmarshal(buildData, prodJob.event)
+	require.NoError(t, err)
+
+	prodAttrs := pcommon.NewMap()
+	prodJob.setAttributes(prodAttrs)
+
+	envName, _ = prodAttrs.Get(AttributeCICDTaskEnvironmentName)
+	require.Equal(t, "production", envName.Str())
+
+	deploymentTier, _ = prodAttrs.Get(AttributeGitLabEnvironmentDeploymentTier)
+	require.Equal(t, "production", deploymentTier.Str())
+
+	failureReason, _ := prodAttrs.Get(AttributeGitLabJobFailureReason)
+	require.Equal(t, "script_failure", failureReason.Str())
+
+	allowFailure, _ := prodAttrs.Get(AttributeGitLabJobAllowFailure)
+	require.True(t, allowFailure.Bool())
+}
+
+func TestRunnerAttributes(t *testing.T) {
+	tests := []struct {
+		name           string
+		runnerData     string
+		expectedType   string
+		expectedShared bool
+		expectedTags   []string
+	}{
+		{
+			name:           "instance runner with tags",
+			runnerData:     `{"id":100,"description":"shared-runner","active":true,"is_shared":true,"runner_type":"instance_type","tags":["docker","linux","amd64"]}`,
+			expectedType:   "instance_type",
+			expectedShared: true,
+			expectedTags:   []string{"docker", "linux", "amd64"},
+		},
+		{
+			name:           "group runner",
+			runnerData:     `{"id":200,"description":"group-runner","active":true,"is_shared":false,"runner_type":"group_type","tags":["kubernetes"]}`,
+			expectedType:   "group_type",
+			expectedShared: false,
+			expectedTags:   []string{"kubernetes"},
+		},
+		{
+			name:           "project runner without tags",
+			runnerData:     `{"id":300,"description":"project-runner","active":true,"is_shared":false,"runner_type":"project_type","tags":[]}`,
+			expectedType:   "project_type",
+			expectedShared: false,
+			expectedTags:   []string{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var runner struct {
+				ID          int      `json:"id"`
+				Description string   `json:"description"`
+				Active      bool     `json:"active"`
+				IsShared    bool     `json:"is_shared"`
+				RunnerType  string   `json:"runner_type"`
+				Tags        []string `json:"tags"`
+			}
+			err := json.Unmarshal([]byte(tt.runnerData), &runner)
+			require.NoError(t, err)
+
+			job := &glPipelineJob{
+				event: &glJobEvent{
+					ID:     1,
+					Name:   "test-job",
+					Runner: runner,
+				},
+				jobURL: "https://example.com/job/1",
+			}
+
+			attrs := pcommon.NewMap()
+			job.setAttributes(attrs)
+
+			runnerType, _ := attrs.Get(AttributeCICDWorkerType)
+			require.Equal(t, tt.expectedType, runnerType.Str())
+
+			isShared, _ := attrs.Get(AttributeCICDWorkerShared)
+			require.Equal(t, tt.expectedShared, isShared.Bool())
+
+			if len(tt.expectedTags) > 0 {
+				tags, found := attrs.Get(AttributeCICDWorkerTags)
+				require.True(t, found, "expected runner tags")
+				require.Equal(t, len(tt.expectedTags), tags.Slice().Len())
+				for i, expectedTag := range tt.expectedTags {
+					require.Equal(t, expectedTag, tags.Slice().At(i).Str())
+				}
+			} else {
+				_, found := attrs.Get(AttributeCICDWorkerTags)
+				require.False(t, found, "unexpected runner tags for runner without tags")
 			}
 		})
 	}
