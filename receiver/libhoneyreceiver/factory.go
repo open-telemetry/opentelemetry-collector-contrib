d7dd19f638b229c865fa91f37f1f4819adbcbf57
Mike Terhar <miketerhar@honeycomb.io>
2025-10-16 13:04:13 -0400

    Fix attribution for decompression middlware
    Co-authored-by: Alex Boten <223565+codeboten@users.noreply.github.com>

--- receiver/libhoneyreceiver/factory.go
+++ receiver/libhoneyreceiver/factory.go
@@ -42,7 +42,7 @@ func createDefaultConfig() component.Config {
 		HTTP: configoptional.Default(HTTPConfig{
 			ServerConfig: confighttp.ServerConfig{
 				Endpoint: endpointStr,
-				// Disable OTEL decompression middleware - we handle it internally for better error logging
+				// Disable default decompression middleware - we handle it internally for better error logging
 				CompressionAlgorithms: []string{},
 			},
 			TracesURLPaths: defaultTracesURLPaths,
