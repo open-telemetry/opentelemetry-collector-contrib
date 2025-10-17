9c718a25a1ca97ef7cfacc68092afc3cc3b3c316
Ibraheem Aboulnaga <ibraheem.aboulnaga@datadoghq.com>
2025-10-17 13:00:13 -0400

    comments and chlog

--- pkg/datadog/gates.go
+++ pkg/datadog/gates.go
@@ -6,4 +6,6 @@ package datadog // import "github.com/open-telemetry/opentelemetry-collector-con
 import "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/datadog/featuregates"
 
 // MetricRemappingDisabledFeatureGate is re-exported for backward compatibility with DataDog agent code
+//
+// Deprecated [v0.138.0]: This type has been deprecated, use pkg/datadog/featuregates.MetricRemappingDisabledFeatureGate instead
 var MetricRemappingDisabledFeatureGate = featuregates.MetricRemappingDisabledFeatureGate
