c004e85a4675da7210fca4b3af08fcc07e261c19
Steve Freed <freedgitr@gmail.com>
2025-10-17 11:48:44 -0400

    fix: add goporto
    Signed-off-by: Steve Freed <freedgitr@gmail.com>

--- receiver/redfishreceiver/internal/redfish/models.go
+++ receiver/redfishreceiver/internal/redfish/models.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package redfish
+package redfish // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/redfishreceiver/internal/redfish"
 
 import "strings"
 
