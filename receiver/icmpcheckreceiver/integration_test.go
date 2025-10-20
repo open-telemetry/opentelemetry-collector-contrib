f47a1d87fbd81e274fe149ee71e61545ff9bf639
Jan Korona <jakorona@cisco.com>
2025-10-20 23:10:53 +0200

    add test

--- receiver/icmpcheckreceiver/integration_test.go
+++ receiver/icmpcheckreceiver/integration_test.go
@@ -15,6 +15,26 @@ import (
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/pdatatest/pmetrictest"
 )
 
+func TestPingSudoIntegration(t *testing.T) {
+	scraperinttest.NewIntegrationTest(
+		NewFactory(),
+		scraperinttest.WithCustomConfig(
+			func(_ *testing.T, cfg component.Config, _ *scraperinttest.ContainerInfo) {
+				rCfg := cfg.(*Config)
+				rCfg.CollectionInterval = 100 * time.Millisecond
+				rCfg.Targets = []PingTarget{
+					{Host: "github.com", PingCount: 4},
+				}
+			},
+		),
+		scraperinttest.WithCompareTimeout(30*time.Second),
+		scraperinttest.WithCompareOptions(
+			pmetrictest.IgnoreMetricValues("ping.rtt.max", "ping.rtt.min", "ping.rtt.avg"),
+			pmetrictest.IgnoreStartTimestamp(),
+			pmetrictest.IgnoreTimestamp()),
+	).Run(t)
+}
+
 func TestIntegration(t *testing.T) {
 	scraperinttest.NewIntegrationTest(
 		NewFactory(),
