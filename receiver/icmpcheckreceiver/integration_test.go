f381df5de5e13a361de2955ab79b7d5609896809
Jan Korona <jakorona@cisco.com>
2025-10-20 12:19:12 +0200

    [receiver/icmpcheckreceiver] Add integration test

---
+++ receiver/icmpcheckreceiver/integration_test.go
@@ -0,0 +1,34 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package icmpcheckreceiver
+
+import (
+	"testing"
+	"time"
+
+	"go.opentelemetry.io/collector/component"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/internal/coreinternal/scraperinttest"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/pdatatest/pmetrictest"
+)
+
+func TestIntegration(t *testing.T) {
+	scraperinttest.NewIntegrationTest(
+		NewFactory(),
+		scraperinttest.WithCustomConfig(
+			func(_ *testing.T, cfg component.Config, _ *scraperinttest.ContainerInfo) {
+				rCfg := cfg.(*Config)
+				rCfg.CollectionInterval = 100 * time.Millisecond
+				rCfg.Targets = []PingTarget{
+					{Host: "github.com", PingCount: 4},
+				}
+			},
+		),
+		scraperinttest.WithCompareTimeout(30*time.Second),
+		scraperinttest.WithCompareOptions(
+			pmetrictest.IgnoreMetricValues("ping.rtt.max", "ping.rtt.min", "ping.rtt.avg"),
+			pmetrictest.IgnoreStartTimestamp(),
+			pmetrictest.IgnoreTimestamp()),
+	).Run(t)
+}
