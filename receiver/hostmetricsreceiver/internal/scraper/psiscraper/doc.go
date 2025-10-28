221d6fd0a39150712750d69a75151fa6db1b75d4
alpineq <alpineq@protonmail.com>
2025-10-28 13:37:52 +0300

    [receiver/hostmetrics] Add Pressure Stall Information (PSI) scraper

---
+++ receiver/hostmetricsreceiver/internal/scraper/psiscraper/doc.go
@@ -0,0 +1,7 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+//go:generate mdatagen metadata.yaml
+
+// Package psiscraper scrapes pressure stall information (PSI) metrics.
+package psiscraper // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/hostmetricsreceiver/internal/scraper/psiscraper"
