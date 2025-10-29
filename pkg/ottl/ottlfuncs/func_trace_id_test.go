4583350032bd3146825bad85cbd33565b8f88012
Andrew Cholakian <andrewvc@elastic.co>
2025-10-29 14:31:34 -0500

    Add one last test

--- pkg/ottl/ottlfuncs/func_trace_id_test.go
+++ pkg/ottl/ottlfuncs/func_trace_id_test.go
@@ -53,6 +53,10 @@ func Test_traceID_validation(t *testing.T) {
 			name:  "byte slice longer than 16 (17)",
 			bytes: []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17},
 		},
+		{
+			name:  "byte slice longer than 32 (33)",
+			bytes: []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
