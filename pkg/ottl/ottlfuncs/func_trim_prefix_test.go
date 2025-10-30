d94654cbc16c2f6abb2ae2e0b2d58a5d1884f1ff
Bogdan Drutu <bogdandrutu@gmail.com>
2025-10-30 07:53:16 -0700

    Add TrimPrefix and TrimSuffix to OTTL (#43883)
    This is a much optimal way to remove prefix/suffix compare with
    `replace_pattern(name, "^prefixed", "")`
    
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

---
+++ pkg/ottl/ottlfuncs/func_trim_prefix_test.go
@@ -0,0 +1,94 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package ottlfuncs
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/pdata/pcommon"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl"
+)
+
+func Test_TrimPrefix(t *testing.T) {
+	tests := []struct {
+		name     string
+		target   any
+		prefix   ottl.StringGetter[any]
+		expected string
+	}{
+		{
+			name:     "has prefix true",
+			target:   "hello world",
+			prefix:   &ottl.StandardStringGetter[any]{Getter: func(context.Context, any) (any, error) { return "hello ", nil }},
+			expected: "world",
+		},
+		{
+			name:     "has prefix false",
+			target:   "hello world",
+			prefix:   &ottl.StandardStringGetter[any]{Getter: func(context.Context, any) (any, error) { return " world", nil }},
+			expected: "hello world",
+		},
+		{
+			name:     "target pcommon.Value",
+			target:   pcommon.NewValueStr("hello world"),
+			prefix:   &ottl.StandardStringGetter[any]{Getter: func(context.Context, any) (any, error) { return "hello", nil }},
+			expected: " world",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			factory := NewTrimPrefixFactory[any]()
+			exprFunc, err := factory.CreateFunction(
+				ottl.FunctionContext{},
+				&TrimPrefixArguments[any]{
+					Target: ottl.StandardStringGetter[any]{
+						Getter: func(context.Context, any) (any, error) {
+							return tt.target, nil
+						},
+					},
+					Prefix: tt.prefix,
+				})
+			assert.NoError(t, err)
+			result, err := exprFunc(t.Context(), nil)
+			require.NoError(t, err)
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
+
+func Test_TrimPrefix_Error(t *testing.T) {
+	target := &ottl.StandardStringGetter[any]{
+		Getter: func(context.Context, any) (any, error) {
+			return true, nil
+		},
+	}
+	prefix := &ottl.StandardStringGetter[any]{
+		Getter: func(context.Context, any) (any, error) {
+			return "test", nil
+		},
+	}
+	exprFunc := trimPrefix[any](target, prefix)
+	_, err := exprFunc(t.Context(), nil)
+	require.Error(t, err)
+}
+
+func Test_TrimPrefix_Error_prefix(t *testing.T) {
+	target := &ottl.StandardStringGetter[any]{
+		Getter: func(context.Context, any) (any, error) {
+			return true, nil
+		},
+	}
+	prefix := &ottl.StandardStringGetter[any]{
+		Getter: func(context.Context, any) (any, error) {
+			return true, nil
+		},
+	}
+	exprFunc := trimPrefix[any](target, prefix)
+	_, err := exprFunc(t.Context(), nil)
+	require.Error(t, err)
+}
---
+++ pkg/ottl/ottlfuncs/func_trim_suffix_test.go
@@ -0,0 +1,94 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package ottlfuncs
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/pdata/pcommon"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl"
+)
+
+func Test_TrimSuffix(t *testing.T) {
+	tests := []struct {
+		name     string
+		target   any
+		prefix   ottl.StringGetter[any]
+		expected string
+	}{
+		{
+			name:     "has prefix true",
+			target:   "hello world",
+			prefix:   &ottl.StandardStringGetter[any]{Getter: func(context.Context, any) (any, error) { return "hello ", nil }},
+			expected: "hello world",
+		},
+		{
+			name:     "has prefix false",
+			target:   "hello world",
+			prefix:   &ottl.StandardStringGetter[any]{Getter: func(context.Context, any) (any, error) { return " world", nil }},
+			expected: "hello",
+		},
+		{
+			name:     "target pcommon.Value",
+			target:   pcommon.NewValueStr("hello world"),
+			prefix:   &ottl.StandardStringGetter[any]{Getter: func(context.Context, any) (any, error) { return "world", nil }},
+			expected: "hello ",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			factory := NewTrimSuffixFactory[any]()
+			exprFunc, err := factory.CreateFunction(
+				ottl.FunctionContext{},
+				&TrimSuffixArguments[any]{
+					Target: ottl.StandardStringGetter[any]{
+						Getter: func(context.Context, any) (any, error) {
+							return tt.target, nil
+						},
+					},
+					Suffix: tt.prefix,
+				})
+			assert.NoError(t, err)
+			result, err := exprFunc(t.Context(), nil)
+			require.NoError(t, err)
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
+
+func Test_TrimSuffix_Error(t *testing.T) {
+	target := &ottl.StandardStringGetter[any]{
+		Getter: func(context.Context, any) (any, error) {
+			return true, nil
+		},
+	}
+	prefix := &ottl.StandardStringGetter[any]{
+		Getter: func(context.Context, any) (any, error) {
+			return "test", nil
+		},
+	}
+	exprFunc := trimSuffix[any](target, prefix)
+	_, err := exprFunc(t.Context(), nil)
+	require.Error(t, err)
+}
+
+func Test_TrimSuffix_Error_prefix(t *testing.T) {
+	target := &ottl.StandardStringGetter[any]{
+		Getter: func(context.Context, any) (any, error) {
+			return true, nil
+		},
+	}
+	prefix := &ottl.StandardStringGetter[any]{
+		Getter: func(context.Context, any) (any, error) {
+			return true, nil
+		},
+	}
+	exprFunc := trimSuffix[any](target, prefix)
+	_, err := exprFunc(t.Context(), nil)
+	require.Error(t, err)
+}
