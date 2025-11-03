f0bc2a15743de5e9a4c101eda3ba59030bf2aa03
Khushi Jain <khushi.jain@elastic.co>
2025-11-03 12:36:31 +0530

    add util test

--- pkg/stanza/fileconsumer/internal/util/util.go
+++ pkg/stanza/fileconsumer/internal/util/util.go
@@ -12,22 +12,6 @@ import (
 
 const magicHeader = "\x1f\x8b" // RFC 1952 magic bytes
 
-// MapCopy deep copies the provided attributes map.
-func MapCopy(m map[string]any) map[string]any {
-	newMap := make(map[string]any, len(m))
-	for k, v := range m {
-		switch typedVal := v.(type) {
-		case map[string]any:
-			newMap[k] = MapCopy(typedVal)
-		default:
-			// Assume any other values are safe to directly copy.
-			// Struct types and slice types shouldn't appear in attribute maps from pipelines
-			newMap[k] = v
-		}
-	}
-	return newMap
-}
-
 // IsGzipFile checks if a file is of gzip type by reading its header
 func IsGzipFile(f *os.File) bool {
 	header := make([]byte, len(magicHeader))
--- pkg/stanza/fileconsumer/internal/util/util_test.go
+++ pkg/stanza/fileconsumer/internal/util/util_test.go
@@ -10,30 +10,8 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
-func TestMapCopy(t *testing.T) {
-	initMap := map[string]any{
-		"mapVal": map[string]any{
-			"nestedVal": "value1",
-		},
-		"intVal": 1,
-		"strVal": "OrigStr",
-	}
-
-	copyMap := MapCopy(initMap)
-	// Mutate values on the copied map
-	copyMap["mapVal"].(map[string]any)["nestedVal"] = "overwrittenValue"
-	copyMap["intVal"] = 2
-	copyMap["strVal"] = "CopyString"
-
-	// Assert that the original map should have the same values
-	assert.Equal(t, "value1", initMap["mapVal"].(map[string]any)["nestedVal"])
-	assert.Equal(t, 1, initMap["intVal"])
-	assert.Equal(t, "OrigStr", initMap["strVal"])
-}
 
 func TestIsGzipFile(t *testing.T) {
 	temp, err := os.Create(filepath.Join(t.TempDir(), "test.log"))
