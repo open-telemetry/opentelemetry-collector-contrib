b8f9f4fee1a293bf84285dbca7140f1c937e8c1b
braydonk <braydonk@google.com>
2025-11-12 19:55:41 +0000

    add filelog.windows.caseInsensitive featuregate

--- pkg/stanza/fileconsumer/matcher/internal/finder/finder_windows.go
+++ pkg/stanza/fileconsumer/matcher/internal/finder/finder_windows.go
@@ -14,16 +14,33 @@ import (
 func getDefaultDoublestarOptions() []doublestar.GlobOption {
 	// On Windows, filepaths are case-insensitive by default. As a result,
 	// we want our globs to be case-insensitive.
-	return []doublestar.GlobOption{doublestar.WithCaseInsensitive()}
+
+	// This is currently guarded by a featuregate, which will eventually become
+	// the default.
+	options := []doublestar.GlobOption{}
+	if WindowsCaseInsensitiveFeatureGate.IsEnabled() {
+		options = append(options, doublestar.WithCaseInsensitive())
+	}
+	return options
 }
 
 func pathExcluded(excludes []string, path string) bool {
 	// To allow case-insensitive matching, the path and exclude
 	// are unified to lowercase before matching.
-	lowerPath := strings.ToLower(path)
+
+	if WindowsCaseInsensitiveFeatureGate.IsEnabled() {
+		lowerPath := strings.ToLower(path)
+		for _, exclude := range excludes {
+			lowerExclude := strings.ToLower(exclude)
+			if itMatches, _ := doublestar.PathMatch(lowerExclude, lowerPath); itMatches {
+				return true
+			}
+		}
+		return false
+	}
+
 	for _, exclude := range excludes {
-		lowerExclude := strings.ToLower(exclude)
-		if itMatches, _ := doublestar.PathMatch(lowerExclude, lowerPath); itMatches {
+		if itMatches, _ := doublestar.PathMatch(exclude, path); itMatches {
 			return true
 		}
 	}
