9dcb67da79458d8eebb37d0afe5c693d9e2f70a2
Jade Guiton <jade.guiton@datadoghq.com>
2025-11-17 13:16:25 +0100

    Add back conditional dependency download and add comments to script

--- .github/workflows/scripts/free-disk-space.sh
+++ .github/workflows/scripts/free-disk-space.sh
@@ -6,6 +6,10 @@
 echo "Available disk space before:"
 df -h /
 
+# The Android SDK is the biggest culprit for the lack of disk space in CI.
+# It is installed into /usr/local/lib/android manually (ie. not with apt) by this script:
+# https://github.com/actions/runner-images/blob/main/images/ubuntu/scripts/build/install-android-sdk.sh
+# so let's delete the directory manually.
 echo "Deleting unused Android SDK and tools..."
 sudo rm -rf /usr/local/lib/android
 
