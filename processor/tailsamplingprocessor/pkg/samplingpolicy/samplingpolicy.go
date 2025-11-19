3dc3b4d430293ad96b729139d4dc4f9e53c37b1c
Chris Marchbanks <csmarchbanks@gmail.com>
2025-11-18 18:25:40 -0700

    Add parent span to sampling TraceData

--- processor/tailsamplingprocessor/pkg/samplingpolicy/samplingpolicy.go
+++ processor/tailsamplingprocessor/pkg/samplingpolicy/samplingpolicy.go
@@ -23,6 +23,8 @@ type TraceData struct {
 	ReceivedBatches ptrace.Traces
 	// FinalDecision.
 	FinalDecision Decision
+	// ParentSpan
+	ParentSpan *ptrace.Span
 }
 
 // Decision gives the status of sampling decision.
