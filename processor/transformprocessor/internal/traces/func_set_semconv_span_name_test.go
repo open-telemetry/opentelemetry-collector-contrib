b9b9e30b4190c95c69615ccd00625d74419ea90b
Cyrille Le Clerc <cyrille.leclerc@grafana.com>
2025-11-20 16:30:59 +0100

    make generate

--- processor/transformprocessor/internal/traces/func_set_semconv_span_name_test.go
+++ processor/transformprocessor/internal/traces/func_set_semconv_span_name_test.go
@@ -6,12 +6,13 @@ package traces
 import (
 	"testing"
 
-	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/ottlspan"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/ptrace"
 	"go.opentelemetry.io/otel/attribute"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/ottlspan"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl"
 )
