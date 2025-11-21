29cbb24ff8ac4e3182cb241184ef2256bab399ef
Jade Guiton <jade.guiton@datadoghq.com>
2025-11-21 17:19:53 +0100

    Add comment to tracking issue when remaining space is too low

--- .github/workflows/scripts/check-disk-space.sh
+++ .github/workflows/scripts/check-disk-space.sh
@@ -9,6 +9,12 @@ free_space=$(df -BM --output=avail / | sed '1d;s/[^0-9]//g')
 used_space=$((BASELINE_SPACE - free_space))
 echo "Job used $used_space MiB of disk space, $free_space MiB remain."
 if [ "$used_space" -gt 14336 ]; then
-  echo "WARNING: The amount used exceeds the guaranteed 14 GiB of free space."
-  exit 1
+  echo "WARNING: The amount of space used exceeds the 14 GiB guaranteed by Github."
+fi
+if [ "$free_space" -lt 14500 ]; then
+  echo "WARNING: The amount of space remaining is dangerously low."
+  if [ "$GITHUB_REPOSITORY_OWNER" = "open-telemetry" ] && { [ "$GITHUB_REF" = "refs/heads/main" ] || [ "$GITHUB_HEAD_REF" = "jade-guiton-dd:check-disk-space" ]; }; then
+    echo "Adding comment on tracking issue..."
+    gh issue comment 44458 -b "Job $GITHUB_JOB in workflow $GITHUB_WORKFLOW ended with $free_space MiB of disk space"
+  fi
 fi
