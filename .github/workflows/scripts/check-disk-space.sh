55ac5042358b89dea2963d5015fb4d1812119080
Jade Guiton <jade.guiton@datadoghq.com>
2025-11-24 10:24:39 +0100

    Fix log message capitalization
    Co-authored-by: Curtis Robert <crobert@splunk.com>

--- .github/workflows/scripts/check-disk-space.sh
+++ .github/workflows/scripts/check-disk-space.sh
@@ -9,7 +9,7 @@ free_space=$(df -BM --output=avail / | sed '1d;s/[^0-9]//g')
 used_space=$((BASELINE_SPACE - free_space))
 echo "Job used $used_space MiB of disk space, $free_space MiB remain."
 if [ "$used_space" -gt 14336 ]; then
-  echo "WARNING: The amount of space used exceeds the 14 GiB guaranteed by Github."
+  echo "WARNING: The amount of space used exceeds the 14 GiB guaranteed by GitHub."
 fi
 if [ "$free_space" -lt 1024 ]; then
   echo "WARNING: The amount of space remaining is dangerously low."
