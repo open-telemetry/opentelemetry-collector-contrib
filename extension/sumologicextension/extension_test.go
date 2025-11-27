9bd8a2acbbd2b9315dcb3366c0e7e7337fbf5e2a
Jade Guiton <jade.guiton@datadoghq.com>
2025-11-27 17:52:03 +0100

    Fix one more

--- extension/sumologicextension/extension_test.go
+++ extension/sumologicextension/extension_test.go
@@ -1605,6 +1605,8 @@ func TestUpdateMetadataRequestPayload(t *testing.T) {
 	se, err := newSumologicExtension(cfg, zap.NewNop(), component.NewID(metadata.Type), "1.0.0")
 	require.NoError(t, err)
 
+	require.NoError(t, se.Start(t.Context(), componenttest.NewNopHost()))
+
 	httpClient, err := se.getHTTPClient(t.Context(), se.conf.ClientConfig, api.OpenRegisterResponsePayload{})
 	require.NoError(t, err)
 
