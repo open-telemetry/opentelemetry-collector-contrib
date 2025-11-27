8a2b5edb3af607701f38eab750f1a19595c08f5a
cjk <ckalbrener@splunk.com>
2025-11-27 10:01:57 +0000

    ran make generate

--- receiver/sqlserverreceiver/internal/metadata/generated_logs_test.go
+++ receiver/sqlserverreceiver/internal/metadata/generated_logs_test.go
@@ -4,6 +4,9 @@ package metadata
 
 import (
 	"context"
+	"testing"
+	"time"
+
 	"github.com/stretchr/testify/assert"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/plog"
@@ -11,8 +14,6 @@ import (
 	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zaptest/observer"
-	"testing"
-	"time"
 )
 
 type eventsTestDataSet int
