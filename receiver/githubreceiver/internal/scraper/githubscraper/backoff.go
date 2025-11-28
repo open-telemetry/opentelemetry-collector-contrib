8e00516672a8f6f1807ad967e9dd10c86649f7d1
Adriel Perkins <adriel@adrielperkins.com>
2025-11-28 13:16:23 -0500

    [chore] make goporto

--- receiver/githubreceiver/internal/scraper/githubscraper/backoff.go
+++ receiver/githubreceiver/internal/scraper/githubscraper/backoff.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package githubscraper
+package githubscraper // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/githubreceiver/internal/scraper/githubscraper"
 
 import (
 	"context"
