ee97014d993bb36f3dfb8a202527caca57c6c819
Antoine Toulme <atoulme@splunk.com>
2025-12-01 20:28:15 -0700

    [chore] update to latest semconv (#44640)
    Relates to #22095

--- receiver/dockerstatsreceiver/integration_test.go
+++ receiver/dockerstatsreceiver/integration_test.go
@@ -20,7 +20,7 @@ import (
 	"go.opentelemetry.io/collector/pdata/pmetric"
 	rcvr "go.opentelemetry.io/collector/receiver"
 	"go.opentelemetry.io/collector/receiver/receivertest"
-	conventions "go.opentelemetry.io/otel/semconv/v1.27.0"
+	conventions "go.opentelemetry.io/otel/semconv/v1.37.0"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zaptest"
 
