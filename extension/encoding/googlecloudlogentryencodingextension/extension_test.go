a579ddce3b62edacacc0d9bda9eba389c3d017c9
Álex Cámara Lara <alex.camara@elastic.co>
2025-12-02 11:55:10 +0100

    [encoding/googlecloudlogentry] Add support for Application Load Balancer Logs (#44438)
    <!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
    Ex. Adding a feature - Explain what this achieves.-->
    #### Description
    This PR adds support for parsing both types of GCP Application Load
    Balancers
    ([regional](https://docs.cloud.google.com/load-balancing/docs/https/https-reg-logging-monitoring#log-fields)
    and
    [global](https://docs.cloud.google.com/load-balancing/docs/https/https-logging-monitoring#log-fields)).
    With this changes, the user will be able to see load balancer logs
    reported information in log attributes instead of pasted as a raw json
    string into the log body, that could cause the user to overlook
    important info.
    
    Also modified armor log parsing to allow parse more than one policy
    types included in a single log entry.
    <!-- Issue number (e.g. #1234) or full URL to issue, if applicable. -->
    #### Link to tracking issue
    Solves #44274
    
    <!--Describe what testing was performed and which tests were added.-->
    #### Testing
    Added unit tests for new files and updated existing ones.
    
    <!--Describe the documentation added.-->
    #### Documentation
    Updated README.md
    <!--Please delete paragraphs that you did not use before submitting.-->

--- extension/encoding/googlecloudlogentryencodingextension/extension_test.go
+++ extension/encoding/googlecloudlogentryencodingextension/extension_test.go
@@ -202,6 +202,21 @@ func TestPayloads(t *testing.T) {
 			expectedFilename: "testdata/armorlog/enforced_edge_security_policy_expected.yaml",
 		},
 		{
+			name:             "armor log - two security policies",
+			logFilename:      "testdata/armorlog/two_security_policies.json",
+			expectedFilename: "testdata/armorlog/two_security_policies_expected.yaml",
+		},
+		{
+			name:             "application load balancer log - regional",
+			logFilename:      "testdata/apploadbalancer/regional_external_application_load_balancer.json",
+			expectedFilename: "testdata/apploadbalancer/regional_external_application_load_balancer_expected.yaml",
+		},
+		{
+			name:             "application load balancer log - global",
+			logFilename:      "testdata/apploadbalancer/global_external_application_load_balancer.json",
+			expectedFilename: "testdata/apploadbalancer/global_external_application_load_balancer_expected.yaml",
+		},
+		{
 			name:             "proxy nlb log - basic connection",
 			logFilename:      "testdata/proxynlb/proxynlb-basic.json",
 			expectedFilename: "testdata/proxynlb/proxynlb-basic_expected.yaml",
