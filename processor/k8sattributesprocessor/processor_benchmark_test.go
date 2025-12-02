e704eb656c0d56dc8c83ef8a7c5d26cf67ceeea4
odubajDT <ondrej.dubaj@dynatrace.com>
2025-12-02 13:21:47 +0100

    fix lint
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- processor/k8sattributesprocessor/processor_benchmark_test.go
+++ processor/k8sattributesprocessor/processor_benchmark_test.go
@@ -4,7 +4,6 @@
 package k8sattributesprocessor
 
 import (
-	"context"
 	"net"
 	"testing"
 
@@ -36,12 +35,12 @@ func BenchmarkProcessTraces(b *testing.B) {
 	)
 	require.NoError(b, err)
 
-	require.NoError(b, p.Start(context.Background(), componenttest.NewNopHost()))
+	require.NoError(b, p.Start(b.Context(), componenttest.NewNopHost()))
 	defer func() {
-		require.NoError(b, p.Shutdown(context.Background()))
+		require.NoError(b, p.Shutdown(b.Context()))
 	}()
 
-	ctx := client.NewContext(context.Background(), client.Info{
+	ctx := client.NewContext(b.Context(), client.Info{
 		Addr: &net.IPAddr{
 			IP: net.IPv4(1, 1, 1, 1),
 		},
