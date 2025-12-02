c8efaec35a8df964a7bef461098889265b4b0f67
cjk <ckalbrener@splunk.com>
2025-12-02 13:39:01 +0000

    Backout unintended merge

--- receiver/postgresqlreceiver/scraper.go
+++ receiver/postgresqlreceiver/scraper.go
@@ -227,8 +227,6 @@ func (p *postgreSQLScraper) collectQuerySamples(ctx context.Context, dbClient cl
 			atts[dbAttributePrefix+"wait_event_type"].(string),
 			atts[dbAttributePrefix+"query_id"].(string),
 			atts["duration"].(float64),
-			atts[dbAttributePrefix+"procedure_id"].(string),
-			atts[dbAttributePrefix+"procedure_name"].(string),
 		)
 	}
 }
