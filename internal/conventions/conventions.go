ea2df507cc65a5eb52b65dcd21e792fe137d8e84
alex boten <223565+codeboten@users.noreply.github.com>
2025-12-10 10:06:20 -0800

    update cmd/opampsupervisor
    Signed-off-by: alex boten <223565+codeboten@users.noreply.github.com>

--- internal/conventions/conventions.go
+++ internal/conventions/conventions.go
@@ -5,16 +5,21 @@ package conventions // import "github.com/open-telemetry/opentelemetry-collector
 
 import semconv "go.opentelemetry.io/otel/semconv/v1.38.0"
 
+const SchemaURL = semconv.SchemaURL
+
 // stable as of v1.38.0
 const (
 	ExceptionMessageKey    = semconv.ExceptionMessageKey
 	ExceptionStacktraceKey = semconv.ExceptionStacktraceKey
 	ExceptionTypeKey       = semconv.ExceptionTypeKey
 	ServiceNameKey         = semconv.ServiceNameKey
-	ServiceInstanceIDKey   = semconv.ServiceInstanceIDKey
+	ServiceVersionKey      = semconv.ServiceVersionKey
 )
 
 // development as of v1.38.0
 const (
-	HostNameKey = semconv.HostNameKey
+	HostNameKey          = semconv.HostNameKey
+	HostArchKey          = semconv.HostArchKey
+	OSTypeKey            = semconv.OSTypeKey
+	ServiceInstanceIDKey = semconv.ServiceInstanceIDKey
 )
