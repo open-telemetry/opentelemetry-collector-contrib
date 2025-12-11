6d2bf6f07b1e1e9b78566b4615378a2979b018ec
Arthur Silva Sens <arthursens2005@gmail.com>
2025-12-11 17:39:44 -0300

    lint 2
    Signed-off-by: Arthur Silva Sens <arthursens2005@gmail.com>

--- exporter/prometheusremotewriteexporter/config_test.go
+++ exporter/prometheusremotewriteexporter/config_test.go
@@ -9,7 +9,7 @@ import (
 	"time"
 
 	"github.com/cenkalti/backoff/v5"
-	"github.com/prometheus/prometheus/config"
+	remoteapi "github.com/prometheus/client_golang/exp/api/remote"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component"
@@ -82,7 +82,7 @@ func TestLoadConfig(t *testing.T) {
 				TargetInfo: TargetInfo{
 					Enabled: true,
 				},
-				RemoteWriteProtoMsg: config.RemoteWriteProtoMsgV1,
+				RemoteWriteProtoMsg: remoteapi.WriteV1MessageType,
 			},
 		},
 		{
