c4e2e9bcf916e31b969e5dfd388ba4a9ef96644e
leiwingqueen <leiwingqueen@gmail.com>
2025-12-12 00:25:06 +0800

    fix benchmark test
    Signed-off-by: leiwingqueen <leiwingqueen@gmail.com>

--- receiver/prometheusremotewritereceiver/receiver_bench_test.go
+++ receiver/prometheusremotewritereceiver/receiver_bench_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 
 	"github.com/prometheus/prometheus/model/labels"
-	"go.opentelemetry.io/collector/pdata/pcommon"
 )
 
 // makeLabels builds a labels.Labels with the given total number of labels.
@@ -24,40 +23,15 @@ func makeLabels(total int) labels.Labels {
 	return labels.FromMap(m)
 }
 
-// extractAttributesNoEnsure is a copy of extractAttributes without the EnsureCapacity call.
-// It intentionally avoids pre-sizing the returned pcommon.Map so we can compare allocations.
-func extractAttributesNoEnsure(ls labels.Labels) pcommon.Map {
-	attrs := pcommon.NewMap()
-	labelMap := ls.Map()
-	for labelName, labelValue := range labelMap {
-		if labelName == "instance" || labelName == "job" ||
-			labelName == labels.MetricName ||
-			labelName == "otel_scope_name" || labelName == "otel_scope_version" {
-			continue
-		}
-		attrs.PutStr(labelName, labelValue)
-	}
-	return attrs
-}
-
 func BenchmarkExtractAttributes(b *testing.B) {
 	sizes := []int{5, 20, 100, 500}
 	for _, sz := range sizes {
 		ls := makeLabels(sz)
-		b.Run(fmt.Sprintf("%d_withEnsure", sz), func(b *testing.B) {
+		b.Run(fmt.Sprintf("%d", sz), func(b *testing.B) {
 			b.ReportAllocs()
 			b.ResetTimer()
 			for i := 0; i < b.N; i++ {
 				m := extractAttributes(ls)
-				_ = m.Len()
-			}
-		})
-
-		b.Run(fmt.Sprintf("%d_withoutEnsure", sz), func(b *testing.B) {
-			b.ReportAllocs()
-			b.ResetTimer()
-			for i := 0; i < b.N; i++ {
-				m := extractAttributesNoEnsure(ls)
 				_ = m.Len()
 			}
 		})
