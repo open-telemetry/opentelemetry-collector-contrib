adb02fc476245ee81ec32b510eb20cc748456c18
Alex Boten <223565+codeboten@users.noreply.github.com>
2025-12-16 14:06:34 -0800

    Update connector/slowsqlconnector/factory.go

--- connector/slowsqlconnector/factory.go
+++ connector/slowsqlconnector/factory.go
@@ -12,7 +12,7 @@ import (
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/connector"
 	"go.opentelemetry.io/collector/consumer"
-	conventions "go.opentelemetry.io/otel/semconv/v1.37.0"
+	conventions "go.opentelemetry.io/otel/semconv/v1.38.0"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/connector/slowsqlconnector/internal/metadata"
 )
