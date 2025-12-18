e35b751a54659c361aa50f9dece67c5ee452784d
Dónal O'Sullivan <donal.osullivan@elastic.co>
2025-12-18 14:01:23 +0000

    Remove the defer function and use b.Cleanup for hostmetrics

--- receiver/hostmetricsreceiver/hostmetrics_receiver_test.go
+++ receiver/hostmetricsreceiver/hostmetrics_receiver_test.go
@@ -255,7 +255,6 @@ func benchmarkScrapeMetrics(b *testing.B, cfg *Config) {
 	require.NoError(b, err)
 
 	require.NoError(b, receiver.Start(b.Context(), componenttest.NewNopHost()))
-	defer func() { assert.NoError(b, receiver.Shutdown(b.Context())) }()
 
 	for b.Loop() {
 		tickerCh <- time.Now()
@@ -265,6 +264,10 @@ func benchmarkScrapeMetrics(b *testing.B, cfg *Config) {
 	if !sink.receivedMetrics {
 		b.Fail()
 	}
+
+	b.Cleanup(func() {
+		require.NoError(b, receiver.Shutdown(b.Context()))
+	})
 }
 
 func Benchmark_ScrapeCpuMetrics(b *testing.B) {
