52d8863606f71e11430a69b48c7bf171e04a6ef8
Curtis Robert <crobert@splunk.com>
2025-12-19 11:30:44 -0800

    [pkg/translator/splunk] Separate translation logic to its own package (#45011)
    <!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
    Ex. Adding a feature - Explain what this achieves.-->
    #### Description
    The translation from OTel to HEC was being done directly in the Splunk
    HEC exporter, but it is not dependent on the exporter itself. This
    translation logic can be used by other consumers without any need for
    going through the HEC exporter. The goal is to use this in other repos
    outside of contrib, that's why it's being moved to `pkg` instead of
    `internal`.
    
    <!--Describe what testing was performed and which tests were added.-->
    #### Testing
    Logic hasn't changed, just renamed and moved, existing tests have been
    updated and are passing.

--- pkg/translator/splunk/traces_to_splunk.go
+++ pkg/translator/splunk/traces_to_splunk.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package splunkhecexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/splunkhecexporter"
+package splunk // import "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/translator/splunk"
 
 import (
 	"go.opentelemetry.io/collector/pdata/pcommon"
@@ -47,26 +47,18 @@ type hecSpan struct {
 	Links      []hecLink         `json:"links,omitempty"`
 }
 
-func mapSpanToSplunkEvent(resource pcommon.Resource, span ptrace.Span, config *Config) *splunk.Event {
-	sourceKey := config.OtelAttrsToHec.Source
-	sourceTypeKey := config.OtelAttrsToHec.SourceType
-	indexKey := config.OtelAttrsToHec.Index
-	hostKey := config.OtelAttrsToHec.Host
-
+func SpanToSplunkEvent(resource pcommon.Resource, span ptrace.Span, mapping HecToOtelAttrs, source, sourceType, index string) *Event {
 	host := unknownHostName
-	source := config.Source
-	sourceType := config.SourceType
-	index := config.Index
 	commonFields := map[string]any{}
 	for k, v := range resource.Attributes().All() {
 		switch k {
-		case hostKey:
+		case mapping.Host:
 			host = v.Str()
-		case sourceKey:
+		case mapping.Source:
 			source = v.Str()
-		case sourceTypeKey:
+		case mapping.SourceType:
 			sourceType = v.Str()
-		case indexKey:
+		case mapping.Index:
 			index = v.Str()
 		case splunk.HecTokenLabel:
 			// ignore
@@ -75,7 +67,7 @@ func mapSpanToSplunkEvent(resource pcommon.Resource, span ptrace.Span, config *C
 		}
 	}
 
-	se := &splunk.Event{
+	se := &Event{
 		Time:       timestampToSecondsWithMillisecondPrecision(span.StartTimestamp()),
 		Host:       host,
 		Source:     source,
