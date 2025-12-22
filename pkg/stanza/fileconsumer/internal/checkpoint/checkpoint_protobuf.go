9207219ed439d3ab7c259889224e0884c8e40c2a
Paulo Dias <paulodias.gm@gmail.com>
2025-12-22 11:01:26 +0000

    chore: make goporto
    Signed-off-by: Paulo Dias <paulodias.gm@gmail.com>

--- pkg/stanza/fileconsumer/internal/checkpoint/checkpoint_protobuf.go
+++ pkg/stanza/fileconsumer/internal/checkpoint/checkpoint_protobuf.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package checkpoint
+package checkpoint // import "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/fileconsumer/internal/checkpoint"
 
 import (
 	"context"
