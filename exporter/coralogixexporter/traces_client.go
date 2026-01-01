1e4350cb57f6ff7818d7eeb6b6c30e5c8279978d
Israel Blancas <iblancasa@gmail.com>
2026-01-01 06:16:08 +0100

    Improve log messages when a partial success happens for traces (#44927)
    #### Link to tracking issue
    Fixes #44926
    
    ### Testing
    - Manual E2E tests
    - Added unit tests
    
    ---------
    
    Signed-off-by: Israel Blancas <iblancasa@gmail.com>

--- exporter/coralogixexporter/traces_client.go
+++ exporter/coralogixexporter/traces_client.go
@@ -12,6 +12,8 @@ import (
 	"go.opentelemetry.io/collector/pdata/ptrace"
 	"go.opentelemetry.io/collector/pdata/ptrace/ptraceotlp"
 	"go.uber.org/zap"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/coralogixexporter/internal/validation"
 )
 
 func newTracesExporter(cfg component.Config, set exporter.Settings) (*tracesExporter, error) {
@@ -83,29 +85,15 @@ func (e *tracesExporter) pushTraces(ctx context.Context, td ptrace.Traces) error
 		}
 
 		if e.settings.Logger.Level() == zap.DebugLevel {
-			// We need to deduplicate the trace IDs because the same trace ID
-			// can be sent multiple times
-			traceIDSet := make(map[string]struct{})
-			rss := td.ResourceSpans()
-			for i := 0; i < rss.Len(); i++ {
-				ss := rss.At(i).ScopeSpans()
-				for j := 0; j < ss.Len(); j++ {
-					spans := ss.At(j).Spans()
-					for k := 0; k < spans.Len(); k++ {
-						traceIDSet[spans.At(k).TraceID().String()] = struct{}{}
-					}
-				}
-			}
-			traceIDs := make([]string, 0, len(traceIDSet))
-			for traceID := range traceIDSet {
-				traceIDs = append(traceIDs, traceID)
-			}
-			logFields = append(logFields, zap.Strings("trace_ids", traceIDs))
+			logFields = append(logFields, validation.BuildPartialSuccessLogFieldsForTraces(
+				partialSuccess.ErrorMessage(),
+				td,
+				cxAppNameAttrName,
+				cxSubsystemNameAttrName,
+			)...)
 		}
 
-		e.settings.Logger.Error("Partial success response from Coralogix",
-			logFields...,
-		)
+		e.settings.Logger.Error("Partial success response from Coralogix", logFields...)
 	}
 
 	e.rateError.errorCount.Store(0)
