b3ba01f9e3408e8456ca857e478f5b5410c1420d
Nico Stewart <antonblock@users.noreply.github.com>
2026-01-05 18:17:04 -0500

    [receiver/aerospikereceiver]: Move mocks to internal package (#45231)
    <!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
    Ex. Adding a feature - Explain what this achieves.-->
    #### Description
    
    Move mock structs to internal package. Put mocks from `cluster/mocks` in
    `internal/cluster/mocks` to avoid import cycle
    
    <!-- Issue number (e.g. #1234) or full URL to issue, if applicable. -->
    #### Link to tracking issue
    Fixes #43995

--- receiver/aerospikereceiver/cluster/node_test.go
+++ receiver/aerospikereceiver/cluster/node_test.go
@@ -10,7 +10,7 @@ import (
 	as "github.com/aerospike/aerospike-client-go/v8"
 	"github.com/stretchr/testify/require"
 
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/aerospikereceiver/cluster/mocks"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/aerospikereceiver/internal/cluster/mocks"
 )
 
 func TestNode_NewConnNode(t *testing.T) {
