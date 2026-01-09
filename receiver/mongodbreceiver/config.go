d1029c3fde1a1b1716a6f4c88a8633df38da2e69
Hardik Choksi <writetohardikchoksi@gmail.com>
2026-01-09 10:45:31 +0530

    Add auth_mechanism_properties and changelog entry for auth_mechanism, auth_source and auth_mechanism_properties support

--- receiver/mongodbreceiver/config.go
+++ receiver/mongodbreceiver/config.go
@@ -27,14 +27,15 @@ type Config struct {
 	// MetricsBuilderConfig defines which metrics/attributes to enable for the scraper
 	metadata.MetricsBuilderConfig `mapstructure:",squash"`
 	// Deprecated - Transport option will be removed in v0.102.0
-	Hosts            []confignet.TCPAddrConfig `mapstructure:"hosts"`
-	Username         string                    `mapstructure:"username"`
-	Password         configopaque.String       `mapstructure:"password"`
-	AuthMechanism    string                    `mapstructure:"auth_mechanism,omitempty"`
-	AuthSource       string                    `mapstructure:"auth_source,omitempty"`
-	ReplicaSet       string                    `mapstructure:"replica_set,omitempty"`
-	Timeout          time.Duration             `mapstructure:"timeout"`
-	DirectConnection bool                      `mapstructure:"direct_connection"`
+	Hosts                   []confignet.TCPAddrConfig `mapstructure:"hosts"`
+	Username                string                    `mapstructure:"username"`
+	Password                configopaque.String       `mapstructure:"password"`
+	AuthMechanism           string                    `mapstructure:"auth_mechanism,omitempty"`
+	AuthSource              string                    `mapstructure:"auth_source,omitempty"`
+	AuthMechanismProperties map[string]string         `mapstructure:"auth_mechanism_properties,omitempty"`
+	ReplicaSet              string                    `mapstructure:"replica_set,omitempty"`
+	Timeout                 time.Duration             `mapstructure:"timeout"`
+	DirectConnection        bool                      `mapstructure:"direct_connection"`
 }
 
 func (c *Config) Validate() error {
@@ -85,6 +86,9 @@ func (c *Config) ClientOptions(secondary bool) *options.ClientOptions {
 			if c.AuthSource != "" {
 				credential.AuthSource = c.AuthSource
 			}
+			if len(c.AuthMechanismProperties) > 0 {
+				credential.AuthMechanismProperties = c.AuthMechanismProperties
+			}
 			clientOptions.SetAuth(credential)
 		}
 
@@ -121,6 +125,9 @@ func (c *Config) ClientOptions(secondary bool) *options.ClientOptions {
 		}
 		if c.AuthSource != "" {
 			credential.AuthSource = c.AuthSource
+		}
+		if len(c.AuthMechanismProperties) > 0 {
+			credential.AuthMechanismProperties = c.AuthMechanismProperties
 		}
 		clientOptions.SetAuth(credential)
 	}
