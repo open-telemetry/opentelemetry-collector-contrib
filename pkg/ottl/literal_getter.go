3517397869983b88fbee066c3bb5b2edcf4ab3c5
Bogdan Drutu <bogdandrutu@gmail.com>
2026-01-09 10:42:50 -0800

    [chore] Small improvements in literal getter
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- pkg/ottl/literal_getter.go
+++ pkg/ottl/literal_getter.go
@@ -7,12 +7,6 @@ import (
 	"context"
 )
 
-// literalGetter is an optional interface that allows Getter implementations to indicate
-// if they support literal values retrieval.
-type literalGetter interface {
-	isLiteral()
-}
-
 type literal[K any, T any] struct {
 	value T
 }
@@ -25,25 +19,14 @@ func (l *literal[K, T]) Get(context.Context, K) (T, error) {
 	return l.value, nil
 }
 
-func (*literal[K, T]) isLiteral() {}
-
-func isLiteralGetter[K, V any](getter typedGetter[K, V]) bool {
-	_, isLiteral := getter.(literalGetter)
-	return isLiteral
-}
-
 // GetLiteralValue retrieves the literal value from the given getter.
 // If the getter is not a literal getter, or if the value it's currently holding is not a
 // literal value, it returns the zero value of V and false.
 func GetLiteralValue[K, V any](getter typedGetter[K, V]) (V, bool) {
-	if !isLiteralGetter(getter) {
+	lit, isLiteral := getter.(*literal[K, V])
+	if !isLiteral {
 		return *new(V), false
 	}
 
-	val, err := getter.Get(context.Background(), *new(K))
-	if err != nil {
-		return *new(V), false
-	}
-
-	return val, true
+	return lit.value, true
 }
