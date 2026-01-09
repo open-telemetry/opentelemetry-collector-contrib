a39ce7fab1db1f53a1ebae904f0b34773d14ae46
Kaise Cheng <kaise.cheng@elastic.co>
2026-01-09 18:24:51 +0000

    disable lint for package name common

--- processor/filterprocessor/internal/common/processor.go
+++ processor/filterprocessor/internal/common/processor.go
@@ -1,6 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+//revive:disable:var-naming
 package common // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/filterprocessor/internal/common"
 
 import (
