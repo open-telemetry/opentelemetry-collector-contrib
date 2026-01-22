33757eadc09c3d5c9761a6c35aa7e0f367e1f9b0
bruno-domazet <brdo@tv2.dk>
2026-01-22 11:02:55 +0100

    PR suggestions
    - fail fast if telemetry can't be initialized in newBearerTokenAuth
    - defer capturing client call status til after the RoundTrip

--- extension/bearertokenauthextension/factory.go
+++ extension/bearertokenauthextension/factory.go
@@ -35,5 +35,5 @@ func createDefaultConfig() component.Config {
 }
 
 func createExtension(_ context.Context, set extension.Settings, cfg component.Config) (extension.Extension, error) {
-	return newBearerTokenAuth(cfg.(*Config), set.TelemetrySettings), nil
+	return newBearerTokenAuth(cfg.(*Config), set.TelemetrySettings)
 }
