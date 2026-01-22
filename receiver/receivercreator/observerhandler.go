046326531d6a09e8ba046c00d075b2953853e145
Khushi Jain <khushi.jain@elastic.co>
2026-01-22 15:10:14 +0530

    fix lint

--- receiver/receivercreator/observerhandler.go
+++ receiver/receivercreator/observerhandler.go
@@ -209,7 +209,7 @@ func (obs *observerHandler) startReceiver(template receiverTemplate, env observe
 		zap.String("endpoint_id", string(e.ID)),
 	)
 
-        // TODO: we should consider about applying sensitive data redaction
+	// TODO: we should consider about applying sensitive data redaction
 	obs.params.Logger.Debug("starting receiver with resolved config", zap.Any("config", resolvedConfig))
 
 	var receiver component.Component
