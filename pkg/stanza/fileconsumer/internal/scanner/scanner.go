0e49b87b5a816e3396c80fa8035d56a1d3367266
Khushi Jain <khushi.jain@elastic.co>
2026-01-23 23:06:19 +0530

    should not impact performance

--- pkg/stanza/fileconsumer/internal/scanner/scanner.go
+++ pkg/stanza/fileconsumer/internal/scanner/scanner.go
@@ -24,15 +24,24 @@ type Scanner struct {
 func New(r io.Reader, maxLogSize int, buf []byte, startOffset int64, splitFunc bufio.SplitFunc, isGzip bool) *Scanner {
 	s := &Scanner{Scanner: bufio.NewScanner(r), pos: startOffset}
 	s.Buffer(buf, maxLogSize)
-	scanFunc := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
-		advance, token, err = splitFunc(data, atEOF)
-		s.pos += int64(advance)
-		// flush data if we are at EOF and it is a gzip file
-		// this is because gzip reader reads the entire compressed stream in one go
-		if isGzip && atEOF && len(data) != 0 {
-			return len(data), data, nil
+	var scanFunc bufio.SplitFunc
+	if !isGzip {
+		scanFunc = func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+			advance, token, err = splitFunc(data, atEOF)
+			s.pos += int64(advance)
+			return advance, token, err
 		}
-		return advance, token, err
+	} else {
+		scanFunc = func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+			advance, token, err = splitFunc(data, atEOF)
+			s.pos += int64(advance)
+			// flush data if we are at EOF and it is a gzip file
+			// this is because gzip reader reads the entire compressed stream in one go
+			if atEOF && len(data) != 0 {
+				return len(data), data, nil
+			}
+			return advance, token, err
+		}
 	}
 	s.Split(scanFunc)
 	return s
