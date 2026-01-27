999172bc25d0fbf2eb81661e757d6d2e350b6758
Paulo Dias <paulodias.gm@gmail.com>
2026-01-27 11:30:03 +0000

    fix: update waitForData call
    Signed-off-by: Paulo Dias <paulodias.gm@gmail.com>

--- processor/resourcedetectionprocessor/e2e_test.go
+++ processor/resourcedetectionprocessor/e2e_test.go
@@ -668,6 +668,7 @@ func TestE2EAlibabaECSDetector(t *testing.T) {
 	metricsConsumer := new(consumertest.MetricsSink)
 	shutdownSink := startUpSink(t, metricsConsumer)
 	defer shutdownSink()
+	startEntries := len(metricsConsumer.AllMetrics())
 
 	testID := uuid.NewString()[:8]
 	collectorObjs := k8stest.CreateCollectorObjects(t, k8sClient, testID, filepath.Join(".", "testdata", "e2e", "alibaba_ecs", "collector"), map[string]string{}, "")
@@ -679,7 +680,7 @@ func TestE2EAlibabaECSDetector(t *testing.T) {
 	}()
 
 	wantEntries := 10
-	waitForData(t, wantEntries, metricsConsumer)
+	waitForData(t, metricsConsumer, startEntries, wantEntries)
 
 	// Uncomment to regenerate golden file
 	// golden.WriteMetrics(t, expectedFile+".actual", metricsConsumer.AllMetrics()[len(metricsConsumer.AllMetrics())-1])
