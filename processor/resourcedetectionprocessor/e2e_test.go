2cace8f41a3dbe59ff3753ac1e2cad1244cb32b9
Paulo Dias <paulodias.gm@gmail.com>
2026-01-27 10:36:53 +0000

    fix: fix missing parameters for waitForData
    Signed-off-by: Paulo Dias <paulodias.gm@gmail.com>

--- processor/resourcedetectionprocessor/e2e_test.go
+++ processor/resourcedetectionprocessor/e2e_test.go
@@ -1161,6 +1161,7 @@ func TestE2EKubeadmDetector(t *testing.T) {
 	metricsConsumer := new(consumertest.MetricsSink)
 	shutdownSink := startUpSink(t, metricsConsumer)
 	defer shutdownSink()
+	startEntries := len(metricsConsumer.AllMetrics())
 
 	testID := uuid.NewString()[:8]
 	collectorObjs := k8stest.CreateCollectorObjects(t, k8sClient, testID, filepath.Join(".", "testdata", "e2e", "kubeadm", "collector"), map[string]string{}, "")
@@ -1172,7 +1173,7 @@ func TestE2EKubeadmDetector(t *testing.T) {
 	}()
 
 	wantEntries := 10
-	waitForData(t, wantEntries, metricsConsumer)
+	waitForData(t, metricsConsumer, startEntries, wantEntries)
 
 	// Uncomment to regenerate golden file
 	// golden.WriteMetrics(t, expectedFile+".actual", metricsConsumer.AllMetrics()[len(metricsConsumer.AllMetrics())-1])
