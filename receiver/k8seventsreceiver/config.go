346f580f13f0935ed2aedaf7de9e5b985ee9f1ef
odubajDT <ondrej.dubaj@dynatrace.com>
2026-01-27 14:46:22 +0100

    [chore][receiver/k8sevents] update k8seventsreceiver to use k8sinventory
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- receiver/k8seventsreceiver/config.go
+++ receiver/k8seventsreceiver/config.go
@@ -5,6 +5,7 @@ package k8seventsreceiver // import "github.com/open-telemetry/opentelemetry-col
 
 import (
 	"go.opentelemetry.io/collector/component"
+	"k8s.io/client-go/dynamic"
 	k8s "k8s.io/client-go/kubernetes"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/internal/k8sconfig"
@@ -20,7 +21,8 @@ type Config struct {
 	K8sLeaderElector *component.ID `mapstructure:"k8s_leader_elector"`
 
 	// For mocking
-	makeClient func(apiConf k8sconfig.APIConfig) (k8s.Interface, error)
+	makeClient        func(apiConf k8sconfig.APIConfig) (k8s.Interface, error)
+	makeDynamicClient func(apiConf k8sconfig.APIConfig) (dynamic.Interface, error)
 }
 
 func (cfg *Config) Validate() error {
@@ -32,4 +34,11 @@ func (cfg *Config) getK8sClient() (k8s.Interface, error) {
 		cfg.makeClient = k8sconfig.MakeClient
 	}
 	return cfg.makeClient(cfg.APIConfig)
+}
+
+func (cfg *Config) getDynamicClient() (dynamic.Interface, error) {
+	if cfg.makeDynamicClient == nil {
+		cfg.makeDynamicClient = k8sconfig.MakeDynamicClient
+	}
+	return cfg.makeDynamicClient(cfg.APIConfig)
 }
