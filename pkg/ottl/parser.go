496f4e78332efc564e42a66b30c98beb5bedfd9a
Edmo Vamerlatti Costa <11836452+edmocosta@users.noreply.github.com>
2026-02-02 14:24:45 +0100

    [pkg/ottl] Fix debug logs panic in OTTL parser (#45731)
    <!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
    Ex. Adding a feature - Explain what this achieves.-->
    #### Description
    
    This PR fixes the debug logs panic in OTTL parser when the transform
    context holds any reference to removed (`RemoveIf`) pdata. See
    https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/44705
    for more details.
    
    Whenever it finds an invalid PData, it replaces the invalid value by
    `<deleted>`, following the same original structure.
    If it's an object, it replaces it with: `{"<deleted>": "<deleted>" }`
    If it's a field value: `"<deleted>"`
    Array items: `["<deleted>", "foo", "bar"]`
    Array object items: `[{"<deleted>": "<deleted>" }, {"resource": {...}}]`
    
    
    **Example:**
    
    2026-01-29T15:06:06.859+0100 DEBUG ottl/parser.go:475 condition
    evaluation result {"condition":
    "datapoint.attributes[\"my.counter.attr\"] != nil", "match": false,
    "TransformContext": {"resource": {"attributes":
    {"service.name": "my.service", "timestamp": "2018-12-01T16:17:18Z"},
    "dropped_attribute_count": 0}, "scope":
    {"attributes": {"my.scope.attribute": "some scope attribute"},
    "dropped_attribute_count": 0, "name": "my.library",
    "version": "1.0.0"}, "metric": {"description": "I am a Counter", "name":
    "my.counter", "unit": "1", "type": "Sum",
    "metadata": {}, "aggregation_temporality": "Delta", "is_monotonic":
    true, "datapoints": [**{"\<deleted\>": "\<deleted\>"}**,
    {"attributes": {"another.counter.attr": "another value"}, "exemplars":
    [], "flags": 0, "start_time_unix_nano":
    1544712660300000000, "time_unix_nano": 1544712660300000000,
    "value_double": 2}]}, "datapoint": {"attributes":
    {"another.counter.attr": "another value"}, "exemplars": [], "flags": 0,
    "start_time_unix_nano": 1544712660300000000,
    "time_unix_nano": 1544712660300000000, "value_double": 2}, "cache": {}}}
    
    
    <!-- Issue number (e.g. #1234) or full URL to issue, if applicable. -->
    #### Link to tracking issue
    Fixes
    https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/44705
    
    <!--Describe what testing was performed and which tests were added.-->
    #### Testing
     - Added unit tests
     - Functional tests
    
    <!--Please delete paragraphs that you did not use before submitting.-->

--- pkg/ottl/parser.go
+++ pkg/ottl/parser.go
@@ -34,7 +34,7 @@ func (s *Statement[K]) Execute(ctx context.Context, tCtx K) (any, bool, error) {
 	condition, err := s.condition.Eval(ctx, tCtx)
 	defer func() {
 		if s.telemetrySettings.Logger.Core().Enabled(zap.DebugLevel) {
-			s.telemetrySettings.Logger.Debug("TransformContext after statement execution", zap.String("statement", s.origText), zap.Bool("condition matched", condition), zap.Any("TransformContext", tCtx))
+			s.telemetrySettings.Logger.Debug("TransformContext after statement execution", zap.String("statement", s.origText), zap.Bool("condition matched", condition), newTransformContextField(tCtx))
 		}
 	}()
 	if err != nil {
@@ -472,7 +472,7 @@ func (c *ConditionSequence[K]) Eval(ctx context.Context, tCtx K) (bool, error) {
 	for _, condition := range c.conditions {
 		match, err := condition.Eval(ctx, tCtx)
 		if c.telemetrySettings.Logger.Core().Enabled(zap.DebugLevel) {
-			c.telemetrySettings.Logger.Debug("condition evaluation result", zap.String("condition", condition.origText), zap.Bool("match", match), zap.Any("TransformContext", tCtx))
+			c.telemetrySettings.Logger.Debug("condition evaluation result", zap.String("condition", condition.origText), zap.Bool("match", match), newTransformContextField(tCtx))
 		}
 		if err != nil {
 			if c.errorMode == PropagateError {
