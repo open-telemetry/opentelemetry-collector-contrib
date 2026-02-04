open-telemetry/opentelemetry-collector-contrib

commit 8c6cb2e95d8a504bb8ab3f677548c91c355e244f

Support jwt-bearer grant-type (RFC7523) in oauth2 extension (#44318)
<!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
Ex. Adding a feature - Explain what this achieves.-->
#### Description
This PR supports the grant-type defined by
[RFC7523](https://datatracker.ietf.org/doc/html/rfc7523) "JSON Web Token
(JWT) Profile for OAuth 2.0 Client Authentication and Authorization
Grants" in extensiont/oauth2clientauth.

This flow is needed for some OAuth providers, like [STACKIT Service
Accounts](https://docs.stackit.cloud/stackit/en/usage-of-the-service-account-keys-in-stackit-175112464.html)


<!-- Issue number (e.g. #1234) or full URL to issue, if applicable. -->
#### Link to tracking issue
Fixes
https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/44314

<!--Describe what testing was performed and which tests were added.-->
#### Testing
I've added a test where the signed token is evaluated field by field.
I've also test locally using STACKIT Service Account endpoint and I've
retrieved a valid access token.


<!--Describe the documentation added.-->
#### Documentation

<!--Please delete paragraphs that you did not use before submitting.-->

---------

Signed-off-by: Jorge Turrado <jorge.turrado@mail.schwarz>
Signed-off-by: Jorge Turrado <jorge_turrado@hotmail.es>

extension/oauth2clientauthextension/extension.go
@@ -15,7 +15,6 @@ import (
 	"go.uber.org/multierr"
 	"go.uber.org/zap"
 	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/clientcredentials"
 	"google.golang.org/grpc/credentials"
 	grpcOAuth "google.golang.org/grpc/credentials/oauth"
 )
@@ -26,15 +25,20 @@ var (
 	_ extensionauth.GRPCClient = (*clientAuthenticator)(nil)
 )
 
+type TokenSourceConfiguration interface {
+	TokenSource(context.Context) oauth2.TokenSource
+	TokenEndpoint() string
+}
+
 // clientAuthenticator provides implementation for providing client authentication using OAuth2 client credentials
 // workflow for both gRPC and HTTP clients.
 type clientAuthenticator struct {
 	component.StartFunc
 	component.ShutdownFunc
 
-	clientCredentials *clientCredentialsConfig
-	logger            *zap.Logger
-	client            *http.Client
+	credentials TokenSourceConfiguration
+	logger      *zap.Logger
+	client      *http.Client
 }
 
 type errorWrappingTokenSource struct {
@@ -57,20 +61,26 @@ func newClientAuthenticator(cfg *Config, logger *zap.Logger) (*clientAuthenticat
 	}
 	transport.TLSClientConfig = tlsCfg
 
+	var credentials TokenSourceConfiguration
+
+	switch cfg.GrantType {
+	case grantTypeJWTBearer:
+		credentials, err = newJwtGrantTypeConfig(cfg)
+		if err != nil {
+			return nil, err
+		}
+	case grantTypeClientCredentials, "":
+		credentials, err = newJwtGrantTypeConfig(cfg)
+		if err != nil {
+			return nil, err
+		}
+	default:
+		return nil, fmt.Errorf("unknown grant type %q", cfg.GrantType)
+	}
+
 	return &clientAuthenticator{
-		clientCredentials: &clientCredentialsConfig{
-			Config: clientcredentials.Config{
-				ClientID:       cfg.ClientID,
-				ClientSecret:   string(cfg.ClientSecret),
-				TokenURL:       cfg.TokenURL,
-				Scopes:         cfg.Scopes,
-				EndpointParams: cfg.EndpointParams,
-			},
-			ClientIDFile:     cfg.ClientIDFile,
-			ClientSecretFile: cfg.ClientSecretFile,
-			ExpiryBuffer:     cfg.ExpiryBuffer,
-		},
-		logger: logger,
+		credentials: credentials,
+		logger:      logger,
 		client: &http.Client{
 			Transport: transport,
 			Timeout:   cfg.Timeout,
@@ -94,8 +104,8 @@ func (o *clientAuthenticator) RoundTripper(base http.RoundTripper) (http.RoundTr
 	ctx := context.WithValue(context.Background(), oauth2.HTTPClient, o.client)
 	return &oauth2.Transport{
 		Source: errorWrappingTokenSource{
-			ts:       o.clientCredentials.TokenSource(ctx),
-			tokenURL: o.clientCredentials.TokenURL,
+			ts:       o.credentials.TokenSource(ctx),
+			tokenURL: o.credentials.TokenEndpoint(),
 		},
 		Base: base,
 	}, nil
@@ -107,8 +117,8 @@ func (o *clientAuthenticator) PerRPCCredentials() (credentials.PerRPCCredentials
 	ctx := context.WithValue(context.Background(), oauth2.HTTPClient, o.client)
 	return grpcOAuth.TokenSource{
 		TokenSource: errorWrappingTokenSource{
-			ts:       o.clientCredentials.TokenSource(ctx),
-			tokenURL: o.clientCredentials.TokenURL,
+			ts:       o.credentials.TokenSource(ctx),
+			tokenURL: o.credentials.TokenEndpoint(),
 		},
 	}, nil
 }

extension/oauth2clientauthextension/extension_test.go
@@ -4,9 +4,13 @@
 package oauth2clientauthextension
 
 import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"strings"
 	"testing"
 	"time"
 
@@ -19,94 +23,7 @@ import (
 	grpcOAuth "google.golang.org/grpc/credentials/oauth"
 )
 
-func TestOAuthClientSettings(t *testing.T) {
-	// test files for TLS testing
-	var (
-		testCAFile   = "testdata/testCA.pem"
-		testCertFile = "testdata/test-cert.pem"
-		testKeyFile  = "testdata/test-key.pem"
-	)
-
-	tests := []struct {
-		name          string
-		settings      *Config
-		shouldError   bool
-		expectedError string
-	}{
-		{
-			name: "all_valid_settings",
-			settings: &Config{
-				ClientID:       "testclientid",
-				ClientSecret:   "testsecret",
-				EndpointParams: url.Values{"audience": []string{"someaudience"}},
-				TokenURL:       "https://example.com/v1/token",
-				Scopes:         []string{"resource.read"},
-				Timeout:        2,
-				ExpiryBuffer:   10 * time.Second,
-				TLS: configtls.ClientConfig{
-					Config: configtls.Config{
-						CAFile:   testCAFile,
-						CertFile: testCertFile,
-						KeyFile:  testKeyFile,
-					},
-					Insecure:           false,
-					InsecureSkipVerify: false,
-				},
-			},
-			shouldError:   false,
-			expectedError: "",
-		},
-		{
-			name: "invalid_tls",
-			settings: &Config{
-				ClientID:     "testclientid",
-				ClientSecret: "testsecret",
-				TokenURL:     "https://example.com/v1/token",
-				Scopes:       []string{"resource.read"},
-				Timeout:      2,
-				ExpiryBuffer: 15 * time.Second,
-				TLS: configtls.ClientConfig{
-					Config: configtls.Config{
-						CAFile:   testCAFile,
-						CertFile: "nonexistent.cert",
-						KeyFile:  testKeyFile,
-					},
-					Insecure:           false,
-					InsecureSkipVerify: false,
-				},
-			},
-			shouldError:   true,
-			expectedError: "failed to load TLS config: failed to load TLS cert and key",
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			rc, err := newClientAuthenticator(test.settings, zap.NewNop())
-			if test.shouldError {
-				assert.ErrorContains(t, err, test.expectedError)
-				return
-			}
-			assert.NoError(t, err)
-			assert.Equal(t, test.settings.Scopes, rc.clientCredentials.Scopes)
-			assert.Equal(t, test.settings.TokenURL, rc.clientCredentials.TokenURL)
-			assert.EqualValues(t, test.settings.ClientSecret, rc.clientCredentials.ClientSecret)
-			assert.Equal(t, test.settings.ClientID, rc.clientCredentials.ClientID)
-			assert.Equal(t, test.settings.Timeout, rc.client.Timeout)
-			assert.Equal(t, test.settings.ExpiryBuffer, rc.clientCredentials.ExpiryBuffer)
-			assert.Equal(t, test.settings.EndpointParams, rc.clientCredentials.EndpointParams)
-
-			// test tls settings
-			transport := rc.client.Transport.(*http.Transport)
-			tlsClientConfig := transport.TLSClientConfig
-			tlsTestSettingConfig, err := test.settings.TLS.LoadTLSConfig(t.Context())
-			assert.NoError(t, err)
-			assert.Equal(t, tlsClientConfig.Certificates, tlsTestSettingConfig.Certificates)
-		})
-	}
-}
-
-func TestOAuthClientSettingsCredsConfig(t *testing.T) {
+func TestOAuthClientCredentialsSettingsConfig(t *testing.T) {
 	var (
 		testCredsFile        = "testdata/test-cred.txt"
 		testCredsEmptyFile   = "testdata/test-cred-empty.txt"
@@ -120,6 +37,27 @@ func TestOAuthClientSettingsCredsConfig(t *testing.T) {
 		shouldError          bool
 		expectedError        error
 	}{
+		{
+			name: "all_values",
+			settings: &Config{
+				ClientID:       "testclientid",
+				ClientSecret:   "testsecret",
+				EndpointParams: url.Values{"audience": []string{"someaudience"}},
+				TokenURL:       "https://example.com/v1/token",
+				Scopes:         []string{"resource.read"},
+				Timeout:        2,
+				ExpiryBuffer:   10 * time.Second,
+			},
+			expectedClientConfig: &clientcredentials.Config{
+				ClientID:       "testclientid",
+				ClientSecret:   "testsecret",
+				TokenURL:       "https://example.com/v1/token",
+				Scopes:         []string{"resource.read"},
+				EndpointParams: url.Values{"audience": []string{"someaudience"}},
+			},
+			shouldError:   false,
+			expectedError: nil,
+		},
 		{
 			name: "client_id_file",
 			settings: &Config{
@@ -168,8 +106,8 @@ func TestOAuthClientSettingsCredsConfig(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			rc, _ := newClientAuthenticator(test.settings, zap.NewNop())
-			cfg, err := rc.clientCredentials.createConfig()
+			credentials := newClientCredentialsGrantTypeConfig(test.settings)
+			cfg, err := credentials.createConfig()
 			if test.shouldError {
 				assert.Error(t, err)
 				assert.ErrorIs(t, err, test.expectedError)
@@ -178,6 +116,98 @@ func TestOAuthClientSettingsCredsConfig(t *testing.T) {
 			assert.NoError(t, err)
 			assert.Equal(t, test.expectedClientConfig.ClientID, cfg.ClientID)
 			assert.Equal(t, test.expectedClientConfig.ClientSecret, cfg.ClientSecret)
+		})
+	}
+}
+
+func TestOAuthJwtGrantTypeSettings(t *testing.T) {
+	// test files for TLS testing
+	var (
+		testCAFile   = "testdata/testCA.pem"
+		testCertFile = "testdata/test-cert.pem"
+		testKeyFile  = "testdata/test-key.pem"
+	)
+
+	tests := []struct {
+		name          string
+		settings      *Config
+		shouldError   bool
+		expectedError string
+	}{
+		{
+			name: "all_valid_settings",
+			settings: &Config{
+				ClientID:               "testclientid",
+				ClientCertificateKey:   "testsecret",
+				EndpointParams:         url.Values{"audience": []string{"someaudience"}},
+				TokenURL:               "https://example.com/v1/token",
+				Scopes:                 []string{"resource.read"},
+				SignatureAlgorithm:     "RS512",
+				Iss:                    "my-issuer",
+				Audience:               "my-audience",
+				ClientCertificateKeyID: "1234",
+				Claims:                 map[string]any{"random": "value"},
+				Timeout:                2,
+				ExpiryBuffer:           10 * time.Second,
+				TLS: configtls.ClientConfig{
+					Config: configtls.Config{
+						CAFile:   testCAFile,
+						CertFile: testCertFile,
+						KeyFile:  testKeyFile,
+					},
+					Insecure:           false,
+					InsecureSkipVerify: false,
+				},
+			},
+			shouldError:   false,
+			expectedError: "",
+		},
+		{
+			name: "empty_signature_algorithm_defaults",
+			settings: &Config{
+				SignatureAlgorithm: "RS256",
+			},
+			shouldError: false,
+		},
+		{
+			name: "invalid_signature_algorithm_fails",
+			settings: &Config{
+				ClientID:             "testclientid",
+				ClientCertificateKey: "testsecret",
+				TokenURL:             "https://example.com/v1/token",
+				SignatureAlgorithm:   "INVALID_ALGORITHM",
+			},
+			shouldError:   true,
+			expectedError: "invalid signature algorithm",
+		},
+		{
+			name: "iss_falls_back_to_client_id",
+			settings: &Config{
+				ClientID:           "testclientid",
+				SignatureAlgorithm: "RS256",
+				Iss:                "testclientid",
+			},
+			shouldError: false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			rc, err := newJwtGrantTypeConfig(test.settings)
+			if test.shouldError {
+				assert.ErrorContains(t, err, test.expectedError)
+				return
+			}
+			assert.NoError(t, err)
+			assert.Equal(t, test.settings.Scopes, rc.Scopes)
+			assert.Equal(t, test.settings.TokenURL, rc.TokenURL)
+			assert.EqualValues(t, test.settings.ClientCertificateKey, rc.PrivateKey)
+			assert.Equal(t, test.settings.ClientID, rc.Subject)
+			assert.Equal(t, test.settings.SignatureAlgorithm, rc.SigningAlgorithm.Name)
+			assert.Equal(t, test.settings.ClientCertificateKeyID, rc.PrivateKeyID)
+			assert.Equal(t, test.settings.EndpointParams, rc.EndpointParams)
+			assert.Equal(t, test.settings.Iss, rc.Iss)
+			assert.Equal(t, test.settings.Claims, rc.PrivateClaims)
 		})
 	}
 }
@@ -197,7 +227,7 @@ func TestRoundTripper(t *testing.T) {
 		shouldError bool
 	}{
 		{
-			name: "returns_http_round_tripper",
+			name: "returns_empty_grant_http_round_tripper",
 			settings: &Config{
 				ClientID:     "testclientid",
 				ClientSecret: "testsecret",
@@ -205,6 +235,39 @@ func TestRoundTripper(t *testing.T) {
 				Scopes:       []string{"resource.read"},
 			},
 			shouldError: false,
+		},
+		{
+			name: "returns_client_credentials_grant_http_round_tripper",
+			settings: &Config{
+				ClientID:     "testclientid",
+				ClientSecret: "testsecret",
+				GrantType:    "client_credentials",
+				TokenURL:     "https://example.com/v1/token",
+				Scopes:       []string{"resource.read"},
+			},
+			shouldError: false,
+		},
+		{
+			name: "returns_jwt_bearer_grant_http_round_tripper",
+			settings: &Config{
+				ClientID:             "testclientid",
+				ClientCertificateKey: "testsecret",
+				GrantType:            "urn:ietf:params:oauth:grant-type:jwt-bearer",
+				TokenURL:             "https://example.com/v1/token",
+				Scopes:               []string{"resource.read"},
+			},
+			shouldError: false,
+		},
+		{
+			name: "invalid_grant_type_returns_error",
+			settings: &Config{
+				ClientID:     "testclientid",
+				ClientSecret: "testsecret",
+				GrantType:    "worong_grant_type",
+				TokenURL:     "https://example.com/v1/token",
+				Scopes:       []string{"resource.read"},
+			},
+			shouldError: true,
 		},
 	}
 
@@ -244,13 +307,25 @@ func TestOAuth2PerRPCCredentials(t *testing.T) {
 		expectedErr error
 	}{
 		{
-			name: "returns_http_round_tripper",
+			name: "returns_client_credentials_grant_http_round_tripper",
 			settings: &Config{
 				ClientID:     "testclientid",
 				ClientSecret: "testsecret",
 				TokenURL:     "https://example.com/v1/token",
 				Scopes:       []string{"resource.read"},
 				Timeout:      1,
+			},
+			shouldError: false,
+		},
+		{
+			name: "returns_jwt_bearer_grant_http_round_tripper",
+			settings: &Config{
+				ClientID:             "testclientid",
+				ClientCertificateKey: "testsecret",
+				GrantType:            "urn:ietf:params:oauth:grant-type:jwt-bearer",
+				TokenURL:             "https://example.com/v1/token",
+				Scopes:               []string{"resource.read"},
+				Timeout:              1,
 			},
 			shouldError: false,
 		},
@@ -314,4 +389,94 @@ func TestFailContactingOAuth(t *testing.T) {
 	_, err = client.Do(req)
 	assert.ErrorIs(t, err, errFailedToGetSecurityToken)
 	assert.ErrorContains(t, err, serverURL.String())
+}
+
+func TestJwtOAuth(t *testing.T) {
+	tokenTS := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		t.Helper()
+
+		auth := r.Header.Get("Authorization")
+		if auth == "" {
+			assert.NoErrorf(t, r.ParseForm(), "Failed to parse form")
+			auth = r.FormValue("assertion")
+		}
+
+		jwtParts := strings.Split(auth, ".")
+		assert.Lenf(t, jwtParts, 3, "Expected JWT to have 3 parts, got %d", len(jwtParts))
+
+		// Decode the JWT payload.
+		payload, err := base64.RawURLEncoding.DecodeString(jwtParts[1])
+		assert.NoErrorf(t, err, "Failed to decode JWT payload: %v", err)
+
+		var jwt struct {
+			Aud     string `json:"aud"`
+			Scope   string `json:"scope"`
+			Sub     string `json:"sub"`
+			Iss     string `json:"iss"`
+			Integer int    `json:"integer"`
+		}
+
+		err = json.Unmarshal(payload, &jwt)
+		assert.NoErrorf(t, err, "Failed to unmarshal JWT payload: %v", err)
+
+		assert.Equalf(t, "common-test", jwt.Aud, "Expected aud to be 'common-test', got '%s'", jwt.Aud)
+		assert.Equalf(t, "A B", jwt.Scope, "Expected scope to be 'A B', got '%s'", jwt.Scope)
+		assert.Equalf(t, "common", jwt.Sub, "Expected sub to be 'common', got '%s'", jwt.Sub)
+		assert.Equalf(t, "https://example.com", jwt.Iss, "Expected iss to be 'https://example.com', got '%s'", jwt.Iss)
+		assert.Equalf(t, 1, jwt.Integer, "Expected integer to be 1, got '%d'", jwt.Integer)
+
+		w.Header().Add("Content-Type", "application/json")
+
+		type oauth2TestServerResponse struct {
+			AccessToken string `json:"access_token"`
+			TokenType   string `json:"token_type"`
+		}
+		res, _ := json.Marshal(oauth2TestServerResponse{
+			AccessToken: "12345",
+			TokenType:   "Bearer",
+		})
+		w.Header().Add("Content-Type", "application/json")
+		_, _ = w.Write(res)
+	}))
+	defer tokenTS.Close()
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		auth := r.Header.Get("Authorization")
+		assert.Equalf(t, "Bearer 12345", auth, "bad auth, expected %s, got %s", "Bearer 12345", auth)
+		fmt.Fprintln(w, "Hello, client")
+	}))
+	defer ts.Close()
+
+	serverURL, err := url.Parse(tokenTS.URL)
+	require.NoError(t, err)
+
+	oauth2Authenticator, err := newClientAuthenticator(&Config{
+		ClientID:                 "1",
+		ClientCertificateKeyFile: "testdata/client.key",
+		GrantType:                "urn:ietf:params:oauth:grant-type:jwt-bearer",
+		Scopes:                   []string{"A", "B"},
+		TokenURL:                 serverURL.String(),
+		Claims: map[string]any{
+			"iss":     "https://example.com",
+			"aud":     "common-test",
+			"sub":     "common",
+			"integer": 1,
+		},
+		EndpointParams: url.Values{"hi": []string{"hello"}},
+	}, zap.NewNop())
+	require.NoError(t, err)
+
+	transport := http.DefaultTransport.(*http.Transport).Clone()
+	baseRoundTripper := http.RoundTripper(transport)
+	roundTripper, err := oauth2Authenticator.RoundTripper(baseRoundTripper)
+	require.NoError(t, err)
+
+	client := &http.Client{
+		Transport: roundTripper,
+	}
+
+	req, err := http.NewRequest(http.MethodPost, ts.URL, http.NoBody)
+	require.NoError(t, err)
+	_, err = client.Do(req)
+	assert.NoError(t, err)
 }

.chloggen/jwt-bearer-grant.yaml
@@ -1,0 +1,28 @@
+# Use this changelog template to create an entry for release notes.
+
+# One of 'breaking', 'deprecation', 'new_component', 'enhancement', 'bug_fix'
+change_type: enhancement
+
+# The name of the component, or a single word describing the area of concern, (e.g. receiver/filelog)
+component: extension/oauth2client
+
+# A brief description of the change.  Surround your text with quotes ("") if it needs to start with a backtick (`).
+note: Support jwt-bearer grant-type (RFC7523)
+
+# Mandatory: One or more tracking issues related to the change. You can use the PR number here if no issue exists.
+issues:
+  - 44314
+
+# (Optional) One or more lines of additional information to render under the primary note.
+# These lines will be padded with 2 spaces and then inserted directly into the document.
+# Use pipe (|) for multiline entries.
+subtext:
+
+# If your change doesn't affect end users or the exported elements of any package,
+# you should instead start your pull request title with [chore] or use the "Skip Changelog" label.
+# Optional: The change log or logs in which this entry should be included.
+# e.g. '[user]' or '[user, api]'
+# Include 'user' if the change is relevant to end users.
+# Include 'api' if there is a change to a library API.
+# Default: '[user]'
+change_logs: [user]

extension/oauth2clientauthextension/clientcredentialsconfig.go
@@ -15,6 +15,25 @@ import (
 	"golang.org/x/oauth2/clientcredentials"
 )
 
+const (
+	grantTypeClientCredentials = "client_credentials"
+)
+
+func newClientCredentialsGrantTypeConfig(cfg *Config) *clientCredentialsConfig {
+	return &clientCredentialsConfig{
+		Config: clientcredentials.Config{
+			ClientID:       cfg.ClientID,
+			ClientSecret:   string(cfg.ClientSecret),
+			TokenURL:       cfg.TokenURL,
+			Scopes:         cfg.Scopes,
+			EndpointParams: cfg.EndpointParams,
+		},
+		ClientIDFile:     cfg.ClientIDFile,
+		ClientSecretFile: cfg.ClientSecretFile,
+		ExpiryBuffer:     cfg.ExpiryBuffer,
+	}
+}
+
 // clientCredentialsConfig is a clientcredentials.Config wrapper to allow
 // values read from files in the ClientID and ClientSecret fields.
 //
@@ -93,6 +112,10 @@ func (c *clientCredentialsConfig) createConfig() (*clientcredentials.Config, err
 
 func (c *clientCredentialsConfig) TokenSource(ctx context.Context) oauth2.TokenSource {
 	return oauth2.ReuseTokenSourceWithExpiry(nil, clientCredentialsTokenSource{ctx: ctx, config: c}, c.ExpiryBuffer)
+}
+
+func (c *clientCredentialsConfig) TokenEndpoint() string {
+	return c.TokenURL
 }
 
 func (ts clientCredentialsTokenSource) Token() (*oauth2.Token, error) {

extension/oauth2clientauthextension/config.go
@@ -14,9 +14,11 @@ import (
 )
 
 var (
-	errNoClientIDProvided     = errors.New("no ClientID provided in the OAuth2 exporter configuration")
-	errNoTokenURLProvided     = errors.New("no TokenURL provided in OAuth Client Credentials configuration")
-	errNoClientSecretProvided = errors.New("no ClientSecret provided in OAuth Client Credentials configuration")
+	errNoClientIDProvided          = errors.New("no ClientID provided in the OAuth2 exporter configuration")
+	errNoClientCertificateProvided = errors.New("no ClientCertificateKey provided in the OAuth2 exporter configuration")
+	errInvalidSignatureAlg         = errors.New("invalid signature algorithm")
+	errNoTokenURLProvided          = errors.New("no TokenURL provided in OAuth Client Credentials configuration")
+	errNoClientSecretProvided      = errors.New("no ClientSecret provided in OAuth Client Credentials configuration")
 )
 
 // Config stores the configuration for OAuth2 Client Credentials (2-legged OAuth2 flow) setup.
@@ -34,6 +36,43 @@ type Config struct {
 
 	// ClientSecretFile is the file pathg to read the application's secret from.
 	ClientSecretFile string `mapstructure:"client_secret_file"`
+
+	// ClientCertificateKeyID is the Key ID to include in the jwt. Only used if
+	// GrantType is set to "urn:ietf:params:oauth:grant-type:jwt-bearer".
+	ClientCertificateKeyID string `mapstructure:"client_certificate_key_id"`
+
+	// ClientCertificateKey is the application's private key. Only used if
+	// GrantType is set to "urn:ietf:params:oauth:grant-type:jwt-bearer".
+	ClientCertificateKey configopaque.String `mapstructure:"client_certificate_key"`
+
+	// ClientSecrClientCertificateKeyFileetFile is the file pathg to read the application's secret from. Only used if
+	// GrantType is set to "urn:ietf:params:oauth:grant-type:jwt-bearer".
+	ClientCertificateKeyFile string `mapstructure:"client_certificate_key_file"`
+
+	// GrantType is the OAuth2 grant type to use. It can be one of
+	// "client_credentials" or "urn:ietf:params:oauth:grant-type:jwt-bearer" (RFC 7523).
+	// Default value is "client_credentials"
+	GrantType string `mapstructure:"grant_type"`
+
+	// SignatureAlgorithm is the RSA algorithm used to sign JWT token. Only used if
+	// GrantType is set to "urn:ietf:params:oauth:grant-type:jwt-bearer".
+	// Default value is RS256 and valid values RS256, RS384, RS512
+	SignatureAlgorithm string `mapstructure:"signature_algorithm,omitempty"`
+
+	// Iss is the OAuth client identifier used when communicating with
+	// the configured OAuth provider. Default value is client_id. Only used if
+	// GrantType is set to "urn:ietf:params:oauth:grant-type:jwt-bearer".
+	Iss string `mapstructure:"iss,omitempty"`
+
+	// Audience optionally specifies the intended audience of the
+	// request.  If empty, the value of TokenURL is used as the
+	// intended audience. Only used if
+	// GrantType is set to "urn:ietf:params:oauth:grant-type:jwt-bearer".
+	Audience string `mapstructure:"audience,omitempty"`
+
+	// Claims is a map of claims to be added to the JWT token. Only used if
+	// GrantType is set to "urn:ietf:params:oauth:grant-type:jwt-bearer".
+	Claims map[string]any `mapstructure:"claims,omitempty"`
 
 	// EndpointParams specifies additional parameters for requests to the token endpoint.
 	EndpointParams url.Values `mapstructure:"endpoint_params"`
@@ -65,9 +104,16 @@ func (cfg *Config) Validate() error {
 	if cfg.ClientID == "" && cfg.ClientIDFile == "" {
 		return errNoClientIDProvided
 	}
-	if cfg.ClientSecret == "" && cfg.ClientSecretFile == "" {
-		return errNoClientSecretProvided
+	if cfg.GrantType == grantTypeJWTBearer {
+		if cfg.ClientCertificateKey == "" && cfg.ClientCertificateKeyFile == "" {
+			return errNoClientCertificateProvided
+		}
+	} else {
+		if cfg.ClientSecret == "" && cfg.ClientSecretFile == "" {
+			return errNoClientSecretProvided
+		}
 	}
+
 	if cfg.TokenURL == "" {
 		return errNoTokenURLProvided
 	}

commit 95dc24a597eba4d20c796757b71b77606921fd64

[extension/oauth2clientauth] fix client-credentials grant type (#45787)
#### Description

Fix a regression introduced by
https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/44318,
which led to the extension using the JWT bearer auth flow even when
client-credentials is configured.

#### Link to tracking issue

Fixes #45786

#### Testing

Added a unit test that failed with the bug.

#### Documentation

N/A

.chloggen/oauth2clientauthextension-clientcredentials-jwt.yaml
@@ -1,0 +1,27 @@
+# Use this changelog template to create an entry for release notes.
+
+# One of 'breaking', 'deprecation', 'new_component', 'enhancement', 'bug_fix'
+change_type: bug_fix
+
+# The name of the component, or a single word describing the area of concern, (e.g. receiver/filelog)
+component: extension/oauth2client
+
+# A brief description of the change.  Surround your text with quotes ("") if it needs to start with a backtick (`).
+note: Fix oauth2clientauth client-credentials grant type
+
+# Mandatory: One or more tracking issues related to the change. You can use the PR number here if no issue exists.
+issues: [45786]
+
+# (Optional) One or more lines of additional information to render under the primary note.
+# These lines will be padded with 2 spaces and then inserted directly into the document.
+# Use pipe (|) for multiline entries.
+subtext:
+
+# If your change doesn't affect end users or the exported elements of any package,
+# you should instead start your pull request title with [chore] or use the "Skip Changelog" label.
+# Optional: The change log or logs in which this entry should be included.
+# e.g. '[user]' or '[user, api]'
+# Include 'user' if the change is relevant to end users.
+# Include 'api' if there is a change to a library API.
+# Default: '[user]'
+change_logs: [user]

extension/oauth2clientauthextension/extension.go
@@ -70,10 +70,7 @@ func newClientAuthenticator(cfg *Config, logger *zap.Logger) (*clientAuthenticat
 			return nil, err
 		}
 	case grantTypeClientCredentials, "":
-		credentials, err = newJwtGrantTypeConfig(cfg)
-		if err != nil {
-			return nil, err
-		}
+		credentials = newClientCredentialsGrantTypeConfig(cfg)
 	default:
 		return nil, fmt.Errorf("unknown grant type %q", cfg.GrantType)
 	}

extension/oauth2clientauthextension/extension_test.go
@@ -391,6 +391,70 @@ func TestFailContactingOAuth(t *testing.T) {
 	assert.ErrorContains(t, err, serverURL.String())
 }
 
+func TestClientCredentials(t *testing.T) {
+	resourceServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		auth := r.Header.Get("Authorization")
+		if auth != "Bearer ok" {
+			t.Errorf("unexpected Authorization header: %q", auth)
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("success"))
+	}))
+	defer resourceServer.Close()
+
+	var tokenRequests int
+	var tokenRequest *http.Request
+	var tokenRequestForm url.Values
+	tokenServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tokenRequests++
+		if err := r.ParseForm(); err != nil {
+			panic(err)
+		}
+		tokenRequest = r
+		tokenRequestForm = r.PostForm
+
+		w.Header().Set("Content-Type", "application/json")
+		_, _ = w.Write([]byte(`{"access_token":"ok","token_type":"Bearer"}`))
+	}))
+	defer tokenServer.Close()
+
+	cfg := &Config{
+		ClientID:     "test-client-id",
+		ClientSecret: "test-client-secret",
+		GrantType:    "client_credentials",
+		TokenURL:     tokenServer.URL,
+		Scopes:       []string{"scope1"},
+	}
+	oauth2Authenticator, err := newClientAuthenticator(cfg, zap.NewNop())
+	require.NoError(t, err)
+
+	roundTripper, err := oauth2Authenticator.RoundTripper(http.DefaultTransport)
+	require.NoError(t, err)
+
+	client := &http.Client{Transport: roundTripper}
+	resp, err := client.Get(resourceServer.URL)
+	require.NoError(t, err)
+	defer resp.Body.Close()
+
+	// Token endpoint must have been called with client_credentials flow
+	require.Equal(t, 1, tokenRequests, "token endpoint should have been called once")
+
+	// client-credentials will first attempt with client ID and secret in the
+	// Authorization header, and if that fails it will fall back to passing them
+	// as form values. We just check the first here.
+	assert.Equal(t, url.Values{
+		"grant_type": []string{"client_credentials"},
+		"scope":      []string{"scope1"},
+	}, tokenRequestForm)
+
+	username, password, ok := tokenRequest.BasicAuth()
+	require.True(t, ok)
+	assert.Equal(t, "test-client-id", username)
+	assert.Equal(t, "test-client-secret", password)
+}
+
 func TestJwtOAuth(t *testing.T) {
 	tokenTS := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		t.Helper()