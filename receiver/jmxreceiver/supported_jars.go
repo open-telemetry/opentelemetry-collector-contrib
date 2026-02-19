32ab6b94e8ddab7c7b398c205bdfe730393b5665
otelbot <197425009+otelbot@users.noreply.github.com>
2026-02-19 02:14:01 +0000

    Add JMX metrics gatherer version 1.54.0-alpha

--- receiver/jmxreceiver/supported_jars.go
+++ receiver/jmxreceiver/supported_jars.go
@@ -65,6 +65,10 @@ var jmxScraperVersions = map[string]supportedJar{
 // If you change this variable name, please open an issue in opentelemetry-java-contrib
 // so that repository's release automation can be updated
 var jmxMetricsGathererVersions = map[string]supportedJar{
+	"ff04b1b0109d4b4fd1a21191424807ea16a447aad4b9a203476a01512bfc7c79": {
+		version: "1.54.0-alpha",
+		jar:     "JMX metrics gatherer",
+	},
 	"10da69b4a8017403e81bc79f0cdd92813e1f6926ef9bbea4e1bae911d0ffe1a9": {
 		version: "1.53.0-alpha",
 		jar:     "JMX metrics gatherer",
