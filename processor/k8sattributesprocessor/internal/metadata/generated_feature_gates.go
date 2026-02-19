1b977c49e939f6d0d2d8a2ef91c59ed0248f9f38
Christos Markou <chrismarkou92@gmail.com>
2026-02-19 11:37:04 +0200

    [k8sattributes] rename internal telemetry (#45940)
    <!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
    Ex. Adding a feature - Explain what this achieves.-->
    #### Description
    
    This PR applies the renaming of the internal telemetry metrics in order
    to use dots instead of underscores in alignment with
    https://github.com/open-telemetry/opentelemetry-collector/issues/12909
    and OTel SemConv.
    
    This will happen through a feature gate pair:
    - `processor.k8sattributes.telemetry.disableOldFormatMetrics`
    - `processor.k8sattributes.telemetry.enableNewFormatMetrics`
    
    My suggestion will be to keep these gates in alpha for 3 releases, then
    promote them to beta for another 3 before making them stable.
    
    The renames are the following:
    
    ```
    otelsvc_k8s_daemonset_added -> otelcol.k8s.watcher.daemonset.added
    otelsvc_k8s_daemonset_deleted -> otelcol.k8s.watcher.daemonset.deleted
    otelsvc_k8s_daemonset_updated -> otelcol.k8s.watcher.daemonset.updated
    otelsvc_k8s_deployment_added -> otelcol.k8s.watcher.deployment.added
    otelsvc_k8s_deployment_deleted -> otelcol.k8s.watcher.deployment.deleted
    otelsvc_k8s_deployment_updated -> otelcol.k8s.watcher.deployment.updated
    otelsvc_k8s_job_added -> otelcol.k8s.watcher.job.added
    otelsvc_k8s_job_deleted -> otelcol.k8s.watcher.job.deleted
    otelsvc_k8s_job_updated -> otelcol.k8s.watcher.job.updated
    otelsvc_k8s_namespace_added -> otelcol.k8s.watcher.namespace.added
    otelsvc_k8s_namespace_deleted -> otelcol.k8s.watcher.namespace.deleted
    otelsvc_k8s_namespace_updated -> otelcol.k8s.watcher.namespace.updated
    otelsvc_k8s_node_added -> otelcol.k8s.watcher.node.added
    otelsvc_k8s_node_deleted -> otelcol.k8s.watcher.node.deleted
    otelsvc_k8s_node_updated -> otelcol.k8s.watcher.node.updated
    otelsvc_k8s_pod_added -> otelcol.k8s.watcher.pod.added
    otelsvc_k8s_pod_deleted -> otelcol.k8s.watcher.pod.deleted
    otelsvc_k8s_pod_updated -> otelcol.k8s.watcher.pod.updated
    otelsvc_k8s_pod_table_size -> otelcol.k8s.watcher.pod_cache.size
    otelsvc_k8s_replicaset_added -> otelcol.k8s.watcher.replicaset.added
    otelsvc_k8s_replicaset_deleted -> otelcol.k8s.watcher.replicaset.deleted
    otelsvc_k8s_replicaset_updated -> otelcol.k8s.watcher.replicaset.updated
    otelsvc_k8s_statefulset_added -> otelcol.k8s.watcher.statefulset.added
    otelsvc_k8s_statefulset_deleted -> otelcol.k8s.watcher.statefulset.deleted
    otelsvc_k8s_statefulset_updated -> otelcol.k8s.watcher.statefulset.updated
    ```
    
    `otelcol_otelsvc_k8s_ip_lookup_miss` -> [DEPRECATED] Number of times pod
    by IP lookup failed
    
    > **Deprecated since 0.146.0**
    > Use otelcol.k8s.pod.association instead
    
    <!-- Issue number (e.g. #1234) or full URL to issue, if applicable. -->
    #### Link to tracking issue
    Fixes
    https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/45871
    
    <!--Describe what testing was performed and which tests were added.-->
    #### Testing
    ~
    
    <!--Describe the documentation added.-->
    #### Documentation
    Updated
    
    <!--Please delete paragraphs that you did not use before submitting.-->
    
    Signed-off-by: ChrsMark <chrismarkou92@gmail.com>

--- processor/k8sattributesprocessor/internal/metadata/generated_feature_gates.go
+++ processor/k8sattributesprocessor/internal/metadata/generated_feature_gates.go
@@ -30,3 +30,19 @@ var ProcessorK8sattributesEmitV1K8sConventionsFeatureGate = featuregate.GlobalRe
 	featuregate.WithRegisterReferenceURL("https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/44589"),
 	featuregate.WithRegisterFromVersion("v0.145.0"),
 )
+
+var ProcessorK8sattributesTelemetryDisableOldFormatMetricsFeatureGate = featuregate.GlobalRegistry().MustRegister(
+	"processor.k8sattributes.telemetry.disableOldFormatMetrics",
+	featuregate.StageAlpha,
+	featuregate.WithRegisterDescription("When enabled, old formatted internal telemetry metrics are disabled."),
+	featuregate.WithRegisterReferenceURL("https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/45871"),
+	featuregate.WithRegisterFromVersion("v0.146.0"),
+)
+
+var ProcessorK8sattributesTelemetryEnableNewFormatMetricsFeatureGate = featuregate.GlobalRegistry().MustRegister(
+	"processor.k8sattributes.telemetry.enableNewFormatMetrics",
+	featuregate.StageAlpha,
+	featuregate.WithRegisterDescription("When enabled, new formatted internal telemetry metrics are enabled."),
+	featuregate.WithRegisterReferenceURL("https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/45871"),
+	featuregate.WithRegisterFromVersion("v0.146.0"),
+)
