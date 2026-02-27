cf127e6ff800b18537165bbd845ec9b62d9450e0
Isaac Flores <isaac.flores@elastic.co>
2026-02-27 11:48:56 -0800

    fix summary.go lint issue

--- exporter/elasticsearchexporter/internal/datapoints/summary.go
+++ exporter/elasticsearchexporter/internal/datapoints/summary.go
@@ -34,7 +34,7 @@ func (dp Summary) Value() (pcommon.Value, error) {
 	return vm, nil
 }
 
-func (dp Summary) DynamicTemplate(_ pmetric.Metric, mode DynamicTemplateMode) string {
+func (Summary) DynamicTemplate(_ pmetric.Metric, mode DynamicTemplateMode) string {
 	if mode == DynamicTemplateModeECS {
 		return "summary_metrics"
 	}
