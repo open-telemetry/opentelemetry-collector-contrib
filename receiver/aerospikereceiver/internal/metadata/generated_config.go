0e3653e01805ad616ca4bf3747ce3c368585580c
Mohammed ElDegwi <mohammedeldegwi@gmail.com>
2026-03-09 07:08:38 +0000

    chore: make generate
    Signed-off-by: Mohammed ElDegwi <mohammedeldegwi@gmail.com>

--- receiver/aerospikereceiver/internal/metadata/generated_config.go
+++ receiver/aerospikereceiver/internal/metadata/generated_config.go
@@ -54,11 +54,6 @@ func (ms *MetricConfig) Unmarshal(parser *confmap.Conf) error {
 	return nil
 }
 
-// AttributeConfig holds configuration information for a particular metric.
-type AttributeConfig struct {
-	Enabled bool `mapstructure:"enabled"`
-}
-
 // MetricsConfig provides config for aerospike metrics.
 type MetricsConfig struct {
 	AerospikeNamespaceDiskAvailable                   MetricConfig `mapstructure:"aerospike.namespace.disk.available"`
