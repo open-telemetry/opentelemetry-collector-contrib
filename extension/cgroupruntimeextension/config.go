55053ab4f03e8b4b92a3d33176d5ced2f3d7049d
singhvibhanshu <singhvibhanshu@hotmail.com>
2026-03-10 10:10:48 +0530

    applied the suggested changes

--- extension/cgroupruntimeextension/config.go
+++ extension/cgroupruntimeextension/config.go
@@ -22,8 +22,10 @@ type GoMaxProcsConfig struct {
 }
 
 type GoMemLimitConfig struct {
-	Enabled         bool          `mapstructure:"enabled"`
-	Ratio           float64       `mapstructure:"ratio"`
+	Enabled bool    `mapstructure:"enabled"`
+	Ratio   float64 `mapstructure:"ratio"`
+	// RefreshInterval configures the refresh interval for GOMEMLIMIT.
+	// Setting the duration to 0 (default) disables periodic auto update.
 	RefreshInterval time.Duration `mapstructure:"refresh_interval"`
 	// prevent unkeyed literal initialization
 	_ struct{}
@@ -33,6 +35,9 @@ type GoMemLimitConfig struct {
 func (cfg *Config) Validate() error {
 	if cfg.GoMemLimit.Ratio <= 0 || cfg.GoMemLimit.Ratio > 1 {
 		return errors.New("gomemlimit ratio must be in the (0.0,1.0] range")
+	}
+	if cfg.GoMemLimit.RefreshInterval < 0 {
+		return errors.New("refresh_interval: requires non negative value")
 	}
 	return nil
 }
