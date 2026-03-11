ed310ba91475d975de4f3c437f98a71782fc0521
ebrdarSplunk <ebrdar@cisco.com>
2026-03-11 12:20:04 +0000

    disabling default event collection

--- receiver/postgresqlreceiver/internal/metadata/generated_config.go
+++ receiver/postgresqlreceiver/internal/metadata/generated_config.go
@@ -208,10 +208,10 @@ type EventsConfig struct {
 func DefaultEventsConfig() EventsConfig {
 	return EventsConfig{
 		DbServerQuerySample: EventConfig{
-			Enabled: true,
+			Enabled: false,
 		},
 		DbServerTopQuery: EventConfig{
-			Enabled: true,
+			Enabled: false,
 		},
 	}
 }
