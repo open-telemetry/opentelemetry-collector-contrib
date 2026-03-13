b7e097815e45cb470a57dcf387530771e073fd01
Elmir <ebrdar@cisco.com>
2026-03-13 16:48:11 +0000

    Add command_type attribute in the top_query template (#46839)
    <!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
    Ex. Adding a feature - Explain what this achieves.-->
    #### Description
    Currently we dont extract command_type. As a result it doesn't allow
    flexibility down-stream to filter on which queries on specific scenarios
    you may wish to exclude
    
    <!-- Issue number (e.g. #1234) or full URL to issue, if applicable. -->
    #### Link to tracking issue
    Fixes
    https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/46838
    
    <!--Describe what testing was performed and which tests were added.-->
    #### Testing
    updated unit tests and local build connecting to local oracle instance
    
    <!--Describe the documentation added.-->
    #### Documentation
    Updated

--- receiver/oracledbreceiver/scraper.go
+++ receiver/oracledbreceiver/scraper.go
@@ -96,9 +96,10 @@ const (
 	procedureExecutionsMetric   = "PROCEDURE_EXECUTIONS"
 
 	// Stored procedure columns
-	objectIDAttr   = "PROGRAM_ID"
-	objectNameAttr = "PROCEDURE_NAME"
-	objectTypeAttr = "PROCEDURE_TYPE"
+	objectIDAttr    = "PROGRAM_ID"
+	objectNameAttr  = "PROCEDURE_NAME"
+	objectTypeAttr  = "PROCEDURE_TYPE"
+	commandTypeAttr = "COMMAND_TYPE"
 )
 
 var (
@@ -533,6 +534,7 @@ type queryMetricCacheHit struct {
 	objectID     int64
 	objectName   string
 	objectType   string
+	commandType  int64
 }
 
 func (s *oracleScraper) scrapeLogs(ctx context.Context) (plog.Logs, error) {
@@ -601,6 +603,11 @@ func (s *oracleScraper) collectTopNMetricData(ctx context.Context, logs plog.Log
 				objectID, _ = strconv.ParseInt(row[objectIDAttr], 10, 64)
 			}
 
+			var commandType int64
+			if row[commandTypeAttr] != "" {
+				commandType, _ = strconv.ParseInt(row[commandTypeAttr], 10, 64)
+			}
+
 			hit := queryMetricCacheHit{
 				sqlID:        row[sqlIDAttr],
 				queryText:    row[sqlTextAttr],
@@ -610,6 +617,7 @@ func (s *oracleScraper) collectTopNMetricData(ctx context.Context, logs plog.Log
 				objectID:     objectID,
 				objectName:   row[objectNameAttr],
 				objectType:   row[objectTypeAttr],
+				commandType:  commandType,
 			}
 
 			var possiblePurge bool
@@ -677,6 +685,7 @@ func (s *oracleScraper) collectTopNMetricData(ctx context.Context, logs plog.Log
 			asFloatInSeconds(hit.metrics[applicationWaitTimeMetric]),
 			hit.metrics[bufferGetsMetric],
 			asFloatInSeconds(hit.metrics[clusterWaitTimeMetric]),
+			hit.commandType,
 			asFloatInSeconds(hit.metrics[concurrencyWaitTimeMetric]),
 			asFloatInSeconds(hit.metrics[cpuTimeMetric]),
 			hit.metrics[queryDirectReadsMetric],
