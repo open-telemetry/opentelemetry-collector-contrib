367959afad2fca96212f39b133d50ed4acaf729e
Antoine Toulme <atoulme@splunk.com>
2026-03-18 23:24:13 -0700

    [datadog] document aix support

---
+++ connector/datadogconnector/factory_aix.go
@@ -0,0 +1,10 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+//go:build aix
+package datadogconnector // import "github.com/open-telemetry/opentelemetry-collector-contrib/connector/datadogconnector"
+import "go.opentelemetry.io/collector/connector"
+
+func NewFactory() connector.Factory {
+	panic("aix is not supported")
+}
---
+++ exporter/datadogexporter/factory_aix.go
@@ -0,0 +1,9 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+//go:build !aix
+package datadogexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/datadogexporter"
+
+func NewFactory() exporter.Factory {
+	panic("aix is not supported")
+}
