f72cd6db2a3e258cd88d72b6a2dadc1ee3d6edeb
Antoine Toulme <atoulme@splunk.com>
2026-03-19 19:28:01 -0700

    generate

--- connector/datadogconnector/factory_aix.go
+++ connector/datadogconnector/factory_aix.go
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 //go:build aix
+
 package datadogconnector // import "github.com/open-telemetry/opentelemetry-collector-contrib/connector/datadogconnector"
 import "go.opentelemetry.io/collector/connector"
 
