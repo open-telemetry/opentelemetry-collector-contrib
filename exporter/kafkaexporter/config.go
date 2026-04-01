9b570f46604d466de445f98a558dbf835cb49b71
Vihas Makwana <121151420+VihasMakwana@users.noreply.github.com>
2026-04-01 15:25:53 +0530

    Update exporter/kafkaexporter/config.go
    Co-authored-by: Paulo Dias <44772900+paulojmdias@users.noreply.github.com>

--- exporter/kafkaexporter/config.go
+++ exporter/kafkaexporter/config.go
@@ -50,7 +50,6 @@ type RecordPartitionerConfig struct {
 }
 
 func (c *RecordPartitionerConfig) Validate() error {
-	fmt.Println(c.Type)
 	switch c.Type {
 	case RecordPartitionerTypeSaramaCompatible,
 		RecordPartitionerTypeRoundRobin,
