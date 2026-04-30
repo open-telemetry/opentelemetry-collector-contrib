04e12f4a1c796872fbfd0ed8a2f288907be23e5a
Mohammed ElDegwi <mohammedeldegwi@gmail.com>
2026-04-30 07:34:38 +0000

    make gci
    Signed-off-by: Mohammed ElDegwi <mohammedeldegwi@gmail.com>

--- pkg/ottl/ottlfuncs/func_split.go
+++ pkg/ottl/ottlfuncs/func_split.go
@@ -8,8 +8,9 @@ import (
 	"errors"
 	"strings"
 
-	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl"
 	"go.opentelemetry.io/collector/pdata/pcommon"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl"
 )
 
 type SplitArguments[K any] struct {
