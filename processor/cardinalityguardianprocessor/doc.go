793a34c2b6f5ed54670fffaac91634998a37a09d
YElayyat <210003647+YElayyat@users.noreply.github.com>
2026-05-01 17:06:35 -0700

    Address CI failures: update go:generate to use make mdatagen

--- processor/cardinalityguardianprocessor/doc.go
+++ processor/cardinalityguardianprocessor/doc.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-//go:generate mdatagen metadata.yaml
+//go:generate make mdatagen
 
 // Package cardinalityguardianprocessor provides a processor that drops high-cardinality metrics.
 package cardinalityguardianprocessor // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/cardinalityguardianprocessor"
