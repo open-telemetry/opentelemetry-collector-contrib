94fb9b29a513165aa30d538f46662f9a5c19c294
Himanshu Singh <himanshu.singh@sumologic.com>
2026-05-09 12:44:55 +0530

    bench: add target_info and scope labels to BenchmarkRemoteWrite

--- receiver/prometheusremotewritereceiver/receiver_bench_test.go
+++ receiver/prometheusremotewritereceiver/receiver_bench_test.go
@@ -15,9 +15,6 @@ import (
 	remoteapi "github.com/prometheus/client_golang/exp/api/remote"
 	"github.com/prometheus/prometheus/model/labels"
 	writev2 "github.com/prometheus/prometheus/prompb/io/prometheus/write/v2"
-	"go.opentelemetry.io/collector/pdata/pcommon"
-
-	"github.com/open-telemetry/opentelemetry-collector-contrib/internal/exp/metrics/identity"
 )
 
 // makeLabels builds a labels.Labels with the given total number of labels.
@@ -53,28 +50,64 @@ func BenchmarkExtractAttributes(b *testing.B) {
 
 // makeWriteV2Request builds a deterministic writev2.Request with the requested number of series,
 // samples per series, and extra labels per series.
+// It includes a target_info series with a few extra resource-level labels (service_name,
+// service_version, cloud_provider) sharing the same job/instance as all regular metrics,
+// and otel_scope_name, otel_scope_version, otel_scope_schema_url, and a custom scope attribute
+// on every regular metric series.
 func makeWriteV2Request(numSeries, samplesPerSeries, extraLabels int) *writev2.Request {
-	symbols := []string{"", "__name__", "job", "my_job", "instance", "my_instance"}
-	// extra labels
-	extraLabelIndexStart := len(symbols)
-	for i := range extraLabels {
-		k := fmt.Sprintf("k%d", i)
-		v := fmt.Sprintf("v%d", i)
-		symbols = append(symbols, k, v)
+	symbols := []string{
+		"",
+		"__name__",
+		"job",
+		"my_job",
+		"instance",
+		"my_instance",
+		"target_info",
+		"service_name",
+		"my_service",
+		"service_version",
+		"v1.0",
+		"cloud_provider",
+		"gcp",
+		"otel_scope_name",
+		"bench_scope",
+		"otel_scope_version",
+		"v0.1.0",
+		"otel_scope_schema_url",
+		"https://example.com/schema",
+		"otel_scope_env",
+		"prod",
 	}
 
-	ts := make([]writev2.TimeSeries, 0, numSeries)
+	extraLabelIndexStart := len(symbols)
+	for i := range extraLabels {
+		symbols = append(symbols, fmt.Sprintf("k%d", i), fmt.Sprintf("v%d", i))
+	}
+
+	ts := make([]writev2.TimeSeries, 0, numSeries+1)
+
+	// target_info sets resource attributes for all series sharing the same job/instance.
+	ts = append(ts, writev2.TimeSeries{
+		Metadata:   writev2.Metadata{Type: writev2.Metadata_METRIC_TYPE_GAUGE},
+		LabelsRefs: []uint32{1, 6, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12},
+		Samples:    []writev2.Sample{{Value: 1, Timestamp: 1}},
+	})
+
 	for i := range numSeries {
-		labelRefs := []uint32{1} // __name__
-		// Add a metric name symbol per series to avoid all series being identical
 		metricName := fmt.Sprintf("metric_%d", i)
 		symbols = append(symbols, metricName)
-		labelRefs = append(labelRefs, uint32(len(symbols)-1))
-		// job & instance
-		labelRefs = append(labelRefs, 2, 3, 4, 5)
-		// extra labels
+		nameIdx := uint32(len(symbols) - 1)
+
+		labelRefs := []uint32{
+			1, nameIdx,
+			2, 3,
+			4, 5,
+			13, 14,
+			15, 16,
+			17, 18,
+			19, 20,
+		}
 		for j := range extraLabels {
-			// add refs to key and value
 			labelRefs = append(labelRefs, uint32(extraLabelIndexStart+2*j), uint32(extraLabelIndexStart+2*j+1))
 		}
 
@@ -84,7 +117,7 @@ func makeWriteV2Request(numSeries, samplesPerSeries, extraLabels int) *writev2.R
 		}
 
 		ts = append(ts, writev2.TimeSeries{
-			Metadata:   writev2.Metadata{Type: writev2.Metadata_METRIC_TYPE_GAUGE, HelpRef: 0, UnitRef: 0},
+			Metadata:   writev2.Metadata{Type: writev2.Metadata_METRIC_TYPE_GAUGE},
 			LabelsRefs: labelRefs,
 			Samples:    samples,
 		})
@@ -137,39 +170,5 @@ func BenchmarkRemoteWrite(b *testing.B) {
 				})
 			}
 		}
-	}
-}
-
-// BenchmarkMetricIdentityHash measures the cost of hashing a metric identity,
-// which is the core operation performed per time series during translation.
-func BenchmarkMetricIdentityHash(b *testing.B) {
-	res := pcommon.NewResource()
-	res.Attributes().PutStr("job", "test_job")
-	res.Attributes().PutStr("instance", "localhost:9090")
-	resourceID := identity.OfResource(res)
-
-	si := scopeInfo{Name: "instrumentation_scope", Version: "1.0.0"}
-
-	b.ReportAllocs()
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		mi := createMetricIdentity(resourceID, "http_requests_total", "1", si, writev2.Metadata_METRIC_TYPE_COUNTER)
-		_ = mi.Hash()
-	}
-}
-
-// BenchmarkExemplarKeyHash measures the cost of hashing an exemplar key.
-func BenchmarkExemplarKeyHash(b *testing.B) {
-	key := exemplarKey{
-		ScopeName:    "instrumentation_scope",
-		ScopeVersion: "1.0.0",
-		MetricName:   "http_requests_total",
-		MetricType:   writev2.Metadata_METRIC_TYPE_COUNTER,
-	}
-
-	b.ReportAllocs()
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_ = key.hash()
 	}
 }
