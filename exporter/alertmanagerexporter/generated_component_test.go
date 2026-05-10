cd00c21c203e30eb6b38e34266916addcde306e3
singhvibhanshu <singhvibhanshu@hotmail.com>
2026-05-10 18:10:04 +0530

    ran make generate schema
    Signed-off-by: singhvibhanshu <singhvibhanshu@hotmail.com>

--- exporter/alertmanagerexporter/generated_component_test.go
+++ exporter/alertmanagerexporter/generated_component_test.go
@@ -36,6 +36,7 @@ func TestComponentLifecycle(t *testing.T) {
 		createFn func(ctx context.Context, set exporter.Settings, cfg component.Config) (component.Component, error)
 		name     string
 	}{
+
 		{
 			name: "logs",
 			createFn: func(ctx context.Context, set exporter.Settings, cfg component.Config) (component.Component, error) {
