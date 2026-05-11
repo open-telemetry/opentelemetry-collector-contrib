7ed32fb2f6f1b7d88599f361e2eefdad64f3d21a
Andrew Wilkins <axw@elastic.co>
2026-05-11 08:50:11 +0800

    [chore] [exporter/kafka] refactor marshaler interface (#48215)
    #### Description
    
    Refactor marshalers to incrementally yield (key, value []byte) pairs,
    rather than returning a whole slice of messages. The exporter now pools
    and reuses slices of messages to reduce allocations.
    
    The callback approach will enable us to later create a []*kgo.Record
    slice directly, and avoid an intermediate slice, further reducing
    allocations.
    
    #### Link to tracking issue
    
    None
    
    #### Testing
    
    N/A
    
    #### Documentation
    
    N/A

--- exporter/kafkaexporter/internal/kafkaclient/message.go
+++ exporter/kafkaexporter/internal/kafkaclient/message.go
@@ -3,19 +3,9 @@
 
 package kafkaclient // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/kafkaexporter/internal/kafkaclient"
 
-import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/kafkaexporter/internal/marshaler"
-
-// Messages is a collection of messages (with count) to be sent to Kafka.
-type Messages struct {
-	// Count is the total number of messages across all topics.
-	// Populating this field allows the downstream method to preallocate
-	// the slice of messages, which can improve performance.
-	Count         int
-	TopicMessages []TopicMessages
-}
-
-// TopicMessages represents a collection of messages for a specific topic.
-type TopicMessages struct {
-	Topic    string
-	Messages []marshaler.Message
+// Message is a single Kafka message with its destination topic, key, and value.
+type Message struct {
+	Topic string
+	Key   []byte
+	Value []byte
 }
