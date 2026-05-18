open-telemetry/opentelemetry-collector-contrib

commit 7082982741b226894a6183d511f8780b409678bf

[connector/servicegraphconnector] Add native histograms support (#39951)
<!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
Ex. Adding a feature - Explain what this achieves.-->
#### Description
issue:
https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/35460

connector/servicegraphconnector/connector.go
@@ -13,6 +13,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/lightstep/go-expohisto/structure"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/pdata/pcommon"
@@ -75,9 +76,11 @@ type serviceGraphConnector struct {
 	reqClientDurationSecondsCount        map[string]uint64
 	reqClientDurationSecondsSum          map[string]float64
 	reqClientDurationSecondsBucketCounts map[string][]uint64
+	reqClientDurationExpHistogram        map[string]*structure.Histogram[float64]
 	reqServerDurationSecondsCount        map[string]uint64
 	reqServerDurationSecondsSum          map[string]float64
 	reqServerDurationSecondsBucketCounts map[string][]uint64
+	reqServerDurationExpHistogram        map[string]*structure.Histogram[float64]
 	reqDurationBounds                    []float64
 
 	metricMutex sync.RWMutex
@@ -95,12 +98,15 @@ func newConnector(set component.TelemetrySettings, config component.Config, next
 		set.Logger.Warn("'metrics_exporter' is deprecated and will be removed in a future release. Please remove it from the configuration.")
 	}
 
-	bounds := defaultLatencyHistogramBuckets
-	if legacyLatencyUnitMsFeatureGate.IsEnabled() {
-		bounds = legacyDefaultLatencyHistogramBuckets
-	}
-	if pConfig.LatencyHistogramBuckets != nil {
-		bounds = mapDurationsToFloat(pConfig.LatencyHistogramBuckets)
+	var bounds []float64
+	if pConfig.ExponentialHistogramMaxSize == 0 {
+		bounds = defaultLatencyHistogramBuckets
+		if legacyLatencyUnitMsFeatureGate.IsEnabled() {
+			bounds = legacyDefaultLatencyHistogramBuckets
+		}
+		if pConfig.LatencyHistogramBuckets != nil {
+			bounds = mapDurationsToFloat(pConfig.LatencyHistogramBuckets)
+		}
 	}
 
 	if pConfig.CacheLoop <= 0 {
@@ -141,9 +147,11 @@ func newConnector(set component.TelemetrySettings, config component.Config, next
 		reqClientDurationSecondsCount:        make(map[string]uint64),
 		reqClientDurationSecondsSum:          make(map[string]float64),
 		reqClientDurationSecondsBucketCounts: make(map[string][]uint64),
+		reqClientDurationExpHistogram:        make(map[string]*structure.Histogram[float64]),
 		reqServerDurationSecondsCount:        make(map[string]uint64),
 		reqServerDurationSecondsSum:          make(map[string]float64),
 		reqServerDurationSecondsBucketCounts: make(map[string][]uint64),
+		reqServerDurationExpHistogram:        make(map[string]*structure.Histogram[float64]),
 		reqDurationBounds:                    bounds,
 		keyToMetric:                          make(map[string]metricSeries),
 		shutdownCh:                           make(chan any),
@@ -426,23 +434,53 @@ func (p *serviceGraphConnector) updateDurationMetrics(key string, serverDuration
 }
 
 func (p *serviceGraphConnector) updateServerDurationMetrics(key string, duration float64) {
-	index := sort.SearchFloat64s(p.reqDurationBounds, duration) // Search bucket index
-	if _, ok := p.reqServerDurationSecondsBucketCounts[key]; !ok {
-		p.reqServerDurationSecondsBucketCounts[key] = make([]uint64, len(p.reqDurationBounds)+1)
+	if p.reqDurationBounds == nil {
+		histogram, ok := p.reqServerDurationExpHistogram[key]
+		if !ok {
+			histogram = new(structure.Histogram[float64])
+			cfg := structure.NewConfig(
+				structure.WithMaxSize(p.config.ExponentialHistogramMaxSize),
+			)
+			histogram.Init(cfg)
+			p.reqServerDurationExpHistogram[key] = histogram
+		}
+
+		histogram.Update(duration)
+	} else {
+		index := sort.SearchFloat64s(p.reqDurationBounds, duration) // Search bucket index
+		if _, ok := p.reqServerDurationSecondsBucketCounts[key]; !ok {
+			p.reqServerDurationSecondsBucketCounts[key] = make([]uint64, len(p.reqDurationBounds)+1)
+		}
+
+		p.reqServerDurationSecondsSum[key] += duration
+		p.reqServerDurationSecondsCount[key]++
+		p.reqServerDurationSecondsBucketCounts[key][index]++
 	}
-	p.reqServerDurationSecondsSum[key] += duration
-	p.reqServerDurationSecondsCount[key]++
-	p.reqServerDurationSecondsBucketCounts[key][index]++
 }
 
 func (p *serviceGraphConnector) updateClientDurationMetrics(key string, duration float64) {
-	index := sort.SearchFloat64s(p.reqDurationBounds, duration) // Search bucket index
-	if _, ok := p.reqClientDurationSecondsBucketCounts[key]; !ok {
-		p.reqClientDurationSecondsBucketCounts[key] = make([]uint64, len(p.reqDurationBounds)+1)
+	if p.reqDurationBounds == nil {
+		histogram, ok := p.reqClientDurationExpHistogram[key]
+		if !ok {
+			histogram = new(structure.Histogram[float64])
+			cfg := structure.NewConfig(
+				structure.WithMaxSize(p.config.ExponentialHistogramMaxSize),
+			)
+			histogram.Init(cfg)
+			p.reqClientDurationExpHistogram[key] = histogram
+		}
+
+		histogram.Update(duration)
+	} else {
+		index := sort.SearchFloat64s(p.reqDurationBounds, duration) // Search bucket index
+		if _, ok := p.reqClientDurationSecondsBucketCounts[key]; !ok {
+			p.reqClientDurationSecondsBucketCounts[key] = make([]uint64, len(p.reqDurationBounds)+1)
+		}
+
+		p.reqClientDurationSecondsSum[key] += duration
+		p.reqClientDurationSecondsCount[key]++
+		p.reqClientDurationSecondsBucketCounts[key][index]++
 	}
-	p.reqClientDurationSecondsSum[key] += duration
-	p.reqClientDurationSecondsCount[key]++
-	p.reqClientDurationSecondsBucketCounts[key][index]++
 }
 
 func buildDimensions(e *store.Edge) pcommon.Map {
@@ -549,13 +587,30 @@ func (p *serviceGraphConnector) collectLatencyMetrics(ilm pmetric.ScopeMetrics) 
 }
 
 func (p *serviceGraphConnector) collectClientLatencyMetrics(ilm pmetric.ScopeMetrics) error {
-	if len(p.reqClientDurationSecondsCount) > 0 {
-		mDuration := ilm.Metrics().AppendEmpty()
-		mDuration.SetName("traces_service_graph_request_client")
-		mDuration.SetUnit(secondsUnit)
-		if legacyLatencyUnitMsFeatureGate.IsEnabled() {
-			mDuration.SetUnit(millisecondsUnit)
+	mDuration := pmetric.NewMetric()
+	mDuration.SetName("traces_service_graph_request_client")
+	mDuration.SetUnit(secondsUnit)
+	if legacyLatencyUnitMsFeatureGate.IsEnabled() {
+		mDuration.SetUnit(millisecondsUnit)
+	}
+
+	if p.reqDurationBounds == nil {
+		mDuration.SetEmptyExponentialHistogram().SetAggregationTemporality(pmetric.AggregationTemporalityCumulative)
+		for key, expHistogram := range p.reqClientDurationExpHistogram {
+			dpDuration := mDuration.ExponentialHistogram().DataPoints().AppendEmpty()
+			dpDuration.SetStartTimestamp(pcommon.NewTimestampFromTime(p.startTime))
+			dimensions, ok := p.dimensionsForSeries(key)
+			if !ok {
+				return fmt.Errorf("failed to find dimensions for key %s", key)
+			}
+
+			dimensions.CopyTo(dpDuration.Attributes())
+			dpDuration.SetCount(expHistogram.Count())
+			dpDuration.SetSum(expHistogram.Sum())
+			pdatautil.ExpoHistToExponentialDataPoint(expHistogram, dpDuration)
 		}
+		mDuration.CopyTo(ilm.Metrics().AppendEmpty())
+	} else if len(p.reqClientDurationSecondsCount) > 0 {
 		// TODO: Support other aggregation temporalities
 		mDuration.SetEmptyHistogram().SetAggregationTemporality(pmetric.AggregationTemporalityCumulative)
 		timestamp := pcommon.NewTimestampFromTime(p.nowWithOffset())
@@ -577,18 +632,38 @@ func (p *serviceGraphConnector) collectClientLatencyMetrics(ilm pmetric.ScopeMet
 
 			dimensions.CopyTo(dpDuration.Attributes())
 		}
+		mDuration.CopyTo(ilm.Metrics().AppendEmpty())
 	}
 	return nil
 }
 
 func (p *serviceGraphConnector) collectServerLatencyMetrics(ilm pmetric.ScopeMetrics, mName string) error {
-	if len(p.reqServerDurationSecondsCount) > 0 {
-		mDuration := ilm.Metrics().AppendEmpty()
-		mDuration.SetName(mName)
-		mDuration.SetUnit(secondsUnit)
-		if legacyLatencyUnitMsFeatureGate.IsEnabled() {
-			mDuration.SetUnit(millisecondsUnit)
+	timestamp := pcommon.NewTimestampFromTime(time.Now())
+	mDuration := pmetric.NewMetric()
+	mDuration.SetName(mName)
+	mDuration.SetUnit(secondsUnit)
+	if legacyLatencyUnitMsFeatureGate.IsEnabled() {
+		mDuration.SetUnit(millisecondsUnit)
+	}
+
+	if p.reqDurationBounds == nil {
+		mDuration.SetEmptyExponentialHistogram().SetAggregationTemporality(pmetric.AggregationTemporalityCumulative)
+		for key, expHistogram := range p.reqServerDurationExpHistogram {
+			dpDuration := mDuration.ExponentialHistogram().DataPoints().AppendEmpty()
+			dpDuration.SetTimestamp(timestamp)
+			dpDuration.SetStartTimestamp(pcommon.NewTimestampFromTime(p.startTime))
+			dimensions, ok := p.dimensionsForSeries(key)
+			if !ok {
+				return fmt.Errorf("failed to find dimensions for key %s", key)
+			}
+
+			dimensions.CopyTo(dpDuration.Attributes())
+			dpDuration.SetCount(expHistogram.Count())
+			dpDuration.SetSum(expHistogram.Sum())
+			pdatautil.ExpoHistToExponentialDataPoint(expHistogram, dpDuration)
 		}
+		mDuration.CopyTo(ilm.Metrics().AppendEmpty())
+	} else if len(p.reqServerDurationSecondsCount) > 0 {
 		// TODO: Support other aggregation temporalities
 		mDuration.SetEmptyHistogram().SetAggregationTemporality(pmetric.AggregationTemporalityCumulative)
 		timestamp := pcommon.NewTimestampFromTime(p.nowWithOffset())
@@ -610,6 +685,7 @@ func (p *serviceGraphConnector) collectServerLatencyMetrics(ilm pmetric.ScopeMet
 
 			dimensions.CopyTo(dpDuration.Attributes())
 		}
+		mDuration.CopyTo(ilm.Metrics().AppendEmpty())
 	}
 	return nil
 }

connector/servicegraphconnector/connector_test.go
@@ -733,6 +733,99 @@ func TestVirtualNodeClientLabels(t *testing.T) {
 	require.NoError(t, err)
 }
 
+func TestExponentialHistogram(t *testing.T) {
+	// Prepare
+	set := componenttest.NewNopTelemetrySettings()
+	set.Logger = zaptest.NewLogger(t)
+
+	cfg := &Config{
+		Dimensions: []string{"some-attribute", "non-existing-attribute"},
+		Store: StoreConfig{
+			MaxItems: 10,
+			TTL:      time.Nanosecond,
+		},
+		ExponentialHistogramMaxSize: 4,
+	}
+	conn, err := newConnector(set, cfg, newMockMetricsExporter())
+	require.NoError(t, err)
+	assert.NoError(t, conn.Start(context.Background(), componenttest.NewNopHost()))
+
+	// Send spans to the connector
+	assert.NoError(t, conn.ConsumeTraces(context.Background(), buildSampleTrace(t, "val")))
+
+	// Force collection
+	if runtime.GOOS == "windows" {
+		// On Windows timing doesn't tick forward quickly for the store data to expire, force a wait before expiring.
+		time.Sleep(time.Second)
+	}
+	conn.store.Expire()
+	md, err := conn.buildMetrics()
+	assert.NoError(t, err)
+
+	assert.Equal(t, 3, md.MetricCount())
+
+	rms := md.ResourceMetrics()
+	assert.Equal(t, 1, rms.Len())
+
+	sms := rms.At(0).ScopeMetrics()
+	assert.Equal(t, 1, sms.Len())
+
+	ms := sms.At(0).Metrics()
+	assert.Equal(t, 3, ms.Len())
+
+	mCount := ms.At(0)
+	verifyCount(t, mCount)
+
+	expectAttributes := pcommon.NewMap()
+	expectAttributes.PutStr("client", "some-service")
+	expectAttributes.PutStr("server", "some-service")
+	expectAttributes.PutStr("connection_type", "")
+	expectAttributes.PutBool("failed", false)
+	expectAttributes.PutStr("client_some-attribute", "val")
+
+	mServerDuration := ms.At(1)
+	assert.Equal(t, "traces_service_graph_request_server", mServerDuration.Name())
+	expectServerDp := pmetric.NewExponentialHistogramDataPoint()
+	expectServerDp.SetCount(1)
+	expectServerDp.SetSum(2)
+	expectServerDp.SetMin(2)
+	expectServerDp.SetMax(2)
+	expectServerDp.SetZeroCount(0)
+	expectServerDp.SetScale(20)
+	expectServerDp.Positive().SetOffset(1048575)
+	expectServerDp.Positive().BucketCounts().FromRaw([]uint64{1})
+	expectAttributes.CopyTo(expectServerDp.Attributes())
+	verifyExpDuration(t, mServerDuration, expectServerDp)
+
+	mClientDuration := ms.At(2)
+	assert.Equal(t, "traces_service_graph_request_client", mClientDuration.Name())
+	expectClientDp := pmetric.NewExponentialHistogramDataPoint()
+	expectClientDp.SetCount(1)
+	expectClientDp.SetSum(1)
+	expectClientDp.SetMin(1)
+	expectClientDp.SetMax(1)
+	expectClientDp.SetZeroCount(0)
+	expectClientDp.SetScale(20)
+	expectClientDp.Positive().SetOffset(-1)
+	expectClientDp.Positive().BucketCounts().FromRaw([]uint64{1})
+	expectAttributes.CopyTo(expectClientDp.Attributes())
+	verifyExpDuration(t, mClientDuration, expectClientDp)
+
+	assert.NoError(t, conn.Shutdown(context.Background()))
+}
+
+func verifyExpDuration(t *testing.T, m pmetric.Metric, expectedDp pmetric.ExponentialHistogramDataPoint) {
+	assert.Equal(t, pmetric.MetricTypeExponentialHistogram, m.Type())
+	dps := m.ExponentialHistogram().DataPoints()
+	assert.Equal(t, 1, dps.Len())
+	dp := dps.At(0)
+
+	// ignore time
+	dp.SetTimestamp(pcommon.Timestamp(0))
+	dp.SetStartTimestamp(pcommon.Timestamp(0))
+	assert.Equal(t, expectedDp, dp)
+}
+
 // ptr returns a pointer to the given value.
 func ptr[T any](value T) *T {
 	return &value

.chloggen/fix-issue-35460.yaml
@@ -1,0 +1,29 @@
+# Use this changelog template to create an entry for release notes.
+
+# One of 'breaking', 'deprecation', 'new_component', 'enhancement', 'bug_fix'
+change_type: enhancement
+
+# The name of the component, or a single word describing the area of concern, (e.g. filelogreceiver)
+component: servicegraphconnector
+
+# A brief description of the change.  Surround your text with quotes ("") if it needs to start with a backtick (`).
+note: Add native histograms support
+
+# Mandatory: One or more tracking issues related to the change. You can use the PR number here if no issue exists.
+issues: [35460]
+
+# (Optional) One or more lines of additional information to render under the primary note.
+# These lines will be padded with 2 spaces and then inserted directly into the document.
+# Use pipe (|) for multiline entries.
+subtext: |
+  Add a new setting `exponential_histogram_max_size` to specify the maximum number of buckets for each positive or negative range in an exponential histogram. 
+  Ensure that either `latency_histogram_buckets` or `exponential_histogram_max_size` is used, but not both.
+
+# If your change doesn't affect end users or the exported elements of any package,
+# you should instead start your pull request title with [chore] or use the "Skip Changelog" label.
+# Optional: The change log or logs in which this entry should be included.
+# e.g. '[user]' or '[user, api]'
+# Include 'user' if the change is relevant to end users.
+# Include 'api' if there is a change to a library API.
+# Default: '[user]'
+change_logs: [user]

connector/exceptionsconnector/go.mod
@@ -38,6 +38,7 @@ require (
 	github.com/knadh/koanf/maps v0.1.2 // indirect
 	github.com/knadh/koanf/providers/confmap v1.0.0 // indirect
 	github.com/knadh/koanf/v2 v2.2.2 // indirect
+	github.com/lightstep/go-expohisto v1.0.0 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect

connector/exceptionsconnector/go.sum
@@ -37,6 +37,8 @@ github.com/kr/pretty v0.3.1 h1:flRD4NNwYAUpkphVc1HcthR4KEIFJ65n8Mw5qdRn3LE=
 github.com/kr/pretty v0.3.1/go.mod h1:hoEshYVHaxMs3cyo3Yncou5ZscifuDolrwPKZanG3xk=
 github.com/kr/text v0.2.0 h1:5Nx0Ya0ZqY2ygV366QzturHI13Jq95ApcVaJBhpS+AY=
 github.com/kr/text v0.2.0/go.mod h1:eLer722TekiGuMkidMxC/pM04lWEeraHUUmBw8l2grE=
+github.com/lightstep/go-expohisto v1.0.0 h1:UPtTS1rGdtehbbAF7o/dhkWLTDI73UifG8LbfQI7cA4=
+github.com/lightstep/go-expohisto v1.0.0/go.mod h1:xDXD0++Mu2FOaItXtdDfksfgxfV0z1TMPa+e/EUd0cs=
 github.com/mitchellh/copystructure v1.2.0 h1:vpKXTN4ewci03Vljg/q9QvCGUDttBOGBIa15WveJJGw=
 github.com/mitchellh/copystructure v1.2.0/go.mod h1:qLl+cE2AmVv+CoeAwDPye/v+N2HKCj9FbZEVFJRxO9s=
 github.com/mitchellh/reflectwalk v1.0.2 h1:G2LzWKi524PWgd3mLHV8Y5k7s6XUvT0Gef6zxSIeXaQ=

connector/servicegraphconnector/config.go
@@ -4,6 +4,7 @@
 package servicegraphconnector // import "github.com/open-telemetry/opentelemetry-collector-contrib/connector/servicegraphconnector"
 
 import (
+	"errors"
 	"time"
 )
 
@@ -16,7 +17,11 @@ type Config struct {
 
 	// LatencyHistogramBuckets is the list of durations representing latency histogram buckets.
 	// See defaultLatencyHistogramBucketsMs in processor.go for the default value.
+	// make sure use either `LatencyHistogramBuckets` or `ExponentialHistogramMaxSize`
 	LatencyHistogramBuckets []time.Duration `mapstructure:"latency_histogram_buckets"`
+
+	// ExponentialHistogramMaxSize is the setting of exponential histogram
+	ExponentialHistogramMaxSize int32 `mapstructure:"exponential_histogram_max_size"`
 
 	// Dimensions defines the list of additional dimensions on top of the provided:
 	// - client
@@ -66,4 +71,17 @@ type StoreConfig struct {
 
 	// prevent unkeyed literal initialization
 	_ struct{}
+}
+
+// Validate checks if the connector configuration is valid.
+func (c *Config) Validate() error {
+	if c.LatencyHistogramBuckets == nil && c.ExponentialHistogramMaxSize < 0 {
+		return errors.New("`exponential_histogram_max_size` can not be negative")
+	}
+
+	if c.LatencyHistogramBuckets != nil && c.ExponentialHistogramMaxSize > 0 {
+		return errors.New("use either `latency_histogram_buckets` or `exponential_histogram_max_size`")
+	}
+
+	return nil
 }

connector/servicegraphconnector/go.mod
@@ -3,6 +3,7 @@ module github.com/open-telemetry/opentelemetry-collector-contrib/connector/servi
 go 1.23.0
 
 require (
+	github.com/lightstep/go-expohisto v1.0.0
 	github.com/open-telemetry/opentelemetry-collector-contrib/internal/pdatautil v0.131.0
 	github.com/open-telemetry/opentelemetry-collector-contrib/pkg/golden v0.131.0
 	github.com/open-telemetry/opentelemetry-collector-contrib/pkg/pdatatest v0.131.0

connector/servicegraphconnector/go.sum
@@ -65,6 +65,8 @@ github.com/kr/text v0.2.0 h1:5Nx0Ya0ZqY2ygV366QzturHI13Jq95ApcVaJBhpS+AY=
 github.com/kr/text v0.2.0/go.mod h1:eLer722TekiGuMkidMxC/pM04lWEeraHUUmBw8l2grE=
 github.com/kylelemons/godebug v1.1.0 h1:RPNrshWIDI6G2gRW9EHilWtl7Z6Sb1BR0xunSBf0SNc=
 github.com/kylelemons/godebug v1.1.0/go.mod h1:9/0rRGxNHcop5bhtWyNeEfOS8JIWk580+fNqagV/RAw=
+github.com/lightstep/go-expohisto v1.0.0 h1:UPtTS1rGdtehbbAF7o/dhkWLTDI73UifG8LbfQI7cA4=
+github.com/lightstep/go-expohisto v1.0.0/go.mod h1:xDXD0++Mu2FOaItXtdDfksfgxfV0z1TMPa+e/EUd0cs=
 github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 h1:6E+4a0GO5zZEnZ81pIr0yLvtUWk2if982qA3F3QD6H4=
 github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0/go.mod h1:zJYVVT2jmtg6P3p1VtQj7WsuWi/y4VnjVBn7F8KPB3I=
 github.com/mitchellh/copystructure v1.2.0 h1:vpKXTN4ewci03Vljg/q9QvCGUDttBOGBIa15WveJJGw=

commit a51b4af92d09462eebb7e6f664c4cbe95731ecc2

[connector/service_graph] Fix client exponential histogram emitted with zero timestamp (#48049)
#### Description

I've been trying out native histograms, and hit an issue with the
service graph's client metric. The setup was otelcol-contrib sending to
Grafana Cloud.

My config was:

```yaml
  service_graph:
    exponential_histogram_max_size: 160
    virtual_node_peer_attributes:
      - peer.service
      - db.system.name
      - server.address
    virtual_node_extra_label: true
```

The `collectClientLatencyMetrics` method does not call `SetTimestamp` on
the exponential histogram data point, leaving Timestamp at zero.


https://github.com/open-telemetry/opentelemetry-collector-contrib/blob/83112925aa74330db39368ca78cd714ca3fda515/connector/servicegraphconnector/connector.go#L587-L610

Backends like Mimir reject the resulting
`traces_service_graph_request_client_` series with
err-mimir-too-far-in-past, timestamp: 0.

Logs of the resulting error:

```
1777537131460	2026-04-30T08:18:51.460Z	Exporting failed. Dropping data. code_file_path=go.opentelemetry.io/collector/exporter/exporterhelper@v0.151.0/internal/queue_sender.go code_function_name=go.opentelemetry.io/collector/exporter/exporterhelper/internal.NewQueueSender.func1 code_line_number=50 code_stacktrace=go.opentelemetry.io/collector/exporter/exporterhelper/internal.NewQueueSender.func1
	go.opentelemetry.io/collector/exporter/exporterhelper@v0.151.0/internal/queue_sender.go:50
go.opentelemetry.io/collector/exporter/exporterhelper/internal/queuebatch.(*disabledBatcher[...]).Consume
	go.opentelemetry.io/collector/exporter/exporterhelper@v0.151.0/internal/queuebatch/disabled_batcher.go:23
go.opentelemetry.io/collector/exporter/exporterhelper/internal/queue.(*asyncQueue[...]).Start.func1
	go.opentelemetry.io/collector/exporter/exporterhelper@v0.151.0/internal/queue/async_queue.go:47
sync.(*WaitGroup).Go.func1
	sync/waitgroup.go:258 dropped_items=72 error=not retryable error: Permanent error: rpc error: code = InvalidArgument desc = error exporting items, request to https://otlp-gateway-prod-eu-west-2.grafana.net/otlp/v1/metrics responded with HTTP Status Code 400, Message=received a sample whose timestamp is too far in the past, timestamp: 0 series: 'traces_service_graph_request_client_seconds' (err-mimir-too-far-in-past). To adjust the related per-tenant limit, configure -validation.past-grace-period, or contact your service administrator., Details=[] otelcol_component_id=otlp_http/grafanacloud otelcol_component_kind=exporter otelcol_signal=metrics scope_name=go.opentelemetry.io/collector/service
```

This change sets the timestamp using `nowWithOffset`, to match the
client explicit-bucket branch.

As a comparison, here's the server metric:


https://github.com/open-telemetry/opentelemetry-collector-contrib/blob/83112925aa74330db39368ca78cd714ca3fda515/connector/servicegraphconnector/connector.go#L638-L663

(An aside, the server metric method uses a mix of `time.Now()` and
`nowWithOffset()`. Maybe a bug on the server metric side?)

#### Link to tracking issue

Bug was introduced in
https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/39951.

.chloggen/fix-servicegraph-client-exp-histogram-timestamp.yaml
@@ -1,0 +1,14 @@
+change_type: bug_fix
+
+component: connector/service_graph
+
+note: Fix client-side exponential histograms being emitted with a zero timestamp, causing backends like Grafana Cloud Mimir to reject them.
+
+issues: [48049]
+
+subtext: |
+  collectClientLatencyMetrics omitted SetTimestamp on the exponential-histogram data point, so the
+  zero-value timestamp leaked through. The server-side function and the client-side explicit-bucket
+  branch were already correct.
+
+change_logs: [user]

connector/servicegraphconnector/connector.go
@@ -585,6 +585,7 @@ func (p *serviceGraphConnector) collectLatencyMetrics(ilm pmetric.ScopeMetrics) 
 }
 
 func (p *serviceGraphConnector) collectClientLatencyMetrics(ilm pmetric.ScopeMetrics) error {
+	timestamp := pcommon.NewTimestampFromTime(p.nowWithOffset())
 	mDuration := pmetric.NewMetric()
 	mDuration.SetName("traces_service_graph_request_client")
 	mDuration.SetUnit(secondsUnit)
@@ -596,6 +597,7 @@ func (p *serviceGraphConnector) collectClientLatencyMetrics(ilm pmetric.ScopeMet
 		mDuration.SetEmptyExponentialHistogram().SetAggregationTemporality(pmetric.AggregationTemporalityCumulative)
 		for key, expHistogram := range p.reqClientDurationExpHistogram {
 			dpDuration := mDuration.ExponentialHistogram().DataPoints().AppendEmpty()
+			dpDuration.SetTimestamp(timestamp)
 			dpDuration.SetStartTimestamp(pcommon.NewTimestampFromTime(p.startTime))
 			dimensions, ok := p.dimensionsForSeries(key)
 			if !ok {

connector/servicegraphconnector/connector_test.go
@@ -852,6 +852,9 @@ func verifyExpDuration(t *testing.T, m pmetric.Metric, expectedDp pmetric.Expone
 	assert.Equal(t, 1, dps.Len())
 	dp := dps.At(0)
 
+	assert.NotZero(t, dp.Timestamp(), "timestamp must be set")
+	assert.NotZero(t, dp.StartTimestamp(), "start timestamp must be set")
+
 	// ignore time
 	dp.SetTimestamp(pcommon.Timestamp(0))
 	dp.SetStartTimestamp(pcommon.Timestamp(0))