967b16069d148a6c8157aebfd71faa490183e1c9
Christos Markou <chrismarkou92@gmail.com>
2026-05-21 10:13:05 +0300

    [k8sattributes] fix container.image.tags silent overwrite in v1 mode (#48455)
    <!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
    Ex. Adding a feature - Explain what this achieves.-->
    #### Description
    Preparing for
    https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/45869,
    found this small inconsistency. Fixing this now along with small docs'
    improvements and unit-test addition.
    
    <!-- Issue number (e.g. #1234) or full URL to issue, if applicable. -->
    #### Link to tracking issue
    Related to
    https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/44483
    
    <!--Describe what testing was performed and which tests were added.-->
    #### Testing
    Tuned
    
    <!--Describe the documentation added.-->
    #### Documentation
    Tuned
    
    <!--Please delete paragraphs that you did not use before submitting.-->
    
    Signed-off-by: ChrsMark <chrismarkou92@gmail.com>

--- processor/k8sattributesprocessor/processor.go
+++ processor/k8sattributesprocessor/processor.go
@@ -375,9 +375,11 @@ func (kp *kubernetesprocessor) addContainerAttributes(attrs pcommon.Map, pod *ku
 		setResourceAttribute(attrs, containerImageTag, containerSpec.ImageTag)
 	}
 	if enableStable && len(containerSpec.ImageTags) > 0 {
-		sliceVal := attrs.PutEmptySlice(string(conventions.ContainerImageTagsKey))
-		for _, tag := range containerSpec.ImageTags {
-			sliceVal.AppendEmpty().SetStr(tag)
+		if _, found := attrs.Get(string(conventions.ContainerImageTagsKey)); !found {
+			sliceVal := attrs.PutEmptySlice(string(conventions.ContainerImageTagsKey))
+			for _, tag := range containerSpec.ImageTags {
+				sliceVal.AppendEmpty().SetStr(tag)
+			}
 		}
 	}
 	if containerSpec.ServiceInstanceID != "" {
