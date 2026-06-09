8d557b0f21641834001ac22c1fec47f54f3c538f
Vibhanshu Singh <singhvibhanshu@hotmail.com>
2026-06-09 06:29:55 +0530

    [pkg/translator/zipkin] migrate semantic conventions from v1.25.0 to v1.40.0 (#48904)
    Fixes #45089
    
    `http.response.status_code` is the replacement attribute for
    `http.status_code`
    (Here is the reference link:
    https://opentelemetry.io/docs/specs/semconv/registry/attributes/http/#http-status-code)
    
    `otel.scope.name` is the replacement attribute for `otel.library.name`
    (Here is the reference link:
    https://opentelemetry.io/docs/specs/semconv/registry/attributes/otel/#otel-library-name)
    
    `otel.scope.version` is the replacement attribute `otel.library.version`
    (Here is the referenec link:
    https://opentelemetry.io/docs/specs/semconv/registry/attributes/otel/#otel-library-version)
    
    Signed-off-by: singhvibhanshu <singhvibhanshu@hotmail.com>

--- pkg/translator/zipkin/internal/metadata/generated_feature_gates.go
+++ pkg/translator/zipkin/internal/metadata/generated_feature_gates.go
@@ -6,6 +6,14 @@ import (
 	"go.opentelemetry.io/collector/featuregate"
 )
 
+var PkgTranslatorZipkinDontEmitV0HTTPConventionsFeatureGate = featuregate.GlobalRegistry().MustRegister(
+	"pkg.translator.zipkin.DontEmitV0HttpConventions",
+	featuregate.StageAlpha,
+	featuregate.WithRegisterDescription("When enabled, the Zipkin translator no longer emits the deprecated http.status_code attribute (semconv v1.25.0). Requires pkg.translator.zipkin.EmitV1HttpConventions to also be enabled."),
+	featuregate.WithRegisterReferenceURL("https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/45089"),
+	featuregate.WithRegisterFromVersion("v0.154.0"),
+)
+
 var PkgTranslatorZipkinDontEmitV0NetworkConventionsFeatureGate = featuregate.GlobalRegistry().MustRegister(
 	"pkg.translator.zipkin.DontEmitV0NetworkConventions",
 	featuregate.StageBeta,
@@ -14,10 +22,34 @@ var PkgTranslatorZipkinDontEmitV0NetworkConventionsFeatureGate = featuregate.Glo
 	featuregate.WithRegisterFromVersion("v0.147.0"),
 )
 
+var PkgTranslatorZipkinDontEmitV0ScopeConventionsFeatureGate = featuregate.GlobalRegistry().MustRegister(
+	"pkg.translator.zipkin.DontEmitV0ScopeConventions",
+	featuregate.StageAlpha,
+	featuregate.WithRegisterDescription("When enabled, the Zipkin translator no longer emits the deprecated otel.library.name and otel.library.version attributes (semconv v1.25.0). Requires pkg.translator.zipkin.EmitV1ScopeConventions to also be enabled."),
+	featuregate.WithRegisterReferenceURL("https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/45089"),
+	featuregate.WithRegisterFromVersion("v0.154.0"),
+)
+
+var PkgTranslatorZipkinEmitV1HTTPConventionsFeatureGate = featuregate.GlobalRegistry().MustRegister(
+	"pkg.translator.zipkin.EmitV1HttpConventions",
+	featuregate.StageAlpha,
+	featuregate.WithRegisterDescription("When enabled, the Zipkin translator emits http.response.status_code (semconv v1.40.0) instead of the deprecated http.status_code (semconv v1.25.0)."),
+	featuregate.WithRegisterReferenceURL("https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/45089"),
+	featuregate.WithRegisterFromVersion("v0.154.0"),
+)
+
 var PkgTranslatorZipkinEmitV1NetworkConventionsFeatureGate = featuregate.GlobalRegistry().MustRegister(
 	"pkg.translator.zipkin.EmitV1NetworkConventions",
 	featuregate.StageBeta,
 	featuregate.WithRegisterDescription("When enabled, the Zipkin translator emits network.local.address and network.peer.address and service.peer.name (semconv v1.40.0) instead of the deprecated net.host.ip, net.peer.ip, and peer.service (semconv v1.12.0)."),
 	featuregate.WithRegisterReferenceURL("https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/45041"),
 	featuregate.WithRegisterFromVersion("v0.147.0"),
+)
+
+var PkgTranslatorZipkinEmitV1ScopeConventionsFeatureGate = featuregate.GlobalRegistry().MustRegister(
+	"pkg.translator.zipkin.EmitV1ScopeConventions",
+	featuregate.StageAlpha,
+	featuregate.WithRegisterDescription("When enabled, the Zipkin translator emits otel.scope.name and otel.scope.version (semconv v1.40.0) instead of the deprecated otel.library.name and otel.library.version (semconv v1.25.0)."),
+	featuregate.WithRegisterReferenceURL("https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/45089"),
+	featuregate.WithRegisterFromVersion("v0.154.0"),
 )
