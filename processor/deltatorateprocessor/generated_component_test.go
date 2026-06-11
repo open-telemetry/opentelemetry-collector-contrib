312de9f6ba4fd8cddfebaec24892c7f47ba03157
Ogulcan Aydogan <ogulcanaydogan@hotmail.com>
2026-06-11 09:19:07 +0100

    [processor/deltatorate] rename type to delta_to_rate
    Signed-off-by: Ogulcan Aydogan <ogulcanaydogan@hotmail.com>

--- processor/deltatorateprocessor/generated_component_test.go
+++ processor/deltatorateprocessor/generated_component_test.go
@@ -20,7 +20,7 @@ import (
 	"go.opentelemetry.io/collector/processor/processortest"
 )
 
-var typ = component.MustNewType("deltatorate")
+var typ = component.MustNewType("delta_to_rate")
 
 func TestComponentFactoryType(t *testing.T) {
 	require.Equal(t, typ, NewFactory().Type())
