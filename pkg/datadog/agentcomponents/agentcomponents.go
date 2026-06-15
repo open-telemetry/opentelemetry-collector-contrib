35ce5dd959b61ebfe5af7517cf1a536bd09ee2ae
maxime mouial <hush-hush@users.noreply.github.com>
2026-06-15 18:03:37 +0200

    Stop using deprecated config module from the Datadog Agent (#49071)
    #### Description
    
    This PR use the dedicated `create` package to handle the Datadog config.
    `viperconfig` will soon be removed from the Agent repo.
    
    #### Testing
    
    There is no change in behavior introduced here. Running the CI will be
    enough.
    
    ---------
    
    Co-authored-by: Yang Song <songy23@users.noreply.github.com>

--- pkg/datadog/agentcomponents/agentcomponents.go
+++ pkg/datadog/agentcomponents/agentcomponents.go
@@ -14,10 +14,10 @@ import (
 	corelog "github.com/DataDog/datadog-agent/comp/core/log/def"
 	"github.com/DataDog/datadog-agent/comp/forwarder/defaultforwarder"
 	logsconfig "github.com/DataDog/datadog-agent/comp/logs/agent/config"
+	pkgconfigcreate "github.com/DataDog/datadog-agent/pkg/config/create"
 	pkgconfigmodel "github.com/DataDog/datadog-agent/pkg/config/model"
 	pkgconfigsetup "github.com/DataDog/datadog-agent/pkg/config/setup"
 	pkgconfigutils "github.com/DataDog/datadog-agent/pkg/config/utils"
-	"github.com/DataDog/datadog-agent/pkg/config/viperconfig"
 	"github.com/DataDog/datadog-agent/pkg/metrics"
 	"github.com/DataDog/datadog-agent/pkg/serializer"
 	zlib "github.com/DataDog/datadog-agent/pkg/util/compression/impl-zlib"
@@ -142,7 +142,7 @@ func NewSerializerComponent(cfg coreconfig.Component, logger corelog.Component, 
 // This function uses the options pattern to allow different modules to configure
 // the component with their specific needs.
 func NewConfigComponent(options ...ConfigOption) coreconfig.Component {
-	pkgconfig := viperconfig.NewConfig("DD", "DD", strings.NewReplacer(".", "_"))
+	pkgconfig := pkgconfigcreate.NewConfig("DD", "")
 
 	// Apply all configuration options
 	for _, opt := range options {
