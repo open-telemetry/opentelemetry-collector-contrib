660e0ae01c8e7552f4a6dc141de6dceeac4eccc5
Pratik Mahalle <mahallepratik683@gmail.com>
2026-06-30 07:25:05 +0530

    ci: ci was failing due to regenerate command

--- processor/tailsamplingprocessor/internal/metadata/generated_feature_gates.go
+++ processor/tailsamplingprocessor/internal/metadata/generated_feature_gates.go
@@ -11,7 +11,7 @@ var ProcessorTailsamplingprocessorDefaultOTTLErrorModeIgnoreFeatureGate = featur
 	featuregate.StageAlpha,
 	featuregate.WithRegisterDescription("Changes the default error_mode of the ottl_condition policy from propagate to ignore"),
 	featuregate.WithRegisterReferenceURL("https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/48420"),
-	featuregate.WithRegisterFromVersion("v0.153.0"),
+	featuregate.WithRegisterFromVersion("v0.155.0"),
 )
 
 var ProcessorTailsamplingprocessorMetricstatcountbytessampledFeatureGate = featuregate.GlobalRegistry().MustRegister(
