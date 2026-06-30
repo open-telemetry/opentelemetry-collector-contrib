d4294b2c028bf7eaf0678afad09cea70a89f7274
sreenathv <sreenathv@splunk.com>
2026-06-30 10:42:40 +0100

    lint fix

--- receiver/oracledbreceiver/scraper_test.go
+++ receiver/oracledbreceiver/scraper_test.go
@@ -120,7 +120,7 @@ var cacheValue = map[string]int64{
 	"PROCEDURE_EXECUTIONS":    200413,
 }
 
-const SqlPlanTable = "V$SQL_PLAN_STATISTICS_ALL"
+const SQLPlanTable = "V$SQL_PLAN_STATISTICS_ALL"
 
 func TestScraper_Scrape(t *testing.T) {
 	tests := []struct {
@@ -411,7 +411,7 @@ func TestScraper_ScrapeTopNLogs(t *testing.T) {
 		{
 			name: "valid collection",
 			dbclientFn: func(_ *sql.DB, s string, _ *zap.Logger) dbClient {
-				if strings.Contains(s, SqlPlanTable) {
+				if strings.Contains(s, SQLPlanTable) {
 					metricRowFile := readFile("oracleQueryPlanData.txt")
 					unmarshalErr := json.Unmarshal(metricRowFile, &logRowData)
 					if unmarshalErr == nil {
@@ -1006,7 +1006,7 @@ func TestTopNLogsDiscardedWhenExecutionCountUnchanged(t *testing.T) {
 			return nil, nil
 		},
 		clientProviderFunc: func(_ *sql.DB, s string, _ *zap.Logger) dbClient {
-			if strings.Contains(s, SqlPlanTable) {
+			if strings.Contains(s, SQLPlanTable) {
 				metricRowFile := readFile("oracleQueryPlanData.txt")
 				_ = json.Unmarshal(metricRowFile, &logRowData)
 				return &fakeDbClient{Responses: [][]metricRow{logRowData}}
@@ -1097,7 +1097,7 @@ func TestTopNLogsProcedureNameEmpty(t *testing.T) {
 			return nil, nil
 		},
 		clientProviderFunc: func(_ *sql.DB, s string, _ *zap.Logger) dbClient {
-			if strings.Contains(s, SqlPlanTable) {
+			if strings.Contains(s, SQLPlanTable) {
 				return &fakeDbClient{Responses: [][]metricRow{planData}}
 			}
 			return &fakeDbClient{Responses: [][]metricRow{metricsData}}
@@ -1147,7 +1147,7 @@ func TestScrapesTopNLogsOnlyWhenIntervalHasElapsed(t *testing.T) {
 		{
 			name: "valid collection",
 			dbclientFn: func(_ *sql.DB, s string, _ *zap.Logger) dbClient {
-				if strings.Contains(s, SqlPlanTable) {
+				if strings.Contains(s, SQLPlanTable) {
 					metricRowFile := readFile("oracleQueryPlanData.txt")
 					unmarshalErr := json.Unmarshal(metricRowFile, &logRowData)
 					if unmarshalErr == nil {
@@ -1281,7 +1281,7 @@ func TestObfuscateCacheHitsHandlesTruncatedSQL(t *testing.T) {
 			return nil, nil
 		},
 		clientProviderFunc: func(_ *sql.DB, s string, _ *zap.Logger) dbClient {
-			if strings.Contains(s, SqlPlanTable) {
+			if strings.Contains(s, SQLPlanTable) {
 				return &fakeDbClient{Responses: [][]metricRow{{}}}
 			}
 			return &fakeDbClient{Responses: [][]metricRow{metricsData}}
