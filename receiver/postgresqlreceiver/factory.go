16fe41a91ba7083fb1ec7ed7d624013b7ee47026
Alex Boten <223565+codeboten@users.noreply.github.com>
2026-07-02 15:51:36 -0700

    remove unused func, clean up linting
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>

--- receiver/postgresqlreceiver/factory.go
+++ receiver/postgresqlreceiver/factory.go
@@ -88,7 +88,7 @@ func createMetricsReceiver(
 ) (receiver.Metrics, error) {
 	cfg := rConf.(*Config)
 
-	var clientFactory = newPoolClientFactory(cfg)
+	clientFactory := newPoolClientFactory(cfg)
 
 	ns := newPostgreSQLScraper(params, cfg, clientFactory, newCache(1), newTTLCache[string](1, time.Second))
 	s, err := scraper.NewMetrics(ns.scrape, scraper.WithShutdown(ns.shutdown))
@@ -111,7 +111,7 @@ func createLogsReceiver(
 ) (receiver.Logs, error) {
 	cfg := receiverCfg.(*Config)
 
-	var clientFactory = newPoolClientFactory(cfg)
+	clientFactory := newPoolClientFactory(cfg)
 
 	opts := make([]scraperhelper.ControllerOption, 0)
 
