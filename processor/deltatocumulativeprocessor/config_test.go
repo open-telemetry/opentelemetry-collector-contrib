900b5652066ab413f059266ed5051e457f60718e
Ansen Garvin <agarvin@newrelic.com>
2026-07-13 12:54:21 -0700

    ci: add test to load deprecated config

--- processor/deltatocumulativeprocessor/config_test.go
+++ processor/deltatocumulativeprocessor/config_test.go
@@ -65,3 +65,51 @@ func TestLoadConfig(t *testing.T) {
 		})
 	}
 }
+
+func TestLoadDeprecatedConfig(t *testing.T) {
+	t.Parallel()
+
+	cm, err := confmaptest.LoadConf(filepath.Join("testdata", "config-deprecated.yaml"))
+	require.NoError(t, err)
+
+	tests := []struct {
+		id       component.ID
+		expected component.Config
+	}{
+		{
+			id: component.NewIDWithName(metadata.DeprecatedType, "all"),
+			expected: &Config{
+				MaxStale:   1 * time.Minute,
+				MaxStreams: 10,
+			},
+		},
+		{
+			id: component.NewIDWithName(metadata.DeprecatedType, "set-valid-max_stale"),
+			expected: &Config{
+				MaxStale:   2 * time.Minute,
+				MaxStreams: math.MaxInt,
+			},
+		},
+		{
+			id: component.NewIDWithName(metadata.DeprecatedType, "set-valid-max_streams"),
+			expected: &Config{
+				MaxStale:   5 * time.Minute,
+				MaxStreams: 20,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.id.String(), func(t *testing.T) {
+			factory := NewFactory()
+			cfg := factory.CreateDefaultConfig()
+
+			sub, err := cm.Sub(tt.id.String())
+			require.NoError(t, err)
+			require.NoError(t, sub.Unmarshal(cfg))
+
+			assert.NoError(t, xconfmap.Validate(cfg))
+			assert.Equal(t, tt.expected, cfg)
+		})
+	}
+}
