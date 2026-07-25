bf1833a65724854cf3c9334234291c3189112582
harshitt13 <find.harshitkushwaha@gmail.com>
2026-07-25 17:03:17 +0530

    applied codex suggestion
    Signed-off-by: harshitt13 <find.harshitkushwaha@gmail.com>

--- receiver/activedirectorydsreceiver/counters.go
+++ receiver/activedirectorydsreceiver/counters.go
@@ -63,9 +63,6 @@ func (w *watchers) Scrape(name string) (float64, error) {
 
 	cv, err := v.ScrapeData()
 	if err != nil {
-		if winperfcounters.IsIgnorableError(err) {
-			return 0, nil
-		}
 		return 0, err
 	}
 
