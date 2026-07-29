76fd296ca5e3301adf1b9ffd5d7afd01dfa52081
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2026-07-29 15:45:13 +0300

    feat: init setup of sd_notify extension
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

---
+++ extension/sdnotifyextension/sd_notify.go
@@ -0,0 +1,57 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package sdnotifyextension // import "github.com/open-telemetry/opentelemetry-collector-contrib/extension/sdnotifyextension"
+
+import (
+	"context"
+	"os"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/extension"
+	"go.opentelemetry.io/collector/extension/extensioncapabilities"
+	"go.uber.org/zap"
+)
+
+type sdnotify struct {
+	cfg    *Config
+	logger *zap.Logger
+	host   component.Host
+
+	ctx    context.Context
+	cancel context.CancelFunc
+	sigCh  chan os.Signal
+}
+
+// Extension is the union of capability interfaces sdnotify implements.
+type Extension interface {
+	extension.Extension
+	extensioncapabilities.PipelineWatcher
+}
+
+var _ Extension = (*sdnotify)(nil)
+
+func newSDNotify(cfg *Config, logger *zap.Logger) *sdnotify {
+	return &sdnotify{
+		cfg:    cfg,
+		logger: logger,
+		sigCh:  make(chan os.Signal, 1),
+	}
+}
+
+func (s *sdnotify) Start(startCtx context.Context, host component.Host) error {
+	s.host = host
+	return nil
+}
+
+func (s *sdnotify) Shutdown(_ context.Context) error {
+	return nil
+}
+
+func (s *sdnotify) Ready() error {
+	return nil
+}
+
+func (s *sdnotify) NotReady() error {
+	return nil
+}
