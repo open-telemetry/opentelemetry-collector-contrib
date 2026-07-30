36c9eef971fee7e1d3998aded6be51ca837456e0
Yang Song <yang.song@datadoghq.com>
2026-07-30 08:40:28 -0400

    [chore] make generate

--- processor/deltatocumulativeprocessor/config_test.go
+++ processor/deltatocumulativeprocessor/config_test.go
@@ -14,6 +14,7 @@ import (
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/confmap"
 	"go.opentelemetry.io/collector/confmap/confmaptest"
+	"go.opentelemetry.io/collector/confmap/xconfmap"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/deltatocumulativeprocessor/internal/metadata"
 )
