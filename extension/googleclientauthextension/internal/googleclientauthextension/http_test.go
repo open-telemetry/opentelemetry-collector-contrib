7dac6aedff01c2964803e04a3fbd9b8a68a2fd0e
David Ashpole <dashpole@google.com>
2026-08-12 15:32:13 +0000

    Address review comment: execute RoundTrip in TestRoundTripperWithProxyAuth

--- extension/googleclientauthextension/internal/googleclientauthextension/http_test.go
+++ extension/googleclientauthextension/internal/googleclientauthextension/http_test.go
@@ -166,7 +166,24 @@ func (fn roundTripperFunc) RoundTrip(r *http.Request) (*http.Response, error) {
 }
 
 func TestRoundTripperWithProxyAuth(t *testing.T) {
-	t.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "testdata/fake_creds.json")
+	fakeToken := oauth2.Token{
+		AccessToken:  "accessToken",
+		TokenType:    "tokenType",
+		RefreshToken: "refreshToken",
+		ExpiresIn:    1,
+	}
+	b, err := json.Marshal(fakeToken)
+	assert.NoError(t, err)
+	tokenString := string(b)
+	srvProvidingTokens := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = fmt.Fprint(w, tokenString)
+	}))
+	defer srvProvidingTokens.Close()
+	t.Setenv("GCE_METADATA_HOST", srvProvidingTokens.Listener.Addr().String())
+	t.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "")
+	t.Setenv("HOME", t.TempDir())
+
 	ca := clientAuthenticator{
 		config: &Config{
 			Project:      "my-project",
@@ -175,14 +192,24 @@ func TestRoundTripperWithProxyAuth(t *testing.T) {
 			TokenHeader:  proxyAuthorizationHeader,
 		},
 	}
-	err := ca.Start(t.Context(), nil)
+	err = ca.Start(t.Context(), nil)
 	assert.NoError(t, err)
 
-	rt, err := ca.RoundTripper(roundTripperFunc(func(_ *http.Request) (*http.Response, error) {
-		return nil, nil
+	rt, err := ca.RoundTripper(roundTripperFunc(func(r *http.Request) (*http.Response, error) {
+		assert.Equal(t, "other-project", r.Header.Get("X-Goog-User-Project"))
+		assert.Equal(t, "my-project", r.Header.Get("X-Goog-Project-ID"))
+		assert.Equal(t, "tokenType accessToken", r.Header.Get("Proxy-Authorization"))
+		assert.Empty(t, r.Header.Get("Authorization"))
+		assert.Equal(t, "bar", r.Header.Get("foo"))
+		return &http.Response{}, nil
 	}))
 	assert.NoError(t, err)
 	assert.IsType(t, &proxyAuthTransport{}, rt)
+
+	header := make(http.Header)
+	header.Set("foo", "bar")
+	_, err = rt.RoundTrip(&http.Request{Header: header})
+	assert.NoError(t, err)
 }
 
 func TestProxyAuthTransportRoundTrip(t *testing.T) {
