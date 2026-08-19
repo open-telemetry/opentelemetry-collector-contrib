6634a869966bb867db77565771915ab33db65e25
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2026-08-19 18:14:57 +0300

    fix: add build linux directive for tests, the extension should be used only on Linux
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- extension/sdnotifyextension/integration_test.go
+++ extension/sdnotifyextension/integration_test.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-//go:build integration
+//go:build linux && integration
 
 package sdnotifyextension
 
--- extension/sdnotifyextension/sd_notify_test.go
+++ extension/sdnotifyextension/sd_notify_test.go
@@ -1,6 +1,8 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+//go:build linux && integration
+
 package sdnotifyextension
 
 import (
