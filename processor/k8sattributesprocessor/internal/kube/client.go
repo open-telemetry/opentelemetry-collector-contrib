b31c56b28c7bfbb66d9b90ab9f61c08b0e3894f4
odubajDT <ondrej.dubaj@dynatrace.com>
2026-08-20 14:39:03 +0200

    [processor/k8s_attributes] support extracting labels and annotations from ReplicaSets
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- processor/k8sattributesprocessor/internal/kube/client.go
+++ processor/k8sattributesprocessor/internal/kube/client.go
@@ -235,7 +235,8 @@ func New(
 	//      resource, so we must associate Pods with Deployments through ReplicaSets first.
 	needReplicaSetInformer := (c.Rules.DeploymentName && !c.Rules.DeploymentNameFromReplicaSet) ||
 		c.Rules.DeploymentUID ||
-		c.extractDeploymentLabelsAnnotations()
+		c.extractDeploymentLabelsAnnotations() ||
+		c.extractReplicaSetLabelsAnnotations()
 
 	if needReplicaSetInformer {
 		if informersFactory.newReplicaSetInformer == nil {
@@ -1424,6 +1425,20 @@ func (c *WatchClient) extractDaemonSetAttributes(d *meta_v1.PartialObjectMetadat
 	return tags
 }
 
+func (c *WatchClient) extractReplicaSetAttributes(d *meta_v1.PartialObjectMetadata) map[string]string {
+	tags := map[string]string{}
+
+	for _, r := range c.Rules.Labels {
+		r.extractFromReplicaSetMetadata(d.Labels, tags, conventions.K8SReplicaSetLabel)
+	}
+
+	for _, r := range c.Rules.Annotations {
+		r.extractFromReplicaSetMetadata(d.Annotations, tags, conventions.K8SReplicaSetAnnotation)
+	}
+
+	return tags
+}
+
 func (c *WatchClient) extractJobAttributes(d *meta_v1.PartialObjectMetadata) map[string]string {
 	tags := map[string]string{}
 
@@ -1454,6 +1469,7 @@ func (c *WatchClient) podFromAPI(pod *api_v1.Pod) *Pod {
 	}
 
 	if replicaset, ok := c.GetReplicaSet(getPodReplicaSetUID(pod)); ok {
+		newPod.ReplicaSetUID = replicaset.UID
 		if replicaset.Deployment.UID != "" {
 			newPod.DeploymentUID = replicaset.Deployment.UID
 		}
@@ -1873,6 +1889,22 @@ func (c *WatchClient) extractDaemonSetLabelsAnnotations() bool {
 	return false
 }
 
+func (c *WatchClient) extractReplicaSetLabelsAnnotations() bool {
+	for _, r := range c.Rules.Labels {
+		if r.From == MetadataFromReplicaSet {
+			return true
+		}
+	}
+
+	for _, r := range c.Rules.Annotations {
+		if r.From == MetadataFromReplicaSet {
+			return true
+		}
+	}
+
+	return false
+}
+
 func (c *WatchClient) extractJobLabelsAnnotations() bool {
 	for _, r := range c.Rules.Labels {
 		if r.From == MetadataFromJob {
@@ -2072,6 +2104,8 @@ func (c *WatchClient) addOrUpdateReplicaSet(replicaSet *meta_v1.PartialObjectMet
 			break
 		}
 	}
+
+	newReplicaSet.Attributes = c.extractReplicaSetAttributes(replicaSet)
 
 	c.m.Lock()
 	if uid != "" {
