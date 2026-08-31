6f1782e56b5d9d235f42cee6896afc7d0cc2f295
Roger Coll <rogercoll@protonmail.com>
2026-08-31 09:35:22 +0200

    fix import paths

--- exporter/elasticsearchexporter/internal/serializer/otelserializer/profile_test.go
+++ exporter/elasticsearchexporter/internal/serializer/otelserializer/profile_test.go
@@ -15,7 +15,7 @@ import (
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/pprofile"
 
-	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/elasticsearchexporter/internal/serializer/otelserializer/serializeprofiles"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/elasticsearchexporter/internal/serializer/ecsserializer/serializeprofiles"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/pdatatest/pprofiletest"
 )
 
