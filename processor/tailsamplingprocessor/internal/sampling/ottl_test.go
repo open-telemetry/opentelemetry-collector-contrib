e16d0d24d3a8dcd90637647a677be040a8f66667
zanarelli <zanarelli.dev@gmail.com>
2026-09-03 18:12:13 -0300

    style(processor/tailsampling): fix gci import ordering in metadata and sampling tests
    Signed-off-by: zanarelli <zanarelli.dev@gmail.com>

--- processor/tailsamplingprocessor/internal/sampling/ottl_test.go
+++ processor/tailsamplingprocessor/internal/sampling/ottl_test.go
@@ -9,10 +9,9 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component/componenttest"
+	"go.opentelemetry.io/collector/featuregate"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/ptrace"
-
-	"go.opentelemetry.io/collector/featuregate"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/tailsamplingprocessor/internal/metadata"
