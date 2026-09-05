adc86c0002493bd3afa7c81c182668f7ed7155f6
Antoine Toulme <atoulme@splunk.com>
2026-09-04 18:58:24 -0700

    [all] fix panics when running with AIX

--- extension/tailstorage/pebbletailstorageextension/factory_aix.go
+++ extension/tailstorage/pebbletailstorageextension/factory_aix.go
@@ -5,8 +5,27 @@
 
 package pebbletailstorageextension // import "github.com/open-telemetry/opentelemetry-collector-contrib/extension/tailstorage/pebbletailstorageextension"
 
-import "go.opentelemetry.io/collector/extension"
+import (
+	"context"
+	"errors"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/extension"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/tailstorage/pebbletailstorageextension/internal/metadata"
+)
 
 func NewFactory() extension.Factory {
-	panic("aix is not supported")
+	return extension.NewFactory(
+		metadata.Type,
+		func() component.Config {
+			return nil
+		},
+		createAix,
+		metadata.ExtensionStability,
+	)
+}
+
+func createAix(context.Context, extension.Settings, component.Config) (extension.Extension, error) {
+	return nil, errors.New("pebbletailstorageextension is not supported on aix")
 }
--- receiver/pulsarreceiver/factory_aix.go
+++ receiver/pulsarreceiver/factory_aix.go
@@ -5,10 +5,33 @@
 
 package pulsarreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/pulsarreceiver"
 import (
+	"context"
+	"errors"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/receiver"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/pulsarreceiver/internal/metadata"
 )
 
 // NewFactory creates Pulsar exporter factory.
 func NewFactory() receiver.Factory {
-	panic("AIX is not supported")
+	return receiver.NewFactory(metadata.Type, func() component.Config {
+		return nil
+	}, receiver.WithLogs(createLogs, metadata.LogsStability),
+		receiver.WithMetrics(createMetrics, metadata.MetricsStability),
+		receiver.WithTraces(createTraces, metadata.TracesStability))
+}
+
+func createTraces(context.Context, receiver.Settings, component.Config, consumer.Traces) (receiver.Traces, error) {
+	return nil, errors.New("pulsarreceiver is not supported on AIX")
+}
+
+func createMetrics(context.Context, receiver.Settings, component.Config, consumer.Metrics) (receiver.Metrics, error) {
+	return nil, errors.New("pulsarreceiver is not supported on AIX")
+}
+
+func createLogs(context.Context, receiver.Settings, component.Config, consumer.Logs) (receiver.Logs, error) {
+	return nil, errors.New("pulsarreceiver is not supported on AIX")
 }
