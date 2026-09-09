d60ffb335df70f95bd11701fee2d8d6d2815cbe2
TylerHelmuth <12352919+TylerHelmuth@users.noreply.github.com>
2026-09-09 16:56:31 -0600

    remove code

--- pkg/ottl/internal/metadata/generated_feature_gates.go
+++ pkg/ottl/internal/metadata/generated_feature_gates.go
@@ -12,7 +12,7 @@ var OttlPanicDuplicateNameFeatureGate = featuregate.GlobalRegistry().MustRegiste
 	featuregate.WithRegisterDescription("When enabled, the CreateFactoryMap panics if the name is duplicated."),
 	featuregate.WithRegisterReferenceURL("https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/44630"),
 	featuregate.WithRegisterFromVersion("v0.141.0"),
-	featuregate.WithRegisterToVersion("v0.160.0"),
+	featuregate.WithRegisterToVersion("v0.161.0"),
 )
 
 var OttlContextsEnableOTelColContextFeatureGate = featuregate.GlobalRegistry().MustRegister(
