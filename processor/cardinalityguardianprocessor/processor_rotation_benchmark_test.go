c9231b3be69d8cc847bb3821aa657267c034c76c
Sean Marciniak <smarciniak@splunk.com>
2026-09-17 14:44:13 +0930

    Adding changes to include density check in benchmark

--- processor/cardinalityguardianprocessor/processor_rotation_benchmark_test.go
+++ processor/cardinalityguardianprocessor/processor_rotation_benchmark_test.go
@@ -4,9 +4,13 @@
 package cardinalityguardianprocessor
 
 import (
+	"encoding/binary"
 	"fmt"
 	"testing"
 
+	"github.com/axiomhq/hyperloglog"
+	"github.com/cespare/xxhash/v2"
+	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer/consumertest"
 	"go.opentelemetry.io/collector/processor/processortest"
@@ -52,12 +56,12 @@ func benchmarkCardinalityProcessorRotate(b *testing.B, dense bool) {
 	for i := range trackers {
 		tracker := newTracker()
 		for value := range valuesPerSketch {
-			hashValue := uint64(i)<<32 | uint64(value)
+			hashValue := rotationBenchmarkHash(i, value)
 			tracker.current.InsertHash(hashValue)
-			if dense {
-				tracker.previous.InsertHash(hashValue)
-			}
+			tracker.previous.InsertHash(hashValue)
 		}
+		verifyRotationBenchmarkSketch(b, tracker.current, dense)
+		verifyRotationBenchmarkSketch(b, tracker.previous, dense)
 
 		key := trackerKey{
 			metricName: fmt.Sprintf("rotation_benchmark_metric_%d", i),
@@ -82,9 +86,60 @@ func benchmarkCardinalityProcessorRotate(b *testing.B, dense bool) {
 		// benchmark after two iterations.
 		b.StopTimer()
 		for trackerIndex, tracker := range trackers {
-			tracker.insert(uint64(i) + uint64(trackerIndex))
+			tracker.insert(rotationBenchmarkHash(i, trackerIndex))
 		}
 		b.StartTimer()
 		p.rotate()
+	}
+}
+
+// rotationBenchmarkHash matches the processor's use of InsertHash: the input
+// must be uniformly distributed, rather than a sequential counter.
+func rotationBenchmarkHash(a, b int) uint64 {
+	var input [16]byte
+	binary.LittleEndian.PutUint64(input[:8], uint64(a))
+	binary.LittleEndian.PutUint64(input[8:], uint64(b))
+	return xxhash.Sum64(input[:])
+}
+
+func verifyRotationBenchmarkSketch(tb testing.TB, sketch *hyperloglog.Sketch, wantDense bool) {
+	tb.Helper()
+	data, err := sketch.MarshalBinary()
+	require.NoError(tb, err, "marshal benchmark sketch")
+	require.GreaterOrEqual(tb, len(data), 4, "marshal benchmark sketch returned too few bytes")
+
+	wantRepresentation := byte(1) // sparse
+	if wantDense {
+		wantRepresentation = 0 // dense
+	}
+	require.Equal(tb, wantRepresentation, data[3], "benchmark sketch representation")
+	if wantDense {
+		require.Equal(tb, 8+(1<<14), len(data), "dense benchmark sketch marshal size")
+	}
+}
+
+func TestRotationBenchmarkSketchMarshal(t *testing.T) {
+	for _, test := range []struct {
+		name            string
+		valuesPerSketch int
+		wantDense       bool
+	}{
+		{name: "sparse", valuesPerSketch: 32},
+		{name: "dense", valuesPerSketch: 20000, wantDense: true},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			sketch := hyperloglog.New14()
+			for value := range test.valuesPerSketch {
+				sketch.InsertHash(rotationBenchmarkHash(0, value))
+			}
+
+			verifyRotationBenchmarkSketch(t, sketch, test.wantDense)
+			data, err := sketch.MarshalBinary()
+			require.NoError(t, err)
+
+			var roundTrip hyperloglog.Sketch
+			require.NoError(t, roundTrip.UnmarshalBinary(data))
+			require.Equal(t, sketch.Estimate(), roundTrip.Estimate())
+		})
 	}
 }
