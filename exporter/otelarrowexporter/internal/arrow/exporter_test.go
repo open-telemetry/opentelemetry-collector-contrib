d37722d128c907594f9e2a9c9542719d2ec5b928
Murphy Chen <minquan.chen@daocloud.io>
2026-09-17 10:37:08 +0800

    Fix flaky test in otelarrowexporter

--- exporter/otelarrowexporter/internal/arrow/exporter_test.go
+++ exporter/otelarrowexporter/internal/arrow/exporter_test.go
@@ -623,13 +623,13 @@ func TestArrowExporterHeaders(t *testing.T) {
 				}
 
 				sendCtx := ctx
+				var sendCancel context.CancelFunc
 				if withDeadline {
-					var sendCancel context.CancelFunc
-					sendCtx, sendCancel = context.WithTimeout(sendCtx, 1*time.Second)
-					defer sendCancel()
+					sendCtx, sendCancel = context.WithTimeout(sendCtx, 10*time.Second)
 				}
 
 				sent, err := tc.exporter.SendAndWait(sendCtx, input)
+				sendCancel()
 				require.NoError(t, err)
 				require.True(t, sent)
 			}
