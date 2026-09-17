98f0355bff1c7e4278b22831c6d3537803587361
Alex Boten <223565+codeboten@users.noreply.github.com>
2026-09-17 08:17:34 -0700

    [receiver/jaeger] remove use of deprecated config in test (#51136)
    #### Description
    
    Removes unnecessary server configuration in tests. Part of #49316
    
    #### Authorship
    
    - [x] I, a human, wrote this pull request description myself.
    
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>

--- receiver/jaegerreceiver/trace_receiver_test.go
+++ receiver/jaegerreceiver/trace_receiver_test.go
@@ -120,11 +120,6 @@ func TestReception(t *testing.T) {
 	addr := testutil.GetAvailableLocalAddress(t)
 	// 1. Create the Jaeger receiver aka "server"
 	thriftHTTPServerConfig := confighttp.NewDefaultServerConfig()
-	// TODO: See https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/49316.
-	thriftHTTPServerConfig.WriteTimeout = 0
-	thriftHTTPServerConfig.ReadHeaderTimeout = 0
-	thriftHTTPServerConfig.IdleTimeout = 0           //nolint:staticcheck // SA1019: see TODO above
-	thriftHTTPServerConfig.KeepAlivesEnabled = false //nolint:staticcheck // SA1019: see TODO above
 	thriftHTTPServerConfig.NetAddr = confignet.AddrConfig{
 		Endpoint:  addr,
 		Transport: confignet.TransportTypeTCP,
