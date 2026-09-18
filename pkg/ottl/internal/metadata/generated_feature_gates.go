94988e3c4d1189910a8adc075a0f31dceb2a2ad2
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2026-09-18 13:24:29 -0600

    remove ottl.set.allowNil feature gate

--- pkg/ottl/internal/metadata/generated_feature_gates.go
+++ pkg/ottl/internal/metadata/generated_feature_gates.go
@@ -13,12 +13,3 @@ var OttlFunctionsEnableLambdaFeatureGate = featuregate.GlobalRegistry().MustRegi
 	featuregate.WithRegisterReferenceURL("https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/48227"),
 	featuregate.WithRegisterFromVersion("v0.155.0"),
 )
-
-var OttlSetAllowNilFeatureGate = featuregate.GlobalRegistry().MustRegister(
-	"ottl.set.allowNil",
-	featuregate.StageStable,
-	featuregate.WithRegisterDescription("When enabled, the set function passes nil values directly to the target."),
-	featuregate.WithRegisterReferenceURL("https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/49741"),
-	featuregate.WithRegisterFromVersion("v0.158.0"),
-	featuregate.WithRegisterToVersion("v1.0.0"),
-)
