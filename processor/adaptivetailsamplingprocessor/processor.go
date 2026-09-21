351efaf95d9a2be9487b20ef9b9bf20174181b46
Yingrong Zhao <22300958+VinozzZ@users.noreply.github.com>
2026-09-21 12:17:39 -0400

    [processor/adaptivetailsampling] default max_keys to 500 (#50594)
    #### Description
    
    Use high cardinality attributes as `fingerprint_attributes` can d
    egrades goal accuracy directly. An adaptive sampler can never drop a
    key's rate below 1 (it must always keep at least one instance of any key
    it's tracking), so as the number of distinct keys grows, more o
    f them sit at that rate-1 floor with only a handful of eve
    nts each, and the sampler can't discard enough traffic acr
    oss the keyspace to hit the configured `goal_percentage`/`
    goal_throughput`. Keeping the key space small is what make
    s the adaptive samplers effective.
    Setting a default value for max_key config in samplers that support this
    option.
    
    #### Link to tracking issue
    Fixes #50538
    
    #### Testing
    
    Through load testing, it shows that high cardinality attributes can
    cause unstable sampling results.
    
    #### Documentation
    documentation in readme changed to document the new default value for
    `max_keys`
    
    #### Authorship
    
    - [x] I, a human, wrote this pull request description myself.
    
    ---------
    
    Co-authored-by: Mike Goldsmith <goldsmith.mike@gmail.com>

--- processor/adaptivetailsamplingprocessor/processor.go
+++ processor/adaptivetailsamplingprocessor/processor.go
@@ -255,7 +255,7 @@ func newSamplerForRule(rc *RuleConfig) (sampler.Sampler, []sampler.Selector, err
 			GoalSamplingPercentage: sc.GoalPercentage,
 			AdjustmentInterval:     sc.AdjustmentInterval,
 			Weight:                 sc.Weight,
-			MaxKeys:                sc.MaxKeys,
+			MaxKeys:                sc.effectiveMaxKeys(),
 		})
 		if err != nil {
 			return nil, nil, err
@@ -279,7 +279,7 @@ func newSamplerForRule(rc *RuleConfig) (sampler.Sampler, []sampler.Selector, err
 				InitialSamplingRate:  initialRate,
 				UpdateFrequency:      sc.UpdateFrequency,
 				LookbackFrequency:    sc.LookbackFrequency,
-				MaxKeys:              sc.MaxKeys,
+				MaxKeys:              sc.effectiveMaxKeys(),
 			})
 		} else {
 			s, err = sampler.NewEMAThroughput(sampler.EMAThroughputConfig{
@@ -287,7 +287,7 @@ func newSamplerForRule(rc *RuleConfig) (sampler.Sampler, []sampler.Selector, err
 				InitialSamplingRate:  initialRate,
 				AdjustmentInterval:   sc.AdjustmentInterval,
 				Weight:               sc.Weight,
-				MaxKeys:              sc.MaxKeys,
+				MaxKeys:              sc.effectiveMaxKeys(),
 			})
 		}
 		if err != nil {
